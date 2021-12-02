35d37ae3b0615b22d8fea251b63944227e6c30e9
Glenn Wedin <glenn.wedin@nav.no>
2021-12-02 13:07:47 +0100

    react-router

--- apps/etterlatte-saksbehandling-ui/client/src/App.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/App.tsx
@@ -1,39 +1,56 @@
 import React, { useContext, useEffect } from "react";
 import { Grid, Cell } from "@navikt/ds-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@navikt/ds-css";
 import "./App.css";
 import { AppContext, IAppContext } from "./store/AppContext";
 //import { login } from "./shared/api/user";
 import { Decorator } from "./components/decorator";
 import { ws } from "./mocks/wsmock";
+import { Container } from "./shared/styled";
 
-ws()
+ws();
 
 function App() {
-    const ctx = useContext<IAppContext>(AppContext);   
+    const ctx = useContext<IAppContext>(AppContext);
     useEffect(() => {
         (async () => {
             //const res = await login();
             //console.log(res);
-            ctx.dispatch({type: "login", data: {loggedIn: true, fnr: "09038829766"} })
+            ctx.dispatch({ type: "login", data: { loggedIn: true, fnr: "09038829766" } });
         })();
     }, [ctx]);
-
 
     return (
         <div className="app">
             {ctx.state.userReducer.loggedIn ? (
                 <>
                     <Decorator />
-                    <div className="container">
-                        <Grid>
-                            <Cell className={"navds-story-cell"} xs={12} sm={6} lg={4}>
-                                <h1>De etterlatte</h1>
-                            </Cell>
-                        </Grid>
-                    </div>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={
+                                    // dras ut i egen component
+                                    <Container>
+                                        <Grid>
+                                            <Cell className={"navds-story-cell"} xs={12} sm={6} lg={4}>
+                                                <h1>De etterlatte</h1>
+                                            </Cell>
+                                        </Grid>
+                                    </Container>
+                                }
+                            />
+                            <Route
+                                path="/side"
+                                element={
+                                    <div>en annen side</div>
+                                }
+                            />
+                        </Routes>
+                    </BrowserRouter>
                 </>
-            ): (
+            ) : (
                 <div>Ikke logget inn</div>
             )}
         </div>
---
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/styled.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  max-width: 60em;
+  margin: 0 auto;
+`;

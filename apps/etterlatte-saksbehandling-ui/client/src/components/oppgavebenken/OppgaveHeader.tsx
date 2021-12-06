92027271328d1d339a2b13f4553b5df94bf3b7f4
kristinaskatun <kristinaskatun@gmail.com>
2021-12-06 11:06:55 +0100

    Legg til editorconfig i ui

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/OppgaveHeader.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/OppgaveHeader.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import {Heading} from "@navikt/ds-react";
-
+import { Heading } from "@navikt/ds-react";
 
 
 const OppgaveHeader = () => {
 
     return (
-            <Heading size={"medium"} spacing>Oppgavebenken</Heading>
+        <Heading size={"medium"} spacing>Oppgavebenken</Heading>
     )
 }
 
-export default  OppgaveHeader;
+export default OppgaveHeader;

9106fd53cec6a4d11e25f3be7d8c555d5dd9b184
Glenn Wedin <glenn.wedin@nav.no>
2022-01-17 12:55:40 +0100

    sjekker output

--- apps/etterlatte-api/src/main/kotlin/behandling/BehandlingKlient.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/BehandlingKlient.kt
@@ -21,7 +21,7 @@ class BehandlingKlient(config: Config) : EtterlatteBehandling {
     private val downstreamResourceClient = DownstreamResourceClient(azureAdClient)
 
     @Suppress("UNCHECKED_CAST")
-    override suspend fun hentPerson(fnr: String, accessToken: String): List<Sak> {
+    override suspend fun hentPerson(fnr: String, accessToken: String): String {
         val objectMapper = jacksonObjectMapper()
         try {
             logger.info("Henter saker fra behandling")
@@ -32,7 +32,7 @@ class BehandlingKlient(config: Config) : EtterlatteBehandling {
                 .get(Resource(conf.getString("behandling.client.id"), conf.getString("behandling.resource.url")+"/personer/{fnr}/saker"), accessToken).mapBoth(
                     success = { json -> json },
                     failure = { throwableErrorMessage -> throw Error(throwableErrorMessage.message) }
-                ).response as List<Sak>
+                ).response as String
         } catch (e: Exception) {
             logger.error("Henting av person fra behandling feilet", e)
             throw e

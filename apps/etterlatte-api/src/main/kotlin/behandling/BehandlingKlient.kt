4340500827551505c2a93347f93ee54074d0d261
Glenn Wedin <glenn.wedin@nav.no>
2022-01-17 13:26:39 +0100

    parsing av data

--- apps/etterlatte-api/src/main/kotlin/behandling/BehandlingKlient.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/BehandlingKlient.kt
@@ -13,6 +13,8 @@ interface EtterlatteBehandling {
     suspend fun hentPerson(fnr: String, accessToken: String): Any?
 }
 
+data class BehandlingPersonResult (val saker: List<Sak>)
+
 class BehandlingKlient(config: Config) : EtterlatteBehandling {
     private val logger = LoggerFactory.getLogger(BehandlingKlient::class.java)
 
@@ -21,18 +23,22 @@ class BehandlingKlient(config: Config) : EtterlatteBehandling {
     private val downstreamResourceClient = DownstreamResourceClient(azureAdClient)
 
     @Suppress("UNCHECKED_CAST")
-    override suspend fun hentPerson(fnr: String, accessToken: String): String {
-        //val objectMapper = jacksonObjectMapper()
+    override suspend fun hentPerson(fnr: String, accessToken: String): BehandlingPersonResult {
+        val objectMapper = jacksonObjectMapper()
         try {
             logger.info("Henter saker fra behandling")
-            val response = downstreamResourceClient
-                .get(Resource(conf.getString("behandling.client.id"), conf.getString("behandling.resource.url")+"/personer/{fnr}/saker"), accessToken).mapBoth(
+            val json = downstreamResourceClient
+                .get(
+                    Resource(
+                        conf.getString("behandling.client.id"),
+                        conf.getString("behandling.resource.url") + "/personer/{fnr}/saker"
+                    ), accessToken
+                ).mapBoth(
                     success = { json -> json },
                     failure = { throwableErrorMessage -> throw Error(throwableErrorMessage.message) }
                 ).response
 
-            println(response)
-            return "ok";
+            return objectMapper.readValue(json.toString(), BehandlingPersonResult::class.java)
         } catch (e: Exception) {
             logger.error("Henting av person fra behandling feilet", e)
             throw e
--- apps/etterlatte-api/src/main/kotlin/behandling/BehandlingService.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/BehandlingService.kt
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory
 class BehandlingService(private val klient: BehandlingKlient) {
     private val logger = LoggerFactory.getLogger(BehandlingService::class.java)
 
-    suspend fun hentPerson(fnr: String, accessToken: String): String {
+    suspend fun hentPerson(fnr: String, accessToken: String): BehandlingPersonResult {
         logger.info("Henter person fra behandling")
         return klient.hentPerson(fnr, accessToken)
     }

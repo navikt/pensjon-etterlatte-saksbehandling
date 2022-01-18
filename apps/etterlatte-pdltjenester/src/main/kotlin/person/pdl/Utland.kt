7f316907290adbce4128322a66a847f5b4adb6c2
jorgesti <jorgen.stikbakke@nav.no>
2022-01-18 13:50:05 +0100

    Lagt til dataklasse for utland

---
+++ apps/etterlatte-pdltjenester/src/main/kotlin/person/pdl/Utland.kt
@@ -0,0 +1,63 @@
+package person.pdl
+
+data class Utland(
+    val `data`: Data
+)
+data class Data(
+    val hentPerson: HentPerson
+)
+
+data class Statsborgerskap(
+    val gyldigFraOgMed: String,
+    val gyldigTilOgMed: Any,
+    val land: String,
+    val metadata: MetadataX
+)
+
+data class HentPerson(
+    val innflyttingTilNorge: List<InnflyttingTilNorge>,
+    val statsborgerskap: List<Statsborgerskap>,
+    val utflyttingFraNorge: List<Any>
+)
+
+data class InnflyttingTilNorge(
+    val folkeregistermetadata: Folkeregistermetadata,
+    val fraflyttingsland: String,
+    val fraflyttingsstedIUtlandet: Any,
+    val metadata: Metadata
+)
+
+data class Folkeregistermetadata(
+    val aarsak: Any,
+    val ajourholdstidspunkt: String,
+    val gyldighetstidspunkt: String,
+    val kilde: String,
+    val opphoerstidspunkt: Any,
+    val sekvens: Any
+)
+
+data class Endringer(
+    val kilde: String,
+    val registrert: String,
+    val registrertAv: String,
+    val systemkilde: String,
+    val type: String
+)
+
+data class EndringerX(
+    val registrert: String,
+    val type: String
+)
+
+data class Metadata(
+    val endringer: List<Endringer>,
+    val historisk: Boolean,
+    val master: String,
+    val opplysningsId: String
+)
+
+data class MetadataX(
+    val endringer: List<EndringerX>,
+    val master: String,
+    val opplysningsId: String
+)

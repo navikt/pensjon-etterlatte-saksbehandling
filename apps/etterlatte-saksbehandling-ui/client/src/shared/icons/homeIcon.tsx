dc81fd8b8f6782140ed91a2e553a39808e1a5e44
Marie Fremstad <Marie.Fremstad@nav.no>
2022-01-24 09:36:31 +0100

    Iconer for personopplysninger

---
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/icons/homeIcon.tsx
@@ -0,0 +1,20 @@
+export const HomeIcon = () => {
+  return (
+    <svg
+      width="20px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      focusable="false"
+      role="img"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 0l10 10v14h-9v-6h-2v6H2V10L12 0zm0 2.829l-8 7.999V22h5v-6h6v6h5V10.829l-8-8z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  )
+}

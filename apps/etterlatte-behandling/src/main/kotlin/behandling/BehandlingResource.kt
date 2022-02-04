02f2bc4f9dca353619e033d9f115d2c5b1a11f46
Glenn Wedin <glenn.wedin@nav.no>
2022-02-04 09:26:09 +0100

    cleanup

--- apps/etterlatte-behandling/src/main/kotlin/behandling/BehandlingResource.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/BehandlingResource.kt
@@ -3,19 +3,6 @@ package no.nav.etterlatte.behandling
 import com.fasterxml.jackson.databind.node.ObjectNode
 import no.nav.etterlatte.libs.common.behandling.Behandlingsopplysning
 
-/*
-data class BehandlingSammendragListe(val behandlinger: List<BehandlingSammendrag>)
-data class BehandlingSammendrag(val id: UUID, val sak: Long, val status: BehandlingStatus)
-data class DetaljertBehandling(
-    val id: UUID,
-    val sak: Long,
-    val grunnlag: List<Behandlingsopplysning>,
-    val vilkårsprøving: Vilkårsprøving?,
-    val beregning: Beregning?,
-    val fastsatt: Boolean = false
-)*/
-
-
 data class BehandlingsBehov(
     val sak: Long,
     val opplysninger: List<Behandlingsopplysning<ObjectNode>>?

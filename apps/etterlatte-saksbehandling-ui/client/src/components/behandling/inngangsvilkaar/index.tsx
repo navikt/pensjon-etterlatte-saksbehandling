6ee3df8452cde8ef7bf0a904eb99ea0ccf9cab2d
kristinaskatun <kristinaskatun@gmail.com>
2022-02-11 14:01:09 +0100

    Mock behandlingsopplysninger i frontend

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/inngangsvilkaar/index.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/inngangsvilkaar/index.tsx
@@ -1,31 +1,33 @@
-import styled from "styled-components";
-import { Content } from "../../../shared/styled";
-import { Status, VilkaarStatus, VilkaarType } from "./types";
-import { Vilkaar } from "./vilkaar";
+import styled from 'styled-components'
+import { Content } from '../../../shared/styled'
+import { Status, VilkaarStatus, VilkaarType } from './types'
+import { Vilkaar } from './vilkaar'
+import Doedsdato from './Doedsdato'
 
 export const Inngangsvilkaar = () => {
-    return (
-        <Content>
-            <VilkaarListe>
-                <Vilkaar
-                    vilkaar={{
-                        vilkaarDone: Status.DONE,
-                        vilkaarType: VilkaarType.doedsdato,
-                        vilkaarStatus: VilkaarStatus.OPPFYLT,
-                    }}
-                />
-                <Vilkaar
-                    vilkaar={{
-                        vilkaarDone: Status.NOT_DONE,
-                        vilkaarType: VilkaarType.alderBarn,
-                        vilkaarStatus: VilkaarStatus.IKKE_OPPFYLT,
-                    }}
-                />
-            </VilkaarListe>
-        </Content>
-    );
-};
+  return (
+    <Content>
+      <VilkaarListe>
+        <Doedsdato />
+        <Vilkaar
+          vilkaar={{
+            vilkaarDone: Status.DONE,
+            vilkaarType: VilkaarType.doedsdato,
+            vilkaarStatus: VilkaarStatus.OPPFYLT,
+          }}
+        />
+        <Vilkaar
+          vilkaar={{
+            vilkaarDone: Status.NOT_DONE,
+            vilkaarType: VilkaarType.soeker_foedselsdato,
+            vilkaarStatus: VilkaarStatus.IKKE_OPPFYLT,
+          }}
+        />
+      </VilkaarListe>
+    </Content>
+  )
+}
 
 const VilkaarListe = styled.div`
-    padding: 0 2em;
-`;
+  padding: 0 2em;
+`

0880735f7219e6919ce41e07ebf36ac39aa4f3e8
kristinaskatun <kristinaskatun@gmail.com>
2022-02-15 13:05:02 +0100

    Legg til doedsdato er registert i vilkår

--- apps/etterlatte-vilkaar/src/main/kotlin/barnepensjon/BarnepensjonVilkaar.kt
+++ apps/etterlatte-vilkaar/src/main/kotlin/barnepensjon/BarnepensjonVilkaar.kt
@@ -5,56 +5,58 @@ import no.nav.etterlatte.libs.common.behandling.opplysningstyper.Foedselsdato
 import no.nav.etterlatte.libs.common.behandling.opplysningstyper.Foreldre
 import no.nav.etterlatte.libs.common.vikaar.VilkaarOpplysning
 import no.nav.etterlatte.vilkaar.model.*
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
 
 
 fun brukerErUnder20(
     foedselsdato: List<VilkaarOpplysning<Foedselsdato>>,
-    doedsdato: List<VilkaarOpplysning<Doedsdato>>
+    doedsdato: List<VilkaarOpplysning<Doedsdato>>,
 ): VurdertVilkaar {
-    val foedselsdatoPdl = foedselsdato.find { it.kilde.type == "pdl" }
-    val doedsdatoPdl = doedsdato.find { it.kilde.type == "pdl" }
-
-    val vilkaarResultat = if (foedselsdatoPdl == null || doedsdatoPdl == null) {
-        VilkaarVurderingsResultat.KAN_IKKE_VURDERE_PGA_MANGLENDE_OPPLYSNING
-    } else {
-        val foersteIMndEtterDoedsdato =
-            doedsdatoPdl.opplysning.doedsdato.minusDays(doedsdatoPdl.opplysning.doedsdato.dayOfMonth.toLong() - 1)
-                .plusMonths(1)
-        if (foedselsdatoPdl.opplysning.foedselsdato.plusYears(20) > foersteIMndEtterDoedsdato) {
-            VilkaarVurderingsResultat.IKKE_OPPFYLT
-        } else {
-            VilkaarVurderingsResultat.OPPFYLT
-        }
-    }
-
-    return VurdertVilkaar("brukerErUnder20", vilkaarResultat, listOf(foedselsdato, doedsdato).flatten())
+    return VurdertVilkaar(
+        "brukerErUnder20",
+        vurderOpplysning { hentSoekerFoedselsdato(foedselsdato).plusYears(20) < hentVirkningsdato(doedsdato) },
+        listOf(foedselsdato, doedsdato).flatten()
+    )
 }
-
 
 fun doedsfallErRegistrert(
     doedsdato: List<VilkaarOpplysning<Doedsdato>>,
-    foreldre: List<VilkaarOpplysning<Foreldre>>
+    foreldre: List<VilkaarOpplysning<Foreldre>>,
 ): VurdertVilkaar {
-    val doedsdatoPdl = doedsdato.find { it.kilde.type == "pdl" }
-    val foreldrePdl = foreldre.find { it.kilde.type == "pdl" }
-
-    val vilkaarResultat = if (doedsdatoPdl == null) {
-        VilkaarVurderingsResultat.IKKE_OPPFYLT
-    } else if (foreldrePdl == null) {
-        VilkaarVurderingsResultat.KAN_IKKE_VURDERE_PGA_MANGLENDE_OPPLYSNING
-    } else {
-        val foreldreFnr = foreldrePdl.opplysning.foreldre?.map { it.foedselsnummer.value }
-        val avdoedErForelder = foreldreFnr?.contains(doedsdatoPdl.opplysning.foedselsnummer)
-
-        if (avdoedErForelder!!) {
-            VilkaarVurderingsResultat.OPPFYLT
-        } else {
-            VilkaarVurderingsResultat.IKKE_OPPFYLT
-        }
-    }
-
-    return VurdertVilkaar("doedsdatoErRegistrert", vilkaarResultat, listOf(doedsdato, foreldre).flatten())
-
+    return VurdertVilkaar("doedsdatoErRegistrert",
+        vurderOpplysning { hentFnrForeldre(foreldre).contains(hentDoedsdato(doedsdato).foedselsnummer)} ,
+        listOf(doedsdato, foreldre).flatten())
 }
+
+fun vurderOpplysning(vurdering: () -> Boolean) = try {
+    if (vurdering()) VilkaarVurderingsResultat.OPPFYLT else VilkaarVurderingsResultat.IKKE_OPPFYLT
+} catch (ex: KanIkkeVurderePgaManglendeOpplysning) {
+    VilkaarVurderingsResultat.KAN_IKKE_VURDERE_PGA_MANGLENDE_OPPLYSNING
+}
+
+fun hentFnrForeldre(foreldre: List<VilkaarOpplysning<Foreldre>>): List<String> {
+    return foreldre.find { it.kilde.type == "pdl" }?.opplysning?.foreldre?.map { it.foedselsnummer.value }
+        ?: throw KanIkkeVurderePgaManglendeOpplysning()
+}
+
+fun hentSoekerFoedselsdato(foedselsdato: List<VilkaarOpplysning<Foedselsdato>>): LocalDate {
+    return foedselsdato.find { it.kilde.type == "pdl" }?.opplysning?.foedselsdato
+        ?: throw KanIkkeVurderePgaManglendeOpplysning()
+}
+
+fun hentDoedsdato(doedsdato: List<VilkaarOpplysning<Doedsdato>>): Doedsdato {
+    return doedsdato.find { it.kilde.type == "pdl" }?.opplysning
+        ?: throw KanIkkeVurderePgaManglendeOpplysning()
+}
+
+fun hentVirkningsdato(doedsdato: List<VilkaarOpplysning<Doedsdato>>): LocalDate {
+    val doedsdato = doedsdato.find { it.kilde.type == "pdl" }?.opplysning?.doedsdato
+    return doedsdato?.with(TemporalAdjusters.firstDayOfNextMonth()) ?: throw KanIkkeVurderePgaManglendeOpplysning()
+}
+
+class KanIkkeVurderePgaManglendeOpplysning : IllegalStateException()
+
+
 
 
--- apps/etterlatte-vilkaar/src/main/kotlin/model/VilkaarService.kt
+++ apps/etterlatte-vilkaar/src/main/kotlin/model/VilkaarService.kt
@@ -7,6 +7,7 @@ import no.nav.etterlatte.libs.common.vikaar.VilkaarOpplysning
 import no.nav.etterlatte.libs.common.behandling.opplysningstyper.*
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.vilkaar.barnepensjon.brukerErUnder20
+import no.nav.etterlatte.vilkaar.barnepensjon.doedsfallErRegistrert
 
 
 class VilkaarService {
@@ -15,18 +16,20 @@ class VilkaarService {
         val soekerFoedselsdato =
             opplysninger.filter { it.opplysingType == Opplysningstyper.SOEKER_FOEDSELSDATO_V1.value }
                 .map { setOpplysningType<Foedselsdato>(it) }
-        val avdoedDoedsfall = opplysninger.filter { it.opplysingType == Opplysningstyper.AVDOED_DOEDSFALL_V1.value }
+
+        val avdoedDoedsdato = opplysninger.filter { it.opplysingType == Opplysningstyper.AVDOED_DOEDSFALL_V1.value }
             .map { setOpplysningType<Doedsdato>(it) }
 
-        brukerErUnder20(soekerFoedselsdato)
+        val soekerRelasjonForeldre =
+            opplysninger.filter { it.opplysingType == Opplysningstyper.SOEKER_RELASJON_FORELDRE_V1.value }
+                .map { setOpplysningType<Foreldre>(it) }
 
-
+        brukerErUnder20(soekerFoedselsdato, avdoedDoedsdato)
+        doedsfallErRegistrert(avdoedDoedsdato, soekerRelasjonForeldre)
 
     }
 
-
     companion object {
-
         inline fun <reified T> setOpplysningType(opplysning: VilkaarOpplysning<out Any>): VilkaarOpplysning<T> {
             return VilkaarOpplysning(
                 opplysning.opplysingType,
@@ -35,6 +38,4 @@ class VilkaarService {
             )
         }
     }
-
-
 }

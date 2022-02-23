bec28a6f1419ed02c5dc276487dbfc35de0a7714
jorgesti <jorgen.stikbakke@nav.no>
2022-02-23 11:27:36 +0100

    nytt navn til klienten

--- apps/etterlatte-fordeler/src/main/kotlin/pdl/PdlTjenesterKlient.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/pdl/PdlTjenesterKlient.kt
@@ -2,16 +2,9 @@ package no.nav.etterlatte.pdl
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.accept
-import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.request.post
-import io.ktor.client.utils.EmptyContent.contentType
-import io.ktor.http.ContentType
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.http.contentType
-import no.nav.etterlatte.libs.common.logging.X_CORRELATION_ID
-import no.nav.etterlatte.libs.common.logging.getCorrelationId
-import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import no.nav.etterlatte.libs.common.person.HentPersonRequest
 import no.nav.etterlatte.libs.common.person.Person
 import org.slf4j.LoggerFactory

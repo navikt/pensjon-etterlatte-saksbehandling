739904bc865010673ff38641118e9bbab264928c
Glenn Wedin <glenn.wedin@nav.no>
2022-02-28 15:31:28 +0100

    unused var

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/personopplysninger/index.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/personopplysninger/index.tsx
@@ -12,44 +12,42 @@ import { OpplysningsType } from '../inngangsvilkaar/types'
 import { KildeType } from '../../../store/reducers/BehandlingReducer'
 import { format } from 'date-fns'
 
-
-
-
 export const Personopplysninger = () => {
-  const ctx = useContext(AppContext);
+  const ctx = useContext(AppContext)
   const [personer, setPersoner] = useState<{ person: IPersonFraSak; foreldre: IPersonFraSak[] }>()
-  const [soeknadGyldigBegrunnelse, setSoeknadGyldigBegrunnelse] = useState("")
+  const [soeknadGyldigBegrunnelse, setSoeknadGyldigBegrunnelse] = useState('')
 
-  const grunnlag = ctx.state.behandlingReducer.grunnlag;
+  const grunnlag = ctx.state.behandlingReducer.grunnlag
 
-  const pdlPerson: any = grunnlag.find(g => g.opplysningType === OpplysningsType.soeker_personinfo && g.kilde.type === KildeType.pdl);
-  const privatPerson: any = grunnlag.find(g => g.opplysningType === OpplysningsType.soeker_personinfo && g.kilde.type === KildeType.privatperson);
-  const avdodPerson: any = grunnlag.find(g => g.opplysningType === OpplysningsType.avdoed_personinfo);
-  const mottattDato = grunnlag.find(g => g.opplysningType === OpplysningsType.soeknad_mottatt);
-  const sosken = grunnlag.find(g => g.opplysningType === OpplysningsType.relasjon_soksken);
-  const dodsfall = grunnlag.find(g => g.opplysningType === OpplysningsType.avdoed_doedsfall);
+  const pdlPerson: any = grunnlag.find(
+    (g) => g.opplysningType === OpplysningsType.soeker_personinfo && g.kilde.type === KildeType.pdl
+  )
+  const privatPerson: any = grunnlag.find(
+    (g) => g.opplysningType === OpplysningsType.soeker_personinfo && g.kilde.type === KildeType.privatperson
+  )
+  const avdodPerson: any = grunnlag.find((g) => g.opplysningType === OpplysningsType.avdoed_personinfo)
+  const mottattDato = grunnlag.find((g) => g.opplysningType === OpplysningsType.soeknad_mottatt)
+  const sosken = grunnlag.find((g) => g.opplysningType === OpplysningsType.relasjon_soksken)
+  const dodsfall = grunnlag.find((g) => g.opplysningType === OpplysningsType.avdoed_doedsfall)
   // const omsorg = grunnlag.find(g => g.opplysningType === OpplysningsType.omsorg);
-    
+
   useEffect(() => {
     //TODO: Henter info om barn og foreldre fra PDL, type IPersonFraRegister er det som trengs per dags dato fra sketchene.
-    hentPersonerMedRelasjon().then(
-      (personer: { person: IPersonFraSak; foreldre: IPersonFraSak[]}) => {
-        setPersoner(personer)
-      }
-    )
+    hentPersonerMedRelasjon().then((personer: { person: IPersonFraSak; foreldre: IPersonFraSak[] }) => {
+      setPersoner(personer)
+    })
   }, [])
-  
 
   // console.log(grunnlag);
-  console.log('her', sosken);
-  const soskenListe: IPersonFraSak[] = sosken?.opplysning.soesken.map((opplysning: any, i: number) => {
+  console.log('her', sosken)
+  const soskenListe: IPersonFraSak[] = sosken?.opplysning.soesken.map((opplysning: any) => {
     return {
       navn: `${opplysning.fornavn} ${opplysning.etternavn}`,
       personStatus: PersonStatus.ETTERLATT,
       rolle: RelatertPersonsRolle.BARN,
-      adressenavn: "annet",
-      fnr: "332"
-    };
+      adressenavn: 'annet',
+      fnr: '332',
+    }
   })
 
   return (
@@ -69,27 +67,39 @@ export const Personopplysninger = () => {
         <InfoWrapper>
           <DetailWrapper>
             <Detail size="small">Mottaker</Detail>
-            <Detail size="medium" className="detail">{privatPerson.opplysning.fornavn} {privatPerson.opplysning.etternavn} ({pdlPerson?.opplysning.fornavn} {})</Detail>
+            <Detail size="medium" className="detail">
+              {privatPerson.opplysning.fornavn} {privatPerson.opplysning.etternavn} ({pdlPerson?.opplysning.fornavn} {})
+            </Detail>
           </DetailWrapper>
           <DetailWrapper>
             <Detail size="small">Søknad mottatt</Detail>
-            <Detail size="medium" className="detail">{format(new Date(mottattDato?.opplysning.mottattDato), "dd.MM.yyyy")}</Detail>
+            <Detail size="medium" className="detail">
+              {format(new Date(mottattDato?.opplysning.mottattDato), 'dd.MM.yyyy')}
+            </Detail>
           </DetailWrapper>
           <DetailWrapper>
             <Detail size="small">Dato for dødsfall</Detail>
-            <Detail size="medium" className="detail">{format(new Date(dodsfall?.opplysning.doedsdato), "dd.MM.yyyy")}</Detail>
+            <Detail size="medium" className="detail">
+              {format(new Date(dodsfall?.opplysning.doedsdato), 'dd.MM.yyyy')}
+            </Detail>
           </DetailWrapper>
           <DetailWrapper>
             <Detail size="small">Avdøde</Detail>
-            <Detail size="medium" className="detail">{avdodPerson?.opplysning.fornavn} {avdodPerson?.opplysning.etternavn}</Detail>
+            <Detail size="medium" className="detail">
+              {avdodPerson?.opplysning.fornavn} {avdodPerson?.opplysning.etternavn}
+            </Detail>
           </DetailWrapper>
           <DetailWrapper>
             <Detail size="small">Søknad fremsatt av</Detail>
-            <Detail size="medium" className="detail">Gjenlevende mor</Detail>
+            <Detail size="medium" className="detail">
+              Gjenlevende mor
+            </Detail>
           </DetailWrapper>
           <DetailWrapper>
             <Detail size="small">Foreldreansvar</Detail>
-            <Detail size="medium" className="detail">Gjenlevende mor</Detail>
+            <Detail size="medium" className="detail">
+              Gjenlevende mor
+            </Detail>
           </DetailWrapper>
         </InfoWrapper>
 
@@ -101,29 +111,31 @@ export const Personopplysninger = () => {
             <PersonInfo person={personer.person} />
             {personer.foreldre.map((foreldre, key) => (
               <PersonInfo key={key} person={foreldre} />
-            ))}      
+            ))}
           </>
-            )}
+        )}
         {soskenListe?.map((person) => (
-            <PersonInfo key={`soesken_`} person={person} />
+          <PersonInfo key={`soesken_`} person={person} />
         ))}
         <RadioGroupWrapper>
-        <RadioGroup legend="Er søknaden gyldig fremsatt?" size="small" className="radioGroup">
-          <Radio value="10">Ja</Radio>
-          <Radio value="20">Nei</Radio>
-        </RadioGroup>
-        <Textarea
-          label="Begrunnelse (hvis aktuelt)"
-          value={soeknadGyldigBegrunnelse}
-          onChange={(e) => setSoeknadGyldigBegrunnelse(e.target.value)}
-          minRows={2}
-          maxLength={400}
-          size="small"
-        />
-        <Button variant="primary" size="medium" className="button">
-          Bekreft og gå videre
-        </Button>
-        <Link href="#" className="link">Avbryt og behandle i pesys</Link>
+          <RadioGroup legend="Er søknaden gyldig fremsatt?" size="small" className="radioGroup">
+            <Radio value="10">Ja</Radio>
+            <Radio value="20">Nei</Radio>
+          </RadioGroup>
+          <Textarea
+            label="Begrunnelse (hvis aktuelt)"
+            value={soeknadGyldigBegrunnelse}
+            onChange={(e) => setSoeknadGyldigBegrunnelse(e.target.value)}
+            minRows={2}
+            maxLength={400}
+            size="small"
+          />
+          <Button variant="primary" size="medium" className="button">
+            Bekreft og gå videre
+          </Button>
+          <Link href="#" className="link">
+            Avbryt og behandle i pesys
+          </Link>
         </RadioGroupWrapper>
       </ContentHeader>
     </Content>

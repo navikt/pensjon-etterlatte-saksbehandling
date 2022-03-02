9b17f40b28fa3791e5315245bcd44c502505f540
jaakobls <jakobls@gmail.com>
2022-03-02 16:34:23 +0100

    Implementert flere tester

--- apps/etterlatte-opplysninger-fra-pdl/src/test/kotlin/opplysninger/kilde/pdl/OpplysningsByggerServiceTest.kt
+++ apps/etterlatte-opplysninger-fra-pdl/src/test/kotlin/opplysninger/kilde/pdl/OpplysningsByggerServiceTest.kt
@@ -1,8 +1,10 @@
 package opplysninger.kilde.pdl
 
+import io.mockk.mockk
 import no.nav.etterlatte.libs.common.behandling.Behandlingsopplysning
 import no.nav.etterlatte.libs.common.behandling.opplysningstyper.Opplysningstyper
 import no.nav.etterlatte.libs.common.behandling.opplysningstyper.PersonInfo
+import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.person.Adresse
 import no.nav.etterlatte.libs.common.person.Adressebeskyttelse
 import no.nav.etterlatte.libs.common.person.FamilieRelasjon
@@ -10,12 +12,17 @@ import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import no.nav.etterlatte.libs.common.person.Person
 import no.nav.etterlatte.libs.common.person.Sivilstatus
 import no.nav.etterlatte.libs.common.person.Utland
+import no.nav.etterlatte.libs.common.soeknad.dataklasser.Barnepensjon
 import no.nav.etterlatte.libs.common.soeknad.dataklasser.common.PersonType
 import no.nav.etterlatte.opplysninger.kilde.pdl.OpplysningsByggerService
+import no.nav.etterlatte.opplysninger.kilde.pdl.lagOpplysning
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertThrows
+import java.io.FileNotFoundException
 import java.time.Instant
 
 import java.time.LocalDate
@@ -32,7 +39,10 @@ internal class OpplysningsByggerServiceTest {
         val avdoedPdlMock = mockPerson(GYLDIG_FNR_AVDOED, doedsdato = LocalDate.parse("2022-01-02"))
         val gjenlevendeForelderPdlMock = mockPerson(GYLDIG_FNR_GJENLEVENDE_FORELDER)
         val opplysningsByggerService = OpplysningsByggerService()
+        val komplettBarnepensjonssoknad = readSoknadAsBarnepensjon("/fullMessage2.json")
+        val ukomplettBarnepensjonssoknad = readSoknadAsBarnepensjon("/fullMessage5.json")
 
+        /* metode fra etterlatte-fordeler */
         fun mockPerson(
             fnr: String = "11057523044",
             foedselsaar: Int = 2010,
@@ -89,6 +99,17 @@ internal class OpplysningsByggerServiceTest {
 
          */
 
+
+        fun readSoknadAsBarnepensjon(file: String): Barnepensjon {
+            val skjemaInfo = objectMapper.writeValueAsString(
+                objectMapper.readTree(readFile(file)).get("@skjema_info")
+            )
+            return objectMapper.readValue(skjemaInfo, Barnepensjon::class.java)
+        }
+
+        fun readFile(file: String) = Companion::class.java.getResource(file)?.readText()
+            ?: throw FileNotFoundException("Fant ikke filen $file")
+
     }
 
     @Test
@@ -97,11 +118,9 @@ internal class OpplysningsByggerServiceTest {
     }
 
     @Test
-    fun `skal lage opplysning om gjenlevende forelder`() {
-        val opplysningGjenlevendeForelder = opplysningsByggerService.gjenlevendeForelderOpplysning(
-            gjenlevendeForelderPdlMock, Opplysningstyper.GJENLEVENDE_FORELDER_PERSONINFO_V1.value
-        )
-        opplysningGjenlevendeForelder.apply {
+    fun `lagOpplysning skal lag Behandlingsopplysning på rett format`() {
+        val behandlingsopplysning = lagOpplysning("opplysningstype", mockk<PersonInfo>())
+        behandlingsopplysning.apply {
             assertEquals(36, id.toString().length)
             assertEquals(Behandlingsopplysning.Pdl::class.java, kilde.javaClass)
             assertEquals(KILDE_PDL, (kilde as Behandlingsopplysning.Pdl).navn)
@@ -109,8 +128,18 @@ internal class OpplysningsByggerServiceTest {
                 (kilde as Behandlingsopplysning.Pdl).tidspunktForInnhenting in Instant.now()
                     .minusSeconds(10)..Instant.now().plusSeconds(1)
             )
-            assertEquals(Opplysningstyper.GJENLEVENDE_FORELDER_PERSONINFO_V1.value, opplysningType)
             assertTrue(meta.asText().isEmpty())
+            assertEquals(PersonInfo::class.java, opplysning.javaClass)
+        }
+    }
+
+    @Test
+    fun `skal lage opplysning om gjenlevende forelder`() {
+        val opplysningGjenlevendeForelder = opplysningsByggerService.gjenlevendeForelderOpplysning(
+            gjenlevendeForelderPdlMock, Opplysningstyper.GJENLEVENDE_FORELDER_PERSONINFO_V1.value
+        )
+        opplysningGjenlevendeForelder.apply {
+            assertEquals(Opplysningstyper.GJENLEVENDE_FORELDER_PERSONINFO_V1.value, opplysningType)
 
             assertEquals(PersonInfo::class.java, opplysning.javaClass)
             assertEquals(gjenlevendeForelderPdlMock.fornavn, opplysning.fornavn)
@@ -122,31 +151,73 @@ internal class OpplysningsByggerServiceTest {
     }
 
     @Test
-    fun personOpplysning() {
+    fun `skal lage personopplysning om avdød`() {
+        val personopplysningAvdoed =
+            opplysningsByggerService.personOpplysning(
+                avdoedPdlMock, Opplysningstyper.AVDOED_PERSONINFO_V1.value, PersonType.AVDOED
+            )
+
+        personopplysningAvdoed.apply {
+            assertEquals(Opplysningstyper.AVDOED_PERSONINFO_V1.value, opplysningType)
+
+            assertEquals(PersonInfo::class.java, opplysning.javaClass)
+            assertEquals(avdoedPdlMock.fornavn, opplysning.fornavn)
+            assertEquals(avdoedPdlMock.etternavn, opplysning.etternavn)
+            assertEquals(Foedselsnummer.of(GYLDIG_FNR_AVDOED), opplysning.foedselsnummer)
+            //assertEquals(gjenlevendeForelderPdlMock.adresse, opplysning.adresse)
+            assertEquals(PersonType.AVDOED, opplysning.type)
+        }
     }
 
     @Test
+    fun `skal lage personopplysning med rett persontype for søker`() {
+        val personopplysningSoeker =
+            opplysningsByggerService.personOpplysning(
+                soekerPdlMock, Opplysningstyper.SOEKER_PERSONINFO_V1.value, PersonType.BARN
+            )
+        personopplysningSoeker.apply {
+            assertEquals(PersonType.BARN, opplysning.type)
+        }
+    }
+
+    @Test
+    @Disabled
     fun avdoedDodsdato() {
+        val avdoedDoedsdato =
+            opplysningsByggerService.avdoedDodsdato(avdoedPdlMock, Opplysningstyper.AVDOED_DOEDSFALL_V1.value)
+
     }
 
     @Test
     fun soekerFoedselsdato() {
     }
 
+
     @Test
     fun soekerRelasjonForeldre() {
+
     }
 
     @Test
-    fun hentAvdoedFnr() {
+    fun `skal hente avdoedes foedselsnummer`() {
+        val avdoedesFnr = opplysningsByggerService.hentAvdoedFnr(komplettBarnepensjonssoknad)
+        assertEquals("22128202440", avdoedesFnr)
     }
 
     @Test
-    fun hentGjenlevendeForelderFnr() {
+    fun `skal kaste exception ved forsoek paa henting av avdoedes foedselsnummer`() {
+        assertThrows<Exception> { opplysningsByggerService.hentAvdoedFnr(ukomplettBarnepensjonssoknad) }
     }
 
     @Test
-    fun `skal lage Behandlingsopplysning om opplysningstype`() {
-
+    fun `skal hente gjenlevende forelders foedselsnummer`() {
+        val gjenlevendeForelderFnr = opplysningsByggerService.hentGjenlevendeForelderFnr(komplettBarnepensjonssoknad)
+        assertEquals("03108718357", gjenlevendeForelderFnr)
     }
+
+    @Test
+    fun `skal kaste exception ved forsoek paa henting av gjenlevendes foedselsnummer`() {
+        assertThrows<Exception> { opplysningsByggerService.hentGjenlevendeForelderFnr(ukomplettBarnepensjonssoknad) }
+    }
+
 }

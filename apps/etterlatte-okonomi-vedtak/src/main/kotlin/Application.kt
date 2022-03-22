af2ce54c73c6522bb574f9e114acde25284b2e2d
henrikg <henrik.gundersen@nav.no>
2022-03-22 13:45:11 +0100

    Henter username for service user

--- apps/etterlatte-okonomi-vedtak/src/main/kotlin/Application.kt
+++ apps/etterlatte-okonomi-vedtak/src/main/kotlin/Application.kt
@@ -3,15 +3,25 @@ package no.nav.etterlatte
 import no.nav.etterlatte.vedtaksoversetter.OppdragMapper
 import no.nav.etterlatte.vedtaksoversetter.Vedtaksoversetter
 import no.nav.helse.rapids_rivers.RapidApplication
+import org.slf4j.LoggerFactory
 
 fun main() {
+    val logger = LoggerFactory.getLogger(Vedtaksoversetter::class.java)
+
     val env = System.getenv().toMutableMap().apply {
         put("KAFKA_CONSUMER_GROUP_ID", requireNotNull(get("NAIS_APP_NAME")).replace("-", ""))
     }
 
+    val srvUsername = env["srvuser"]
+    val srvPassword = env["srvpwd"]
+    logger.info("Serviceuser: $srvUsername")
+
     RapidApplication.create(env)
         .also {
-            Vedtaksoversetter(rapidsConnection = it, oppdragMapper = OppdragMapper)
+            Vedtaksoversetter(
+                rapidsConnection = it,
+                oppdragMapper = OppdragMapper,
+            )
         }.start()
 }
 

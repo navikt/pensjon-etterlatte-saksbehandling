2c9a3070bffc8d4f704ff03fd8513fe2342ee8dc
jorgesti <jorgen.stikbakke@nav.no>
2022-03-29 11:19:18 +0200

    Rettet deserialisering og oppdatert tester

--- apps/etterlatte-vilkaar-kafka/src/main/kotlin/LesVilkaarsmelding.kt
+++ apps/etterlatte-vilkaar-kafka/src/main/kotlin/LesVilkaarsmelding.kt
@@ -1,5 +1,18 @@
-import com.fasterxml.jackson.databind.JsonNode
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import no.nav.etterlatte.libs.common.behandling.BehandlingSammendragListe
+import no.nav.etterlatte.libs.common.behandling.Behandlingsopplysning
 import no.nav.etterlatte.libs.common.logging.withLogContext
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.vikaar.VilkaarOpplysning
@@ -8,7 +21,6 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
-import java.util.*
 
 internal class LesVilkaarsmelding(
     rapidsConnection: RapidsConnection,
@@ -18,9 +30,6 @@ internal class LesVilkaarsmelding(
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("@event", "BEHANDLING:GRUNNLAGENDRET") }
-            //validate { it.requireKey("@vilkaarsopplysninger") }
-            //validate { it.rejectKey("@sak_id") }
-            //validate { it.rejectKey("@behandling_id") }
             validate { it.requireKey("grunnlag")}
             validate {it.rejectKey("@vilkaarsvurdering")}
             validate { it.interestedIn("@correlation_id") }
@@ -31,15 +40,11 @@ internal class LesVilkaarsmelding(
     override fun onPacket(packet: JsonMessage, context: MessageContext) =
         withLogContext(packet.correlationId()) {
 
-            //TODO Logikk for å kalle vilkarService
-            val vilkaarsVurdering = vilkaar.mapVilkaar(
-                objectMapper.treeToValue(
-                    packet["grunnlag"],
-                    JsonNodeDto::class.java
-                ).opplysninger
-            )
-            //val sak = packet["@sak_id"]
-            //val behandlingsid = packet["@behandling_id"]
+
+
+            val grunnlagListe = packet["grunnlag"].toString()
+             val hmm = objectMapper.readValue<List<VilkaarOpplysning<ObjectNode>>>(grunnlagListe)
+            val vilkaarsVurdering = vilkaar.mapVilkaar(hmm)
 
             packet["@vilkaarsvurdering"] = vilkaarsVurdering
 
@@ -49,5 +54,5 @@ internal class LesVilkaarsmelding(
 
 private fun JsonMessage.correlationId(): String? = get("@correlation_id").textValue()
 
-
-data class JsonNodeDto(val opplysninger: List<VilkaarOpplysning<ObjectNode>>)
+//data class RequestDto(val opplysninger: List<VilkaarOpplysning<ObjectNode>>)
+//data class RequestDto(val opplysninger: TypeReference<List<VilkaarOpplysning<ObjectNode>>>)

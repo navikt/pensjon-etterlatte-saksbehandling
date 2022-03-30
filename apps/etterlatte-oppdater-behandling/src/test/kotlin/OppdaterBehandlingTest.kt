767f8a20d7f9412125c3f502ac50b0e5dd85b011
jorgesti <jorgen.stikbakke@nav.no>
2022-03-30 15:55:56 +0200

    begynt på tester

---
+++ apps/etterlatte-oppdater-behandling/src/test/kotlin/OppdaterBehandlingTest.kt
@@ -0,0 +1,38 @@
+import io.ktor.http.*
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.etterlatte.BehandlingsService
+import no.nav.etterlatte.OppdaterBehandling
+import no.nav.etterlatte.libs.common.behandling.Behandlingsopplysning
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.FileNotFoundException
+
+internal class OppdaterBehandlingTest {
+
+    private val behandlingService = mockk<BehandlingsService>()
+
+    private val inspector = TestRapid().apply { OppdaterBehandling(this, behandlingService) }
+
+    @Test
+    fun `skal fordele gyldig soknad til behandling`() {
+        every { behandlingService.leggTilVilkaarsresultat(any(),any()) }.returns(Unit)
+
+        val inspector = inspector.apply { sendTestMessage(fullMelding) }.inspektør
+
+        //Assertions.assertEquals("ey_fordelt", inspector.message(0).get("@event").asText())
+        //Assertions.assertEquals("true", inspector.message(0).get("@soeknad_fordelt").asText())
+
+    }
+
+
+    companion object {
+        val fullMelding = readFile("/fullMelding.json")
+
+    }
+}
+fun readFile(file: String) = OppdaterBehandlingTest::class.java.getResource(file)?.readText()
+    ?: throw FileNotFoundException("Fant ikke filen $file")

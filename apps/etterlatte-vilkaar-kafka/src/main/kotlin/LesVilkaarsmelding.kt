0fe87b00146b8267885f6b69817bbaeb5c0c0d4f
jorgesti <jorgen.stikbakke@nav.no>
2022-03-30 11:08:39 +0200

    Oppdatert vilkar-kafka til å returnere vilkaarsResultat

--- apps/etterlatte-vilkaar-kafka/src/main/kotlin/LesVilkaarsmelding.kt
+++ apps/etterlatte-vilkaar-kafka/src/main/kotlin/LesVilkaarsmelding.kt
@@ -1,21 +1,10 @@
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.*
-import com.fasterxml.jackson.databind.json.JsonMapper
+
 import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.treeToValue
-import no.nav.etterlatte.libs.common.behandling.BehandlingSammendragListe
-import no.nav.etterlatte.libs.common.behandling.Behandlingsopplysning
+import model.VilkaarService
 import no.nav.etterlatte.libs.common.logging.withLogContext
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.vikaar.VilkaarOpplysning
-import no.nav.etterlatte.model.VilkaarService
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection

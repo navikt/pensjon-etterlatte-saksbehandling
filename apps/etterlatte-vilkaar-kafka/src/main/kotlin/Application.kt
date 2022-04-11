f0f02ae8d875eb2dc25981972c14c3caf458b73c
Lars Erik Dahl <lars.erik.dahl@nav.no>
2022-04-11 11:00:57 +0200

    etterlatte-vilkaar-kafka: Endret fra attesterings-databasen til sin egen

--- apps/etterlatte-vilkaar-kafka/src/main/kotlin/Application.kt
+++ apps/etterlatte-vilkaar-kafka/src/main/kotlin/Application.kt
@@ -1,17 +1,27 @@
 package no.nav.etterlatte
 
+import BehandlingOpprettet
+import DataSourceBuilder
 import LesVilkaarsmelding
 import no.nav.etterlatte.model.VilkaarService
 import no.nav.helse.rapids_rivers.RapidApplication
+import vilkaar.Dao
+import vilkaar.VurderVilkaar
+import vilkaar.VurderteVilkaarDao
 
 fun main() {
+
+    val datasource = DataSourceBuilder(System.getenv()).also { it.migrate() }.dataSource
+
     System.getenv().toMutableMap().apply {
         put("KAFKA_CONSUMER_GROUP_ID", get("NAIS_APP_NAME")!!.replace("-", ""))
         //TODO refaktorere ut appbuilder
     }.also { env ->
         RapidApplication.create(env)
-            .also { LesVilkaarsmelding(it, VilkaarService()) }
-            .start()
+            .also {
+                LesVilkaarsmelding(it, VilkaarService())
+                BehandlingOpprettet(it, VurderVilkaar(Dao(datasource, ::VurderteVilkaarDao)))
+            }.start()
     }
 }
 

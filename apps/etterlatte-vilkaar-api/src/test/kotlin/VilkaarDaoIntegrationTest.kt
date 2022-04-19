754133c314fd5d98be7ca46a2b2f41b129c63721
jaakobls <jakobls@gmail.com>
2022-04-19 17:45:04 +0200

    Lagrer en grunnlagshendelse og et vurdert vilkaar til databasen i test.

--- apps/etterlatte-vilkaar-api/src/test/kotlin/VilkaarDaoIntegrationTest.kt
+++ apps/etterlatte-vilkaar-api/src/test/kotlin/VilkaarDaoIntegrationTest.kt
@@ -2,14 +2,19 @@ package no.nav.etterlatte
 
 import no.nav.etterlatte.config.DataSourceBuilder
 import no.nav.etterlatte.config.StandardJdbcUrlBuilder
+import no.nav.etterlatte.libs.common.objectMapper
+import no.nav.etterlatte.libs.common.vikaar.Grunnlagshendelse
+import no.nav.etterlatte.libs.common.vikaar.VilkarIBehandling
 import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
+import java.sql.Types
 import java.util.*
 import javax.sql.DataSource
 
@@ -19,8 +24,10 @@ internal class VilkaarDaoIntegrationTest {
     @Container
     private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:14")
 
+    private lateinit var flyway: Flyway
     private lateinit var dataSource: DataSource
     private lateinit var vilkaarDao: VilkaarDao
+    private val behandlingId = UUID.randomUUID()
 
     @BeforeAll
     fun beforeAll() {
@@ -35,30 +42,83 @@ internal class VilkaarDaoIntegrationTest {
             vilkaarDao = VilkaarDaoJdbc(dataSource)
 
             // TODO ikke optimalt at man må kopiere inn skjema fra annen app for å få testet dette
-            migrate(dataSource)
+            flyway = Flyway
+                .configure()
+                .dataSource(dataSource)
+                .load()
         }
     }
 
-    fun migrate(dataSource: DataSource) =
-        Flyway.configure()
-            .dataSource(dataSource)
-            .load()
-            .migrate()
+    @BeforeEach
+    internal fun setup() {
+        flyway.clean()
+        flyway.migrate()
+        dataSource.lagreGrunnlagsHendelse()
+        dataSource.lagreVurdertVilkaar()
+    }
 
     @AfterAll
     fun afterAll() {
         postgreSQLContainer.stop()
     }
 
+    fun DataSource.lagreGrunnlagsHendelse(
+        hendelser: List<Grunnlagshendelse> = grunnlagsHendelsesListeForBehandling(behandlingId.toString())
+    ) {
+        this.connection.use { connection ->
+            val stmt =
+                connection.prepareStatement("INSERT INTO grunnlagshendelse(behandling, opplysning, kilde, opplysningtype, hendelsenummer, hendelsetype, hendelseref) VALUES(?, ?, ?, ?, ?, ?, ?)")
+
+            stmt.use {
+                hendelser.forEach { hendelse ->
+                    it.setObject(1, hendelse.behandling)
+                    it.setString(2, hendelse.opplysning?.opplysning?.let { objectMapper.writeValueAsString(it) })
+                    it.setString(3, hendelse.opplysning?.kilde?.let { objectMapper.writeValueAsString(it) })
+                    it.setString(4, hendelse.opplysning?.opplysningType?.name)
+                    it.setLong(5, hendelse.hendelsenummer)
+                    it.setString(6, hendelse.hendelsetype.name)
+                    hendelse.hendelsereferanse?.let { it2 -> it.setLong(7, it2) } ?: it.setNull(
+                        7,
+                        Types.BIGINT
+                    )
+                    it.executeUpdate()
+                }
+            }
+        }
+    }
+
+    fun DataSource.lagreVurdertVilkaar(
+        vilkarIBehandling: VilkarIBehandling = vilkaarResultatForBehandling(behandlingId.toString())
+    ) {
+        this.connection.use { connection ->
+            val stmt =
+                connection.prepareStatement("INSERT INTO vurdertvilkaar(behandling, versjon, vilkaarResultat, avdoedSoeknad, soekerSoeknad, soekerPdl, avdoedPdl, gjenlevendePdl) values(?, ?, ?, ?, ?, ?, ?, ?)")
+
+            stmt.use {
+                it.setObject(1, vilkarIBehandling.behandling)
+                it.setLong(2, vilkarIBehandling.versjon)
+                it.setString(3, objectMapper.writeValueAsString(vilkarIBehandling.vilkaarResultat))
+                it.setString(4, vilkarIBehandling.grunnlag.avdoedSoeknad?.let { objectMapper.writeValueAsString(it) })
+                it.setString(5, vilkarIBehandling.grunnlag.soekerSoeknad?.let { objectMapper.writeValueAsString(it) })
+                it.setString(6, vilkarIBehandling.grunnlag.soekerPdl?.let { objectMapper.writeValueAsString(it) })
+                it.setString(7, vilkarIBehandling.grunnlag.avdoedPdl?.let { objectMapper.writeValueAsString(it) })
+                it.setString(
+                    8,
+                    vilkarIBehandling.grunnlag.gjenlevendePdl?.let { objectMapper.writeValueAsString(it) })
+
+                it.executeUpdate()
+            }
+        }
+
+
+    }
+
     @Test
     fun `hent vilkaar`() {
         // TODO legg til en vilkaarsvurdering for å teste uthenting
+        val vilkaarResultat = vilkaarDao.hentVilkaarResultat(behandlingId.toString())
 
-        val behandlingId = UUID.randomUUID().toString()
-
-        val vilkaarResultat = vilkaarDao.hentVilkaarResultat(behandlingId)
-
-        assertNull(vilkaarResultat)
+        assertNotNull(vilkaarResultat)
     }
 
 }

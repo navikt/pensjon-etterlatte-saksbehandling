c0bd9735ad824e1b976ccbd6383009295c61080a
kristinaskatun <kristinaskatun@gmail.com>
2022-05-03 14:54:27 +0200

    Ny test av db

---
+++ apps/etterlatte-behandling/src/main/resources/db.migration/V1__create_initial_tables.sql
@@ -0,0 +1,28 @@
+
+CREATE TABLE sak
+(
+    id BIGSERIAL
+        PRIMARY KEY,
+    fnr TEXT,
+    sakType TEXT
+);
+
+
+CREATE TABLE behandling
+(
+    id UUID
+        PRIMARY KEY,
+    sak_id BIGINT NOT NULL
+        CONSTRAINT behandling_sak_id_fk
+            REFERENCES sak (id),
+    behandling_opprettet TIMESTAMP,
+    sist_endret TIMESTAMP,
+    soekand_mottatt_dato DATE,
+    innsender TEXT,
+    soeker TEXT,
+    gjenlevende TEXT,
+    avdoed TEXT,
+    soesken TEXT,
+    gyldighetssproving TEXT,
+    status TEXT, -- gyldig_soeknad, ikke_gyldig_soeknad, avbrutt, under_behandling, fattet_vedtak, attestert, iverksatt,
+);

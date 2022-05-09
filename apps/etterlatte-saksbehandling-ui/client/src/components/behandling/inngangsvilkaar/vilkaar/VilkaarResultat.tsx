805941a2b70f505b5b0cde22cf7f7a87425ceafc
Marie Fremstad <Marie.Fremstad@nav.no>
2022-05-09 13:17:33 +0200

    fjerne randomizeForTest - ferdig med test

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/inngangsvilkaar/vilkaar/VilkaarResultat.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/inngangsvilkaar/vilkaar/VilkaarResultat.tsx
@@ -2,7 +2,7 @@ import { VurderingsResultat } from '../../../../store/reducers/BehandlingReducer
 import React from 'react'
 import { VilkaarBorder } from '../styled'
 import styled from 'styled-components'
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 import { BehandlingHandlingKnapper } from '../../handlinger/BehandlingHandlingKnapper'
 import { VilkaarsVurderingKnapper } from '../../handlinger/vilkaarsvurderingKnapper'
 
@@ -12,19 +12,7 @@ type Props = {
   dato: string
 }
 
-/** Bare funksjoner for at Arnt skulle få teste grensesnittet */
-const randomizeForTest = (): VurderingsResultat => {
-  const result = [
-    VurderingsResultat.OPPFYLT,
-    VurderingsResultat.KAN_IKKE_VURDERE_PGA_MANGLENDE_OPPLYSNING,
-    VurderingsResultat.IKKE_OPPFYLT,
-  ]
-  return result[Math.floor(Math.random() * 3)]
-}
-const resultat = randomizeForTest() //TODO: fjern
-/** Bare funksjoner for at Arnt skulle få teste grensesnittet */
-
-export const VilkaarResultat: React.FC<Props> = ({ id, /*resultat,*/ dato }) => {
+export const VilkaarResultat: React.FC<Props> = ({ id, resultat, dato }) => {
   const datoFormatert = format(new Date(dato), 'dd.MM.yyyy')
 
   let tekst = ''

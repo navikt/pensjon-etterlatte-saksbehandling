ce0e301ff3b6a4e8b950e7ff886cd35ba29ff79d
jaakobls <jakobls@gmail.com>
2022-05-10 15:57:26 +0200

    Implementerer funksjonalitet for å kunne trigge grensesnittavstemming og manuelt sette kvittering via kafka

---
+++ apps/etterlatte-okonomi-vedtak/src/main/kotlin/utbetaling/common/Oppgave.kt
@@ -0,0 +1,10 @@
+package no.nav.etterlatte.utbetaling.common
+
+enum class Oppgavetype {
+    START_GRENSESNITTAVSTEMMING, SETT_KVITTERING_MANUELT
+}
+
+data class Oppgave(
+    val oppgavetype: Oppgavetype,
+    val vedtakId: String?
+)

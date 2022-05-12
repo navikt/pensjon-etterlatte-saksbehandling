25ffa7964f1aadd6b9f7913f88bd1f0000b2d0c2
jorgesti <jorgen.stikbakke@nav.no>
2022-05-12 15:01:14 +0200

    Opplysninger-fra-pdl: Fikset river og la til test for kafkaflyt

--- apps/etterlatte-opplysninger-fra-pdl/src/main/kotlin/opplysninger/kilde/pdl/BesvarOpplysningsbehov.kt
+++ apps/etterlatte-opplysninger-fra-pdl/src/main/kotlin/opplysninger/kilde/pdl/BesvarOpplysningsbehov.kt
@@ -22,8 +22,7 @@ internal class BesvarOpplysningsbehov(
     init {
         River(rapidsConnection).apply {
             validate { it.requireKey("@behov") }
-            //TODO endre til saksId
-            validate { it.requireKey("behandling") }
+            validate { it.requireKey("sak") }
             validate { it.requireKey("fnr") }
             validate { it.requireKey("rolle") }
             validate { it.rejectKey("opplysning") }
---
+++ apps/etterlatte-opplysninger-fra-pdl/src/test/kotlin/opplysninger/kilde/pdl/BesvarOpplysningsbehovTest.kt
@@ -0,0 +1,68 @@
+package opplysninger.kilde.pdl
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstyper
+import no.nav.etterlatte.libs.common.person.*
+import no.nav.etterlatte.opplysninger.kilde.pdl.BesvarOpplysningsbehov
+import no.nav.etterlatte.opplysninger.kilde.pdl.Pdl
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.FileNotFoundException
+import java.time.LocalDate
+
+class BesvarOpplysningsbehovTest {
+        companion object {
+            val melding = readFile("/behov.json")
+
+            val pdlMock = mockk<Pdl>()
+            fun readFile(file: String) = Companion::class.java.getResource(file)?.readText()
+                ?: throw FileNotFoundException("Fant ikke filen $file")
+
+            fun mockPerson(
+                fnr: String = "11057523044",
+                foedselsaar: Int = 2010,
+                foedselsdato: LocalDate? = LocalDate.parse("2010-04-19"),
+                doedsdato: LocalDate? = null,
+                adressebeskyttelse: Adressebeskyttelse = Adressebeskyttelse.UGRADERT,
+                statsborgerskap: String = "NOR",
+                foedeland: String = "NOR",
+                sivilstatus: Sivilstatus = Sivilstatus.UGIFT,
+                utland: Utland? = null,
+                bostedsadresse: Adresse? = null,
+                familieRelasjon: FamilieRelasjon? = null,
+            ) = Person(fornavn = "Ola",
+                etternavn = "Nordmann",
+                foedselsnummer = Foedselsnummer.of(fnr),
+                foedselsaar = foedselsaar,
+                foedselsdato = foedselsdato,
+                doedsdato = doedsdato,
+                adressebeskyttelse = adressebeskyttelse,
+                bostedsadresse = bostedsadresse?.let { listOf(bostedsadresse) } ?: emptyList(),
+                deltBostedsadresse = emptyList(),
+                oppholdsadresse = emptyList(),
+                kontaktadresse = emptyList(),
+                statsborgerskap = statsborgerskap,
+                foedeland = foedeland,
+                sivilstatus = sivilstatus,
+                utland = utland,
+                familieRelasjon = familieRelasjon)
+
+        }
+
+
+        private val inspector = TestRapid().apply { BesvarOpplysningsbehov(this, pdlMock) }
+
+        @Test
+        fun `skal lese melding om opplysningsbehov og returnere info fra PDL`() {
+
+            every { pdlMock.hentPdlModell(any(), any()) } answers { mockPerson(fnr = firstArg())  }
+            val inspector = inspector.apply { sendTestMessage(melding) }.inspektør
+
+            Assertions.assertEquals(Opplysningstyper.SOEKER_PDL_V1.name, inspector.message(0).get("@behov").asText())
+            Assertions.assertEquals("Ola", inspector.message(0).get("opplysning").get("opplysning").get("fornavn").asText())
+            Assertions.assertEquals(1, inspector.size)
+        }
+    }
+

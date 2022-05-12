de96b2014459fbd076b303f536ea32f890f9da44
Glenn Wedin <glenn.wedin@nav.no>
2022-05-12 09:31:28 +0200

    test river

---
+++ apps/etterlatte-vedtaksvurdering/src/test/kotlin/RRTest.kt
@@ -0,0 +1,22 @@
+package no.nav.etterlatte
+
+import no.nav.etterlatte.database.VedtaksvurderingRepository
+import no.nav.etterlatte.rivers.LagreVilkaarsresultat
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RRTest {
+
+    /*
+    private val vedtaksvurderingService = VedtaksvurderingService(vedtaksvurderingRepository)
+    private val inspector = TestRapid().apply { LagreVilkaarsresultat(this, vedtaksvurderingService) }
+    */
+
+    @Test
+    fun doStuff() {
+
+    }
+
+}

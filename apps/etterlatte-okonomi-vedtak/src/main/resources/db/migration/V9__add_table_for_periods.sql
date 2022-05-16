a2f4ab75739d5b4512fcd21568ce81ab0a2d7152
jaakobls <jakobls@gmail.com>
2022-05-16 15:42:02 +0200

    utbetalingslinje-tabell i database og utvidet utbetalingsmodell

---
+++ apps/etterlatte-okonomi-vedtak/src/main/resources/db/migration/V9__add_table_for_periods.sql
@@ -0,0 +1,24 @@
+ALTER TABLE utbetalingsoppdrag RENAME TO utbetaling;
+
+CREATE TABLE utbetalingslinje (
+    id                      VARCHAR(32)                 PRIMARY KEY,
+    opprettet               TIMESTAMP WITH TIME ZONE    NOT NULL,
+    periode_fra             DATE                        NOT NULL,
+    periode_til             DATE                        ,
+    beloep                  NUMERIC                     NOT NULL,
+    utbetaling_id           VARCHAR                     REFERENCES utbetaling(id),
+    erstatter_id            VARCHAR(32)                 DEFAULT NULL,
+    sak_id                  VARCHAR(32)                 REFERENCES utbetaling(sak_id)
+);
+
+-- id  opprettet   periode_fra     periode_til     beloep   utbetaling_id  erstatter_id    sak_id
+-- 1   01.01.22    01 22                           10000    1                              1
+-- 2   01.04.22    05 22                           5000     2              1               1
+-- 3   01.01.23    02 23           05 23           7000     3              2               1
+-- 4   01.01.23    06 23                           3000     3              2               1
+
+-- SELECT * from utbetalingslinje as ul, utbetaling as u
+-- INNER JOIN ON u.id == ul.id
+-- WHERE u.sak_id == ?
+
+--  SELECT * from utbetalingslinje WHERE sak_id == 1;

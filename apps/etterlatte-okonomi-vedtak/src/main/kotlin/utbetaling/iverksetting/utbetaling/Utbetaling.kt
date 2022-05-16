32e39c1f490428edd9857b6ceb9ad772bb61dd31
jaakobls <jakobls@gmail.com>
2022-05-16 15:42:02 +0200

    utbetalingslinje-tabell i database og utvidet utbetalingsmodell

--- apps/etterlatte-okonomi-vedtak/src/main/kotlin/utbetaling/iverksetting/utbetaling/Utbetaling.kt
+++ apps/etterlatte-okonomi-vedtak/src/main/kotlin/utbetaling/iverksetting/utbetaling/Utbetaling.kt
@@ -1,8 +1,10 @@
 package no.nav.etterlatte.utbetaling.iverksetting.utbetaling
 
+import no.nav.etterlatte.domene.vedtak.Periode
 import no.nav.etterlatte.libs.common.vedtak.Vedtak
 import no.nav.etterlatte.utbetaling.common.Tidspunkt
 import no.trygdeetaten.skjema.oppdrag.Oppdrag
+import java.math.BigDecimal
 
 enum class UtbetalingStatus {
     SENDT,
@@ -16,6 +18,11 @@ data class VedtakId(val value: String)
 data class SakId(val value: String)
 data class BehandlingId(val value: String)
 data class Foedselsnummer(val value: String)
+
+data class Sak(
+    val sakid: SakId,
+    val utbetalinger: List<Utbetaling>,
+)
 
 data class Utbetaling(
     val id: Long,
@@ -32,5 +39,22 @@ data class Utbetaling(
     val kvittering: Oppdrag? = null,
     val kvitteringBeskrivelse: String? = null,
     val kvitteringFeilkode: String? = null,
-    val kvitteringMeldingKode: String? = null
+    val kvitteringMeldingKode: String? = null,
+    val utbetalingslinjer: List<Utbetalingslinje>
 )
+
+data class Utbetalingslinje(
+    val id: Long,
+    val opprettet: Tidspunkt,
+    val periode: Periode,
+    val beloep: BigDecimal,
+    val erstatterId: Long
+)
+
+/*
+01.01 Vedtak: Jan 22 - Des 22: 10000 kr (opphøres eksplisitt)
+01.02 Vedtak: Juni 22 - Feb 23: 7000 kr (opphøres implisitt)
+01.03 Opphør: fom april 22
+
+ */
+

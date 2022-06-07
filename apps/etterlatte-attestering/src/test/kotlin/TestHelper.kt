40aa441cc12a4f51d047d1da744a69b1e2c1d913
kristinaskatun <kristinaskatun@gmail.com>
2022-06-07 14:16:10 +0200

    Reverser lagt til felt

--- apps/etterlatte-attestering/src/test/kotlin/TestHelper.kt
+++ apps/etterlatte-attestering/src/test/kotlin/TestHelper.kt
@@ -36,8 +36,7 @@ fun vedtak(vedtakId: String = "1") = Vedtak(
             enhetsnummer = "9999",
             datoEnhetFOM = LocalDate.parse("1999-09-28")
         )
-    ),
-    attestasjon()
+    )
 )
 
 fun attestasjon() = Attestasjon(

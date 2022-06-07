59b889d97ff7ed362e2b45744bf8e0671c4ec4e4
jorgesti <jorgen.stikbakke@nav.no>
2022-06-07 12:54:34 +0200

    Etterlatte-beregning: lagt til test på grunnbeløp

---
+++ apps/etterlatte-beregning/src/test/kotlin/model/GrunnbeloepTest.kt
@@ -0,0 +1,24 @@
+package model
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class GrunnbeloepTest {
+
+    @Test
+    fun `Sjekk spesifikk G gir rett beløp`(){
+        val tidspunkt = LocalDate.of(2022,1,1)
+        val grunnbeloep = Grunnbeloep.hentGjeldendeG(tidspunkt)
+        Assertions.assertEquals(grunnbeloep.grunnbeløpPerMåned,8867 )
+    }
+    @Test
+    fun `sjekk siste G i periode gir rett beløp`(){
+        val tidspunkt = LocalDate.of(2022,1,1)
+        val grunnbeloep = Grunnbeloep.hentGforPeriode(tidspunkt)
+        Assertions.assertEquals(grunnbeloep.first().grunnbeløpPerMåned,8867 )
+        Assertions.assertEquals(grunnbeloep.last().grunnbeløpPerMåned,9290 )
+        Assertions.assertEquals(grunnbeloep.size, 2)
+    }
+
+}

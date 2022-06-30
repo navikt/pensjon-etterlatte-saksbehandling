5c82d8f980fe3e48b34f986718660aecce5d972c
Glenn Wedin <glenn.wedin@nav.no>
2022-06-30 11:44:35 +0200

    doedsdato

--- apps/etterlatte-opplysninger-fra-inntektskomponenten/src/main/kotlin/opplysninger/kilde/inntektskomponenten/HentOpplysningerFraInntektskomponenten.kt
+++ apps/etterlatte-opplysninger-fra-inntektskomponenten/src/main/kotlin/opplysninger/kilde/inntektskomponenten/HentOpplysningerFraInntektskomponenten.kt
@@ -43,20 +43,18 @@ internal class HentOpplysningerFraInntektskomponenten(
                     Opplysningstyper.AVDOED_INNTEKT_V1.name,
                 )
             ) {
-                val fnr = Foedselsnummer.of(packet["fnr"].asText()) // Todo feil fnr?
                 print("fnr ${packet["fnr"].asText()} - doedsdato ${packet["doedsdato"].asText()}")
-                if(packet["fnr"].asText() == null) {
-                    logger.error("Fødselsnummer mangler - kan ikke hente opplysninger")
+                if(!packet["doedsdato"].isNull && !packet["fnr"].isNull) {
+                    val fnr = Foedselsnummer.of(packet["fnr"].asText())
+                    val doedsdato = LocalDate.parse(packet["doedsdato"].asText())
+                    val arbeidsforhold = aaregService.hentArbeidsforhold(fnr)
+                    val inntektliste = inntektsKomponentenService.hentInntektListe(fnr, doedsdato)
+                    val opplysninger = opplysningsBygger.byggOpplysninger(inntektliste, arbeidsforhold)
+                    packet["opplysning"] = opplysninger
+                    context.publish(packet.toJson())
+                } else {
+                    logger.error("Kunne ikke hente opplysinger pga manglende fnr/doedsdato")
                 }
-                if(packet["doedsdato"].asText() == null) {
-                    logger.error("Dødsdato mangler - kan ikke hente opplysninger")
-                }
-                val doedsdato = LocalDate.parse(packet["doedsdato"].asText())
-                val arbeidsforhold = aaregService.hentArbeidsforhold(fnr)
-                val inntektliste = inntektsKomponentenService.hentInntektListe(fnr, doedsdato)
-                val opplysninger = opplysningsBygger.byggOpplysninger(inntektliste, arbeidsforhold)
-                packet["opplysning"] = opplysninger
-                context.publish(packet.toJson())
 
             }
 

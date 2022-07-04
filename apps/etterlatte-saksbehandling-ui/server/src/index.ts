7de9e8aaa8c580bb7ad42cd7933f772ec5f9f1cf
Emil Schrøder <emil.schroder@nav.no>
2022-07-04 16:18:25 +0200

    brev: Bruk default scope

--- apps/etterlatte-saksbehandling-ui/server/src/index.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/index.ts
@@ -51,7 +51,7 @@ if (isDev) {
 } else {
   logger.info('Proxy-kall')
   app.use('/api', expressProxy(`${process.env.API_URL}`, 'api://783cea60-43b5-459c-bdd3-de3325bd716a/.default'))
-  app.use('/brev', expressProxy(`${process.env.BREV_API_URL}`, 'api://d6add52a-5807-49cd-a181-76908efee836'))
+  app.use('/brev', expressProxy(`${process.env.BREV_API_URL}`, 'api://d6add52a-5807-49cd-a181-76908efee836/.default'))
 }
 
 app.use(/^(?!.*\/(internal|static)\/).*$/, (req: any, res: any) => {

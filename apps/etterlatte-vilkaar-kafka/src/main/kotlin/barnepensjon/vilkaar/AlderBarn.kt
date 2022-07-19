ded9c577ff26fa676cdb14d8654ec2e9151b111e
Aryan Iranzamini <aryan.iranzamini@bekk.no>
2022-07-19 12:40:59 +0200

    Etterlatte-vilkaar: Legg til kriterie om at søker må være i live

--- apps/etterlatte-vilkaar-kafka/src/main/kotlin/barnepensjon/vilkaar/AlderBarn.kt
+++ apps/etterlatte-vilkaar-kafka/src/main/kotlin/barnepensjon/vilkaar/AlderBarn.kt
@@ -13,14 +13,44 @@ fun vilkaarBrukerErUnder20(
     avdoedPdl: VilkaarOpplysning<Person>?,
 ): VurdertVilkaar {
     val soekerErUnder20 = kriterieSoekerErUnder20(soekerPdl, avdoedPdl)
+    val soekerErILive = kriterieSoekerErILive(soekerPdl, avdoedPdl)
 
     return VurdertVilkaar(
         vilkaartype,
-        setVikaarVurderingFraKriterier(listOf(soekerErUnder20)),
+        setVikaarVurderingFraKriterier(listOf(soekerErILive, soekerErUnder20)),
         null,
-        listOf(soekerErUnder20),
+        listOf(soekerErILive, soekerErUnder20),
         LocalDateTime.now()
     )
+}
+
+fun kriterieSoekerErILive(soekerPdl: VilkaarOpplysning<Person>?, avdoedPdl: VilkaarOpplysning<Person>?): Kriterie {
+    if (soekerPdl == null || avdoedPdl == null) {
+        return Kriterie(
+            Kriterietyper.SOEKER_ER_I_LIVE,
+            VurderingsResultat.KAN_IKKE_VURDERE_PGA_MANGLENDE_OPPLYSNING,
+            emptyList()
+        )
+    }
+
+    val opplysningsGrunnlag = listOf(
+        Kriteriegrunnlag(
+            soekerPdl.id,
+            KriterieOpplysningsType.DOEDSDATO,
+            soekerPdl.kilde,
+            Doedsdato(soekerPdl.opplysning.doedsdato, soekerPdl.opplysning.foedselsnummer)
+        ),
+        Kriteriegrunnlag(
+            avdoedPdl.id,
+            KriterieOpplysningsType.DOEDSDATO,
+            avdoedPdl.kilde,
+            Doedsdato(avdoedPdl.opplysning.doedsdato, avdoedPdl.opplysning.foedselsnummer)
+        )
+    )
+    // TODO ai: Sjekk om det er vits att søker med dødsdato fortsatt har oppfyllt vurdering
+    val resultat = vurderOpplysning { soekerPdl.opplysning.doedsdato == null || (soekerPdl.opplysning.doedsdato!!.isAfter(hentVirkningsdato(avdoedPdl))) }
+
+    return Kriterie(Kriterietyper.SOEKER_ER_I_LIVE, resultat, opplysningsGrunnlag)
 }
 
 fun kriterieSoekerErUnder20(
@@ -43,7 +73,7 @@ fun kriterieSoekerErUnder20(
                 soekerPdl.kilde,
                 Foedselsdato(soekerPdl.opplysning.foedselsdato, soekerPdl.opplysning.foedselsnummer)
             )
-        }
+        },
     )
 
     val resultat = if (soekerPdl == null || avdoedPdl == null) {
--- apps/etterlatte-vilkaar-kafka/src/test/kotlin/barnepensjon/vilkaar/AlderBarnTest.kt
+++ apps/etterlatte-vilkaar-kafka/src/test/kotlin/barnepensjon/vilkaar/AlderBarnTest.kt
@@ -3,6 +3,7 @@ package barnepensjon.vilkaar
 import adresserNorgePdl
 import lagMockPersonPdl
 import mapTilVilkaarstypePerson
+import no.nav.etterlatte.barnepensjon.hentVirkningsdato
 import no.nav.etterlatte.barnepensjon.vilkaarBrukerErUnder20
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import no.nav.etterlatte.libs.common.vikaar.Vilkaartyper
@@ -12,25 +13,28 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class AlderBarnTest {
+    private val personBarnOver20 = lagMockPersonPdl(
+        foedselsdatoBarnOver20,
+        fnrBarn, null, adresserNorgePdl(), null
+    )
+    private val personBarnUnder20 = lagMockPersonPdl(
+        foedselsdatoBarnUnder20,
+        fnrBarn, null, adresserNorgePdl(), null
+    )
+    private val personAvdoedMedDoedsdato =
+        lagMockPersonPdl(
+            foedselsdatoBarnUnder20,
+            fnrAvdoed,
+            doedsdatoPdl, adresserNorgePdl(), null
+        )
+    private val personAvdoedUtenDoedsdato =
+        lagMockPersonPdl(
+            foedselsdatoBarnUnder20,
+            fnrAvdoed, null, adresserNorgePdl(), null
+        )
 
     @Test
     fun vurderAlderErUnder20() {
-        val personBarnOver20 = lagMockPersonPdl(
-            foedselsdatoBarnOver20,
-            fnrBarn, null, adresserNorgePdl(), null)
-        val personBarnUnder20 = lagMockPersonPdl(
-            foedselsdatoBarnUnder20,
-            fnrBarn, null, adresserNorgePdl(), null)
-        val personAvdoedMedDoedsdato =
-            lagMockPersonPdl(
-                foedselsdatoBarnUnder20,
-                fnrAvdoed,
-                doedsdatoPdl, adresserNorgePdl(), null)
-        val personAvdoedUtenDoedsdato =
-            lagMockPersonPdl(
-                foedselsdatoBarnUnder20,
-                fnrAvdoed, null, adresserNorgePdl(), null)
-
         val vurderingBarnOver20 = vilkaarBrukerErUnder20(
             Vilkaartyper.SOEKER_ER_UNDER_20,
             mapTilVilkaarstypePerson(personBarnOver20),
@@ -57,6 +61,30 @@ class AlderBarnTest {
 
     }
 
+    @Test
+    fun `søker i live oppfyller vurdering`() {
+        val vurdering = vilkaarBrukerErUnder20(Vilkaartyper.SOEKER_ER_UNDER_20, mapTilVilkaarstypePerson(personBarnUnder20), mapTilVilkaarstypePerson(personAvdoedMedDoedsdato))
+
+        Assertions.assertEquals(VurderingsResultat.OPPFYLT, vurdering.resultat)
+    }
+
+    // TODO ai: Dobbelsjekk denne regelen
+    @Test
+    fun `søker med dødsdato må ha dødsdato etter virkningsdato for oppfyllt vurdering`() {
+        val personMedDødsdato = personBarnUnder20.copy(doedsdato = hentVirkningsdato(mapTilVilkaarstypePerson(personAvdoedMedDoedsdato)).plusDays(1))
+        val vurdering = vilkaarBrukerErUnder20(Vilkaartyper.SOEKER_ER_UNDER_20, mapTilVilkaarstypePerson(personMedDødsdato), mapTilVilkaarstypePerson(personAvdoedMedDoedsdato))
+
+        Assertions.assertEquals(VurderingsResultat.OPPFYLT, vurdering.resultat)
+    }
+
+    @Test
+    fun `søker med dødsdato tidligere enn virkningsdato oppfyller ikke vurdering`() {
+        val personMedDødsdato = personBarnUnder20.copy(doedsdato = hentVirkningsdato(mapTilVilkaarstypePerson(personAvdoedMedDoedsdato)).minusDays(1))
+        val vurdering = vilkaarBrukerErUnder20(Vilkaartyper.SOEKER_ER_UNDER_20, mapTilVilkaarstypePerson(personMedDødsdato), mapTilVilkaarstypePerson(personAvdoedMedDoedsdato))
+
+        Assertions.assertEquals(VurderingsResultat.IKKE_OPPFYLT, vurdering.resultat)
+    }
+
     companion object {
         val foedselsdatoBarnOver20 = LocalDate.parse("2000-08-29")
         val foedselsdatoBarnUnder20 = LocalDate.parse("2020-06-10")
@@ -64,5 +92,4 @@ class AlderBarnTest {
         val fnrBarn = Foedselsnummer.of("19040550081")
         val fnrAvdoed = Foedselsnummer.of("19078504903")
     }
-
 }

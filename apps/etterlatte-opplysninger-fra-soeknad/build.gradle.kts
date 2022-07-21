517c705a19b6363ed3908f5b44adc1d6d0e5781f
Lars Erik Dahl <lars.erik.dahl@nav.no>
2022-07-21 11:20:30 +0200

    avkorting, grunnlag, opplysning-fra-soeknad: Bump til ktor2

--- apps/etterlatte-opplysninger-fra-soeknad/build.gradle.kts
+++ apps/etterlatte-opplysninger-fra-soeknad/build.gradle.kts
@@ -1,20 +1,11 @@
-
 plugins {
-    id("etterlatte.rapids-and-rivers")
+    id("etterlatte.rapids-and-rivers-ktor2")
 }
 
 dependencies {
-    implementation(Ktor.OkHttp)
-    implementation(Ktor.ClientCore)
-    implementation(Ktor.ClientLoggingJvm)
-    implementation(Ktor.ClientAuth)
-    implementation(Ktor.ClientJackson)
-
-    implementation(project(":libs:ktorclient-auth-clientcredentials"))
     implementation(project(":libs:common"))
     implementation(project(":libs:rapidsandrivers-extras"))
 
-    testImplementation(Ktor.ClientMock)
     testImplementation(MockK.MockK)
     testImplementation(Kotlinx.CoroutinesCore)
 }

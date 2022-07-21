ca56913c4e0aeba43ed288aa45c160170d0842bb
Lars Erik Dahl <lars.erik.dahl@nav.no>
2022-07-21 11:09:53 +0200

    etterlatte-beregning: Bump til ktor2

--- apps/etterlatte-beregning/build.gradle.kts
+++ apps/etterlatte-beregning/build.gradle.kts
@@ -1,32 +1,11 @@
 
 plugins {
     id("etterlatte.common")
-    id("etterlatte.rapids-and-rivers")
+    id("etterlatte.rapids-and-rivers-ktor2")
 }
 
 dependencies {
     implementation(project(":libs:common"))
-    implementation(Ktor.ServerCore)
-    implementation(Ktor.ServerCio)
-    implementation(Ktor.ClientCore)
-    implementation(Ktor.ClientJackson)
-    implementation(Ktor.ClientCioJvm)
-    implementation(Ktor.ClientAuth)
-    implementation(Ktor.ClientLogging)
-    implementation(Ktor.MetricsMicrometer)
-    implementation(Ktor.Jackson)
-    implementation(Ktor.Auth)
-
-    implementation(Micrometer.Prometheus)
-    implementation(Jackson.DatatypeJsr310)
-    implementation(Jackson.DatatypeJdk8)
-    implementation(Jackson.ModuleKotlin)
-
-    implementation(NavFelles.TokenClientCore)
-    implementation(NavFelles.TokenValidationKtor)
-
     testImplementation(MockK.MockK)
-    testImplementation(Ktor.ClientMock)
-    testImplementation(Ktor.ServerTests)
     testImplementation(Kotlinx.CoroutinesCore)
 }

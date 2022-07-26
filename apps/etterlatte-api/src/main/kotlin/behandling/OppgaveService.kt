22d3dcc9b6414c3f4c505004c24c52b85163a7cd
Aryan Iranzamini <aryan.iranzamini@bekk.no>
2022-07-26 10:19:32 +0200

    Etterlatte-api : Gjør alle asynkrone kall i parallel

--- apps/etterlatte-api/src/main/kotlin/behandling/OppgaveService.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/OppgaveService.kt
@@ -1,13 +1,13 @@
 package no.nav.etterlatte.behandling
 
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import no.nav.etterlatte.libs.common.behandling.BehandlingSammendrag
 import no.nav.etterlatte.libs.common.behandling.BehandlingStatus
 import no.nav.etterlatte.libs.common.behandling.BehandlingType
 import no.nav.etterlatte.libs.common.behandling.OppgaveStatus
 import no.nav.etterlatte.libs.common.vikaar.Familiemedlemmer
-import no.nav.etterlatte.libs.common.vikaar.PersoninfoAvdoed
 import no.nav.etterlatte.typer.Sak
 import org.slf4j.LoggerFactory
 import java.util.*
@@ -42,29 +42,35 @@ class OppgaveService(private val behandlingKlient: BehandlingKlient, private val
     suspend fun hentAlleOppgaver(accessToken: String): Oppgaver {
         logger.info("Henter alle oppgaver")
 
-        return Oppgaver(behandlingKlient.hentOppgaver(accessToken).oppgaver.map {
-            coroutineScope {
-                val vedtak = async { vedtakKlient.hentVedtak(it.behandlingId.toString(), accessToken) }
-
-                Oppgave(
-                    it.behandlingId,
-                    it.sak.id,
-                    it.behandlingStatus,
-                    it.oppgaveStatus,
-                    it.sak.sakType,
-                    BehandlingType.FØRSTEGANGSBEHANDLING,
-                    it.regdato.toLocalDateTime().toString(),
-                    it.fristDato.atStartOfDay().toString(),
-                    it.sak.ident,
-                    "",
-                    "",
-                    Handling.BEHANDLE,
-                    antallSoesken = vedtak.await().kommerSoekerTilgodeResultat?.familieforhold?.let { familieforhold ->
-                        hentAntallSøsken(familieforhold)
+        return coroutineScope {
+            behandlingKlient.hentOppgaver(accessToken).oppgaver
+                .map { oppgave ->
+                    async {
+                        Oppgave(
+                            behandlingsId = oppgave.behandlingId,
+                            sakId = oppgave.sak.id,
+                            status = oppgave.behandlingStatus,
+                            oppgaveStatus = oppgave.oppgaveStatus,
+                            soeknadType = oppgave.sak.sakType,
+                            behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING,
+                            regdato = oppgave.regdato.toLocalDateTime().toString(),
+                            fristdato = oppgave.fristDato.atStartOfDay().toString(),
+                            fnr = oppgave.sak.ident,
+                            beskrivelse = "",
+                            saksbehandler = "",
+                            handling = Handling.BEHANDLE,
+                            antallSoesken = vedtakKlient.hentVedtak(
+                                oppgave.behandlingId.toString(),
+                                accessToken
+                            ).kommerSoekerTilgodeResultat?.familieforhold?.let { familieforhold ->
+                                hentAntallSøsken(familieforhold)
+                            }
+                        )
                     }
-                )
-            }
-        })
+                }
+                .awaitAll()
+                .let { Oppgaver(it) }
+        }
     }
 }
 

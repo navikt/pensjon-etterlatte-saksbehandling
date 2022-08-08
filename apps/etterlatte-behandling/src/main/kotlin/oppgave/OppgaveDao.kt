2b147113e9ec35f3a27cb73ba630e6bc5bc379f2
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2022-08-08 14:19:00 +0200

    Behandling: Legger til fallback-dato i OppgaveDao
    Behandlinger er ikke nødvendligvis søknader lengre, og vi har dermed ikke nødvendigvis
    søknad innsendt dato som grunnlag for oppgave-dataene.
    
    Det er noe mer fundamentalt merkelig med å ha oppgaveDao og behandlingDao samtidig, men det er en fiks for en annen dag.

--- apps/etterlatte-behandling/src/main/kotlin/oppgave/OppgaveDao.kt
+++ apps/etterlatte-behandling/src/main/kotlin/oppgave/OppgaveDao.kt
@@ -35,14 +35,16 @@ class OppgaveDao(private val datasource: DataSource) {
 
         datasource.connection.use {
             val stmt =  it.prepareStatement("""
-                |SELECT b.id, b.sak_id, soekand_mottatt_dato, fnr, sakType, status, oppgave_status
+                |SELECT b.id, b.sak_id, soekand_mottatt_dato, fnr, sakType, status, oppgave_status, behandling_opprettet
                 |FROM behandling b inner join sak s on b.sak_id = s.id  
                 |where status in ${aktuelleStatuser.joinToString(separator = ", ", prefix = "(", postfix = ")") { "'${it.name}'" }}""".trimMargin().also {
                     println(it)
             }
                 )
             return stmt.executeQuery().toList {
-                val mottattDato = getTimestamp("soekand_mottatt_dato").toLocalDateTime().atZone(ZoneId.of("UTC"))
+                val mottattDato = getTimestamp("soekand_mottatt_dato")?.toLocalDateTime()?.atZone(ZoneId.of("UTC"))
+                    ?: getTimestamp("behandling_opprettet")?.toLocalDateTime()?.atZone(ZoneId.of("UTC"))
+                    ?: throw IllegalStateException("Vi har en behandling som hverken har soekand mottatt dato eller behandling opprettet dato ")
                 Oppgave(getObject("id") as UUID,
                     BehandlingStatus.valueOf(getString("status")),
                     OppgaveStatus.valueOf(getString("oppgave_status")),

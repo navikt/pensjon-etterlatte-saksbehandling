9eb15be8b18c735e1c0e35fc29d0bffb9d2d1f61
Emil Schrøder <emil.schroder@nav.no>
2022-08-17 17:17:05 +0200

    etterlatte-testdata: all null

--- apps/etterlatte-testdata/src/main/kotlin/dolly/Model.kt
+++ apps/etterlatte-testdata/src/main/kotlin/dolly/Model.kt
@@ -3,7 +3,7 @@ package dolly
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Bruker(val brukerId: String?, val brukerType: String?, val navIdent: String, val epost: String?)
+data class Bruker(val brukerId: String?, val brukerType: String?, val navIdent: String?, val epost: String?)
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Gruppe(val id: Long, val navn: String, val hensikt: String)

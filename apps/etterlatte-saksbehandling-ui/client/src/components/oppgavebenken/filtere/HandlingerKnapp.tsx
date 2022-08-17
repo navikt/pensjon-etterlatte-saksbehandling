c85c20906e4a5d26973b2dfbd241531a8947caf3
Aryan Iranzamini <aryan.iranzamini@bekk.no>
2022-08-17 09:43:41 +0200

    Etterlatte-saksbehandling-ui: Fjern console.logs og ubrukt kode

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/filtere/HandlingerKnapp.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/filtere/HandlingerKnapp.tsx
@@ -1,18 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Button } from '@navikt/ds-react'
-import { AppContext } from '../../../store/AppContext'
 import { handlinger, Handlinger } from '../typer/oppgavebenken'
 import { useNavigate } from 'react-router-dom'
 
-const HandlingerKnapp: React.FC<{ saksbehandler: string; handling: Handlinger; behandlingsId: string }> = ({
-  saksbehandler,
-  handling,
-  behandlingsId,
-}) => {
+const HandlingerKnapp: React.FC<{ handling: Handlinger; behandlingsId: string }> = ({ handling, behandlingsId }) => {
   const navigate = useNavigate()
-  const innloggetSaksbehandler = useContext(AppContext).state.saksbehandlerReducer.navn
-  const sakErTildeltInnloggetSaksbehandler = innloggetSaksbehandler === saksbehandler
-  console.log(sakErTildeltInnloggetSaksbehandler)
 
   const goToBehandling = () => {
     navigate(`behandling/${behandlingsId}/soeknadsoversikt`)

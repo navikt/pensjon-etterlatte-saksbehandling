fab02d9e025a3cf8f57a63d3f1fca08fe4382cc7
kristinaskatun <kristinaskatun@gmail.com>
2022-08-19 15:09:46 +0200

    Liten fiks

--- apps/etterlatte-saksbehandling-ui/client/src/shared/api/person.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/api/person.ts
@@ -1,13 +1,18 @@
+import { IApiResponse } from './types'
+
 const path = process.env.REACT_APP_VEDTAK_URL
 
-export const getPerson = async (fnr: string) => {
+export const getPerson = async (fnr: string): Promise<IApiResponse<any>> => {
   try {
     const result: Response = await fetch(`${path}/api/personer/${fnr}`)
     if (result.ok) {
       return { status: result.status, data: await result.json() }
+    } else {
+      console.log('error i fetch', result)
+      return { status: 400, data: await result.json() }
     }
   } catch (e) {
     console.log('response errorrrrrr', e)
-    throw new Error('Det skjedde en feil')
+    return { status: 500, data: e }
   }
 }

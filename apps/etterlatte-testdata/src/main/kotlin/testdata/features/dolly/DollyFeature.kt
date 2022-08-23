5c30741e3b6bc01bd7ce7a27bb1f0920686e77ac
Espen Schei <espen.schei@nav.no>
2022-08-23 08:55:26 +0200

    etterlatte-testdata: fiks merge-feil

--- apps/etterlatte-testdata/src/main/kotlin/testdata/features/dolly/DollyFeature.kt
+++ apps/etterlatte-testdata/src/main/kotlin/testdata/features/dolly/DollyFeature.kt
@@ -11,7 +11,6 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.etterlatte.*
-import no.nav.etterlatte.batch.JsonMessage
 import no.nav.etterlatte.libs.common.toJson
 import java.time.OffsetDateTime
 import java.util.*

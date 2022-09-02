ac02a45a5e6e1bbcf2c9998718eef971c3dbdd2e
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2022-09-02 13:11:29 +0200

    Migrerer gamle enum-verdier for vedtak i utbetaling

---
+++ apps/etterlatte-utbetaling/src/main/resources/db/migration/V6__fix_forstegangsbehandling.sql
@@ -0,0 +1,5 @@
+UPDATE
+    utbetaling SET vedtak = jsonb_set(vedtak::jsonb,
+                                        '{behandling,type}' , 'FØRSTEGANGSBEHANDLING')::text
+WHERE
+            vedtak::json->'behandling'->>'type' = 'FORSTEGANGSBEHANDLING';

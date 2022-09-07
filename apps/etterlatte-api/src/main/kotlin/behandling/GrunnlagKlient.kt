ce052d254519db719fcadb90aacd8f7b172c9720
Espen Schei <espen.schei@nav.no>
2022-09-07 14:08:31 +0200

    etterlatte-api: Forsøk å håndtere at man får 404 fra grunnlag dersom opplysning ikke finnes.

--- apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagKlient.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagKlient.kt
@@ -1,15 +1,21 @@
 package no.nav.etterlatte.behandling
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.michaelbull.result.get
 import com.github.michaelbull.result.mapBoth
 import com.typesafe.config.Config
 import io.ktor.client.HttpClient
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstyper
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.SaksbehandlerMedlemskapsperioder
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.person.Person
-import no.nav.etterlatte.libs.common.toJson
 import no.nav.etterlatte.libs.ktorobo.AzureAdClient
 import no.nav.etterlatte.libs.ktorobo.DownstreamResourceClient
 import no.nav.etterlatte.libs.ktorobo.Resource
@@ -29,7 +35,7 @@ interface EtterlatteGrunnlag {
     ): Grunnlagsopplysning<SaksbehandlerMedlemskapsperioder>?
 }
 
-class GrunnlagKlient(config: Config, httpClient: HttpClient) : EtterlatteGrunnlag {
+class GrunnlagKlient(config: Config, private val httpClient: HttpClient) : EtterlatteGrunnlag {
     private val logger = LoggerFactory.getLogger(GrunnlagKlient::class.java)
 
     private val azureAdClient = AzureAdClient(config)
@@ -74,31 +80,23 @@ class GrunnlagKlient(config: Config, httpClient: HttpClient) : EtterlatteGrunnla
         try {
             logger.info("Henter opplysning ($opplysningsType) fra grunnlag for sak med id $sakId.")
 
-            val json = downstreamResourceClient
-                .get(
-                    resource = Resource(
-                        clientId = clientId,
-                        url = "$resourceUrl/grunnlag/$sakId/$opplysningsType"
-                    ),
-                    accessToken = accessToken
-                )
-                .mapBoth(
-                    success = { json -> json },
-                    failure = { throwableErrorMessage -> throw Error(throwableErrorMessage.message) }
-                ).response
+            val token = azureAdClient.getOnBehalfOfAccessTokenForResource(
+                listOf("api://ce96a301-13db-4409-b277-5b27f464d08b/.default"),
+                accessToken
+            ).get()?.accessToken ?: ""
 
-            logger.info("Response fra grunnlag: $json")
-
-            val response = try {
-                objectMapper.readValue<Grunnlagsopplysning<SaksbehandlerMedlemskapsperioder>>(json.toString())
-            } catch (ex: Exception) {
-                logger.error("Klarte ikke parse response fra grunnlag", ex)
-                null
+            val response = httpClient.get("$resourceUrl/grunnlag/$sakId/$opplysningsType") {
+                header("Authorization", "Bearer $token")
+                accept(ContentType.Application.Json)
             }
 
-            logger.info("Parset response: ${response?.toJson()}")
-
-            return response
+            return when (response.status) {
+                HttpStatusCode.OK -> objectMapper.readValue<Grunnlagsopplysning<SaksbehandlerMedlemskapsperioder>>(
+                    response.bodyAsText()
+                )
+                HttpStatusCode.NotFound -> null
+                else -> throw Exception("Feil i kall mot grunnlag. Status: ${response.status}")
+            }
         } catch (e: Exception) {
             logger.error("Henting av opplysning ($opplysningsType) fra grunnlag for sak med id $sakId feilet.")
             throw e

5f90ca7993af4a6c06c89968f0de965bd868b3df
jorgesti <jorgen.stikbakke@nav.no>
2022-09-16 13:51:11 +0200

    etterlatte-fordeler: fiks av navnestandard på metric navn

--- apps/etterlatte-fordeler/src/main/kotlin/fordeler/FordelerMetricLogger.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/fordeler/FordelerMetricLogger.kt
@@ -4,12 +4,12 @@ import io.prometheus.client.Counter
 import no.nav.etterlatte.fordeler.FordelerResultat.IkkeGyldigForBehandling
 
 private val defaultKriterierIkkeOppfyltMetric = Counter.build(
-    "etterlatte_fordeler_kriterier_ikke_oppfylt",
+    "etterlatte_fordeler_kriterier_ikke_oppfylt_total",
     "Søknad avvist i fordeler fordi ett eller flere kriterier ikke er oppfylt"
 ).labelNames("kriterie").create()
 
 private val defaultFordelerStatusMetric = Counter.build(
-    "etterlatte_fordeler_soknad_fordelt",
+    "etterlatte_fordeler_soknad_fordelt_total",
     "Viser om en søknad er gyldig for behandling eller ikke"
 ).labelNames("fordelt").create()
 

e26afea704354f4ecf1f81ffab6ccb508f9ccbe4
Espen Schei <espen.schei@nav.no>
2022-09-19 12:46:41 +0200

    etterlatte-api: Oppdater med routes for å slette/oppdatere saksbehandler-perioder.

--- apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagRoute.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagRoute.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.etterlatte.getAccessToken
@@ -31,7 +32,7 @@ fun Route.grunnlagRoute(service: GrunnlagService) {
                     call.respond("Behandlings-id mangler")
                 } else {
                     call.respond(
-                        service.lagreAvdoedMedlemskapPeriode(
+                        service.upsertAvdoedMedlemskapPeriode(
                             behandlingId,
                             body.toDomain(call.navIdent),
                             call.navIdent,
@@ -41,6 +42,33 @@ fun Route.grunnlagRoute(service: GrunnlagService) {
                 }
             } catch (ex: Exception) {
                 logger.error("lagring av medlemskapsperiode feilet", ex)
+                throw ex
+            }
+        }
+
+        delete("/saksbehandler/periode/{behandlingId}/{saksbehandlerPeriodeId}") {
+            try {
+                val behandlingId = call.parameters["behandlingId"]
+                val saksbehandlerPeriodeId = call.parameters["saksbehandlerPeriodeId"]
+
+                if (behandlingId == null) {
+                    call.response.status(HttpStatusCode(400, "Bad request"))
+                    call.respond("Behandlings-id mangler")
+                } else if (saksbehandlerPeriodeId == null) {
+                    call.response.status(HttpStatusCode(400, "Bad request"))
+                    call.respond("saksbehandlerPeriodeId mangler")
+                } else {
+                    call.respond(
+                        service.slettAvdoedMedlemskapPeriode(
+                            behandlingId = behandlingId,
+                            saksbehandlerPeriodeId = saksbehandlerPeriodeId,
+                            saksbehandlerId = call.navIdent,
+                            getAccessToken(call)
+                        )
+                    )
+                }
+            } catch (ex: Exception) {
+                logger.error("Sletting av medlemskapsperiode feilet", ex)
                 throw ex
             }
         }

83f6e09d9faea278848f2319dbcb517cef9c3132
Emil Schrøder <emil.schroder@nav.no>
2022-09-27 16:23:21 +0200

    brev-api: fix feil

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/adresse/AdresseServiceMock.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/adresse/AdresseServiceMock.kt
@@ -1,9 +1,10 @@
 package no.nav.etterlatte.adresse
 
 import no.nav.etterlatte.brev.model.Mottaker
+import no.nav.etterlatte.libs.common.person.Foedselsnummer
 
 class AdresseServiceMock : AdresseService {
-    override suspend fun hentMottakerAdresse(id: String): Mottaker {
-        return Mottaker("Fornavnet", "Organisasjon", "Veien 22", id, "Oslo")
+    override suspend fun hentMottakerAdresse(id: Foedselsnummer): Mottaker {
+        return Mottaker("Fornavnet", "Organisasjon", "Veien 22", id.value, "Oslo")
     }
 }

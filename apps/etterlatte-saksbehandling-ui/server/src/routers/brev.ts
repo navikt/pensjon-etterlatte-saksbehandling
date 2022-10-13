87b59855364b68e50de5b3708dba2b405086130c
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2022-10-13 14:01:52 +0200

    Fanger feil i async-routes (#172)
    Express kræsjer hvis en route-handler har en uncaught rejection i promise,
    som da tar ned appen.

--- apps/etterlatte-saksbehandling-ui/server/src/routers/brev.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/routers/brev.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 import fetch from 'node-fetch'
 import request from 'request'
 
@@ -7,139 +7,175 @@ const router = Router({ mergeParams: true })
 const apiUrl = process.env.BREV_API_URL || 'http://localhost:8085'
 
 // Hent alle brev tilknyttet behandling ID
-router.get('/behandling/:behandlingId', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}`
-
-  const data = await fetch(path)
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.get('/innkommende/:fnr', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/innkommende/${req.params.fnr}`
-  const response = await fetch(path)
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-  res.send(await response)
-})
-
-router.post('/innkommende/:journalpostId/:dokumentInfoId', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/innkommende/${req.params.journalpostId}/${req.params.dokumentInfoId}`
-  const result = await fetch(path, { method: 'POST' }).then((res) => ({ status: res.status, data: res.buffer() }))
-
-  if (result.status == 200) {
-    res.contentType('application/pdf')
-    res.send(await result.data)
-  } else {
-    res.sendStatus(result.status)
+router.get('/behandling/:behandlingId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}`
+    const response = await fetch(path)
+    const json = await response.json()
+    res.send(json)
+  } catch (e) {
+    next(e)
   }
 })
 
-router.delete('/:brevId', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/${req.params.brevId}`
-
-  await fetch(path, { method: 'DELETE' })
-    .then((response) => res.sendStatus(response.status))
-    .catch(() => res.sendStatus(500))
-})
-
-router.get('/maler', async (req: Request, res: Response) => {
-  const data = await fetch(`${apiUrl}/brev/maler`)
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.get('/mottakere', async (req: Request, res: Response) => {
-  const data = await fetch(`${apiUrl}/brev/mottakere`)
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.post('/behandling/:behandlingId', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}`
-
-  const data = await fetch(path, {
-    method: 'POST',
-    body: JSON.stringify(req.body),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.post('/behandling/:behandlingId/vedtak', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}/vedtak`
-
-  const data = await fetch(path, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.text())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.post('/:brevId/ferdigstill', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/${req.params.brevId}/ferdigstill`
-
-  const data = await fetch(path, { method: 'POST' })
-    .then((res) => res.json())
-    .catch(() => res.sendStatus(500))
-
-  res.send(await data)
-})
-
-router.post('/:brevId/pdf', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/${req.params.brevId}/pdf`
-
-  const result = await fetch(path, { method: 'POST' }).then((res) => ({ status: res.status, data: res.buffer() }))
-
-  if (result.status == 200) {
-    res.contentType('application/pdf')
-    res.send(await result.data)
-  } else {
-    res.sendStatus(result.status)
+router.get('/innkommende/:fnr', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/innkommende/${req.params.fnr}`
+    const response = await fetch(path)
+    const json = await response.json()
+    res.send(json)
+  } catch (e) {
+    next(e)
   }
 })
 
-router.post('/pdf/:behandlingId', async (req: Request, res: Response, body) => {
-  const path = `${apiUrl}/brev/pdf/${req.params.behandlingId}`
+router.post('/innkommende/:journalpostId/:dokumentInfoId', async (req: Request, res: Response, next) => {
+  try {
+    const path = `${apiUrl}/brev/innkommende/${req.params.journalpostId}/${req.params.dokumentInfoId}`
+    const response = await fetch(path, { method: 'POST' })
+    const data = await response.buffer()
 
-  req.pipe(request.post(path, body)).pipe(res)
+    if (response.status == 200) {
+      res.contentType('application/pdf')
+      res.send(data)
+    } else {
+      res.sendStatus(response.status)
+    }
+  } catch (e) {
+    next(e)
+  }
 })
 
-router.post('/forhaandsvisning', async (req: Request, res: Response) => {
-  const path = `${apiUrl}/brev/forhaandsvisning`
+router.delete('/:brevId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/${req.params.brevId}`
+    const response = await fetch(path, { method: 'DELETE' })
+    res.sendStatus(response.status)
+  } catch (e) {
+    next(e)
+  }
+})
 
-  const result = await fetch(path, {
-    method: 'POST',
-    body: JSON.stringify(req.body),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((res) => ({
-    status: res.status,
-    data: res.buffer(),
-  }))
+router.get('/maler', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const response = await fetch(`${apiUrl}/brev/maler`)
+    const data = await response.json()
 
-  if (result.status == 200) {
-    res.contentType('application/pdf')
-    res.send(await result.data)
-  } else {
-    res.sendStatus(result.status)
+    res.send(await data)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.get('/mottakere', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const response = await fetch(`${apiUrl}/brev/mottakere`)
+    const data = await response.json()
+
+    res.send(data)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/behandling/:behandlingId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}`
+
+    const response = await fetch(path, {
+      method: 'POST',
+      body: JSON.stringify(req.body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+
+    res.send(data)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/behandling/:behandlingId/vedtak', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/behandling/${req.params.behandlingId}/vedtak`
+
+    const response = await fetch(path, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.text()
+
+    res.send(data)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/:brevId/ferdigstill', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/${req.params.brevId}/ferdigstill`
+
+    const response = await fetch(path, { method: 'POST' })
+    const data = await response.json()
+
+    res.send(data)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/:brevId/pdf', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/${req.params.brevId}/pdf`
+
+    const result = await fetch(path, { method: 'POST' })
+    const data = await result.buffer()
+
+    if (result.status == 200) {
+      res.contentType('application/pdf')
+      res.send(data)
+    } else {
+      res.sendStatus(result.status)
+    }
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/pdf/:behandlingId', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/pdf/${req.params.behandlingId}`
+    req.pipe(request.post(path, next)).pipe(res)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/forhaandsvisning', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const path = `${apiUrl}/brev/forhaandsvisning`
+
+    const result = await fetch(path, {
+      method: 'POST',
+      body: JSON.stringify(req.body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await result.buffer()
+
+    if (result.status == 200) {
+      res.contentType('application/pdf')
+      res.send(data)
+    } else {
+      res.sendStatus(result.status)
+    }
+  } catch (e) {
+    next(e)
   }
 })
 

0cbe83d65f4b3edf4a55b0b4ce550a1140cf4e09
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2022-10-18 14:44:00 +0200

    EY-952 Fix conflicts

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/vilkaarsvurdering/vilkaar/VilkaarGrunnlagsStoette.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/vilkaarsvurdering/vilkaar/VilkaarGrunnlagsStoette.tsx
@@ -3,7 +3,7 @@ import { AlderBarn } from './AlderBarn'
 
 export const VilkaarGrunnlagsStoette = ({ vilkaar }: { vilkaar: Vilkaar }) => {
   const finnGrunnlag = (vilkaar: Vilkaar) => {
-    switch (vilkaar.type) {
+    switch (vilkaar.hovedvilkaar.type) {
       case 'ALDER_BARN':
         return <AlderBarn grunnlag={vilkaar.grunnlag} />
       default:

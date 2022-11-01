8166c1aabcbdc8bf54cb9094fb4eb6b8b144d6d8
Nicolas N <nicolas.nordhagen@nav.no>
2022-11-01 09:22:51 +0100

    Kjøre vedtaksvurdering på andre porter enn brev-api

--- apps/etterlatte-vedtaksvurdering/src/test/kotlin/LocalDevApplication.kt
+++ apps/etterlatte-vedtaksvurdering/src/test/kotlin/LocalDevApplication.kt
@@ -8,10 +8,10 @@ fun main() {
 
     val env = System.getenv().toMutableMap()
     env["LOCAL_DEV"] = "true"
-    env["DB_JDBC_URL"] = "jdbc:postgresql://localhost:5432/postgres"
+    env["DB_JDBC_URL"] = "jdbc:postgresql://localhost:5433/postgres"
     env["DB_PASSWORD"] = "postgres"
     env["DB_USERNAME"] = "postgres"
-    env["HTTP_PORT"] = "8085"
+    env["HTTP_PORT"] = "8086"
     env["KAFKA_BOOTSTRAP_SERVERS"] = "0.0.0.0:9092"
     env["KAFKA_RAPID_TOPIC"] = "etterlatte.dodsmelding"
     env["NAIS_APP_NAME"] = "etterlatte-vedtakvurdering"

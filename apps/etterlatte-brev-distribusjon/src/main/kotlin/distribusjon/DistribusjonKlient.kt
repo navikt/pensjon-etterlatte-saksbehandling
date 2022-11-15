0b6e5f5a69282db9edc30210a64f9438763dcd41
Emil Schrøder <emil.schroder@nav.no>
2022-11-15 15:40:05 +0100

    Håndter 409 respons

--- apps/etterlatte-brev-distribusjon/src/main/kotlin/distribusjon/DistribusjonKlient.kt
+++ apps/etterlatte-brev-distribusjon/src/main/kotlin/distribusjon/DistribusjonKlient.kt
@@ -2,11 +2,13 @@ package no.nav.etterlatte.distribusjon
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.accept
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import no.nav.etterlatte.libs.common.distribusjon.DistribuerJournalpostRequest
 import no.nav.etterlatte.libs.common.distribusjon.DistribuerJournalpostResponse
@@ -19,12 +21,17 @@ class DistribusjonKlient(private val client: HttpClient, private val url: String
     private val logger = LoggerFactory.getLogger(DistribusjonKlient::class.java)
 
     suspend fun distribuerJournalpost(request: DistribuerJournalpostRequest): DistribuerJournalpostResponse = try {
-        client.post("$url/distribuerjournalpost") {
+        val response = client.post("$url/distribuerjournalpost") {
             accept(ContentType.Application.Json)
             contentType(ContentType.Application.Json)
             header(X_CORRELATION_ID, getXCorrelationId())
             setBody(request)
-        }.body()
+        }
+        if (response.status in listOf(HttpStatusCode.OK, HttpStatusCode.Conflict)) {
+            response.body()
+        } else {
+            throw ResponseException(response,"Ukjent respons fra dokumentdistribusjon")
+        }
     } catch (exception: Exception) {
         logger.error("Feil i kall mot dokumentdistribusjon: ", exception)
         throw DistribusjonException("Feil i kall mot dokumentdistribusjon", exception)

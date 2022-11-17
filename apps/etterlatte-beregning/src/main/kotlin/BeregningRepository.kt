d633bf2aa19601a3c6baf9f8159dabc65944291e
Simen Johnsrud <simen.johnsrud92@gmail.com>
2022-11-17 16:26:31 +0100

    EY-1093 WIP Lage egen tabell for beregningsperioder

--- apps/etterlatte-beregning/src/main/kotlin/BeregningRepository.kt
+++ apps/etterlatte-beregning/src/main/kotlin/BeregningRepository.kt
@@ -22,26 +22,43 @@ class BeregningRepositoryImpl(private val dataSource: DataSource) : BeregningRep
     override fun lagre(
         beregning: Beregning
     ): Beregning {
-        using(sessionOf(dataSource)) {
-            it.transaction { tx ->
-                queryOf(
-                    statement = Queries.lagreBeregning,
-                    paramMap = mapOf(
-                        "beregningId" to beregning.beregningId,
-                        "behandlingId" to beregning.behandlingId,
-                        "beregnetDato" to beregning.beregnetDato,
-                        "beregningsperioder" to beregning.beregningsperioder.toJson(),
-                        "sakId" to beregning.grunnlagMetadata.sakId,
-                        "grunnlagVersjon" to beregning.grunnlagMetadata.versjon
-                    )
-                ).let { query -> tx.run(query.asUpdate) }
+        using(sessionOf(dataSource)) { session ->
+            session.transaction { tx ->
+                {
+                    queryOf(
+                        statement = Queries.lagreBeregning,
+                        paramMap = mapOf(
+                            "beregningId" to beregning.beregningId,
+                            "behandlingId" to beregning.behandlingId,
+                            "beregnetDato" to beregning.beregnetDato,
+                            "beregningsperioder" to beregning.beregningsperioder.toJson(),
+                            "sakId" to beregning.grunnlagMetadata.sakId,
+                            "grunnlagVersjon" to beregning.grunnlagMetadata.versjon
+                        )
+                    ).let { query -> tx.run(query.asUpdate) }
+
+                    beregning.beregningsperioder.forEach {
+                        queryOf(
+                            statement = Queries.lagreBeregningPeriode,
+                            paramMap = mapOf(
+                                "beregningId" to beregning.beregningId,
+                                "datoFOM" to it.datoFOM,
+                                "datoTOM" to it.datoTOM,
+                                "utbetaltBeloep" to it.utbetaltBeloep,
+                                "soeskenFlokk" to it.soeskenFlokk,
+                                "grunnbeloepMnd" to it.grunnbelopMnd,
+                                "grunnbeloep" to it.grunnbelopMnd
+                            )
+                        ).let { query -> tx.run(query.asUpdate) }
+                    }
+                }
             }
         }
         return hent(beregning.behandlingId)
     }
 
     override fun hent(behandlingId: UUID): Beregning = using(sessionOf(dataSource)) {
-        it.transaction { tx ->
+        it.transaction { tx -> // TODO: Få inn henting av beregningsperioder
             queryOf(
                 statement = Queries.hentBeregning,
                 paramMap = mapOf("behandlingId" to behandlingId)
@@ -52,18 +69,18 @@ class BeregningRepositoryImpl(private val dataSource: DataSource) : BeregningRep
 
 private fun toBeregning(row: Row) = with(row) {
     Beregning(
-        beregningId = uuid(DatabaseColumns.BeregningId.navn),
-        behandlingId = uuid(DatabaseColumns.BehandlingId.navn),
-        beregnetDato = localDateTime(DatabaseColumns.BeregnetDato.navn),
-        beregningsperioder = objectMapper.readValue(string(DatabaseColumns.Beregningsperioder.navn)),
+        beregningId = uuid(BeregningDatabaseColumns.BeregningId.navn),
+        behandlingId = uuid(BeregningDatabaseColumns.BehandlingId.navn),
+        beregnetDato = localDateTime(BeregningDatabaseColumns.BeregnetDato.navn),
+        beregningsperioder = objectMapper.readValue(string(BeregningDatabaseColumns.Beregningsperioder.navn)),
         grunnlagMetadata = Metadata(
-            sakId = long(DatabaseColumns.SakId.navn),
-            versjon = long(DatabaseColumns.GrunnlagVersjon.navn)
+            sakId = long(BeregningDatabaseColumns.SakId.navn),
+            versjon = long(BeregningDatabaseColumns.GrunnlagVersjon.navn)
         )
     )
 }
 
-private enum class DatabaseColumns(val navn: String) {
+private enum class BeregningDatabaseColumns(val navn: String) {
     BeregningId("beregningId"),
     BehandlingId("behandlingId"),
     Beregningsperioder("beregningsperioder"),
@@ -72,17 +89,37 @@ private enum class DatabaseColumns(val navn: String) {
     GrunnlagVersjon("grunnlagVersjon")
 }
 
+private enum class BeregningsperiodeDatabaseColumns(val navn: String) {
+    BeregningId("beregningId"),
+    DatoFOM("datoFOM"),
+    DatoTOM("datoTOM"),
+    UtbetaltBeloep("utbetaltBeloep"),
+    SoeskenFlokk("soeskenFlokk"),
+    GrunnbeloepMnd("grunnbeloepMnd"),
+    Grunnbeloep("grunnbeloep")
+}
+
 private object Queries {
     val hentBeregning = """
         |SELECT * 
-        |FROM beregning WHERE ${DatabaseColumns.BehandlingId.navn} = :behandlingId::UUID
+        |FROM beregning WHERE ${BeregningDatabaseColumns.BehandlingId.navn} = :behandlingId::UUID
+    """.trimMargin()
+
+    val hentBeregningsperioder = """
+        |SELECT * 
+        |FROM beregningsperioder WHERE ${BeregningsperiodeDatabaseColumns.BeregningId.navn} = :beregningId::UUID
     """.trimMargin()
 
     val lagreBeregning = """
-        |INSERT INTO beregning(${DatabaseColumns.BeregningId.navn}, ${DatabaseColumns.BehandlingId.navn}, ${DatabaseColumns.BeregnetDato.navn}, ${DatabaseColumns.Beregningsperioder.navn}, ${DatabaseColumns.SakId.navn}, ${DatabaseColumns.GrunnlagVersjon.navn}) 
+        |INSERT INTO beregning(${BeregningDatabaseColumns.BeregningId.navn}, ${BeregningDatabaseColumns.BehandlingId.navn}, ${BeregningDatabaseColumns.BeregnetDato.navn}, ${BeregningDatabaseColumns.Beregningsperioder.navn}, ${BeregningDatabaseColumns.SakId.navn}, ${BeregningDatabaseColumns.GrunnlagVersjon.navn}) 
         |VALUES(:beregningId::UUID, :behandlingId::UUID, :beregnetDato::TIMESTAMP, :beregningsperioder::JSONB, :sakId::BIGINT, :grunnlagVersjon::BIGINT) 
-        |ON CONFLICT (${DatabaseColumns.BeregningId.navn})
+        |ON CONFLICT (${BeregningDatabaseColumns.BeregningId.navn})
         |DO UPDATE SET 
-        |   ${DatabaseColumns.Beregningsperioder.navn} = EXCLUDED.${DatabaseColumns.Beregningsperioder.navn}, ${DatabaseColumns.BeregnetDato} = EXCLUDED.${DatabaseColumns.BeregnetDato}
+        |   ${BeregningDatabaseColumns.Beregningsperioder.navn} = EXCLUDED.${BeregningDatabaseColumns.Beregningsperioder.navn}, ${BeregningDatabaseColumns.BeregnetDato} = EXCLUDED.${BeregningDatabaseColumns.BeregnetDato}
+    """.trimMargin()
+
+    val lagreBeregningPeriode = """
+        |INSERT INTO beregningsperiode(${BeregningsperiodeDatabaseColumns.BeregningId.navn}, ${BeregningsperiodeDatabaseColumns.DatoFOM.navn}, ${BeregningsperiodeDatabaseColumns.DatoTOM.navn}, ${BeregningsperiodeDatabaseColumns.UtbetaltBeloep.navn}, ${BeregningsperiodeDatabaseColumns.SoeskenFlokk.navn}, ${BeregningsperiodeDatabaseColumns.GrunnbeloepMnd.navn}, ${BeregningsperiodeDatabaseColumns.Grunnbeloep.navn}) 
+        |VALUES(:beregningId::UUID, :datoFOM::TEXT, :datoTOM::TEXT, :utbetaltBeloep::BIGINT, :soeskenFlokk::JSONB, :grunnbeloepMnd::BIGINT, :grunnbeloep::BIGINT) 
     """.trimMargin()
 }
--- apps/etterlatte-beregning/src/main/resources/db.migration/V1__create_initial_tables.sql
+++ apps/etterlatte-beregning/src/main/resources/db.migration/V1__create_initial_tables.sql
@@ -6,3 +6,16 @@ create TABLE beregning (
     sakId BIGINT,
     versjon BIGINT
 );
+
+create TABLE beregningsperiode (
+    beregningId UUID NOT NULL,
+    CONSTRAINT beregning FOREIGN KEY(beregningId) REFERENCES beregning(beregningId),
+
+    datoFOM TEXT,
+    datoTOM TEXT,
+    utbetaltBeloep BIGINT,
+    soeskenFlokk JSONB,
+    grunnbeloepMnd BIGINT,
+    grunnbeloep BIGINT
+);
+CREATE INDEX index_name ON beregningsperiode (beregningId);

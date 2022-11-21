b78a9c0df17cd328e08e302fb218812514673e68
Simen Johnsrud <simen.johnsrud92@gmail.com>
2022-11-21 13:17:10 +0100

    EY-1092 Fjern unødvendige dependencies

--- apps/etterlatte-beregning/build.gradle.kts
+++ apps/etterlatte-beregning/build.gradle.kts
@@ -6,28 +6,15 @@ plugins {
 
 dependencies {
 
-    implementation(Ktor2.OkHttp)
-    implementation(Ktor2.ServerCore)
-    implementation(Ktor2.ServerCio)
-    implementation(Ktor2.Auth)
-    implementation(Ktor2.Jackson)
     implementation(Ktor2.CallLogging)
     implementation(Ktor2.StatusPages)
     implementation(Ktor2.ServerContentNegotiation)
-    implementation(Ktor2.ClientCore)
-    implementation(Ktor2.ClientContentNegotiation)
-    implementation(Ktor2.ClientAuth)
     implementation(NavFelles.TokenValidationKtor2)
 
     implementation(project(":libs:ktor2client-onbehalfof"))
-
-    implementation(Jackson.DatatypeJsr310)
-    implementation(Jackson.ModuleKotlin)
-
     implementation(project(":libs:common"))
     implementation(Database.HikariCP)
     implementation(Database.FlywayDB)
-    implementation(Database.Postgresql)
     implementation(Database.KotliQuery)
 
     testImplementation(MockK.MockK)

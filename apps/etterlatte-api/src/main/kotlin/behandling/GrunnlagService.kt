549fec098569a6751e2cacea578645e0c184f434
Henrik Gundersen <henrik.gundersen@nav.no>
2022-11-22 17:04:01 +0100

    Fjerner medlemskap fra grunnlag da dette hører til gammel vilkårsvurdering (#296)
    - Koden er tilpasset å legge til medlemskapsperioder fra vilkårsvurdering - dette konseptet finnes ikke lenger
    - Kommer sannsynligvis en ny variant for å sette opp medlemskap / trygdetid senere
    
    EY-1172

--- apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagService.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/GrunnlagService.kt
@@ -3,96 +3,21 @@ package no.nav.etterlatte.behandling
 import no.nav.etterlatte.kafka.JsonMessage
 import no.nav.etterlatte.kafka.KafkaProdusent
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
-import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.AvdoedesMedlemskapsperiode
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Beregningsgrunnlag
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstype
-import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.SaksbehandlerMedlemskapsperioder
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.SoeskenMedIBeregning
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.periode.Periode
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import java.time.Instant
-import java.time.YearMonth
 import java.util.*
 
 data class GrunnlagResult(val response: String)
 
 class GrunnlagService(
     private val behandlingKlient: BehandlingKlient,
-    private val rapid: KafkaProdusent<String, String>,
-    private val grunnlagKlient: EtterlatteGrunnlag
+    private val rapid: KafkaProdusent<String, String>
 ) {
-
-    suspend fun upsertAvdoedMedlemskapPeriode(
-        behandlingId: String,
-        periode: AvdoedesMedlemskapsperiode,
-        saksbehandlerId: String,
-        token: String
-    ): GrunnlagResult {
-        val behandling = behandlingKlient.hentBehandling(behandlingId, token)
-        val avdoedFnr = behandling.avdoed?.singleOrNull()
-            ?: throw RuntimeException("Flere avdøde oppdaget på sak ${behandling.sak} ved upserting av opplysning") // TODO sj: Håndterer ikke flere avdøde
-        val periodisertOpplysning =
-            lagOpplysning(
-                opplysningsType = Opplysningstype.MEDLEMSKAPSPERIODE,
-                kilde = Grunnlagsopplysning.Saksbehandler(saksbehandlerId, Instant.now()),
-                opplysning = periode,
-                fnr = Foedselsnummer.of(avdoedFnr),
-                periode = Periode(YearMonth.from(periode.fraDato), YearMonth.from(periode.tilDato))
-            )
-
-        rapid.publiser(
-            behandlingId,
-            JsonMessage.newMessage(
-                eventName = "OPPLYSNING:NY",
-                map = mapOf("opplysning" to periodisertOpplysning, "sakId" to behandling.sak)
-            ).toJson()
-        )
-
-        return GrunnlagResult("Lagret")
-    }
-
-    suspend fun slettAvdoedMedlemskapPeriode(
-        behandlingId: String,
-        saksbehandlerPeriodeId: String,
-        saksbehandlerId: String,
-        token: String
-    ): GrunnlagResult {
-        val behandling = behandlingKlient.hentBehandling(behandlingId, token)
-        val avdoedFnr = behandling.avdoed?.singleOrNull()
-            ?: throw RuntimeException("Flere avdøde oppdaget på sak ${behandling.sak} ved sletting av opplysning") // TODO sj: Håndterer ikke flere avdøde
-        val grunnlag: Grunnlagsopplysning<SaksbehandlerMedlemskapsperioder>? = grunnlagKlient.finnPerioder(
-            behandling.sak,
-            Opplysningstype.MEDLEMSKAPSPERIODE,
-            token
-        )
-
-        val grunnlagUtenPeriode = grunnlag?.opplysning?.perioder?.singleOrNull { it.id != saksbehandlerPeriodeId }
-            ?: throw RuntimeException("Fant ikke opplysning med id $saksbehandlerPeriodeId på sak ${behandling.sak}")
-
-        val periodisertOpplysning =
-            // TODO ai: vurder å implementere "sletting"
-            lagOpplysning(
-                opplysningsType = Opplysningstype.MEDLEMSKAPSPERIODE,
-                kilde = Grunnlagsopplysning.Saksbehandler(saksbehandlerId, Instant.now()),
-                opplysning = null,
-                fnr = Foedselsnummer.of(avdoedFnr),
-                periode = Periode(
-                    YearMonth.from(grunnlagUtenPeriode.fraDato),
-                    YearMonth.from(grunnlagUtenPeriode.tilDato)
-                )
-            )
-
-        rapid.publiser(
-            behandlingId,
-            JsonMessage.newMessage(
-                eventName = "OPPLYSNING:NY",
-                map = mapOf("opplysning" to periodisertOpplysning, "sakId" to behandling.sak)
-            ).toJson()
-        )
-
-        return GrunnlagResult("Slettet")
-    }
 
     suspend fun lagreSoeskenMedIBeregning(
         behandlingId: String,

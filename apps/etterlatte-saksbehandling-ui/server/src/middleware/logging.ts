d10bf0911d75731e5b3d8d25edb3f305e46a6455
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2022-11-28 16:16:57 +0100

    Logg http statuscode from frackend

--- apps/etterlatte-saksbehandling-ui/server/src/middleware/logging.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/middleware/logging.ts
@@ -2,11 +2,17 @@ import { NextFunction, Request, Response } from 'express'
 import { logger } from '../utils/logger'
 
 const requestLoggerIncludeHeaders = (req: Request, res: Response, next: NextFunction) => {
+  res.on('finish', () => {
+    logger.info('Response code', { statusCode: res.statusCode })
+  })
   logger.info('Request', { ...req.headers, url: req.url })
   next()
 }
 
 const requestLoggerExcludeHeaders = (req: Request, res: Response, next: NextFunction) => {
+  res.on('finish', () => {
+    logger.info('Response code', { statusCode: res.statusCode })
+  })
   logger.info('Request', { url: req.url })
   next()
 }

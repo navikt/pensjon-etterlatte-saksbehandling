6886bd3357cd4aae277ae20f6e98b1edad9db151
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2022-12-05 10:36:17 +0100

    Bugfix/avkorting (#329)
    * Oppdatere avkorting test og testdata
    
    * Populer beregningsmelding med grunnlag

--- apps/etterlatte-avkorting/src/test/kotlin/LesAvkortingsmeldingTest.kt
+++ apps/etterlatte-avkorting/src/test/kotlin/LesAvkortingsmeldingTest.kt
@@ -1,7 +1,9 @@
 import model.AvkortingService
+import no.nav.etterlatte.libs.common.event.BehandlingGrunnlagEndret
 import no.nav.etterlatte.libs.common.rapidsandrivers.eventNameKey
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.io.FileNotFoundException
 
@@ -16,15 +18,12 @@ internal class LesAvkortingsmeldingTest {
 
     private val inspector = TestRapid().apply { LesAvkortingsmelding(this, AvkortingService()) }
 
-    // TODO fikse denne
-    // @Test
+    @Test
     fun `skal vurdere viklaar til gyldig`() {
         val inspector = inspector.apply { sendTestMessage(melding) }.inspektør
 
-        Assertions.assertEquals("BEHANDLING:GRUNNLAGENDRET", inspector.message(0).get(eventNameKey).asText())
-        // TODO oppdatere testen
-        // Assertions.assertEquals(3, inspector.message(0).get("vilkaarsvurdering").size())
-
-        // verify { fordelerMetricLogger.logMetricFordelt() }
+        Assertions.assertEquals(BehandlingGrunnlagEndret.eventName, inspector.message(0).get(eventNameKey).asText())
+        Assertions.assertEquals(4, inspector.message(0).get("vilkaarsvurdering").size())
+        Assertions.assertEquals(5, inspector.message(0).get("beregning").size())
     }
 }

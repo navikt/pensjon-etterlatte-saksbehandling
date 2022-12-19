a02d4adbee14b8ce5b709b8ac8b3ea46f36a62f8
Nicolas N <nicolas.nordhagen@nav.no>
2022-12-19 13:34:37 +0100

    Forenkle uthenting av grunnlag

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/behandling/Behandling.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/behandling/Behandling.kt
@@ -8,8 +8,10 @@ import no.nav.etterlatte.libs.common.grunnlag.Grunnlag
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
 import no.nav.etterlatte.libs.common.grunnlag.hentDoedsdato
 import no.nav.etterlatte.libs.common.grunnlag.hentFoedselsnummer
+import no.nav.etterlatte.libs.common.grunnlag.hentKonstantOpplysning
 import no.nav.etterlatte.libs.common.grunnlag.hentNavn
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.InnsenderSoeknad
+import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstype
 import no.nav.etterlatte.libs.common.soeknad.dataklasser.common.Spraak
 import no.nav.etterlatte.libs.common.vedtak.VedtakType
 import java.time.LocalDate
@@ -20,7 +22,6 @@ data class Behandling(
     val spraak: Spraak,
     val persongalleri: Persongalleri,
     val vedtak: ForenkletVedtak,
-    val grunnlag: Grunnlag,
     val utbetalingsinfo: Utbetalingsinfo? = null
 ) {
     init {
@@ -71,4 +72,25 @@ fun Grunnlag.mapAvdoed(): Avdoed = with(this.familie) {
         navn = avdoed.hentNavn()!!.verdi.let { "${it.fornavn} ${it.etternavn}" },
         doedsdato = avdoed.hentDoedsdato()!!.verdi!!
     )
+}
+
+fun Grunnlag.mapInnsender(): Innsender = with(this.sak) {
+    val opplysning = hentKonstantOpplysning<InnsenderSoeknad>(Opplysningstype.INNSENDER_SOEKNAD_V1)
+
+    val innsender = requireNotNull(opplysning?.verdi) {
+        "Sak (id=${metadata.sakId}) mangler opplysningstype INNSENDER_SOEKNAD_V1"
+    }
+
+    Innsender(
+        navn = innsender.let { "${it.fornavn} ${it.etternavn}" },
+        fnr = innsender.foedselsnummer.value
+    )
+}
+
+fun Grunnlag.mapSpraak(): Spraak = with(this.sak) {
+    val opplysning = hentKonstantOpplysning<Spraak>(Opplysningstype.SPRAAK)
+
+    requireNotNull(opplysning?.verdi) {
+        "Sak (id=${metadata.sakId}) mangler opplysningstype SPRAAK"
+    }
 }
--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/grunnlag/GrunnlagKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/grunnlag/GrunnlagKlient.kt
@@ -5,10 +5,6 @@ import com.typesafe.config.Config
 import io.ktor.client.HttpClient
 import no.nav.etterlatte.libs.common.deserialize
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlag
-import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
-import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.InnsenderSoeknad
-import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstype
-import no.nav.etterlatte.libs.common.soeknad.dataklasser.common.Spraak
 import no.nav.etterlatte.libs.ktorobo.AzureAdClient
 import no.nav.etterlatte.libs.ktorobo.DownstreamResourceClient
 import no.nav.etterlatte.libs.ktorobo.Resource
@@ -24,30 +20,24 @@ class GrunnlagKlient(config: Config, httpClient: HttpClient) {
     private val clientId = config.getString("grunnlag.client.id")
     private val baseUrl = config.getString("grunnlag.resource.url")
 
-    suspend fun hentGrunnlag(sakid: Long, accessToken: String): Grunnlag =
-        hentGrunnlagJson(sakid, null, accessToken)
-            .let { deserialize(it.toString()) }
+    suspend fun hentGrunnlag(sakid: Long, accessToken: String): Grunnlag {
+        try {
+            val json =
+                downstreamResourceClient.get(
+                    Resource(clientId, "$baseUrl/api/grunnlag/$sakid"),
+                    accessToken
+                ).mapBoth(
+                    success = { json -> json },
+                    failure = { errorMessage ->
+                        logger.error("Henting av grunnlag for sakId:$sakid feilet", errorMessage.throwable)
+                        null
+                    }
+                )?.response
 
-    suspend fun hentInnsender(sakid: Long, accessToken: String): Grunnlagsopplysning<InnsenderSoeknad> =
-        hentGrunnlagJson(sakid, Opplysningstype.INNSENDER_SOEKNAD_V1, accessToken)
-            .let { deserialize(it.toString()) }
-
-    suspend fun hentSpraak(sakId: Long, accessToken: String): Grunnlagsopplysning<Spraak> =
-        hentGrunnlagJson(sakId, Opplysningstype.SPRAAK, accessToken)
-            .let { deserialize(it.toString()) }
-
-    private suspend fun hentGrunnlagJson(sakid: Long, type: Opplysningstype?, accessToken: String): Any? {
-        val typeParam = if (type == null) "" else "/$type"
-
-        return downstreamResourceClient.get(
-            Resource(clientId, "$baseUrl/api/grunnlag/$sakid$typeParam"),
-            accessToken
-        ).mapBoth(
-            success = { json -> json },
-            failure = { errorMessage ->
-                logger.error("Henting av grunnlag (type=$type) for sakid=$sakid feilet", errorMessage.throwable)
-                null
-            }
-        )?.response
+            return deserialize(json.toString())
+        } catch (e: Exception) {
+            logger.error("Henting av grunnlag for sakId:$sakid feilet", e)
+            throw e
+        }
     }
 }

9e47c109e8f67029586969c2396016800f6b7e2e
Emil Schrøder <emil.schroder@nav.no>
2022-12-27 12:07:44 +0100

    Bytt ut tags til designbiblioteket
    EY-1451

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/fargetags/nasjonalitetsType.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/fargetags/nasjonalitetsType.tsx
@@ -1,20 +1,4 @@
-import { formaterEnumTilLesbarString } from '~utils/formattering'
-import { ColorTagBehandling } from './styled'
-
 export enum INasjonalitetsType {
   NASJONAL = 'nasjonal',
   UTLAND = 'utland',
-}
-
-const colors = {
-  [INasjonalitetsType.NASJONAL]: '#6da16b',
-  [INasjonalitetsType.UTLAND]: '#c4c38b',
-}
-
-export const NasjonalitetsType: React.FC<{ type: INasjonalitetsType }> = ({ type }) => {
-  return <ColorTagBehandling color={getColor(type)}>{formaterEnumTilLesbarString(type)}</ColorTagBehandling>
-}
-
-function getColor(type: INasjonalitetsType) {
-  return colors[type]
 }

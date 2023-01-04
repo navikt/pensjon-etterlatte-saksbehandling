6919782f43f8e644b03126110a34c410c544f665
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2023-01-04 13:00:37 +0100

    Viser grunnlagsendringshendelser basert på samsvar
    Nå som grunnlagsendringshendelser baserer seg på en sjekk om det er en
    forskjell mellom det vi har i grunnlag og det som ligger i PDL må
    dette også vises fram i frontend. Utseende er veldig WIP utviklerdesign,
    det trengs litt avklaringer her med fag og designere

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/grunnlagshendelser/Grunnlagsendringshendelser.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/grunnlagshendelser/Grunnlagsendringshendelser.tsx
@@ -1,94 +1,241 @@
 import {
-  Doedshendelse,
+  AnsvarligeForeldreSamsvar,
+  BarnSamsvar,
+  DoedsdatoSamsvar,
   Grunnlagsendringshendelse,
+  GrunnlagsendringStatus,
   GrunnlagsendringsType,
-  Grunnlagsinformasjon,
-  Utflyttingshendelse,
+  Saksrolle,
+  SamsvarMellomGrunnlagOgPdl,
+  UtlandSamsvar,
 } from '../typer'
-import { Table } from '@navikt/ds-react'
 import { formaterStringDato } from '~utils/formattering'
+import styled from 'styled-components'
+import { Heading } from '@navikt/ds-react'
 
 const teksterForGrunnlagshendelser: Record<GrunnlagsendringsType, string> = {
-  GJENLEVENDE_FORELDER_DOED: 'Dødsfall gjenlevende forelder',
-  SOEKER_DOED: 'Dødsfall søker',
-  SOESKEN_DOED: 'Dødsfall søsken',
-  UTFLYTTING: 'Utflytting',
+  ANSVARLIGE_FORELDRE: 'Ansvarlige foreldre',
+  BARN: 'Barn',
+  UTLAND: 'Ut-/innflytting',
+  DOEDSDATO: 'Dødsfall',
 }
 
-const VisUtflytting = ({ utflytting }: { utflytting: Utflyttingshendelse }) => {
+const formaterKanskjeStringDato = (fallback = 'Ukjent dato', dato?: string): string =>
+  dato ? formaterStringDato(dato) : fallback
+
+const VisDoedsdatoSamsvar = (props: { data: DoedsdatoSamsvar }) => {
   return (
-    <p>
-      {`${utflytting.fnr} har flyttet ut til ${utflytting.tilflyttingsLand}
-      ${utflytting?.tilflyttingsstedIUtlandet ? utflytting.tilflyttingsstedIUtlandet : ''} den 
-      ${formaterStringDato(utflytting.utflyttingsdato)}`}
-    </p>
+    <HendelseSammenligning>
+      <dt>Dødsdato i grunnlag</dt>
+      <dd>{formaterKanskjeStringDato('Ingen', props.data.fraGrunnlag)}</dd>
+      <dt>Dødsdato i PDL</dt>
+      <dd>{formaterKanskjeStringDato('Ingen', props.data.fraGrunnlag)}</dd>
+    </HendelseSammenligning>
   )
 }
 
-const formaterKanskjeStringDato = (dato?: string): string => (dato ? formaterStringDato(dato) : 'Ukjent dato')
+const VisBarnSamsvar = (props: { data: BarnSamsvar }) => {
+  return (
+    <HendelseSammenligning>
+      <dt>Barn i grunnlag</dt>
+      <dd>{props.data.fraGrunnlag?.join(', ') ?? 'Ingen barn'}</dd>
+      <dt>Barn i PDL</dt>
+      <dd>{props.data.fraPdl?.join(', ') ?? 'Ingen barn'}</dd>
+    </HendelseSammenligning>
+  )
+}
 
-const VisDoedshendelse = ({ doedshendelse }: { doedshendelse: Doedshendelse }) => (
-  <p>
-    {doedshendelse.avdoedFnr} døde {formaterKanskjeStringDato(doedshendelse.doedsdato)}
-  </p>
-)
+const LandListeMedDatoOgOverskrift = (props: { tittel: string; land: { land?: string; dato?: string }[] }) => {
+  if (!props.land || props.land.length === 0) {
+    return (
+      <>
+        <strong>{props.tittel}</strong>
+        <p>Ingen</p>
+      </>
+    )
+  }
+  return (
+    <>
+      <strong>{props.tittel}</strong>
+      <ul style={{ listStyle: 'none' }}>
+        {props.land.map(({ land, dato }, i) => (
+          <li key={i}>
+            {land} - {formaterKanskjeStringDato(dato)}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
-const HendelseVisning = (props: { data?: Grunnlagsinformasjon }) => {
+const VisUtlandSamsvar = (props: { data: UtlandSamsvar }) => {
+  return (
+    <HendelseSammenligning>
+      <dt>Ut-/innflyttinger i grunnlag</dt>
+      <dd>
+        <LandListeMedDatoOgOverskrift
+          tittel={'Utflyttinger'}
+          land={
+            props.data.fraGrunnlag?.utflyttingFraNorge?.map(({ tilflyttingsland, dato }) => ({
+              land: tilflyttingsland,
+              dato,
+            })) ?? []
+          }
+        />
+        <LandListeMedDatoOgOverskrift
+          tittel={'Innflyttinger'}
+          land={
+            props.data.fraGrunnlag?.innflyttingTilNorge?.map(({ fraflyttingsland, dato }) => ({
+              land: fraflyttingsland,
+              dato,
+            })) ?? []
+          }
+        />
+      </dd>
+      <dt>Ut-/innflyttinger i PDL</dt>
+      <dd>
+        <LandListeMedDatoOgOverskrift
+          tittel={'Utflyttinger'}
+          land={
+            props.data.fraPdl?.utflyttingFraNorge?.map(({ tilflyttingsland, dato }) => ({
+              land: tilflyttingsland,
+              dato,
+            })) ?? []
+          }
+        />
+        <LandListeMedDatoOgOverskrift
+          tittel={'Innflyttinger'}
+          land={
+            props.data.fraPdl?.innflyttingTilNorge?.map(({ fraflyttingsland, dato }) => ({
+              land: fraflyttingsland,
+              dato,
+            })) ?? []
+          }
+        />
+      </dd>
+    </HendelseSammenligning>
+  )
+}
+
+const VisAnsvarligeForeldreSamsvar = (props: { data: AnsvarligeForeldreSamsvar }) => {
+  return (
+    <HendelseSammenligning>
+      <dt>Ansvarlige foreldre i grunnlag</dt>
+      <dd>{props.data.fraGrunnlag?.join(', ') ?? 'Ingen ansvarlige foreldre'}</dd>
+      <dt>Ansvarlige foreldre i PDL</dt>
+      <dd>{props.data.fraPdl?.join(', ') ?? 'Ingen ansvarlige foreldre'}</dd>
+    </HendelseSammenligning>
+  )
+}
+
+const HendelseVisning = (props: { data?: SamsvarMellomGrunnlagOgPdl }) => {
+  const type = props.data?.type
   switch (props.data?.type) {
-    case 'UTFLYTTING':
-      return <VisUtflytting utflytting={props.data.hendelse} />
-    case 'GJENLEVENDE_FORELDER_DOED':
-    case 'SOEKER_DOED':
-    case 'SOESKEN_DOED':
-      return <VisDoedshendelse doedshendelse={props.data.hendelse} />
+    case 'DOEDSDATO':
+      return <VisDoedsdatoSamsvar data={props.data} />
+    case 'BARN':
+      return <VisBarnSamsvar data={props.data} />
+    case 'UTLAND':
+      return <VisUtlandSamsvar data={props.data} />
+    case 'ANSVARLIGE_FORELDRE':
+      return <VisAnsvarligeForeldreSamsvar data={props.data} />
     default:
-      return <p>Ukjent hendelse</p>
+      return <p>Ukjent hendelse av type {type}</p>
   }
 }
 
-const TabellForHendelser = ({ hendelser }: { hendelser: Grunnlagsendringshendelse[] }) => (
-  <Table>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Type</Table.HeaderCell>
-        <Table.HeaderCell>Dato</Table.HeaderCell>
-        <Table.HeaderCell>Ekstra data</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      {hendelser.map((hendelse) => (
-        <Table.Row key={hendelse.id}>
-          <Table.DataCell>{teksterForGrunnlagshendelser[hendelse.type]}</Table.DataCell>
-          <Table.DataCell>{formaterStringDato(hendelse.opprettet)}</Table.DataCell>
-          <Table.DataCell>
-            <HendelseVisning data={hendelse.data} />
-          </Table.DataCell>
-        </Table.Row>
-      ))}
-    </Table.Body>
-  </Table>
+const HendelseSammenligning = styled.dl`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(2, auto);
+  grid-gap: 5px;
+
+  p {
+    margin: 0;
+  }
+`
+// TODO: Hele dette designet må tas en avsjekk med fag / design om det
+//      1. dekker behovet for å se relevante opplysninger
+//      2. må fikses så det ikke ser helt løk ut
+const HendelseWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 5px;
+  background-color: #f5f5f5;
+  margin-bottom: 30px;
+`
+
+const HendelseMetaWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  border-bottom: 1px solid black;
+`
+
+const HendelseMetaItem = styled.div`
+  display: inline-block;
+  width: 33%;
+  padding-right: 10px;
+  padding: 5px;
+  font-weight: bold;
+
+  small {
+    font-weight: normal;
+    font-size: 14px;
+  }
+`
+
+const teksterForSaksrolle: Record<Saksrolle, string> = {
+  AVDOED: 'Avdød',
+  GJENLEVENDE: 'Gjenlevende',
+  INNSENDER: 'Innsender',
+  SOEKER: 'Søker',
+  SOESKEN: 'Søsken',
+  UKJENT: 'Ukjent',
+}
+
+const ListeAvHendelser = ({ hendelser }: { hendelser: Grunnlagsendringshendelse[] }) => (
+  <div>
+    {hendelser.map((hendelse) => (
+      <HendelseWrapper key={hendelse.id}>
+        <HendelseMetaWrapper>
+          <HendelseMetaItem>{teksterForGrunnlagshendelser[hendelse.type]}</HendelseMetaItem>
+          <HendelseMetaItem>
+            Gjelder {teksterForSaksrolle[hendelse.hendelseGjelderRolle].toLowerCase()} ({hendelse.gjelderPerson})
+          </HendelseMetaItem>
+          <HendelseMetaItem>Opprettet {formaterStringDato(hendelse.opprettet)}</HendelseMetaItem>
+        </HendelseMetaWrapper>
+        <div>
+          <HendelseVisning data={hendelse.samsvarMedPdl} />
+        </div>
+      </HendelseWrapper>
+    ))}
+  </div>
 )
 
+const STATUSER_SOM_ER_UHAANDTERTE: Readonly<GrunnlagsendringStatus[]> = ['VENTER_PAA_JOBB', 'SJEKKET_AV_JOBB'] as const
+
 export const Grunnlagshendelser = ({ hendelser }: { hendelser: Grunnlagsendringshendelse[] }) => {
-  const uhaandterteHendelser = hendelser.filter((hendelse) =>
-    ['IKKE_VURDERT', 'GYLDIG_OG_KAN_TAS_MED_I_BEHANDLING'].includes(hendelse.status)
-  )
-  const vurderteHendelser = hendelser.filter(
-    (hendelse) => !['IKKE_VURDERT', 'GYLDIG_OG_KAN_TAS_MED_I_BEHANDLING'].includes(hendelse.status)
-  )
+  const uhaandterteHendelser = hendelser.filter((hendelse) => STATUSER_SOM_ER_UHAANDTERTE.includes(hendelse.status))
+  const vurderteHendelser = hendelser.filter((hendelse) => !STATUSER_SOM_ER_UHAANDTERTE.includes(hendelse.status))
 
   return (
     <>
       {uhaandterteHendelser.length > 0 ? (
         <div>
-          <h2>Uhåndterte hendelser</h2>
-          <TabellForHendelser hendelser={uhaandterteHendelser} />
+          <Heading size="medium" level="2">
+            Uhåndterte hendelser
+          </Heading>
+          <ListeAvHendelser hendelser={uhaandterteHendelser} />
         </div>
       ) : null}
       {vurderteHendelser.length > 0 ? (
         <div>
-          <h2>Tidligere hendelser</h2>
-          <TabellForHendelser hendelser={vurderteHendelser} />
+          <Heading size="medium" level="2">
+            Tidligere hendelser
+          </Heading>
+          <ListeAvHendelser hendelser={vurderteHendelser} />
         </div>
       ) : null}
     </>

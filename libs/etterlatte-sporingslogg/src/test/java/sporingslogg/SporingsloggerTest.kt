1e78de55a1149ccba61c7a083e7c07ef2897c448
Mads Opheim <mads.opheim@nav.no>
2023-01-06 10:27:02 +0100

    Test også på manglande tilgang

--- libs/etterlatte-sporingslogg/src/test/java/sporingslogg/SporingsloggerTest.kt
+++ libs/etterlatte-sporingslogg/src/test/java/sporingslogg/SporingsloggerTest.kt
@@ -10,15 +10,15 @@ import java.time.ZonedDateTime
 
 internal class SporingsloggerTest {
 
+    private val timestamp =
+        ZonedDateTime.of(
+            LocalDate.of(2020, Month.FEBRUARY, 1),
+            LocalTime.NOON,
+            ZoneId.of("Europe/Oslo")
+        ).toInstant()
+
     @Test
     fun `skal logge i CEF-format`() {
-        /* Formatet: CEF:Version|Device Vendor|Device Product|Device Version|Device Event Class ID|Name|Severity|[Extension] */
-        val timestamp =
-            ZonedDateTime.of(
-                LocalDate.of(2020, Month.FEBRUARY, 1),
-                LocalTime.NOON,
-                ZoneId.of("Europe/Oslo")
-            ).toInstant()
         val entry = CEFEntry(
             deviceEventClassId = DeviceEventClassId.Access,
             deviceProduct = "vilkaarsvurdering",
@@ -34,9 +34,29 @@ internal class SporingsloggerTest {
             )
         )
         assertEquals(
-            "CEF:0|pensjon|etterlatte-vilkaarsvurdering|1.0|audit:access|On-behalf-of access|INFO|" +
-                "end=1580554800000 suid=U123456 duid=10458210115 sproc=vilkaarsvurdering " +
-                "flexString1Label=Decision flexString1=Permit msg=Internbruker sjekker barnepensjon for innbygger",
+            "CEF:0|pensjon|etterlatte-vilkaarsvurdering|1.0|audit:access|On-behalf-of access|INFO|end=1580554800000 suid=U123456 duid=10458210115 sproc=vilkaarsvurdering flexString1Label=Decision flexString1=Permit msg=Internbruker sjekker barnepensjon for innbygger", // ktlint-disable max-line-length
+            entry.format()
+        )
+    }
+
+    @Test
+    fun `kan logge manglende tilgang`() {
+        val entry = CEFEntry(
+            deviceEventClassId = DeviceEventClassId.Update,
+            deviceProduct = "vilkaarsvurdering",
+            name = Name.OnBehalfOfAccess,
+            severity = Severity.WARN,
+            extension = Extension(
+                endTime = timestamp,
+                sourceUserId = "U123456",
+                destinationUserId = "10458210115",
+                sourceProcessName = "vilkaarsvurdering",
+                flexString1 = Decision.Deny,
+                message = "Internbruker oppdaterer barnepensjon for innbygger"
+            )
+        )
+        assertEquals(
+            "CEF:0|pensjon|etterlatte-vilkaarsvurdering|1.0|audit:update|On-behalf-of access|WARN|end=1580554800000 suid=U123456 duid=10458210115 sproc=vilkaarsvurdering flexString1Label=Decision flexString1=Deny msg=Internbruker oppdaterer barnepensjon for innbygger", // ktlint-disable max-line-length
             entry.format()
         )
     }

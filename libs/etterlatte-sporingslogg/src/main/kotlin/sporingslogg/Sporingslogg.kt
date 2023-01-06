87ebf41680ede14753f496adbafba93053591132
Mads Opheim <mads.opheim@nav.no>
2023-01-06 13:13:37 +0100

    Pirk

--- libs/etterlatte-sporingslogg/src/main/kotlin/sporingslogg/Sporingslogg.kt
+++ libs/etterlatte-sporingslogg/src/main/kotlin/sporingslogg/Sporingslogg.kt
@@ -2,7 +2,7 @@ package sporingslogg
 
 class Sporingslogg {
 
-    val auditLogger = org.slf4j.LoggerFactory.getLogger("AUDIT_LOGGER")
+    private val auditLogger = org.slf4j.LoggerFactory.getLogger("auditLogger")
 
     fun logg(sporingsrequest: Sporingsrequest) {
         auditLogger.info(sporingsrequest.tilCEFEntry().format())

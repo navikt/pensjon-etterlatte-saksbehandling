58a5a009a844cb47dc1b8e5a1bdd80979a55a356
Mads Opheim <mads.opheim@nav.no>
2023-01-09 16:13:55 +0100

    Import

--- apps/etterlatte-api/src/main/kotlin/behandling/BehandlingRoute.kt
+++ apps/etterlatte-api/src/main/kotlin/behandling/BehandlingRoute.kt
@@ -7,7 +7,6 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route

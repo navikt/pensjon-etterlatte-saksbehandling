1c8209c67beae27f039956e52a5aac68c3f5d93b
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2023-01-10 16:50:01 +0100

    Fjerner testside som uansett ikke fungerer og ikke er i bruk (#442)

--- apps/etterlatte-saksbehandling-ui/client/src/App.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/App.tsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import '@navikt/ds-css'
-import "@navikt/ds-css-internal";
+import '@navikt/ds-css-internal'
 import Oppgavebenken from './components/oppgavebenken/Oppgavebenken'
 import { Behandling } from './components/behandling/Behandling'
 import { HeaderWrapper } from './shared/header/Header'
 import { Person } from './components/person/Person'
 import useInnloggetSaksbehandler from './shared/hooks/useInnloggetSaksbehandler'
-import { Testside } from './components/testside/Testside'
 import nb from 'date-fns/locale/nb'
 import { registerLocale } from 'react-datepicker'
 
@@ -25,7 +24,6 @@ function App() {
               <Route path="/oppgavebenken" element={<Oppgavebenken />} />
               <Route path="/person/:fnr" element={<Person />} />
               <Route path="/behandling/:behandlingId/*" element={<Behandling />} />
-              <Route path="/test" element={<Testside />} />
             </Routes>
           </BrowserRouter>
         </div>

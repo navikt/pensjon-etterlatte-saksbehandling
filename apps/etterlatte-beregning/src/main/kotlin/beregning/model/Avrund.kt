800dc95fc12a6a9204fd0566ace08c6e14c45839
henrikg <henrik.gundersen@nav.no>
2023-01-18 14:27:02 +0100

    Fjerner gammel beregningslogikk

--- apps/etterlatte-beregning/src/main/kotlin/beregning/model/Avrund.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.etterlatte.beregning.model
-
-import java.math.BigDecimal
-import java.math.RoundingMode
-
-internal fun avrund(value: BigDecimal): Int {
-    return value.setScale(0, RoundingMode.HALF_UP).toInt()
-}

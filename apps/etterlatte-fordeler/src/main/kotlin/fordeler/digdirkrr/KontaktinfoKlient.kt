1fcba1026f01dbdddbad3e406901b67558b5afa3
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-01-19 11:48:49 +0100

    EY-1296 Add nav-call-id

--- apps/etterlatte-fordeler/src/main/kotlin/fordeler/digdirkrr/KontaktinfoKlient.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/fordeler/digdirkrr/KontaktinfoKlient.kt
@@ -9,16 +9,19 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import no.nav.etterlatte.libs.common.logging.getCorrelationId
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import org.slf4j.LoggerFactory
 
 class KontaktinfoKlient(private val client: HttpClient, private val apiUrl: String) {
     private val logger = LoggerFactory.getLogger(this.javaClass.name)
 
+    // TODO: må håndtere 404 hvis personen ikke finnes i PDL eller 403 om appen ikke har tilgang til vedkommende
     suspend fun hentSpraak(foedselsnummer: Foedselsnummer): KontaktInfo {
         return client.get("$apiUrl/rest/v1/person") {
             accept(ContentType.Application.Json)
             contentType(ContentType.Application.Json)
+            header("Nav-Call-Id", getCorrelationId())
             header("Nav-Personident", foedselsnummer.value)
         }.body()
     }

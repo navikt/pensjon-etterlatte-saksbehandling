2b2e22f17a373c10591de91f76929b7be399f442
Mads Opheim <mads.opheim@nav.no>
2023-01-26 12:02:24 +0100

    EY-1621: mock-oauth2-server bruker ein gamal versjon av Eclipse-lisensen, som snyk klagar på. Den nyaste er på ganske ny MIT

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -2,7 +2,7 @@ object NavFelles {
     const val RapidAndRiversKtor2 = "com.github.navikt:rapids-and-rivers:2022061809451655538329.d6deccc62862"
     const val TokenClientCore = "no.nav.security:token-client-core:2.1.2"
     const val TokenValidationKtor2 = "no.nav.security:token-validation-ktor-v2:2.1.2"
-    const val MockOauth2Server = "no.nav.security:mock-oauth2-server:0.5.4"
+    const val MockOauth2Server = "no.nav.security:mock-oauth2-server:0.5.7"
 }
 
 object Cache {

7a76952d3b226f7db7b0de9772226888dffc354d
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-01-26 12:47:50 +0100

    EY-1624 Redirecte til forside hvis ugydlig url (#529)

--- apps/etterlatte-saksbehandling-ui/client/src/App.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import '@navikt/ds-css'
 import '@navikt/ds-css-internal'
 import Oppgavebenken from './components/oppgavebenken/Oppgavebenken'
-import { Behandling } from './components/behandling/Behandling'
-import { HeaderWrapper } from './shared/header/Header'
-import { Person } from './components/person/Person'
+import { Behandling } from '~components/behandling/Behandling'
+import { HeaderWrapper } from '~shared/header/Header'
+import { Person } from '~components/person/Person'
 import useInnloggetSaksbehandler from './shared/hooks/useInnloggetSaksbehandler'
 import nb from 'date-fns/locale/nb'
 import { registerLocale } from 'react-datepicker'
@@ -24,6 +24,7 @@ function App() {
               <Route path="/oppgavebenken" element={<Oppgavebenken />} />
               <Route path="/person/:fnr" element={<Person />} />
               <Route path="/behandling/:behandlingId/*" element={<Behandling />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </BrowserRouter>
         </div>

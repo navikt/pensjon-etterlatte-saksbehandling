5e518fae981981461cdf649db837afe3847c9850
Simen Johnsrud <simejo@users.noreply.github.com>
2023-01-27 10:33:11 +0100

    Update apps/etterlatte-saksbehandling-ui/client/src/shared/api/person.ts
    EY-1619 Bedre regex
    
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>

--- apps/etterlatte-saksbehandling-ui/client/src/shared/api/person.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/api/person.ts
@@ -1,7 +1,7 @@
 import { apiClient, ApiResponse } from './apiClient'
 import { IPersonResult } from '~components/person/typer'
 
-export const INVALID_FNR = (input: string | undefined) => /[a-zA-Z]/g.test(input ?? '') || input?.length !== 11
+export const INVALID_FNR = (input: string | undefined) => !/^\d{11}$/.test(input ?? '')
 
 export const getPerson = async (fnr: string): Promise<ApiResponse<IPersonResult>> => {
   return apiClient.post(`/grunnlag/person`, {

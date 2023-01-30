90a249858fafc396a6a0d5745f28216b1417ddbc
Mads Opheim <mads.opheim@nav.no>
2023-01-30 15:25:42 +0100

    EY-1633: Oppgraderer også avro-avhengnadane

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -46,8 +46,8 @@ object Ktor2 {
 
 object Kafka {
     const val Clients = "org.apache.kafka:kafka-clients:3.1.1"
-    const val Avro = "org.apache.avro:avro:1.11.0"
-    const val AvroSerializer = "io.confluent:kafka-avro-serializer:7.1.1"
+    const val Avro = "org.apache.avro:avro:1.11.1"
+    const val AvroSerializer = "io.confluent:kafka-avro-serializer:7.3.1"
     const val EmbeddedEnv = "no.nav:kafka-embedded-env:3.2.1"
 }
 

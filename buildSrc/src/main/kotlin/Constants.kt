dbaba7ee697d5a8ec315b4f42c3716ec326d20cb
Mads Opheim <mads.opheim@nav.no>
2023-01-30 15:31:45 +0100

    EY-1633: Oppgraderer også kafka-klient, pluss variabel for testcontainers som bør ha samme versjon

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -45,7 +45,7 @@ object Ktor2 {
 }
 
 object Kafka {
-    const val Clients = "org.apache.kafka:kafka-clients:3.1.1"
+    const val Clients = "org.apache.kafka:kafka-clients:3.3.2"
     const val Avro = "org.apache.avro:avro:1.11.1"
     const val AvroSerializer = "io.confluent:kafka-avro-serializer:7.3.1"
     const val EmbeddedEnv = "no.nav:kafka-embedded-env:3.2.1"
@@ -103,6 +103,7 @@ object Database {
 }
 
 object TestContainer {
-    const val Jupiter = "org.testcontainers:junit-jupiter:1.17.3"
-    const val Postgresql = "org.testcontainers:postgresql:1.17.3"
+    private const val version = "1.17.3"
+    const val Jupiter = "org.testcontainers:junit-jupiter:$version"
+    const val Postgresql = "org.testcontainers:postgresql:$version"
 }

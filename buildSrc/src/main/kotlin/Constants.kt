b553202bce62a64d487abe686ce06675e56c9259
Mads Opheim <mads.opheim@nav.no>
2023-01-30 13:21:40 +0100

    EY-1633: Variabel for coroutines-avhengnadar som bør vera like

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -10,8 +10,9 @@ object Cache {
 }
 
 object Kotlinx {
-    const val CoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
-    const val CoroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4"
+    private const val version = "1.6.4"
+    const val CoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+    const val CoroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
 }
 
 object Ktor2 {

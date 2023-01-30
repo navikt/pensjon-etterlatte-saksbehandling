a1b26294ad5b6f0ed3506ef002a0e4ab2abef713
Mads Opheim <mads.opheim@nav.no>
2023-01-30 09:17:27 +0100

    EY-1633: Nyare embedded-env, i eit forsøk på å transitivt få bort log4j1

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -45,7 +45,7 @@ object Kafka {
     const val Clients = "org.apache.kafka:kafka-clients:3.1.1"
     const val Avro = "org.apache.avro:avro:1.11.0"
     const val AvroSerializer = "io.confluent:kafka-avro-serializer:7.1.1"
-    const val EmbeddedEnv = "no.nav:kafka-embedded-env:3.1.6"
+    const val EmbeddedEnv = "no.nav:kafka-embedded-env:3.2.1"
 }
 
 object Jackson {

f66a21240083c09e4869315202bd05b0ebabd2ef
henrikg <henrik.gundersen@nav.no>
2023-02-05 18:25:31 +0100

    Unngå å opprette flere db-pools

--- apps/etterlatte-behandling/src/main/kotlin/BeanFactory.kt
+++ apps/etterlatte-behandling/src/main/kotlin/BeanFactory.kt
@@ -193,7 +193,8 @@ abstract class CommonFactory : BeanFactory {
 class EnvBasedBeanFactory(val env: Map<String, String>) : CommonFactory() {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun dataSource() = DataSourceBuilder.createDataSource(env)
+    private val dataSource: DataSource by lazy { DataSourceBuilder.createDataSource(env) }
+    override fun dataSource() = dataSource
 
     override fun rapid(): KafkaProdusent<String, String> {
         return kafkaConfig().standardProducer(env.getValue("KAFKA_RAPID_TOPIC"))

fe946dd37840fdbbaff763db956227b558995a58
Mads Opheim <mads.opheim@nav.no>
2023-02-06 15:20:50 +0100

    EY-1633: Enkel vasking av input som vi sender direkte vidare. Verksemder kan jo heite så mykje forskjellig, så feks ktor sin innebygde escapeIfNeeded tar tak i for mykje. Det å fjerne :// og protokollen før kjens som noko som bør ha veldig låg risiko for falske positive, og samtidig noko som bør ta unna det meste av det problematiske

---
+++ apps/etterlatte-brreg/src/test/kotlin/no/nav/etterlatte/enhetsregister/EnhetsregisterRouteKtTest.kt
@@ -0,0 +1,25 @@
+package no.nav.etterlatte.enhetsregister
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class EnhetsregisterRouteKtTest {
+
+    @Test
+    fun `url i inputnavn blir haandtert`() {
+        val url = "http://localhost:123/mistenkelig"
+        val vaska = vaskInput(url) {
+            ""
+        }
+        assertEquals("localhost:123/mistenkelig", vaska)
+    }
+
+    @Test
+    fun `vanleg inputnavn blir haandtert uten endring`() {
+        val url = "Lorem Ipsum Dolor"
+        val vaska = vaskInput(url) {
+            ""
+        }
+        assertEquals(url, vaska)
+    }
+}

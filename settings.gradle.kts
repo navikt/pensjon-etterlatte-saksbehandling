72a50c595e0e7a11b084d2ae73cb9f6b69385c02
Henrik Gundersen <henrik.gundersen@nav.no>
2023-02-07 21:20:50 +0100

    Fjerner etterlatte-overvaaking da denne kun ble brukt som POC (#674)
    Denne appen ble tidligere brukt som POC for AzureAD integrasjon. Dette har vi nå flere andre apper som har tatt i bruk.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 }
 include(
     "apps:etterlatte-fordeler",
-    "apps:etterlatte-overvaaking",
     "apps:etterlatte-pdltjenester",
     "apps:etterlatte-behandling",
     "apps:etterlatte-brreg",

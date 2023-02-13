d8e741b175b9463088b7e41e552a7237d3e546e6
Nicolas N <nicolas.nordhagen@nav.no>
2023-02-13 09:39:19 +0100

    EY-1698 Bruke cache i regoppslag og navansatt klientene

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/RegoppslagKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/RegoppslagKlient.kt
@@ -1,12 +1,14 @@
 package no.nav.etterlatte.brev.adresse
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import no.nav.etterlatte.libs.common.logging.getXCorrelationId
 import org.slf4j.LoggerFactory
+import java.time.Duration
 
 class RegoppslagKlient(
     private val client: HttpClient,
@@ -14,13 +16,24 @@ class RegoppslagKlient(
 ) {
     private val logger = LoggerFactory.getLogger(RegoppslagKlient::class.java)
 
-    suspend fun hentMottakerAdresse(ident: String): RegoppslagResponseDTO = try {
-        logger.info("Henter mottakeradresse fra regoppslag")
+    private val cache = Caffeine.newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(15))
+        .build<String, RegoppslagResponseDTO>()
 
-        client.get("$url/regoppslag/${ident}") {
-            header("x_correlation_id", getXCorrelationId())
-            header("Nav_Call_Id", getXCorrelationId())
-        }.body()
+    suspend fun hentMottakerAdresse(ident: String): RegoppslagResponseDTO = try {
+        val regoppslagCache = cache.getIfPresent(ident)
+
+        if (regoppslagCache != null) {
+            logger.info("Fant cachet mottakeradresse")
+            regoppslagCache
+        } else {
+            logger.info("Ingen cachet mottakeradresse funnet. Henter fra regoppslag")
+
+            client.get("$url/regoppslag/${ident}") {
+                header("x_correlation_id", getXCorrelationId())
+                header("Nav_Call_Id", getXCorrelationId())
+            }.body()
+        }
     } catch (exception: Exception) {
         throw AdresseException("Feil i kall mot Regoppslag", exception)
     }
--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/navansatt/NavansattKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/navansatt/NavansattKlient.kt
@@ -1,5 +1,6 @@
 package no.nav.etterlatte.brev.navansatt
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
@@ -7,6 +8,7 @@ import io.ktor.client.request.header
 import no.nav.etterlatte.brev.adresse.AdresseException
 import no.nav.etterlatte.libs.common.logging.getXCorrelationId
 import org.slf4j.LoggerFactory
+import java.time.Duration
 
 class NavansattKlient(
     private val client: HttpClient,
@@ -14,12 +16,24 @@ class NavansattKlient(
 ) {
     private val logger = LoggerFactory.getLogger(NavansattKlient::class.java)
 
+    private val cache = Caffeine.newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(15))
+        .build<String, SaksbehandlerInfo>()
+
     suspend fun hentSaksbehandlerInfo(ident: String): SaksbehandlerInfo = try {
-        logger.info("Henter informasjon om saksbehandler")
-        client.get("$url/navansatt/$ident") {
-            header("x_correlation_id", getXCorrelationId())
-            header("Nav_Call_Id", getXCorrelationId())
-        }.body()
+        val saksbehandlerCache = cache.getIfPresent(ident)
+
+        if (saksbehandlerCache != null) {
+            logger.info("Fant cachet saksbehandler med ident $ident")
+            saksbehandlerCache
+        } else {
+            logger.info("Henter info om saksbehandler med ident $ident")
+
+            client.get("$url/navansatt/$ident") {
+                header("x_correlation_id", getXCorrelationId())
+                header("Nav_Call_Id", getXCorrelationId())
+            }.body()
+        }
     } catch (exception: Exception) {
         throw AdresseException("Feil i kall mot navansatt med ident: $ident", exception)
     }

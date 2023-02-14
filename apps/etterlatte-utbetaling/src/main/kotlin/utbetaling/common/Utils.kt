580583a6216df4e64da4070be06f91201e6e7fae
Henrik Gundersen <henrik.gundersen@nav.no>
2023-02-14 21:42:30 +0100

    Støtte for kjøreplan i konsistensavstemming (#740)
    * Støtte for kjøreplan i konsistensavstemming
    
    EY-1718

--- apps/etterlatte-utbetaling/src/main/kotlin/utbetaling/common/Utils.kt
+++ apps/etterlatte-utbetaling/src/main/kotlin/utbetaling/common/Utils.kt
@@ -7,6 +7,7 @@ import no.nav.etterlatte.libs.common.tidspunkt.toTidspunkt
 import java.time.Clock
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.Month
 import java.time.YearMonth
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
@@ -15,6 +16,19 @@ import java.util.*
 import javax.xml.datatype.DatatypeConstants
 import javax.xml.datatype.DatatypeFactory
 import javax.xml.datatype.XMLGregorianCalendar
+
+fun Int.januar(year: Int): LocalDate = LocalDate.of(year, Month.JANUARY, this)
+fun Int.februar(year: Int): LocalDate = LocalDate.of(year, Month.FEBRUARY, this)
+fun Int.mars(year: Int): LocalDate = LocalDate.of(year, Month.MARCH, this)
+fun Int.april(year: Int): LocalDate = LocalDate.of(year, Month.APRIL, this)
+fun Int.mai(year: Int): LocalDate = LocalDate.of(year, Month.MAY, this)
+fun Int.juni(year: Int): LocalDate = LocalDate.of(year, Month.JUNE, this)
+fun Int.juli(year: Int): LocalDate = LocalDate.of(year, Month.JULY, this)
+fun Int.august(year: Int): LocalDate = LocalDate.of(year, Month.AUGUST, this)
+fun Int.september(year: Int): LocalDate = LocalDate.of(year, Month.SEPTEMBER, this)
+fun Int.oktober(year: Int): LocalDate = LocalDate.of(year, Month.OCTOBER, this)
+fun Int.november(year: Int): LocalDate = LocalDate.of(year, Month.NOVEMBER, this)
+fun Int.desember(year: Int): LocalDate = LocalDate.of(year, Month.DECEMBER, this)
 
 fun ZonedDateTime.next(atTime: LocalTime): Date {
     return if (this.toLocalTime().isAfter(atTime)) {
--- apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/avstemming/KonsistensavstemmingJobTest.kt
+++ apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/avstemming/KonsistensavstemmingJobTest.kt
@@ -1,23 +1,30 @@
 package no.nav.etterlatte.utbetaling.avstemming
 
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import no.nav.etterlatte.libs.common.tidspunkt.norskTidssone
 import no.nav.etterlatte.libs.jobs.LeaderElection
+import no.nav.etterlatte.utbetaling.common.februar
 import no.nav.etterlatte.utbetaling.iverksetting.utbetaling.Saktype
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
+import java.time.Clock
+import java.time.temporal.ChronoUnit
 
 internal class KonsistensavstemmingJobTest {
 
     private val konsistensavstemmingService: KonsistensavstemmingService = mockk {
     }
     private val leaderElection: LeaderElection = mockk()
+    private val datoEksekvering = 1.februar(2023)
     private val konsistensavstemming = KonsistensavstemmingJob.Konsistensavstemming(
         konsistensavstemmingService = konsistensavstemmingService,
+        kjoereplan = setOf(datoEksekvering),
         leaderElection = leaderElection,
-        jobbNavn = "jobb"
+        jobbNavn = "jobb",
+        clock = Clock.fixed(datoEksekvering.atStartOfDay(norskTidssone).toInstant(), norskTidssone)
     )
 
     @Test
@@ -32,9 +39,13 @@ internal class KonsistensavstemmingJobTest {
 
     @Test
     fun `skal konsistensavstemme for Barnepensjon siden pod er leader`() {
-        val idag = LocalDate.now()
         every { leaderElection.isLeader() } returns true
-        every { konsistensavstemmingService.konsistensavstemmingErKjoertIDag(Saktype.BARNEPENSJON, idag) } returns false
+        every {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                datoEksekvering
+            )
+        } returns false
         every { konsistensavstemmingService.startKonsistensavstemming(any(), any()) } returns emptyList()
 
         konsistensavstemming.run()
@@ -44,26 +55,108 @@ internal class KonsistensavstemmingJobTest {
     }
 
     @Test
-    fun `skal konsistensavstemme for barnepensjon naar jobb ikke er kjoert samme dag`() {
-        val idag = LocalDate.now()
+    fun `skal ikke konsistensavstemme for barnepensjon naar datoen ikke er en del av kjoereplan`() {
         every { leaderElection.isLeader() } returns true
-        every { konsistensavstemmingService.konsistensavstemmingErKjoertIDag(Saktype.BARNEPENSJON, idag) } returns false
-        every { konsistensavstemmingService.startKonsistensavstemming(idag, Saktype.BARNEPENSJON) } returns emptyList()
+
+        val dagForTestMinusFemDager = datoEksekvering.atStartOfDay(norskTidssone).toInstant().minus(5, ChronoUnit.DAYS)
+
+        val konsistensavstemming = KonsistensavstemmingJob.Konsistensavstemming(
+            konsistensavstemmingService = konsistensavstemmingService,
+            kjoereplan = setOf(datoEksekvering),
+            leaderElection = leaderElection,
+            jobbNavn = "jobb",
+            clock = Clock.fixed(dagForTestMinusFemDager, norskTidssone)
+        )
 
         konsistensavstemming.run()
 
-        verify(exactly = 1) { konsistensavstemmingService.startKonsistensavstemming(idag, Saktype.BARNEPENSJON) }
+        verify(exactly = 0) {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                datoEksekvering
+            )
+        }
+        verify(exactly = 0) {
+            konsistensavstemmingService.startKonsistensavstemming(
+                datoEksekvering,
+                Saktype.BARNEPENSJON
+            )
+        }
+        confirmVerified(konsistensavstemmingService)
+    }
+
+    @Test
+    fun `skal konsistensavstemme for barnepensjon naar datoen er en del av kjoereplan`() {
+        every { leaderElection.isLeader() } returns true
+        every {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                datoEksekvering
+            )
+        } returns false
+        every { konsistensavstemmingService.startKonsistensavstemming(any(), any()) } returns emptyList()
+
+        konsistensavstemming.run()
+
+        verify(exactly = 1) {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                any()
+            )
+        }
+        verify(exactly = 1) { konsistensavstemmingService.startKonsistensavstemming(any(), Saktype.BARNEPENSJON) }
+        confirmVerified(konsistensavstemmingService)
+    }
+
+    @Test
+    fun `skal konsistensavstemme for barnepensjon naar jobb ikke er kjoert samme dag`() {
+        every { leaderElection.isLeader() } returns true
+        every {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                datoEksekvering
+            )
+        } returns false
+        every {
+            konsistensavstemmingService.startKonsistensavstemming(
+                datoEksekvering,
+                Saktype.BARNEPENSJON
+            )
+        } returns emptyList()
+
+        konsistensavstemming.run()
+
+        verify(exactly = 1) {
+            konsistensavstemmingService.startKonsistensavstemming(
+                datoEksekvering,
+                Saktype.BARNEPENSJON
+            )
+        }
     }
 
     @Test
     fun `skal ikke konsistensavstemme for barnepensjon naar jobb er kjoert samme dag`() {
-        val idag = LocalDate.now()
         every { leaderElection.isLeader() } returns true
-        every { konsistensavstemmingService.konsistensavstemmingErKjoertIDag(Saktype.BARNEPENSJON, idag) } returns true
-        every { konsistensavstemmingService.startKonsistensavstemming(idag, Saktype.BARNEPENSJON) } returns emptyList()
+        every {
+            konsistensavstemmingService.konsistensavstemmingErKjoertIDag(
+                Saktype.BARNEPENSJON,
+                datoEksekvering
+            )
+        } returns true
+        every {
+            konsistensavstemmingService.startKonsistensavstemming(
+                datoEksekvering,
+                Saktype.BARNEPENSJON
+            )
+        } returns emptyList()
 
         konsistensavstemming.run()
 
-        verify(exactly = 0) { konsistensavstemmingService.startKonsistensavstemming(idag, Saktype.BARNEPENSJON) }
+        verify(exactly = 0) {
+            konsistensavstemmingService.startKonsistensavstemming(
+                datoEksekvering,
+                Saktype.BARNEPENSJON
+            )
+        }
     }
 }

9320189ad0b63c4189daff26d0de082a5581dc73
Mads Opheim <mads.opheim@nav.no>
2023-02-14 12:46:56 +0100

    EY-1704: Sett opp vilkårsvurdering-asynk-app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,7 @@ include(
     "apps:etterlatte-hendelser-pdl",
     "apps:etterlatte-statistikk",
     "apps:etterlatte-vilkaarsvurdering",
+    "apps:etterlatte-vilkaarsvurdering-asynk",
     "libs:ktor2client-auth-clientcredentials",
     "libs:common",
     "libs:ktor2client-onbehalfof",

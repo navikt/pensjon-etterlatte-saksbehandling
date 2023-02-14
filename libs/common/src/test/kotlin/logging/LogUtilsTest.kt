9b820c68c911a7da19c127edbd911d8a76628329
Chris Searle <chris@chrissearle.org>
2023-02-14 11:42:00 +0100

    EY-948 - behold MDC hvis en exception har blitt kastet (#732)

---
+++ libs/common/src/test/kotlin/logging/LogUtilsTest.kt
@@ -0,0 +1,36 @@
+package no.nav.etterlatte.libs.common.logging
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.slf4j.MDC
+import kotlin.RuntimeException
+
+internal class LogUtilsTest {
+    private val testCorrelationID = "TEST_CORRELATION_ID"
+
+    @Test
+    fun `Sjekk MDC correlation ID - vanlig flyt`() {
+        assert(MDC.get(CORRELATION_ID) == null)
+
+        withLogContext(correlationId = testCorrelationID) {
+            assert(MDC.get(CORRELATION_ID) == testCorrelationID)
+        }
+
+        assert(MDC.get(CORRELATION_ID) == null)
+    }
+
+    @Test
+    fun `Sjekk MDC correlation ID - exception flyt`() {
+        assert(MDC.get(CORRELATION_ID) == null)
+
+        assertThrows<RuntimeException> {
+            withLogContext(correlationId = testCorrelationID) {
+                assert(MDC.get(CORRELATION_ID) == testCorrelationID)
+
+                throw RuntimeException("Oops")
+            }
+
+            assert(MDC.get(CORRELATION_ID) == testCorrelationID)
+        }
+    }
+}

84bf8a605eb608bdd4c9a03dbba4133c41d32724
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-02-17 11:25:27 +0100

    EY-1742 Su inspirert config

--- apps/etterlatte-hendelser-pdl/src/main/kotlin/hendelserpdl/leesah/LivetErEnStroemAvHendelser.kt
+++ apps/etterlatte-hendelser-pdl/src/main/kotlin/hendelserpdl/leesah/LivetErEnStroemAvHendelser.kt
@@ -2,7 +2,6 @@ package no.nav.etterlatte.hendelserpdl.leesah
 
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import kotlinx.coroutines.GlobalScope
@@ -13,6 +12,8 @@ import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.security.auth.SecurityProtocol
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.util.*
@@ -33,34 +34,39 @@ class LivetErEnStroemAvHendelser(env: Map<String, String>) : ILivetErEnStroemAvH
         val startuptask = {
             val properties = Properties().apply {
                 put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env["KAFKA_BROKERS"])
-                put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL")
-                put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
+                put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+                put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+                put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
+                put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
                 put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env["KAFKA_TRUSTSTORE_PATH"])
                 put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
-                // Nais doc: Password needed to use the keystore and truststore
-                put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
                 put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env["KAFKA_KEYSTORE_PATH"])
                 put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
 
-                put(ConsumerConfig.GROUP_ID_CONFIG, env["LEESAH_KAFKA_GROUP_ID"])
+                put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
+                // Nais doc: Password needed to use the keystore and truststore
 
-                put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1000")
+                put(ConsumerConfig.GROUP_ID_CONFIG, env["LEESAH_KAFKA_GROUP_ID"])
+                put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1000)
                 put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                put(ConsumerConfig.CLIENT_ID_CONFIG, env["NAIS_APP_NAME"])
                 put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
                 put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, femSekunder)
+                put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
+                put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer::class.java)
 
-                put(SCHEMA_REGISTRY_URL_CONFIG, env["KAFKA_SCHEMA_REGISTRY"])
-                put(USER_INFO_CONFIG, "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}")
-                put(
-                    "schema.registry.basic.auth.user.info",
-                    "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
-                )
+                put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true)
                 put(BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
 
-                put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer::class.java)
-                put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer::class.java)
-                put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true)
-                put(ConsumerConfig.CLIENT_ID_CONFIG, env["NAIS_APP_NAME"])
+                put(
+                    KafkaAvroDeserializerConfig.USER_INFO_CONFIG,
+                    "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
+                )
+                put(SCHEMA_REGISTRY_URL_CONFIG, env["KAFKA_SCHEMA_REGISTRY"])
+                /*put(
+                    "schema.registry.basic.auth.user.info",
+                    "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
+                )*/
             }
 
             val consumer: KafkaConsumer<String?, String?> = KafkaConsumer<String?, String?>(properties)

86d2bc6e8e1a99170c1d726de7f07938ca5cfe9b
Mads Opheim <mads.opheim@nav.no>
2023-02-26 22:00:10 +0100

    EY-1823: Litt av poenget må vel vera å ikkje kaste feilmeldinga vidare, sånn at r&r ikkje stoppar opp på exceptionen

--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/Feilhaandtering.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/Feilhaandtering.kt
@@ -8,16 +8,16 @@ import org.slf4j.LoggerFactory
 
 val feilhaandteringLogger = LoggerFactory.getLogger("feilhaandtering-kafka")
 
-fun withFeilhaandtering(packet: JsonMessage, context: MessageContext, block: () -> Unit): Unit =
+fun withFeilhaandtering(packet: JsonMessage, context: MessageContext, block: () -> Unit): Unit? =
     innerFeilhaandtering(packet, context, block)
 
-private fun <T> innerFeilhaandtering(packet: JsonMessage, context: MessageContext, block: () -> T): T {
-    try {
-        return block()
+private fun <T> innerFeilhaandtering(packet: JsonMessage, context: MessageContext, block: () -> T): T? {
+    return try {
+        block()
     } catch (e: Exception) {
         feilhaandteringLogger.warn("Håndtering av melding ${packet.id} feila.")
         packet.eventName = EventNames.FEILA
         context.publish(packet.toJson())
-        throw e
+        null
     }
 }

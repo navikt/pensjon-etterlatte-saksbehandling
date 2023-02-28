f24d8a00aae6247ce809271ff2e4cd90d7faf9a4
Mads Opheim <mads.opheim@nav.no>
2023-02-28 11:35:47 +0100

    EY-1835: Startar å subklasse

--- libs/etterlatte-token-model/src/main/kotlin/no/nav/etterlatte/token/Bruker.kt
+++ libs/etterlatte-token-model/src/main/kotlin/no/nav/etterlatte/token/Bruker.kt
@@ -1,10 +1,8 @@
 package no.nav.etterlatte.token
 
-data class Bruker(
-    val accessToken: String,
-    val saksbehandler: String?,
-    val oid: String?,
-    val sub: String?
+sealed class Bruker(
+    open val accessToken: String,
+    val saksbehandler: String?
 ) {
     val saksbehandlerIdentEllerSystemnavn: String =
         if (erMaskinTilMaskin()) {
@@ -13,7 +11,7 @@ data class Bruker(
             saksbehandler!!
         }
 
-    fun erMaskinTilMaskin() = (oid == sub) && (oid != null)
+    abstract fun erMaskinTilMaskin(): Boolean
     fun saksbehandlerEnhet(saksbehandlere: Map<String, String>): String {
         if (erMaskinTilMaskin()) {
             return Fagsaksystem.EY.name
@@ -23,21 +21,13 @@ data class Bruker(
             ?: throw SaksbehandlerManglerEnhetException("Saksbehandler $saksbehandler mangler enhet fra secret")
     }
 
-    init {
-        if (!(erMaskinTilMaskin() || saksbehandler != null)) {
-            throw Exception(
-                "Er ikke maskin-til-maskin, og Navident er null i token, sannsynligvis manglende claim NAVident"
-            )
-        }
-    }
-
     companion object {
         private fun erMaskinTilMaskin(oid: String?, sub: String?) = (oid == sub) && (oid != null)
         fun of(accessToken: String, saksbehandler: String?, oid: String? = null, sub: String? = null): Bruker {
             return if (erMaskinTilMaskin(oid = oid, sub = sub)) {
-                Bruker(accessToken, null, oid, sub)
+                System(accessToken, oid!!, sub!!)
             } else if (saksbehandler != null) {
-                Bruker(accessToken, saksbehandler, null, null)
+                Saksbehandler(accessToken, saksbehandler)
             } else {
                 throw Exception(
                     "Er ikke maskin-til-maskin, og Navident er null i token, sannsynligvis manglende claim NAVident"
@@ -45,6 +35,14 @@ data class Bruker(
             }
         }
     }
+}
+
+data class System(override val accessToken: String, val oid: String, val sub: String) : Bruker(accessToken, null) {
+    override fun erMaskinTilMaskin() = true
+}
+
+data class Saksbehandler(override val accessToken: String, val ident: String) : Bruker(accessToken, ident) {
+    override fun erMaskinTilMaskin() = false
 }
 
 enum class Claims {

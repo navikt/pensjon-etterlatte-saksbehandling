b2d63f28693687cad5c035609e91baea45defb48
henrikg <henrik.gundersen@nav.no>
2023-03-02 13:03:07 +0100

    La til flere tester for beregning av omstillingsstønad

--- apps/etterlatte-beregning/src/test/kotlin/beregning/BeregnOmstillingsstoenadServiceTest.kt
+++ apps/etterlatte-beregning/src/test/kotlin/beregning/BeregnOmstillingsstoenadServiceTest.kt
@@ -13,6 +13,7 @@ import no.nav.etterlatte.beregning.regler.bruker
 import no.nav.etterlatte.libs.common.behandling.BehandlingType
 import no.nav.etterlatte.libs.common.behandling.DetaljertBehandling
 import no.nav.etterlatte.libs.common.beregning.Beregningstype
+import no.nav.etterlatte.libs.common.vilkaarsvurdering.VilkaarsvurderingUtfall
 import no.nav.etterlatte.libs.testdata.behandling.VirkningstidspunktTestData
 import no.nav.etterlatte.libs.testdata.grunnlag.GrunnlagTestData
 import org.junit.jupiter.api.Test
@@ -30,7 +31,7 @@ internal class BeregnOmstillingsstoenadServiceTest {
 
     @Test
     fun `skal beregne omstillingsstoenad foerstegangsbehandling`() {
-        val behandling = behandling(BehandlingType.FØRSTEGANGSBEHANDLING)
+        val behandling = mockBehandling(BehandlingType.FØRSTEGANGSBEHANDLING)
         val grunnlag = GrunnlagTestData().hentOpplysningsgrunnlag()
 
         coEvery { grunnlagKlient.hentGrunnlag(any(), any()) } returns grunnlag
@@ -59,7 +60,98 @@ internal class BeregnOmstillingsstoenadServiceTest {
         }
     }
 
-    private fun behandling(type: BehandlingType, virk: YearMonth = VIRKNINGSTIDSPUNKT_JAN_23): DetaljertBehandling =
+    @Test
+    fun `skal beregne omstillingsstoenad ved revurdering`() {
+        val behandling = mockBehandling(BehandlingType.REVURDERING)
+        val grunnlag = GrunnlagTestData().hentOpplysningsgrunnlag()
+
+        coEvery { grunnlagKlient.hentGrunnlag(any(), any()) } returns grunnlag
+        coEvery { vilkaarsvurderingKlient.hentVilkaarsvurdering(any(), any()) } returns mockk {
+            every { resultat?.utfall } returns VilkaarsvurderingUtfall.OPPFYLT
+        }
+
+        runBlocking {
+            val beregning = beregnOmstillingsstoenadService.beregn(behandling, bruker)
+
+            with(beregning) {
+                beregningId shouldNotBe null
+                behandlingId shouldBe behandling.id
+                type shouldBe Beregningstype.OMS
+                beregnetDato shouldNotBe null
+                grunnlagMetadata shouldBe grunnlag.metadata
+                beregningsperioder.size shouldBe 1
+                with(beregningsperioder.first()) {
+                    utbetaltBeloep shouldBe OMS_BELOEP_JAN_23
+                    datoFOM shouldBe behandling.virkningstidspunkt?.dato
+                    datoTOM shouldBe null
+                    grunnbelopMnd shouldBe GRUNNBELOEP_JAN_23
+                    trygdetid shouldBe MAKS_TRYGDETID
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `skal opphoere ved revurdering og vilkaarsvurdering ikke oppfylt`() {
+        val behandling = mockBehandling(BehandlingType.REVURDERING)
+        val grunnlag = GrunnlagTestData().hentOpplysningsgrunnlag()
+
+        coEvery { grunnlagKlient.hentGrunnlag(any(), any()) } returns grunnlag
+        coEvery { vilkaarsvurderingKlient.hentVilkaarsvurdering(any(), any()) } returns mockk {
+            every { resultat?.utfall } returns VilkaarsvurderingUtfall.IKKE_OPPFYLT
+        }
+
+        runBlocking {
+            val beregning = beregnOmstillingsstoenadService.beregn(behandling, bruker)
+
+            with(beregning) {
+                beregningId shouldNotBe null
+                behandlingId shouldBe behandling.id
+                type shouldBe Beregningstype.OMS
+                beregnetDato shouldNotBe null
+                grunnlagMetadata shouldBe grunnlag.metadata
+                beregningsperioder.size shouldBe 1
+                with(beregningsperioder.first()) {
+                    utbetaltBeloep shouldBe 0
+                    datoFOM shouldBe behandling.virkningstidspunkt?.dato
+                    datoTOM shouldBe null
+                    grunnbelopMnd shouldBe GRUNNBELOEP_JAN_23
+                    trygdetid shouldBe 0
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `skal sette beloep til 0 ved manuelt opphoer`() {
+        val behandling = mockBehandling(BehandlingType.MANUELT_OPPHOER)
+        val grunnlag = GrunnlagTestData().hentOpplysningsgrunnlag()
+
+        coEvery { grunnlagKlient.hentGrunnlag(any(), any()) } returns grunnlag
+
+        runBlocking {
+            val beregning = beregnOmstillingsstoenadService.beregn(behandling, bruker)
+
+            with(beregning) {
+                beregningId shouldNotBe null
+                behandlingId shouldBe behandling.id
+                type shouldBe Beregningstype.OMS
+                beregnetDato shouldNotBe null
+                grunnlagMetadata shouldBe grunnlag.metadata
+                beregningsperioder.size shouldBe 1
+                with(beregningsperioder.first()) {
+                    utbetaltBeloep shouldBe 0
+                    datoFOM shouldBe behandling.virkningstidspunkt?.dato
+                    datoTOM shouldBe null
+                    grunnbelopMnd shouldBe GRUNNBELOEP_JAN_23
+                    soeskenFlokk shouldBe null
+                    trygdetid shouldBe 0
+                }
+            }
+        }
+    }
+
+    private fun mockBehandling(type: BehandlingType, virk: YearMonth = VIRKNINGSTIDSPUNKT_JAN_23): DetaljertBehandling =
         mockk<DetaljertBehandling>().apply {
             every { id } returns randomUUID()
             every { sak } returns 1

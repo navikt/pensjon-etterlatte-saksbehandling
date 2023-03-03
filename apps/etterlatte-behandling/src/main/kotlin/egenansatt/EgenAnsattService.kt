5688a92eaa8466aae39df1401456b7880c19481c
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-03-03 19:31:10 +0100

    Drop local var

--- apps/etterlatte-behandling/src/main/kotlin/egenansatt/EgenAnsattService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/egenansatt/EgenAnsattService.kt
@@ -5,9 +5,7 @@ import no.nav.etterlatte.sak.SakService
 class EgenAnsattService(private val sakService: SakService) {
 
     fun haandterSkjerming(skjermetHendelse: SkjermetHendelse) {
-        val finnSaker = sakService.finnSaker(skjermetHendelse.fnr)
-        finnSaker
-            .map { it.id }
+        sakService.finnSaker(skjermetHendelse.fnr).map { it.id }
             .also {
                 if (it.isNotEmpty()) {
                     sakService.markerSakerMedSkjerming(it, skjermetHendelse.skjermet)

314255ddd3d11b809a54e2840bfa6a1f62b65594
Mads Opheim <mads.opheim@nav.no>
2023-03-09 14:44:04 +0100

    EY-1795: Flyttar konverteringsmetode over frå Instant til Tidspunkt. Rotproblemet er at denne er LocalDateTime i utgangspunktet, så håper å kunne slette heile metoden etter kvart, men inntil da er det fint at vi er på Tidspunkt

--- libs/common/src/main/kotlin/tidspunkt/MidlertidigeKonverteringsfunksjoner.kt
+++ libs/common/src/main/kotlin/tidspunkt/MidlertidigeKonverteringsfunksjoner.kt
@@ -1,6 +1,5 @@
 package no.nav.etterlatte.libs.common.tidspunkt
 
-import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -10,9 +9,8 @@ Denne fila samlar extension functions for tidskonvertering vi forhåpentlegvis i
 sia målet er å gå over til å bruke Tidspunkt ganske konsekvent
  */
 
-fun Instant?.toLocalDateTimeNorskTid() = this?.let { LocalDateTime.ofInstant(it, norskTidssone) }
-
 fun LocalDateTime.toTidspunkt(zoneId: ZoneId = standardTidssoneUTC) = Tidspunkt(atZone(zoneId).toInstant())
 fun LocalDateTime.toNorskTidspunkt() = toTidspunkt(zoneId = norskTidssone)
 fun Tidspunkt.toNorskTid(): ZonedDateTime = ZonedDateTime.ofInstant(this.instant, norskTidssone)
 fun Tidspunkt.toLocalDatetimeUTC(): LocalDateTime = LocalDateTime.ofInstant(this.instant, standardTidssoneUTC)
+fun Tidspunkt.toLocalDatetimeNorskTid(): LocalDateTime = LocalDateTime.ofInstant(this.instant, norskTidssone)

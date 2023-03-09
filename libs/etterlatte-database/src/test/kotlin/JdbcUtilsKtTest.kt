e45b7e853f7153c9e266b2be747139630ed67a78
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2023-03-09 09:52:55 +0100

    EY-1881 toList konsumerer hele resultSet selv om en verdi blir null (#920)
    * toList konsumerer hele resultSet selv om en verdi blir null
    
    * Trenger bare en false

---
+++ libs/etterlatte-database/src/test/kotlin/JdbcUtilsKtTest.kt
@@ -0,0 +1,23 @@
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.etterlatte.libs.database.toList
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.sql.ResultSet
+
+class JdbcUtilsKtTest {
+
+    @Test
+    fun `toList stopper ikke hvis block() gir null og resultset har verdier`() {
+        val resultSet: ResultSet = mockk()
+        every {
+            resultSet.next()
+        } returnsMany listOf(true, true, true, true, false)
+
+        val block: ResultSet.() -> String? = mockk()
+        every { resultSet.block() } returnsMany listOf("1", "2", null, "4")
+
+        val liste = resultSet.toList(block)
+        Assertions.assertEquals(liste, listOf("1", "2", null, "4"))
+    }
+}

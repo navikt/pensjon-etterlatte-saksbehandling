297c2b07614ac4d80242ff0e7ab9500df65fdd42
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-03-09 09:54:16 +0100

    Feature/improve egen ansatt lytter (#914)
    * Forbedre egenansattlytter med tester
    
    * Add prod config

---
+++ apps/etterlatte-behandling/src/test/kotlin/EgenAnsattServiceTest/EgenAnsattServiceTest.kt
@@ -0,0 +1,88 @@
+package no.nav.etterlatte
+
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import no.nav.etterlatte.egenansatt.EgenAnsattService
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.person.Foedselsnummer
+import no.nav.etterlatte.libs.common.skjermet.EgenAnsattSkjermet
+import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
+import no.nav.etterlatte.libs.database.DataSourceBuilder
+import no.nav.etterlatte.libs.database.migrate
+import no.nav.etterlatte.sak.RealSakService
+import no.nav.etterlatte.sak.SakDao
+import no.nav.etterlatte.sak.SakService
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import java.sql.Connection
+import javax.sql.DataSource
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EgenAnsattServiceTest {
+
+    @Container
+    private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:14")
+
+    private lateinit var dataSource: DataSource
+    private lateinit var sakRepo: SakDao
+    private lateinit var sakService: SakService
+    private lateinit var egenAnsattService: EgenAnsattService
+
+    @BeforeAll
+    fun beforeAll() {
+        postgreSQLContainer.start()
+        postgreSQLContainer.withUrlParam("user", postgreSQLContainer.username)
+        postgreSQLContainer.withUrlParam("password", postgreSQLContainer.password)
+
+        dataSource = DataSourceBuilder.createDataSource(
+            jdbcUrl = postgreSQLContainer.jdbcUrl,
+            username = postgreSQLContainer.username,
+            password = postgreSQLContainer.password
+        ).apply { migrate() }
+
+        val connection = dataSource.connection
+        sakRepo = SakDao { connection }
+        sakService = spyk(RealSakService(sakRepo))
+        egenAnsattService = EgenAnsattService(sakService)
+    }
+
+    @BeforeEach
+    fun before() {
+        Kontekst.set(
+            Context(
+                mockk(),
+                object : DatabaseKontekst {
+                    override fun activeTx(): Connection {
+                        throw IllegalArgumentException()
+                    }
+
+                    override fun <T> inTransaction(block: () -> T): T {
+                        return block()
+                    }
+                }
+            )
+        )
+    }
+
+    @Test
+    fun test() {
+        val fnr = Foedselsnummer.of("08071272487").value
+        sakService.finnEllerOpprettSak(fnr, SakType.BARNEPENSJON)
+        val fnr2 = Foedselsnummer.of("19078504903").value
+        sakService.finnEllerOpprettSak(fnr2, SakType.BARNEPENSJON)
+
+        assertNotNull(sakService.finnSak(fnr, SakType.BARNEPENSJON))
+        assertNotNull(sakService.finnSak(fnr2, SakType.BARNEPENSJON))
+
+        val egenAnsattSkjermet = EgenAnsattSkjermet(fnr, Tidspunkt.now(), true)
+        egenAnsattService.haandterSkjerming(egenAnsattSkjermet)
+
+        verify { sakService.markerSakerMedSkjerming(any(), any()) }
+    }
+}
---
+++ libs/common/src/main/kotlin/skjermet/EgenAnsattSkjermet.kt
@@ -0,0 +1,9 @@
+package no.nav.etterlatte.libs.common.skjermet
+
+import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
+
+data class EgenAnsattSkjermet(
+    val fnr: String,
+    val inntruffet: Tidspunkt,
+    val skjermet: Boolean
+)

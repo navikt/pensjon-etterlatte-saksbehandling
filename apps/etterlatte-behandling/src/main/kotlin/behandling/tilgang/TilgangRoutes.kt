805526f210614878124810a77ebd8df87c618d82
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-03-10 17:29:30 +0100

    Log behandlingstilgang

--- apps/etterlatte-behandling/src/main/kotlin/behandling/tilgang/TilgangRoutes.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/tilgang/TilgangRoutes.kt
@@ -1,9 +1,11 @@
 package no.nav.etterlatte.behandling.tilgang
 
 import io.ktor.server.application.call
+import io.ktor.server.application.log
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.application
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
@@ -14,6 +16,7 @@ import no.nav.etterlatte.sak.SakService
 import no.nav.etterlatte.sak.SakServiceAdressebeskyttelse
 
 internal fun Route.tilgangRoutes(sakService: SakService, sakServiceAdressebeskyttelse: SakServiceAdressebeskyttelse) {
+    val logger = application.log
     route("/tilgang") {
         post("/person") {
             val fnr = call.receive<String>()
@@ -24,6 +27,7 @@ internal fun Route.tilgangRoutes(sakService: SakService, sakServiceAdressebeskyt
         get("/behandling/{$BEHANDLINGSID_CALL_PARAMETER}") {
             val harTilgang =
                 !sakServiceAdressebeskyttelse.behandlingHarAdressebeskyttelse(behandlingsId.toString())
+            logger.info("har tilgang til behandling $behandlingsId $harTilgang")
             call.respond(harTilgang)
         }
 

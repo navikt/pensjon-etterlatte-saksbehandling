705bc192d95f739277f4f9b9f8d86d8e4884867f
Mads Opheim <mads.opheim@nav.no>
2023-03-13 08:39:16 +0100

    EY-1795: Testar funksjonalitet i Tidspunkt

--- libs/common/src/test/kotlin/tidspunkt/TidspunktTest.kt
+++ libs/common/src/test/kotlin/tidspunkt/TidspunktTest.kt
@@ -3,6 +3,7 @@ package tidspunkt
 import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.common.tidspunkt.standardTidssoneUTC
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 import java.time.Instant
 import java.time.LocalDate
@@ -31,5 +32,20 @@ class TidspunktTest {
             medTid,
             Tidspunkt(Instant.from(ZonedDateTime.of(localDate, LocalTime.of(20, 10, 5), standardTidssoneUTC)))
         )
+    }
+
+    @Test
+    fun `tidspunktet blir runda av`() {
+        val instant =
+            Instant.from(
+                ZonedDateTime.of(
+                    LocalDate.of(2022, Month.JUNE, 2),
+                    LocalTime.of(10, 11, 12, 13),
+                    standardTidssoneUTC
+                )
+            )
+        val tidspunkt = Tidspunkt(instant)
+        assertNotEquals(instant, tidspunkt.instant)
+        assertEquals(tidspunkt.instant, instant.truncatedTo(Tidspunkt.unit))
     }
 }

e7e765002fd90fe913dce0e7ad3ca2e201dbb02d
Mads Opheim <mads.opheim@nav.no>
2023-03-13 11:38:30 +0100

    EY-1897: Forenklar oppsettet i gyldig søknad-applikasjonen

--- apps/etterlatte-gyldig-soeknad/src/main/kotlin/Application.kt
+++ apps/etterlatte-gyldig-soeknad/src/main/kotlin/Application.kt
@@ -4,25 +4,20 @@ import no.nav.etterlatte.gyldigsoeknad.barnepensjon.FordeltSoeknadRiver
 import no.nav.etterlatte.gyldigsoeknad.barnepensjon.GyldigSoeknadService
 import no.nav.etterlatte.gyldigsoeknad.config.AppBuilder
 import no.nav.etterlatte.gyldigsoeknad.omstillingsstoenad.InnsendtSoeknadRiver
-import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.etterlatte.rapidsandrivers.init
+import no.nav.helse.rapids_rivers.RapidsConnection
 
-fun main() {
-    System.getenv().toMutableMap().apply {
-        put("KAFKA_CONSUMER_GROUP_ID", get("NAIS_APP_NAME")!!.replace("-", ""))
-    }.also { env ->
-        AppBuilder(env).also { ab ->
-            RapidApplication.create(env)
-                .also {
-                    FordeltSoeknadRiver(
-                        it,
-                        GyldigSoeknadService(ab.createPdlClient()),
-                        ab.createBehandlingClient()
-                    )
-                    InnsendtSoeknadRiver(
-                        it,
-                        ab.createBehandlingClient()
-                    )
-                }.start()
-        }
+fun main() = init(
+    { AppBuilder(it) },
+    { rc: RapidsConnection, ab: AppBuilder ->
+        FordeltSoeknadRiver(
+            rc,
+            GyldigSoeknadService(ab.createPdlClient()),
+            ab.createBehandlingClient()
+        )
+        InnsendtSoeknadRiver(
+            rc,
+            ab.createBehandlingClient()
+        )
     }
-}
+)

a89dd869607a772bd50165e3cfffeae56da644e1
Nicolas N <Watercolours@users.noreply.github.com>
2023-03-13 16:08:54 +0100

    EY-1820 Opprydding i rivers (#954)

--- libs/common/src/main/kotlin/event/BehandlingRiverKey.kt
+++ libs/common/src/main/kotlin/event/BehandlingRiverKey.kt
@@ -2,15 +2,8 @@ package no.nav.etterlatte.libs.common.event
 
 interface IBehandlingRiverKey {
     val behandlingObjectKey get() = "behandling"
-    val sakObjectKey get() = "sak"
-    val behandlingIdKey get() = "behandling.id"
     val sakIdKey get() = "sakId"
-    val fnrSoekerKey get() = "fnrSoeker"
-    val behandlingOpprettetKey get() = "behandlingOpprettet"
     val persongalleriKey get() = "persongalleri"
-    val revurderingAarsakKey get() = "revurderingsaarsak"
-    val manueltOpphoerAarsakKey get() = "manueltOpphoerAarsak"
-    val manueltOpphoerfritekstAarsakKey get() = "manueltOpphoerFritekstAarsak"
 }
 
 object BehandlingRiverKey : IBehandlingRiverKey

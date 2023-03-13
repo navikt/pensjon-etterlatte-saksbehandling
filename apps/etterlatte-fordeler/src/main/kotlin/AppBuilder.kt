91350a914a2932959ee7869781311189f30c2f83
Mads Opheim <mads.opheim@nav.no>
2023-03-13 12:00:03 +0100

    EY-1897: Standardiserer initialiseringa også av fordeler

--- apps/etterlatte-fordeler/src/main/kotlin/AppBuilder.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/AppBuilder.kt
@@ -1,11 +1,14 @@
 package no.nav.etterlatte
 
 import io.ktor.client.HttpClient
-import no.nav.etterlatte.libs.common.requireEnvValue
+import no.nav.etterlatte.libs.common.Miljoevariabler
+import no.nav.etterlatte.libs.database.DataSourceBuilder
+import no.nav.etterlatte.libs.database.migrate
 import no.nav.etterlatte.libs.ktor.httpClientClientCredentials
 import no.nav.etterlatte.pdltjenester.PdlTjenesterKlient
+import rapidsandrivers.RapidsAndRiversAppBuilder
 
-class AppBuilder(private val env: Map<String, String>) {
+class AppBuilder(private val env: Miljoevariabler) : RapidsAndRiversAppBuilder(env) {
 
     internal fun pdlTjenesterKlient() = PdlTjenesterKlient(
         client = pdlTjenesterHttpClient,
@@ -20,4 +23,10 @@ class AppBuilder(private val env: Map<String, String>) {
             azureAppScope = env.requireEnvValue("PDL_AZURE_SCOPE")
         )
     }
+
+    fun longFeature(featureName: String, default: Long = 0): Long {
+        return (env[featureName]?.toLong() ?: default).takeIf { it > -1 } ?: Long.MAX_VALUE
+    }
+
+    fun createDataSource() = DataSourceBuilder.createDataSource(env.props).apply { migrate() }
 }
--- apps/etterlatte-fordeler/src/main/kotlin/Application.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/Application.kt
@@ -4,36 +4,26 @@ import no.nav.etterlatte.fordeler.Fordeler
 import no.nav.etterlatte.fordeler.FordelerKriterier
 import no.nav.etterlatte.fordeler.FordelerRepository
 import no.nav.etterlatte.fordeler.FordelerService
-import no.nav.etterlatte.libs.database.DataSourceBuilder
-import no.nav.etterlatte.libs.database.migrate
-import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.etterlatte.rapidsandrivers.init
+import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
 
 val sikkerLogg = LoggerFactory.getLogger("sikkerLogg")
 
 fun main() {
     sikkerLogg.info("SikkerLogg: etterlatte-fordeler oppstart")
-
-    val env = System.getenv().toMutableMap().apply {
-        put("KAFKA_CONSUMER_GROUP_ID", requireNotNull(get("NAIS_APP_NAME")).replace("-", ""))
-    }
-    val dataSource = DataSourceBuilder.createDataSource(env).apply { migrate() }
-
-    val appBuilder = AppBuilder(env)
-    RapidApplication.create(env)
-        .also {
+    init(
+        { AppBuilder(it) },
+        { rc: RapidsConnection, ab: AppBuilder ->
             Fordeler(
-                rapidsConnection = it,
+                rapidsConnection = rc,
                 fordelerService = FordelerService(
                     FordelerKriterier(),
-                    appBuilder.pdlTjenesterKlient(),
-                    FordelerRepository(dataSource),
-                    maxFordelingTilDoffen = env.longFeature("FEATURE_MAX_FORDELING_TIL_DOFFEN")
+                    ab.pdlTjenesterKlient(),
+                    FordelerRepository(ab.createDataSource()),
+                    maxFordelingTilDoffen = ab.longFeature("FEATURE_MAX_FORDELING_TIL_DOFFEN")
                 )
             )
-        }.start()
-}
-
-fun Map<String, String>.longFeature(featureName: String, default: Long = 0): Long {
-    return (this[featureName]?.toLong() ?: default).takeIf { it > -1 } ?: Long.MAX_VALUE
+        }
+    )
 }

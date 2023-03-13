f2e209321bc44d12006211c029fa29ddfb386588
Mads Opheim <mads.opheim@nav.no>
2023-03-13 09:43:27 +0100

    EY-1795: Betre med eksplisitt kasting enn !!

--- libs/common/src/main/kotlin/tidspunkt/DatabaseinteraksjonUtils.kt
+++ libs/common/src/main/kotlin/tidspunkt/DatabaseinteraksjonUtils.kt
@@ -7,5 +7,7 @@ import java.sql.Timestamp
 fun Tidspunkt.toTimestamp(): Timestamp = Timestamp.from(this.instant)
 fun Timestamp.toTidspunkt(): Tidspunkt = Tidspunkt(this.toInstant())
 fun PreparedStatement.setTidspunkt(index: Int, value: Tidspunkt?) = setTimestamp(index, value?.toTimestamp())
-fun ResultSet.getTidspunkt(name: String): Tidspunkt = getTidspunktOrNull(name)!!
+fun ResultSet.getTidspunkt(name: String): Tidspunkt =
+    getTidspunktOrNull(name) ?: throw IllegalStateException("Forventa at tidspunkt for $name ikke er null")
+
 fun ResultSet.getTidspunktOrNull(name: String): Tidspunkt? = getTimestamp(name)?.toInstant()?.let { Tidspunkt(it) }

7d3587ad575dd2c0ae0c9ff5f8b26e0c9c7c7445
Mads Opheim <mads.opheim@nav.no>
2023-03-14 10:42:22 +0100

    EY-1898: Droppar buildSrc-metoden for no, kan heller inntil vidare setje det manuelt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -40,21 +40,4 @@ tasks {
         warnUsedUndeclared = true
         warnUnusedDeclared = true
     }
-    withType<Test> {
-        testLogging.exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-    }
-}
-setupTestLogging()
-
-fun Project.setupTestLogging() {
-    for (sub in subprojects) {
-        sub.tasks.withType<Test> {
-            testLogging {
-                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-                showStackTraces = true
-                showStandardStreams = true
-                showCauses = true
-            }
-        }
-    }
 }

14a511321c0b42f2a41feeba9bbfa7a9c5215063
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-03-14 10:33:45 +0100

    Commit offset after reading

--- apps/etterlatte-hendelser-pdl/src/main/kotlin/hendelserpdl/leesah/KafkaConsumerHendelserPdl.kt
+++ apps/etterlatte-hendelser-pdl/src/main/kotlin/hendelserpdl/leesah/KafkaConsumerHendelserPdl.kt
@@ -3,6 +3,7 @@ package no.nav.etterlatte.hendelserpdl.leesah
 import kotlinx.coroutines.delay
 import no.nav.person.pdl.leesah.Personhendelse
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.slf4j.LoggerFactory
 import java.time.Duration
 
 class KafkaConsumerHendelserPdl(
@@ -10,6 +11,7 @@ class KafkaConsumerHendelserPdl(
     env: Map<String, String>,
     kafkaEnvironment: KafkaConsumerConfiguration = KafkaEnvironment()
 ) {
+    private val logger = LoggerFactory.getLogger(this.javaClass.name)
     private var antallMeldinger = 0
     private val leesahtopic = env["LEESAH_TOPIC_PERSON"]
     private val consumer: KafkaConsumer<String, Personhendelse>
@@ -24,6 +26,12 @@ class KafkaConsumerHendelserPdl(
         val meldinger = consumer.poll(Duration.ofSeconds(10L))
         meldinger.forEach {
             personHendelseFordeler.haandterHendelse(it.value())
+        }
+        try {
+            consumer.commitSync()
+        } catch (e: Exception) {
+            logger.error("Kunne ikke committe offsett")
+            throw e
         }
         antallMeldinger += meldinger.count()
         if (meldinger.isEmpty) delay(500)

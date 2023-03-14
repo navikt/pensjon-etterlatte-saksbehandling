06980f5db57462d49d689451c3f87e7b0bf016db
Espen Schei <espen.schei@nav.no>
2023-03-14 11:45:50 +0100

    Sjekk mot behandling for tilgang før man returnerer dokumenter (#953)
    * Sjekk mot behandling for tilgang før man returnerer dokumenter
    
    Det blir muligens gjort en sjekk av dette hos saf også, men greit å være på den sikre siden.
    
    EY-1887
    
    * Rydder opp i uthenting av access token

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/behandling/BehandlingKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/behandling/BehandlingKlient.kt
@@ -5,14 +5,16 @@ import com.github.michaelbull.result.mapBoth
 import com.typesafe.config.Config
 import io.ktor.client.HttpClient
 import no.nav.etterlatte.libs.common.BehandlingTilgangsSjekk
+import no.nav.etterlatte.libs.common.PersonTilgangsSjekk
 import no.nav.etterlatte.libs.common.objectMapper
+import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import no.nav.etterlatte.libs.ktorobo.AzureAdClient
 import no.nav.etterlatte.libs.ktorobo.DownstreamResourceClient
 import no.nav.etterlatte.libs.ktorobo.Resource
 import no.nav.etterlatte.token.Saksbehandler
 import java.util.*
 
-class BehandlingKlient(config: Config, httpClient: HttpClient) : BehandlingTilgangsSjekk {
+class BehandlingKlient(config: Config, httpClient: HttpClient) : BehandlingTilgangsSjekk, PersonTilgangsSjekk {
 
     private val azureAdClient = AzureAdClient(config)
     private val downstreamResourceClient = DownstreamResourceClient(azureAdClient, httpClient)
@@ -36,6 +38,26 @@ class BehandlingKlient(config: Config, httpClient: HttpClient) : BehandlingTilga
                 )
         } catch (e: Exception) {
             throw BehandlingKlientException("Sjekking av tilgang for behandling feilet", e)
+        }
+    }
+
+    override suspend fun harTilgangTilPerson(foedselsnummer: Foedselsnummer, bruker: Saksbehandler): Boolean {
+        try {
+            return downstreamResourceClient
+                .post(
+                    resource = Resource(
+                        clientId = clientId,
+                        url = "$resourceUrl/tilgang/person"
+                    ),
+                    bruker = bruker,
+                    postBody = foedselsnummer.value
+                )
+                .mapBoth(
+                    success = { resource -> resource.response.let { objectMapper.readValue(it.toString()) } },
+                    failure = { throwableErrorMessage -> throw throwableErrorMessage }
+                )
+        } catch (e: Exception) {
+            throw BehandlingKlientException("Sjekking av tilgang for person feilet", e)
         }
     }
 }

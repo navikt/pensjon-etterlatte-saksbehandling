016e82a4b2eccba0fb2dd3a574001fc0c14b5e8f
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-03-15 13:04:24 +0100

    test tilgangsroutes og litt mer rundt adressebeskyttelse

--- apps/etterlatte-behandling/src/main/kotlin/sak/SakDaoAdressebeskyttelse.kt
+++ apps/etterlatte-behandling/src/main/kotlin/sak/SakDaoAdressebeskyttelse.kt
@@ -1,10 +1,55 @@
 package no.nav.etterlatte.sak
 
 import no.nav.etterlatte.libs.common.pdlhendelse.AdressebeskyttelseGradering
+import no.nav.etterlatte.libs.database.single
 import no.nav.etterlatte.libs.database.singleOrNull
+import no.nav.etterlatte.libs.database.toList
 import javax.sql.DataSource
 
 class SakDaoAdressebeskyttelse(private val datasource: DataSource) {
+
+    fun enAvSakeneHarAdresseBeskyttelse(sakIder: List<Long>): Boolean {
+        datasource.connection.use {
+            val statement = it.prepareStatement(
+                """
+                 SELECT count(*) as gradert 
+                 from sak
+                 where id = any(?)
+                 AND adressebeskyttelse is NOT NULL AND adressebeskyttelse IN (?, ?, ?)
+                """.trimIndent()
+            )
+            statement.setArray(1, it.createArrayOf("bigint", sakIder.toTypedArray()))
+            statement.setString(
+                2,
+                AdressebeskyttelseGradering.STRENGT_FORTROLIG.toString()
+            )
+            statement.setString(
+                3,
+                AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND.toString()
+            )
+            statement.setString(
+                4,
+                AdressebeskyttelseGradering.FORTROLIG.toString()
+            )
+            val resultSet = statement.executeQuery()
+            return resultSet.single {
+                getInt(1)
+            } > 0
+        }
+    }
+    fun finnSaker(fnr: String): List<Sak> {
+        datasource.connection.use {
+            val statement = it.prepareStatement("SELECT id, sakType, fnr from sak where fnr = ?")
+            statement.setString(1, fnr)
+            return statement.executeQuery().toList {
+                Sak(
+                    sakType = enumValueOf(getString(2)),
+                    ident = getString(3),
+                    id = getLong(1)
+                )
+            }
+        }
+    }
 
     fun oppdaterAdresseBeskyttelse(id: Long, adressebeskyttelseGradering: AdressebeskyttelseGradering): Int {
         datasource.connection.use {
--- apps/etterlatte-behandling/src/main/kotlin/sak/SakServiceAdressebeskyttelse.kt
+++ apps/etterlatte-behandling/src/main/kotlin/sak/SakServiceAdressebeskyttelse.kt
@@ -5,10 +5,16 @@ import no.nav.etterlatte.libs.common.pdlhendelse.AdressebeskyttelseGradering
 interface SakServiceAdressebeskyttelse {
     fun behandlingHarAdressebeskyttelse(behandlingId: String): Boolean
     fun oppdaterAdressebeskyttelse(id: Long, adressebeskyttelseGradering: AdressebeskyttelseGradering): Int
+    fun sjekkOmSakHarAdresseBeskyttelse(fnr: String): Boolean
 }
 
 class SakServiceAdressebeskyttelseImpl(private val dao: SakDaoAdressebeskyttelse) : SakServiceAdressebeskyttelse {
 
+    private fun finnSakerForPerson(person: String) = dao.finnSaker(person)
+    override fun sjekkOmSakHarAdresseBeskyttelse(fnr: String): Boolean {
+        val sakIder = this.finnSakerForPerson(fnr).map { it.id }
+        return dao.enAvSakeneHarAdresseBeskyttelse(sakIder)
+    }
     override fun behandlingHarAdressebeskyttelse(behandlingId: String) =
         dao.sjekkOmBehandlingHarAdressebeskyttelse(behandlingId).harBeskyttelse()
 

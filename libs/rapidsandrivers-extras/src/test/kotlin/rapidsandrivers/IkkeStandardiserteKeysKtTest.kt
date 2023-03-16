aca51dd640f82c06a39eac6e3b1dff2cf8f260bb
Mads Opheim <mads.opheim@nav.no>
2023-03-16 14:53:34 +0100

    EY-1905: Testar på sjølve json-konverteringa

---
+++ libs/rapidsandrivers-extras/src/test/kotlin/rapidsandrivers/IkkeStandardiserteKeysKtTest.kt
@@ -0,0 +1,24 @@
+package rapidsandrivers
+
+import no.nav.helse.rapids_rivers.JsonMessage
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class IkkeStandardiserteKeysKtTest {
+    @Test
+    fun `behandlingsid-liste blir lik foer og etter`() {
+        val melding = JsonMessage.newMessage(mapOf())
+        val ider = listOf<UUID>(UUID.randomUUID(), UUID.randomUUID())
+        melding.tilbakestilteBehandlinger = ider
+        Assertions.assertEquals(ider, melding.tilbakestilteBehandlinger)
+    }
+
+    @Test
+    fun `kan ha tom behandlingsid-liste`() {
+        val melding = JsonMessage.newMessage(mapOf())
+        val ider = listOf<UUID>()
+        melding.tilbakestilteBehandlinger = ider
+        Assertions.assertEquals(ider, melding.tilbakestilteBehandlinger)
+    }
+}

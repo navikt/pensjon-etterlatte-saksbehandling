367eb6cc036bb83614a4fc023f13c93e98a25af4
henrikg <henrik.gundersen@nav.no>
2023-03-16 22:53:02 +0100

    Fjerner BEREGNET status da denne er fra gammel flyt

---
+++ apps/etterlatte-vedtaksvurdering/src/main/resources/db/migration/V18__remove_beregnet_status.sql
@@ -0,0 +1,1 @@
+UPDATE vedtak SET vedtakstatus = 'OPPRETTET' WHERE vedtakstatus = 'BEREGNET';
--- libs/common/src/main/kotlin/behandling/VedtakStatus.kt
+++ libs/common/src/main/kotlin/behandling/VedtakStatus.kt
@@ -1,7 +1,6 @@
 package no.nav.etterlatte.libs.common.behandling
 
 enum class VedtakStatus {
-    BEREGNET, // TODO denne skal vekk men krever migrering i DB
     OPPRETTET,
     FATTET_VEDTAK,
     ATTESTERT,

b1cf0c688233775ddd9990d50228af328862cc3b
Mads Opheim <mads.opheim@nav.no>
2023-03-17 14:56:10 +0100

    EY-1929: Slettar ubrukte metodar

--- libs/common/src/main/kotlin/Retry.kt
+++ libs/common/src/main/kotlin/Retry.kt
@@ -6,16 +6,7 @@ import no.nav.etterlatte.libs.common.RetryResult.Success
 sealed class RetryResult<T> {
     data class Success<T>(val content: T, val previousExceptions: List<Exception> = emptyList()) : RetryResult<T>()
 
-    data class Failure<T>(val exceptions: List<Exception> = emptyList()) : RetryResult<T>() {
-        fun lastError() = exceptions.lastOrNull()
-    }
-}
-
-suspend fun <T> unsafeRetry(times: Int = 2, block: suspend () -> T) = retry(times, block).let {
-    when (it) {
-        is Success -> it.content
-        is Failure -> throw it.exceptions.last()
-    }
+    data class Failure<T>(val exceptions: List<Exception> = emptyList()) : RetryResult<T>()
 }
 
 suspend fun <T> retry(times: Int = 2, block: suspend () -> T) = retryInner(times, emptyList(), block)

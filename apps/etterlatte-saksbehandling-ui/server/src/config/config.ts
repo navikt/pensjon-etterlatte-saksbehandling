493cb7cd1ca617143ed0498e686e23dc724df631
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-03-24 10:13:23 +0100

    Fix lazy loading for env vars så det ikke tryner lokalt

--- apps/etterlatte-saksbehandling-ui/server/src/config/config.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/config/config.ts
@@ -14,44 +14,47 @@ export const AdConfig = {
   tokenEndpoint: requireEnvValue('AZURE_OPENID_CONFIG_TOKEN_ENDPOINT'),
 }
 
-function hentApiConfigFraEnv(): typeof API_CONFIG_FROM_ENV {
-  const kjoererLokalt = !process.env.NAIS_CLUSTER_NAME // NAIS_CLUSTER_NAME er satt i gcp
-  if (kjoererLokalt) {
-    return LOKAL_API_CONFIG
+function hentApiConfigFraEnv() {
+  const kjorerIGCP = process.env.NAIS_CLUSTER_NAME // NAIS_CLUSTER_NAME er satt i gcp
+  if (kjorerIGCP) {
+    return API_CONFIG_FROM_ENV()
+  } else {
+    return LOKAL_API_CONFIG()
   }
-  return API_CONFIG_FROM_ENV
 }
 
-const LOKAL_API_CONFIG = {
-  vilkaarsvurdering: {
-    url: 'https://etterlatte-vilkaarsvurdering.dev.intern.nav.no',
-    scope: 'api://f4cf400f-8ef9-406f-baf1-8218f8f7edac/.default',
-  },
-  behandling: {
-    url: 'https://etterlatte-behandling.dev.intern.nav.no',
-    scope: 'api://59967ac8-009c-492e-a618-e5a0f6b3e4e4/.default',
-  },
-  grunnlag: {
-    url: 'https://etterlatte-grunnlag.dev.intern.nav.no',
-    scope: 'api://ce96a301-13db-4409-b277-5b27f464d08b/.default',
-  },
-  brev: {
-    url: 'https://etterlatte-brev-api.dev.intern.nav.no',
-    scope: 'api://d6add52a-5807-49cd-a181-76908efee836/.default',
-  },
-  beregning: {
-    url: 'https://etterlatte-beregning.dev.intern.nav.no',
-    scope: 'api://b07cf335-11fb-4efa-bd46-11f51afd5052/.default',
-  },
-  vedtak: {
-    url: 'https://etterlatte-vedtaksvurdering.dev.intern.nav.no',
-    scope: 'api://069b1b2c-0a06-4cc9-8418-f100b8ff71be/.default',
-  },
-  trygdetid: {
-    url: 'https://etterlatte-trygdetid.dev.intern.nav.no',
-    scope: 'api://8385435e-f3d7-45ec-be59-ebd1c71df735/.default',
-  },
-} as const
+const LOKAL_API_CONFIG = () => {
+  return {
+    vilkaarsvurdering: {
+      url: 'https://etterlatte-vilkaarsvurdering.dev.intern.nav.no',
+      scope: 'api://f4cf400f-8ef9-406f-baf1-8218f8f7edac/.default',
+    },
+    behandling: {
+      url: 'https://etterlatte-behandling.dev.intern.nav.no',
+      scope: 'api://59967ac8-009c-492e-a618-e5a0f6b3e4e4/.default',
+    },
+    grunnlag: {
+      url: 'https://etterlatte-grunnlag.dev.intern.nav.no',
+      scope: 'api://ce96a301-13db-4409-b277-5b27f464d08b/.default',
+    },
+    brev: {
+      url: 'https://etterlatte-brev-api.dev.intern.nav.no',
+      scope: 'api://d6add52a-5807-49cd-a181-76908efee836/.default',
+    },
+    beregning: {
+      url: 'https://etterlatte-beregning.dev.intern.nav.no',
+      scope: 'api://b07cf335-11fb-4efa-bd46-11f51afd5052/.default',
+    },
+    vedtak: {
+      url: 'https://etterlatte-vedtaksvurdering.dev.intern.nav.no',
+      scope: 'api://069b1b2c-0a06-4cc9-8418-f100b8ff71be/.default',
+    },
+    trygdetid: {
+      url: 'https://etterlatte-trygdetid.dev.intern.nav.no',
+      scope: 'api://8385435e-f3d7-45ec-be59-ebd1c71df735/.default',
+    },
+  }
+}
 
 function requireEnvValue(key: string): string {
   const envValue = process.env[key]
@@ -61,35 +64,37 @@ function requireEnvValue(key: string): string {
   throw new Error(`Env is missing required key ${key}`)
 }
 
-const API_CONFIG_FROM_ENV = {
-  vilkaarsvurdering: {
-    url: requireEnvValue('VILKAARSVURDERING_API_URL'),
-    scope: requireEnvValue('VILKAARSVURDERING_API_SCOPE'),
-  },
-  behandling: {
-    url: requireEnvValue('BEHANDLING_API_URL'),
-    scope: requireEnvValue('BEHANDLING_API_SCOPE'),
-  },
-  grunnlag: {
-    url: requireEnvValue('GRUNNLAG_API_URL'),
-    scope: requireEnvValue('GRUNNLAG_API_SCOPE'),
-  },
-  brev: {
-    url: requireEnvValue('BREV_API_URL'),
-    scope: requireEnvValue('BREV_API_SCOPE'),
-  },
-  beregning: {
-    url: requireEnvValue('BEREGNING_API_URL'),
-    scope: requireEnvValue('BEREGNING_API_SCOPE'),
-  },
-  vedtak: {
-    url: requireEnvValue('VEDTAK_API_URL'),
-    scope: requireEnvValue('VEDTAK_API_SCOPE'),
-  },
-  trygdetid: {
-    url: requireEnvValue('TRYGDETID_API_URL'),
-    scope: requireEnvValue('TRYGDETID_API_SCOPE'),
-  },
-} as const
+const API_CONFIG_FROM_ENV = () => {
+  return {
+    vilkaarsvurdering: {
+      url: requireEnvValue('VILKAARSVURDERING_API_URL'),
+      scope: requireEnvValue('VILKAARSVURDERING_API_SCOPE'),
+    },
+    behandling: {
+      url: requireEnvValue('BEHANDLING_API_URL'),
+      scope: requireEnvValue('BEHANDLING_API_SCOPE'),
+    },
+    grunnlag: {
+      url: requireEnvValue('GRUNNLAG_API_URL'),
+      scope: requireEnvValue('GRUNNLAG_API_SCOPE'),
+    },
+    brev: {
+      url: requireEnvValue('BREV_API_URL'),
+      scope: requireEnvValue('BREV_API_SCOPE'),
+    },
+    beregning: {
+      url: requireEnvValue('BEREGNING_API_URL'),
+      scope: requireEnvValue('BEREGNING_API_SCOPE'),
+    },
+    vedtak: {
+      url: requireEnvValue('VEDTAK_API_URL'),
+      scope: requireEnvValue('VEDTAK_API_SCOPE'),
+    },
+    trygdetid: {
+      url: requireEnvValue('TRYGDETID_API_URL'),
+      scope: requireEnvValue('TRYGDETID_API_SCOPE'),
+    },
+  }
+}
 
 export const ApiConfig = hentApiConfigFraEnv()

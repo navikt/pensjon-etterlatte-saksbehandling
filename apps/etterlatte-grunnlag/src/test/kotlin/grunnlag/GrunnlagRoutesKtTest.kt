a9815fba35da65f6d5ca3ae040dfd1839ef68659
Nicolas N <Watercolours@users.noreply.github.com>
2023-03-27 10:50:08 +0200

    Fikse feil i grunnlag + tester (#1109)

--- apps/etterlatte-grunnlag/src/test/kotlin/grunnlag/GrunnlagRoutesKtTest.kt
+++ apps/etterlatte-grunnlag/src/test/kotlin/grunnlag/GrunnlagRoutesKtTest.kt
@@ -9,21 +9,33 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.log
 import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.testing.testApplication
+import io.mockk.Called
+import io.mockk.clearAllMocks
 import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.etterlatte.klienter.BehandlingKlient
+import no.nav.etterlatte.libs.common.behandling.PersonMedSakerOgRoller
+import no.nav.etterlatte.libs.common.behandling.SakOgRolle
+import no.nav.etterlatte.libs.common.behandling.Saksrolle
 import no.nav.etterlatte.libs.common.serialize
 import no.nav.etterlatte.libs.ktor.AZURE_ISSUER
 import no.nav.etterlatte.libs.ktor.restModule
 import no.nav.etterlatte.libs.testdata.grunnlag.GrunnlagTestData
+import no.nav.etterlatte.libs.testdata.grunnlag.SOEKER_FOEDSELSNUMMER
 import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import testsupport.buildTestApplicationConfigurationForOauth
+import java.util.UUID
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class GrunnlagRoutesKtTest {
@@ -41,7 +53,17 @@ internal class GrunnlagRoutesKtTest {
         server.start()
         val httpServer = server.config.httpServer
         hoconApplicationConfig = buildTestApplicationConfigurationForOauth(httpServer.port(), AZURE_ISSUER, CLIENT_ID)
+    }
+
+    @BeforeEach
+    fun beforeEach() {
         coEvery { behandlingKlient.harTilgangTilSak(any(), any()) } returns true
+    }
+
+    @AfterEach
+    fun afterEach() {
+        confirmVerified(grunnlagService, behandlingKlient)
+        clearAllMocks()
     }
 
     @AfterAll
@@ -60,6 +82,18 @@ internal class GrunnlagRoutesKtTest {
         ).serialize()
     }
 
+    private val systemBruker: String by lazy {
+        val mittsystem = UUID.randomUUID().toString()
+        server.issueToken(
+            issuerId = AZURE_ISSUER,
+            audience = CLIENT_ID,
+            claims = mapOf(
+                "sub" to mittsystem,
+                "oid" to mittsystem
+            )
+        ).serialize()
+    }
+
     @Test
     fun `returnerer 401 uten gyldig token`() {
         testApplication {
@@ -73,6 +107,8 @@ internal class GrunnlagRoutesKtTest {
 
             Assertions.assertEquals(HttpStatusCode.Unauthorized, response.status)
         }
+
+        coVerify(exactly = 1) { behandlingKlient wasNot Called }
     }
 
     @Test
@@ -95,6 +131,9 @@ internal class GrunnlagRoutesKtTest {
 
             Assertions.assertEquals(HttpStatusCode.NotFound, response.status)
         }
+
+        verify(exactly = 1) { grunnlagService.hentOpplysningsgrunnlag(any()) }
+        coVerify(exactly = 1) { behandlingKlient.harTilgangTilSak(any(), any()) }
     }
 
     @Test
@@ -119,5 +158,66 @@ internal class GrunnlagRoutesKtTest {
             Assertions.assertEquals(HttpStatusCode.OK, response.status)
             Assertions.assertEquals(serialize(testData), response.body<String>())
         }
+
+        verify(exactly = 1) { grunnlagService.hentOpplysningsgrunnlag(any()) }
+        coVerify(exactly = 1) { behandlingKlient.harTilgangTilSak(any(), any()) }
+    }
+
+    @Test
+    fun `roller tilknyttet person`() {
+        val response = PersonMedSakerOgRoller(
+            SOEKER_FOEDSELSNUMMER.value, listOf(
+                SakOgRolle(1, Saksrolle.SOEKER)
+            )
+        )
+        every { grunnlagService.hentSakerOgRoller(any()) } returns response
+
+        testApplication {
+            environment {
+                config = hoconApplicationConfig
+            }
+            application {
+                restModule(this.log, routePrefix = "api") { grunnlagRoute(grunnlagService, behandlingKlient) }
+            }
+            val actualResponse = client.get("api/grunnlag/person/${SOEKER_FOEDSELSNUMMER.value}/roller") {
+                headers {
+                    append(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    append(HttpHeaders.Authorization, "Bearer $systemBruker")
+                }
+            }
+
+            Assertions.assertEquals(HttpStatusCode.OK, actualResponse.status)
+            Assertions.assertEquals(serialize(response), actualResponse.body<String>())
+        }
+
+        verify(exactly = 1) { grunnlagService.hentSakerOgRoller(any()) }
+        coVerify { behandlingKlient wasNot Called }
+    }
+
+    @Test
+    fun `saker tilknyttet person`() {
+        val response: Set<Long> = setOf(1, 2, 3)
+        every { grunnlagService.hentAlleSakerForIdent(any()) } returns response
+
+        testApplication {
+            environment {
+                config = hoconApplicationConfig
+            }
+            application {
+                restModule(this.log, routePrefix = "api") { grunnlagRoute(grunnlagService, behandlingKlient) }
+            }
+            val actualResponse = client.get("api/grunnlag/person/${SOEKER_FOEDSELSNUMMER.value}/saker") {
+                headers {
+                    append(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    append(HttpHeaders.Authorization, "Bearer $systemBruker")
+                }
+            }
+
+            Assertions.assertEquals(HttpStatusCode.OK, actualResponse.status)
+            Assertions.assertEquals(serialize(response), actualResponse.body<String>())
+        }
+
+        verify(exactly = 1) { grunnlagService.hentAlleSakerForIdent(any()) }
+        coVerify { behandlingKlient wasNot Called }
     }
 }

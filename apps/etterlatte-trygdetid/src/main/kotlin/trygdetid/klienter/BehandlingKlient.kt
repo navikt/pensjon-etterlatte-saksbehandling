94db0bec46cc033fea80733fa83d37f1d1c47331
Henrik Gundersen <henrik.gundersen@nav.no>
2023-03-27 15:50:27 +0200

    Sjekker tilgang mot behandling (#1104)
    EY-1928

---
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/BehandlingKlient.kt
@@ -0,0 +1,46 @@
+package no.nav.etterlatte.trygdetid.klienter
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.michaelbull.result.mapBoth
+import com.typesafe.config.Config
+import io.ktor.client.HttpClient
+import no.nav.etterlatte.libs.common.BehandlingTilgangsSjekk
+import no.nav.etterlatte.libs.common.objectMapper
+import no.nav.etterlatte.libs.ktorobo.AzureAdClient
+import no.nav.etterlatte.libs.ktorobo.DownstreamResourceClient
+import no.nav.etterlatte.libs.ktorobo.Resource
+import no.nav.etterlatte.token.Saksbehandler
+import org.slf4j.LoggerFactory
+import java.util.*
+
+class BehandlingKlientException(override val message: String, override val cause: Throwable) : Exception(message, cause)
+
+class BehandlingKlient(config: Config, httpClient: HttpClient) : BehandlingTilgangsSjekk {
+    private val logger = LoggerFactory.getLogger(BehandlingKlient::class.java)
+
+    private val azureAdClient = AzureAdClient(config)
+    private val downstreamResourceClient = DownstreamResourceClient(azureAdClient, httpClient)
+    private val clientId = config.getString("behandling.client.id")
+    private val resourceUrl = config.getString("behandling.resource.url")
+
+    override suspend fun harTilgangTilBehandling(behandlingId: UUID, bruker: Saksbehandler): Boolean {
+        try {
+            logger.info("Sjekker tilgang til behandling $behandlingId")
+
+            return downstreamResourceClient
+                .get(
+                    resource = Resource(
+                        clientId = clientId,
+                        url = "$resourceUrl/tilgang/behandling/$behandlingId"
+                    ),
+                    bruker = bruker
+                )
+                .mapBoth(
+                    success = { resource -> resource.response.let { objectMapper.readValue(it.toString()) } },
+                    failure = { throwableErrorMessage -> throw throwableErrorMessage }
+                )
+        } catch (e: Exception) {
+            throw BehandlingKlientException("Sjekking av tilgang for behandling feilet", e)
+        }
+    }
+}

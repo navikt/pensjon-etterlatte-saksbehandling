318f7f7962ca670a188e7d9ad3c0bb06b989f8c1
Mads Opheim <mads.opheim@nav.no>
2023-03-29 13:31:15 +0200

    Innfører hjelpemetodar for å gjera logikken meir forståeleg

--- apps/etterlatte-behandling/src/main/kotlin/config/ConfigUtils.kt
+++ apps/etterlatte-behandling/src/main/kotlin/config/ConfigUtils.kt
@@ -13,35 +13,30 @@ fun samle(config: Config, env: Map<String, String>): Map<String, String> {
     return conf + env
 }
 
-private fun unwrap(set: MutableSet<Node>, pair: Node): Set<Node> {
-    when (pair.value) {
-        is Map<*, *> -> {
-            pair.value.entries.forEach {
-                set.addAll(
-                    unwrap(
-                        set,
-                        Node(pair.key + "." + it.key.toString(), it.value)
-                    )
-                )
-            }
-            return set
-        }
+private fun unwrap(set: MutableSet<Node>, pair: Node): Set<Node> = when (pair.value) {
+    is Map<*, *> -> set.addAllAndReturn(unwrapMap(pair.value, pair))
+    is List<*> -> set.addAllAndReturn(unwrapList(pair.value, pair))
+    is String -> mutableSetOf(Node(pair.key, pair.value.toString()))
+    else -> set
+}
 
-        is List<*> -> {
-            pair.value.forEach {
-                set.addAll(
-                    unwrap(
-                        set,
-                        Node(pair.key, it)
-                    )
-                )
-            }
-            return set
-        }
+private fun unwrapList(value: List<*>, pair: Node) = value.flatMap {
+    unwrap(
+        mutableSetOf(),
+        Node(pair.key, it)
+    )
+}
 
-        is String -> return mutableSetOf(Node(pair.key, pair.value.toString()))
-        else -> return set
-    }
+private fun unwrapMap(value: Map<*, *>, pair: Node) = value.entries.flatMap {
+    unwrap(
+        mutableSetOf(),
+        Node(pair.key + "." + it.key.toString(), it.value)
+    )
+}
+
+private fun <E> MutableSet<E>.addAllAndReturn(newELements: Collection<E>): MutableSet<E> {
+    addAll(newELements)
+    return this
 }
 
 private data class Node(val key: String, val value: Any?)

f74e3c4fc8c1f1885ea458f6c15e87ec2643c804
Chris Searle <chris@chrissearle.org>
2023-03-29 10:05:49 +0200

    FORTROLIG - ingen enhet

--- apps/etterlatte-behandling/src/main/kotlin/common/Enheter.kt
+++ apps/etterlatte-behandling/src/main/kotlin/common/Enheter.kt
@@ -5,7 +5,6 @@ import no.nav.etterlatte.libs.common.person.Foedselsnummer
 enum class Enheter(val enhetNr: String, val navn: String) {
     STRENGT_FORTROLIG("2103", "NAV Vikafossen"),
     STRENGT_FORTROLIG_UTLAND("2103", "NAV Vikafossen"),
-    FORTROLIG("????", ""),
     EGEN_ANSATTE("4883", "NAV Familie- og pensjonsytelser Egne ansatte"),
     DEFAULT("4808", "NAV Familie- og pensjonsytelser Porsgrunn")
 }

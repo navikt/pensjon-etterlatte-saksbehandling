cc1f45ec45001c9b46df3ad6281c6a56d46e319b
Mads Opheim <mads.opheim@nav.no>
2023-03-29 11:46:40 +0200

    Samler miljøvariablar frå både application.conf og env

--- apps/etterlatte-behandling/src/main/kotlin/config/ConfigUtils.kt
+++ apps/etterlatte-behandling/src/main/kotlin/config/ConfigUtils.kt
@@ -1,15 +1,32 @@
 package no.nav.etterlatte.config
 
 import com.typesafe.config.Config
-import com.typesafe.config.ConfigValue
-import com.typesafe.config.ConfigValueType
 
-fun samle(config: Config, env: Map<String, String>) =
-    config.entrySet().associate { it.key to it.value.asString() } + env
+fun samle(config: Config, env: Map<String, String>): Map<String, String> {
+    val conf = config
+        .root()
+        .toList()
+        .map { unwrap(mutableSetOf(), Pair(it.first, it.second.unwrapped())) }
+        .filter { it.isNotEmpty() }
+        .flatten()
+        .associate { it.first to it.second }
+    return conf + env
+}
 
-private fun ConfigValue.asString(): String = when (valueType()) {
-    ConfigValueType.STRING, ConfigValueType.NUMBER, ConfigValueType.BOOLEAN -> toString()
-    ConfigValueType.NULL, null -> ""
-    ConfigValueType.OBJECT -> toString() // todo
-    ConfigValueType.LIST -> toString() // todo
+fun unwrap(set: MutableSet<Pair<String, String>>, pair: Pair<String, Any?>): Set<Pair<String, String>> {
+    if (pair.second is Map<*, *>) {
+        (pair.second as Map<*, *>).entries.forEach {
+            set.addAll(
+                unwrap(
+                    set,
+                    Pair(pair.first + "." + it.key.toString(), it.value)
+                )
+            )
+        }
+        return set
+    }
+    if (pair.second is String) {
+        set.add(Pair(pair.first, pair.second.toString()))
+    }
+    return set
 }
---
+++ apps/etterlatte-behandling/src/test/kotlin/config/ConfigUtilsTest.kt
@@ -0,0 +1,21 @@
+package no.nav.etterlatte.config
+
+import com.typesafe.config.ConfigFactory
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ConfigUtilsTest {
+
+    @Test
+    fun `samler verdier fra baade applicationconf og miljoevariabler`() {
+        val conf = ConfigFactory.load()
+        val env = mapOf(
+            "key1" to "value1",
+            "funksjonsbrytere.enabled" to "false"
+        )
+        val samla = samle(conf, env)
+        Assertions.assertEquals("false", samla["funksjonsbrytere.enabled"])
+        Assertions.assertEquals("https://unleash.nais.io/api/", samla["funksjonsbrytere.unleash.uri"])
+        Assertions.assertEquals("value1", samla["key1"])
+    }
+}

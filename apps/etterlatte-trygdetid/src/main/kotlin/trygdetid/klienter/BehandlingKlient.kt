fc7b17e67baef6b4bd7c8641e7cad8f714eba37c
henrikg <henrik.gundersen@nav.no>
2023-03-30 22:18:04 +0200

    Forenkler behandling klient

--- apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/BehandlingKlient.kt
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/BehandlingKlient.kt
@@ -45,21 +45,17 @@ class BehandlingKlient(config: Config, httpClient: HttpClient) : BehandlingTilga
         }
     }
 
-    suspend fun kanBeregnes(behandlingId: UUID, bruker: Bruker, commit: Boolean): Boolean {
+    suspend fun kanBeregnes(behandlingId: UUID, bruker: Bruker): Boolean {
         logger.info("Sjekker om behandling med behandlingId=$behandlingId kan beregnes")
         val resource = Resource(clientId = clientId, url = "$resourceUrl/behandlinger/$behandlingId/beregn")
 
-        val response = when (commit) {
-            false -> downstreamResourceClient.get(resource, bruker)
-            true -> downstreamResourceClient.post(resource, bruker, "{}")
-        }
-
-        return response.mapBoth(
-            success = { true },
-            failure = {
-                logger.info("Behandling med id $behandlingId kan ikke beregnes, commit=$commit")
-                false
-            }
-        )
+        return downstreamResourceClient.get(resource, bruker)
+            .mapBoth(
+                success = { true },
+                failure = {
+                    logger.info("Behandling med id $behandlingId kan ikke beregnes")
+                    false
+                }
+            )
     }
 }

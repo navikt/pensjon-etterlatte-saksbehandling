d5cbb918692ffbe69223c2e0ef9c0da955ba0ed6
Emil Schrøder <emil.schroder@nav.no>
2023-03-31 16:16:31 +0200

    Fiks sivilstandmapper foedselsnummer

--- apps/etterlatte-pdltjenester/src/main/kotlin/pdl/mapper/SivilstandMapper.kt
+++ apps/etterlatte-pdltjenester/src/main/kotlin/pdl/mapper/SivilstandMapper.kt
@@ -1,7 +1,7 @@
 package no.nav.etterlatte.pdl.mapper
 
 import kotlinx.coroutines.runBlocking
-import no.nav.etterlatte.libs.common.person.Foedselsnummer
+import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
 import no.nav.etterlatte.libs.common.person.Sivilstand
 import no.nav.etterlatte.libs.common.person.Sivilstatus
 import no.nav.etterlatte.pdl.PdlSivilstand
@@ -14,7 +14,7 @@ object SivilstandMapper {
         sivilstander.map {
             Sivilstand(
                 sivilstatus = it.type.let { type -> Sivilstatus.valueOf(type.name) },
-                relatertVedSiviltilstand = it.relatertVedSivilstand?.let { Foedselsnummer.of(it) },
+                relatertVedSiviltilstand = it.relatertVedSivilstand?.let { Folkeregisteridentifikator.of(it) },
                 gyldigFraOgMed = it.gyldigFraOgMed,
                 bekreftelsesdato = it.bekreftelsesdato,
                 kilde = it.metadata.master

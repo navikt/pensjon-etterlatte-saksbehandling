bfc1c50b1883c6e104ab88bf64bba4ae1190c66e
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2023-04-03 14:57:06 +0200

    Svarer med egen PdlFeil for spesifikke feil mot pdltjenester

--- apps/etterlatte-pdltjenester/src/main/kotlin/person/PersonRoute.kt
+++ apps/etterlatte-pdltjenester/src/main/kotlin/person/PersonRoute.kt
@@ -9,6 +9,8 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import no.nav.etterlatte.libs.common.pdl.PdlFeil
+import no.nav.etterlatte.libs.common.pdl.PdlFeilAarsak
 import no.nav.etterlatte.libs.common.person.FamilieRelasjonManglerIdent
 import no.nav.etterlatte.libs.common.person.HentGeografiskTilknytningRequest
 import no.nav.etterlatte.libs.common.person.HentPdlIdentRequest
@@ -37,9 +39,21 @@ fun Route.personRoute(service: PersonService) {
                 try {
                     service.hentOpplysningsperson(hentPersonRequest).let { call.respond(it) }
                 } catch (e: PdlFantIkkePerson) {
-                    call.respond(HttpStatusCode.NotFound)
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        PdlFeil(
+                            aarsak = PdlFeilAarsak.FANT_IKKE_PERSON,
+                            detaljer = "Fnr ${hentPersonRequest.foedselsnummer} finnes ikke i PDL"
+                        )
+                    )
                 } catch (e: FamilieRelasjonManglerIdent) {
-                    call.respond(HttpStatusCode.ExpectationFailed, e.message)
+                    call.respond(
+                        HttpStatusCode.InternalServerError,
+                        PdlFeil(
+                            aarsak = PdlFeilAarsak.INGEN_IDENT_FAMILIERELASJON,
+                            detaljer = e.message
+                        )
+                    )
                 }
             }
         }
---
+++ libs/common/src/main/kotlin/pdl/PdlFeil.kt
@@ -0,0 +1,7 @@
+package no.nav.etterlatte.libs.common.pdl
+
+data class PdlFeil(val aarsak: PdlFeilAarsak, val detaljer: String?)
+
+enum class PdlFeilAarsak {
+    FANT_IKKE_PERSON, INGEN_IDENT_FAMILIERELASJON
+}

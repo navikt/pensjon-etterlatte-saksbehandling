5bc6053f90eb52b60ce556fb2135e6882eb7c206
Mads Opheim <mads.opheim@nav.no>
2023-04-04 09:43:32 +0200

    EY-1976: Opprettar sak og behandling under migrering

--- apps/etterlatte-behandling/src/main/kotlin/behandling/migrering/MigreringService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/migrering/MigreringService.kt
@@ -1,13 +1,38 @@
 package no.nav.etterlatte.behandling.omregning
 
+import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.behandling.BehandlingHendelseType
+import no.nav.etterlatte.behandling.BehandlingHendelserKanal
+import no.nav.etterlatte.behandling.foerstegangsbehandling.FoerstegangsbehandlingFactory
 import no.nav.etterlatte.inTransaction
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.sak.SakService
+import rapidsandrivers.migrering.MigreringRequest
 import java.util.*
 
-class MigreringService {
-    fun migrer(): UUID {
+class MigreringService(
+    private val sakService: SakService,
+    private val foerstegangsbehandlingFactory: FoerstegangsbehandlingFactory,
+    private val behandlingsHendelser: BehandlingHendelserKanal
+) {
+    fun migrer(request: MigreringRequest): UUID {
         val behandlingId = inTransaction {
-            UUID.randomUUID()
+            opprettSakOgBehandling(request)
         }
+        runBlocking { sendHendelse(behandlingId) }
         return behandlingId
     }
+
+    private fun opprettSakOgBehandling(request: MigreringRequest): UUID =
+        foerstegangsbehandlingFactory.opprettFoerstegangsbehandling(
+            finnEllerOpprettSak(request).id,
+            request.mottattDato,
+            request.persongalleri
+        ).lagretBehandling.id
+
+    private fun finnEllerOpprettSak(request: MigreringRequest) =
+        sakService.finnEllerOpprettSak(request.fnr, SakType.BARNEPENSJON)
+
+    private suspend fun sendHendelse(behandlingId: UUID) =
+        behandlingsHendelser.send(behandlingId to BehandlingHendelseType.OPPRETTET)
 }
--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/migrering/MigreringRequest.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/migrering/MigreringRequest.kt
@@ -1,5 +1,12 @@
 package rapidsandrivers.migrering
 
-data class MigreringRequest(val pesysId: PesysId)
+import no.nav.etterlatte.libs.common.behandling.Persongalleri
+
+data class MigreringRequest(
+    val pesysId: PesysId,
+    val fnr: String,
+    val mottattDato: String,
+    val persongalleri: Persongalleri
+)
 
 data class PesysId(val id: String)

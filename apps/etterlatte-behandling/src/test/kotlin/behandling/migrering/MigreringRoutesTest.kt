59539dccde18d3876685b4bb85e5e03e97676783
Mads Opheim <mads.opheim@nav.no>
2023-04-04 10:59:31 +0200

    EY-1976: Test og feilretting

--- apps/etterlatte-behandling/src/test/kotlin/behandling/migrering/MigreringRoutesTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/migrering/MigreringRoutesTest.kt
@@ -1,17 +1,31 @@
 package behandling.migrering
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.testing.testApplication
 import no.nav.etterlatte.BehandlingIntegrationTest
-import no.nav.etterlatte.funksjonsbrytere.FellesFeatureToggle
+import no.nav.etterlatte.behandling.omregning.MigreringResponse
+import no.nav.etterlatte.libs.common.behandling.Persongalleri
+import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
 import no.nav.etterlatte.module
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import rapidsandrivers.migrering.MigreringRequest
+import rapidsandrivers.migrering.PesysId
+import java.time.LocalDateTime
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MigreringRoutesTest : BehandlingIntegrationTest() {
 
     @BeforeAll
@@ -32,10 +46,27 @@ class MigreringRoutesTest : BehandlingIntegrationTest() {
                 }
             }
             application { module(beanFactory) }
-
-            Assertions.assertTrue(
-                beanFactory.featureToggleService().isEnabled(FellesFeatureToggle.NoOperationToggle, false)
+            val fnr = Folkeregisteridentifikator.of("08071272487")
+            val request = MigreringRequest(
+                pesysId = PesysId("1"),
+                fnr = fnr,
+                mottattDato = LocalDateTime.now(),
+                persongalleri = Persongalleri(fnr.value, "innsender", emptyList(), emptyList(), emptyList())
             )
+
+            val response: MigreringResponse = client.post("/migrering") {
+                addAuthToken(tokenSaksbehandler)
+                contentType(ContentType.Application.Json)
+                setBody(request)
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.Created, status)
+            }.body()
+
+            client.get("/saker/${response.sakId}") {
+                addAuthToken(tokenSaksbehandler)
+            }.apply {
+                Assertions.assertEquals(HttpStatusCode.OK, status)
+            }
         }
     }
 }

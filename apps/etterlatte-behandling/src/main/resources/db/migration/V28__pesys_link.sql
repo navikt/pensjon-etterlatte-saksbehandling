a4539a26330a7b1527fa6abfd75085c4097c1889
Mads Opheim <mads.opheim@nav.no>
2023-04-12 11:35:46 +0200

    EY-1976: Pesys-ID og vår sak-id skal vera unike, så tar med det i databasen

--- apps/etterlatte-behandling/src/main/resources/db/migration/V28__pesys_link.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V28__pesys_link.sql
@@ -1,8 +1,8 @@
 CREATE TABLE pesyskopling
 (
     id BIGSERIAL PRIMARY KEY,
-    pesys_id VARCHAR NOT NULL,
-    sak_id BIGINT NOT NULL
+    pesys_id VARCHAR UNIQUE NOT NULL,
+    sak_id BIGINT UNIQUE NOT NULL
         CONSTRAINT pesys_sak_id_fk
             REFERENCES sak (id),
     opprettet TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP

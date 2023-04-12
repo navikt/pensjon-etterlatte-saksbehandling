97cdb7b0018497a07be88f4c7cfcb2de262b5c3c
Mads Opheim <mads.opheim@nav.no>
2023-04-12 13:36:33 +0200

    EY-1977: Trur det er lurt å ha ein mekanisme som sørger for at endepunkt og klient er i synk. Prøver meg på ein variant. Litt billig-versjonen av MicroProfile REST Client, men det dekkjer iallfall ein del av behovet

--- apps/etterlatte-vilkaarsvurdering-kafka/src/main/kotlin/vilkaarsvurdering/services/VilkaarsvurderingService.kt
+++ apps/etterlatte-vilkaarsvurdering-kafka/src/main/kotlin/vilkaarsvurdering/services/VilkaarsvurderingService.kt
@@ -3,22 +3,26 @@ package no.nav.etterlatte.vilkaarsvurdering.services
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 import no.nav.etterlatte.vilkaarsvurdering.OpprettVilkaarsvurderingFraBehandling
+import vilkaarsvurdering.VilkaarsvurderingAPI
 import java.util.*
 
 interface VilkaarsvurderingService {
     fun kopierForrigeVilkaarsvurdering(behandlingId: UUID, behandlingViOmregnerFra: UUID)
-    fun opprettVilkaarsvurdering(behandlingId: UUID)
+    fun opprettVilkaarsvurdering(behandlingId: UUID): HttpResponse
 }
 
 class VilkaarsvurderingServiceImpl(private val vilkaarsvurderingKlient: HttpClient, private val url: String) :
     VilkaarsvurderingService {
     override fun kopierForrigeVilkaarsvurdering(behandlingId: UUID, behandlingViOmregnerFra: UUID) {
         runBlocking {
-            vilkaarsvurderingKlient.post("$url/api/vilkaarsvurdering/$behandlingId/kopier") {
+            vilkaarsvurderingKlient.post(
+                "url/${VilkaarsvurderingAPI.basisrute}/${VilkaarsvurderingAPI.kopier(behandlingId.toString())}"
+            ) {
                 contentType(ContentType.Application.Json)
                 setBody(OpprettVilkaarsvurderingFraBehandling(behandlingViOmregnerFra))
             }
@@ -26,6 +30,10 @@ class VilkaarsvurderingServiceImpl(private val vilkaarsvurderingKlient: HttpClie
     }
 
     override fun opprettVilkaarsvurdering(behandlingId: UUID) = runBlocking {
-//        vilkaarsvurderingKlient.post()
+        vilkaarsvurderingKlient.post(
+            "url/${VilkaarsvurderingAPI.basisrute}/${VilkaarsvurderingAPI.opprett(behandlingId.toString())}"
+        ) {
+            contentType(ContentType.Application.Json)
+        }
     }
 }
---
+++ libs/etterlatte-vilkaarsvurdering-model/src/main/kotlin/vilkaarsvurdering/VilkaarsvurderingAPI.kt
@@ -0,0 +1,8 @@
+package vilkaarsvurdering
+
+object VilkaarsvurderingAPI {
+    const val basisrute = "api/vilkaarsvurdering"
+
+    fun kopier(behandlingsId: String) = "$behandlingsId/kopier"
+    fun opprett(behandlingsId: String) = "$behandlingsId/opprett"
+}

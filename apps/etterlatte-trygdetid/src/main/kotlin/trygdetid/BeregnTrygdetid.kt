faa9dd62810bbf8b688c61bf96774608667471c7
henrikg <henrik.gundersen@nav.no>
2023-04-14 13:39:33 +0200

    Oppdaterer regelreferanser

--- apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/BeregnTrygdetid.kt
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/BeregnTrygdetid.kt
@@ -12,7 +12,7 @@ import no.nav.etterlatte.libs.regler.og
 import java.time.LocalDate
 import java.time.Period
 
-val GYLDIG_FRA = LocalDate.of(1900, 1, 1)
+val TRYGDETID_OMS_DATO = LocalDate.of(1900, 1, 1)
 
 data class TrygdetidPeriodeGrunnlag(
     val periodeFra: FaktumNode<LocalDate>,
@@ -24,52 +24,65 @@ data class TotalTrygdetidGrunnlag(
 )
 
 val periodeFra: Regel<TrygdetidPeriodeGrunnlag, LocalDate> = finnFaktumIGrunnlag(
-    gjelderFra = GYLDIG_FRA,
+    gjelderFra = TRYGDETID_OMS_DATO,
     beskrivelse = "Periode fra",
     finnFaktum = TrygdetidPeriodeGrunnlag::periodeFra,
     finnFelt = { it }
 )
 
 val periodeTil: Regel<TrygdetidPeriodeGrunnlag, LocalDate> = finnFaktumIGrunnlag(
-    gjelderFra = GYLDIG_FRA,
+    gjelderFra = TRYGDETID_OMS_DATO,
     beskrivelse = "Periode til",
     finnFaktum = TrygdetidPeriodeGrunnlag::periodeTil,
     finnFelt = { it }
 )
 
 val beregnTrygdetidMellomToDatoer = RegelMeta(
-    gjelderFra = GYLDIG_FRA,
+    gjelderFra = TRYGDETID_OMS_DATO,
     beskrivelse = "Beregner trygdetid mellom to datoer i år, måneder og dager",
-    regelReferanse = RegelReferanse(id = "TODO")
+    regelReferanse = RegelReferanse(id = "REGEL-TRYGDETID-BEREGNE-PERIODE")
 ) benytter periodeFra og periodeTil med { periodeFra, periodeTil ->
     Period.between(periodeFra, periodeTil.plusDays(1))
 }
 
 val beregnetTrygdetidPerioder: Regel<TotalTrygdetidGrunnlag, List<Period>> = finnFaktumIGrunnlag(
-    gjelderFra = GYLDIG_FRA,
+    gjelderFra = TRYGDETID_OMS_DATO,
     beskrivelse = "Beregnet trygdetidsperioder",
     finnFaktum = TotalTrygdetidGrunnlag::beregnetTrygdetidPerioder,
     finnFelt = { it }
 )
 
 val maksTrygdetid = definerKonstant<TotalTrygdetidGrunnlag, Int>(
-    gjelderFra = GYLDIG_FRA,
+    gjelderFra = TRYGDETID_OMS_DATO,
     beskrivelse = "Full trygdetidsopptjening er 40 år",
-    regelReferanse = RegelReferanse("TODO"),
+    regelReferanse = RegelReferanse("REGEL-TOTAL-TRYGDETID-MAKS-ANTALL-ÅR"),
     verdi = 40
 )
 
-val beregnAntallAarTrygdetid = RegelMeta(
-    gjelderFra = GYLDIG_FRA,
-    beskrivelse = "Beregner antall år trygdetid totalt",
-    regelReferanse = RegelReferanse(id = "TODO")
-) benytter beregnetTrygdetidPerioder og maksTrygdetid med { beregnetTrygdetidPerioder, maksTrygdetid ->
-    // TODO denne må regelspesifiseres
-    val totalTrygdetid = beregnetTrygdetidPerioder.reduce { acc, period -> acc.plus(period) }.normalized()
-    val avrundOpp = totalTrygdetid.months > 0 || totalTrygdetid.days > 0
-    if (avrundOpp) {
-        minOf(totalTrygdetid.years + 1, maksTrygdetid)
+val totalTrygdetidFraPerioder = RegelMeta(
+    gjelderFra = TRYGDETID_OMS_DATO,
+    beskrivelse = "Beregner trygdetid fra perioder",
+    regelReferanse = RegelReferanse(id = "REGEL-TOTAL-TRYGDETID-SLÅ-SAMMEN-PERIODER")
+) benytter beregnetTrygdetidPerioder med { beregnetTrygdetidPerioder ->
+    beregnetTrygdetidPerioder.reduce { acc, period -> acc.plus(period) }.normalized()
+}
+
+val totalTrygdetidAvrundet = RegelMeta(
+    gjelderFra = TRYGDETID_OMS_DATO,
+    beskrivelse = "Runder av trygdetid opp til hele år",
+    regelReferanse = RegelReferanse(id = "REGEL-TOTAL-TRYGDETID-AVRUNDING")
+) benytter totalTrygdetidFraPerioder med { totalTrygdetidFraPerioder ->
+    if (totalTrygdetidFraPerioder.months > 0 || totalTrygdetidFraPerioder.days > 0) {
+        totalTrygdetidFraPerioder.years + 1
     } else {
-        minOf(totalTrygdetid.years, maksTrygdetid)
+        totalTrygdetidFraPerioder.years
     }
+}
+
+val beregnAntallAarTrygdetid = RegelMeta(
+    gjelderFra = TRYGDETID_OMS_DATO,
+    beskrivelse = "Beregner antall år trygdetid totalt",
+    regelReferanse = RegelReferanse(id = "REGEL-TOTAL-TRYGDETID")
+) benytter totalTrygdetidAvrundet og maksTrygdetid med { totalTrygdetidAvrundet, maksTrygdetid ->
+    minOf(totalTrygdetidAvrundet, maksTrygdetid)
 }

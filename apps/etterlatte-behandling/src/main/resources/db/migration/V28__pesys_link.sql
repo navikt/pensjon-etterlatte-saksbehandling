adaa4e22b0bd96d9fe903e1dc400a6259a230b97
Mads Opheim <mads.opheim@nav.no>
2023-04-16 19:10:09 +0200

    EY-1844: Rettar mergefeil

--- apps/etterlatte-behandling/src/main/resources/db/migration/V28__pesys_link.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pesyskopling
-(
-    id BIGSERIAL PRIMARY KEY,
-    pesys_id VARCHAR UNIQUE NOT NULL,
-    sak_id BIGINT UNIQUE NOT NULL
-        CONSTRAINT pesys_sak_id_fk
-            REFERENCES sak (id),
-    opprettet TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
-);

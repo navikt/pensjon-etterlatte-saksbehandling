d10ab8e7739d6b9d3cee7ed8abdd48682d5d5b81
Mads Opheim <mads.opheim@nav.no>
2023-04-17 12:27:09 +0200

    EY-1977: Mangla ein slash

--- libs/etterlatte-vilkaarsvurdering-model/src/main/kotlin/vilkaarsvurdering/VilkaarsvurderingAPI.kt
+++ libs/etterlatte-vilkaarsvurdering-model/src/main/kotlin/vilkaarsvurdering/VilkaarsvurderingAPI.kt
@@ -3,6 +3,6 @@ package vilkaarsvurdering
 object VilkaarsvurderingAPI {
     const val basisrute = "api/vilkaarsvurdering"
 
-    fun kopier(behandlingsId: String) = "$behandlingsId/kopier"
-    fun opprett(behandlingsId: String) = "$behandlingsId/opprett"
+    fun kopier(behandlingsId: String) = "/$behandlingsId/kopier"
+    fun opprett(behandlingsId: String) = "/$behandlingsId/opprett"
 }

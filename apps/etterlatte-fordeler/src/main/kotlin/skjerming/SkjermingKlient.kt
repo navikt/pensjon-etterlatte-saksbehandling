df58188553f66ac6011aa6972537f9f9751eff36
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-04-18 10:40:04 +0200

    Bruk post for å skjule fnr i nytt endepunkt og skru på for dev

--- apps/etterlatte-fordeler/src/main/kotlin/skjerming/SkjermingKlient.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/skjerming/SkjermingKlient.kt
@@ -3,9 +3,13 @@ package no.nav.etterlatte.skjerming
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.accept
-import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import no.nav.etterlatte.libs.common.logging.X_CORRELATION_ID
+import no.nav.etterlatte.libs.common.logging.getXCorrelationId
 
 class SkjermingKlient(
     private val httpClient: HttpClient,
@@ -13,9 +17,16 @@ class SkjermingKlient(
 ) {
 
     suspend fun personErSkjermet(fnr: String): Boolean {
-        return httpClient.get("$url/skjermet?personident=$fnr") {
+        return httpClient.post("$url/skjermet") {
             accept(ContentType.Application.Json)
+            header(X_CORRELATION_ID, getXCorrelationId())
+            header("Nav_Call_Id", getXCorrelationId())
             contentType(ContentType.Application.Json)
+            setBody(SkjermetDataRequestDTO(personident = fnr))
         }.body()
     }
 }
+
+internal class SkjermetDataRequestDTO(
+    val personident: String? = null
+)

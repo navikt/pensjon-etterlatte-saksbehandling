726d21fc82997dde7f485cdb749475daf987d3e1
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-04-18 11:51:47 +0200

    Add correlation header for behandlingsklient

--- apps/etterlatte-fordeler/src/main/kotlin/behandling/BehandlingKlient.kt
+++ apps/etterlatte-fordeler/src/main/kotlin/behandling/BehandlingKlient.kt
@@ -3,12 +3,15 @@ package no.nav.etterlatte.behandling
 import com.fasterxml.jackson.databind.node.ObjectNode
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import no.nav.etterlatte.libs.common.FoedselsnummerDTO
 import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.logging.X_CORRELATION_ID
+import no.nav.etterlatte.libs.common.logging.getXCorrelationId
 
 class BehandlingKlient(
     private val httpClient: HttpClient,
@@ -18,6 +21,8 @@ class BehandlingKlient(
         return httpClient.post("$url/personer/saker/$sakType") {
             contentType(ContentType.Application.Json)
             setBody(FoedselsnummerDTO(fnr))
+            header(X_CORRELATION_ID, getXCorrelationId())
+            header("Nav_Call_Id", getXCorrelationId())
         }.body<ObjectNode>()["id"].longValue()
     }
 }

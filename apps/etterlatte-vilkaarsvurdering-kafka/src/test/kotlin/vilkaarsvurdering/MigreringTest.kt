79a0128d7dec011d8c0f543cc98d4857a599ac6b
Mads Opheim <mads.opheim@nav.no>
2023-04-18 08:14:34 +0200

    EY-1977: Test

---
+++ apps/etterlatte-vilkaarsvurdering-kafka/src/test/kotlin/vilkaarsvurdering/MigreringTest.kt
@@ -0,0 +1,45 @@
+package vilkaarsvurdering
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import no.nav.etterlatte.vilkaarsvurdering.Migrering
+import no.nav.etterlatte.vilkaarsvurdering.services.VilkaarsvurderingService
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Test
+import rapidsandrivers.BEHANDLING_ID_KEY
+import rapidsandrivers.migrering.Migreringshendelser
+import java.util.*
+
+internal class MigreringTest {
+    private val vilkaarsvurderingServiceMock = mockk<VilkaarsvurderingService> {
+        coEvery { oppdaterTotalVurdering(any(), any()) } returns mockk()
+    }
+    private val testRapid = TestRapid()
+        .apply { Migrering(this, vilkaarsvurderingServiceMock) }
+
+    @Test
+    fun `tar opp migrer vilkaarsvurdering-event, kopierer vilkaarsvurdering og poster ny BEREGN-melding`() {
+        val behandlingId = UUID.randomUUID()
+
+        val melding = JsonMessage.newMessage(
+            mapOf(
+                "@event_name" to Migreringshendelser.VILKAARSVURDER,
+                "sakId" to 1,
+                BEHANDLING_ID_KEY to behandlingId
+            )
+        ).toJson()
+        testRapid.sendTestMessage(melding)
+
+        coVerify(exactly = 1) {
+            vilkaarsvurderingServiceMock.oppdaterTotalVurdering(
+                behandlingId,
+                any()
+            )
+        }
+        with(testRapid.inspektør.message(0)) {
+            org.junit.jupiter.api.Assertions.assertEquals(Migreringshendelser.BEREGN, this["@event_name"].asText())
+        }
+    }
+}

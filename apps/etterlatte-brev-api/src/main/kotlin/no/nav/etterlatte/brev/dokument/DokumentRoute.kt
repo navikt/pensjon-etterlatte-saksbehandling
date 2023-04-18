adbb03f9301cf1156dbd8156c9177019b3848fe7
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2023-04-18 13:40:38 +0200

    withFoedselsnummer henter ut DTO og bruker det (#1271)

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/dokument/DokumentRoute.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/dokument/DokumentRoute.kt
@@ -1,7 +1,6 @@
 package no.nav.etterlatte.brev.dokument
 
 import io.ktor.server.application.call
-import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
@@ -9,16 +8,13 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.etterlatte.brev.journalpost.BrukerIdType
 import no.nav.etterlatte.brev.tilgangssjekk.BehandlingKlient
-import no.nav.etterlatte.libs.common.FoedselsnummerDTO
 import no.nav.etterlatte.libs.common.withFoedselsnummer
 import no.nav.etterlatte.libs.ktor.bruker
 
 fun Route.dokumentRoute(safService: SafService, behandlingKlient: BehandlingKlient) {
     route("dokumenter") {
         post {
-            val foedselsnummerDTO = call.receive<FoedselsnummerDTO>()
-            val fnr = foedselsnummerDTO.foedselsnummer
-            withFoedselsnummer(fnr, behandlingKlient) { foedselsnummer ->
+            withFoedselsnummer(behandlingKlient) { foedselsnummer ->
                 val result = safService.hentDokumenter(foedselsnummer.value, BrukerIdType.FNR, bruker)
 
                 if (result.error == null) {

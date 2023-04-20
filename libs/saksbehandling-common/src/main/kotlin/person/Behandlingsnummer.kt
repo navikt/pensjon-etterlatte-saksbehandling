d26034193e4bc17f98e4f5eda7239971f9f78724
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-04-20 10:51:37 +0200

    EY-2049 Lenke til behandlingskatalogen for etterlatte

--- libs/saksbehandling-common/src/main/kotlin/person/Behandlingsnummer.kt
+++ libs/saksbehandling-common/src/main/kotlin/person/Behandlingsnummer.kt
@@ -1,5 +1,6 @@
 package no.nav.etterlatte.libs.common.person
 
+// behandlingsnummere: https://behandlingskatalog.nais.adeo.no/team/cf8730ca-3aa7-4a94-97ec-75bcc620b63f
 enum class Behandlingsnummer(val behandlingsnummer: String) {
     BARNEPENSJON("B359"),
     OMSTILLINGSSTOENAD("B373")

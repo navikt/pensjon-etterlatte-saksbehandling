de311f466fa86a465a84d3f88ecce6604e025485
Henrik Gundersen <henrik.gundersen@nav.no>
2023-04-21 15:11:10 +0200

    Endrer bruk kjøreplan fra J til N i utbetaling (#1294)
    Dette for å beregne saker tilbake i tid tidligere enn ved neste månedskjøring.
    
    EY-2057

--- apps/etterlatte-utbetaling/src/main/kotlin/utbetaling/common/OppdragDefaults.kt
+++ apps/etterlatte-utbetaling/src/main/kotlin/utbetaling/common/OppdragDefaults.kt
@@ -12,7 +12,7 @@ object OppdragDefaults {
     const val MOTTAKENDE_KOMPONENTKODE = "OS" // Oppdragsystemet
     const val SAKSBEHANDLER_ID_SYSTEM_ETTERLATTEYTELSER = "EY" // Placeholder for å identifisere
     const val UTBETALINGSFREKVENS = "MND"
-    const val KJOEREPLAN_SAMMEN_MED_NESTE_PLANLAGTE_UTBETALING = "J"
+    const val KJOEREPLAN_SAMMEN_MED_NESTE_PLANLAGTE_UTBETALING = "N"
     const val AKSJONSKODE_OPPDATER = "1"
     val KODEKOMPONENT = OppdragKlassifikasjonskode.BARNEPENSJON_OPTP
     val DATO_OPPDRAG_GJELDER_FOM = LocalDate.EPOCH.toXMLDate()
@@ -27,6 +27,6 @@ object OppdragDefaults {
 
 object OppdragslinjeDefaults {
     const val UTBETALINGSFREKVENS = "MND"
-    const val KJOEREPLAN_SAMMEN_MED_NESTE_PLANLAGTE_UTBETALING = "J"
+    const val KJOEREPLAN_SAMMEN_MED_NESTE_PLANLAGTE_UTBETALING = "N"
     val FRADRAG_ELLER_TILLEGG = TfradragTillegg.T
 }

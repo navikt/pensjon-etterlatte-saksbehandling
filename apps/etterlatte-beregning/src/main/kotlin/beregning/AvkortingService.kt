dc4188d98c1f34af86f93a83de60d84ed6b9ebee
bjorngolberg <bjorn.golberg@nav.no>
2023-04-26 13:36:42 +0200

    EY-2052 lagre og hente avkortinggrunnlag database

--- apps/etterlatte-beregning/src/main/kotlin/beregning/AvkortingService.kt
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/AvkortingService.kt
@@ -9,7 +9,7 @@ class AvkortingService(
 ) {
 
     private val logger = LoggerFactory.getLogger(AvkortingService::class.java)
-    fun hentAvkorting(behandlingId: UUID): Avkorting {
+    fun hentAvkorting(behandlingId: UUID): Avkorting? {
         logger.info("Henter avkorting for behandlingId=$behandlingId")
         return avkortingRepository.hentAvkorting(behandlingId)
     }
@@ -20,7 +20,7 @@ class AvkortingService(
         avkortingGrunnlag: AvkortingGrunnlag
     ): Avkorting = tilstandssjekk(behandlingId, bruker) {
         logger.info("Lagre grunnlag for avkorting for behandlingId=$behandlingId")
-        avkortingRepository.lagreAvkortingGrunnlag(behandlingId, avkortingGrunnlag)
+        avkortingRepository.lagreEllerOppdaterAvkortingGrunnlag(behandlingId, avkortingGrunnlag)
     }
 
     private suspend fun tilstandssjekk(behandlingId: UUID, bruker: Bruker, block: suspend () -> Avkorting): Avkorting {

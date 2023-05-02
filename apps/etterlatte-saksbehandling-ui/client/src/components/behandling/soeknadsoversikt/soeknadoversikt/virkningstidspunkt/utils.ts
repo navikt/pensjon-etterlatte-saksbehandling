4c1ab101c141d1fe3f09e81a73457d83cf7907f0
Espen Schei <espen.schei@nav.no>
2023-05-02 13:29:28 +0200

    Endre tekst og hjemmel på virkningstidspunkt ved opphør
    Det er forskjellig hjemmel og tekst på virkningstidspunkt alt etter om det er innvilgelse, revurdering eller opphør.
    
    EY-1601

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/soeknadoversikt/virkningstidspunkt/utils.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/soeknadoversikt/virkningstidspunkt/utils.ts
@@ -18,7 +18,14 @@ export const BP_FOERSTEGANGSBEHANDLING_HJEMLER: Array<Hjemmel> = [
 export const BP_REVURDERING_HJEMLER: Array<Hjemmel> = [
   {
     lenke: 'https://lovdata.no/lov/1997-02-28-19/§22-12',
-    tittel: 'Folketrygdloven § 22-12 fjerde, femte og sjette ledd ',
+    tittel: 'Folketrygdloven § 22-12 fjerde, femte og sjette ledd',
+  },
+]
+
+export const BP_OPPHOER_HJEMLER: Array<Hjemmel> = [
+  {
+    lenke: 'https://lovdata.no/lov/1997-02-28-19/§22-12',
+    tittel: 'Folketrygdloven § 22-12 sjette ledd',
   },
 ]
 
@@ -26,6 +33,8 @@ export const BP_FOERSTEGANGSBEHANDLING_BESKRIVELSE =
   'Barnepensjon kan tidligst innvilges fra og med den første i måneden etter dødsfallet og den kan gis for opptil tre år før søknaden er mottatt.'
 export const BP_REVURDERING_BESKRIVELSE =
   'Barnepensjonen blir satt opp fra og med den måneden vilkårene for dette er oppfylt, og satt ned fra og med måneden etter den måneden vilkårene for dette er oppfylt. Barnepensjonen stanses ved utgangen av den måneden retten til ytelsen faller bort.'
+export const BP_OPPHOER_BESKRIVELSE =
+  'Barnepensjonen stanses ved utgangen av den måneden retten til ytelsen faller bort.'
 
 export const OMS_FOERSTEGANGSBEHANDLING_BESKRIVELSE =
   'Omstillingsstønad kan innvilges fra og med den første i måneden etter dødsfallet, men kan som hovedregel ikke gis for mer enn tre måneder før søknaden er mottatt hos NAV.'

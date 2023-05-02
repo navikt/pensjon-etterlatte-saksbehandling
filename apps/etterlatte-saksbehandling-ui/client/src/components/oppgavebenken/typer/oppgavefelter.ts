d37457dd8987d7c92cc7e5e6c5f7b202c394f7cf
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-05-02 12:52:28 +0200

    Velg -> Vis alle filter (#1347)
    * Velg -> Vis alle filter
    
    * Fix references

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/typer/oppgavefelter.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenken/typer/oppgavefelter.ts
@@ -77,7 +77,7 @@ export const initialOppgaveFelter = (): IOppgaveFelter => {
       label: 'Oppgavetype',
       filter: {
         type: 'select',
-        selectedValue: OppgaveTypeFilter.VELG,
+        selectedValue: OppgaveTypeFilter.ALLE,
         nedtrekksliste: oppgaveTypeFilter,
       },
     },
@@ -86,7 +86,7 @@ export const initialOppgaveFelter = (): IOppgaveFelter => {
       label: 'Ytelse',
       filter: {
         type: 'select',
-        selectedValue: SoeknadTypeFilter.VELG,
+        selectedValue: SoeknadTypeFilter.ALLE,
         nedtrekksliste: soeknadTypeFilter,
       },
     },
@@ -95,7 +95,7 @@ export const initialOppgaveFelter = (): IOppgaveFelter => {
       label: 'Status',
       filter: {
         type: 'select',
-        selectedValue: StatusFilter.VELG,
+        selectedValue: StatusFilter.ALLE,
         nedtrekksliste: statusFilter,
       },
     },
@@ -104,7 +104,7 @@ export const initialOppgaveFelter = (): IOppgaveFelter => {
       label: 'Enhet',
       filter: {
         type: 'select',
-        selectedValue: EnhetFilter.VELG,
+        selectedValue: EnhetFilter.ALLE,
         nedtrekksliste: enhetFilter,
       },
     },

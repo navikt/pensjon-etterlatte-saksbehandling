45607d20cac3163bea6892035216152694dd53eb
Mads Opheim <mads.opheim@nav.no>
2023-05-03 09:58:15 +0200

    EY-1978: Imports

--- apps/etterlatte-trygdetid-kafka/src/main/kotlin/Application.kt
+++ apps/etterlatte-trygdetid-kafka/src/main/kotlin/Application.kt
@@ -1,6 +1,8 @@
 package no.nav.etterlatte
 
 import no.nav.etterlatte.libs.common.Miljoevariabler
+import no.nav.etterlatte.trygdetid.kafka.AppBuilder
+import no.nav.etterlatte.trygdetid.kafka.MigreringHendelser
 import no.nav.helse.rapids_rivers.RapidApplication
 import rapidsandrivers.getRapidEnv
 

5794ad72679e85bc92c596f604e0282d8515f745
Chris Searle <chris@chrissearle.org>
2023-05-03 15:21:56 +0200

    EY-1818 - kun data migrering

--- apps/etterlatte-beregning/build.gradle.kts
+++ apps/etterlatte-beregning/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     implementation(project(":libs:etterlatte-ktor"))
     implementation(project(":libs:etterlatte-database"))
     implementation(project(":libs:ktor2client-onbehalfof"))
+    implementation(project(":libs:etterlatte-jobs"))
+    implementation(project(":libs:etterlatte-funksjonsbrytere"))
 
     implementation(libs.database.flywaydb)
     implementation(libs.database.kotliquery)

c4811d3f83e880bb7152909f04606d75bbb3f06c
henrikg <henrik.gundersen@nav.no>
2023-05-04 14:01:45 +0200

    Fikser navn database-skjema

--- apps/etterlatte-beregning/src/main/kotlin/beregning/Avkorting.kt
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/Avkorting.kt
@@ -1,7 +1,7 @@
 package no.nav.etterlatte.beregning
 
-import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
 import com.fasterxml.jackson.databind.JsonNode
+import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
 import no.nav.etterlatte.libs.common.periode.Periode
 import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import java.util.*
@@ -17,8 +17,8 @@ data class AvkortingGrunnlag(
     val aarsinntekt: Int,
     val gjeldendeAar: Int,
     val spesifikasjon: String,
-    val beregnetAvkorting: List<BeregnetAvkortingGrunnlag>
-    val kilde: Grunnlagsopplysning.Saksbehandler,
+    val beregnetAvkorting: List<BeregnetAvkortingGrunnlag>,
+    val kilde: Grunnlagsopplysning.Saksbehandler
 )
 
 data class BeregnetAvkortingGrunnlag(

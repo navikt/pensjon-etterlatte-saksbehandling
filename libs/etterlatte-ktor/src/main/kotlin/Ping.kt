d0e8d989f1a5d701ef795d077f83fae7c013e523
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-05-22 17:45:46 +0200

    EY-2217 Move pingable interface to etterlatte-ktor

---
+++ libs/etterlatte-ktor/src/main/kotlin/Ping.kt
@@ -0,0 +1,59 @@
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+
+interface Pingable {
+    val serviceName: String
+    val beskrivelse: String
+    val endpoint: String
+    suspend fun ping(): PingResult
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun asyncPing() {
+        GlobalScope.launch(newSingleThreadContext("pingThread")) {
+            ping()
+        }
+    }
+}
+
+class PingResult private constructor(
+    private val serviceName: String,
+    private val status: ServiceStatus,
+    private val endpoint: String,
+    private val beskrivelse: String,
+    private val errorMessage: String?
+) {
+    fun getStatus(): ServiceStatus {
+        return status
+    }
+
+    fun toStringServiceDown(): String {
+        return "Servicename: $serviceName endpoint: $endpoint beskrivelse $beskrivelse errorMessage: $errorMessage "
+    }
+
+    companion object {
+        fun up(serviceName: String, endpoint: String, description: String): PingResult {
+            return PingResult(serviceName, ServiceStatus.UP, endpoint, description, null)
+        }
+
+        fun down(serviceName: String, endpoint: String, errorMessage: String?, description: String): PingResult {
+            return PingResult(serviceName, ServiceStatus.DOWN, endpoint, description, errorMessage)
+        }
+    }
+}
+
+enum class ServiceStatus(private val code: Int, private val color: String) {
+    DOWN(1, "red"), UP(0, "green");
+
+    /**
+     * 0 = OK, 1 = ERROR, ref. https://confluence.adeo.no/display/AURA/Selftest
+     */
+    fun code(): Int {
+        return code
+    }
+
+    fun color(): String {
+        return color
+    }
+}

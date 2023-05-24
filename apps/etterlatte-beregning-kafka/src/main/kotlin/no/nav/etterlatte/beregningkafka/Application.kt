1fea3488256394c774cd4a841014f3ada2614565
Mads Opheim <mads.opheim@nav.no>
2023-05-25 00:21:29 +0300

    EY-1979: Initialiserer

--- apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/Application.kt
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/Application.kt
@@ -1,6 +1,7 @@
 package no.nav.etterlatte
 
 import no.nav.etterlatte.beregningkafka.AppBuilder
+import no.nav.etterlatte.beregningkafka.MigreringHendelser
 import no.nav.etterlatte.beregningkafka.OmregningHendelser
 import no.nav.etterlatte.libs.common.Miljoevariabler
 import no.nav.helse.rapids_rivers.RapidApplication
@@ -9,10 +10,12 @@ import rapidsandrivers.getRapidEnv
 fun main() {
     val rapidEnv = getRapidEnv()
     RapidApplication.create(rapidEnv).also { rapidsConnection ->
+        val beregningService = AppBuilder(Miljoevariabler(rapidEnv)).createBeregningService()
         OmregningHendelser(
             rapidsConnection,
-            AppBuilder(Miljoevariabler(rapidEnv)).createBeregningService(),
+            beregningService,
             AppBuilder(Miljoevariabler(rapidEnv)).createTrygdetidService()
         )
+        MigreringHendelser(rapidsConnection, beregningService)
     }.start()
 }

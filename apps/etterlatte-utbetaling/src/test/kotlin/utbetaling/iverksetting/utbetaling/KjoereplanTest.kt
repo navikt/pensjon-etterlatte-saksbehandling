910aeb4fd4c8ea435ad97e4ff2e45e5c02665ea1
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-05-24 15:23:04 +0200

    Test deserialisering kjoereplan

--- apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/iverksetting/utbetaling/KjoereplanTest.kt
+++ apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/iverksetting/utbetaling/KjoereplanTest.kt
@@ -3,6 +3,8 @@ package utbetaling.iverksetting.utbetaling
 import no.nav.etterlatte.utbetaling.iverksetting.utbetaling.Kjoereplan
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class KjoereplanTest {
 
@@ -10,5 +12,16 @@ class KjoereplanTest {
     fun `kjoereplan toString mapper til riktig oppdragverdi`() {
         assertEquals("J", Kjoereplan.NESTE_PLANLAGTE_UTBETALING.toString())
         assertEquals("N", Kjoereplan.MED_EN_GANG.toString())
+    }
+
+    @Test
+    fun `Kjoereplan fraVerdi mapper riktig`() {
+        assertEquals(Kjoereplan.MED_EN_GANG, Kjoereplan.fraKode("n"))
+        assertEquals(Kjoereplan.MED_EN_GANG, Kjoereplan.fraKode(" N "))
+        assertEquals(Kjoereplan.NESTE_PLANLAGTE_UTBETALING, Kjoereplan.fraKode(" j "))
+        assertEquals(Kjoereplan.NESTE_PLANLAGTE_UTBETALING, Kjoereplan.fraKode("J"))
+
+        assertThrows<IllegalArgumentException> { Kjoereplan.fraKode("b") }
+        assertThrows<IllegalArgumentException> { Kjoereplan.fraKode("av og til") }
     }
 }

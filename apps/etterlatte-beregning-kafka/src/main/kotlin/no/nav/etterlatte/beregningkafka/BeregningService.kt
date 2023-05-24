9cd8d9eac11b4b6c2fe706a346cd8aea7f64cb28
Mads Opheim <mads.opheim@nav.no>
2023-05-25 00:19:53 +0300

    EY-1979: Beregning for migrering

--- apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
@@ -2,8 +2,12 @@ package no.nav.etterlatte.beregningkafka
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.beregning.grunnlag.BarnepensjonBeregningsGrunnlag
 import java.util.*
 
 class BeregningService(
@@ -16,6 +20,13 @@ class BeregningService(
 
     fun opprettBeregningsGrunnlag(omregningsId: UUID, forrigeBehandlingsId: UUID): HttpResponse = runBlocking {
         beregningApp.post("$url/api/beregning/beregningsgrunnlag/$omregningsId/fra/$forrigeBehandlingsId") {}
+    }
+
+    fun opprettBeregningsgrunnlag(behandlingId: UUID, request: BarnepensjonBeregningsGrunnlag) = runBlocking {
+        beregningApp.post("$url/api/beregning/beregningsgrunnlag/$behandlingId/barnepensjon") {
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }
     }
 
     fun regulerAvkorting(behandlingId: UUID, forrigeBehandlingId: UUID): HttpResponse = runBlocking {

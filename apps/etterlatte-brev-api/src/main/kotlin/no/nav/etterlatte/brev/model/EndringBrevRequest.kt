97361087bb943f4085852670f2529280132d3e60
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-05-25 15:35:33 +0200

    Enkelt brev for revurderinger

---
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/EndringBrevRequest.kt
@@ -0,0 +1,43 @@
+package no.nav.etterlatte.brev.model
+
+import no.nav.etterlatte.brev.behandling.Avdoed
+import no.nav.etterlatte.brev.behandling.Behandling
+import no.nav.etterlatte.brev.behandling.Soeker
+import no.nav.etterlatte.brev.behandling.Utbetalingsinfo
+import no.nav.etterlatte.libs.common.behandling.RevurderingAarsak
+
+class EndringBrevRequest(
+    val saksnummer: String,
+    val utbetalingsinfo: Utbetalingsinfo,
+    val barn: Soeker,
+    val revurderingsaarsak: RevurderingAarsak,
+    val avdoed: Avdoed,
+    override val spraak: Spraak,
+    override val avsender: Avsender,
+    override val mottaker: BrevMottaker,
+    override val attestant: Attestant?
+) : BrevRequest() {
+
+    override fun templateName(): String = "endring"
+
+    companion object {
+        fun fraVedtak(
+            behandling: Behandling,
+            avsender: Avsender,
+            mottaker: BrevMottaker,
+            attestant: Attestant?
+        ): EndringBrevRequest = EndringBrevRequest(
+            spraak = behandling.spraak,
+            avsender = avsender,
+            mottaker = mottaker,
+            attestant = attestant,
+            saksnummer = behandling.sakId.toString(),
+            utbetalingsinfo = behandling.utbetalingsinfo!!,
+            barn = behandling.persongalleri.soeker,
+            avdoed = behandling.persongalleri.avdoed,
+            revurderingsaarsak = requireNotNull(behandling.revurderingsaarsak) {
+                "Endringsbrev trenger en revurderingsaarsak"
+            }
+        )
+    }
+}

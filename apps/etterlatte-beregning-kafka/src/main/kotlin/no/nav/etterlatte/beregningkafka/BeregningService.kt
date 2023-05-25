1ad96b2af5a502f48b6f97aefc8dc409ef87f3e4
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-05-25 20:21:48 +0300

    EY-1979: Migrering for beregning (#1452)
    * EY-1979: Beregning for migrering
    
    * EY-1979: Initialiserer
    
    * EY-1979: Pirk
    
    * EY-1979: Viser kva det er som er problemet ved ukomplett grunnlag
    
    * EY-1979: Viser kva som er problemet med varer ut perioden
    
    * EY-1979: Kanskje tom her skal vera null?

--- apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
@@ -2,8 +2,12 @@ package no.nav.etterlatte.beregningkafka
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.beregning.grunnlag.BarnepensjonBeregningsGrunnlag
 import java.util.*
 
 class BeregningService(
@@ -15,10 +19,17 @@ class BeregningService(
     }
 
     fun opprettBeregningsGrunnlag(omregningsId: UUID, forrigeBehandlingsId: UUID): HttpResponse = runBlocking {
-        beregningApp.post("$url/api/beregning/beregningsgrunnlag/$omregningsId/fra/$forrigeBehandlingsId") {}
+        beregningApp.post("$url/api/beregning/beregningsgrunnlag/$omregningsId/fra/$forrigeBehandlingsId")
+    }
+
+    fun opprettBeregningsgrunnlag(behandlingId: UUID, request: BarnepensjonBeregningsGrunnlag) = runBlocking {
+        beregningApp.post("$url/api/beregning/beregningsgrunnlag/$behandlingId/barnepensjon") {
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }
     }
 
     fun regulerAvkorting(behandlingId: UUID, forrigeBehandlingId: UUID): HttpResponse = runBlocking {
-        beregningApp.post("$url/api/beregning/avkorting/$behandlingId/med/$forrigeBehandlingId") {}
+        beregningApp.post("$url/api/beregning/avkorting/$behandlingId/med/$forrigeBehandlingId")
     }
 }
---
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/MigreringHendelser.kt
@@ -0,0 +1,74 @@
+package no.nav.etterlatte.beregningkafka
+
+import io.ktor.client.call.body
+import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.beregning.grunnlag.BarnepensjonBeregningsGrunnlag
+import no.nav.etterlatte.beregning.grunnlag.GrunnlagMedPeriode
+import no.nav.etterlatte.beregning.grunnlag.Institusjonsopphold
+import no.nav.etterlatte.libs.common.beregning.BeregningDTO
+import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.SoeskenMedIBeregning
+import no.nav.etterlatte.libs.common.logging.withLogContext
+import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
+import no.nav.etterlatte.libs.common.rapidsandrivers.correlationId
+import no.nav.etterlatte.libs.common.rapidsandrivers.eventName
+import no.nav.etterlatte.rapidsandrivers.migrering.MigreringRequest
+import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
+import no.nav.etterlatte.rapidsandrivers.migrering.hendelseData
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import org.slf4j.LoggerFactory
+import rapidsandrivers.BEHANDLING_ID_KEY
+import rapidsandrivers.BEREGNING_KEY
+import rapidsandrivers.HENDELSE_DATA_KEY
+import rapidsandrivers.behandlingId
+import rapidsandrivers.withFeilhaandtering
+
+internal class MigreringHendelser(rapidsConnection: RapidsConnection, private val beregningService: BeregningService) :
+    River.PacketListener {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        River(rapidsConnection).apply {
+            eventName(Migreringshendelser.BEREGN)
+            validate { it.requireKey(BEHANDLING_ID_KEY) }
+            validate { it.requireKey(HENDELSE_DATA_KEY) }
+            correlationId()
+        }.register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        withLogContext(packet.correlationId) {
+            withFeilhaandtering(packet, context, Migreringshendelser.BEREGN) {
+                val behandlingId = packet.behandlingId
+                logger.info("Mottatt beregnings-migreringshendelse for $BEHANDLING_ID_KEY $behandlingId")
+
+                beregningService.opprettBeregningsgrunnlag(behandlingId, tilGrunnlagDTO(packet.hendelseData))
+
+                runBlocking {
+                    val beregning = beregningService.beregn(behandlingId).body<BeregningDTO>()
+                    packet[BEREGNING_KEY] = beregning
+                }
+                packet.eventName = Migreringshendelser.VEDTAK
+                context.publish(packet.toJson())
+                logger.info("Publiserte oppdatert migreringshendelse fra beregning for behandling $behandlingId")
+            }
+        }
+    }
+
+    private fun tilGrunnlagDTO(request: MigreringRequest): BarnepensjonBeregningsGrunnlag =
+        BarnepensjonBeregningsGrunnlag(
+            soeskenMedIBeregning = listOf(
+                GrunnlagMedPeriode(
+                    fom = request.trygdetidsgrunnlag.fom,
+                    tom = null,
+                    data = request.persongalleri.soesken.map {
+                        SoeskenMedIBeregning(foedselsnummer = Folkeregisteridentifikator.of(it), skalBrukes = true)
+                    }
+                )
+            ),
+            institusjonsopphold = Institusjonsopphold(false)
+        )
+}

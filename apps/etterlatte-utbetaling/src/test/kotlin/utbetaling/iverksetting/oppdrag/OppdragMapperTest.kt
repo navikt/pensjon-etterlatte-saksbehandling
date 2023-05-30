95b951691360dfdf9859e510d9da682f8eb7aa36
jorgesti <jorgen.stikbakke@nav.no>
2023-05-30 15:53:16 +0200

    Forbedret OppdragMapperTest

--- apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/iverksetting/oppdrag/OppdragMapperTest.kt
+++ apps/etterlatte-utbetaling/src/test/kotlin/utbetaling/iverksetting/oppdrag/OppdragMapperTest.kt
@@ -2,6 +2,7 @@ package no.nav.etterlatte.utbetaling.iverksetting.oppdrag
 
 import no.nav.etterlatte.utbetaling.iverksetting.utbetaling.Saktype
 import no.nav.etterlatte.utbetaling.utbetaling
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
@@ -10,13 +11,11 @@ internal class OppdragMapperTest {
     @Test
     fun oppdragFraVedtak() {
         val oppdrag = OppdragMapper.oppdragFraUtbetaling(utbetaling(sakType = Saktype.BARNEPENSJON), true)
-
-        assertNotNull(oppdrag)
+        assertEquals(oppdrag.oppdrag110.kodeFagomraade, "BARNEPE")
     }
     @Test
     fun oppdragFraVedtakOMS() {
         val oppdrag = OppdragMapper.oppdragFraUtbetaling(utbetaling(sakType = Saktype.OMSTILLINGSSTOENAD), true)
-
-        assertNotNull(oppdrag)
+        assertEquals(oppdrag.oppdrag110.kodeFagomraade, "OMSTILL")
     }
 }

526e84b332b60b761561e6bc302bbb448d7f14d5
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-06-01 12:27:02 +0200

    Normalisert utskrift av enkelte land (#1489)
    * Normalisert utskrift av enkelte land
    
    * Fix oppslag mot normalisertbeskrivelse
    
    * Add isokode
    
    * Add test

--- apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/KodeverkService.kt
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/KodeverkService.kt
@@ -2,6 +2,7 @@ package no.nav.etterlatte.trygdetid
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.etterlatte.trygdetid.klienter.Beskrivelse
+import no.nav.etterlatte.trygdetid.klienter.BetydningMedIsoKode
 import no.nav.etterlatte.trygdetid.klienter.KodeverkKlient
 import no.nav.etterlatte.trygdetid.klienter.KodeverkResponse
 import java.util.concurrent.TimeUnit
@@ -17,8 +18,27 @@ class KodeverkService(private val klient: KodeverkKlient) {
 
         return landkoder
             .betydninger
-            .flatMap { (_, betydninger) -> betydninger }
-            .mapNotNull { it.beskrivelser["nb"]?.let { beskrivelse -> Land(it.gyldigFra, it.gyldigTil, beskrivelse) } }
+            .flatMap { (isoLandkode, betydninger) ->
+                betydninger.map {
+                    BetydningMedIsoKode(
+                        gyldigFra = it.gyldigFra,
+                        gyldigTil = it.gyldigTil,
+                        beskrivelser = it.beskrivelser,
+                        isolandkode = isoLandkode
+                    )
+                }
+            }
+            .mapNotNull { betydningMedIsoKode ->
+                betydningMedIsoKode.beskrivelser["nb"]?.let { beskrivelse ->
+                    Land(
+                        isoLandkode = betydningMedIsoKode.isolandkode,
+                        betydningMedIsoKode.gyldigFra,
+                        betydningMedIsoKode.gyldigTil,
+                        LandNormalisert.hentBeskrivelse(betydningMedIsoKode.isolandkode)
+                            ?.let { Beskrivelse(term = beskrivelse.term, tekst = it) } ?: beskrivelse
+                    )
+                }
+            }
     }
 }
 
@@ -27,6 +47,7 @@ private enum class CacheKey {
 }
 
 data class Land(
+    val isoLandkode: String,
     val gyldigFra: String,
     val gyldigTil: String,
     val beskrivelse: Beskrivelse
--- apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/KodeverkKlient.kt
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/KodeverkKlient.kt
@@ -34,3 +34,10 @@ data class KodeverkResponse(val betydninger: Map<String, List<Betydning>>)
 
 data class Beskrivelse(val term: String, val tekst: String)
 data class Betydning(val gyldigFra: String, val gyldigTil: String, val beskrivelser: Map<String, Beskrivelse>)
+
+data class BetydningMedIsoKode(
+    val gyldigFra: String,
+    val gyldigTil: String,
+    val beskrivelser: Map<String, Beskrivelse>,
+    val isolandkode: String
+)

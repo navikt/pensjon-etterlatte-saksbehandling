b91e3b4c20e5146a7f3cad92e856d4afee7e4d46
Mads Opheim <mads.opheim@nav.no>
2023-06-05 08:51:59 +0200

    EY-2168: Startar å definere reglar. Uferdig og uverifisert

---
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/regler/barnepensjon/Institusjonsoppholdregler.kt
@@ -0,0 +1,26 @@
+package beregning.regler.barnepensjon
+
+import no.nav.etterlatte.beregning.grunnlag.InstitusjonsoppholdBeregningsgrunnlag
+import no.nav.etterlatte.beregning.grunnlag.Reduksjon
+import no.nav.etterlatte.beregning.regler.barnepensjon.BP_1967_DATO
+import no.nav.etterlatte.beregning.regler.barnepensjon.BarnepensjonGrunnlag
+import no.nav.etterlatte.libs.regler.Regel
+import no.nav.etterlatte.libs.regler.RegelReferanse
+import no.nav.etterlatte.libs.regler.definerKonstant
+import no.nav.etterlatte.libs.regler.finnFaktumIGrunnlag
+import no.nav.etterlatte.regler.Beregningstall
+
+val institusjonsoppholdFaktor = definerKonstant<BarnepensjonGrunnlag, Beregningstall>(
+    gjelderFra = BP_1967_DATO,
+    beskrivelse = "Institusjonsopphold har i utgangspunktet 10% av full sats",
+    regelReferanse = RegelReferanse("BP-BEREGNING-1967-INSTITUSJONSOPPHOLD-FAKTOR"),
+    verdi = Beregningstall(0.1)
+)
+
+val institusjonsoppholdRegel: Regel<BarnepensjonGrunnlag, Reduksjon> =
+    finnFaktumIGrunnlag(
+        gjelderFra = BP_1967_DATO,
+        beskrivelse = "Finner søkers institusjonsopphold",
+        finnFaktum = BarnepensjonGrunnlag::institusjonsopphold,
+        finnFelt = InstitusjonsoppholdBeregningsgrunnlag::reduksjon
+    )

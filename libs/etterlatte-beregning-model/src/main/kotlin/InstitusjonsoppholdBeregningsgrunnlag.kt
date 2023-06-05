9627ec74542760e5286444554c10a98e372bc2e3
Mads Opheim <mads.opheim@nav.no>
2023-06-05 09:10:44 +0200

    EY-2168: Modellerer inn reduksjonsandelen

--- libs/etterlatte-beregning-model/src/main/kotlin/InstitusjonsoppholdBeregningsgrunnlag.kt
+++ libs/etterlatte-beregning-model/src/main/kotlin/InstitusjonsoppholdBeregningsgrunnlag.kt
@@ -4,12 +4,26 @@ data class InstitusjonsoppholdBeregningsgrunnlag(
     val reduksjon: Reduksjon,
     val egenReduksjon: Int? = null,
     val begrunnelse: String? = null
-)
+) {
+    fun redusertProsent() = if (reduksjon.erEgendefinert()) {
+        Prosent(egenReduksjon!!)
+    } else {
+        reduksjon.verdi!!
+    }
+}
 
-enum class Reduksjon {
-    VELG_REDUKSJON,
-    JA_VANLIG,
-    NEI_KORT_OPPHOLD,
-    JA_EGEN_PROSENT_AV_G,
-    NEI_HOEYE_UTGIFTER_BOLIG
+enum class Reduksjon(val verdi: Prosent?) {
+    VELG_REDUKSJON(null),
+    JA_VANLIG(Prosent(90)),
+    NEI_KORT_OPPHOLD(Prosent(0)),
+    JA_EGEN_PROSENT_AV_G(null),
+    NEI_HOEYE_UTGIFTER_BOLIG(Prosent(0));
+
+    fun erEgendefinert() = this == JA_EGEN_PROSENT_AV_G
+}
+
+data class Prosent(val verdi: Int) {
+    init {
+        require(verdi in 0..100)
+    }
 }

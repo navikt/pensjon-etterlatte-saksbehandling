3f46bfd9ede6355805b33ddd10b3cbdd1a215a34
Mads Opheim <mads.opheim@nav.no>
2023-06-07 16:02:06 +0200

    EY-2296: brev-api også

--- apps/etterlatte-brev-api/build.gradle.kts
+++ apps/etterlatte-brev-api/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(project(":libs:saksbehandling-common"))
     implementation(project(":libs:etterlatte-ktor"))
+    implementation(project(":libs:etterlatte-behandling-model"))
     implementation(project(":libs:etterlatte-database"))
     implementation(project(":libs:ktor2client-onbehalfof"))
     implementation(project(":libs:ktor2client-auth-clientcredentials"))

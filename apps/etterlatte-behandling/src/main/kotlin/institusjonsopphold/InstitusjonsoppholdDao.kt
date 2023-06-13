712d36f46e2de1bcb791aa4215146968d95cb7b3
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-06-13 14:03:17 +0200

    EY-2327 Uuid

--- apps/etterlatte-behandling/src/main/kotlin/institusjonsopphold/InstitusjonsoppholdDao.kt
+++ apps/etterlatte-behandling/src/main/kotlin/institusjonsopphold/InstitusjonsoppholdDao.kt
@@ -9,7 +9,7 @@ class InstitusjonsoppholdDao(private val connection: () -> Connection) {
             val statement = prepareStatement(
                 "INSERT INTO institusjonsoppholdhendelse(id, sak_id, kanGiReduksjon, kanGiReduksjonTekst," +
                     "merEnnTreMaaneder, merEnnTreMaanederTekst) " +
-                    "VALUES(?, ?, ?, ?, ?, ?)"
+                    "VALUES(?::UUID, ?, ?, ?, ?, ?)"
             )
             statement.setString(1, UUID.randomUUID().toString())
             statement.setLong(2, sakId)
@@ -17,7 +17,7 @@ class InstitusjonsoppholdDao(private val connection: () -> Connection) {
             statement.setString(4, institusjonoppholdBegrunnelse.kanGiReduksjonAvYtelseBegrunnelse)
             statement.setString(5, institusjonoppholdBegrunnelse.forventetVarighetMerEnn3Maaneder.toString())
             statement.setString(6, institusjonoppholdBegrunnelse.forventetVarighetMerEnn3MaanederBegrunnelse)
-            statement.executeQuery()
+            statement.executeUpdate()
         }
     }
 }

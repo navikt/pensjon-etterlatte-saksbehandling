2ca67962da8512481eaa2b40a4d3165506dfd67e
Mads Opheim <mads.opheim@nav.no>
2023-06-14 14:02:51 +0200

    EY-1578: Objekt heller enn boolean-duo

--- libs/saksbehandling-common/src/main/kotlin/behandling/RevurderingAarsak.kt
+++ libs/saksbehandling-common/src/main/kotlin/behandling/RevurderingAarsak.kt
@@ -1,33 +1,55 @@
 package no.nav.etterlatte.libs.common.behandling
 
+import no.nav.etterlatte.libs.common.behandling.KanBrukesIMiljoe.DevOgProd
+import no.nav.etterlatte.libs.common.behandling.KanBrukesIMiljoe.IngenMiljoe
+import no.nav.etterlatte.libs.common.behandling.KanBrukesIMiljoe.KunIDev
 import no.nav.etterlatte.libs.common.clusternavn
 
 private val SAKTYPE_OMS = listOf(SakType.OMSTILLINGSSTOENAD)
 private val SAKTYPE_BP = listOf(SakType.BARNEPENSJON)
 private val SAKTYPE_BP_OMS = SAKTYPE_BP + SAKTYPE_OMS
 
+sealed class KanBrukesIMiljoe {
+    abstract val prod: Boolean
+    abstract val dev: Boolean
+
+    object KunIDev : KanBrukesIMiljoe() {
+        override val prod = false
+        override val dev = true
+    }
+
+    object DevOgProd : KanBrukesIMiljoe() {
+        override val prod = true
+        override val dev = true
+    }
+
+    object IngenMiljoe : KanBrukesIMiljoe() {
+        override val prod = false
+        override val dev = false
+    }
+}
+
 enum class RevurderingAarsak(
     private val gyldigFor: List<SakType>,
-    private val kanBrukesDev: Boolean,
-    private val kanBrukesProd: Boolean,
+    private val miljoe: KanBrukesIMiljoe,
     val girOpphoer: Boolean,
     val skalSendeBrev: Boolean
 ) {
-    ANSVARLIGE_FORELDRE(SAKTYPE_BP, false, false, false, true),
-    SOESKENJUSTERING(SAKTYPE_BP, true, false, false, true),
-    UTLAND(SAKTYPE_BP, false, false, false, true),
-    BARN(SAKTYPE_BP, false, false, false, true),
-    VERGEMAAL_ELLER_FREMTIDSFULLMAKT(SAKTYPE_BP, false, false, false, true),
-    REGULERING(SAKTYPE_BP_OMS, true, true, false, false),
-    DOEDSFALL(SAKTYPE_BP_OMS, true, false, true, false),
-    INNTEKTSENDRING(SAKTYPE_OMS, true, false, false, true),
-    OMGJOERING_AV_FARSKAP(SAKTYPE_BP, true, false, true, true);
+    ANSVARLIGE_FORELDRE(SAKTYPE_BP, IngenMiljoe, false, true),
+    SOESKENJUSTERING(SAKTYPE_BP, KunIDev, false, true),
+    UTLAND(SAKTYPE_BP, IngenMiljoe, false, true),
+    BARN(SAKTYPE_BP, IngenMiljoe, false, true),
+    VERGEMAAL_ELLER_FREMTIDSFULLMAKT(SAKTYPE_BP, IngenMiljoe, false, true),
+    REGULERING(SAKTYPE_BP_OMS, DevOgProd, false, false),
+    DOEDSFALL(SAKTYPE_BP_OMS, KunIDev, true, false),
+    INNTEKTSENDRING(SAKTYPE_OMS, KunIDev, false, true),
+    OMGJOERING_AV_FARSKAP(SAKTYPE_BP, KunIDev, true, true);
 
     fun kanBrukesIMiljo(): Boolean = when (clusternavn()) {
         null -> true
-        GcpEnv.PROD.name -> this.kanBrukesProd
-        GcpEnv.DEV.name -> this.kanBrukesDev
-        else -> this.kanBrukesDev
+        GcpEnv.PROD.name -> miljoe.prod
+        GcpEnv.DEV.name -> miljoe.dev
+        else -> miljoe.dev
     }
 
     fun gyldigForSakType(sakType: SakType): Boolean = gyldigFor.any { it == sakType }

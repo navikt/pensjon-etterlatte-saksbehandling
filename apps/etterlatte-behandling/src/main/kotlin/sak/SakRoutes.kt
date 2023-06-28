13a37c4a10bedce6a23628e31fdc966a098cfdb4
Mads Opheim <mads.opheim@nav.no>
2023-06-28 08:27:13 +0200

    EY-2347: Fjernar dobbel slash

--- apps/etterlatte-behandling/src/main/kotlin/sak/SakRoutes.kt
+++ apps/etterlatte-behandling/src/main/kotlin/sak/SakRoutes.kt
@@ -32,7 +32,7 @@ internal fun Route.sakSystemRoutes(
 ) {
     val logger = LoggerFactory.getLogger(this::class.java)
 
-    route("/api/saker//{$SAKID_CALL_PARAMETER}/behandlinger/sisteIverksatte") {
+    route("/api/saker/{$SAKID_CALL_PARAMETER}/behandlinger/sisteIverksatte") {
         get {
             logger.info("Henter siste iverksatte behandling for $sakId")
             when (val sisteIverksatteBehandling = generellBehandlingService.hentSisteIverksatte(sakId)) {

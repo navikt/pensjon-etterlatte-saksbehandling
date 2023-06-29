89a8ac901271460bd495cfd1e2ac7a39ba6bacfb
Espen Schei <espen.schei@nav.no>
2023-06-29 14:32:53 +0200

    fjern ubrukt import

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/useBehandling.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/useBehandling.ts
@@ -1,5 +1,4 @@
 import { useAppSelector } from '~store/Store'
-import { IDetaljertBehandling } from '~shared/types/IDetaljertBehandling'
 import { IBehandlingReducer } from '~store/reducers/BehandlingReducer'
 
 export function useBehandling(): IBehandlingReducer | null {

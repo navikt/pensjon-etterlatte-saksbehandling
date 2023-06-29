4e9e043437d0f1638257e8dcedd023cfd7c24cf3
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-06-29 15:43:20 +0200

    EY-2408 Lagt til noen db-indekser i vilkårsvurdering basert på stats fra prod samt explain (#1664)

---
+++ apps/etterlatte-vilkaarsvurdering/src/main/resources/db/migration/V14__legg_til_index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX ON delvilkaar (vilkaar_id);
+CREATE INDEX ON delvilkaar (vilkaar_type);
+CREATE INDEX ON vilkaar (vilkaarsvurdering_id);
+CREATE INDEX ON vilkaarsvurdering (behandling_id);
+CREATE INDEX ON grunnlag (vilkaar_id);

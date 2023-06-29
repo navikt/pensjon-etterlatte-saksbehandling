04a7c4660c2d7a71c4b0940ae764c272b3231b96
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-06-29 11:51:15 +0200

    EY-2405 Lagt til noen db-indekser basert på stats fra prod (#1662)

---
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V41__legg_til_index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX ON behandling (sak_id);
+CREATE INDEX ON grunnlagsendringshendelse (sak_id);
+CREATE INDEX ON grunnlagsendringshendelse (behandling_id);
+CREATE INDEX ON grunnlagsendringshendelse (status);

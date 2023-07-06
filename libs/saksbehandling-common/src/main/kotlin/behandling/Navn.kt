ae7604dc115b97ecc72bebe3ab244195e9bddeab
Mads Opheim <mads.opheim@nav.no>
2023-07-06 13:22:41 +0200

    EY-2433: Forenkling etter kodegjennomgang

--- libs/saksbehandling-common/src/main/kotlin/behandling/Navn.kt
+++ libs/saksbehandling-common/src/main/kotlin/behandling/Navn.kt
@@ -1,5 +1,5 @@
 package no.nav.etterlatte.libs.common.behandling
 
 data class Navn(val fornavn: String, val mellomnavn: String? = null, val etternavn: String) {
-    fun formaterNavn() = "$fornavn " + if (mellomnavn != null) { "$mellomnavn " } else { "" } + etternavn
+    override fun toString(): String = listOfNotNull(fornavn, mellomnavn, etternavn).joinToString(" ")
 }

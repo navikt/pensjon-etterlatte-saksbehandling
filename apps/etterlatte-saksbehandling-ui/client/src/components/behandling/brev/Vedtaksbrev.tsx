8a52ffa74b2b9fa6fc2212754cdf015a12f04ade
Nicolas N <nicolas.nordhagen@nav.no>
2023-07-06 10:34:13 +0200

    Sjekke om brev skal sendes

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brev/Vedtaksbrev.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brev/Vedtaksbrev.tsx
@@ -8,8 +8,12 @@ import { useParams } from 'react-router-dom'
 import { Soeknadsdato } from '../soeknadsoversikt/soeknadoversikt/Soeknadsdato'
 import styled from 'styled-components'
 import { SendTilAttesteringModal } from '../handlinger/sendTilAttesteringModal'
-import { hentBehandlesFraStatus, manueltBrevKanRedigeres } from '~components/behandling/felles/utils'
-import { IBehandlingsType, IDetaljertBehandling } from '~shared/types/IDetaljertBehandling'
+import {
+  behandlingSkalSendeBrev,
+  hentBehandlesFraStatus,
+  manueltBrevKanRedigeres,
+} from '~components/behandling/felles/utils'
+import { IDetaljertBehandling } from '~shared/types/IDetaljertBehandling'
 import MottakerPanel from '~components/behandling/brev/detaljer/MottakerPanel'
 import ForhaandsvisningBrev from '~components/behandling/brev/ForhaandsvisningBrev'
 import Spinner from '~shared/Spinner'
@@ -28,8 +32,7 @@ export const Vedtaksbrev = (props: { behandling: IDetaljertBehandling }) => {
     if (!behandlingId || !sak) return
 
     const fetchVedtaksbrev = async () => {
-      const skalIkkeHenteVedtaksbrev = props.behandling.behandlingType === IBehandlingsType.REVURDERING
-      if (skalIkkeHenteVedtaksbrev) {
+      if (!behandlingSkalSendeBrev(props.behandling)) {
         return
       }
 

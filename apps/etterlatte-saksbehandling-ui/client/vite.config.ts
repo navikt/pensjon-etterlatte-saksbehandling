4cf26663e133ed6a826a9fa984e8812ee9562a12
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-07-12 15:21:59 +0200

    Vise basert på clustername (#1762)

--- apps/etterlatte-saksbehandling-ui/client/vite.config.ts
+++ apps/etterlatte-saksbehandling-ui/client/vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://0.0.0.0:8080',
-        changeOrigin: true,
-      },
-      '/brev': {
-        target: 'http://0.0.0.0:8080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      'process.env.NAIS_CLUSTER_NAME': env.NAIS_CLUSTER_NAME,
+    },
+    server: {
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://0.0.0.0:8080',
+          changeOrigin: true,
+        },
+        '/brev': {
+          target: 'http://0.0.0.0:8080',
+          changeOrigin: true,
+        },
       },
     },
-  },
-  build: {
-    sourcemap: true,
-  },
+    build: {
+      sourcemap: true,
+    },
+  }
 })

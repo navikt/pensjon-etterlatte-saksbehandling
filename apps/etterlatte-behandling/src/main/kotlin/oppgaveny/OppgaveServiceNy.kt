ec3d960ba68d7cc09fe639312a423bc43c50394d
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-07-12 12:28:34 +0200

    Intransaction rundt db spørringer for oppgaver ny

--- apps/etterlatte-behandling/src/main/kotlin/oppgaveny/OppgaveServiceNy.kt
+++ apps/etterlatte-behandling/src/main/kotlin/oppgaveny/OppgaveServiceNy.kt
@@ -1,12 +1,15 @@
 package no.nav.etterlatte.oppgaveny
 
+import no.nav.etterlatte.inTransaction
 import no.nav.etterlatte.sak.SakDao
 import no.nav.etterlatte.tilgangsstyring.SaksbehandlerMedRoller
 
 class OppgaveServiceNy(private val oppgaveDaoNy: OppgaveDaoNy, private val sakDao: SakDao) {
     // bruker: SaksbehandlerMedRoller må på en måte inn her
     fun finnOppgaverForBruker(bruker: SaksbehandlerMedRoller): List<OppgaveNy> {
-        return oppgaveDaoNy.hentOppgaver()
+        return inTransaction {
+            oppgaveDaoNy.hentOppgaver()
+        }
     }
 
     fun opprettNyOppgaveMedSakOgReferanse(referanse: String, sakId: Long, oppgaveType: OppgaveType) {
@@ -15,6 +18,6 @@ class OppgaveServiceNy(private val oppgaveDaoNy: OppgaveDaoNy, private val sakDa
     }
 
     fun lagreOppgave(oppgaveNy: OppgaveNy) { // TODO: skal det være noen verifisering her? evt en egen metode for route
-        oppgaveDaoNy.lagreOppgave(oppgaveNy)
+        return oppgaveDaoNy.lagreOppgave(oppgaveNy)
     }
 }

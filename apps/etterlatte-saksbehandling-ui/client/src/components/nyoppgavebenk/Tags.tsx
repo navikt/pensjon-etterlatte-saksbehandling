23069c5a85f688fd11b751c42bae61bcd8220fb4
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-07-25 16:37:26 +0200

    Record for saktype tagdata

--- apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/Tags.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/Tags.tsx
@@ -4,18 +4,14 @@ import { Variants } from '~shared/Tags'
 
 export const SaktypeTag = (props: { sakType: Saktype }) => {
   const { sakType } = props
-  const getVariantAndTExt = (): [Variants, string] => {
-    switch (sakType) {
-      case 'BARNEPENSJON':
-        return [Variants.INFO, 'Barnepensjon']
-      case 'OMSTILLINGSSTOENAD':
-        return [Variants.NEUTRAL, 'Omstillingsstønad']
-    }
+  const SAKTYPE_TIL_TAGDATA: Record<Saktype, { variant: Variants; text: string }> = {
+    BARNEPENSJON: { variant: Variants.INFO, text: 'Barnepensjon' },
+    OMSTILLINGSSTOENAD: { variant: Variants.NEUTRAL, text: 'Omstillingsstønad' },
   }
-  const variant = getVariantAndTExt()
+  const tagdata = SAKTYPE_TIL_TAGDATA[sakType]
   return (
     <>
-      <Tag variant={variant[0]}>{variant[1]}</Tag>
+      <Tag variant={tagdata.variant}>{tagdata.text}</Tag>
     </>
   )
 }
@@ -23,19 +19,19 @@ export const SaktypeTag = (props: { sakType: Saktype }) => {
 export const OppgavetypeTag = (props: { oppgavetype: Oppgavetype }) => {
   const { oppgavetype } = props
 
-    const OPPGAVETYPE_TIL_TAGDATA: Record<Oppgavetype, { variant: Variants; text: string }> = {
-        FOERSTEGANGSBEHANDLING: { variant: Variants.INFO, text: 'Førstegangsbehandling' },
-        REVURDERING: { variant: Variants.NEUTRAL, text: 'Revurdering' },
-        HENDELSE: { variant: Variants.ALT1, text: 'Hendelse' },
-        MANUELT_OPPHOER: { variant: Variants.ALT2, text: 'Manuelt opphør' },
-        EKSTERN: { variant: Variants.ALT3, text: 'Ekstern' },
-        ATTESTERING: { variant: Variants.ALT3_FILLED, text: 'Attestering' },
-    } as const
+  const OPPGAVETYPE_TIL_TAGDATA: Record<Oppgavetype, { variant: Variants; text: string }> = {
+    FOERSTEGANGSBEHANDLING: { variant: Variants.INFO, text: 'Førstegangsbehandling' },
+    REVURDERING: { variant: Variants.NEUTRAL, text: 'Revurdering' },
+    HENDELSE: { variant: Variants.ALT1, text: 'Hendelse' },
+    MANUELT_OPPHOER: { variant: Variants.ALT2, text: 'Manuelt opphør' },
+    EKSTERN: { variant: Variants.ALT3, text: 'Ekstern' },
+    ATTESTERING: { variant: Variants.ALT3_FILLED, text: 'Attestering' },
+  } as const
 
   const tagdata = OPPGAVETYPE_TIL_TAGDATA[oppgavetype]
-    return (
-      <>
-          <Tag variant={tagdata.variant}>{tagdata.text}</Tag>
-      </>
-    )
+  return (
+    <>
+      <Tag variant={tagdata.variant}>{tagdata.text}</Tag>
+    </>
+  )
 }

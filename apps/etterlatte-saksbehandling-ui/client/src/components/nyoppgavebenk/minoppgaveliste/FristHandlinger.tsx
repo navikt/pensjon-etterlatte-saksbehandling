7fe598548c07ffa5c84ff7c5c3fb3782a86d0173
Emil Schrøder <emil.schroder@nav.no>
2023-08-02 20:05:20 +0200

    Bytt til å bruke datepicker og vis ny dato i tabellen

--- apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/minoppgaveliste/FristHandlinger.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/minoppgaveliste/FristHandlinger.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { isFailure, isPending, isSuccess, useApiCall } from '~shared/hooks/useApiCall'
 import { erOppgaveRedigerbar, Oppgavestatus, redigerFristApi } from '~shared/api/oppgaverny'
-import { Alert, Button, Heading, Label, Modal, MonthPicker } from '@navikt/ds-react'
+import { Alert, Button, Heading, Modal, DatePicker } from '@navikt/ds-react'
 import { ApiErrorAlert } from '~ErrorBoundary'
 import { formaterStringDato } from '~utils/formattering'
 import { PencilIcon } from '@navikt/aksel-icons'
 import styled from 'styled-components'
-import { isBefore } from 'date-fns'
+import { add, isBefore } from 'date-fns'
 
 const Buttonwrapper = styled.div`
   margin: 4rem 1rem 1rem 1rem;
@@ -20,17 +20,23 @@ const FristWrapper = styled.span<{ fristHarPassert: boolean; utenKnapp?: boolean
   padding: ${(p) => p.utenKnapp && '12px 20px'};
 `
 
-export const FristHandlinger = (props: { status: Oppgavestatus; frist: string; oppgaveId: string; sakId: number }) => {
-  const { frist, oppgaveId, sakId, status } = props
+export const FristHandlinger = (props: {
+  status: Oppgavestatus
+  orginalFrist: string
+  oppgaveId: string
+  sakId: number
+}) => {
+  const { orginalFrist, oppgaveId, sakId, status } = props
   const [open, setOpen] = useState(false)
+  const [frist, setFrist] = useState<string>()
   const [nyFrist, setnyFrist] = useState<Date>(new Date())
   const [redigerfristSvar, redigerFrist, resetredigerFristApi] = useApiCall(redigerFristApi)
 
   const erRedigerbar = erOppgaveRedigerbar(status)
 
   const generateFromDate = () => {
-    const naa = new Date()
-    naa.setMonth(naa.getMonth() - 1)
+    // Legger til 1 dag siden ny frist må være fremover i tid
+    const naa = add(new Date(), { days: 1 })
     return naa
   }
   const generateToDate = () => {
@@ -39,14 +45,22 @@ export const FristHandlinger = (props: { status: Oppgavestatus; frist: string; o
     return naa
   }
 
-  const redigerDatoMedtimerForAAslippetidssoneproblemUTC = (valgDato: Date) => {
-    if (valgDato) {
-      const datoSomSisteDagIMaaneden = new Date(valgDato.getFullYear(), valgDato.getMonth() + 1, 0)
-      datoSomSisteDagIMaaneden.setHours(17)
-      setnyFrist(datoSomSisteDagIMaaneden)
+  const redigerDatoMedtimerForAAslippetidssoneproblemUTC = (valgtDato: Date) => {
+    if (valgtDato) {
+      valgtDato.setHours(17)
+      setnyFrist(valgtDato)
       resetredigerFristApi()
     }
   }
+  useEffect(() => {
+    if (isSuccess(redigerfristSvar)) {
+      setFrist(nyFrist.toISOString())
+    }
+  }, [redigerfristSvar])
+
+  useEffect(() => {
+    if (orginalFrist) setFrist(orginalFrist)
+  }, [orginalFrist])
 
   return (
     <>
@@ -57,8 +71,8 @@ export const FristHandlinger = (props: { status: Oppgavestatus; frist: string; o
               <Heading spacing level="2" size="medium" id="modal-heading">
                 Velg ny frist
               </Heading>
-              <MonthPicker.Standalone
-                onMonthSelect={redigerDatoMedtimerForAAslippetidssoneproblemUTC}
+              <DatePicker.Standalone
+                onSelect={redigerDatoMedtimerForAAslippetidssoneproblemUTC}
                 selected={nyFrist}
                 dropdownCaption
                 fromDate={generateFromDate()}
@@ -78,7 +92,7 @@ export const FristHandlinger = (props: { status: Oppgavestatus; frist: string; o
                   Lagre ny frist
                 </Button>
                 <Button variant="secondary" onClick={() => setOpen(!open)}>
-                  Avbryt
+                  Lukk
                 </Button>
               </Buttonwrapper>
             </Modal.Content>

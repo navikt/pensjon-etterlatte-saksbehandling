8ab68a2caeb15b22d2d58d52c0cac0f80bb5d13f
henrikg <henrik.gundersen@nav.no>
2023-08-11 11:05:31 +0200

    Fikser spørring for å hente id

--- apps/etterlatte-vilkaarsvurdering/src/main/resources/db/migration/V16__gjenlevende_unntaksvilkaar_yrkesskade.sql
+++ apps/etterlatte-vilkaarsvurdering/src/main/resources/db/migration/V16__gjenlevende_unntaksvilkaar_yrkesskade.sql
@@ -2,7 +2,7 @@ DO $$
     DECLARE
         id_value uuid;
     BEGIN
-        FOR id_value IN (SELECT DISTINCT vilkaar.id FROM vilkaar, delvilkaar WHERE vilkaar.id = delvilkaar.vilkaar_id AND delvilkaar.vilkaar_type = 'OMS_GJENLEVENDES_MEDLEMSKAP_UNNTAK_YRKESSKADE')
+        FOR id_value IN (SELECT DISTINCT vilkaar.id FROM vilkaar, delvilkaar WHERE vilkaar.id = delvilkaar.vilkaar_id AND delvilkaar.vilkaar_type = 'OMS_GJENLEVENDES_MEDLEMSKAP')
             LOOP
                 INSERT INTO delvilkaar (vilkaar_id, vilkaar_type, hovedvilkaar, tittel, beskrivelse, paragraf, ledd, bokstav, lenke, resultat, spoersmaal) VALUES (id_value, 'OMS_GJENLEVENDES_MEDLEMSKAP_UNNTAK_YRKESSKADE', false, 'Ja, dødsfallet skyldes en godkjent yrkes-skade/sykdom', null, '§ 17-2', null, null, null, null, null);
             END LOOP;

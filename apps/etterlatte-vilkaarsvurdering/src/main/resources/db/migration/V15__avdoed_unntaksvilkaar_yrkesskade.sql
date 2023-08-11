45726ab1186602278473aa0df6ae320754f81548
Henrik Gundersen <henrik.gundersen@nav.no>
2023-08-11 11:23:26 +0200

    Legger til unntaksvilkår for yrkesskade ved omstillingsstønad (#1956)
    * Legger til unntaksvilkår for yrkesskade
    
    * Script for å oppdatere eksisterende vurderinger
    
    * Bytter om rekkefølge på vilkår
    
    * Fikser spørring for å hente id

---
+++ apps/etterlatte-vilkaarsvurdering/src/main/resources/db/migration/V15__avdoed_unntaksvilkaar_yrkesskade.sql
@@ -0,0 +1,9 @@
+DO $$
+    DECLARE
+        id_value uuid;
+    BEGIN
+        FOR id_value IN (SELECT DISTINCT vilkaar.id FROM vilkaar, delvilkaar WHERE vilkaar.id = delvilkaar.vilkaar_id AND delvilkaar.vilkaar_type = 'OMS_AVDOEDES_MEDLEMSKAP')
+            LOOP
+                INSERT INTO delvilkaar (vilkaar_id, vilkaar_type, hovedvilkaar, tittel, beskrivelse, paragraf, ledd, bokstav, lenke, resultat, spoersmaal) VALUES (id_value, 'OMS_AVDOEDES_MEDLEMSKAP_UNNTAK_YRKESSKADE', false, 'Ja, dødsfallet skyldes en godkjent yrkes-skade/sykdom', null, '§ 17-2', null, null, null, null, null);
+            END LOOP;
+    END $$;

88d35bf2d5dc74750f86deedec82977e5d6416b2
bjorng <bjorn.golberg@gmail.com>
2023-08-14 14:21:13 +0200

    Teller antall aktive og avslutta oppgaver til oppgaveMetrics

--- apps/etterlatte-behandling/src/main/kotlin/metrics/MetrikkerDao.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/MetrikkerDao.kt
@@ -1,7 +1,15 @@
 package no.nav.etterlatte.metrics
 
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.oppgaveNy.OppgaveKilde
+import no.nav.etterlatte.libs.common.oppgaveNy.OppgaveNy
+import no.nav.etterlatte.libs.common.oppgaveNy.OppgaveType
+import no.nav.etterlatte.libs.common.oppgaveNy.Status
+import no.nav.etterlatte.libs.common.tidspunkt.getTidspunkt
+import no.nav.etterlatte.libs.common.tidspunkt.getTidspunktOrNull
 import no.nav.etterlatte.libs.database.toList
 import org.slf4j.LoggerFactory
+import java.sql.ResultSet
 import java.util.*
 import javax.sql.DataSource
 
@@ -9,19 +17,35 @@ class MetrikkerDao(private val dataSource: DataSource) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    fun antallOppgaver(): Double {
+    fun hentAlleOppgaver(): List<OppgaveNy> {
         dataSource.connection.use {
             val statement = it.prepareStatement(
                 """
-                    SELECT id 
+                    SELECT *
                     FROM oppgave
                 """.trimIndent()
             )
-            val saker = statement.executeQuery().toList {
-                getObject("id") as UUID
+            return statement.executeQuery().toList {
+               asOppgaveNy()
             }
-            logger.info("Antall saker: ${saker.size}")
-            return saker.size.toDouble()
         }
+    }
+
+    private fun ResultSet.asOppgaveNy(): OppgaveNy {
+        return OppgaveNy(
+            id = getObject("id") as UUID,
+            status = Status.valueOf(getString("status")),
+            enhet = getString("enhet"),
+            sakId = getLong("sak_id"),
+            kilde = getString("kilde")?.let { OppgaveKilde.valueOf(it) },
+            type = OppgaveType.valueOf(getString("type")),
+            saksbehandler = getString("saksbehandler"),
+            referanse = getString("referanse"),
+            merknad = getString("merknad"),
+            opprettet = getTidspunkt("opprettet"),
+            sakType = SakType.valueOf(getString("saktype")),
+            fnr = getString("fnr"),
+            frist = getTidspunktOrNull("frist")
+        )
     }
 }
--- apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
@@ -1,6 +1,5 @@
 package no.nav.etterlatte.metrics
 
-import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.MeterBinder
@@ -10,18 +9,38 @@ class OppgaveMetrics(private val metrikkerDao: MetrikkerDao) : MeterBinder {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
     private var antallOppgaver = -1.0
+    private var antallAktiveOppgaver = -1.0
+    private var antallAvsluttaOppgaver = -1.0
+
     override fun bindTo(registry: MeterRegistry) {
+        val alleOppgaver = metrikkerDao.hentAlleOppgaver()
+
+        val aktive = alleOppgaver.filter { !it.erAvsluttet() }
+        val avsluttet = alleOppgaver.filter { it.erAvsluttet() }
+
         Gauge.builder("antall_oppgaver") {
             antallOppgaver.also {
-                antallOppgaver = metrikkerDao.antallOppgaver()
+                antallOppgaver = alleOppgaver.size.toDouble()
                 logger.info("Antall oppgaver: $antallOppgaver")
             }
-        }.description("Hello antall oppgaver")
+        }.description("Antall oppgaver")
             .register(registry)
-        Counter.builder("hvor_fine_er_oppgavene")
-            .apply {
-                description("hello counter test")
-                register(registry)
+
+        Gauge.builder("antall_aktive_oppgaver") {
+            antallAktiveOppgaver.also {
+                antallAktiveOppgaver = aktive.size.toDouble()
+                logger.info("Antall aktive oppgaver: $antallAktiveOppgaver")
             }
+        }.description("Antall aktive oppgaver")
+            .register(registry)
+
+        Gauge.builder("antall_avslutta_oppgaver") {
+            antallAvsluttaOppgaver.also {
+                antallAvsluttaOppgaver = avsluttet.size.toDouble()
+                logger.info("Antall avslutta oppgaver: $antallAvsluttaOppgaver")
+            }
+        }.description("Antall avslutta oppgaver")
+            .register(registry)
     }
+
 }

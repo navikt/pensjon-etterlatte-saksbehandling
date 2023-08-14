e6d17c806f92f08a93c4757d385f4c08a91abef1
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-08-14 10:09:06 +0200

    Prøver å ikke bruke databasecontext

--- apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
@@ -4,20 +4,20 @@ import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.MeterBinder
-import no.nav.etterlatte.inTransaction
 import org.slf4j.LoggerFactory
 
 class OppgaveMetrics(private val metrikkerDao: MetrikkerDao) : MeterBinder {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
+    private var antallOppgaver = metrikkerDao.antallOppgaver()
+
     override fun bindTo(registry: MeterRegistry) {
-        val antallOppgaver = inTransaction {
-            metrikkerDao.antallOppgaver()
-        }
         logger.info("Antall oppgaver: $antallOppgaver")
-        
+
         Gauge.builder("antall_oppgaver") {
-            antallOppgaver
+            antallOppgaver.also {
+                antallOppgaver = metrikkerDao.antallOppgaver()
+            }
         }.description("Hello antall oppgaver")
             .register(registry)
         Counter.builder("hvor_fine_er_oppgavene")

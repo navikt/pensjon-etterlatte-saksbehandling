960d233c881b9616b4b1f1f5d3507312024b34a7
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-08-14 09:40:41 +0200

    inTransaction

--- apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/OppgaveMetrics.kt
@@ -4,12 +4,14 @@ import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.MeterBinder
+import no.nav.etterlatte.inTransaction
 
 class OppgaveMetrics(private val metrikkerDao: MetrikkerDao) : MeterBinder {
     override fun bindTo(registry: MeterRegistry) {
-        val runtime = Runtime.getRuntime()
-        Gauge.builder("antall_oppgaver", runtime) {
-            metrikkerDao.antallOppgaver()
+        Gauge.builder("antall_oppgaver") {
+            inTransaction {
+                metrikkerDao.antallOppgaver()
+            }
         }.description("Hello antall oppgaver")
             .register(registry)
         Counter.builder("hvor_fine_er_oppgavene")

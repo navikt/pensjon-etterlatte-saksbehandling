3ef827f06a91d4255ba7e5933e82618407feae03
bjorng <bjorn.golberg@gmail.com>
2023-08-14 09:14:26 +0200

    Teller oppgaver i database og legger til i metrikker

--- apps/etterlatte-behandling/src/main/kotlin/metrics/MetrikkerDao.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/MetrikkerDao.kt
@@ -1,5 +1,26 @@
 package no.nav.etterlatte.metrics
 
+import no.nav.etterlatte.libs.database.toList
+import org.slf4j.LoggerFactory
 import java.sql.Connection
+import java.util.UUID
 
-class MetrikkerDao(private val connection: () -> Connection)
+class MetrikkerDao(private val connection: () -> Connection) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun antallOppgaver(): Double {
+        with(connection()) {
+            val statement = prepareStatement(
+                """
+                    SELECT id 
+                    FROM oppgave
+                """.trimIndent()
+            )
+            val saker = statement.executeQuery().toList{
+                getObject("id") as UUID
+            }
+            return saker.size.toDouble()
+        }
+    }
+}

0c588fde15adaee75734849d8b72db2fbb79a278
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-08-17 14:39:52 +0200

    Hent saksbehandler for behandling ved å ta siste oppgave på behandlingen som ikke er attestert

--- apps/etterlatte-behandling/src/main/kotlin/oppgaveny/OppgaveServiceNy.kt
+++ apps/etterlatte-behandling/src/main/kotlin/oppgaveny/OppgaveServiceNy.kt
@@ -304,7 +304,8 @@ class OppgaveServiceNy(
             oppgaveDaoNy.hentOppgaverForBehandling(behandlingsId.toString())
         }
 
-        return oppgaverforBehandling.single { it.type === OppgaveType.FOERSTEGANGSBEHANDLING }.saksbehandler
+        val oppgaverForBehandlingUtenAttesterting = oppgaverforBehandling.filter { it.type !== OppgaveType.ATTESTERING }
+        return oppgaverForBehandlingUtenAttesterting.sortedBy { it.opprettet }[0].saksbehandler
     }
 
     private fun lagreOppgave(oppgaveNy: OppgaveNy): OppgaveNy {

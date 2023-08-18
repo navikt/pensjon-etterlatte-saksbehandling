1acbf3f856566e263335f783b489624add8da2f2
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-08-18 12:14:27 +0200

    EY-2529 Dto for samordningsinformasjonen

--- apps/etterlatte-samordning-vedtak/src/main/kotlin/no/nav/etterlatte/samordning/vedtak/VedtakRoute.kt
+++ apps/etterlatte-samordning-vedtak/src/main/kotlin/no/nav/etterlatte/samordning/vedtak/VedtakRoute.kt
@@ -7,6 +7,18 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import java.time.LocalDate
+
+data class SamordningVedtakDto(
+    val vedtakId: Long,
+    val sakstype: String,
+    val virkningsdato: LocalDate,
+    val resultatkode: String,
+    val stoppaarsak: String?,
+    val anvendtTrygdetid: Int,
+    val omstillingsstoenadBrutto: Int,
+    val omstillingsstoenadNetto: Int
+)
 
 fun Route.vedtakRoute() {
     route("vedtak") {
@@ -15,7 +27,18 @@ fun Route.vedtakRoute() {
         get("{vedtakId}") {
             val vedtakId = requireNotNull(call.parameters["vedtakId"]).toLong()
 
-            call.respond("OK - $vedtakId")
+            val vedtaksinfo = SamordningVedtakDto(
+                vedtakId = vedtakId,
+                sakstype = "OMSTILLINGSSTOENAD",
+                virkningsdato = LocalDate.now(),
+                resultatkode = "RESULTATKODE",
+                stoppaarsak = null,
+                anvendtTrygdetid = 40,
+                omstillingsstoenadBrutto = 12000,
+                omstillingsstoenadNetto = 9500
+            )
+
+            call.respond(vedtaksinfo)
         }
     }
 }

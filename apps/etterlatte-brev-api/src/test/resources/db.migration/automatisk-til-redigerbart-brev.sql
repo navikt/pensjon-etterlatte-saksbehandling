d2fc5f59200986eacade0b1645fdaa66cef68663
Mads Opheim <mads.opheim@nav.no>
2023-08-25 13:30:20 +0200

    EY-2583: Ekstra sjekk på at det er innvilgels

--- apps/etterlatte-brev-api/src/test/resources/db.migration/automatisk-til-redigerbart-brev.sql
+++ apps/etterlatte-brev-api/src/test/resources/db.migration/automatisk-til-redigerbart-brev.sql
@@ -3,10 +3,12 @@ with ids as (
     select distinct b.id
     from brev b
              inner join hendelse h on b.id = h.brev_id
+    inner join innhold i on b.id = i.brev_id
     where b.id not in (select distinct brev_id
                        from hendelse
                        where status_id in ('FERDIGSTILT', 'JOURNALFOERT', 'DISTRIBUERT'))
       and b.prosess_type = 'AUTOMATISK'
+    and i.tittel = 'Vedtak om innvilgelse'
 )
 update innhold set payload='[]' where brev_id in (select id from ids);
 
@@ -14,10 +16,12 @@ with ids as (
     select distinct b.id
     from brev b
              inner join hendelse h on b.id = h.brev_id
+             inner join innhold i on b.id = i.brev_id
     where b.id not in (select distinct brev_id
                        from hendelse
                        where status_id in ('FERDIGSTILT', 'JOURNALFOERT', 'DISTRIBUERT'))
       and b.prosess_type = 'AUTOMATISK'
+      and i.tittel = 'Vedtak om innvilgelse'
 )
 
 

d50dc7f68cf9321764a54cabb4edf780bf87cf30
Mads Opheim <mads.opheim@nav.no>
2023-08-25 13:28:23 +0200

    EY-2583: SQL for å gjera uferdige automatiske brev til redigerbare. Skal køyrast når vi skru på ny innvilgelsesmal for BP og med det går frå automatisk til redigerbart brev der

---
+++ apps/etterlatte-brev-api/src/test/resources/db.migration/automatisk-til-redigerbart-brev.sql
@@ -0,0 +1,27 @@
+begin transaction;
+with ids as (
+    select distinct b.id
+    from brev b
+             inner join hendelse h on b.id = h.brev_id
+    where b.id not in (select distinct brev_id
+                       from hendelse
+                       where status_id in ('FERDIGSTILT', 'JOURNALFOERT', 'DISTRIBUERT'))
+      and b.prosess_type = 'AUTOMATISK'
+)
+update innhold set payload='[]' where brev_id in (select id from ids);
+
+with ids as (
+    select distinct b.id
+    from brev b
+             inner join hendelse h on b.id = h.brev_id
+    where b.id not in (select distinct brev_id
+                       from hendelse
+                       where status_id in ('FERDIGSTILT', 'JOURNALFOERT', 'DISTRIBUERT'))
+      and b.prosess_type = 'AUTOMATISK'
+)
+
+
+update brev set prosess_type = 'REDIGERBAR' where id in (select id from ids);
+
+
+commit transaction;

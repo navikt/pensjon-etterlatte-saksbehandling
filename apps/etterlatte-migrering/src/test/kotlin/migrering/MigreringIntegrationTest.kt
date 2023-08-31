c663766ba87d2becb988a23f572d6a731b2ad1ee
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-08-31 14:29:47 +0200

    EY-2595: Samkøyre logikken for å migrere ei sak og den for å migrere alle (#2044)
    * EY-2595: Justerer flyten så det å migrere alle saker bruker logikken for å migrere ei sak. Bruker samme oppsett som for regulering med at kvar sak vi migrerer får si eiga melding som går ut vidare
    
    * EY-2595: Utsetje å faktisk/tilsynelatande kople opp mot PEN til dei har ting på plass

--- apps/etterlatte-migrering/src/test/kotlin/migrering/MigreringIntegrationTest.kt
+++ apps/etterlatte-migrering/src/test/kotlin/migrering/MigreringIntegrationTest.kt
@@ -9,18 +9,14 @@ import no.nav.etterlatte.funksjonsbrytere.DummyFeatureToggleService
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
 import no.nav.etterlatte.libs.common.rapidsandrivers.EVENT_NAME_KEY
-import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.database.DataSourceBuilder
 import no.nav.etterlatte.libs.database.POSTGRES_VERSION
 import no.nav.etterlatte.libs.database.migrate
 import no.nav.etterlatte.migrering.pen.BarnepensjonGrunnlagResponse
 import no.nav.etterlatte.migrering.pen.PenKlient
-import no.nav.etterlatte.rapidsandrivers.migrering.Beregning
-import no.nav.etterlatte.rapidsandrivers.migrering.Enhet
 import no.nav.etterlatte.rapidsandrivers.migrering.MigreringRequest
 import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
 import no.nav.etterlatte.rapidsandrivers.migrering.PesysId
-import no.nav.etterlatte.rapidsandrivers.migrering.Trygdetid
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.AfterEach
@@ -30,10 +26,8 @@ import org.junit.jupiter.api.Test
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import rapidsandrivers.SAK_ID_KEY
-import java.math.BigDecimal
 import java.time.Month
 import java.time.YearMonth
-import java.util.*
 import javax.sql.DataSource
 
 class MigreringIntegrationTest {
@@ -59,61 +53,11 @@ class MigreringIntegrationTest {
     fun stop() = postgreSQLContainer.stop()
 
     @Test
-    fun `skal sende migreringsmelding for hver enkelt sak`() {
-        testApplication {
-            val repository = PesysRepository(datasource)
-            val syntetiskFnr = "19078504903"
-            val sakInn = Pesyssak(
-                UUID.randomUUID(),
-                PesysId(4),
-                Enhet("4808"),
-                Folkeregisteridentifikator.of(syntetiskFnr),
-                Folkeregisteridentifikator.of(syntetiskFnr),
-                emptyList(),
-                YearMonth.now(),
-                YearMonth.now().minusYears(10),
-                Beregning(
-                    brutto = BigDecimal(1000),
-                    netto = BigDecimal(1000),
-                    anvendtTrygdetid = BigDecimal(40),
-                    datoVirkFom = Tidspunkt.now(),
-                    g = BigDecimal(100000)
-                ),
-                Trygdetid(emptyList()),
-                false
-            )
-            repository.lagrePesyssak(sakInn)
-            val featureToggleService = DummyFeatureToggleService().also {
-                it.settBryter(MigreringFeatureToggle.SendSakTilMigrering, true)
-            }
-            val inspector = TestRapid()
-                .apply {
-                    Migrering(
-                        this,
-                        repository,
-                        sakmigrerer = Sakmigrerer(repository, featureToggleService)
-                    )
-                }
-
-            val melding = JsonMessage.newMessage(
-                mapOf(EVENT_NAME_KEY to Migreringshendelser.START_MIGRERING)
-            )
-            inspector.sendTestMessage(melding.toJson())
-
-            val melding1 = inspector.inspektør.message(0)
-
-            val request = objectMapper.readValue(melding1.get("request").asText(), MigreringRequest::class.java)
-            assertEquals(PesysId(4), request.pesysId)
-            assertEquals(sakInn.soeker, request.soeker)
-        }
-    }
-
-    @Test
     fun `kan ta imot og handtere respons fra PEN`() {
         testApplication {
             val repository = PesysRepository(datasource)
             val featureToggleService = DummyFeatureToggleService().also {
-                it.settBryter(MigreringFeatureToggle.SendSakTilMigrering, false)
+                it.settBryter(MigreringFeatureToggle.SendSakTilMigrering, true)
             }
             val responsFraPEN = objectMapper.readValue<BarnepensjonGrunnlagResponse>(
                 this::class.java.getResource("/penrespons.json")!!.readText()
@@ -142,6 +86,12 @@ class MigreringIntegrationTest {
                 assertEquals(get(0).pesysId.id, 22974139)
                 assertEquals(get(0).virkningstidspunkt, YearMonth.of(2023, Month.SEPTEMBER))
             }
+
+            val melding1 = inspector.inspektør.message(0)
+
+            val request = objectMapper.readValue(melding1.get("request").asText(), MigreringRequest::class.java)
+            assertEquals(PesysId(22974139), request.pesysId)
+            assertEquals(Folkeregisteridentifikator.of("06421594773"), request.soeker)
         }
     }
 }

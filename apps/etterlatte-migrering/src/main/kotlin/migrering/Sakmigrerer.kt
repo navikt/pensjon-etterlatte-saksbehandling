430778e76b1b208d1ff4d9417660636eb9c73057
Mads Opheim <mads.opheim@nav.no>
2023-09-01 10:54:00 +0200

    EY-2595: Seinare i flyten forventar vi å få feltet kalt hendelsedata, ikkje request

--- apps/etterlatte-migrering/src/main/kotlin/migrering/Sakmigrerer.kt
+++ apps/etterlatte-migrering/src/main/kotlin/migrering/Sakmigrerer.kt
@@ -5,13 +5,12 @@ import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstype
 import no.nav.etterlatte.libs.common.rapidsandrivers.BEHOV_NAME_KEY
 import no.nav.etterlatte.libs.common.rapidsandrivers.eventName
-import no.nav.etterlatte.libs.common.toJson
 import no.nav.etterlatte.rapidsandrivers.migrering.FNR_KEY
 import no.nav.etterlatte.rapidsandrivers.migrering.MigreringRequest
 import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
 import no.nav.etterlatte.rapidsandrivers.migrering.PesysId
+import no.nav.etterlatte.rapidsandrivers.migrering.hendelseData
 import no.nav.etterlatte.rapidsandrivers.migrering.pesysId
-import no.nav.etterlatte.rapidsandrivers.migrering.request
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import org.slf4j.LoggerFactory
@@ -35,7 +34,7 @@ internal class Sakmigrerer(
     ) {
         packet.eventName = Migreringshendelser.MIGRER_SAK
         val request = tilMigreringsrequest(sak)
-        packet.request = request.toJson()
+        packet.hendelseData = request
         if (featureToggleService.isEnabled(MigreringFeatureToggle.SendSakTilMigrering, false)) {
             sendSakTilMigrering(packet, request, context, sak)
         } else {
--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/migrering/PesysKeys.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/migrering/PesysKeys.kt
@@ -2,11 +2,9 @@ package no.nav.etterlatte.rapidsandrivers.migrering
 
 import no.nav.etterlatte.libs.common.behandling.Persongalleri
 import no.nav.etterlatte.libs.common.objectMapper
-import no.nav.etterlatte.libs.common.toJson
 import no.nav.helse.rapids_rivers.JsonMessage
 import rapidsandrivers.HENDELSE_DATA_KEY
 
-const val REQUEST = "request"
 const val FNR_KEY = "fnr"
 const val ROLLE_KEY = "rolle"
 const val VILKAARSVURDERT_KEY = "vilkaarsvurdert"
@@ -14,12 +12,6 @@ const val TRYGDETID_KEY = "trygdetid"
 const val MIGRERING_GRUNNLAG_KEY = "migrering_grunnlag"
 const val PERSONGALLERI = "persongalleri"
 const val PESYS_ID = "pesysId"
-
-var JsonMessage.request: String
-    get() = this[REQUEST].toJson()
-    set(name) {
-        this[REQUEST] = name
-    }
 
 var JsonMessage.hendelseData: MigreringRequest
     get() = objectMapper.treeToValue(this[HENDELSE_DATA_KEY], MigreringRequest::class.java)

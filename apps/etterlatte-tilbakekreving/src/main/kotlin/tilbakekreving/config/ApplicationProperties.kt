8897e9815b950fdd96cca4234149b5ac79592ee1
henrikg <henrik.gundersen@nav.no>
2023-09-11 12:05:53 +0200

    EY-2626 Oppdaterer properties og lokal kjøring

--- apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/config/ApplicationProperties.kt
+++ apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/config/ApplicationProperties.kt
@@ -12,11 +12,11 @@ data class ApplicationProperties(
     companion object {
         fun fromEnv(env: Map<String, String>) = env.run {
             ApplicationProperties(
-                mqHost = value("OPPDRAG_MQ_HOSTNAME"),
-                mqPort = value("OPPDRAG_MQ_PORT").toInt(),
-                mqQueueManager = value("OPPDRAG_MQ_MANAGER"),
-                mqChannel = value("OPPDRAG_MQ_CHANNEL"),
-                mqKravgrunnlagQueue = value("OPPDRAG_KRAVGRUNNLAG_MQ_NAME"),
+                mqHost = value("MQ_HOSTNAME"),
+                mqPort = value("MQ_PORT").toInt(),
+                mqQueueManager = value("MQ_MANAGER"),
+                mqChannel = value("MQ_CHANNEL"),
+                mqKravgrunnlagQueue = value("KRAVGRUNNLAG_MQ_NAME"),
                 serviceUserUsername = value("srvuser"),
                 serviceUserPassword = value("srvpwd")
             )

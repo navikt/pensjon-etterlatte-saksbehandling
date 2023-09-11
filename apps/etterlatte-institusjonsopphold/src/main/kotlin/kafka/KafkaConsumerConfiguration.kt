8a29436b818ce6b09525f0e263c6d7efe8f8d828
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-09-11 15:41:50 +0200

    Riktig variabelnavn for MAX_POLL_INTERVAL_MS_CONFIG timeout (#2138)

--- apps/etterlatte-institusjonsopphold/src/main/kotlin/kafka/KafkaConsumerConfiguration.kt
+++ apps/etterlatte-institusjonsopphold/src/main/kotlin/kafka/KafkaConsumerConfiguration.kt
@@ -19,55 +19,59 @@ interface KafkaConsumerConfiguration {
 
 class KafkaEnvironment : KafkaConsumerConfiguration {
     override fun generateKafkaConsumerProperties(env: Map<String, String>): Properties {
-        val fiveMinutesInMs = Duration.ofMinutes(8L).toMillis().toInt()
-        val properties = Properties().apply {
-            put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env["KAFKA_BROKERS"])
-            put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
-            put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
-            put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
-            put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
-            put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env["KAFKA_TRUSTSTORE_PATH"])
-            put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
-            put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env["KAFKA_KEYSTORE_PATH"])
-            put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
+        val eightMinutesInMs = Duration.ofMinutes(8L).toMillis().toInt()
+        val properties =
+            Properties().apply {
+                put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env["KAFKA_BROKERS"])
+                put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+                put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+                put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
+                put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+                put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env["KAFKA_TRUSTSTORE_PATH"])
+                put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
+                put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env["KAFKA_KEYSTORE_PATH"])
+                put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
 
-            put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
-            // Nais doc: Password needed to use the keystore and truststore
+                put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, env["KAFKA_CREDSTORE_PASSWORD"])
+                // Nais doc: Password needed to use the keystore and truststore
 
-            put(ConsumerConfig.GROUP_ID_CONFIG, env["INSTITUSJONSOPPHOLD_GROUP_ID"])
-            put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100)
-            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
-            put(ConsumerConfig.CLIENT_ID_CONFIG, env["NAIS_APP_NAME"])
-            put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-            put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, fiveMinutesInMs)
-            put(CommonClientConfigs.SESSION_TIMEOUT_MS_CONFIG, Duration.ofSeconds(20L).toMillis().toInt())
-            put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-            put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer::class.java)
+                put(ConsumerConfig.GROUP_ID_CONFIG, env["INSTITUSJONSOPPHOLD_GROUP_ID"])
+                put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100)
+                put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false)
+                put(ConsumerConfig.CLIENT_ID_CONFIG, env["NAIS_APP_NAME"])
+                put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+                put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, eightMinutesInMs)
+                put(CommonClientConfigs.SESSION_TIMEOUT_MS_CONFIG, Duration.ofSeconds(20L).toMillis().toInt())
+                put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
+                put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer::class.java)
 
-            put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
+                put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
 
-            put(
-                SchemaRegistryClientConfig.USER_INFO_CONFIG,
-                "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
-            )
-            put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, env["KAFKA_SCHEMA_REGISTRY"])
-            put(
-                "schema.registry.basic.auth.user.info",
-                "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
-            )
+                put(
+                    SchemaRegistryClientConfig.USER_INFO_CONFIG,
+                    "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
+                )
+                put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, env["KAFKA_SCHEMA_REGISTRY"])
+                put(
+                    "schema.registry.basic.auth.user.info",
+                    "${env["KAFKA_SCHEMA_REGISTRY_USER"]}:${env["KAFKA_SCHEMA_REGISTRY_PASSWORD"]}"
+                )
 
-            put(
-                ConsumerConfig.ISOLATION_LEVEL_CONFIG,
-                IsolationLevel.READ_COMMITTED.toString().lowercase(Locale.getDefault())
-            )
-        }
+                put(
+                    ConsumerConfig.ISOLATION_LEVEL_CONFIG,
+                    IsolationLevel.READ_COMMITTED.toString().lowercase(Locale.getDefault())
+                )
+            }
         return properties
     }
 
     class JsonDeserializer : org.apache.kafka.common.serialization.Deserializer<KafkaOppholdHendelse> {
         private val mapper = jacksonObjectMapper()
 
-        override fun deserialize(topic: String?, data: ByteArray): KafkaOppholdHendelse {
+        override fun deserialize(
+            topic: String?,
+            data: ByteArray
+        ): KafkaOppholdHendelse {
             return mapper.readValue(data)
         }
     }

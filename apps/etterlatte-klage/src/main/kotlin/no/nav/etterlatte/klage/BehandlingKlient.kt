e1e535faf3cdaa63e330db462d6e9cecb30b2f31
Mads Opheim <mads.opheim@nav.no>
2023-09-14 10:11:44 +0200

    EY-2594: Sia det feilar på å lesa string som objekt, prøver å heller ha sjølve eventen som string og heller lesa det over til objekt etterpå

--- apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/BehandlingKlient.kt
+++ apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/BehandlingKlient.kt
@@ -1,5 +1,6 @@
 package no.nav.etterlatte.klage
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.typesafe.config.Config
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
@@ -9,6 +10,7 @@ import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 import no.nav.etterlatte.klage.modell.BehandlingEvent
 import no.nav.etterlatte.libs.common.Vedtaksloesning
+import no.nav.etterlatte.libs.common.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 
@@ -18,14 +20,16 @@ class BehandlingKlient(config: Config, val behandlingHttpClient: HttpClient) {
     private val clientId = config.getString("behandling.client.id")
     private val resourceUrl = config.getString("behandling.resource.url")
 
-    fun haandterHendelse(record: ConsumerRecord<String, BehandlingEvent>) {
+    fun haandterHendelse(record: ConsumerRecord<String, String>) {
         logger.debug(
             "Behandler klage-record med id: {}, partition {}, offset: {}",
             record.key(),
             record.partition(),
             record.offset()
         )
-        val klageHendelse = record.value()
+        val klageHendelseString = record.value()
+        logger.info("Mottok $klageHendelseString")
+        val klageHendelse = objectMapper.readValue<BehandlingEvent>(klageHendelseString)
         logger.info(
             "Håndterer klagehendelse ${klageHendelse.eventId}"
         )

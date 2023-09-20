0ef339afcfcf6515cb84958be851022e55c2b675
bjorng <bjorn.golberg@gmail.com>
2023-09-20 17:49:16 +0200

    EY-2661 Endepunkt for å teste tilbakekreving uten å sende meldinger på kø

--- apps/etterlatte-tilbakekreving/src/main/kotlin/Application.kt
+++ apps/etterlatte-tilbakekreving/src/main/kotlin/Application.kt
@@ -10,9 +10,15 @@ import io.ktor.server.routing.routing
 import no.nav.etterlatte.libs.common.logging.sikkerLoggOppstartOgAvslutning
 import no.nav.etterlatte.libs.ktor.healthApi
 import no.nav.etterlatte.libs.ktor.metricsModule
+import no.nav.etterlatte.libs.ktor.restModule
 import no.nav.etterlatte.libs.ktor.setReady
 import no.nav.etterlatte.tilbakekreving.config.ApplicationContext
 import no.nav.etterlatte.tilbakekreving.config.ApplicationProperties
+import no.nav.etterlatte.tilbakekreving.kravgrunnlag.testKravgrunnlagRoutes
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val sikkerLogg: Logger = LoggerFactory.getLogger("sikkerLogg")
 
 fun main() {
     val applicationProperties = ApplicationProperties.fromEnv(System.getenv())
@@ -25,25 +31,17 @@ class Server(private val context: ApplicationContext) {
         sikkerLoggOppstartOgAvslutning("etterlatte-tilbakekreving")
     }
 
-    private val engine =
-        embeddedServer(
-            factory = CIO,
-            environment =
-                applicationEngineEnvironment {
-                    config = HoconApplicationConfig(ConfigFactory.load())
-                    module {
-                        routing { healthApi() }
-                        metricsModule()
-                    }
-                    connector { port = 8080 }
-                },
-        )
-
-    fun run() =
-        with(context) {
-            // kravgrunnlagConsumer.start() TODO - må få økonomi til å gjøre oppsett for kø
-
-            setReady()
-            engine.start(true)
+    private val engine = embeddedServer(
+        factory = CIO,
+        environment = applicationEngineEnvironment {
+            config = HoconApplicationConfig(ConfigFactory.load())
+            module {
+                routing { healthApi() }
+                metricsModule()
+                restModule(sikkerLogg, withMetrics = false) {
+                    testKravgrunnlagRoutes(service = context.service)
+                }
+            }
+            connector { port = 8080 }
         }
 }

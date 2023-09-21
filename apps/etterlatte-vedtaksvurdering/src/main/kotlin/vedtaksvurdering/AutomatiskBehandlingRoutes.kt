ee2b0f9acb6cf0cad49d279e87b26caf5cace5e8
Mads Opheim <mads.opheim@nav.no>
2023-09-21 11:13:07 +0200

    EY-2694: Fangar feil viss vi ikkje kan opprette eller oppdatere vedtak, og prøver på nytt viss det ikkje fungerer å hente oppgåver

--- apps/etterlatte-vedtaksvurdering/src/main/kotlin/vedtaksvurdering/AutomatiskBehandlingRoutes.kt
+++ apps/etterlatte-vedtaksvurdering/src/main/kotlin/vedtaksvurdering/AutomatiskBehandlingRoutes.kt
@@ -1,5 +1,6 @@
 package no.nav.etterlatte.vedtaksvurdering
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.application.log
 import io.ktor.server.response.respond
@@ -10,6 +11,7 @@ import io.ktor.server.routing.route
 import no.nav.etterlatte.libs.common.BEHANDLINGSID_CALL_PARAMETER
 import no.nav.etterlatte.libs.common.SAKID_CALL_PARAMETER
 import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import no.nav.etterlatte.libs.common.retry
 import no.nav.etterlatte.libs.common.sakId
 import no.nav.etterlatte.libs.common.withBehandlingId
 import no.nav.etterlatte.libs.ktor.brukerTokenInfo
@@ -26,19 +28,28 @@ fun Route.automatiskBehandlingRoutes(
         post("/{$SAKID_CALL_PARAMETER}/{$BEHANDLINGSID_CALL_PARAMETER}/automatisk") {
             withBehandlingId(behandlingKlient) { behandlingId ->
                 logger.info("Håndterer behandling $behandlingId")
-                val nyttVedtak = service.opprettEllerOppdaterVedtak(behandlingId, brukerTokenInfo)
+                val nyttVedtak =
+                    try {
+                        service.opprettEllerOppdaterVedtak(behandlingId, brukerTokenInfo)
+                    } catch (e: Exception) {
+                        logger.warn("Kunne ikke opprette eller oppdatere vedtak", e)
+                        call.respond(HttpStatusCode.InternalServerError)
+                        return@post
+                    }
 
                 logger.info("Fatter vedtak for behandling $behandlingId")
                 service.fattVedtak(behandlingId, brukerTokenInfo)
 
                 logger.info("Tildeler attesteringsoppgave til systembruker")
-                val oppgaveTilAttestering =
-                    behandlingKlient.hentOppgaverForSak(sakId, brukerTokenInfo)
-                        .oppgaver
-                        .filter { it.referanse == behandlingId.toString() }
-                        .filter { it.type == OppgaveType.ATTESTERING }
-                        .filterNot { it.erAvsluttet() }
-                behandlingKlient.tildelSaksbehandler(oppgaveTilAttestering.first(), brukerTokenInfo)
+                retry {
+                    val oppgaveTilAttestering =
+                        behandlingKlient.hentOppgaverForSak(sakId, brukerTokenInfo)
+                            .oppgaver
+                            .filter { it.referanse == behandlingId.toString() }
+                            .filter { it.type == OppgaveType.ATTESTERING }
+                            .filterNot { it.erAvsluttet() }
+                    behandlingKlient.tildelSaksbehandler(oppgaveTilAttestering.first(), brukerTokenInfo)
+                }
 
                 logger.info("Attesterer vedtak for behandling $behandlingId")
                 service.attesterVedtak(

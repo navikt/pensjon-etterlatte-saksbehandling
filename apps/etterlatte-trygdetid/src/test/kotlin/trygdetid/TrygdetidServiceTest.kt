e2332fd3818868c05a0eff9c9c455db98d250495
Emil Schrøder <emil.schroder@nav.no>
2023-09-22 12:47:14 +0200

    Oppdater tester

--- apps/etterlatte-trygdetid/src/test/kotlin/trygdetid/TrygdetidServiceTest.kt
+++ apps/etterlatte-trygdetid/src/test/kotlin/trygdetid/TrygdetidServiceTest.kt
@@ -655,7 +655,6 @@ internal class TrygdetidServiceTest {
             mockk<DetaljertBehandling>().apply {
                 every { id } returns behandlingId
                 every { sak } returns sakId
-                every { revurderingsaarsak } returns RevurderingAarsak.ANNEN
             }
 
         val vilkaarsvurderingDto = mockk<VilkaarsvurderingDto>()
@@ -663,7 +662,6 @@ internal class TrygdetidServiceTest {
         every { vilkaarsvurderingDto.isYrkesskade() } returns false
         coEvery { vilkaarsvurderingKlient.hentVilkaarsvurdering(any(), any()) } returns vilkaarsvurderingDto
         coEvery { behandlingKlient.hentBehandling(behandlingId, saksbehandler) } returns regulering
-        coEvery { behandlingKlient.settBehandlingStatusTrygdetidOppdatert(any(), any()) } returns true
         every { repository.hentTrygdetid(forrigeBehandlingId) } returns forrigeTrygdetid
         every { repository.opprettTrygdetid(any()) } answers { firstArg() }
 
@@ -675,13 +673,11 @@ internal class TrygdetidServiceTest {
             repository.hentTrygdetid(forrigeBehandlingId)
             behandlingKlient.hentBehandling(behandlingId, saksbehandler)
             repository.opprettTrygdetid(match { it.behandlingId == behandlingId })
-            behandlingKlient.settBehandlingStatusTrygdetidOppdatert(behandlingId, saksbehandler)
             vilkaarsvurderingKlient.hentVilkaarsvurdering(any(), any())
         }
         verify {
             regulering.id
             regulering.sak
-            regulering.revurderingsaarsak
             vilkaarsvurderingDto.isYrkesskade()
         }
     }

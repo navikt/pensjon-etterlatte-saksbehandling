1ac5655ef1e9a49cc38914e5692395c2f1838510
Mads Opheim <mads.opheim@nav.no>
2023-09-29 08:55:34 +0200

    EY-2646: Gløymte å oppdatere test

--- apps/etterlatte-behandling/src/test/kotlin/behandling/etterbetaling/EtterbetalingTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/etterbetaling/EtterbetalingTest.kt
@@ -6,6 +6,7 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
@@ -86,7 +87,7 @@ class EtterbetalingTest : BehandlingIntegrationTest() {
 
             val fraDato = LocalDate.now().minusMonths(3)
             val tilDato = LocalDate.now()
-            client.post("/api/behandling/etterbetaling/$behandlingId") {
+            client.put("/api/behandling/etterbetaling/$behandlingId") {
                 addAuthToken(tokenSaksbehandler)
                 header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
                 setBody(

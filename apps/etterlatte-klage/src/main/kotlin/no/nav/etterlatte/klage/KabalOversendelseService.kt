886ebd6b20f4a26f26dd3a838f1b97fb4e761e50
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-10-05 17:57:44 +0200

    Ekstra logging for kabal-oversendelser

--- apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseService.kt
+++ apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseService.kt
@@ -3,6 +3,8 @@ package no.nav.etterlatte.klage
 import no.nav.etterlatte.klage.modell.KabalOversendelse
 import no.nav.etterlatte.libs.common.behandling.EkstradataInnstilling
 import no.nav.etterlatte.libs.common.behandling.Klage
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 interface KabalOversendelseService {
     suspend fun sendTilKabal(
@@ -12,11 +14,15 @@ interface KabalOversendelseService {
 }
 
 class KabalOversendelseServiceImpl(private val kabalKlient: KabalKlient) : KabalOversendelseService {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
     override suspend fun sendTilKabal(
         klage: Klage,
         ekstraDataInnstilling: EkstradataInnstilling,
     ) {
+        logger.info("Mottok en klage med id=${klage.id}, oversender den til Kabal")
         val oversendelse = KabalOversendelse.fra(klage = klage, ekstraData = ekstraDataInnstilling)
         kabalKlient.sendTilKabal(oversendelse)
+        logger.info("Klage med id=${klage.id} oversendt til Kabal uten feil")
     }
 }

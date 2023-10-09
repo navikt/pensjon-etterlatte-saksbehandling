e09af4a18bc37dde4bf18f9c0f7fc5061a1a4cec
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-10-09 15:27:19 +0200

    Fanger feil fra Kabal-api så vi kan logge mer eksplisitt

--- apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalKlient.kt
+++ apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalKlient.kt
@@ -1,12 +1,11 @@
 package no.nav.etterlatte.klage
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import io.ktor.http.isSuccess
 import no.nav.etterlatte.klage.modell.KabalOversendelse
 import no.nav.etterlatte.libs.common.toJson
 
@@ -16,20 +15,22 @@ interface KabalKlient {
 
 class KabalKlientImpl(private val client: HttpClient, private val kabalUrl: String) : KabalKlient {
     override suspend fun sendTilKabal(kabalOversendelse: KabalOversendelse) {
-        val response =
+        try {
             client.post("$kabalUrl/api/oversendelse/v3/sak") {
                 contentType(ContentType.Application.Json)
                 setBody(kabalOversendelse)
             }
-
-        if (!response.status.isSuccess()) {
-            throw KabalKlientException(response.status, kabalOversendelse)
+        } catch (e: ResponseException) {
+            throw KabalKlientException(kabalOversendelse, e)
         }
     }
 }
 
-class KabalKlientException(httpStatusCode: HttpStatusCode, kabalOversendelse: KabalOversendelse) :
+class KabalKlientException(
+    kabalOversendelse: KabalOversendelse,
+    override val cause: Throwable?,
+) :
     Exception(
-        "Fikk en feil mot Kabal-api ($httpStatusCode) i oversending av klage med id: ${kabalOversendelse.fagSak.fagsakId}. " +
-            "Oversendelse: ${kabalOversendelse.toJson()}",
-    )
+            "Fikk en feil mot Kabal-api i oversending av klage med id: ${kabalOversendelse.fagSak.fagsakId}. " +
+                "Oversendelse: ${kabalOversendelse.toJson()}",
+        )

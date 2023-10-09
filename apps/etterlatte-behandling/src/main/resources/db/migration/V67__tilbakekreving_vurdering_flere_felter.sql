ec0ecd5f7d31840afa6e04afabc238e700f28362
bjorng <bjorn.golberg@gmail.com>
2023-10-09 11:53:57 +0200

    EY-2769 - Nye felter vurdering tilbakekreving
    - Nytt felt for hjemmel
    - Nye felter for vurdering av aktsomhet - strafferettsligVurdering, reduseringAvKrav, rentevurdering

---
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V67__tilbakekreving_vurdering_flere_felter.sql
@@ -0,0 +1,4 @@
+alter table tilbakekreving add column vurdering_hjemmel TEXT;
+alter table tilbakekreving add column akstomhet_redusering_av_kravet TEXT;
+alter table tilbakekreving add column aktsomhet_strafferettslig_vurdering TEXT;
+alter table tilbakekreving add column aktsomhet_rentevurdering TEXT;
--- apps/etterlatte-saksbehandling-ui/client/src/components/tilbakekreving/vurdering/TilbakekrevingVurderingOverordnet.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/tilbakekreving/vurdering/TilbakekrevingVurderingOverordnet.tsx
@@ -1,8 +1,9 @@
-import { Button, Label, Radio, RadioGroup, Select } from '@navikt/ds-react'
+import { BodyShort, Button, Label, Radio, RadioGroup, Select } from '@navikt/ds-react'
 import {
   Tilbakekreving,
   TilbakekrevingAarsak,
   TilbakekrevingAktsomhet,
+  TilbakekrevingHjemmel,
   TilbakekrevingVurdering,
 } from '~shared/types/Tilbakekreving'
 import { isPending, useApiCall } from '~shared/hooks/useApiCall'
@@ -76,11 +77,13 @@ export function TilbakekrevingVurderingOverordnet({ tilbakekreving }: { tilbakek
           legend=""
           size="small"
           className="radioGroup"
-          value={vurdering.aktsomhet ?? ''}
+          value={vurdering.aktsomhet.aktsomhet ?? ''}
           onChange={(e) =>
             setVurdering({
               ...vurdering,
-              aktsomhet: TilbakekrevingAktsomhet[e as TilbakekrevingAktsomhet],
+              aktsomhet: {
+                aktsomhet: TilbakekrevingAktsomhet[e as TilbakekrevingAktsomhet],
+              },
             })
           }
         >
@@ -91,6 +94,61 @@ export function TilbakekrevingVurderingOverordnet({ tilbakekreving }: { tilbakek
           </div>
         </RadioGroup>
       </>
+      {vurdering.aktsomhet.aktsomhet === TilbakekrevingAktsomhet.GROV_UAKTSOMHET && (
+        <TextAreaWrapper>
+          <Label>Gjør en strafferettslig vurdering (Valgfri)</Label>
+          <textarea
+            value={vurdering.aktsomhet.strafferettsligVurdering ?? ''}
+            onChange={(e) =>
+              setVurdering({
+                ...vurdering,
+                aktsomhet: {
+                  ...vurdering.aktsomhet,
+                  strafferettsligVurdering: e.target.value,
+                },
+              })
+            }
+          />
+        </TextAreaWrapper>
+      )}
+      {vurdering.aktsomhet.aktsomhet &&
+        [TilbakekrevingAktsomhet.SIMPEL_UAKTSOMHET, TilbakekrevingAktsomhet.GROV_UAKTSOMHET].includes(
+          vurdering.aktsomhet.aktsomhet
+        ) && (
+          <>
+            <TextAreaWrapper>
+              <Label>Redusering av kravet</Label>
+              <BodyShort>Finnes det grunner til å redusere kravet?</BodyShort>
+              <textarea
+                value={vurdering.aktsomhet.reduseringAvKravet ?? ''}
+                onChange={(e) =>
+                  setVurdering({
+                    ...vurdering,
+                    aktsomhet: {
+                      ...vurdering.aktsomhet,
+                      reduseringAvKravet: e.target.value,
+                    },
+                  })
+                }
+              />
+            </TextAreaWrapper>
+            <TextAreaWrapper>
+              <Label>Rentevurdering</Label>
+              <textarea
+                value={vurdering.aktsomhet.rentevurdering ?? ''}
+                onChange={(e) =>
+                  setVurdering({
+                    ...vurdering,
+                    aktsomhet: {
+                      ...vurdering.aktsomhet,
+                      rentevurdering: e.target.value,
+                    },
+                  })
+                }
+              />
+            </TextAreaWrapper>
+          </>
+        )}
       <TextAreaWrapper>
         <Label>Konklusjon</Label>
         <textarea
@@ -103,12 +161,42 @@ export function TilbakekrevingVurderingOverordnet({ tilbakekreving }: { tilbakek
           }
         />
       </TextAreaWrapper>
+      <DropdownWrapper>
+        <Label>Rettslig grunnlag</Label>
+        <Select
+          label="Hjemmel"
+          hideLabel={true}
+          value={vurdering.hjemmel ?? ''}
+          onChange={(e) => {
+            if (e.target.value == '') return
+            setVurdering({
+              ...vurdering,
+              hjemmel: TilbakekrevingHjemmel[e.target.value as TilbakekrevingHjemmel],
+            })
+          }}
+        >
+          <option value="">Velg hjemmel</option>
+          <option value={TilbakekrevingHjemmel.ULOVFESTET}>Lovfestet</option>
+          <option value={TilbakekrevingHjemmel.TJUETO_FEMTEN_EN_LEDD_EN}>& 22-15 1. ledd, 1. punktum</option>
+          <option value={TilbakekrevingHjemmel.TJUETO_FEMTEN_EN_LEDD_TO_FORSETT}>
+            & 22-15 1. ledd, 2. punktum (forsett)
+          </option>
+          <option value={TilbakekrevingHjemmel.TJUETO_FEMTEN_EN_LEDD_TO_UAKTSOMT}>
+            & 22-15 1. ledd, 2. punktum (uaktsomt)
+          </option>
+          <option value={TilbakekrevingHjemmel.TJUETO_FEMTEN_FEM}>& 22-15 5. ledd</option>
+          <option value={TilbakekrevingHjemmel.TJUETO_FEMTEN_SEKS}>& 22-15 6. ledd</option>
+          <option value={TilbakekrevingHjemmel.TJUETO_SEKSTEN}>& 22-16</option>
+        </Select>
+      </DropdownWrapper>
+
       <Button variant="primary" onClick={lagreVurdering} loading={isPending(lagreVurderingStatus)}>
         Lagre vurdering
       </Button>
     </InnholdForm>
   )
 }
+
 const InnholdForm = styled.div`
   padding: 2em 2em 2em 4em;
   width: 25em;
@@ -128,6 +216,11 @@ const TextAreaWrapper = styled.div`
     resize: none;
   }
 `
+const DropdownWrapper = styled.div`
+  margin-top: 2em;
+  margin-bottom: 2em;
+`
+
 const Beskrivelse = styled.div`
   margin: 10px 0;
 `

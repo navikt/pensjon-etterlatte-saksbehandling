841fe5660569bf58f3eacf5e578c0a4924e16d5d
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-10-11 13:05:06 +0200

    Rydder avhengigheter og struktur i klage

--- apps/etterlatte-klage/build.gradle.kts
+++ apps/etterlatte-klage/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
     id("etterlatte.rapids-and-rivers-ktor2")
 }
 
+val klageKodeverkVersion = "1.6.10"
+
 dependencies {
     implementation(project(":libs:etterlatte-behandling-model"))
     implementation(project(":libs:etterlatte-kafka"))
     implementation(project(":libs:etterlatte-ktor"))
     implementation(project(":libs:etterlatte-funksjonsbrytere"))
     implementation(project(":libs:saksbehandling-common"))
+    implementation("no.nav.klage:klage-kodeverk:$klageKodeverkVersion")
+
     implementation(libs.kafka.clients)
     implementation(libs.kafka.avroserializer)
     implementation(libs.ktor2.okhttp)

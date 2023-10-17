63fb07ce4e2bc4fa8f4358549b29910440a0e00d
Mads Opheim <mads.opheim@nav.no>
2023-10-17 09:56:33 +0200

    Språk-endringa ga mykje knot, rullar tilbake den

--- libs/saksbehandling-common/src/main/kotlin/Spraak.kt
+++ libs/saksbehandling-common/src/main/kotlin/Spraak.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue
 enum class Spraak(
     @get:JsonValue val verdi: String,
 ) {
-    NB("NB"),
-    NN("NN"),
-    EN("EN"),
+    NB("nb"),
+    NN("nn"),
+    EN("en"),
 }

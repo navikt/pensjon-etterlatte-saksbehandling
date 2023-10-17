0aa445628da9575acffbf46f684659da4e8e0bf2
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-10-17 13:33:13 +0200

    Feil i query for generellbeahndling brekker behandling mot saksbehandling (#2432)

--- apps/etterlatte-behandling/src/main/resources/db/migration/V69__rette_opp_utland_til_kravpakke_generellbehandling.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V69__rette_opp_utland_til_kravpakke_generellbehandling.sql
@@ -1,3 +1,5 @@
-UPDATE GENERELLBEHANDLING set type = 'KRAVPAKKE_UTLAND', innhold = jsonb_set(innhold, '{type}', '"KRAVPAKKE_UTLAND"', true) where type = 'UTLAND';
+UPDATE GENERELLBEHANDLING set type = 'KRAVPAKKE_UTLAND' where type = 'UTLAND';
+UPDATE GENERELLBEHANDLING set innhold = jsonb_set(innhold, '{type}', '"KRAVPAKKE_UTLAND"', true) where innhold -> 'type' is not null;
+
 
 UPDATE OPPGAVE SET type = 'KRAVPAKKE_UTLAND' where type = 'UTLAND';

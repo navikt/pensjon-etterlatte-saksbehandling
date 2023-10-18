1f44ff0c45afb6efc12f54ca6dece44836ecd141
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-10-18 12:45:04 +0200

    Legger til manglende ident-felt som kom igjennom byggeteststeget

--- apps/etterlatte-brev-api/src/test/kotlin/no/nav/etterlatte/brev/hentinformasjon/BrevdataFacadeImplTest.kt
+++ apps/etterlatte-brev-api/src/test/kotlin/no/nav/etterlatte/brev/hentinformasjon/BrevdataFacadeImplTest.kt
@@ -229,6 +229,7 @@ internal class BrevdataFacadeImplTest {
                     avdoedFyllerSeksti = null,
                 ),
             overstyrtNorskPoengaar = null,
+            ident = null,
         )
 
     private fun opprettBeregning() =

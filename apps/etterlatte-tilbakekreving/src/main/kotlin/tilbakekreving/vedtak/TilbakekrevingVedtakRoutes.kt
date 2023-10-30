cefd8930cad418ff044827344e874d99a0ec3700
henrikg <henrik.gundersen@nav.no>
2023-10-30 13:55:41 +0100

    Kun systembruker kan sende vedtak ved tilbakekreving

--- apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/vedtak/TilbakekrevingVedtakRoutes.kt
+++ apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/vedtak/TilbakekrevingVedtakRoutes.kt
@@ -9,16 +9,19 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.application
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import no.nav.etterlatte.libs.common.kunSystembruker
 
 fun Route.tilbakekrevingVedtakRoutes(tilbakekrevingVedtakService: TilbakekrevingVedtakService) {
     val logger = application.log
 
     route("/api/tilbakekreving/tilbakekrevingsvedtak") {
         post {
-            logger.info("Sender tilbakekrevingsvedtak")
-            val vedtak = call.receive<TilbakekrevingVedtak>()
-            tilbakekrevingVedtakService.sendTilbakekrevingsvedtak(vedtak)
-            call.respond(HttpStatusCode.Created)
+            kunSystembruker {
+                logger.info("Sender tilbakekrevingsvedtak")
+                val vedtak = call.receive<TilbakekrevingVedtak>()
+                tilbakekrevingVedtakService.sendTilbakekrevingsvedtak(vedtak)
+                call.respond(HttpStatusCode.Created)
+            }
         }
     }
 }

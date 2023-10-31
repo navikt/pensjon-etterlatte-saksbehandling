641891d6283253b7f897e152c41c1ea5f63eaced
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-10-31 16:14:03 +0100

    EY-2765 Håndtere oppdatere vedtak til ferdig samordnet

---
+++ apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/no/nav/etterlatte/vedtaksvurdering/samordning/SamordningMottattRiver.kt
@@ -0,0 +1,36 @@
+package no.nav.etterlatte.vedtaksvurdering.samordning
+
+import no.nav.etterlatte.VedtakService
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+import rapidsandrivers.migrering.ListenerMedLogging
+
+internal class SamordningMottattRiver(
+    rapidsConnection: RapidsConnection,
+    private val vedtaksvurderingService: VedtakService,
+) : ListenerMedLogging() {
+    init {
+        initialiserRiver(rapidsConnection, "VEDTAK:SAMORDNING_MOTTATT") {
+            validate { it.requireKey("vedtakId") }
+        }
+    }
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        val vedtakId = packet["vedtakId"].asText()
+        logger.info("Behandle mottatt samordning for vedtak [vedtakId=$vedtakId]")
+
+        try {
+            val samordnetVedtak = vedtaksvurderingService.samordnetVedtak(vedtakId)
+            logger.info("Behandlet samordning ferdig for vedtak [behandlingId=${samordnetVedtak.behandling.id}]")
+        } catch (e: Exception) {
+            logger.error("Feil ved oppdatering av vedtak til SAMORDNET [vedtakId=$vedtakId]")
+        }
+    }
+}
---
+++ apps/etterlatte-vedtaksvurdering-kafka/src/test/kotlin/no/nav/etterlatte/vedtaksvurdering/samordning/SamordningMottattRiverTest.kt
@@ -0,0 +1,36 @@
+package no.nav.etterlatte.vedtaksvurdering.samordning
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.etterlatte.VedtakService
+import no.nav.etterlatte.libs.common.vedtak.VedtakDto
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class SamordningMottattRiverTest {
+    companion object {
+        val melding =
+            """
+            {
+              "@event_name": "VEDTAK:SAMORDNING_MOTTATT",
+              "vedtakId": "67300342"
+            }
+            """.trimIndent()
+    }
+
+    private val vedtakService = mockk<VedtakService>()
+    private val inspector = TestRapid().apply { SamordningMottattRiver(this, vedtakService) }
+
+    @Test
+    fun `skal lese vedtak og oppdatere til samordnet`() {
+        val vedtakId = "67300342"
+        every { vedtakService.samordnetVedtak(vedtakId) } returns mockk<VedtakDto>()
+
+        inspector.sendTestMessage(melding)
+
+        verify { vedtakService.samordnetVedtak(vedtakId) }
+    }
+}

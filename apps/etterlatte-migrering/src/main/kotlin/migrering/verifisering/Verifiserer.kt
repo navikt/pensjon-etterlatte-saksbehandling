d3aeabab437a3e4908a3a5a30458b65230436ec0
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-10-31 13:50:49 +0100

    Fiksar div i migrering (#2521)
    * Ein del saker vi får frå Pesys i test har mismatch med PDL for gjenlevende. Prøver å skru av verifiseringa for å sjå kva som skjer
    
    * Betre feilmelding viss rare trygdetidsperiodar
    
    * Viss saksbehandlar er PESYS treng vi ikkje å slå opp namn
    
    * Viss saksbehandlar er system treng vi ikkje å slå opp namn
    
    * Vi treng ikkje å gå via proxy-en mot PEN lenger
    
    * Oppdaterer accesspolicy
    
    * Oppdaterer clientid
    
    * Oppdaterer PEN-url
    
    * Oppdaterer PEN-url

--- apps/etterlatte-migrering/src/main/kotlin/migrering/verifisering/Verifiserer.kt
+++ apps/etterlatte-migrering/src/main/kotlin/migrering/verifisering/Verifiserer.kt
@@ -1,18 +1,24 @@
 package no.nav.etterlatte.migrering.verifisering
 
+import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
 import no.nav.etterlatte.libs.common.logging.samleExceptions
 import no.nav.etterlatte.libs.common.logging.sikkerlogger
 import no.nav.etterlatte.libs.common.pdl.PersonDTO
 import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
 import no.nav.etterlatte.libs.common.person.PersonRolle
 import no.nav.etterlatte.libs.common.toJson
+import no.nav.etterlatte.migrering.MigreringFeatureToggle
 import no.nav.etterlatte.migrering.Migreringsstatus
 import no.nav.etterlatte.migrering.PesysRepository
 import no.nav.etterlatte.rapidsandrivers.migrering.MigreringRequest
 import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
 import org.slf4j.LoggerFactory
 
-internal class Verifiserer(private val pdlKlient: PDLKlient, private val repository: PesysRepository) {
+internal class Verifiserer(
+    private val pdlKlient: PDLKlient,
+    private val repository: PesysRepository,
+    private val featureToggleService: FeatureToggleService,
+) {
     private val sikkerlogg = sikkerlogger()
     private val logger = LoggerFactory.getLogger(this::class.java)
 
@@ -47,12 +53,17 @@ internal class Verifiserer(private val pdlKlient: PDLKlient, private val reposit
     private fun hentPerson(
         rolle: PersonRolle,
         folkeregisteridentifikator: Folkeregisteridentifikator,
-    ): Result<PersonDTO> =
+    ): Result<PersonDTO?> =
         try {
             Result.success(pdlKlient.hentPerson(rolle, folkeregisteridentifikator))
         } catch (e: Exception) {
             sikkerlogg.warn("Fant ikke person $folkeregisteridentifikator med rolle $rolle i PDL", e)
-            Result.failure(FinsIkkeIPDL(rolle, folkeregisteridentifikator))
+            if (featureToggleService.isEnabled(MigreringFeatureToggle.VerifiserFoerMigrering, true)) {
+                Result.failure(FinsIkkeIPDL(rolle, folkeregisteridentifikator))
+            } else {
+                logger.warn("Har skrudd av at vi feiler migreringa hvis verifisering feiler. Fortsetter.")
+                Result.success(null)
+            }
         }
 }
 

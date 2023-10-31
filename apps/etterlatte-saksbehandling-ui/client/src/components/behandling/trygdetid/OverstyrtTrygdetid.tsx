d1ee79381471f58fd4f04360b21d91024ddaae32
henrikg <henrik.gundersen@nav.no>
2023-10-31 12:00:35 +0100

    EY-2984 Forenkler tekster for eksportbeskrivelse

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/OverstyrtTrygdetid.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/OverstyrtTrygdetid.tsx
@@ -33,6 +33,17 @@ export const OverstyrtTrygdetid = ({
       : 'men har opptjent rett til tilleggspensjon'
   }
 
+  const eksportBeskrivelse = () => {
+    switch (sakType) {
+      case SakType.BARNEPENSJON:
+        return `Fyll ut ved eksportvurdering når pensjonen er innvilget etter unntaksregelen om at avdød har mindre enn 20
+              års botid, ${opptjentTekst()}. Trygdetid skal da være lik antall poengår.`
+      case SakType.OMSTILLINGSSTOENAD:
+        return `Fyll ut ved eksportvurderdering når stønaden er innvilget etter unntaksregelen om å få trygdetid lik
+              avdødes antall poengår hvis samlet botid til avdøde eller gjenlevende er mindre enn 20 år.`
+    }
+  }
+
   return (
     <Overstyrt>
       {(redigerbar || overstyrtNorskPoengaar !== undefined) && (
@@ -44,44 +55,13 @@ export const OverstyrtTrygdetid = ({
       )}
       {!redigerbar && overstyrtNorskPoengaar !== undefined && (
         <>
-          {
-            {
-              [SakType.BARNEPENSJON]: (
-                <BodyShort>
-                  Eksportvurdering - pensjonen er innvilget etter unntaksregelen om at avdød har mindre enn 20 års
-                  botid,
-                  {opptjentTekst()}:
-                </BodyShort>
-              ),
-              [SakType.OMSTILLINGSSTOENAD]: (
-                <BodyShort>
-                  Ved eksport skal trygdetid være lik avdødes antall poengår fordi samlet botid til avdøde eller
-                  gjenlevende er mindre enn 20 år:
-                </BodyShort>
-              ),
-            }[sakType]
-          }
+          <BodyShort>{eksportBeskrivelse()}</BodyShort>
           <AntallAar>{trygdetid.overstyrtNorskPoengaar} år</AntallAar>
         </>
       )}
       {redigerbar && (
         <>
-          {
-            {
-              [SakType.BARNEPENSJON]: (
-                <BodyShort>
-                  Fyll ut ved eksportvurdering når pensjonen er innvilget etter unntaksregelen om at avdød har mindre
-                  enn 20 års botid, {opptjentTekst()}. Trygdetid skal da være lik antall poengår.
-                </BodyShort>
-              ),
-              [SakType.OMSTILLINGSSTOENAD]: (
-                <BodyShort>
-                  Fyll ut ved eksportvurderdering når stønaden er innvilget etter unntaksregelen om å få trygdetid lik
-                  avdødes antall poengår hvis samlet botid til avdøde eller gjenlevende er mindre enn 20 år.
-                </BodyShort>
-              ),
-            }[sakType]
-          }
+          <BodyShort>{eksportBeskrivelse()}</BodyShort>
           <AntallAarFelt
             value={overstyrtNorskPoengaar ?? ''}
             type="text"

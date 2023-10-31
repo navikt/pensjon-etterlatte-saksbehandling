e23d07b54548e65e593502c5a6f8333aae515164
bjorng <bjorn.golberg@gmail.com>
2023-10-31 09:59:41 +0100

    ktor2client-onbehalfof - HttpResponse.checkForError legger til feilrespons som message

--- libs/ktor2client-onbehalfof/src/main/kotlin/Models.kt
+++ libs/ktor2client-onbehalfof/src/main/kotlin/Models.kt
@@ -3,6 +3,7 @@ package no.nav.etterlatte.libs.ktorobo
 import com.fasterxml.jackson.databind.JsonNode
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
+import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.isSuccess
@@ -19,33 +20,23 @@ data class Resource(
 data class ThrowableErrorMessage(
     override val message: String,
     val throwable: Throwable,
-    val downstreamStatusCode: HttpStatusCode? = null,
 ) : Exception(message, throwable)
 
 class HttpStatusRuntimeException(val downstreamStatusCode: HttpStatusCode, override val message: String) :
     RuntimeException(message)
 
 internal fun Throwable.toErr(url: String): Result<JsonNode, ThrowableErrorMessage> {
-    val downstreamStatusCode =
-        when (this) {
-            is HttpStatusRuntimeException -> this.downstreamStatusCode
-            else -> null
-        }
     return Err(
         ThrowableErrorMessage(
             message = "Error response from '$url'",
             throwable = this,
-            downstreamStatusCode = downstreamStatusCode,
         ),
     )
 }
 
-internal fun HttpResponse.checkForError() =
-    if (!this.status.isSuccess()) {
-        throw HttpStatusRuntimeException(
-            this.status,
-            "received response with status ${this.status.value} from downstream api",
-        )
-    } else {
+internal suspend fun HttpResponse.checkForError() =
+    if (this.status.isSuccess()) {
         this
+    } else {
+        throw HttpStatusRuntimeException(HttpStatusCode.InternalServerError, this.body<String>())
     }

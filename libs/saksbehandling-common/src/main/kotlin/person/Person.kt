42d7f598684a6b5fc99a8c0beae3eb68eb4ddb98
Nicolas N <Watercolours@users.noreply.github.com>
2023-10-31 11:54:26 +0100

    EY-2989 Nytt endepunkt for å hente adressebeskyttelse (#2539)
    * EY-2989 Nytt endepunkt for å hente adressebeskyttelse
    
    * Gi etterlatte-hendelser-joark tilgang

--- libs/saksbehandling-common/src/main/kotlin/person/Person.kt
+++ libs/saksbehandling-common/src/main/kotlin/person/Person.kt
@@ -191,3 +191,5 @@ enum class AdressebeskyttelseGradering {
         return this == STRENGT_FORTROLIG_UTLAND || this == STRENGT_FORTROLIG || this == FORTROLIG
     }
 }
+
+fun List<AdressebeskyttelseGradering?>.hentPrioritertGradering() = this.filterNotNull().minOrNull() ?: AdressebeskyttelseGradering.UGRADERT

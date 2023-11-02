1ffecba473170118bc0f006a0c914a1a4464eff0
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2023-11-02 10:46:10 +0100

    Update apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/soeknadoversikt/gyldigFramsattSoeknad/barnepensjon/GyldigFramsattBarnepensjon.tsx
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/soeknadoversikt/gyldigFramsattSoeknad/barnepensjon/GyldigFramsattBarnepensjon.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/soeknadoversikt/gyldigFramsattSoeknad/barnepensjon/GyldigFramsattBarnepensjon.tsx
@@ -56,7 +56,7 @@ export const GyldigFramsattBarnepensjon = ({
         <Beskrivelse>
           Den som har rett til ytelsen må sette frem krav (forelder/verge hvis under 18 år). Om annet må fullmakt ligge
           i saken. Søknaden må være signert og vise hva det søkes om, og den må være fremsatt i riktig land. Søknaden må
-          være fremstilt i riktig land, dette skal være landet bruker er bosatt i.(unntak hvis ikke avtaleland)
+          være fremstilt i riktig land, dette skal være landet bruker er bosatt i (unntak hvis ikke avtaleland).
         </Beskrivelse>
         <InfobokserWrapper>
           <Innsender innsenderErForelder={innsenderErForelder} />

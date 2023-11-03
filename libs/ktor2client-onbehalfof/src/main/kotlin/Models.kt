db3c4b5f8c1cd2d153dd2d09bb04e7f802a3c14c
Bjørn Golberg <bjorn.golberg@nav.no>
2023-11-03 09:15:42 +0100

    HttpResponse.checkForError legger til feilrespons som message (#2540)
    * ktor2client-onbehalfof - HttpResponse.checkForError legger til feilrespons som message
    
    * ktor2client-onbehalftof - Utvider HttpStatusRuntimeException med InternfeilException
    
    ---------
    
    Co-authored-by: bjorng <bjorn.golberg@gmail.com>

--- libs/ktor2client-onbehalfof/src/main/kotlin/Models.kt
+++ libs/ktor2client-onbehalfof/src/main/kotlin/Models.kt
@@ -3,9 +3,11 @@ package no.nav.etterlatte.libs.ktorobo
 import com.fasterxml.jackson.databind.JsonNode
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
+import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.isSuccess
+import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
 
 data class Resource(
     val clientId: String,
@@ -19,33 +21,23 @@ data class Resource(
 data class ThrowableErrorMessage(
     override val message: String,
     val throwable: Throwable,
-    val downstreamStatusCode: HttpStatusCode? = null,
 ) : Exception(message, throwable)
 
 class HttpStatusRuntimeException(val downstreamStatusCode: HttpStatusCode, override val message: String) :
-    RuntimeException(message)
+    InternfeilException(message)
 
 internal fun Throwable.toErr(url: String): Result<JsonNode, ThrowableErrorMessage> {
-    val downstreamStatusCode =
-        when (this) {
-            is HttpStatusRuntimeException -> this.downstreamStatusCode
-            else -> null
-        }
     return Err(
         ThrowableErrorMessage(
             message = "Error response from '$url'",
             throwable = this,
-            downstreamStatusCode = downstreamStatusCode,
         ),
     )
 }
 
-internal fun HttpResponse.checkForError() =
-    if (!this.status.isSuccess()) {
-        throw HttpStatusRuntimeException(
-            this.status,
-            "received response with status ${this.status.value} from downstream api",
-        )
-    } else {
+internal suspend fun HttpResponse.checkForError() =
+    if (this.status.isSuccess()) {
         this
+    } else {
+        throw HttpStatusRuntimeException(this.status, this.body<String>())
     }

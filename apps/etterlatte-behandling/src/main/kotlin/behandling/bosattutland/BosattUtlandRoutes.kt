faba9b722c4f8ec8aed8d8acd69c214962577e07
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-11-07 16:26:35 +0100

    Rm logger unused

--- apps/etterlatte-behandling/src/main/kotlin/behandling/bosattutland/BosattUtlandRoutes.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/bosattutland/BosattUtlandRoutes.kt
@@ -2,11 +2,9 @@ package no.nav.etterlatte.behandling.bosattutland
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.application.log
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.application
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
@@ -15,7 +13,6 @@ import no.nav.etterlatte.libs.common.behandlingId
 import no.nav.etterlatte.libs.common.kunSaksbehandler
 
 internal fun Route.bosattUtlandRoutes(bosattUtlandService: BosattUtlandService) {
-    val logger = application.log
     route("api/bosattutland/{$BEHANDLINGID_CALL_PARAMETER}") {
         post {
             kunSaksbehandler {

e8e61d3bbdd451b45f22362478c27cc7ba9b84bc
Chris Searle <chris@chrissearle.org>
2023-11-07 13:49:26 +0100

    EY-3006 - overstyr beregning grunnlag persistering

--- apps/etterlatte-beregning/src/test/kotlin/beregning/grunnlag/BeregningsGrunnlagRoutesTest.kt
+++ apps/etterlatte-beregning/src/test/kotlin/beregning/grunnlag/BeregningsGrunnlagRoutesTest.kt
@@ -18,7 +18,10 @@ import io.ktor.server.testing.testApplication
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.slot
 import no.nav.etterlatte.beregning.regler.toGrunnlag
 import no.nav.etterlatte.funksjonsbrytere.DummyFeatureToggleService
 import no.nav.etterlatte.klienter.BehandlingKlient
@@ -42,6 +45,7 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import testsupport.buildTestApplicationConfigurationForOauth
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.Month
 import java.time.YearMonth
@@ -511,6 +515,208 @@ internal class BeregningsGrunnlagRoutesTest {
                 header(HttpHeaders.Authorization, "Bearer $systemToken")
             }.let {
                 it.status shouldBe HttpStatusCode.InternalServerError
+            }
+        }
+    }
+
+    @Test
+    fun `skal hente overstyr beregning grunnlag`() {
+        val behandlingId = randomUUID()
+
+        coEvery { behandlingKlient.harTilgangTilBehandling(behandlingId, any()) } returns true
+
+        every { repository.finnOverstyrBeregningGrunnlagForBehandling(behandlingId) } returns
+            listOf(
+                OverstyrBeregningGrunnlagDao(
+                    id = randomUUID(),
+                    behandlingId = behandlingId,
+                    datoFOM = LocalDate.now().minusYears(12L),
+                    datoTOM = LocalDate.now().minusYears(6L),
+                    utbetaltBeloep = 123L,
+                    trygdetid = 10L,
+                    sakId = 1L,
+                ),
+                OverstyrBeregningGrunnlagDao(
+                    id = randomUUID(),
+                    behandlingId = behandlingId,
+                    datoFOM = LocalDate.now().minusYears(6L),
+                    datoTOM = null,
+                    utbetaltBeloep = 456L,
+                    trygdetid = 20L,
+                    sakId = 1L,
+                ),
+            )
+
+        testApplication {
+            environment { config = applicationConfig }
+            application { restModule(log) { beregningsGrunnlag(service, behandlingKlient) } }
+
+            val client =
+                createClient {
+                    install(ContentNegotiation) {
+                        jackson { registerModule(JavaTimeModule()) }
+                    }
+                }
+
+            client.get("/api/beregning/beregningsgrunnlag/$behandlingId/overstyr") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                header(HttpHeaders.Authorization, "Bearer $token")
+            }.let { response ->
+                response.status shouldBe HttpStatusCode.OK
+
+                val grunnlag = objectMapper.readValue(response.bodyAsText(), OverstyrBeregningGrunnlagDTO::class.java)
+
+                grunnlag.perioder.let { perioder ->
+                    perioder.size shouldBe 2
+                    perioder.minBy { it.fom }.let { periode ->
+                        periode.fom shouldBe LocalDate.now().minusYears(12L)
+                        periode.tom shouldBe LocalDate.now().minusYears(6L)
+                        periode.data.utbetaltBeloep shouldBe 123L
+                        periode.data.trygdetid shouldBe 10L
+                    }
+                    perioder.maxBy { it.fom }.let { periode ->
+                        periode.fom shouldBe LocalDate.now().minusYears(6L)
+                        periode.tom shouldBe null
+                        periode.data.utbetaltBeloep shouldBe 456L
+                        periode.data.trygdetid shouldBe 20L
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `skal lagre overstyr beregning grunnlag`() {
+        val behandlingId = randomUUID()
+        val slot = slot<List<OverstyrBeregningGrunnlagDao>>()
+
+        coEvery { behandlingKlient.harTilgangTilBehandling(behandlingId, any()) } returns true
+
+        coEvery { behandlingKlient.hentBehandling(any(), any()) } returns
+            DetaljertBehandling(
+                id = randomUUID(),
+                sak = 222,
+                sakType = SakType.BARNEPENSJON,
+                behandlingOpprettet = LocalDateTime.now(),
+                soeknadMottattDato = null,
+                innsender = null,
+                soeker = "diam",
+                gjenlevende = listOf(),
+                avdoed = listOf(),
+                soesken = listOf(),
+                status = BehandlingStatus.TRYGDETID_OPPDATERT,
+                behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING,
+                virkningstidspunkt = null,
+                revurderingsaarsak = null,
+                prosesstype = Prosesstype.MANUELL,
+                boddEllerArbeidetUtlandet = null,
+                revurderingInfo = null,
+                enhet = "1111",
+                kilde = Vedtaksloesning.GJENNY,
+            )
+
+        every { repository.lagreOverstyrBeregningGrunnlagForBehandling(behandlingId, capture(slot)) } just runs
+
+        every { repository.finnOverstyrBeregningGrunnlagForBehandling(behandlingId) } returns
+            listOf(
+                OverstyrBeregningGrunnlagDao(
+                    id = randomUUID(),
+                    behandlingId = behandlingId,
+                    datoFOM = LocalDate.now().minusYears(12L),
+                    datoTOM = LocalDate.now().minusYears(6L),
+                    utbetaltBeloep = 123L,
+                    trygdetid = 10L,
+                    sakId = 222L,
+                ),
+                OverstyrBeregningGrunnlagDao(
+                    id = randomUUID(),
+                    behandlingId = behandlingId,
+                    datoFOM = LocalDate.now().minusYears(6L),
+                    datoTOM = null,
+                    utbetaltBeloep = 456L,
+                    trygdetid = 20L,
+                    sakId = 222L,
+                ),
+            )
+
+        testApplication {
+            environment { config = applicationConfig }
+            application { restModule(log) { beregningsGrunnlag(service, behandlingKlient) } }
+
+            val client =
+                createClient {
+                    install(ContentNegotiation) {
+                        jackson { registerModule(JavaTimeModule()) }
+                    }
+                }
+
+            client.post("/api/beregning/beregningsgrunnlag/$behandlingId/overstyr") {
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                header(HttpHeaders.Authorization, "Bearer $token")
+                setBody(
+                    OverstyrBeregningGrunnlagDTO(
+                        perioder =
+                            listOf(
+                                GrunnlagMedPeriode(
+                                    data =
+                                        OverstyrBeregningGrunnlag(
+                                            utbetaltBeloep = 123L,
+                                            trygdetid = 10L,
+                                        ),
+                                    fom = LocalDate.now().minusYears(12L),
+                                    tom = LocalDate.now().minusYears(6L),
+                                ),
+                                GrunnlagMedPeriode(
+                                    data =
+                                        OverstyrBeregningGrunnlag(
+                                            utbetaltBeloep = 456L,
+                                            trygdetid = 20L,
+                                        ),
+                                    fom = LocalDate.now().minusYears(6L),
+                                    tom = null,
+                                ),
+                            ),
+                    ),
+                )
+            }.let { response ->
+                response.status shouldBe HttpStatusCode.OK
+
+                val grunnlag = objectMapper.readValue(response.bodyAsText(), OverstyrBeregningGrunnlagDTO::class.java)
+
+                grunnlag.perioder.let { perioder ->
+                    perioder.size shouldBe 2
+                    perioder.minBy { it.fom }.let { periode ->
+                        periode.fom shouldBe LocalDate.now().minusYears(12L)
+                        periode.tom shouldBe LocalDate.now().minusYears(6L)
+                        periode.data.utbetaltBeloep shouldBe 123L
+                        periode.data.trygdetid shouldBe 10L
+                    }
+                    perioder.maxBy { it.fom }.let { periode ->
+                        periode.fom shouldBe LocalDate.now().minusYears(6L)
+                        periode.tom shouldBe null
+                        periode.data.utbetaltBeloep shouldBe 456L
+                        periode.data.trygdetid shouldBe 20L
+                    }
+                }
+
+                slot.captured.let { daoList ->
+                    daoList.size shouldBe 2
+
+                    daoList.minBy { it.datoFOM }.let { dao ->
+                        dao.datoFOM shouldBe LocalDate.now().minusYears(12L)
+                        dao.datoTOM shouldBe LocalDate.now().minusYears(6L)
+                        dao.utbetaltBeloep shouldBe 123L
+                        dao.trygdetid shouldBe 10L
+                        dao.sakId shouldBe 222L
+                    }
+                    daoList.maxBy { it.datoFOM }.let { dao ->
+                        dao.datoFOM shouldBe LocalDate.now().minusYears(6L)
+                        dao.datoTOM shouldBe null
+                        dao.utbetaltBeloep shouldBe 456L
+                        dao.trygdetid shouldBe 20L
+                        dao.sakId shouldBe 222L
+                    }
+                }
             }
         }
     }

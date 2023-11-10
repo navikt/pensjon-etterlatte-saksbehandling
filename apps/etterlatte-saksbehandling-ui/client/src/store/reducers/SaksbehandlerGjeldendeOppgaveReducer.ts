87c0d131d0c93cf40cd2c9b47020da6187012e29
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-11-10 12:07:01 +0100

    EY-2982 State for saksbehandler på gjeldende oppgave (#2612)
    * EY-2982 Reducer for saksbehandler gjeldende oppgave. Slipper å hente hver gang man bytter fane til sjekklista.
    
    * EY-2982 Gjenbruke reducer i attestering
    
    * EY-2982 Added reset som feilhåndtering

---
+++ apps/etterlatte-saksbehandling-ui/client/src/store/reducers/SaksbehandlerGjeldendeOppgaveReducer.ts
@@ -0,0 +1,17 @@
+import { createAction, createReducer } from '@reduxjs/toolkit'
+
+export const setSaksbehandlerGjeldendeOppgave = createAction<string | null>('saksbehandlerGjeldendeOppgave/set')
+export const resetSaksbehandlerGjeldendeOppgave = createAction('saksbehandlerGjeldendeOppgave/reset')
+
+const initialState: { saksbehandler: string | null } = {
+  saksbehandler: null,
+}
+
+export const saksbehandlerGjeldendeOppgaveReducer = createReducer(initialState, (builder) => {
+  builder.addCase(setSaksbehandlerGjeldendeOppgave, (state, action) => {
+    state.saksbehandler = action.payload
+  })
+  builder.addCase(resetSaksbehandlerGjeldendeOppgave, (state) => {
+    state.saksbehandler = null
+  })
+})

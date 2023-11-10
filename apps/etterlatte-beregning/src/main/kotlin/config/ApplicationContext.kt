ac71e9a8a6590d04a2d1cbbf40540a3792380c9e
Chris Searle <chris@chrissearle.org>
2023-11-10 12:07:59 +0100

    EY-3007 - overstyr beregning grunnlag - beregning (#2661)

--- apps/etterlatte-beregning/src/main/kotlin/config/ApplicationContext.kt
+++ apps/etterlatte-beregning/src/main/kotlin/config/ApplicationContext.kt
@@ -6,6 +6,7 @@ import no.nav.etterlatte.avkorting.AvkortingRepository
 import no.nav.etterlatte.avkorting.AvkortingService
 import no.nav.etterlatte.beregning.BeregnBarnepensjonService
 import no.nav.etterlatte.beregning.BeregnOmstillingsstoenadService
+import no.nav.etterlatte.beregning.BeregnOverstyrBeregningService
 import no.nav.etterlatte.beregning.BeregningRepository
 import no.nav.etterlatte.beregning.BeregningService
 import no.nav.etterlatte.beregning.grunnlag.BeregningsGrunnlagRepository
@@ -71,6 +72,11 @@ class ApplicationContext {
             grunnlagKlient = grunnlagKlient,
             trygdetidKlient = trygdetidKlient,
         )
+    val beregnOverstyrBeregningService =
+        BeregnOverstyrBeregningService(
+            beregningsGrunnlagService = beregningsGrunnlagService,
+            grunnlagKlient = grunnlagKlient,
+        )
     val beregningRepository = BeregningRepository(dataSource)
     val beregningService =
         BeregningService(
@@ -78,6 +84,7 @@ class ApplicationContext {
             behandlingKlient = behandlingKlient,
             beregnBarnepensjonService = beregnBarnepensjonService,
             beregnOmstillingsstoenadService = beregnOmstillingsstoenadService,
+            beregnOverstyrBeregningService = beregnOverstyrBeregningService,
             beregningsGrunnlagService = beregningsGrunnlagService,
             trygdetidKlient = trygdetidKlient,
         )

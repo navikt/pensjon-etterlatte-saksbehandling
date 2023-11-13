9bbf9a9996b066dc27492dcffcbb1be991363c83
Mads Opheim <mads.opheim@nav.no>
2023-11-13 13:50:52 +0100

    EY-3110: Prøver å gjera det mest mogleg likt som mot PEN

--- apps/etterlatte-migrering/src/main/kotlin/migrering/krr/KrrKlient.kt
+++ apps/etterlatte-migrering/src/main/kotlin/migrering/krr/KrrKlient.kt
@@ -1,17 +1,17 @@
 package no.nav.etterlatte.migrering.person.krr
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.michaelbull.result.mapBoth
 import com.typesafe.config.Config
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.request.accept
-import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
-import io.ktor.http.contentType
-import io.ktor.http.isSuccess
+import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.person.Folkeregisteridentifikator
+import no.nav.etterlatte.libs.ktorobo.AzureAdClient
+import no.nav.etterlatte.libs.ktorobo.DownstreamResourceClient
+import no.nav.etterlatte.libs.ktorobo.Resource
+import no.nav.etterlatte.migrering.pen.migreringssystembruker
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -19,26 +19,37 @@ interface Krr {
     suspend fun hentDigitalKontaktinformasjon(fnr: Folkeregisteridentifikator): DigitalKontaktinformasjon?
 }
 
-class KrrKlient(private val client: HttpClient, config: Config) : Krr {
+class KrrKlient(client: HttpClient, config: Config) : Krr {
     private val logger: Logger = LoggerFactory.getLogger(KrrKlient::class.java)
+    private val azureAdClient = AzureAdClient(config)
+    private val downstreamResourceClient = DownstreamResourceClient(azureAdClient, client)
     private val url = config.getString("krr.url")
+
+    private val clientId = config.getString("krr.client_id")
 
     override suspend fun hentDigitalKontaktinformasjon(fnr: Folkeregisteridentifikator): DigitalKontaktinformasjon? {
         logger.info("Henter kontaktopplysninger fra KRR.")
 
         return try {
-            val response =
-                client.get("$url/person") {
-                    header(HttpHeaders.NavPersonIdent, fnr.value)
-                    accept(ContentType.Application.Json)
-                    contentType(ContentType.Application.Json)
-                }
-
-            if (response.status.isSuccess()) {
-                response.body()
-            } else {
-                throw ClientRequestException(response, response.toString())
-            }
+            downstreamResourceClient
+                .get(
+                    resource =
+                        Resource(
+                            clientId = clientId,
+                            url = "$url/person",
+                            additionalHeaders =
+                                mapOf(
+                                    HttpHeaders.NavPersonIdent to fnr.value,
+                                    HttpHeaders.Accept to ContentType.Application.Json.toString(),
+                                    HttpHeaders.ContentType to ContentType.Application.Json.toString(),
+                                ),
+                        ),
+                    brukerTokenInfo = migreringssystembruker,
+                )
+                .mapBoth(
+                    success = { resource -> resource.response.let { objectMapper.readValue(it.toString()) } },
+                    failure = { response -> throw KrrException(response) },
+                )
         } catch (cause: Throwable) {
             logger.warn("Klarte ikke å hente kontaktinformasjon fra KRR.", KrrException(cause))
             return null

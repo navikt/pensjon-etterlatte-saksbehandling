425d95e7f0914db1c5d8e76e1d05fbf0445d19a6
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-11-14 01:18:16 +0100

    Flytt sakting til sak.ts

--- apps/etterlatte-saksbehandling-ui/client/src/shared/api/sak.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/api/sak.ts
@@ -1,6 +1,6 @@
 import { apiClient, ApiResponse } from '~shared/api/apiClient'
 import { IUtenlandstilknytning } from '~shared/types/IDetaljertBehandling'
-import { ISak, ISakMedUtenlandstilknytning } from '~shared/types/sak'
+import { ISak, ISakMedUtenlandstilknytning, SakType } from '~shared/types/sak'
 
 export const lagreUtenlandstilknytning = async ({
   sakId,
@@ -26,3 +26,6 @@ export const hentSakMedUtenlandstilknytning = async (
 export const hentSak = async (sakId: number): Promise<ApiResponse<ISak>> => {
   return apiClient.get(`sak/${sakId}`)
 }
+
+export const hentSakForPerson = async (args: { fnr: string; type: SakType }): Promise<ApiResponse<ISak>> =>
+  apiClient.post(`/personer/sak/${args.type}`, { foedselsnummer: args.fnr })

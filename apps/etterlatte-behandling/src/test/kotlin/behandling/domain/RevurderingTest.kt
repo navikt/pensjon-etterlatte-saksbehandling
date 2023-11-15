517383ab73dd911e6aae525fd0e80528f2731573
henrikg <henrik.gundersen@nav.no>
2023-11-15 13:09:55 +0100

    EY-3125 Ved revurdering må man kunne beregne på nytt etter avkorting

--- apps/etterlatte-behandling/src/test/kotlin/behandling/domain/RevurderingTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/domain/RevurderingTest.kt
@@ -57,7 +57,14 @@ internal class RevurderingTest {
         @Test
         fun `kan endre status gjennom gyldig statusendringsflyt`() {
             val actual =
-                revurdering.tilVilkaarsvurdert().tilTrygdetidOppdatert().tilBeregnet().tilFattetVedtak()
+                revurdering
+                    .tilVilkaarsvurdert()
+                    .tilTrygdetidOppdatert()
+                    .tilBeregnet()
+                    .tilAvkortet()
+                    .tilBeregnet()
+                    .tilAvkortet()
+                    .tilFattetVedtak()
                     .tilAttestert().tilIverksatt()
 
             Assertions.assertEquals(BehandlingStatus.IVERKSATT, actual.status)

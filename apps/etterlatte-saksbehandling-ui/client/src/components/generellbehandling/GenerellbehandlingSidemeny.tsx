8a534c7db14620327b07462e9a439bfd55249265
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-11-20 11:28:10 +0100

    EY-3146 Init

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/generellbehandling/GenerellbehandlingSidemeny.tsx
@@ -0,0 +1,26 @@
+import { Sidebar } from '~shared/components/Sidebar'
+import { Generellbehandling, KravpakkeUtland, Status } from '~shared/types/Generellbehandling'
+import { Attestering } from '~components/generellbehandling/Attestering'
+
+const kanAttestere = (status: Status) => {
+  return status === Status.FATTET
+}
+export const GenerellbehandlingSidemeny = (props: {
+  utlandsBehandling: Generellbehandling & { innhold: KravpakkeUtland | null }
+}) => {
+  const { utlandsBehandling } = props
+
+  const attesterbar = kanAttestere(utlandsBehandling.status)
+
+  switch (utlandsBehandling.status) {
+    case Status.OPPRETTET:
+      break
+    case Status.FATTET:
+      break
+    case Status.ATTESTERT:
+      break
+    case Status.AVBRUTT:
+      break
+  }
+  return <Sidebar>{attesterbar && <Attestering />}</Sidebar>
+}

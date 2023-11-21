8929de3566b7e6cd06603a6b6f11680e3180ffb7
Mads Opheim <mads.opheim@nav.no>
2023-11-21 16:20:40 +0100

    EY-3156: Refaktoriserer for å tydeleggjera

--- apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/RapidUtsender.kt
+++ apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/RapidUtsender.kt
@@ -1,30 +1,28 @@
 import no.nav.etterlatte.libs.common.rapidsandrivers.TEKNISK_TID_KEY
 import no.nav.etterlatte.libs.common.rapidsandrivers.eventName
+import no.nav.etterlatte.vedtaksvurdering.RapidInfo
 import no.nav.etterlatte.vedtaksvurdering.VedtakOgRapid
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 
 object RapidUtsender {
     fun sendUt(
-        respons: VedtakOgRapid,
+        vedtakOgRapid: VedtakOgRapid,
         packet: JsonMessage,
         context: MessageContext,
     ) {
-        with(respons.rapidInfo1) {
-            packet.eventName = vedtakhendelse.toString()
-            packet[TEKNISK_TID_KEY] = tekniskTid
-            packet["vedtak"] = vedtak
-            extraParams.forEach { (k, v) -> packet[k] = v }
-            context.publish(behandlingId.toString(), packet.toJson())
-        }
-        respons.rapidInfo2?.let {
-            with(it) {
-                packet.eventName = vedtakhendelse.toString()
-                packet[TEKNISK_TID_KEY] = tekniskTid
-                packet["vedtak"] = vedtak
-                extraParams.forEach { (k, v) -> packet[k] = v }
-                context.publish(behandlingId.toString(), packet.toJson())
-            }
-        }
+        vedtakOgRapid.rapidInfo1.sendUt(packet, context)
+        vedtakOgRapid.rapidInfo2?.sendUt(packet, context)
     }
+}
+
+private fun RapidInfo.sendUt(
+    packet: JsonMessage,
+    context: MessageContext,
+) {
+    packet.eventName = vedtakhendelse.toString()
+    packet[TEKNISK_TID_KEY] = tekniskTid
+    packet["vedtak"] = vedtak
+    extraParams.forEach { (k, v) -> packet[k] = v }
+    context.publish(behandlingId.toString(), packet.toJson())
 }

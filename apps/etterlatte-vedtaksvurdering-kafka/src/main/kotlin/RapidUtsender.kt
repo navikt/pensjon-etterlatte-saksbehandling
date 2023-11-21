16869f66b8a5b3b2fea54d8b23457ac687b3cb31
Mads Opheim <mads.opheim@nav.no>
2023-11-21 16:23:51 +0100

    EY-3156: Litt ekstra logging

--- apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/RapidUtsender.kt
+++ apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/RapidUtsender.kt
@@ -4,25 +4,32 @@ import no.nav.etterlatte.vedtaksvurdering.RapidInfo
 import no.nav.etterlatte.vedtaksvurdering.VedtakOgRapid
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 object RapidUtsender {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun sendUt(
         vedtakOgRapid: VedtakOgRapid,
         packet: JsonMessage,
         context: MessageContext,
     ) {
-        vedtakOgRapid.rapidInfo1.sendUt(packet, context)
-        vedtakOgRapid.rapidInfo2?.sendUt(packet, context)
+        vedtakOgRapid.rapidInfo1.sendUt(packet, context, logger)
+        vedtakOgRapid.rapidInfo2?.sendUt(packet, context, logger)
     }
 }
 
 private fun RapidInfo.sendUt(
     packet: JsonMessage,
     context: MessageContext,
+    logger: Logger,
 ) {
+    logger.info("Sender ut på rapid for hendelse $vedtakhendelse")
     packet.eventName = vedtakhendelse.toString()
     packet[TEKNISK_TID_KEY] = tekniskTid
     packet["vedtak"] = vedtak
     extraParams.forEach { (k, v) -> packet[k] = v }
     context.publish(behandlingId.toString(), packet.toJson())
+    logger.info("Sendte ut på rapid for hendelse $vedtakhendelse")
 }

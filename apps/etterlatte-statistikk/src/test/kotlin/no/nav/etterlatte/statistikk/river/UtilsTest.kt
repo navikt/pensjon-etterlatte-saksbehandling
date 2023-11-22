ecad038a9501c98baf1f053142d971142901c7ec
Mads Opheim <mads.opheim@nav.no>
2023-11-22 11:22:50 +0100

    Kan parse teknisk tid også når den kjem som zoneddatetime

--- apps/etterlatte-statistikk/src/test/kotlin/no/nav/etterlatte/statistikk/river/UtilsTest.kt
+++ apps/etterlatte-statistikk/src/test/kotlin/no/nav/etterlatte/statistikk/river/UtilsTest.kt
@@ -7,8 +7,15 @@ import java.time.Month
 
 class UtilsTest {
     @Test
-    fun `takler migreringsformatet`() {
+    fun `kan parse ZonedDateTime`() {
         val format = "2023-11-22T08:42:18.892199Z"
+        val tid = parseTekniskTid(format, "event1")
+        Assertions.assertEquals(LocalDateTime.of(2023, Month.NOVEMBER, 22, 9, 42, 18, 892199000), tid)
+    }
+
+    @Test
+    fun `kan parse LocalDateTime`() {
+        val format = "2023-11-22T08:42:18.892199"
         val tid = parseTekniskTid(format, "event1")
         Assertions.assertEquals(LocalDateTime.of(2023, Month.NOVEMBER, 22, 8, 42, 18, 892199000), tid)
     }

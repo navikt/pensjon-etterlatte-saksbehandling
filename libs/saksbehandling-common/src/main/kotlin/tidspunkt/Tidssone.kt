4983464306f943be56db53b90596b0d84a7bc58f
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-11-22 11:34:29 +0100

    Kan parse teknisk tid også når han kjem som ZonedDateTime (#2814)
    * Kan parse teknisk tid også når den kjem som zoneddatetime
    
    * Kan parse teknisk tid også når den kjem som zoneddatetime

--- libs/saksbehandling-common/src/main/kotlin/tidspunkt/Tidssone.kt
+++ libs/saksbehandling-common/src/main/kotlin/tidspunkt/Tidssone.kt
@@ -3,6 +3,6 @@ package no.nav.etterlatte.libs.common.tidspunkt
 import java.time.ZoneId
 import java.time.ZoneOffset
 
-internal val norskTidssone: ZoneId = ZoneId.of("Europe/Oslo")
+val norskTidssone: ZoneId = ZoneId.of("Europe/Oslo")
 
 internal val standardTidssoneUTC = ZoneOffset.UTC

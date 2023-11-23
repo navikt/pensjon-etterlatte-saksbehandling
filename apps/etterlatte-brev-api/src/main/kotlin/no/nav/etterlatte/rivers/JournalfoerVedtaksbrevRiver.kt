12c06868c0cb356ee2bd6c9d4ff978dce4758a65
Mads Opheim <mads.opheim@nav.no>
2023-11-23 17:32:13 +0100

    Fiksar sak der oppretting av brev feila

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/JournalfoerVedtaksbrevRiver.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/JournalfoerVedtaksbrevRiver.kt
@@ -57,11 +57,6 @@ internal class JournalfoerVedtaksbrevRiver(
             logger.info("Nytt vedtak med id ${vedtak.vedtakId} er attestert. Ferdigstiller vedtaksbrev.")
             val behandlingId = vedtak.behandlingId
 
-            if (behandlingId == UUID.fromString("8d23f2be-e025-4671-8a26-fddb4634b15c")) {
-                logger.warn("Håndterer denne spesielt for migrering")
-                return
-            }
-
             val vedtaksbrev =
                 service.hentVedtaksbrev(behandlingId)
                     ?: throw NoSuchElementException("Ingen vedtaksbrev funnet på behandlingId=$behandlingId")
--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/migrering/FiksEnkeltbrevRiver.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/migrering/FiksEnkeltbrevRiver.kt
@@ -1,8 +1,10 @@
 package no.nav.etterlatte.rivers.migrering
 
 import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.brev.MigreringBrevRequest
 import no.nav.etterlatte.brev.VedtaksbrevService
 import no.nav.etterlatte.brev.hentinformasjon.VedtaksvurderingService
+import no.nav.etterlatte.brev.model.Brev
 import no.nav.etterlatte.libs.common.rapidsandrivers.eventName
 import no.nav.etterlatte.libs.common.vedtak.VedtakKafkaHendelseType
 import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
@@ -12,6 +14,8 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
+import rapidsandrivers.BEHANDLING_ID_KEY
+import rapidsandrivers.behandlingId
 import rapidsandrivers.migrering.ListenerMedLoggingOgFeilhaandtering
 
 internal class FiksEnkeltbrevRiver(
@@ -23,7 +27,7 @@ internal class FiksEnkeltbrevRiver(
 
     init {
         initialiserRiver(rapidsConnection, Migreringshendelser.FIKS_ENKELTBREV) {
-            validate { it.requireKey(BREV_ID_KEY) }
+            validate { it.requireKey(BEHANDLING_ID_KEY) }
         }
     }
 
@@ -31,15 +35,22 @@ internal class FiksEnkeltbrevRiver(
         packet: JsonMessage,
         context: MessageContext,
     ) {
-        val brevId = packet[BREV_ID_KEY].asLong()
-        val brev = service.hentBrev(id = brevId)
-
-        logger.info("Fikser vedtaksbrev for brev $brevId")
-        val behandlingId = brev.behandlingId!!
+        val behandlingId = packet.behandlingId
+        logger.info("Fikser vedtaksbrev for behandling $behandlingId")
         val brukerTokenInfo = Systembruker("migrering", "migrering")
+        val migreringBrevRequest = MigreringBrevRequest(brutto = 3954)
         runBlocking {
+            val sakId = vedtaksvurderingService.hentVedtak(behandlingId, brukerTokenInfo).sak.id
+            val vedtaksbrev: Brev =
+                service.opprettVedtaksbrev(
+                    sakId,
+                    behandlingId,
+                    brukerTokenInfo,
+                    migreringBrevRequest,
+                )
+            service.genererPdf(vedtaksbrev.id, brukerTokenInfo, migreringBrevRequest)
             service.ferdigstillVedtaksbrev(behandlingId, brukerTokenInfo, true)
-            logger.info("Har oppretta vedtaksbrev for brev $brevId")
+            logger.info("Har oppretta vedtaksbrev for behandling $behandlingId")
 
             packet.eventName = VedtakKafkaHendelseType.ATTESTERT.toString()
             val vedtak = vedtaksvurderingService.hentVedtak(behandlingId, brukerTokenInfo)
@@ -48,5 +59,3 @@ internal class FiksEnkeltbrevRiver(
         context.publish(packet.toJson())
     }
 }
-
-const val BREV_ID_KEY = "brev_id"

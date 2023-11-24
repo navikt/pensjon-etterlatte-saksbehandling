ebe8f0e578d6ff1ae6096fa18267eb6ad109f5ed
Trond Valen <trond.valen@nav.no>
2023-11-24 15:00:17 +0100

    EY-3063: Dropper logging av error til console.

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/Vergeadresse.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/Vergeadresse.tsx
@@ -4,7 +4,6 @@ import { isFailure, Result } from '~shared/hooks/useApiCall'
 export const handleHentVergeadresseError = (result: Result<any>) => {
   if (isFailure(result)) {
     if (result.error.status != 404) {
-      console.error(`Feil ved opprettelse av revurdering, status: ${result.status} error: ${result.error.detail}`)
       return <ApiErrorAlert>Feil oppsto ved henting av eventuell verges adresse. Prøv igjen senere</ApiErrorAlert>
     }
   }

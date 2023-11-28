7233c0d7fc9f1bf87a4566c58c74a49809e58e24
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2023-11-28 12:27:55 +0100

    EY-3198 Laste personopplysninger-redux state etter behandling

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/familieforhold/Familieforhold.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/familieforhold/Familieforhold.tsx
@@ -4,29 +4,15 @@ import { FamilieforholdBarnepensjon } from '~components/behandling/soeknadsovers
 import { FamilieforholdOmstillingsstoenad } from '~components/behandling/soeknadsoversikt/familieforhold/omstillingsstoenad/FamilieforholdOmstillingsstoenad'
 import { Heading } from '@navikt/ds-react'
 import { ContentHeader } from '~shared/styled'
-import { useApiCall } from '~shared/hooks/useApiCall'
-import { hentPersonopplysningerForBehandling } from '~shared/api/grunnlag'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
-import { useAppDispatch } from '~store/Store'
-import { setPersonopplysninger } from '~store/reducers/PersonopplysningerReducer'
 
 export interface PropsFamilieforhold {
   behandling: IDetaljertBehandling
 }
 
 export const Familieforhold = ({ behandling }: PropsFamilieforhold) => {
-  const [, fetchPersonopplysninger] = useApiCall(hentPersonopplysningerForBehandling)
-
   const personopplysninger = usePersonopplysninger()
-
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    fetchPersonopplysninger({ behandlingId: behandling.id, sakType: behandling.sakType }, (result) =>
-      dispatch(setPersonopplysninger(result))
-    )
-  }, [])
 
   return (
     <>
--- apps/etterlatte-saksbehandling-ui/client/src/store/reducers/PersonopplysningerReducer.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/store/reducers/PersonopplysningerReducer.ts
@@ -2,6 +2,7 @@ import { createAction, createReducer } from '@reduxjs/toolkit'
 import { Personopplysninger } from '~shared/types/grunnlag'
 
 export const setPersonopplysninger = createAction<Personopplysninger>('behandlingPersoner/set')
+export const resetPersonopplysninger = createAction('behandlingPersoner/reset')
 
 const initialState: { personopplysninger: Personopplysninger | null } = {
   personopplysninger: null,
@@ -10,5 +11,8 @@ const initialState: { personopplysninger: Personopplysninger | null } = {
 export const personopplysningerReducer = createReducer(initialState, (builder) => {
   builder.addCase(setPersonopplysninger, (state, action) => {
     state.personopplysninger = action.payload
+  })
+  builder.addCase(resetPersonopplysninger, (state) => {
+    state.personopplysninger = null
   })
 })

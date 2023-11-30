f823d69fe4e2ef631f5866cdd3e1fcde3f92dd84
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-11-30 15:31:59 +0100

    Vi trenger nyeste opplysning i tilfelle grunnlaget er oppdatert

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/GrunnlagService.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/GrunnlagService.kt
@@ -303,12 +303,15 @@ class RealGrunnlagService(
 
     override fun hentPersongalleriSamsvar(behandlingId: UUID): PersongalleriSamsvar {
         val grunnlag = opplysningDao.hentAlleGrunnlagForBehandling(behandlingId)
-        val opplysningPersongalleriSak = grunnlag.find { it.opplysning.opplysningType == PERSONGALLERI_V1 }
-        val opplysningPersongalleriPdl = grunnlag.find { it.opplysning.opplysningType == PERSONGALLERI_PDL_V1 }
+        val opplysningPersongalleriSak =
+            grunnlag.filter { it.opplysning.opplysningType == PERSONGALLERI_V1 }.maxByOrNull { it.hendelseNummer }
+        val opplysningPersongalleriPdl =
+            grunnlag.filter { it.opplysning.opplysningType == PERSONGALLERI_PDL_V1 }.maxByOrNull { it.hendelseNummer }
         if (opplysningPersongalleriSak == null) {
             throw GenerellIkkeFunnetException()
         }
-        val persongalleriISak: Persongalleri = objectMapper.readValue(opplysningPersongalleriSak.opplysning.opplysning.toJson())
+        val persongalleriISak: Persongalleri =
+            objectMapper.readValue(opplysningPersongalleriSak.opplysning.opplysning.toJson())
 
         if (opplysningPersongalleriPdl == null) {
             logger.info("Fant ikke persongalleri fra PDL for behandling med id=$behandlingId, gjør ingen samsvarsjekk")
@@ -320,7 +323,8 @@ class RealGrunnlagService(
                 problemer = listOf(),
             )
         }
-        val persongalleriPdl: Persongalleri = objectMapper.readValue(opplysningPersongalleriPdl.opplysning.opplysning.toJson())
+        val persongalleriPdl: Persongalleri =
+            objectMapper.readValue(opplysningPersongalleriPdl.opplysning.opplysning.toJson())
 
         val valideringsfeil =
             valideringsfeilPersongalleriSakPdl(

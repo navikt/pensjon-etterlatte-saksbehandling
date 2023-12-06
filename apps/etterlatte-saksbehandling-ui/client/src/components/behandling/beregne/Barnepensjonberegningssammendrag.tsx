a1b30d801434dbc35cf1115faf6213b889c1e2f4
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-12-06 10:25:09 +0100

    Nytt beregningssammendrag på nytt regelverk BP

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/Barnepensjonberegningssammendrag.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/Barnepensjonberegningssammendrag.tsx
@@ -1,7 +1,12 @@
 import { BodyShort, Heading, Label } from '@navikt/ds-react'
-import { differenceInYears } from 'date-fns'
+import { differenceInYears, isAfter, parseISO } from 'date-fns'
 import styled from 'styled-components'
-import { Beregningsperiode, ReduksjonBP } from '~shared/types/Beregning'
+import {
+  Beregningsperiode,
+  InstitusjonsoppholdIBeregning,
+  OverstyrBeregning,
+  ReduksjonBP,
+} from '~shared/types/Beregning'
 import React from 'react'
 
 interface BeregningsdetaljerPerson {
@@ -11,69 +16,132 @@ interface BeregningsdetaljerPerson {
   foedselsdato: string | Date
 }
 
+const SISTE_MAANED_GAMMELT_REGELVERK = new Date(2023, 11, 31, 0, 0, 0, 0)
+
+const SammendragGammeltRegelverk = (props: {
+  soesken: BeregningsdetaljerPerson[]
+  soeker: BeregningsdetaljerPerson
+  beregningsperiode: Beregningsperiode
+}) => {
+  const { soesken, soeker, beregningsperiode } = props
+
+  return (
+    <>
+      <Heading level="3" size="small">
+        Søskenjustering
+      </Heading>
+      <BodyShort spacing>
+        <strong>§18-5</strong> En forelder død: 40% av G til første barn, 25% av G til resterende. Beløpene slås sammen
+        og fordeles likt.
+      </BodyShort>
+      {soesken && (
+        <>
+          <Label>Beregningen gjelder:</Label>
+          <ul>
+            {beregningsperiode.soeskenFlokk
+              .map((fnr) => soesken.find((p) => p.foedselsnummer === fnr))
+              .concat([soeker])
+              .map((soeskenIFlokken) => {
+                return (
+                  soeskenIFlokken && (
+                    <ListWithoutBullet key={soeskenIFlokken.foedselsnummer}>
+                      {`${soeskenIFlokken.fornavn} ${soeskenIFlokken.etternavn} / ${
+                        soeskenIFlokken.foedselsnummer
+                      } / ${differenceInYears(new Date(), new Date(soeskenIFlokken.foedselsdato))} år`}
+                    </ListWithoutBullet>
+                  )
+                )
+              })}
+          </ul>
+        </>
+      )}
+    </>
+  )
+}
+
+const SammendragNyttRegelverk = (props: { soeker: BeregningsdetaljerPerson }) => {
+  const { soeker } = props
+
+  return (
+    <>
+      <Heading level="3" size="small">
+        Beregning av barnepensjon
+      </Heading>
+      <BodyShort spacing>
+        <strong>§18-5</strong> En forelder død: 100% av G til barnet.
+      </BodyShort>
+
+      <Label>Beregningen gjelder:</Label>
+      <ul>
+        <ListWithoutBullet>
+          {`${soeker.fornavn} ${soeker.etternavn} / ${soeker.foedselsnummer} / ${differenceInYears(
+            new Date(),
+            new Date(soeker.foedselsdato)
+          )} år`}
+        </ListWithoutBullet>
+      </ul>
+    </>
+  )
+}
+
+const SammendragInstitusjonsopphold = (props: { institusjonsopphold: InstitusjonsoppholdIBeregning }) => {
+  const { institusjonsopphold } = props
+
+  return (
+    <>
+      <HeadingWithTopMargin level="3" size="small">
+        Institusjonsopphold
+      </HeadingWithTopMargin>
+      <div>
+        <strong>§18-8</strong> En forelder død: Barnepensjonen reduseres til 10 % av G ved lengre institusjonsopphold.
+        Hvis man har utgifter til bolig kan man likevel slippe reduksjon eller få en lavere reduksjon.
+        <div>
+          <Label>Beregningen gjelder: </Label>
+          <ListWithoutBullet>
+            {ReduksjonBP[institusjonsopphold.reduksjon]}
+            {institusjonsopphold.egenReduksjon && <p>Egen reduksjon: {institusjonsopphold.egenReduksjon}</p>}
+          </ListWithoutBullet>
+        </div>
+      </div>
+    </>
+  )
+}
+
 export const Barnepensjonberegningssammendrag = ({
   beregningsperiode,
+  overstyring,
   soesken,
   soeker,
 }: {
   beregningsperiode: Beregningsperiode
+  overstyring?: OverstyrBeregning
   soesken: BeregningsdetaljerPerson[] | undefined
   soeker: BeregningsdetaljerPerson
 }) => {
+  const datoPeriodeFom = parseISO(beregningsperiode.datoFOM)
+  if (overstyring) {
+    return (
+      <>
+        <Heading level="2" size="small">
+          Overstyrt beregning
+        </Heading>
+        <BodyShort>Beregningen er manuelt overstyrt, på grunn av {overstyring.beskrivelse}.</BodyShort>
+      </>
+    )
+  }
+
+  const erPaaNyttRegelverk = isAfter(datoPeriodeFom, SISTE_MAANED_GAMMELT_REGELVERK)
+
+  console.log('datoer', datoPeriodeFom, erPaaNyttRegelverk)
   return (
     <>
-      {beregningsperiode.soeskenFlokk && (
-        <>
-          <Heading level="1" size="small">
-            Søskenjustering
-          </Heading>
-          <BodyShort spacing>
-            <strong>§18-5</strong> En forelder død: 40% av G til første barn, 25% av G til resterende. Beløpene slås
-            sammen og fordeles likt.
-          </BodyShort>
-          {soesken && (
-            <>
-              <Label>Beregningen gjelder:</Label>
-              <ul>
-                {beregningsperiode.soeskenFlokk
-                  .map((fnr) => soesken?.find((p) => p.foedselsnummer === fnr))
-                  .concat([soeker])
-                  .map((soeskenIFlokken) => {
-                    return (
-                      soeskenIFlokken && (
-                        <ListWithoutBullet key={soeskenIFlokken.foedselsnummer}>
-                          {`${soeskenIFlokken.fornavn} ${soeskenIFlokken.etternavn} / ${
-                            soeskenIFlokken.foedselsnummer
-                          } / ${differenceInYears(new Date(), new Date(soeskenIFlokken.foedselsdato))} år`}
-                        </ListWithoutBullet>
-                      )
-                    )
-                  })}
-              </ul>
-            </>
-          )}
-        </>
+      {erPaaNyttRegelverk ? (
+        <SammendragNyttRegelverk soeker={soeker} />
+      ) : (
+        <SammendragGammeltRegelverk soesken={soesken ?? []} soeker={soeker} beregningsperiode={beregningsperiode} />
       )}
       {beregningsperiode?.institusjonsopphold && (
-        <>
-          <HeadingWithTopMargin level="1" size="small">
-            Institusjonsopphold
-          </HeadingWithTopMargin>
-          <div>
-            <strong>§18-8</strong> En forelder død: Barnepensjonen reduseres til 10 % av G ved lengre
-            institusjonsopphold. Hvis man har utgifter til bolig kan man likevel slippe reduksjon eller få en lavere
-            reduksjon.
-            <div>
-              <strong>Beregningen gjelder: </strong>
-              <ListWithoutBullet>
-                {ReduksjonBP[beregningsperiode.institusjonsopphold.reduksjon]}
-                {beregningsperiode.institusjonsopphold.egenReduksjon && (
-                  <p>Egen reduksjon: {beregningsperiode.institusjonsopphold.egenReduksjon}</p>
-                )}
-              </ListWithoutBullet>
-            </div>
-          </div>
-        </>
+        <SammendragInstitusjonsopphold institusjonsopphold={beregningsperiode.institusjonsopphold} />
       )}
     </>
   )

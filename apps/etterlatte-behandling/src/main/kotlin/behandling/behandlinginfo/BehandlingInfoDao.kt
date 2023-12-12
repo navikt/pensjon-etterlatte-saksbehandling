218b72928780626b0fad80819701fdfd499eea3a
henrikg <henrik.gundersen@nav.no>
2023-12-12 10:55:23 +0100

    EY-3172 Setter oppdatert-tidspunkt direkte i db

--- apps/etterlatte-behandling/src/main/kotlin/behandling/behandlinginfo/BehandlingInfoDao.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/behandlinginfo/BehandlingInfoDao.kt
@@ -3,8 +3,6 @@ package no.nav.etterlatte.behandling.behandlinginfo
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
 import no.nav.etterlatte.libs.common.objectMapper
-import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
-import no.nav.etterlatte.libs.common.tidspunkt.setTidspunkt
 import no.nav.etterlatte.libs.database.setJsonb
 import no.nav.etterlatte.libs.database.singleOrNull
 import java.sql.Connection
@@ -15,16 +13,15 @@ class BehandlingInfoDao(private val connection: () -> Connection) {
     fun lagreBrevutfall(brevutfall: Brevutfall): Brevutfall {
         return connection().prepareStatement(
             """
-            INSERT INTO behandling_info(behandling_id, oppdatert, brevutfall)
-            VALUES (?, ?, ?)
+            INSERT INTO behandling_info(behandling_id, brevutfall)
+            VALUES (?, ?)
             ON CONFLICT (behandling_id) DO 
-            UPDATE SET oppdatert = excluded.oppdatert, brevutfall = excluded.brevutfall
+            UPDATE SET brevutfall = excluded.brevutfall
             """.trimIndent(),
         )
             .apply {
                 setObject(1, brevutfall.behandlingId)
-                setTidspunkt(2, Tidspunkt.now())
-                setJsonb(3, brevutfall)
+                setJsonb(2, brevutfall)
             }
             .run { executeUpdate() }
             .also { require(it == 1) }
@@ -47,16 +44,15 @@ class BehandlingInfoDao(private val connection: () -> Connection) {
     fun lagreEtterbetaling(etterbetaling: EtterbetalingNy): EtterbetalingNy {
         return connection().prepareStatement(
             """
-            INSERT INTO behandling_info(behandling_id, oppdatert, etterbetaling)
-            VALUES (?, ?, ?)
+            INSERT INTO behandling_info(behandling_id, etterbetaling)
+            VALUES (?, ?)
             ON CONFLICT (behandling_id) DO 
-            UPDATE SET oppdatert = excluded.oppdatert, etterbetaling = excluded.etterbetaling
+            UPDATE SET etterbetaling = excluded.etterbetaling
             """.trimIndent(),
         )
             .apply {
                 setObject(1, etterbetaling.behandlingId)
-                setTidspunkt(2, Tidspunkt.now())
-                setJsonb(3, etterbetaling)
+                setJsonb(2, etterbetaling)
             }
             .run { executeUpdate() }
             .also { require(it == 1) }
@@ -69,14 +65,13 @@ class BehandlingInfoDao(private val connection: () -> Connection) {
     fun slettEtterbetaling(behandlingId: UUID): Int {
         return connection().prepareStatement(
             """
-            UPDATE behandling_info SET oppdatert = ?, etterbetaling = ?
+            UPDATE behandling_info SET etterbetaling = ?
             WHERE behandling_id = ?
             """.trimIndent(),
         )
             .apply {
-                setTidspunkt(1, Tidspunkt.now())
-                setJsonb(2, null)
-                setObject(3, behandlingId)
+                setJsonb(1, null)
+                setObject(2, behandlingId)
             }
             .run { executeUpdate() }
             .also { require(it == 1) }
--- apps/etterlatte-behandling/src/main/resources/db/migration/V80__behandling_info.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V80__behandling_info.sql
@@ -2,7 +2,7 @@ create table behandling_info
 (
     behandling_id UUID PRIMARY KEY
         CONSTRAINT behandling_info_behandling_fk_id REFERENCES behandling (id),
-    oppdatert     TIMESTAMP WITH TIME ZONE NOT NULL,
+    oppdatert     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     etterbetaling JSONB,
     brevutfall    JSONB
 )

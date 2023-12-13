4768389ac5a1bbad6b6625ce8f622942940d3cb4
Espen Schei <espen.schei@nav.no>
2023-12-13 12:04:24 +0100

    Rute for å vise informasjon om flyktning i frontend.
    Spesielt viktig for migrerte saker hvor det er anvendt flyktningfordel med 40 års trygdetid.

--- apps/etterlatte-behandling/src/test/kotlin/sak/SakDaoTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/sak/SakDaoTest.kt
@@ -1,16 +1,13 @@
 package no.nav.etterlatte.sak
 
-import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.etterlatte.common.Enheter
 import no.nav.etterlatte.grunnlagsendring.GrunnlagsendringshendelseService
 import no.nav.etterlatte.libs.common.behandling.Flyktning
 import no.nav.etterlatte.libs.common.behandling.SakType
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
-import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.database.DataSourceBuilder
 import no.nav.etterlatte.libs.database.POSTGRES_VERSION
 import no.nav.etterlatte.libs.database.migrate
-import no.nav.etterlatte.libs.database.singleOrNull
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
@@ -60,19 +57,12 @@ internal class SakDaoTest {
     }
 
     @Test
-    fun `kan lagre flyktning`() {
+    fun `kan lagre og hente flyktning`() {
         val flyktning = Flyktning(true, LocalDate.of(2024, 1, 1), "Migrert", Grunnlagsopplysning.Pesys.create())
         val opprettSak = sakRepo.opprettSak("fnr", SakType.BARNEPENSJON, Enheter.PORSGRUNN.enhetNr)
 
         sakRepo.oppdaterFlyktning(opprettSak.id, flyktning)
-
-        val oppdatertFlyktning: Flyktning? =
-            dataSource.connection.use {
-                it.prepareStatement("SELECT * FROM sak WHERE id = ${opprettSak.id}")
-                    .executeQuery().singleOrNull {
-                        objectMapper.readValue(getString("flyktning"))
-                    }
-            }
+        val oppdatertFlyktning = sakRepo.finnFlyktningForSak(opprettSak.id)
 
         Assertions.assertEquals(flyktning, oppdatertFlyktning)
     }

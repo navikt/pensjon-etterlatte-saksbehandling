7fbe26712ec295d546a0b33ae00991be96b4ddbc
Chris Searle <chris@chrissearle.org>
2023-12-13 13:18:18 +0100

    Mini-endring for å trigge tester

--- libs/saksbehandling-common/src/main/kotlin/EnvUtils.kt
+++ libs/saksbehandling-common/src/main/kotlin/EnvUtils.kt
@@ -2,7 +2,7 @@ package no.nav.etterlatte.libs.common
 
 fun Map<String, String>.requireEnvValue(key: String) =
     when (val value = this[key]) {
-        null -> throw IllegalArgumentException("app env is missing required key $key")
+        null -> throw IllegalArgumentException("App env is missing required key $key")
         else -> value
     }
 

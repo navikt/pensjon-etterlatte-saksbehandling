fb954f4b5815024130771ffade855644782a6171
Bjørn Golberg <bjorn.golberg@nav.no>
2023-12-15 13:53:33 +0100

    Manuell migrering - Mulig å overstyre enhet

--- apps/etterlatte-behandling/src/test/kotlin/behandling/BehandlingFactoryTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/BehandlingFactoryTest.kt
@@ -622,6 +622,7 @@ class BehandlingFactoryTest {
                         "nb",
                         Vedtaksloesning.GJENNY,
                         null,
+                        null,
                     ),
                 )
             }

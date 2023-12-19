f66126117cab3d0b7432c507e6c7744c0230c8d0
Nicolas N <Watercolours@users.noreply.github.com>
2023-12-19 10:14:11 +0100

    Fjerne mock router (#3220)
    * Fjerne mock server
    
    * fjerne mockdata json

--- apps/etterlatte-saksbehandling-ui/server/src/index.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from 'express'
 import path from 'path'
 import { ApiConfig, appConf, ClientConfig } from './config/config'
 import { authenticateUser } from './middleware/auth'
-import { mockRouter } from './routers/mockRouter'
 import { logger } from './monitoring/logger'
 import { requestLogger } from './middleware/logging'
 import { tokenMiddleware } from './middleware/getOboToken'
@@ -32,55 +31,49 @@ app.get('/metrics', async (_: Request, res: Response) => {
   res.end(await prometheus.register.metrics())
 })
 
-if (isDev) {
-  logger.info('Mocking all endpoints')
-  app.use('/api', mockRouter)
-} else {
-  app.use('/api/logg', loggerRouter)
+app.use('/api/logg', loggerRouter)
+app.use('/api/feature', unleashRouter)
 
-  app.use('/api/feature', unleashRouter)
+app.use(authenticateUser) // Alle ruter etter denne er authenticated
+app.use('/api/innlogget', innloggetBrukerRouter)
+app.use('/api/norg2', norg2Router)
 
-  app.use(authenticateUser) // Alle ruter etter denne er authenticated
-  app.use('/api/innlogget', innloggetBrukerRouter)
-  app.use('/api/norg2', norg2Router)
+app.use(
+  '/api/vilkaarsvurdering',
+  tokenMiddleware(ApiConfig.vilkaarsvurdering.scope),
+  proxy(ApiConfig.vilkaarsvurdering.url)
+)
 
-  app.use(
-    '/api/vilkaarsvurdering',
-    tokenMiddleware(ApiConfig.vilkaarsvurdering.scope),
-    proxy(ApiConfig.vilkaarsvurdering.url)
-  )
+app.use(
+  [
+    '/api/behandling',
+    '/api/behandlinger/:sakid/manueltopphoer',
+    '/api/personer',
+    '/api/revurdering/:sakid',
+    '/api/stoettederevurderinger',
+    '/api/grunnlagsendringshendelse/:sakid/institusjon',
+    '/api/sak/:sakid',
+    '/api/institusjonsoppholdbegrunnelse/:sakid',
+    '/api/oppgaver',
+    '/api/klage',
+    '/api/tilbakekreving',
+    '/api/generellbehandling',
+    '/api/sjekkliste',
+    '/api/bosattutland',
+  ],
+  tokenMiddleware(ApiConfig.behandling.scope),
+  proxy(ApiConfig.behandling.url)
+)
 
-  app.use(
-    [
-      '/api/behandling',
-      '/api/behandlinger/:sakid/manueltopphoer',
-      '/api/personer',
-      '/api/revurdering/:sakid',
-      '/api/stoettederevurderinger',
-      '/api/grunnlagsendringshendelse/:sakid/institusjon',
-      '/api/sak/:sakid',
-      '/api/institusjonsoppholdbegrunnelse/:sakid',
-      '/api/oppgaver',
-      '/api/klage',
-      '/api/tilbakekreving',
-      '/api/generellbehandling',
-      '/api/sjekkliste',
-      '/api/bosattutland',
-    ],
-    tokenMiddleware(ApiConfig.behandling.scope),
-    proxy(ApiConfig.behandling.url)
-  )
+app.use('/api/grunnlag', tokenMiddleware(ApiConfig.grunnlag.scope), proxy(ApiConfig.grunnlag.url))
 
-  app.use('/api/grunnlag', tokenMiddleware(ApiConfig.grunnlag.scope), proxy(ApiConfig.grunnlag.url))
+app.use('/api/beregning', tokenMiddleware(ApiConfig.beregning.scope), proxy(ApiConfig.beregning.url))
 
-  app.use('/api/beregning', tokenMiddleware(ApiConfig.beregning.scope), proxy(ApiConfig.beregning.url))
+app.use('/api/vedtak', tokenMiddleware(ApiConfig.vedtak.scope), proxy(ApiConfig.vedtak.url))
 
-  app.use('/api/vedtak', tokenMiddleware(ApiConfig.vedtak.scope), proxy(ApiConfig.vedtak.url))
+app.use('/api/trygdetid', tokenMiddleware(ApiConfig.trygdetid.scope), proxy(ApiConfig.trygdetid.url))
 
-  app.use('/api/trygdetid', tokenMiddleware(ApiConfig.trygdetid.scope), proxy(ApiConfig.trygdetid.url))
-
-  app.use(['/api/brev', '/api/dokumenter'], tokenMiddleware(ApiConfig.brev.scope), proxy(ApiConfig.brev.url))
-}
+app.use(['/api/brev', '/api/dokumenter'], tokenMiddleware(ApiConfig.brev.scope), proxy(ApiConfig.brev.url))
 
 app.get('/api/config', (_req: Request, res: Response) => res.send(ClientConfig))
 

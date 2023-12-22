8a421704d523adeb4e84e2e10d72ee755ac08df5
henrikg <henrik.gundersen@nav.no>
2023-12-22 12:57:45 +0100

    Fjerner placeholder-tekst

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/bp/VedleggBP.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/bp/VedleggBP.kt
@@ -2,7 +2,7 @@ package no.nav.etterlatte.brev.model.bp
 
 import no.nav.etterlatte.brev.model.BrevInnholdVedlegg
 import no.nav.etterlatte.brev.model.BrevVedleggKey
-import no.nav.etterlatte.brev.model.SlateHelper
+import no.nav.etterlatte.brev.model.Slate
 
 class VedleggBP {
     companion object {
@@ -12,7 +12,7 @@ class VedleggBP {
             BrevInnholdVedlegg(
                 tittel = "Trygdetid i vedlegg beregning av barnepensjon",
                 key = BrevVedleggKey.BP_BEREGNING_TRYGDETID,
-                payload = SlateHelper.getSlate("/maler/vedlegg/bp_beregning_trygdetid.json"),
+                payload = Slate(),
             )
     }
 }

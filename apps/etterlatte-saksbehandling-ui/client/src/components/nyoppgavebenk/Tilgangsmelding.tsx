74a80402c7906324fa74a48203dc7156c7a16315
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-12-22 10:05:35 +0100

    Litt styling

--- apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/Tilgangsmelding.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/nyoppgavebenk/Tilgangsmelding.tsx
@@ -1,17 +1,23 @@
 import { useAppSelector } from '~store/Store'
 import { Alert, GuidePanel } from '@navikt/ds-react'
+import { Container } from '~shared/styled'
 
 export const Tilgangsmelding = () => {
   const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
 
   if (innloggetSaksbehandler.leseTilgang) {
     return (
-      <GuidePanel>
-        Vi ser at du har lesetilgang til systemet. For å søke opp personer eller saker vennligst benytt søkefelt i
-        hjørnet.
-      </GuidePanel>
+      <Container>
+        <GuidePanel>
+          Du har lesetilgang til systemet. For å søke opp personer eller saker vennligst benytt søkefelt i hjørnet.
+        </GuidePanel>
+      </Container>
     )
   } else {
-    return <Alert variant="error">Det er ikke registrert lese eller skrivetilgang på deg.</Alert>
+    return (
+      <Container>
+        <Alert variant="error">Det er ikke registrert lese eller skrivetilgang på deg.</Alert>
+      </Container>
+    )
   }
 }

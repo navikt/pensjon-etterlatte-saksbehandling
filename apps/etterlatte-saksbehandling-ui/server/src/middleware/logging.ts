a1e7c8e45b0fd3578e23c2f0e2ab489c0c4c10e7
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-01-05 13:43:04 +0100

    Feature/logg request time frackend (#3325)
    * Logg request time frackend
    
    * Forbedring og legge det som key til logginslagg i logstash
    
    * legg det til requestLoggerMiddleware
    
    * Endre til responsetime

--- apps/etterlatte-saksbehandling-ui/server/src/middleware/logging.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/middleware/logging.ts
@@ -2,11 +2,20 @@ import { NextFunction, Request, Response } from 'express'
 import { logger } from '../monitoring/logger'
 import { sanitize, sanitizeUrl } from '../utils/sanitize'
 
-const kanLoggeRequest = (input: string | undefined) => !/(health|metrics)/.test(input ?? '')
+export const kanLoggeRequest = (input: string | undefined) => !/(health|metrics)/.test(input ?? '')
 
-export const requestLoggerMiddleware = (req: Request, _: Response, next: NextFunction) => {
+export const requestLoggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (kanLoggeRequest(req.url)) {
-    logger.info(`${sanitize(req.method)} ${sanitizeUrl(req.url)}`)
+    res.locals.start = Date.now()
+    res.on('finish', function () {
+      const response_time = Date.now() - res.locals.start
+      if (kanLoggeRequest(req.url)) {
+        logger.info({
+          message: `${sanitize(req.method)} ${sanitizeUrl(req.url)} ms: ${response_time}`,
+          ...(response_time && { x_response_time: response_time }),
+        })
+      }
+    })
   }
 
   next()

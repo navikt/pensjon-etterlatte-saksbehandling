847dda56f2b53da604eb1c99c90d7bf26675b56a
Trond Valen <trond.valen@nav.no>
2024-01-08 15:49:51 +0100

    EY-2843: Slår sammen litt, så ikke reducer brukes i en vanlig funksjon.

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/GrunnlagForVirkningstidspunkt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/GrunnlagForVirkningstidspunkt.tsx
@@ -1,35 +1,27 @@
 import { useBehandling } from '~components/behandling/useBehandling'
 import { Info } from '~components/behandling/soeknadsoversikt/Info'
 import { formaterStringDato } from '~utils/formattering'
-import { ReactNode } from 'react'
 import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
 import { formaterGrunnlagKilde } from '~components/behandling/soeknadsoversikt/utils'
 
 export const GrunnlagForVirkningstidspunkt = () => {
   const behandling = useBehandling()
+  const personopplysninger = usePersonopplysninger()
+  const avdoede = personopplysninger?.avdoede || []
   return (
     <>
-      {doedsdatoer()}
+      {avdoede.map((avdod, index) => (
+        <Info
+          key={index}
+          label="Dødsdato"
+          tekst={avdod?.opplysning.doedsdato ? formaterStringDato(avdod?.opplysning.doedsdato) : 'Ikke registrert'}
+          undertekst={formaterGrunnlagKilde(avdod?.kilde)}
+        />
+      ))}
+      {avdoede.length == 0 && <Info key="manglerDoedsdato" label="Dødsdato" tekst="Ikke registrert" />}
       {behandling?.soeknadMottattDato && (
         <Info key="soeknadMottatt" label="Søknad mottatt" tekst={formaterStringDato(behandling.soeknadMottattDato)} />
       )}
     </>
   )
-}
-
-function doedsdatoer(): ReactNode[] {
-  const personopplysninger = usePersonopplysninger()
-  const avdoede = personopplysninger?.avdoede || []
-  const nodes = avdoede.map((avdod, index) => (
-    <Info
-      key={index}
-      label="Dødsdato"
-      tekst={avdod?.opplysning.doedsdato ? formaterStringDato(avdod?.opplysning.doedsdato) : 'Ikke registrert'}
-      undertekst={formaterGrunnlagKilde(avdod?.kilde)}
-    />
-  ))
-  if (!avdoede.find((it) => it.opplysning.doedsdato)) {
-    nodes.push(<Info key="manglerDoedsdato" label="Dødsdato" tekst="Ikke registrert" />)
-  }
-  return nodes
 }

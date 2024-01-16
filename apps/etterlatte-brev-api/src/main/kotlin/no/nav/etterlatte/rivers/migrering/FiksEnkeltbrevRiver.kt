15587b5036a220ab9275f57f452eb146b632af32
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-01-16 09:15:57 +0100

    Send ut migreringsbrev som er oppretta, men ikkje journalført og distribuert (#3411)

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/migrering/FiksEnkeltbrevRiver.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/migrering/FiksEnkeltbrevRiver.kt
@@ -1,11 +1,7 @@
 package no.nav.etterlatte.rivers.migrering
 
 import kotlinx.coroutines.runBlocking
-import no.nav.etterlatte.brev.MigreringBrevRequest
-import no.nav.etterlatte.brev.VedtaksbrevService
 import no.nav.etterlatte.brev.hentinformasjon.VedtaksvurderingService
-import no.nav.etterlatte.brev.model.Brev
-import no.nav.etterlatte.libs.common.behandling.UtlandstilknytningType
 import no.nav.etterlatte.libs.common.rapidsandrivers.eventName
 import no.nav.etterlatte.libs.common.retryOgPakkUt
 import no.nav.etterlatte.libs.common.vedtak.VedtakKafkaHendelseType
@@ -22,7 +18,6 @@ import rapidsandrivers.migrering.ListenerMedLoggingOgFeilhaandtering
 
 internal class FiksEnkeltbrevRiver(
     rapidsConnection: RapidsConnection,
-    private val service: VedtaksbrevService,
     private val vedtaksvurderingService: VedtaksvurderingService,
 ) : ListenerMedLoggingOgFeilhaandtering(BrevEventTypes.FIKS_ENKELTBREV.toString()) {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -30,7 +25,6 @@ internal class FiksEnkeltbrevRiver(
     init {
         initialiserRiver(rapidsConnection, Migreringshendelser.FIKS_ENKELTBREV) {
             validate { it.requireKey(BEHANDLING_ID_KEY) }
-            validate { it.requireKey(SUM) }
         }
     }
 
@@ -42,24 +36,7 @@ internal class FiksEnkeltbrevRiver(
         logger.info("Fikser vedtaksbrev for behandling $behandlingId")
 
         val brukerTokenInfo = Systembruker("migrering", "migrering")
-        val sum = packet[SUM].asInt()
-        val migreringBrevRequest =
-            MigreringBrevRequest(brutto = sum, yrkesskade = false, utlandstilknytningType = null)
         runBlocking {
-            val sakId = retryOgPakkUt { vedtaksvurderingService.hentVedtak(behandlingId, brukerTokenInfo).sak.id }
-            val vedtaksbrev: Brev =
-                retryOgPakkUt {
-                    service.opprettVedtaksbrev(
-                        sakId,
-                        behandlingId,
-                        brukerTokenInfo,
-                        migreringBrevRequest,
-                    )
-                }
-            retryOgPakkUt { service.genererPdf(vedtaksbrev.id, brukerTokenInfo, migreringBrevRequest) }
-            retryOgPakkUt { service.ferdigstillVedtaksbrev(behandlingId, brukerTokenInfo, true) }
-            logger.info("Har oppretta vedtaksbrev for behandling $behandlingId")
-
             packet.eventName = VedtakKafkaHendelseType.ATTESTERT.toString()
             val vedtak = retryOgPakkUt { vedtaksvurderingService.hentVedtak(behandlingId, brukerTokenInfo) }
             packet["vedtak"] = vedtak
@@ -68,10 +45,11 @@ internal class FiksEnkeltbrevRiver(
     }
 }
 
-const val SUM = "sum"
-
 val behandlingerAaJournalfoereBrevFor =
-    listOf<Triple<String, Int, UtlandstilknytningType?>>(
-        Triple("f2ca8e19-4c4f-4691-8521-f996a7f8c9ea", 3954, null),
-        Triple("16f36950-1e9d-4b52-a21c-26d229caaf28", 3954, null),
+    listOf(
+        "18118b9e-78ed-4d9a-b0de-b59c4a6bc061",
+        "81049810-a71f-4d72-84e8-d8c2343683ed",
+        "1d58ad45-d777-4fab-a92d-4e5e8f183850",
+        "1f902cb0-13ae-415b-968a-957c75dc3c3b",
+        "f54392e6-948b-4b31-b8f1-37fa249252f9",
     )

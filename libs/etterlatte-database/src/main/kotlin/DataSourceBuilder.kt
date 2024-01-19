c275fc965facb08645ee3a0809fe17cd16f2dfc0
Mads Opheim <mads.opheim@nav.no>
2024-01-19 15:52:52 +0100

    Kanskje dette er cluet

--- libs/etterlatte-database/src/main/kotlin/DataSourceBuilder.kt
+++ libs/etterlatte-database/src/main/kotlin/DataSourceBuilder.kt
@@ -2,6 +2,7 @@ package no.nav.etterlatte.libs.database
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import no.nav.etterlatte.libs.common.requireEnvValue
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.output.MigrateResult
 import org.slf4j.LoggerFactory
@@ -10,7 +11,22 @@ import javax.sql.DataSource
 object DataSourceBuilder {
     private const val MAX_POOL_SIZE = 10
 
-    fun createDataSource(env: Map<String, String>): DataSource = createDataSource(ApplicationProperties.fromEnv(env))
+    fun createDataSource(
+        env: Map<String, String>,
+        maxPoolSize: Int = MAX_POOL_SIZE,
+    ): DataSource {
+        val jdbcUrl =
+            jdbcUrl(
+                host = env.requireEnvValue("DB_HOST"),
+                port = env.requireEnvValue("DB_PORT").toInt(),
+                databaseName = env.requireEnvValue("DB_DATABASE"),
+            )
+        val username = env.requireEnvValue("DB_USERNAME")
+        val password = env.requireEnvValue("DB_PASSWORD")
+        return createDataSource(jdbcUrl, username, password, maxPoolSize)
+    }
+
+    fun createDataSource2(env: Map<String, String>): DataSource = createDataSource(ApplicationProperties.fromEnv(env))
 
     fun createDataSource(properties: ApplicationProperties) =
         createDataSource(

9bdd8cb59797e26c18e092fa8cdeeeefdb552102
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-01-19 14:55:22 +0100

    Formaterer scriptet litt mer leselig

--- apps/etterlatte-behandling/src/main/resources/db/migration/V86__avslutt_duplikate_hendelser.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V86__avslutt_duplikate_hendelser.sql
@@ -2,14 +2,22 @@
 
 -- Finner duplikate hendelser ved å se på row_num over partisjonen og bare de med row_num > 1
 create temporary view duplikate_grunnlagsendringshendelser AS
-WITH duplikate_ider AS (
-    SELECT id, ROW_NUMBER() over (PARTITION BY samsvar_mellom_pdl_og_grunnlag, sak_id, gjelder_person, hendelse_gjelder_rolle) as row_num
-    FROM grunnlagsendringshendelse
-    WHERE status = 'SJEKKET_AV_JOBB'
-    order by opprettet desc
-)
-SELECT id from duplikate_ider
-where row_num > 1;
+    (
+        WITH duplikate_ider AS (
+            SELECT id,
+                ROW_NUMBER() over (PARTITION BY
+                    samsvar_mellom_pdl_og_grunnlag,
+                    sak_id,
+                    gjelder_person,
+                    hendelse_gjelder_rolle) as row_num
+            FROM grunnlagsendringshendelse
+            WHERE status = 'SJEKKET_AV_JOBB'
+            order by opprettet desc
+        )
+    SELECT id
+    from duplikate_ider
+    where row_num > 1
+);
 
 -- setter de duplikate hendelsene til forkastet
 update grunnlagsendringshendelse
@@ -18,6 +26,10 @@ FROM duplikate_grunnlagsendringshendelser
 where grunnlagsendringshendelse.id = duplikate_grunnlagsendringshendelser.id;
 
 -- setter de duplikate oppgavene til avbrutt av EY med en merknad
-update oppgave set status='AVBRUTT', saksbehandler='EY', merknad='Lukket på grunn av duplikat'
+update oppgave
+set status='AVBRUTT',
+    saksbehandler='EY',
+    merknad='Lukket på grunn av duplikat'
 FROM duplikate_grunnlagsendringshendelser
-where oppgave.referanse = duplikate_grunnlagsendringshendelser.id::text and oppgave.kilde='HENDELSE';
+where oppgave.referanse = duplikate_grunnlagsendringshendelser.id::text
+  and oppgave.kilde = 'HENDELSE';

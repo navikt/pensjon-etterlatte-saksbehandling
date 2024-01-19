3c7653a4f0d349d576c96256a9d308ba0293c94c
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-01-19 16:18:11 +0100

    paralleliser bedre

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ setupTestLogging()
 fun Project.setupTestLogging() {
     for (sub in subprojects) {
         sub.tasks.withType<Test> {
+            maxParallelForks = Runtime.getRuntime().availableProcessors() / 2
             testLogging {
                 exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             }

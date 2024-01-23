0fa9ec682e4504bdfd2d3effeddc168d4a81e8f8
Mads Opheim <mads.opheim@nav.no>
2024-01-23 11:49:43 +0100

    Forenklar SQL-en

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/omregning/OmregningDao.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/omregning/OmregningDao.kt
@@ -5,7 +5,6 @@ import no.nav.etterlatte.libs.database.Transactions
 import no.nav.etterlatte.libs.database.hentListe
 import no.nav.etterlatte.libs.database.transaction
 import java.time.YearMonth
-import java.time.format.DateTimeFormatter
 import javax.sql.DataSource
 
 class OmregningDao(private val datasource: DataSource) : Transactions<OmregningDao> {
@@ -14,26 +13,29 @@ class OmregningDao(private val datasource: DataSource) : Transactions<OmregningD
             this.block(it)
         }
 
-    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-
     fun hentSoekereFoedtIEnGittMaaned(
         maaned: YearMonth,
         tx: TransactionalSession? = null,
     ): List<String> {
-        val aktuellMaaned = maaned.atDay(1).format(formatter).let { Integer.parseInt(it) }
-        val nesteMaaned = maaned.plusMonths(1).atDay(1).format(formatter).let { Integer.parseInt(it) }
         val sql =
             """
             select g.sak_id as sak_id from grunnlagshendelse g
                      inner join grunnlagshendelse g2 on g.sak_id = g2.sak_id and g.fnr = g2.fnr and g.hendelsenummer != g2.hendelsenummer
             where g.opplysning_type = 'FOEDSELSDATO'
             and g2.opplysning_type = 'SOEKER_PDL_V1'
-            and replace(replace(g.opplysning, '"', ''), '-', '') :: INTEGER >= :aktuellMaaned
-            and replace(replace(g.opplysning, '"', ''), '-', '') :: INTEGER < :nesteMaaned 
+            and TO_DATE(g.opplysning, '\"YYYY-MM-DD\"') BETWEEN :aktuellMaaned AND :nesteMaaned
             """.trimIndent()
 
         return tx.session {
-            hentListe(sql, { mapOf("aktuellMaaned" to aktuellMaaned, "nesteMaaned" to nesteMaaned) }) {
+            hentListe(
+                sql,
+                {
+                    mapOf(
+                        "aktuellMaaned" to maaned.atDay(1),
+                        "nesteMaaned" to maaned.plusMonths(1).atDay(1),
+                    )
+                },
+            ) {
                 it.string("sak_id")
             }
         }

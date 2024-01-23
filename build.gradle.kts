ec62832e7c5122cfc0c7f73b65c364774b6dece6
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-01-23 11:36:06 +0100

    Bruk alle kjerner hvis (#3510)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 setupTestLogging()
 
+fun hentAntallKjerner(): Int {
+    val isGithubWorkflow = System.getenv("CI")
+    if (isGithubWorkflow != null) {
+        return Runtime.getRuntime().availableProcessors()
+    }
+    return Runtime.getRuntime().availableProcessors() / 2
+}
+
 fun Project.setupTestLogging() {
     for (sub in subprojects) {
         sub.tasks.withType<Test> {
-            maxParallelForks = Runtime.getRuntime().availableProcessors() / 2
+            maxParallelForks = hentAntallKjerner()
             testLogging {
                 exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
             }

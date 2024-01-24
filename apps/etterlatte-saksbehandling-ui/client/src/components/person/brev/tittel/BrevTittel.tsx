3dc03edbe20ccae2ebf57057d97d280bc3f21fba
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-01-24 10:25:06 +0100

    Lag ny struktur for redigering av brev tittel

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/brev/tittel/BrevTittel.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/brev/tittel/BrevTittel.tsx
@@ -1,36 +1,58 @@
 import React, { useState } from 'react'
-import { Alert, BodyShort, Heading, Panel } from '@navikt/ds-react'
-import RedigerBrevTittelModal from '~components/person/brev/tittel/RedigerBrevTittelModal'
+import { Alert, BodyShort, Button, Heading, Panel } from '@navikt/ds-react'
+import { DocPencilIcon } from '@navikt/aksel-icons'
+import { BrevTittelModal } from '~components/person/brev/tittel/BrevTittelModal'
 
-export default function BrevTittel({ brevId, sakId, tittel }: { brevId: number; sakId: number; tittel: string }) {
+interface Props {
+  brevId: number
+  sakId: number
+  tittel: string
+}
+
+export default function BrevTittel({ brevId, sakId, tittel }: Props) {
   const [nyTittel, setNyTittel] = useState(tittel)
 
-  return (
-    <Panel border>
-      <Heading spacing level="2" size="medium">
-        Tittel
-        <RedigerBrevTittelModal
-          brevId={brevId}
-          sakId={sakId}
-          tittel={tittel}
-          oppdater={(tittel) => setNyTittel(tittel)}
-        />
-      </Heading>
+  const [erModalAapen, setErModalAapen] = useState<boolean>(false)
 
-      {nyTittel ? (
-        <>
-          <BodyShort spacing>{nyTittel}</BodyShort>
-          {tittel !== nyTittel && (
-            <Alert variant="success" size="small">
-              Tittel oppdatert!
-            </Alert>
-          )}
-        </>
-      ) : (
-        <Alert variant="warning" size="small">
-          Tittel mangler
-        </Alert>
-      )}
-    </Panel>
+  return (
+    <>
+      <Panel border>
+        <div>
+          <Heading spacing level="2" size="medium">
+            Tittel
+          </Heading>
+          <Button
+            variant="secondary"
+            onClick={() => setErModalAapen(true)}
+            icon={<DocPencilIcon />}
+            style={{ float: 'right' }}
+            size="small"
+          />
+        </div>
+
+        {nyTittel ? (
+          <>
+            <BodyShort spacing>{nyTittel}</BodyShort>
+            {tittel !== nyTittel && (
+              <Alert variant="success" size="small">
+                Tittel oppdatert!
+              </Alert>
+            )}
+          </>
+        ) : (
+          <Alert variant="warning" size="small">
+            Tittel mangler
+          </Alert>
+        )}
+      </Panel>
+      <BrevTittelModal
+        isOpen={erModalAapen}
+        setIsOpen={setErModalAapen}
+        nyTittel={nyTittel}
+        setNyTittel={setNyTittel}
+        brevId={brevId}
+        sakId={sakId}
+      />
+    </>
   )
 }

a0fb6387a1b5fd3275c90d6e953bcaa2445a89a8
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-01-25 13:00:17 +0100

    Intellij lyver til meg om at den er lagt til...

--- apps/etterlatte-statistikk/build.gradle.kts
+++ apps/etterlatte-statistikk/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":libs:saksbehandling-common"))
     implementation(project(":libs:etterlatte-jobs"))
     implementation(project(":libs:etterlatte-behandling-model"))
+    implementation(project(":libs:etterlatte-tilbakekreving-model"))
     implementation(project(":libs:etterlatte-beregning-model"))
     implementation(project(":libs:etterlatte-database"))
     implementation(project(":libs:etterlatte-ktor"))

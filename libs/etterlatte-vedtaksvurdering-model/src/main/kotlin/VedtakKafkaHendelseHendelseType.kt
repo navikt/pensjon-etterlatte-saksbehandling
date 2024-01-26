0e89755888e9f65c2100949da524b4e5eb9f0eab
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-01-26 16:12:18 +0100

    Fjerne ubrukte hendelse for vedtak

--- libs/etterlatte-vedtaksvurdering-model/src/main/kotlin/VedtakKafkaHendelseHendelseType.kt
+++ libs/etterlatte-vedtaksvurdering-model/src/main/kotlin/VedtakKafkaHendelseHendelseType.kt
@@ -3,8 +3,6 @@ package no.nav.etterlatte.libs.common.vedtak
 import no.nav.etterlatte.libs.common.rapidsandrivers.EventnameHendelseType
 
 enum class VedtakKafkaHendelseHendelseType : EventnameHendelseType {
-    VILKAARSVURDERT, // UBRUKT
-    BEREGNET, // UBRUKT
     FATTET,
     ATTESTERT,
     UNDERKJENT,

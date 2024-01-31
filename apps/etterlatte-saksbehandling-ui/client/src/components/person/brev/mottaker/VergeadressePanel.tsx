168906fbfbd67fd813a22848140a485f14538502
Nicolas N <Watercolours@users.noreply.github.com>
2024-01-31 15:02:07 +0100

    Hotfix: Vergeadresse kræsjer frontend (#3599)
    * Hotfix: Vergeadresse kræsjer frontend
    
    * Update apps/etterlatte-saksbehandling-ui/client/src/components/person/brev/mottaker/VergeadressePanel.tsx
    
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
    
    * opprydding
    
    ---------
    
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/brev/mottaker/VergeadressePanel.tsx
@@ -0,0 +1,49 @@
+import { Mottaker } from '~shared/types/Brev'
+import { formaterFnr } from '~utils/formattering'
+import React from 'react'
+import { Info } from '~components/behandling/soeknadsoversikt/Info'
+import { InfoWrapper } from '~components/behandling/soeknadsoversikt/styled'
+import { Grunnlagsopplysning } from '~shared/types/grunnlag'
+import { KildePersondata } from '~shared/types/kilde'
+import { mapSuccess, Result } from '~shared/api/apiUtils'
+
+interface Props {
+  vergeadresseResult: Result<Grunnlagsopplysning<Mottaker, KildePersondata>>
+}
+
+export const VergeadressePanel = ({ vergeadresseResult }: Props) =>
+  mapSuccess(vergeadresseResult, (grunnlagsopplysning) => {
+    const vergeadresse = grunnlagsopplysning?.opplysning
+
+    if (!vergeadresse) {
+      return null
+    }
+
+    return (
+      <InfoWrapper>
+        <Info
+          wide
+          label="Verges adresse"
+          tekst={
+            <>
+              {!vergeadresse.foedselsnummer && !vergeadresse.orgnummer && `Fødselsnummer/orgnummer: Ikke registrert.`}
+              {!vergeadresse.foedselsnummer && !vergeadresse.orgnummer && <br />}
+              {vergeadresse.foedselsnummer && `Fødselsnummer: ${formaterFnr(vergeadresse.foedselsnummer.value)}`}
+              {vergeadresse.foedselsnummer && <br />}
+              {vergeadresse.navn}
+              {vergeadresse.navn && <br />}
+              {vergeadresse.adresse.adresselinje1}
+              {vergeadresse.adresse.adresselinje1 && <br />}
+              {vergeadresse.adresse.adresselinje2}
+              {vergeadresse.adresse.adresselinje2 && <br />}
+              {vergeadresse.adresse.adresselinje3}
+              {vergeadresse.adresse.adresselinje3 && <br />}
+              {vergeadresse.adresse.postnummer} {vergeadresse.adresse.poststed} {vergeadresse.adresse.land} (
+              {vergeadresse.adresse.landkode})
+            </>
+          }
+        />
+        <br />
+      </InfoWrapper>
+    )
+  })

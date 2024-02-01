2139b93607da18476c91c78387bea79dbdb890a8
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-01 09:16:04 +0100

    Nitpick testeer

--- apps/etterlatte-behandling/src/test/kotlin/BehandlingIntegrationTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/BehandlingIntegrationTest.kt
@@ -23,6 +23,7 @@ import no.nav.etterlatte.behandling.klienter.GrunnlagKlient
 import no.nav.etterlatte.behandling.klienter.NavAnsattKlient
 import no.nav.etterlatte.behandling.klienter.Norg2Klient
 import no.nav.etterlatte.behandling.klienter.OpprettetBrevDto
+import no.nav.etterlatte.behandling.klienter.SaksbehandlerInfo
 import no.nav.etterlatte.behandling.klienter.VedtakKlient
 import no.nav.etterlatte.common.Enheter
 import no.nav.etterlatte.config.ApplicationContext
@@ -582,5 +583,9 @@ class NavAnsattKlientTest : NavAnsattKlient {
 
     override suspend fun hentTemaForSaksbehandler(ident: String): List<SaksbehandlerTema> {
         return listOf(SaksbehandlerTema(SakType.BARNEPENSJON.name), SaksbehandlerTema(SakType.OMSTILLINGSSTOENAD.name))
+    }
+
+    override suspend fun hentSaksbehanderNavn(ident: String): SaksbehandlerInfo? {
+        return SaksbehandlerInfo("ident", "Max Manus")
     }
 }

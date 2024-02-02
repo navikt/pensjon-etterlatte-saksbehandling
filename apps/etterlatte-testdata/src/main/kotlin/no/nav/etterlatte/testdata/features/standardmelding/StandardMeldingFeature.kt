f06e2f7a125958f49ebf3c575f0257265b597818
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-02 14:39:05 +0100

    Standardiser småsteder der det manglet litt

--- apps/etterlatte-testdata/src/main/kotlin/no/nav/etterlatte/testdata/features/standardmelding/StandardMeldingFeature.kt
+++ apps/etterlatte-testdata/src/main/kotlin/no/nav/etterlatte/testdata/features/standardmelding/StandardMeldingFeature.kt
@@ -9,6 +9,7 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import no.nav.etterlatte.TestDataFeature
 import no.nav.etterlatte.kafka.KafkaProdusent
+import no.nav.etterlatte.libs.common.event.SoeknadInnsendtHendelseType
 import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.common.tidspunkt.toLocalDatetimeUTC
 import no.nav.etterlatte.logger
@@ -62,7 +63,7 @@ private fun createRecord(input: String): Pair<String, String> {
     val message =
         JsonMessage.newMessage(
             mapOf(
-                "@event_name" to "trenger_behandling",
+                "@event_name" to SoeknadInnsendtHendelseType.EVENT_NAME_BEHANDLINGBEHOV.lagEventnameForType(),
                 "@skjema_info" to objectMapper.readValue<ObjectNode>(input),
                 "@lagret_soeknad_id" to "TEST-${UUID.randomUUID()}",
                 "@template" to "soeknad",

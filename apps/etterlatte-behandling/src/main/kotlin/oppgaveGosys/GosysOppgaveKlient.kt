1134dbb553a5836683d2376ed390664f69b6d0ca
Nicolas N <nicolas.nordhagen@nav.no>
2024-02-06 13:25:45 +0100

    Oppdatere GosysOppgave iht. manuell journalføring

--- apps/etterlatte-behandling/src/main/kotlin/oppgaveGosys/GosysOppgaveKlient.kt
+++ apps/etterlatte-behandling/src/main/kotlin/oppgaveGosys/GosysOppgaveKlient.kt
@@ -29,7 +29,7 @@ data class GosysApiOppgave(
     val aktoerId: String?,
     val beskrivelse: String,
     val status: String,
-    val fristFerdigstillelse: LocalDate,
+    val fristFerdigstillelse: LocalDate? = null,
 )
 
 data class GosysEndreSaksbehandlerRequest(
@@ -90,7 +90,6 @@ class GosysOppgaveKlientImpl(config: Config, httpClient: HttpClient) : GosysOppg
                     .plus("&tema=EYO")
                     .plus("&limit=1000")
                     .plus(enhetsnr?.let { "&tildeltEnhetsnr=$it" } ?: "")
-//                .plus("&tilordnetRessurs=${brukerTokenInfo.ident()}")
 
             return downstreamResourceClient
                 .get(

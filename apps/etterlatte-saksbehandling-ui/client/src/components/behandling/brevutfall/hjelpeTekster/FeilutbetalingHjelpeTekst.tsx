5929f181786fd0e3cc6890ee7ec9b82007c0cff0
Espen Schei <espen.schei@nav.no>
2024-02-06 10:42:14 +0100

    Legger til infotekst
    EY-3359

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brevutfall/hjelpeTekster/FeilutbetalingHjelpeTekst.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brevutfall/hjelpeTekster/FeilutbetalingHjelpeTekst.tsx
@@ -6,7 +6,12 @@ export const FeilutbetalingHjelpeTekst = (): ReactNode => {
   return (
     <HjelpeTekst>
       Medfører revurderingen en feilutbetaling?
-      <HelpText strategy="fixed">todo: passende hjelpetekst</HelpText>
+      <HelpText strategy="fixed">
+        Hvis ja: Vurder om det skal sendes varsel om eventuell tilbakekreving (TK) av feilutbetalingen (FU). Velger du
+        &quot;ja, det skal sendes varsel&quot; blir varselet vedlagt revurderingsvedtaket. I noen tilfeller skal ikke FU
+        kreves tilbakebetalt fordi beløpet er under 4 rettsgebyr, jf. folketrygdloven § 22-15 sjette ledd. Vurder nøye.
+        Dette valget gir tekst i revurderingsvedtaket om at FU ikke blir krevd tilbakebetalt.
+      </HelpText>
     </HjelpeTekst>
   )
 }

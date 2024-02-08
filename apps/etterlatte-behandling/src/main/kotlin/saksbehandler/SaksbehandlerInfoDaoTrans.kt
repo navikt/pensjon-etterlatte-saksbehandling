57580c385213342a5d52948b27cfbf746b44ff84
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2024-02-08 08:49:56 +0100

    Fiks på at kun saksbehandler identer blir gitt på henting basert på enhet (#3709)
    * Fix
    
    * Fix test

--- apps/etterlatte-behandling/src/main/kotlin/saksbehandler/SaksbehandlerInfoDaoTrans.kt
+++ apps/etterlatte-behandling/src/main/kotlin/saksbehandler/SaksbehandlerInfoDaoTrans.kt
@@ -1,22 +1,26 @@
 package no.nav.etterlatte.saksbehandler
 
+import no.nav.etterlatte.behandling.klienter.SaksbehandlerInfo
 import no.nav.etterlatte.libs.database.setJsonb
 import no.nav.etterlatte.libs.database.toList
 import java.sql.Connection
 
 class SaksbehandlerInfoDaoTrans(private val connection: () -> Connection) {
-    fun hentSaksbehandlereForEnhet(enhet: String): List<String> {
+    fun hentSaksbehandlereForEnhet(enhet: String): List<SaksbehandlerInfo> {
         with(connection()) {
             val statement =
                 prepareStatement(
                     """
-                    SELECT id from saksbehandler_info
+                    SELECT id, navn from saksbehandler_info
                     where enheter @> ?::JSONB
                     """.trimIndent(),
                 )
             statement.setJsonb(1, enhet)
             return statement.executeQuery().toList {
-                getString("id")
+                SaksbehandlerInfo(
+                    getString("id"),
+                    getString("navn"),
+                )
             }
         }
     }

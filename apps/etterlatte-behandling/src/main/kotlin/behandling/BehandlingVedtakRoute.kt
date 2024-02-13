ea7d0ff88088caf94da0ab8168f576c10cfaa9f0
Nicolas N <nicolas.nordhagen@nav.no>
2024-02-13 16:09:27 +0100

    Hotfix: Oppdatere oppgavesaksbehandler

--- apps/etterlatte-behandling/src/main/kotlin/behandling/BehandlingVedtakRoute.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/BehandlingVedtakRoute.kt
@@ -103,10 +103,10 @@ internal fun Route.behandlingVedtakRoute(
                                     merknad = merknadFraAttestant,
                                     saksbehandler = brukerTokenInfo,
                                 )
-                            if (sisteSaksbehandlerIkkeAttestering.saksbehandlerIdent != null) {
+                            if (sisteSaksbehandlerIkkeAttestering.ident != null) {
                                 oppgaveService.tildelSaksbehandler(
                                     ferdigstillOppgaveUnderbehandlingOgLagNyMedType.id,
-                                    sisteSaksbehandlerIkkeAttestering.saksbehandlerIdent!!,
+                                    sisteSaksbehandlerIkkeAttestering.ident!!,
                                 )
                             } else {
                                 logger.warn(

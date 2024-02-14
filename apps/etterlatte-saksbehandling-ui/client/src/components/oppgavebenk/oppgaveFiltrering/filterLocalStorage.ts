0a50bed62147590949a41fff0ea4ca9670e59ec7
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-02-14 15:58:45 +0100

    Legg tilbake filter error catching som forsvant...

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveFiltrering/filterLocalStorage.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveFiltrering/filterLocalStorage.ts
@@ -1,11 +1,29 @@
 import { Filter, initialFilter } from '~components/oppgavebenk/oppgaveFiltrering/oppgavelistafiltre'
+import { logger } from '~utils/logger'
 
 const FILTER_KEY = 'filter'
 
 export const leggFilterILocalStorage = (filter: Filter) => localStorage.setItem(FILTER_KEY, JSON.stringify(filter))
 
 export const hentFilterFraLocalStorage = (): Filter => {
-  const filter = localStorage[FILTER_KEY]
-  if (!!filter) return JSON.parse(filter)
-  else return initialFilter()
+  try {
+    const filterFraLocalStorage = localStorage[FILTER_KEY]
+
+    if (!!filterFraLocalStorage) {
+      const parsetFilter = JSON.parse(filterFraLocalStorage)
+      // TODO: quickfix etter endring i type i localstorage, fjernes etter at
+      //  en viss tid, slik at alle saksbehandlere har kommet seg bort fra den gamel typen
+      return {
+        ...parsetFilter,
+        fristSortering: parsetFilter.fristSortering === 'ingen' ? 'no-order' : parsetFilter.fristSortering,
+        fnrSortering: parsetFilter.fnrSortering === 'ingen' ? 'no-order' : parsetFilter.fnrSortering,
+      }
+    } else {
+      return initialFilter()
+    }
+  } catch (error) {
+    logger.generalError({ message: 'Feil i hentingen av filter fra localstorage' })
+
+    return initialFilter()
+  }
 }

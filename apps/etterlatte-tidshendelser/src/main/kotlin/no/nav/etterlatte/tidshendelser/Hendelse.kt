88113a917576e23a148b7212c9cf9858e364c0bc
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-02-14 15:27:45 +0100

    EY-3495 Aldersovergang barnepensjon ved 21 år (#3803)
    * EY-3495 Jobb for aldersovergang barnepensjon ved 21 år
    
    * EY-3495 finne behandlingsid mtp yrkesskade-sjekk kun ved barnepensjon ved 20 år

--- apps/etterlatte-tidshendelser/src/main/kotlin/no/nav/etterlatte/tidshendelser/Hendelse.kt
+++ apps/etterlatte-tidshendelser/src/main/kotlin/no/nav/etterlatte/tidshendelser/Hendelse.kt
@@ -32,6 +32,7 @@ data class Hendelse(
 enum class JobbType(val beskrivelse: String) {
     AO_BP18("Aldersovergang barnepensjon ved 18 år"),
     AO_BP20("Aldersovergang barnepensjon ved 20 år"),
+    AO_BP21("Aldersovergang barnepensjon ved 21 år"),
 }
 
 enum class JobbStatus {

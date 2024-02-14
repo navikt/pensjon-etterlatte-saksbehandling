f3f6cc7c1661ec8b0a4582c2acd2f5f2a2577999
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-14 12:42:53 +0100

    EY-3480 Legg til test

--- apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/DoedshendelseJobService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/DoedshendelseJobService.kt
@@ -3,7 +3,6 @@ package no.nav.etterlatte.grunnlagsendring.doedshendelse
 import no.nav.etterlatte.behandling.domain.GrunnlagsendringsType
 import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
 import no.nav.etterlatte.grunnlagsendring.GrunnlagsendringshendelseService
-import no.nav.etterlatte.libs.common.tidspunkt.toLocalDatetimeUTC
 import no.nav.etterlatte.libs.common.tidspunkt.toTidspunkt
 import org.slf4j.LoggerFactory
 import java.time.Duration
@@ -26,14 +25,10 @@ class DoedshendelseJobService(
         }
     }
 
-    internal fun hendelserErGyldige(hendelser: List<Doedshendelse>): List<Doedshendelse> {
+    private fun hendelserErGyldige(hendelser: List<Doedshendelse>): List<Doedshendelse> {
         val idag = LocalDateTime.now()
-        val treDagerSiden = idag.minusDays(3L).toTidspunkt()
         return hendelser.filter {
-            val opprettetFor3dagerSiden = it.opprettet.toLocalDatetimeUTC().minusDays(3L)
-            opprettetFor3dagerSiden.isBefore(
-                treDagerSiden.toLocalDatetimeUTC(),
-            ) || Duration.between(it.opprettet, idag.toTidspunkt()).toDays() > 3L
+            Duration.between(it.opprettet, idag.toTidspunkt()).toDays() >= 3L
         }.also { logger.info("Antall gyldige dødsmeldinger ${it.size}") }
     }
 
---
+++ apps/etterlatte-behandling/src/test/kotlin/grunnlagsendring/doedshendelse/DoedshendelseJobServiceTest.kt
@@ -0,0 +1,40 @@
+package no.nav.etterlatte.grunnlagsendring.doedshendelse
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
+import no.nav.etterlatte.grunnlagsendring.GrunnlagsendringshendelseService
+import no.nav.etterlatte.libs.common.tidspunkt.toTidspunkt
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DoedshendelseJobServiceTest {
+    private val dao = mockk<DoedshendelseDao>()
+    private val toggle =
+        mockk<FeatureToggleService> {
+            every { isEnabled(any(), any()) } returns true
+        }
+    private val grunnlagsendringshendelseService = mockk<GrunnlagsendringshendelseService>()
+    private val service = DoedshendelseJobService(dao, toggle, grunnlagsendringshendelseService)
+
+    @Test
+    fun `skal kjøre 1 ny gyldig hendelse og droppe 1`() {
+        val doedshendelse =
+            Doedshendelse.nyHendelse(
+                avdoedFnr = "12345678901",
+                avdoedDoedsdato = LocalDate.now(),
+                beroertFnr = "12345678901",
+                relasjon = Relasjon.BARN,
+            )
+        val doedshendelser = listOf(doedshendelse, doedshendelse.copy(opprettet = LocalDateTime.now().minusDays(3L).toTidspunkt()))
+        every { dao.hentDoedshendelserMedStatus(any()) } returns doedshendelser
+        every { grunnlagsendringshendelseService.opprettHendelseAvTypeForPerson(any(), any()) } returns emptyList()
+        service.run()
+
+        verify { grunnlagsendringshendelseService.opprettHendelseAvTypeForPerson(any(), any()) }
+    }
+}

061ed63d8cb978fc66402c00942eac6014570112
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2024-02-14 09:13:53 +0100

    Legger til tilgangssjekk for behandlinger med klageId (#3773)
    * Legger til søk på saker relatert til klageId
    
    * Faktisk legger ved id i spørringen
    
    * Test for tilgangssjekk for klageId som behandlingId
    
    * Endrer oppsett av databaseconnection for tilgangservicetest

--- apps/etterlatte-behandling/src/test/kotlin/adressebeskyttelse/TilgangServiceTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/adressebeskyttelse/TilgangServiceTest.kt
@@ -5,11 +5,15 @@ import io.mockk.mockk
 import no.nav.etterlatte.DatabaseExtension
 import no.nav.etterlatte.behandling.BehandlingDao
 import no.nav.etterlatte.behandling.BrukerService
+import no.nav.etterlatte.behandling.klage.KlageDao
+import no.nav.etterlatte.behandling.klage.KlageDaoImpl
 import no.nav.etterlatte.behandling.kommerbarnettilgode.KommerBarnetTilGodeDao
 import no.nav.etterlatte.behandling.revurdering.RevurderingDao
 import no.nav.etterlatte.common.Enheter
 import no.nav.etterlatte.common.klienter.SkjermingKlient
 import no.nav.etterlatte.libs.common.behandling.BehandlingType
+import no.nav.etterlatte.libs.common.behandling.InnkommendeKlage
+import no.nav.etterlatte.libs.common.behandling.Klage
 import no.nav.etterlatte.libs.common.behandling.SakType
 import no.nav.etterlatte.libs.common.person.AdressebeskyttelseGradering
 import no.nav.etterlatte.libs.testdata.grunnlag.AVDOED_FOEDSELSNUMMER
@@ -29,6 +33,7 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import java.time.LocalDate
 import javax.sql.DataSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -38,6 +43,7 @@ internal class TilgangServiceTest(val dataSource: DataSource) {
     private lateinit var sakService: SakService
     private lateinit var sakRepo: SakDao
     private lateinit var behandlingRepo: BehandlingDao
+    private lateinit var klageDao: KlageDao
     private val strengtfortroligDev = "5ef775f2-61f8-4283-bf3d-8d03f428aa14"
     private val fortroligDev = "ea930b6b-9397-44d9-b9e6-f4cf527a632a"
     private val egenAnsattDev = "dbe4ad45-320b-4e9a-aaa1-73cca4ee124d"
@@ -46,17 +52,19 @@ internal class TilgangServiceTest(val dataSource: DataSource) {
 
     @BeforeAll
     fun beforeAll() {
+        val connection = dataSource.connection
         tilgangService = TilgangServiceImpl(SakTilgangDao(dataSource))
-        sakRepo = SakDao { dataSource.connection }
+        sakRepo = SakDao { connection }
 
         sakService = SakServiceImpl(sakRepo, skjermingKlient, brukerService)
         behandlingRepo =
             BehandlingDao(
                 KommerBarnetTilGodeDao {
-                    dataSource.connection
+                    connection
                 },
-                RevurderingDao { dataSource.connection },
-            ) { dataSource.connection }
+                RevurderingDao { connection },
+            ) { connection }
+        klageDao = KlageDaoImpl { connection }
     }
 
     @Test
@@ -83,6 +91,55 @@ internal class TilgangServiceTest(val dataSource: DataSource) {
             )
 
         Assertions.assertEquals(false, harTilgangTilBehandling)
+    }
+
+    @Test
+    fun `Skal sjekke tilganger til klager med klageId for behandlingId`() {
+        val fnr = AVDOED_FOEDSELSNUMMER.value
+        val sak = sakRepo.opprettSak(fnr, SakType.BARNEPENSJON, Enheter.defaultEnhet.enhetNr)
+        val jwtclaims = JWTClaimsSet.Builder().claim("groups", strengtfortroligDev).build()
+
+        val saksbehandlerMedStrengtfortrolig =
+            SaksbehandlerMedRoller(
+                Saksbehandler("", "ident", JwtTokenClaims(jwtclaims)),
+                mapOf(AzureGroup.STRENGT_FORTROLIG to strengtfortroligDev),
+            )
+        sakService.oppdaterAdressebeskyttelse(sak.id, AdressebeskyttelseGradering.STRENGT_FORTROLIG)
+        val klage =
+            Klage.ny(
+                sak,
+                InnkommendeKlage(
+                    mottattDato = LocalDate.of(2024, 2, 1),
+                    journalpostId = "123",
+                    innsender = null,
+                ),
+            )
+        klageDao.lagreKlage(klage)
+
+        val saksbehandlerUtenStrengtFortrolig =
+            SaksbehandlerMedRoller(
+                Saksbehandler("", "annenIdent", JwtTokenClaims(jwtclaims)),
+                mapOf(),
+            )
+        val harTilgangStrengtFortroligBehandling =
+            tilgangService.harTilgangTilBehandling(
+                klage.id.toString(),
+                saksbehandlerMedStrengtfortrolig,
+            )
+        val harTilgangStrengtFortroligKlage = tilgangService.harTilgangTilKlage(klage.id.toString(), saksbehandlerMedStrengtfortrolig)
+
+        Assertions.assertTrue(harTilgangStrengtFortroligKlage)
+        Assertions.assertTrue(harTilgangStrengtFortroligBehandling)
+
+        val harTilgangIkkeFortroligBehandling =
+            tilgangService.harTilgangTilBehandling(
+                klage.id.toString(),
+                saksbehandlerUtenStrengtFortrolig,
+            )
+        val harTilgangIkkeFortroligKlage = tilgangService.harTilgangTilKlage(klage.id.toString(), saksbehandlerUtenStrengtFortrolig)
+
+        Assertions.assertFalse(harTilgangIkkeFortroligBehandling)
+        Assertions.assertFalse(harTilgangIkkeFortroligKlage)
     }
 
     @Test

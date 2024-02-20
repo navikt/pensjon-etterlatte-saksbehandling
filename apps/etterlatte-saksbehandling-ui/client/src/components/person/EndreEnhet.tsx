0e8c5bd3cd224f54bda96271658f3bd27e5b4135
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-20 14:00:16 +0100

    Skjul endre enhet også med beskrivelse som ikke gir mening for andre

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/EndreEnhet.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/EndreEnhet.tsx
@@ -5,18 +5,12 @@ import { useApiCall } from '~shared/hooks/useApiCall'
 import { ButtonGroup } from '~components/person/VurderHendelseModal'
 import { isPending } from '~shared/api/apiUtils'
 import { byttEnhetPaaSak } from '~shared/api/sak'
-import { useAppSelector } from '~store/Store'
 
 export const EndreEnhet = ({ sakId }: { sakId: number }) => {
   const [error, setError] = useState<string | null>(null)
   const [open, setOpen] = useState(false)
   const [enhetsFilter, setEnhetsfilter] = useState<EnhetFilterKeys>('VELGENHET')
   const [endreEnhetStatus, endreEnhetKall, resetApiCall] = useApiCall(byttEnhetPaaSak)
-  const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
-
-  if (!innloggetSaksbehandler.skriveTilgang) {
-    return null
-  }
 
   function endreEnhet() {
     if (enhetsFilter === 'VELGENHET') {

96fd824467dab25f5172e719d2944baf38d607bb
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-20 22:25:59 +0100

    reset

--- apps/etterlatte-behandling/src/main/kotlin/config/ApplicationContext.kt
+++ apps/etterlatte-behandling/src/main/kotlin/config/ApplicationContext.kt
@@ -421,7 +421,7 @@ internal class ApplicationContext(
             doedshendelseJobService,
             { leaderElectionKlient.isLeader() },
             0L,
-            interval = if (isProd()) Duration.of(1, ChronoUnit.HOURS) else Duration.of(1, ChronoUnit.SECONDS),
+            interval = if (isProd()) Duration.of(1, ChronoUnit.HOURS) else Duration.of(1, ChronoUnit.MINUTES),
         )
     }
 

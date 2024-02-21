0edf546421bb746a46c98feb7200b08dfd21b4a2
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-02-21 15:49:52 +0100

    Fjern ubrukt metode

--- apps/etterlatte-saksbehandling-ui/client/src/shared/components/datoVelger/datoVelgerUtils.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/components/datoVelger/datoVelgerUtils.ts
@@ -2,6 +2,3 @@ import { format } from 'date-fns'
 
 export const formatDateToLocaleDateOrEmptyString = (date: Date | undefined) =>
   date === undefined ? '' : format(date, 'yyyy-MM-dd')
-
-export const formatDateToLocalDateTimeOrEmptyString = (date: Date | undefined) =>
-  date === undefined ? '' : format(date, "yyyy-MM-dd'T'HH:mm:ss")

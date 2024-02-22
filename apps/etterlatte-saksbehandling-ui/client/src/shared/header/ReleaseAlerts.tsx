3183c80e0c6dcf09ddcbea98ffa8fcafd640c682
Nicolas N <Watercolours@users.noreply.github.com>
2024-02-22 10:29:02 +0100

    EY-3524 Vise utgivelser til saksbehandler (#3845)
    * EY-3524 Vise utgivelser til saksbehandler
    
    * (i dag) var i feil tag

---
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/header/ReleaseAlerts.tsx
@@ -0,0 +1,121 @@
+import { BodyShort, Detail, Dropdown, Heading, InternalHeader, Label } from '@navikt/ds-react'
+import { BellFillIcon } from '@navikt/aksel-icons'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { useApiCall } from '~shared/hooks/useApiCall'
+import { hentUtgivelser, Release } from '~shared/api/github'
+import { isSuccess, mapApiResult } from '~shared/api/apiUtils'
+import { formaterDatoMedKlokkeslett } from '~utils/formattering'
+import { InfoElement } from '~components/behandling/soeknadsoversikt/styled'
+import { isToday } from 'date-fns'
+import { ApiErrorAlert } from '~ErrorBoundary'
+import Spinner from '~shared/Spinner'
+
+const KEY = 'release_id_list'
+const lagreLesteIder = (ider: number[]) => localStorage.setItem(KEY, JSON.stringify(ider))
+const hentLesteIder = (): number[] => {
+  try {
+    return JSON.parse(localStorage[KEY])
+  } catch {
+    return []
+  }
+}
+
+export const ReleaseAlerts = () => {
+  const [status, hentReleases] = useApiCall(hentUtgivelser)
+
+  const [utvigelser, setUtgivelser] = useState<Release[]>([])
+  const [antallUlest, setAntallUlest] = useState(0)
+
+  useEffect(() => {
+    hentReleases({}, (utgivelser) => {
+      const sett = hentLesteIder()
+
+      const antallUleste = utgivelser.filter((u) => !sett.includes(u.id)).length
+      setAntallUlest(antallUleste)
+
+      setUtgivelser(utgivelser)
+    })
+  }, [])
+
+  const open = (isOpen: boolean) => {
+    if (isOpen && isSuccess(status)) {
+      const lesteUtgivelser = utvigelser.map((u) => u.id)
+      lagreLesteIder(lesteUtgivelser)
+      setAntallUlest(0)
+    }
+  }
+
+  return (
+    <Dropdown onOpenChange={(isOpen) => open(isOpen)}>
+      <InternalHeader.Button as={Dropdown.Toggle}>
+        <div style={{ position: 'relative' }}>
+          <BellFillIcon style={{ fontSize: '1.5rem' }} />
+          {antallUlest > 0 && <UnreadCircle>{antallUlest}</UnreadCircle>}
+        </div>
+      </InternalHeader.Button>
+
+      <DropdownMenu>
+        {mapApiResult(
+          status,
+          <Spinner visible label="Henter siste utgivelser" />,
+          () => (
+            <ApiErrorAlert>Kunne ikke hente siste utgivelser</ApiErrorAlert>
+          ),
+          (utgivelser) => (
+            <>
+              <Heading size="small">Hva er nytt i Gjenny?</Heading>
+
+              {utgivelser.map((utgivelse, i) => (
+                <UtgivelseInfo key={`utgivelse-${i}`}>
+                  <InfoElement>
+                    <Label size="small" as="p">
+                      {utgivelse.name}
+                    </Label>
+                    <Detail spacing>
+                      {formaterDatoMedKlokkeslett(utgivelse.published_at)}{' '}
+                      {isToday(new Date(utgivelse.published_at)) && '(i dag)'}
+                    </Detail>
+                    <BodyShort as="div" style={{ whiteSpace: 'pre-wrap' }} size="small">
+                      {utgivelse.body}
+                    </BodyShort>
+                  </InfoElement>
+                </UtgivelseInfo>
+              ))}
+            </>
+          )
+        )}
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+const DropdownMenu = styled(Dropdown.Menu)`
+  width: 300px;
+`
+
+const UnreadCircle = styled.div`
+  width: 15px;
+  height: 15px;
+  padding: 10px;
+  background: var(--nav-error-border);
+  color: white;
+  font-size: 15px;
+  border-radius: 100%;
+
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  top: -10px;
+  right: -10px;
+`
+
+const UtgivelseInfo = styled.div`
+  padding: 1rem 0;
+
+  :not(:last-child) {
+    border-bottom: 1px solid gray;
+  }
+`

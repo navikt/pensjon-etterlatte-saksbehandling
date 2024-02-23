38240ea6389b0db6b5064c1a8efb714214544a35
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-02-23 12:55:58 +0100

    Sjekk for tilgang også ved Utsendingsinfo

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/dokumenter/VisDokument.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/dokumenter/VisDokument.tsx
@@ -3,12 +3,30 @@ import { Journalpost, Journalposttype } from '~shared/types/Journalpost'
 import { FlexRow } from '~shared/styled'
 import { UtsendingsinfoModal } from '~components/person/dokumenter/UtsendingsinfoModal'
 import DokumentModal from '~components/person/dokumenter/DokumentModal'
+import { Alert } from '@navikt/ds-react'
 
 export const VisDokument = ({ dokument }: { dokument: Journalpost }) => {
   return (
     <FlexRow justify="right">
-      {dokument.journalposttype === Journalposttype.U && <UtsendingsinfoModal journalpost={dokument} />}
-      <DokumentModal journalpost={dokument} />
+      {dokument.dokumenter.length === 1 ? (
+        <>
+          {dokument.dokumenter[0].dokumentvarianter[0].saksbehandlerHarTilgang ? (
+            <>
+              {dokument.journalposttype === Journalposttype.U && <UtsendingsinfoModal journalpost={dokument} />}
+              <DokumentModal journalpost={dokument} />
+            </>
+          ) : (
+            <Alert variant="warning" size="small">
+              Ingen tilgang
+            </Alert>
+          )}
+        </>
+      ) : (
+        <>
+          {dokument.journalposttype === Journalposttype.U && <UtsendingsinfoModal journalpost={dokument} />}
+          <DokumentModal journalpost={dokument} />
+        </>
+      )}
     </FlexRow>
   )
 }

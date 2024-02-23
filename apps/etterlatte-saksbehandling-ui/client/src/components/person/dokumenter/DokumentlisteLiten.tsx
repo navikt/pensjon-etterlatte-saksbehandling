b780732ef02b15d8f9ccd39ca4894350afe6591c
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-02-23 13:14:47 +0100

    Legge til warning for manglende tilgang ved liten dokumentliste

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/dokumenter/DokumentlisteLiten.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/dokumenter/DokumentlisteLiten.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Detail, Heading, Link } from '@navikt/ds-react'
+import { Alert, BodyShort, Detail, Heading, Link } from '@navikt/ds-react'
 import { formaterStringDato } from '~utils/formattering'
 import Spinner from '~shared/Spinner'
 import { ExternalLinkIcon } from '@navikt/aksel-icons'
@@ -6,6 +6,7 @@ import { Journalpost, Journalposttype } from '~shared/types/Journalpost'
 
 import { isPending, isSuccess, Result } from '~shared/api/apiUtils'
 import { isFailureHandler } from '~shared/api/IsFailureHandler'
+import { FlexRow } from '~shared/styled'
 
 export const DokumentlisteLiten = ({ dokumenter }: { dokumenter: Result<Journalpost[]> }) => (
   <>
@@ -20,24 +21,47 @@ export const DokumentlisteLiten = ({ dokumenter }: { dokumenter: Result<Journalp
         <div key={dokument.journalpostId}>
           {dokument.dokumenter.map((dokumentInfo) => (
             <BodyShort key={dokumentInfo.dokumentInfoId} as="div" size="small" spacing>
-              <Link
-                href={`/api/dokumenter/${dokument.journalpostId}/${dokumentInfo.dokumentInfoId}`}
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                {dokumentInfo.tittel}
-                <ExternalLinkIcon title={dokument.tittel} />
-              </Link>
-              <Detail>
-                {
-                  {
-                    [Journalposttype.I]: 'Avsender: ',
-                    [Journalposttype.U]: 'Mottaker: ',
-                    [Journalposttype.N]: 'Notat',
-                  }[dokument.journalposttype]
-                }
-                {dokument.avsenderMottaker.navn || 'Ukjent'} ({formaterStringDato(dokument.datoOpprettet)})
-              </Detail>
+              {!dokumentInfo.dokumentvarianter[0].saksbehandlerHarTilgang ? (
+                <>
+                  <Link
+                    href={`/api/dokumenter/${dokument.journalpostId}/${dokumentInfo.dokumentInfoId}`}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    {dokumentInfo.tittel}
+                    <ExternalLinkIcon aria-hidden title={dokument.tittel} />
+                  </Link>
+                  <Detail>
+                    {
+                      {
+                        [Journalposttype.I]: 'Avsender: ',
+                        [Journalposttype.U]: 'Mottaker: ',
+                        [Journalposttype.N]: 'Notat',
+                      }[dokument.journalposttype]
+                    }
+                    {dokument.avsenderMottaker.navn || 'Ukjent'} ({formaterStringDato(dokument.datoOpprettet)})
+                  </Detail>
+                </>
+              ) : (
+                <FlexRow justify="space-between">
+                  <div>
+                    {dokumentInfo.tittel}
+                    <Detail>
+                      {
+                        {
+                          [Journalposttype.I]: 'Avsender: ',
+                          [Journalposttype.U]: 'Mottaker: ',
+                          [Journalposttype.N]: 'Notat',
+                        }[dokument.journalposttype]
+                      }
+                      {dokument.avsenderMottaker.navn || 'Ukjent'} ({formaterStringDato(dokument.datoOpprettet)})
+                    </Detail>
+                  </div>
+                  <Alert variant="warning" size="small">
+                    Ingen tilgang
+                  </Alert>
+                </FlexRow>
+              )}
             </BodyShort>
           ))}
         </div>

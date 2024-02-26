0bc7446a672a065f0e561f05410733de70fe313e
Mads Opheim <mads.opheim@nav.no>
2024-02-26 11:57:00 +0100

    Test

--- apps/etterlatte-behandling/src/test/kotlin/oppgave/OppgaveRoutesTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/oppgave/OppgaveRoutesTest.kt
@@ -1,9 +1,11 @@
 package no.nav.etterlatte.oppgave
 
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
@@ -20,6 +22,9 @@ import no.nav.etterlatte.libs.common.oppgave.OppgaveIntern
 import no.nav.etterlatte.libs.common.oppgave.OppgaveKilde
 import no.nav.etterlatte.libs.common.oppgave.OppgaveListe
 import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import no.nav.etterlatte.libs.common.oppgave.SaksbehandlerEndringDto
+import no.nav.etterlatte.libs.common.oppgave.SettPaaVentRequest
+import no.nav.etterlatte.libs.common.oppgave.Status
 import no.nav.etterlatte.libs.common.sak.Sak
 import no.nav.etterlatte.libs.testdata.grunnlag.AVDOED_FOEDSELSNUMMER
 import no.nav.etterlatte.module
@@ -56,29 +61,77 @@ class OppgaveRoutesTest : BehandlingIntegrationTest() {
                     assertEquals(HttpStatusCode.OK, status)
                 }.body()
 
-            client.post("/oppgaver/sak/${sak.id}/opprett") {
-                val dto =
-                    NyOppgaveDto(OppgaveKilde.EKSTERN, OppgaveType.JOURNALFOERING, "Mottatt journalpost", "12345")
+            val oppgave =
+                client.post("/oppgaver/sak/${sak.id}/opprett") {
+                    val dto =
+                        NyOppgaveDto(OppgaveKilde.EKSTERN, OppgaveType.JOURNALFOERING, "Mottatt journalpost", "12345")
 
+                    addAuthToken(systemBruker)
+                    header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody(dto)
+                }.let {
+                    assertEquals(HttpStatusCode.OK, it.status)
+                    val lestOppgave: OppgaveIntern = it.body()
+                    assertEquals(fnr, lestOppgave.fnr)
+                    assertEquals("12345", lestOppgave.referanse)
+                    assertEquals(OppgaveType.JOURNALFOERING, lestOppgave.type)
+                    lestOppgave
+                }
+
+            hentOppgaver(client, sak, fnr)
+
+            client.post("/api/oppgaver/${oppgave.id}/tildel-saksbehandler") {
+                val dto = SaksbehandlerEndringDto(systemBruker)
                 addAuthToken(systemBruker)
                 header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
                 setBody(dto)
             }.also {
                 assertEquals(HttpStatusCode.OK, it.status)
-                val lestOppgave: OppgaveIntern = it.body()
-                assertEquals(fnr, lestOppgave.fnr)
-                assertEquals("12345", lestOppgave.referanse)
-                assertEquals(OppgaveType.JOURNALFOERING, lestOppgave.type)
             }
 
-            client.get("/api/oppgaver/sak/${sak.id}/oppgaver") {
+            client.post("/api/oppgaver/${oppgave.id}/sett-paa-vent") {
+                val dto = SettPaaVentRequest("", Status.UNDER_BEHANDLING)
                 addAuthToken(systemBruker)
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(dto)
             }.also {
                 assertEquals(HttpStatusCode.OK, it.status)
-                val liste: OppgaveListe = it.body()
-                assertEquals(1, liste.oppgaver.size)
-                assertEquals(fnr, liste.oppgaver.first().fnr)
+            }
+
+            client.put("/api/oppgaver/${oppgave.id}/sett-paa-vent") {
+                val dto = SettPaaVentRequest("", Status.UNDER_BEHANDLING)
+                addAuthToken(systemBruker)
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(dto)
+            }.also {
+                assertEquals(HttpStatusCode.OK, it.status)
+            }
+
+            assertEquals(hentOppgaver(client, sak, fnr).first().status, Status.PAA_VENT)
+
+            client.put("/oppgaver/${oppgave.id}/ventefrist-gaar-ut") {
+                val dto = SettPaaVentRequest("", Status.UNDER_BEHANDLING)
+                addAuthToken(systemBruker)
+                header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(dto)
+            }.also {
+                assertEquals(HttpStatusCode.OK, it.status)
             }
         }
     }
+
+    private suspend fun hentOppgaver(
+        client: HttpClient,
+        sak: Sak,
+        fnr: String,
+    ): List<OppgaveIntern> =
+        client.get("/api/oppgaver/sak/${sak.id}/oppgaver") {
+            addAuthToken(systemBruker)
+        }.let {
+            assertEquals(HttpStatusCode.OK, it.status)
+            val liste: OppgaveListe = it.body()
+            assertEquals(1, liste.oppgaver.size)
+            assertEquals(fnr, liste.oppgaver.first().fnr)
+            liste.oppgaver
+        }
 }

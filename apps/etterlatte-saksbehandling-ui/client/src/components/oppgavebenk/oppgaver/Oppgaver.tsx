88498ca404496677b50a42c7213e7963fe989ad0
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-02-28 14:15:22 +0100

    Ikke slice oppgaver foer de er lastet (#3993)
    * Ikke slice oppgaver foer de er lastet
    
    * Sett default hvis no-order present
    
    * Update

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaver/Oppgaver.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaver/Oppgaver.tsx
@@ -28,8 +28,8 @@ export const Oppgaver = ({
   oppdaterFrist,
   filter,
 }: OppgavelisteProps): ReactNode => {
+  if (!oppgaver.length) return <Alert variant="info">Ingen oppgaver</Alert>
   const [sortering, setSortering] = useState<OppgaveSortering>(hentSorteringFraLocalStorage())
-
   const filtrerteOppgaver = filter
     ? filtrerOppgaver(
         filter.enhetsFilter,
@@ -45,18 +45,17 @@ export const Oppgaver = ({
 
   const sortertFrist = sorterFrist(sortering.fristSortering, filtrerteOppgaver)
   const sorterteOppgaver = sorterFnr(sortering.fnrSortering, sortertFrist)
-
   const [page, setPage] = useState<number>(1)
   const [rowsPerPage, setRowsPerPage] = useState<number>(hentPagineringSizeFraLocalStorage())
 
   let paginerteOppgaver = sorterteOppgaver
   paginerteOppgaver = paginerteOppgaver.slice((page - 1) * rowsPerPage, page * rowsPerPage)
 
+  if (!paginerteOppgaver.length) return <Alert variant="info">Ingen oppgaver</Alert>
+
   useEffect(() => {
     if (paginerteOppgaver.length === 0 && filtrerteOppgaver.length > 0) setPage(1)
   }, [paginerteOppgaver, filtrerteOppgaver])
-
-  if (!paginerteOppgaver.length) return <Alert variant="info">Ingen oppgaver</Alert>
 
   return (
     <>

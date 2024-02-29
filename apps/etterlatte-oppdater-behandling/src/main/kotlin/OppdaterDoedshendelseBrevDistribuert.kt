438ada1ee7973d8c0072ecf448a3337179014050
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-02-29 14:15:23 +0100

    logg feilet oppdatering av dødshendelse av distribuert brev

--- apps/etterlatte-oppdater-behandling/src/main/kotlin/OppdaterDoedshendelseBrevDistribuert.kt
+++ apps/etterlatte-oppdater-behandling/src/main/kotlin/OppdaterDoedshendelseBrevDistribuert.kt
@@ -3,6 +3,7 @@ package no.nav.etterlatte
 import no.nav.etterlatte.brev.BrevHendelseType
 import no.nav.etterlatte.brev.Brevkoder
 import no.nav.etterlatte.libs.common.behandling.DoedshendelseBrevDistribuert
+import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
 import no.nav.etterlatte.rapidsandrivers.BREV_ID_KEY
 import no.nav.etterlatte.rapidsandrivers.BREV_KODE
 import no.nav.etterlatte.rapidsandrivers.ListenerMedLoggingOgFeilhaandtering
@@ -13,6 +14,9 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
+
+class OppdaterDoedshendelseException(override val detail: String, override val cause: Throwable?) :
+    InternfeilException(detail, cause)
 
 internal class OppdaterDoedshendelseBrevDistribuert(
     rapidsConnection: RapidsConnection,
@@ -35,7 +39,12 @@ internal class OppdaterDoedshendelseBrevDistribuert(
         val brevkode = packet[BREV_KODE].asText()
         if (brevkode == Brevkoder.BP_INFORMASJON_DOEDSFALL.name || brevkode == Brevkoder.OMS_INFORMASJON_DOEDSFALL.name) {
             logger.info("Oppdaterer brev distribuert for dødshendelse ${packet.sakId}, ${packet.brevId}")
-            behandlingService.oppdaterDoedshendelseBrevDistribuert(DoedshendelseBrevDistribuert(packet.sakId, packet.brevId))
+            try {
+                behandlingService.oppdaterDoedshendelseBrevDistribuert(DoedshendelseBrevDistribuert(packet.sakId, packet.brevId))
+            } catch (e: Exception) {
+                logger.error("Kunne ikke oppdatere distribuert brev for sak ${packet.sakId} brevid: ${packet.brevId}")
+                throw OppdaterDoedshendelseException("Kan ikke oppdatere doedshendelse ${packet.sakId}", e)
+            }
         }
     }
 }

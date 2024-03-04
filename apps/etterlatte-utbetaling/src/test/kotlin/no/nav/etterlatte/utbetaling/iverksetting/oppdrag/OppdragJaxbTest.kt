72b779f7af21c7c6d6ee449d156a13934192b70a
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-03-04 18:51:10 +0100

    Fikser case sensitive kvitteringsmapping
    Feilet på parsing av testen som ble lagt til i dev

--- apps/etterlatte-utbetaling/src/test/kotlin/no/nav/etterlatte/utbetaling/iverksetting/oppdrag/OppdragJaxbTest.kt
+++ apps/etterlatte-utbetaling/src/test/kotlin/no/nav/etterlatte/utbetaling/iverksetting/oppdrag/OppdragJaxbTest.kt
@@ -4,6 +4,7 @@ import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.utbetaling.common.toUUID30
 import no.nav.etterlatte.utbetaling.readFile
 import no.nav.etterlatte.utbetaling.utbetaling
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -69,6 +70,54 @@ internal class OppdragJaxbTest {
             """.trimIndent()
 
         assertEquals(gyldigOppdragXml, oppdragAsXml)
+    }
+
+    @Test
+    fun `test for case-insensitive replace av oppdrag-xml`() {
+        val oppdragBody =
+            """
+            <?xml version="1.0" encoding="utf-8"?><oppdrag xmlns="http://www.trygdeetaten.no/skjema/oppdrag"><mmel><systemId>231-OPPD</systemId><kodeMelding>B110008F</kodeMelding><alvorlighetsgrad>08</alvorlighetsgrad><beskrMelding>Oppdraget finnes fra før</beskrMelding><programId>K231BB10</programId><sectionNavn>CA10-INPUTKONTROLL</sectionNavn></mmel><oppdrag-110>
+                    <kodeAksjon>1</kodeAksjon>
+                    <kodeEndring>NY</kodeEndring>
+                    <kodeFagomraade>BARNEPE</kodeFagomraade>
+                    <fagsystemId>1268</fagsystemId>
+                    <utbetFrekvens>MND</utbetFrekvens>
+                    <oppdragGjelderId>12312312312</oppdragGjelderId>
+                    <datoOppdragGjelderFom>1970-01-01</datoOppdragGjelderFom>
+                    <saksbehId>Z999999</saksbehId>
+                    <avstemming-115>
+                        <kodeKomponent>ETTERLAT</kodeKomponent>
+                        <nokkelAvstemming>2024-03-04-17.03.45.417064</nokkelAvstemming>
+                        <tidspktMelding>2024-03-04-17.03.45.417064</tidspktMelding>
+                    </avstemming-115>
+                    <oppdrags-enhet-120>
+                        <typeEnhet>BOS</typeEnhet>
+                        <enhet>4819</enhet>
+                        <datoEnhetFom>1900-01-01</datoEnhetFom>
+                    </oppdrags-enhet-120>
+                    <oppdrags-linje-150>
+                        <kodeEndringLinje>NY</kodeEndringLinje>
+                        <vedtakId>1798</vedtakId>
+                        <delytelseId>2863</delytelseId>
+                        <kodeKlassifik>BARNEPENSJON-OPTP</kodeKlassifik>
+                        <datoVedtakFom>2024-03-01</datoVedtakFom>
+                        <sats>9885</sats>
+                        <fradragTillegg>T</fradragTillegg>
+                        <typeSats>MND</typeSats>
+                        <brukKjoreplan>N</brukKjoreplan>
+                        <saksbehId>Z999999</saksbehId>
+                        <utbetalesTilId>12312312312</utbetalesTilId>
+                        <henvisning>ebc391cec0344f2ca6599f31602e6f</henvisning>
+                        <attestant-180>
+                            <attestantId>Z888888</attestantId>
+                        </attestant-180>
+                    </oppdrags-linje-150>
+            </oppdrag-110></oppdrag>
+            """.trimIndent()
+
+        Assertions.assertDoesNotThrow {
+            OppdragJaxb.toOppdrag(oppdragBody)
+        }
     }
 
     @Test

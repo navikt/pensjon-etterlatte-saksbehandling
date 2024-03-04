37aba6040f86e5a12e1b7e250aa79feb7659262f
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-03-04 15:42:21 +0100

    EY-3314 Bedre lesbarhet

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/SamordningSak.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/SamordningSak.tsx
@@ -8,6 +8,7 @@ import { formaterStringDato } from '~utils/formattering'
 import { SakMedBehandlinger } from '~components/person/typer'
 import { Container } from '~shared/styled'
 import { ApiErrorAlert } from '~ErrorBoundary'
+import { Samordningsvedtak } from '~components/vedtak/typer'
 
 export const SamordningSak = ({ sakStatus }: { sakStatus: Result<SakMedBehandlinger> }) => {
   const [samordningdataStatus, hent] = useApiCall(hentSamordningsdataForSak)
@@ -23,45 +24,49 @@ export const SamordningSak = ({ sakStatus }: { sakStatus: Result<SakMedBehandlin
       <Heading size="medium">Samordningsmeldinger</Heading>
 
       {mapResult(samordningdataStatus, {
-        success: (samordningsdata) => (
-          <Table zebraStripes>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>VedtaksID</Table.HeaderCell>
-                <Table.HeaderCell>SamordningsID</Table.HeaderCell>
-                <Table.HeaderCell>Tjenestepensjon</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
-                <Table.HeaderCell>Sendt dato</Table.HeaderCell>
-                <Table.HeaderCell>Mottatt dato</Table.HeaderCell>
-                <Table.HeaderCell>Purret dato</Table.HeaderCell>
-                <Table.HeaderCell>Refusjonskrav</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {samordningsdata
-                .sort((v1, v2) => v2.vedtakId - v1.vedtakId)
-                .map((vedtak) =>
-                  vedtak.samordningsmeldinger.map((mld) => (
-                    <Table.Row key={mld.samId}>
-                      <Table.DataCell>{vedtak.vedtakId}</Table.DataCell>
-                      <Table.DataCell>{mld.samId}</Table.DataCell>
-                      <Table.DataCell>
-                        {mld.tpNr} {mld.tpNavn}
-                      </Table.DataCell>
-                      <Table.DataCell>{mld.meldingstatusKode}</Table.DataCell>
-                      <Table.DataCell>{formaterStringDato(mld.sendtDato)}</Table.DataCell>
-                      <Table.DataCell>{mld.svartDato && formaterStringDato(mld.svartDato)}</Table.DataCell>
-                      <Table.DataCell>{mld.purretDato && formaterStringDato(mld.purretDato)}</Table.DataCell>
-                      <Table.DataCell>{mld.refusjonskrav}</Table.DataCell>
-                    </Table.Row>
-                  ))
-                )}
-            </Table.Body>
-          </Table>
-        ),
+        success: (data) => <SamordningTabell samordningsdata={data} />,
         pending: <Spinner visible={true} label="Henter samordningsdata" />,
         error: () => <ApiErrorAlert>Kunne ikke hente samordningsdata</ApiErrorAlert>,
       })}
     </Container>
+  )
+}
+
+function SamordningTabell({ samordningsdata }: { samordningsdata: Array<Samordningsvedtak> }) {
+  return (
+    <Table zebraStripes>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>VedtaksID</Table.HeaderCell>
+          <Table.HeaderCell>SamordningsID</Table.HeaderCell>
+          <Table.HeaderCell>Tjenestepensjon</Table.HeaderCell>
+          <Table.HeaderCell>Status</Table.HeaderCell>
+          <Table.HeaderCell>Sendt dato</Table.HeaderCell>
+          <Table.HeaderCell>Mottatt dato</Table.HeaderCell>
+          <Table.HeaderCell>Purret dato</Table.HeaderCell>
+          <Table.HeaderCell>Refusjonskrav</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {samordningsdata
+          .sort((v1, v2) => v2.vedtakId - v1.vedtakId)
+          .map((vedtak) =>
+            vedtak.samordningsmeldinger.map((mld) => (
+              <Table.Row key={mld.samId}>
+                <Table.DataCell>{vedtak.vedtakId}</Table.DataCell>
+                <Table.DataCell>{mld.samId}</Table.DataCell>
+                <Table.DataCell>
+                  {mld.tpNr} {mld.tpNavn}
+                </Table.DataCell>
+                <Table.DataCell>{mld.meldingstatusKode}</Table.DataCell>
+                <Table.DataCell>{formaterStringDato(mld.sendtDato)}</Table.DataCell>
+                <Table.DataCell>{mld.svartDato && formaterStringDato(mld.svartDato)}</Table.DataCell>
+                <Table.DataCell>{mld.purretDato && formaterStringDato(mld.purretDato)}</Table.DataCell>
+                <Table.DataCell>{mld.refusjonskrav}</Table.DataCell>
+              </Table.Row>
+            ))
+          )}
+      </Table.Body>
+    </Table>
   )
 }

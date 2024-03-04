87c9d7f85037d8647427036ffedf2d0f932b1354
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-03-04 14:30:39 +0100

    EY-3314 Benytte all-in-one mapResult

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/SamordningSak.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/SamordningSak.tsx
@@ -1,79 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import { Alert, Heading, Table } from '@navikt/ds-react'
+import React, { useEffect } from 'react'
+import { Heading, Table } from '@navikt/ds-react'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { hentSamordningsdataForSak } from '~shared/api/vedtaksvurdering'
-import { Samordningsvedtak } from '~components/vedtak/typer'
-import { isInitial, isPending, isSuccess, Result } from '~shared/api/apiUtils'
+import { isSuccess, mapResult, Result } from '~shared/api/apiUtils'
 import Spinner from '~shared/Spinner'
-import { isFailureHandler } from '~shared/api/IsFailureHandler'
 import { formaterStringDato } from '~utils/formattering'
 import { SakMedBehandlinger } from '~components/person/typer'
 import { Container } from '~shared/styled'
+import { ApiErrorAlert } from '~ErrorBoundary'
 
 export const SamordningSak = ({ sakStatus }: { sakStatus: Result<SakMedBehandlinger> }) => {
-  const [samordningsdata, setSamordningsdata] = useState<Samordningsvedtak[]>()
-  const [hentet, hent] = useApiCall(hentSamordningsdataForSak)
+  const [samordningdataStatus, hent] = useApiCall(hentSamordningsdataForSak)
 
   useEffect(() => {
-    if (isSuccess(sakStatus) && isInitial(hentet)) {
-      hent(sakStatus.data.sak.id, (result) => setSamordningsdata(result))
+    if (isSuccess(sakStatus)) {
+      hent(sakStatus.data.sak.id)
     }
-  })
+  }, [sakStatus])
 
   return (
     <Container>
       <Heading size="medium">Samordningsmeldinger</Heading>
 
-      <Spinner visible={isPending(hentet)} label="Henter data" />
-
-      {isFailureHandler({
-        apiResult: hentet,
-        errorMessage: 'Kunne ikke hente data',
-      })}
-
-      {isSuccess(hentet) && samordningsdata && samordningsdata.length === 0 && (
-        <Alert variant="info">Ingen samordningsdata</Alert>
-      )}
-
-      {isSuccess(hentet) && samordningsdata && (
-        <Table zebraStripes>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>VedtaksID</Table.HeaderCell>
-              <Table.HeaderCell>SamordningsID</Table.HeaderCell>
-              <Table.HeaderCell>Tjenestepensjon</Table.HeaderCell>
-              <Table.HeaderCell>Status</Table.HeaderCell>
-              <Table.HeaderCell>Sendt dato</Table.HeaderCell>
-              <Table.HeaderCell>Mottatt dato</Table.HeaderCell>
-              <Table.HeaderCell>Purret dato</Table.HeaderCell>
-              <Table.HeaderCell>Refusjonskrav</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {!samordningsdata.length && (
+      {mapResult(samordningdataStatus, {
+        success: (samordningsdata) => (
+          <Table zebraStripes>
+            <Table.Header>
               <Table.Row>
-                <Table.DataCell colSpan={7}>Ingen samordningsmeldinger funnet</Table.DataCell>
+                <Table.HeaderCell>VedtaksID</Table.HeaderCell>
+                <Table.HeaderCell>SamordningsID</Table.HeaderCell>
+                <Table.HeaderCell>Tjenestepensjon</Table.HeaderCell>
+                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.HeaderCell>Sendt dato</Table.HeaderCell>
+                <Table.HeaderCell>Mottatt dato</Table.HeaderCell>
+                <Table.HeaderCell>Purret dato</Table.HeaderCell>
+                <Table.HeaderCell>Refusjonskrav</Table.HeaderCell>
               </Table.Row>
-            )}
-            {samordningsdata.map((vedtak) =>
-              vedtak.samordningsmeldinger.map((mld) => (
-                <Table.Row key={mld.samId}>
-                  <Table.DataCell>{vedtak.vedtakId}</Table.DataCell>
-                  <Table.DataCell>{mld.samId}</Table.DataCell>
-                  <Table.DataCell>
-                    {mld.tpNr} {mld.tpNavn}
-                  </Table.DataCell>
-                  <Table.DataCell>{mld.meldingstatusKode}</Table.DataCell>
-                  <Table.DataCell>{formaterStringDato(mld.sendtDato)}</Table.DataCell>
-                  <Table.DataCell>{mld.svartDato && formaterStringDato(mld.svartDato)}</Table.DataCell>
-                  <Table.DataCell>{mld.purretDato && formaterStringDato(mld.purretDato)}</Table.DataCell>
-                  <Table.DataCell>{mld.refusjonskrav}</Table.DataCell>
-                </Table.Row>
-              ))
-            )}
-          </Table.Body>
-        </Table>
-      )}
+            </Table.Header>
+            <Table.Body>
+              {samordningsdata.map((vedtak) =>
+                vedtak.samordningsmeldinger.map((mld) => (
+                  <Table.Row key={mld.samId}>
+                    <Table.DataCell>{vedtak.vedtakId}</Table.DataCell>
+                    <Table.DataCell>{mld.samId}</Table.DataCell>
+                    <Table.DataCell>
+                      {mld.tpNr} {mld.tpNavn}
+                    </Table.DataCell>
+                    <Table.DataCell>{mld.meldingstatusKode}</Table.DataCell>
+                    <Table.DataCell>{formaterStringDato(mld.sendtDato)}</Table.DataCell>
+                    <Table.DataCell>{mld.svartDato && formaterStringDato(mld.svartDato)}</Table.DataCell>
+                    <Table.DataCell>{mld.purretDato && formaterStringDato(mld.purretDato)}</Table.DataCell>
+                    <Table.DataCell>{mld.refusjonskrav}</Table.DataCell>
+                  </Table.Row>
+                ))
+              )}
+            </Table.Body>
+          </Table>
+        ),
+        pending: <Spinner visible={true} label="Henter samordningsdata" />,
+        error: () => <ApiErrorAlert>Kunne ikke hente samordningsdata</ApiErrorAlert>,
+      })}
     </Container>
   )
 }

b8617dc809f35d8c932810b65ad8dc3e4ee7102b
Bjørn Golberg <bjorn.golberg@nav.no>
2024-03-06 15:15:26 +0100

    Gjenoppretting metrics i samme klasse som resten av metrics i behandling

--- apps/etterlatte-behandling/src/main/kotlin/metrics/BehandlingMetrics.kt
+++ apps/etterlatte-behandling/src/main/kotlin/metrics/BehandlingMetrics.kt
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory
 class BehandlingMetrics(
     private val oppgaveMetrikkerDao: OppgaveMetrikkerDao,
     private val behandlingerMetrikkerDao: BehandlingMetrikkerDao,
+    private val gjenopprettingDao: GjenopprettingMetrikkerDao,
     private val registry: CollectorRegistry = CollectorRegistry.defaultRegistry,
 ) : MetrikkUthenter {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -30,6 +31,17 @@ class BehandlingMetrics(
             .register(registry)
     }
 
+    val gjenopprettinger by lazy {
+        Gauge.build("etterlatte_gjenopprettinger", "Antall gjenopprettinger")
+            .labelNames("automatisk", "status", "type", "enhet")
+            .register(registry)
+    }
+
+    val avbruttGrunnetSoeknad by lazy {
+        Gauge.build("etterlatte_gjenopprettinger_soeknad", "Antall gjenopprettinger avbrutt på grunn av søknad")
+            .register(registry)
+    }
+
     override fun run() {
         logger.info("Samler metrikker med ${this::class.simpleName}")
 
@@ -49,6 +61,14 @@ class BehandlingMetrics(
                 it.kilde.name,
                 it.automatiskMigrering,
             ).set(it.antall.toDouble())
+        }
+
+        gjenopprettingDao.gjenopprettinger().forEach {
+            gjenopprettinger.labels(it.automatisk, it.status.name, it.type, it.enhet).set(it.antall.toDouble())
+        }
+
+        gjenopprettingDao.avbruttGrunnetSoeknad().forEach {
+            avbruttGrunnetSoeknad.labels().set(it.toDouble())
         }
     }
 }

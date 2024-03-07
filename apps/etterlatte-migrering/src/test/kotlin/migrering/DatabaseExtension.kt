b6a4405f65c966835f7b4e49c14f68e63b95d726
Mads Opheim <mads.opheim@nav.no>
2024-03-07 14:42:39 +0100

    Skriv med det samme om den siste testen i migrering som ikkje brukte gjenbrukbar database-container til å gjera det

--- apps/etterlatte-migrering/src/test/kotlin/migrering/DatabaseExtension.kt
+++ apps/etterlatte-migrering/src/test/kotlin/migrering/DatabaseExtension.kt
@@ -3,5 +3,12 @@ package no.nav.etterlatte.migrering
 import no.nav.etterlatte.GenerellDatabaseExtension
 import no.nav.etterlatte.ResetDatabaseStatement
 
-@ResetDatabaseStatement("""""")
+@ResetDatabaseStatement(
+    """
+    TRUNCATE TABLE saker_til_migrering;
+    TRUNCATE TABLE feilkjoering;
+    TRUNCATE TABLE dryrun;
+    TRUNCATE TABLE pesyssak CASCADE;
+""",
+)
 class DatabaseExtension : GenerellDatabaseExtension()

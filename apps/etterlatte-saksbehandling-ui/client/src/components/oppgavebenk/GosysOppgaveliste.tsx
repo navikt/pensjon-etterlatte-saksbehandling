4dca9a543ed6f153c5ae8bb0b088e7f7304c3d98
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-03-07 10:02:25 +0100

    Switch for å kun se egne gosys oppgaver

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/GosysOppgaveliste.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/GosysOppgaveliste.tsx
@@ -11,6 +11,10 @@ import { FilterRad } from '~components/oppgavebenk/filtreringAvOppgaver/FilterRa
 import { Filter } from '~components/oppgavebenk/filtreringAvOppgaver/typer'
 import { defaultFiltre } from '~components/oppgavebenk/filtreringAvOppgaver/filtrerOppgaver'
 import { OppgavelisteValg } from '~components/oppgavebenk/velgOppgaveliste/oppgavelisteValg'
+import { Switch } from '@navikt/ds-react'
+import { useAppSelector } from '~store/Store'
+import { Tilgangsmelding } from '~components/oppgavebenk/components/Tilgangsmelding'
+import styled from 'styled-components'
 
 interface Props {
   oppdaterTildeling: (oppgave: OppgaveDTO, saksbehandler: OppgaveSaksbehandler | null, versjon: number | null) => void
@@ -19,11 +23,22 @@ interface Props {
 }
 
 export const GosysOppgaveliste = ({ oppdaterTildeling, saksbehandlereIEnhet, revurderingsaarsaker }: Props) => {
+  const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
+  if (!innloggetSaksbehandler.skriveTilgang) {
+    return <Tilgangsmelding />
+  }
+
   const [filter, setFilter] = useState<Filter>(defaultFiltre)
+
+  const [visKunMineGosysOppgaver, setVisKunMineGosysOppgaver] = useState<boolean>(false)
 
   const [gosysOppgaver, setGosysOppgaver] = useState<Array<OppgaveDTO>>([])
 
   const [gosysOppgaverResult, hentGosysOppgaverFetch] = useApiCall(hentGosysOppgaver)
+
+  const kunMineGosysOppgaver = (): Array<OppgaveDTO> => {
+    return gosysOppgaver.filter((o) => o.saksbehandler?.ident === innloggetSaksbehandler.ident)
+  }
 
   const hentAlleGosysOppgaver = () => {
     hentGosysOppgaverFetch({})
@@ -37,34 +52,40 @@ export const GosysOppgaveliste = ({ oppdaterTildeling, saksbehandlereIEnhet, rev
     if (isSuccess(gosysOppgaverResult)) setGosysOppgaver(gosysOppgaverResult.data)
   }, [gosysOppgaverResult])
 
-  return (
-    <>
-      {mapApiResult(
-        gosysOppgaverResult,
-        <Spinner visible={true} label="Henter nye Gosys oppgaver" />,
-        (error) => (
-          <ApiErrorAlert>{error.detail || 'Kunne ikke hente Gosys oppgaver'}</ApiErrorAlert>
-        ),
-        () => (
-          <>
-            <FilterRad
-              hentAlleOppgaver={hentAlleGosysOppgaver}
-              hentOppgaverStatus={() => {}}
-              filter={filter}
-              setFilter={setFilter}
-              saksbehandlereIEnhet={saksbehandlereIEnhet}
-              oppgavelisteValg={OppgavelisteValg.GOSYS_OPPGAVER}
-            />
-            <Oppgaver
-              oppgaver={gosysOppgaver}
-              oppdaterTildeling={oppdaterTildeling}
-              saksbehandlereIEnhet={saksbehandlereIEnhet}
-              revurderingsaarsaker={revurderingsaarsaker}
-              filter={filter}
-            />
-          </>
-        )
-      )}
-    </>
+  return mapApiResult(
+    gosysOppgaverResult,
+    <Spinner visible={true} label="Henter nye Gosys oppgaver" />,
+    (error) => <ApiErrorAlert>{error.detail || 'Kunne ikke hente Gosys oppgaver'}</ApiErrorAlert>,
+    () => (
+      <>
+        <VisKunMineGosysOppgaverSwitch
+          checked={visKunMineGosysOppgaver}
+          onChange={(e) => {
+            setVisKunMineGosysOppgaver(e.target.checked)
+          }}
+        >
+          Vis mine Gosys oppgaver
+        </VisKunMineGosysOppgaverSwitch>
+        <FilterRad
+          hentAlleOppgaver={hentAlleGosysOppgaver}
+          hentOppgaverStatus={() => {}}
+          filter={filter}
+          setFilter={setFilter}
+          saksbehandlereIEnhet={saksbehandlereIEnhet}
+          oppgavelisteValg={OppgavelisteValg.GOSYS_OPPGAVER}
+        />
+        <Oppgaver
+          oppgaver={visKunMineGosysOppgaver ? kunMineGosysOppgaver() : gosysOppgaver}
+          oppdaterTildeling={oppdaterTildeling}
+          saksbehandlereIEnhet={saksbehandlereIEnhet}
+          revurderingsaarsaker={revurderingsaarsaker}
+          filter={filter}
+        />
+      </>
+    )
   )
 }
+
+const VisKunMineGosysOppgaverSwitch = styled(Switch)`
+  margin-bottom: 0.5rem;
+`

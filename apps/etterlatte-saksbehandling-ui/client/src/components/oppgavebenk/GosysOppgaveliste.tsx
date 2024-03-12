59648836fd6924a772960e7a18b020dd70a83f33
Chris Searle <chris@chrissearle.org>
2024-03-12 13:36:40 +0100

    Fix frontend after rebase

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/GosysOppgaveliste.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/GosysOppgaveliste.tsx
@@ -27,7 +27,8 @@ interface Props {
 
 export const GosysOppgaveliste = ({ saksbehandlereIEnhet }: Props) => {
   const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
-  if (!innloggetSaksbehandler.skriveTilgang) {
+
+  if (!innloggetSaksbehandler.skriveEnheter.length) {
     return <Tilgangsmelding />
   }
 

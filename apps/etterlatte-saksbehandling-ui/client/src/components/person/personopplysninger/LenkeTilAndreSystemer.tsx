2f2ffb8bc81ecd4ca1218e20f9e54597fb074eb7
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-03-13 10:20:33 +0100

    Legge til ekte lenker for pesys, modia og gosys

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/personopplysninger/LenkeTilAndreSystemer.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/personopplysninger/LenkeTilAndreSystemer.tsx
@@ -1,10 +1,16 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useContext } from 'react'
 import { Personopplysning } from '~components/person/personopplysninger/Personopplysning'
 import { ExternalLinkIcon, MonitorIcon } from '@navikt/aksel-icons'
 import { Link, Tag } from '@navikt/ds-react'
 import { SpaceChildren } from '~shared/styled'
+import { ConfigContext } from '~clientConfig'
+import { useParams } from 'react-router-dom'
 
 export const LenkeTilAndreSystemer = (): ReactNode => {
+  const configContext = useContext(ConfigContext)
+
+  const { fnr } = useParams()
+
   return (
     <Personopplysning
       heading="Snarvei til brukers opplysninger i andre system"
@@ -15,7 +21,7 @@ export const LenkeTilAndreSystemer = (): ReactNode => {
           <Tag variant="alt3" size="small">
             Go
           </Tag>
-          <Link href="#">
+          <Link href={`${configContext['gosysUrl']}/personoversikt/fnr=${fnr}`} target="_blank">
             Oversikt i Gosys <ExternalLinkIcon />
           </Link>
         </SpaceChildren>
@@ -23,7 +29,7 @@ export const LenkeTilAndreSystemer = (): ReactNode => {
           <Tag variant="alt3" size="small">
             Py
           </Tag>
-          <Link href="#">
+          <Link href={`${configContext['psakUrl']}/brukeroversikt/fnr=${fnr}`} target="_blank">
             Oversikt i Pesys <ExternalLinkIcon />
           </Link>
         </SpaceChildren>
@@ -31,7 +37,7 @@ export const LenkeTilAndreSystemer = (): ReactNode => {
           <Tag variant="alt3" size="small">
             Mo
           </Tag>
-          <Link href="#">
+          <Link href={`${configContext['modiapersonoversiktUrl']}/person/${fnr}`} target="_blank">
             Oversikt i Modia <ExternalLinkIcon />
           </Link>
         </SpaceChildren>

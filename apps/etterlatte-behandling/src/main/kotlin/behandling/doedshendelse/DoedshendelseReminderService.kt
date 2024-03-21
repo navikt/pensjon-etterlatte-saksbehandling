a4fc85814d2867fb3377c681aa7a9974ce93bc7d
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-03-21 12:16:47 +0100

    Feature/sjekk om bp er soekt etter doedshendelse2mnd (#4289)
    * EY-3497 Sjekk om barn har søkt etter brev
    
    * EY-3497 tester
    
    * PR feedback
    
    * EY-3497 Egen klasse for hendelser til reminderdoehdsnhendelseservice

---
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/doedshendelse/DoedshendelseReminderService.kt
@@ -0,0 +1,60 @@
+package no.nav.etterlatte.behandling.doedshendelse
+
+import no.nav.etterlatte.Context
+import no.nav.etterlatte.Kontekst
+import no.nav.etterlatte.behandling.BehandlingService
+import no.nav.etterlatte.behandling.domain.Foerstegangsbehandling
+import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
+import no.nav.etterlatte.grunnlagsendring.doedshendelse.DoedshendelseDao
+import no.nav.etterlatte.grunnlagsendring.doedshendelse.DoedshendelseFeatureToggle
+import no.nav.etterlatte.libs.common.oppgave.OppgaveKilde
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
+import no.nav.etterlatte.libs.common.tidspunkt.toLocalDatetimeNorskTid
+import no.nav.etterlatte.oppgave.OppgaveService
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import kotlin.math.absoluteValue
+
+class DoedshendelseReminderService(
+    private val featureToggleService: FeatureToggleService,
+    private val doedshendelseDao: DoedshendelseDao,
+    private val behandlingService: BehandlingService,
+    private val oppgaveService: OppgaveService,
+) {
+    fun setupKontekstAndRun(context: Context) {
+        Kontekst.set(context)
+        run()
+    }
+
+    private fun run() {
+        if (featureToggleService.isEnabled(DoedshendelseFeatureToggle.KanLagreDoedshendelse, false)) {
+            val hentAlleFerdigDoedsmeldingerMedBrevBp = hentAlleFerdigDoedsmeldingerMedBrevBp()
+            val toMaanedGamlehendelser = hendelserErGamleNok(hentAlleFerdigDoedsmeldingerMedBrevBp)
+            toMaanedGamlehendelser.forEach { lagOppgaveOmIkkeSoekt(it) }
+        }
+    }
+
+    private fun lagOppgaveOmIkkeSoekt(hendelse: DoedshendelseReminder) {
+        val behandlingerForSak = behandlingService.hentBehandlingerForSak(hendelse.sakId!!)
+        val harSoekt = behandlingerForSak.any { it is Foerstegangsbehandling }
+        if (!harSoekt) {
+            oppgaveService.opprettNyOppgaveMedSakOgReferanse(
+                referanse = hendelse.id.toString(),
+                sakId = hendelse.sakId,
+                oppgaveKilde = OppgaveKilde.HENDELSE,
+                oppgaveType = OppgaveType.VURDER_KONSEKVENS,
+                merknad = "${hendelse.beroertFnr} Har ikke søkt om Barnepensjon 2 måneder etter utsendt brev",
+                frist = Tidspunkt.now().plus(30L, ChronoUnit.DAYS),
+            )
+        }
+    }
+
+    private fun hendelserErGamleNok(hendelser: List<DoedshendelseReminder>): List<DoedshendelseReminder> {
+        val idag = LocalDateTime.now().plusHours(1L)
+        val toMaaneder = 2L
+        return hendelser.filter { ChronoUnit.MONTHS.between(it.endret.toLocalDatetimeNorskTid(), idag).absoluteValue >= toMaaneder }
+    }
+
+    private fun hentAlleFerdigDoedsmeldingerMedBrevBp() = doedshendelseDao.hentDoedshendelserMedStatusFerdigOgUtFallBrevBp()
+}

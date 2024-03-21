8a338de37d9222a7eafe7d27be2d45548a3ad178
Trond Valen <trond.valen@nav.no>
2024-03-21 12:48:29 +0100

    EY-3724: Mulighet for å sende parametre til brevet til brev-api.

---
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/oms/AktivitetspliktBrevdata.kt
@@ -0,0 +1,9 @@
+package no.nav.etterlatte.brev.model.oms
+
+import no.nav.etterlatte.brev.model.BrevDataRedigerbar
+
+data class AktivitetspliktBrevdata(
+    val aktivitetsgrad: Aktivitetsgrad,
+) : BrevDataRedigerbar
+
+enum class Aktivitetsgrad { IKKE_I_AKTIVITET, UNDER_50_PROSENT, OVER_50_PROSENT }

58b028395ce61629f4517ffd9f7513e8a3d6fb12
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-03-22 12:31:47 +0100

    behold moderne testing på db

--- apps/etterlatte-behandling/src/test/kotlin/institusjonsopphold/institusjonsopphold/InstitusjonsoppholdDaoTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/institusjonsopphold/institusjonsopphold/InstitusjonsoppholdDaoTest.kt
@@ -1,40 +1,26 @@
 package institusjonsopphold.institusjonsopphold
 
 import no.nav.etterlatte.ConnectionAutoclosingTest
+import no.nav.etterlatte.DatabaseExtension
 import no.nav.etterlatte.institusjonsopphold.InstitusjonsoppholdBegrunnelse
 import no.nav.etterlatte.institusjonsopphold.InstitusjonsoppholdDao
 import no.nav.etterlatte.libs.common.behandling.JaNei
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlagsopplysning
-import no.nav.etterlatte.libs.database.DataSourceBuilder
-import no.nav.etterlatte.libs.database.migrate
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
+import org.junit.jupiter.api.extension.ExtendWith
 import java.util.UUID
 import javax.sql.DataSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class InstitusjonsoppholdDaoTest {
-    @Container
-    private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:14")
-    private lateinit var dataSource: DataSource
+@ExtendWith(DatabaseExtension::class)
+internal class InstitusjonsoppholdDaoTest(val dataSource: DataSource) {
     private lateinit var institusjonsoppholdDao: InstitusjonsoppholdDao
 
     @BeforeAll
     fun beforeAll() {
-        postgreSQLContainer.start()
-        postgreSQLContainer.withUrlParam("user", postgreSQLContainer.username)
-        postgreSQLContainer.withUrlParam("password", postgreSQLContainer.password)
-
-        dataSource =
-            DataSourceBuilder.createDataSource(
-                jdbcUrl = postgreSQLContainer.jdbcUrl,
-                username = postgreSQLContainer.username,
-                password = postgreSQLContainer.password,
-            ).apply { migrate() }
         institusjonsoppholdDao = InstitusjonsoppholdDao(ConnectionAutoclosingTest(dataSource))
     }
 

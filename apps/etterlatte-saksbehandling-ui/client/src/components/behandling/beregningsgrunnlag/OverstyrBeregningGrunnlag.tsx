9c58a2993c789b4bb05d0c9f491f8db8f44248de
Bjørn Golberg <bjorn.golberg@nav.no>
2024-03-22 15:45:23 +0100

    Smaafikser overstyrt beregning form (#4319)
    * Validere på trygdetid tilhører fnr
    
    * Default fra og med skal være virkningstidspunkt
    
    * Forenkle kode

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregningsgrunnlag/OverstyrBeregningGrunnlag.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregningsgrunnlag/OverstyrBeregningGrunnlag.tsx
@@ -162,7 +162,7 @@ const OverstyrBeregningGrunnlag = (props: { behandling: IBehandlingReducer; over
     fom: Date | undefined = new Date(behandling.virkningstidspunkt!.dato),
     tom: Date | undefined
   ): Date => {
-    return addMonths(tom || fom, 1)
+    return tom ? addMonths(tom, 1) : fom
   }
 
   return (
@@ -298,6 +298,10 @@ function feilIOverstyrBeregningperiode(
     feil.push('TRYGDETID_MANGLER')
   }
 
+  if (!grunnlag.data.trygdetidForIdent) {
+    feil.push('TRYGDETID_MANGLER_FNR')
+  }
+
   const prorataBroekNevner = parseInt(grunnlag.data.prorataBroekNevner ?? '')
   const prorataBroekTeller = parseInt(grunnlag.data.prorataBroekTeller ?? '')
 
@@ -311,7 +315,7 @@ function feilIOverstyrBeregningperiode(
     feil.push('TOM_FOER_FOM')
   }
 
-  if (grunnlag.data.beskrivelse === undefined || grunnlag.data.beskrivelse === '') {
+  if (!grunnlag.data.beskrivelse) {
     feil.push('BESKRIVELSE_MANGLER')
   }
 
@@ -340,6 +344,7 @@ export type FeilIPeriodeGrunnlagAlle =
   | FeilIPeriodeOverstyrBeregning
   | 'BELOEP_MANGLER'
   | 'TRYGDETID_MANGLER'
+  | 'TRYGDETID_MANGLER_FNR'
   | 'BESKRIVELSE_MANGLER'
   | 'PRORATA_MANGLER'
 
@@ -352,6 +357,7 @@ export const teksterFeilIPeriode: Record<FeilIPeriodeGrunnlagAlle, string> = {
   TOM_FOER_FOM: 'Til og med kan ikke være før fra og med',
   BELOEP_MANGLER: 'Utbetalt beløp er påkrevd',
   TRYGDETID_MANGLER: 'Trygdetid er påkrevd',
+  TRYGDETID_MANGLER_FNR: 'Trygdetid tilhører FNR er påkrevd',
   BESKRIVELSE_MANGLER: 'Beskrivelse er påkrevd',
   PRORATA_MANGLER: 'Prorata brøk må ha begge felter fyllt ut hvis det er i bruk',
 } as const

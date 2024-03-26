a5b4ed094cfdeec345466e688b47bc6d8171e78e
Trond Valen <trond.valen@nav.no>
2024-03-26 13:54:11 +0100

    EY-3725: Innfører ny type tidshendelse i oppdater-behandling

---
+++ apps/etterlatte-oppdater-behandling/src/main/kotlin/TidshendelseService.kt
@@ -0,0 +1,156 @@
+package no.nav.etterlatte
+
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.AO_BP20
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.AO_BP21
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.AO_OMS67
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.OMS_DOED_3AAR
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.OMS_DOED_4MND
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.OMS_DOED_5AAR
+import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
+import no.nav.etterlatte.libs.common.behandling.Omregningshendelse
+import no.nav.etterlatte.libs.common.behandling.Prosesstype
+import no.nav.etterlatte.libs.common.behandling.Revurderingaarsak
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType.AKTIVITETSPLIKT_OPPFOELGING
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType.REVURDERING
+import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
+import org.slf4j.LoggerFactory
+import java.time.LocalTime
+import java.util.UUID
+
+class TidshendelseService(
+    private val behandlingService: BehandlingService,
+    private val featureToggleService: FeatureToggleService,
+) {
+    private val logger = LoggerFactory.getLogger(TidshendelseService::class.java)
+
+    enum class TidshendelserJobbType {
+        AO_BP20,
+        AO_BP21,
+        AO_OMS67,
+        OMS_DOED_3AAR,
+        OMS_DOED_5AAR,
+        OMS_DOED_4MND,
+    }
+
+    fun haandterHendelse(hendelse: TidshendelsePacket): TidshendelseResult {
+        if (skalSkippes(hendelse)) {
+            return TidshendelseResult.Skipped
+        }
+        logger.info(
+            """Løpende ytelse: oppretter behandling/oppgave for sak ${hendelse.sakId} 
+                    behandlingsmåned=${hendelse.behandlingsmaaned}""",
+        )
+        if (skalLageOmregning(hendelse)) {
+            try {
+                return behandlingService.opprettOmregning(omregningshendelse(hendelse)).let { response ->
+                    TidshendelseResult.OpprettetOmregning(
+                        response.behandlingId,
+                        response.forrigeBehandlingId,
+                    )
+                }
+            } catch (e: Exception) {
+                logger.error("Kunne ikke opprette omregning [sak=${hendelse.sakId}]", e)
+                return opprettOppgave(hendelse)
+                    ?.let { oppgaveId -> TidshendelseResult.OpprettetOppgave(oppgaveId) }
+                    ?: throw RuntimeException("Oppgaven ble ikke opprettet!")
+            }
+        } else {
+            return opprettOppgave(hendelse)
+                ?.let { oppgaveId -> TidshendelseResult.OpprettetOppgave(oppgaveId) }
+                ?: TidshendelseResult.Skipped
+        }
+    }
+
+    private fun skalSkippes(hendelse: TidshendelsePacket): Boolean {
+        if (hendelse.jobbtype == AO_BP20 && hendelse.harMigrertYrkesskadeFordel) {
+            logger.info("Har migrert yrkesskadefordel: utvidet aldersgrense [sak=${hendelse.sakId}]")
+            return true
+        }
+        if (hendelse.jobbtype in arrayOf(OMS_DOED_3AAR, OMS_DOED_5AAR) && hendelse.harRettUtenTidsbegrensning) {
+            logger.info("Har omstillingsstønad med rett uten tidsbegrensning, opphører ikke [sak=${hendelse.sakId}]")
+            return true
+        }
+        if (hendelse.harLoependeYtelse && hendelse.dryrun) {
+            logger.info(
+                """Dry run: Løpende ytelse: skipper oppretting av behandling/oppgave
+                    for sak ${hendelse.sakId} behandlingsmåned=${hendelse.behandlingsmaaned}""",
+            )
+            return true
+        }
+        if (!hendelse.harLoependeYtelse) {
+            logger.info("Ingen løpende ytelse funnet for sak ${hendelse.sakId}")
+            return true
+        }
+        return false
+    }
+
+    private fun omregningshendelse(hendelse: TidshendelsePacket) =
+        Omregningshendelse(
+            sakId = hendelse.sakId,
+            fradato = hendelse.behandlingsmaaned.plusMonths(1).atDay(1),
+            prosesstype = Prosesstype.AUTOMATISK,
+            revurderingaarsak = Revurderingaarsak.ALDERSOVERGANG,
+            oppgavefrist = hendelse.behandlingsmaaned.atEndOfMonth(),
+        )
+
+    private fun opprettOppgave(hendelse: TidshendelsePacket): UUID? {
+        if (hendelse.jobbtype == OMS_DOED_4MND && !kanOppretteOppgaveForAktivitetsplikt()) {
+            logger.info("Oppgave for varselbrev aktivitetsplikt er skrudd av. Avbryter.")
+            return null
+        }
+        val oppgaveId =
+            behandlingService.opprettOppgave(
+                hendelse.sakId,
+                oppgaveTypeFor(hendelse.jobbtype),
+                referanse = hendelse.behandlingId?.toString(),
+                merknad = generateMerknad(hendelse.jobbtype),
+                frist = Tidspunkt.ofNorskTidssone(hendelse.behandlingsmaaned.atEndOfMonth(), LocalTime.NOON),
+            )
+        logger.info("Opprettet oppgave $oppgaveId [sak=${hendelse.sakId}]")
+        return oppgaveId
+    }
+
+    private fun kanOppretteOppgaveForAktivitetsplikt() =
+        featureToggleService.isEnabled(TidshendelserFeatureToggle.OpprettOppgaveForVarselbrevAktivitetsplikt, false)
+
+    private fun skalLageOmregning(hendelse: TidshendelsePacket) =
+        when (hendelse.jobbtype) {
+            AO_BP20 -> true
+            AO_BP21 -> true
+            AO_OMS67 -> true
+            OMS_DOED_3AAR -> true
+            OMS_DOED_5AAR -> true
+            OMS_DOED_4MND -> false
+        }
+
+    private fun oppgaveTypeFor(type: TidshendelserJobbType): OppgaveType =
+        when (type) {
+            AO_BP20 -> REVURDERING
+            AO_BP21 -> REVURDERING
+            AO_OMS67 -> REVURDERING
+            OMS_DOED_3AAR -> REVURDERING
+            OMS_DOED_5AAR -> REVURDERING
+            OMS_DOED_4MND -> AKTIVITETSPLIKT_OPPFOELGING
+        }
+
+    private fun generateMerknad(type: TidshendelserJobbType): String {
+        return when (type) {
+            AO_BP20 -> "Aldersovergang v/20 år"
+            AO_BP21 -> "Aldersovergang v/21 år"
+            AO_OMS67 -> "Aldersovergang v/67 år"
+            OMS_DOED_3AAR -> "Opphør OMS etter 3 år"
+            OMS_DOED_5AAR -> "Opphør OMS etter 5 år"
+            OMS_DOED_4MND -> "Varselbrev om aktivitetsplikt OMS etter 4 mnd"
+        }
+    }
+}
+
+sealed class TidshendelseResult {
+    data class OpprettetOppgave(val opprettetOppgaveId: UUID) : TidshendelseResult()
+
+    data class OpprettetOmregning(val behandlingId: UUID, val forrigeBehandlingId: UUID) :
+        TidshendelseResult()
+
+    data object Skipped : TidshendelseResult()
+}
---
+++ apps/etterlatte-oppdater-behandling/src/test/kotlin/TidshendelseServiceTest.kt
@@ -0,0 +1,187 @@
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.etterlatte.BehandlingService
+import no.nav.etterlatte.OpprettOmregningResponse
+import no.nav.etterlatte.TidshendelsePacket
+import no.nav.etterlatte.TidshendelseResult
+import no.nav.etterlatte.TidshendelseService
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.AO_BP20
+import no.nav.etterlatte.TidshendelseService.TidshendelserJobbType.OMS_DOED_3AAR
+import no.nav.etterlatte.TidshendelserFeatureToggle
+import no.nav.etterlatte.funksjonsbrytere.DummyFeatureToggleService
+import no.nav.etterlatte.libs.common.behandling.Omregningshendelse
+import no.nav.etterlatte.libs.common.behandling.Prosesstype
+import no.nav.etterlatte.libs.common.behandling.Revurderingaarsak
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Month
+import java.time.YearMonth
+import java.util.UUID
+
+class TidshendelseServiceTest {
+    private val featureToggleService = DummyFeatureToggleService()
+    private val behandlingService = mockk<BehandlingService>()
+    private val tidshendelseService = TidshendelseService(behandlingService, featureToggleService)
+    private val opprettetOppgaveId = UUID.randomUUID()
+    private val forrigeBehandlingId = UUID.randomUUID()
+    private val behandlingId = UUID.randomUUID()
+
+    @BeforeEach
+    fun setUp() {
+        featureToggleService.settBryter(TidshendelserFeatureToggle.OpprettOppgaveForVarselbrevAktivitetsplikt, true)
+
+        every { behandlingService.opprettOppgave(any(), any(), any(), any(), any()) } returns opprettetOppgaveId
+    }
+
+    @Test
+    fun `skal opprette behandling og returnere dens id`() {
+        val hendelseId = UUID.randomUUID()
+        val sakId = 321L
+        val behandlingsmaaned = YearMonth.of(2024, Month.APRIL)
+        val frist = behandlingsmaaned.atEndOfMonth()
+
+        val melding = lagMeldingForVurdertLoependeYtelse(hendelseId, sakId, behandlingsmaaned, dryRun = false)
+
+        val omregningshendelse =
+            Omregningshendelse(
+                sakId = sakId,
+                fradato = behandlingsmaaned.plusMonths(1).atDay(1),
+                prosesstype = Prosesstype.AUTOMATISK,
+                revurderingaarsak = Revurderingaarsak.ALDERSOVERGANG,
+                oppgavefrist = frist,
+            )
+
+        every { behandlingService.opprettOmregning(omregningshendelse) } returns
+            opprettOmregningResponse(sakType = SakType.BARNEPENSJON)
+
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding)) shouldBe
+            TidshendelseResult.OpprettetOmregning(behandlingId, forrigeBehandlingId)
+
+        verify { behandlingService.opprettOmregning(omregningshendelse) }
+    }
+
+    @Test
+    fun `haandterHendelseMedLoependeYtelse med type oms_doed_4mnd skal opprette oppgave`() {
+        val behandlingId = UUID.randomUUID()
+        val opprettetOppgave =
+            tidshendelseService.haandterHendelse(
+                TidshendelsePacket(
+                    lagMeldingForVurdertLoependeYtelse(
+                        sakId = 2,
+                        type = TidshendelserJobbType.OMS_DOED_4MND,
+                        behandlingsmaaned = YearMonth.of(2024, 1),
+                        behandlingId = behandlingId,
+                    ),
+                ),
+            ) as TidshendelseResult.OpprettetOppgave
+
+        opprettetOppgave.opprettetOppgaveId shouldBe opprettetOppgaveId
+        verify {
+            behandlingService.opprettOppgave(
+                sakId = 2,
+                oppgaveType = OppgaveType.AKTIVITETSPLIKT_OPPFOELGING,
+                referanse = behandlingId.toString(),
+                merknad = "Varselbrev om aktivitetsplikt OMS etter 4 mnd",
+                frist = withArg { it shouldNotBe null },
+            )
+        }
+        verify(exactly = 0) {
+            behandlingService.opprettOmregning(any())
+        }
+    }
+
+    @Test
+    fun `skal ikke opprette omregning eller oppgave hvis BP20 og yrkesskadefordel`() {
+        val sakId = 37465L
+        val behandlingsmaaned = YearMonth.of(2024, Month.MARCH)
+
+        val melding = lagMeldingForVurdertLoependeYtelse(sakId, behandlingsmaaned, type = AO_BP20)
+        melding["yrkesskadefordel_pre_20240101"] = true
+
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding))
+
+        verify(exactly = 0) { behandlingService.opprettOmregning(any()) }
+        verify(exactly = 0) { behandlingService.opprettOppgave(sakId, any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun `OMS tre aar siden doedsfall, skal ikke opprette oppgave hvis rett uten tidsbegrensning`() {
+        val hendelseId = UUID.randomUUID()
+        val sakId = 93L
+        val behandlingsmaaned = YearMonth.of(2024, Month.APRIL)
+
+        val melding = lagMeldingForVurdertLoependeYtelse(hendelseId, sakId, behandlingsmaaned, type = OMS_DOED_3AAR)
+        melding["oms_rett_uten_tidsbegrensning"] = true
+
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding))
+
+        verify(exactly = 0) { behandlingService.opprettOppgave(sakId, any(), any(), "Aldersovergang", any()) }
+    }
+
+    @Test
+    fun `skal ikke kalle tjeneste for aa opprette behandling hvis dry-run`() {
+        val hendelseId = UUID.randomUUID()
+        val sakId = 37465L
+        val behandlingsmaaned = YearMonth.of(2024, Month.MARCH)
+
+        val melding = lagMeldingForVurdertLoependeYtelse(hendelseId, sakId, behandlingsmaaned, dryRun = true)
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding))
+
+        verify(exactly = 0) { behandlingService.opprettOmregning(any()) }
+        verify(exactly = 0) { behandlingService.opprettOppgave(any(), any(), any(), any()) }
+    }
+
+    @Test
+    fun `skal opprette oppgave hvis opprett omregning feiler`() {
+        val hendelseId = UUID.randomUUID()
+        val sakId = 37465L
+        val behandlingsmaaned = YearMonth.of(2024, Month.MARCH)
+        every { behandlingService.opprettOmregning(any()) } throws RuntimeException("Feil ved opprettelse av omregning")
+        every { behandlingService.opprettOppgave(any(), any(), any(), any(), any()) } returns opprettetOppgaveId
+
+        val melding = lagMeldingForVurdertLoependeYtelse(hendelseId, sakId, behandlingsmaaned)
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding))
+
+        verify { behandlingService.opprettOmregning(any()) }
+        verify {
+            behandlingService.opprettOppgave(
+                sakId = sakId,
+                oppgaveType = OppgaveType.REVURDERING,
+                referanse = null,
+                merknad = "Aldersovergang v/20 år",
+                frist = any(),
+            )
+        }
+    }
+
+    @Test
+    fun `skal ikke opprette noe hvis feature toggle er av for oms aktivitetsplikt`() {
+        val melding =
+            lagMeldingForVurdertLoependeYtelse(
+                sakId = 37465L,
+                behandlingsmaaned = YearMonth.of(2024, Month.MARCH),
+                type = TidshendelserJobbType.OMS_DOED_4MND,
+            )
+        featureToggleService.settBryter(
+            TidshendelserFeatureToggle.OpprettOppgaveForVarselbrevAktivitetsplikt,
+            false,
+        )
+
+        tidshendelseService.haandterHendelse(TidshendelsePacket(melding))
+
+        verify(exactly = 0) { behandlingService.opprettOmregning(any()) }
+        verify(exactly = 0) {
+            behandlingService.opprettOppgave(any(), any(), any(), any(), any())
+        }
+    }
+
+    fun opprettOmregningResponse(sakType: SakType): OpprettOmregningResponse {
+        return OpprettOmregningResponse(behandlingId, forrigeBehandlingId, sakType)
+    }
+}

799554c7455989e6dfe1f5a795361166d5e3e1b2
Nicolas N <Watercolours@users.noreply.github.com>
2024-04-03 17:41:26 +0200

    FAGSYSTEM-324549 La sb opprette sak hvis ingen finnes (#4396)

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/OpprettSakModal.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { useApiCall } from '~shared/hooks/useApiCall'
+import { Alert, Button, Heading, Modal, Radio, RadioGroup } from '@navikt/ds-react'
+import { PlusCircleIcon } from '@navikt/aksel-icons'
+import { isFailureHandler } from '~shared/api/IsFailureHandler'
+import { FlexRow } from '~shared/styled'
+import { isPending, isSuccess, mapSuccess } from '~shared/api/apiUtils'
+import { hentSakForPerson } from '~shared/api/sak'
+import { SakType } from '~shared/types/sak'
+import { formaterSakstype } from '~utils/formattering'
+
+export const OpprettSakModal = ({ fnr }: { fnr: string }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [sakType, setSakType] = useState<SakType>()
+  const [sakResult, hentEllerOpprettSak] = useApiCall(hentSakForPerson)
+
+  const opprett = () => {
+    if (!sakType) {
+      setIsOpen(false)
+      return
+    }
+
+    hentEllerOpprettSak({ fnr, type: sakType, opprettHvisIkkeFinnes: true }, () => {
+      setTimeout(() => window.location.reload(), 3000)
+    })
+  }
+
+  const avbryt = () => setIsOpen(false)
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setIsOpen(true)} icon={<PlusCircleIcon aria-hidden />}>
+        Opprett sak
+      </Button>
+
+      <Modal open={isOpen} onClose={avbryt} width="medium" aria-label="Opprett sak">
+        <Modal.Body>
+          <Heading size="large" spacing>
+            Opprett sak
+          </Heading>
+
+          <RadioGroup legend="Velg saktype" onChange={(type) => setSakType(type)}>
+            <Radio value={SakType.OMSTILLINGSSTOENAD}>{formaterSakstype(SakType.OMSTILLINGSSTOENAD)}</Radio>
+            <Radio value={SakType.BARNEPENSJON}>{formaterSakstype(SakType.BARNEPENSJON)}</Radio>
+          </RadioGroup>
+
+          <br />
+
+          {!sakType ? (
+            <Alert variant="warning" inline>
+              Du må velge sakstype for å opprette ny sak
+            </Alert>
+          ) : (
+            <Alert variant="info">
+              Du oppretter nå en sak av typen <strong>{formaterSakstype(sakType).toLowerCase()}</strong>
+            </Alert>
+          )}
+
+          {isFailureHandler({
+            apiResult: sakResult,
+            errorMessage: `Kunne ikke opprette sak for ${fnr}`,
+          })}
+
+          {mapSuccess(sakResult, (sak) => (
+            <Alert variant="success">
+              Opprettet {formaterSakstype(sak.sakType)} med id {sak.id}! Laster siden på nytt...
+            </Alert>
+          ))}
+        </Modal.Body>
+
+        <Modal.Footer>
+          <FlexRow justify="right">
+            <Button
+              variant="secondary"
+              type="button"
+              disabled={isPending(sakResult) || isSuccess(sakResult)}
+              onClick={avbryt}
+            >
+              Avbryt
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
+              loading={isPending(sakResult) || isSuccess(sakResult)}
+              disabled={!sakType}
+              onClick={opprett}
+            >
+              Opprett sak
+            </Button>
+          </FlexRow>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}

2d4edbc7a962e46ab5a5618bfba24aa0e516da1f
Chris Searle <chris@chrissearle.org>
2024-04-03 08:57:36 +0200

    FAGSYSTEM-324425 - ikke valider og sett status hvis avslag
    Dette vises ved avslag kun for utlandssaker - og her er TT opsjonelt. Det brukes ikke til noe
    annet enn at sh. vil ha et kopi lagret.

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/TrygdetidVisning.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/TrygdetidVisning.tsx
@@ -46,7 +46,7 @@ const TrygdetidVisning = (props: { behandling: IDetaljertBehandling }) => {
 
   const sjekkGyldighetOgOppdaterStatus = () => {
     oppdaterStatusRequest(behandling.id, (result) => {
-      if (result.statusOppdatert) {
+      if (result.statusOppdatert && vedtaksresultat != 'avslag') {
         dispatch(oppdaterBehandlingsstatus(IBehandlingStatus.TRYGDETID_OPPDATERT))
       }
       next()

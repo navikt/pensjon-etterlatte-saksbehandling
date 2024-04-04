df1f17a112e8dd5068f460bd210d51ed26e3b6ca
Mads Opheim <mads.opheim@nav.no>
2024-04-04 13:48:39 +0200

    Rettar merknad i oppgåver som no ikkje fekk det pga feilen

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V108__merknad_gjenoppretta.sql
@@ -0,0 +1,2 @@
+UPDATE oppgave SET merknad = 'Gjenopptak - Manuell behandling pga aldersovergang' where id = '0a66c872-60d4-474f-ab4b-114e810b5ef0' and merknad = '';
+UPDATE oppgave SET merknad = 'Gjenopptak - Manuell behandling pga aldersovergang' where id = '082f91dd-17fa-43f2-8a9a-a6c8b0b44e5f' and merknad = '';

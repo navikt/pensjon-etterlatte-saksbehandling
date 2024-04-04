ebc8d7bad73a1c2e84ba78d1e13d7ca46a01ac62
Nicolas N <Watercolours@users.noreply.github.com>
2024-04-04 10:25:52 +0200

    skjule SettPaaVent hvis oppgave ferdigstilt (#4402)

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVent.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVent.tsx
@@ -14,12 +14,10 @@ export const SettPaaVent = ({
   redigerbar,
   refreshOppgave,
 }: {
-  oppgave?: OppgaveDTO
+  oppgave: OppgaveDTO
   redigerbar: boolean
   refreshOppgave: () => void
 }) => {
-  if (!oppgave) return null
-
   const [frist, setFrist] = useState<string>(oppgave.frist)
   const [merknad, setMerknad] = useState<string>(oppgave.merknad || '')
   const [settPaaVent, setVisPaaVent] = useState(false)

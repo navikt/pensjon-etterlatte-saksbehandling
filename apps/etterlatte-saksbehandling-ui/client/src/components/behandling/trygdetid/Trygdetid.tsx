853ad6bd53346f2bbff2bb435daaed9cf3a12841
Chris Searle <chris@chrissearle.org>
2024-04-05 10:10:41 +0200

    Trygdetid: Vis navn og ikke bare fnr når det er flere avdøde og vi finner navn i personopplysninger

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/Trygdetid.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/Trygdetid.tsx
@@ -18,6 +18,7 @@ import { VedtakResultat } from '~components/behandling/useVedtaksResultat'
 import { EnkelPersonTrygdetid } from '~components/behandling/trygdetid/EnkelPersonTrygdetid'
 import { BeregnetSamletTrygdetid } from '~components/behandling/trygdetid/detaljer/BeregnetSamletTrygdetid'
 import { useFeatureEnabledMedDefault } from '~shared/hooks/useFeatureToggle'
+import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
 
 const TrygdetidMelding = ({ overskrift, beskrivelse }: { overskrift: string; beskrivelse: string }) => {
   return (
@@ -58,6 +59,20 @@ export const Trygdetid = ({ redigerbar, behandling, vedtaksresultat, virkningsti
   const [trygdetidManglerVedAvslag, setTrygdetidManglerVedAvslag] = useState(false)
 
   const visFlereTrygdetider = useFeatureEnabledMedDefault('foreldreloes', false)
+
+  const personopplysninger = usePersonopplysninger()
+
+  const mapNavn = (fnr: string): string => {
+    const opplysning = personopplysninger?.avdoede?.find(
+      (personOpplysning) => personOpplysning.opplysning.foedselsnummer === fnr
+    )?.opplysning
+
+    if (!opplysning) {
+      return fnr
+    }
+
+    return `${opplysning.fornavn} ${opplysning.etternavn} (${fnr})`
+  }
 
   const oppdaterTrygdetider = (trygdetid: ITrygdetid[]) => {
     setTrygdetider(trygdetid)
@@ -170,13 +185,13 @@ export const Trygdetid = ({ redigerbar, behandling, vedtaksresultat, virkningsti
               <Tabs defaultValue={trygdetider[0].ident}>
                 <Tabs.List>
                   {trygdetider.map((trygdetid) => (
-                    <Tabs.Tab key={trygdetid.ident} value={trygdetid.ident} label={trygdetid.ident} />
+                    <Tabs.Tab key={trygdetid.ident} value={trygdetid.ident} label={mapNavn(trygdetid.ident)} />
                   ))}
                 </Tabs.List>
                 {trygdetider.map((trygdetid) => (
                   <Tabs.Panel value={trygdetid.ident} key={trygdetid.ident}>
                     <HeadingWrapper size="small" level="3">
-                      Trygdetid for {trygdetid.ident}
+                      Trygdetid for {mapNavn(trygdetid.ident)}
                     </HeadingWrapper>
 
                     <EnkelPersonTrygdetid

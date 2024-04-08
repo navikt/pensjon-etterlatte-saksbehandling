b85c98b9e6264a9a511dbb2d556d6a9b60052035
Mads Opheim <mads.opheim@nav.no>
2024-04-08 16:55:54 +0200

    Endrar rekkjefølgja så den som skjer først står øvst

--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/ReguleringHendelseType.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/ReguleringHendelseType.kt
@@ -3,12 +3,12 @@ package no.nav.etterlatte.rapidsandrivers
 import no.nav.etterlatte.libs.common.event.EventnameHendelseType
 
 enum class ReguleringHendelseType : EventnameHendelseType {
+    START_REGULERING,
     FINN_LOEPENDE_YTELSER,
     LOEPENDE_YTELSE_FUNNET,
     BEHANDLING_OPPRETTA,
     VILKAARSVURDERT,
     BEREGNA,
-    START_REGULERING,
     ;
 
     override fun lagEventnameForType(): String = "REGULERING:${this.name}"

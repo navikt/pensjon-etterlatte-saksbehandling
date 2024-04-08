4962d816ced38db15eea0ee7f85c3abc8212034f
Henrik Gundersen <henrik.gundersen@nav.no>
2024-04-08 17:41:44 +0200

    EY-3638: Legger på react-hook-form i tilbakekrevingsflyt (#4435)
    * EY-3638: Legger på react-hook-form i tilbakekrevingsflyt
    
    * EY-3638: Mindre justeringer
    
    * EY-3638: Fikser bug

--- apps/etterlatte-saksbehandling-ui/client/src/shared/components/radioGruppe/ControlledRadioGruppe.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/components/radioGruppe/ControlledRadioGruppe.tsx
@@ -5,9 +5,11 @@ import { RadioGroup } from '@navikt/ds-react'
 interface Props<T extends FieldValues> {
   name: Path<T>
   control: Control<T>
-  errorVedTomInput: string
+  errorVedTomInput?: string
   legend: ReactNode | string
+  hideLegend?: boolean
   radios: ReactNode | Array<ReactNode>
+  size?: 'medium' | 'small'
 }
 
 export const ControlledRadioGruppe = <T extends FieldValues>({
@@ -16,19 +18,30 @@ export const ControlledRadioGruppe = <T extends FieldValues>({
   radios,
   errorVedTomInput,
   legend,
+  hideLegend,
+  size,
 }: Props<T>): ReactNode => {
   return (
     <Controller
       name={name}
       rules={{
-        required: {
-          value: true,
-          message: errorVedTomInput,
-        },
+        required: errorVedTomInput
+          ? {
+              value: true,
+              message: errorVedTomInput,
+            }
+          : undefined,
       }}
       control={control}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <RadioGroup onChange={onChange} value={value} error={error?.message} legend={legend}>
+        <RadioGroup
+          onChange={onChange}
+          value={value}
+          error={error?.message}
+          legend={legend}
+          hideLegend={hideLegend}
+          size={size}
+        >
           {radios}
         </RadioGroup>
       )}

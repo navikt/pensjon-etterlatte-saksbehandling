3c33388f1dfaf0a0735ef022375efd394224a724
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-04-09 12:52:57 +0200

    Legg til alerts for flyktning og utland

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversikt.tsx
@@ -1,28 +1,16 @@
-import { BehandlingTable } from './BehandlingTable'
 import styled from 'styled-components'
-import { Container, FlexRow, GridContainer, SpaceChildren } from '~shared/styled'
+import { Container, SpaceChildren } from '~shared/styled'
 import Spinner from '~shared/Spinner'
-import RelevanteHendelser from '~components/person/uhaandtereHendelser/RelevanteHendelser'
-import { Alert, BodyShort, Heading, HelpText, HStack, ReadMore, Tag } from '@navikt/ds-react'
-import { formaterEnumTilLesbarString, formaterSakstype, formaterStringDato } from '~utils/formattering'
-import { KlageListe } from '~components/person/KlageListe'
-import { tagColors } from '~shared/Tags'
+import { Alert, Heading } from '@navikt/ds-react'
+import { formaterStringDato } from '~utils/formattering'
 import { SakMedBehandlinger } from '~components/person/typer'
-import { isSuccess, mapApiResult, mapResult, Result } from '~shared/api/apiUtils'
+import { isSuccess, mapResult, Result } from '~shared/api/apiUtils'
 import React, { useEffect } from 'react'
 import { useApiCall } from '~shared/hooks/useApiCall'
-import { EndreEnhet } from '~components/person/EndreEnhet'
-import { hentFlyktningStatusForSak, hentNavkontorForPerson } from '~shared/api/sak'
-import { isFailureHandler } from '~shared/api/IsFailureHandler'
-import { useAppSelector } from '~store/Store'
-import { TilbakekrevingListe } from '~components/person/TilbakekrevingListe'
-import { ApiErrorAlert, ApiWarningAlert } from '~ErrorBoundary'
-import { enhetErSkrivbar } from '~components/behandling/felles/utils'
+import { hentFlyktningStatusForSak } from '~shared/api/sak'
 import { SakType } from '~shared/types/sak'
 import { hentMigrertYrkesskadeFordel } from '~shared/api/vilkaarsvurdering'
 import { Vedtaksloesning } from '~shared/types/IDetaljertBehandling'
-import { OpprettSakModal } from '~components/person/sakOgBehandling/OpprettSakModal'
-import { Buildings3Icon, LocationPinIcon } from '@navikt/aksel-icons'
 import { SakOversiktHeader } from '~components/person/sakOgBehandling/SakOversiktHeader'
 import { SakIkkeFunnet } from '~components/person/sakOgBehandling/SakIkkeFunnet'
 import { ForenkletOppgaverTable } from '~components/person/sakOgBehandling/ForenkletOppgaverTable'
@@ -30,15 +18,11 @@ import { ForenkletOppgaverTable } from '~components/person/sakOgBehandling/Foren
 const ETTERLATTEREFORM_DATO = '2024-01'
 
 export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehandlinger>; fnr: string }) => {
-  const [navkontorResult, hentNavkontor] = useApiCall(hentNavkontorForPerson)
-  const [hentFlyktningStatus, hentFlyktning] = useApiCall(hentFlyktningStatusForSak)
-  const [yrkesskadefordelStatus, hentYrkesskadefordel] = useApiCall(hentMigrertYrkesskadeFordel)
-
-  const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
+  const [flyktningResult, hentFlyktning] = useApiCall(hentFlyktningStatusForSak)
+  const [yrkesskadefordelResult, hentYrkesskadefordel] = useApiCall(hentMigrertYrkesskadeFordel)
 
   useEffect(() => {
     if (isSuccess(sakResult)) {
-      hentNavkontor(fnr)
       hentFlyktning(sakResult.data.sak.id)
 
       const migrertBehandling =
@@ -58,11 +42,42 @@ export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehand
       {mapResult(sakResult, {
         pending: <Spinner visible label="Henter sak og behandlinger" />,
         error: (error) => <SakIkkeFunnet error={error} fnr={fnr} />,
-        success: ({ sak, behandlinger }) => (
+        success: ({ sak }) => (
           <SpaceChildren gap="2rem">
-            <SakOversiktHeader sak={sak} navkontorResult={navkontorResult} />
+            <SakOversiktHeader fnr={fnr} sak={sak} />
 
             <Skille />
+
+            {mapResult(flyktningResult, {
+              success: (data) =>
+                !!data?.erFlyktning && (
+                  <>
+                    <div>
+                      <Alert variant="info" size="small">
+                        Saken er markert med flyktning i Pesys og første virkningstidspunkt var{' '}
+                        {formaterStringDato(data.virkningstidspunkt)}
+                      </Alert>
+                    </div>
+
+                    <Skille />
+                  </>
+                ),
+            })}
+
+            {mapResult(yrkesskadefordelResult, {
+              success: (data) =>
+                !!data && (
+                  <>
+                    <div>
+                      <Alert variant="info" size="small">
+                        Søker har yrkesskadefordel fra før 01.01.2024 og har rett til stønad til fylte 21 år.
+                      </Alert>
+                    </div>
+
+                    <Skille />
+                  </>
+                ),
+            })}
 
             <div>
               <Heading size="medium" spacing>
@@ -70,95 +85,16 @@ export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehand
               </Heading>
               <ForenkletOppgaverTable sakId={sak.id} />
             </div>
-
-            <div>
-              <Heading size="medium" spacing>
-                Behandlinger
-              </Heading>
-              <BehandlingTable sakOgBehandlinger={{ sak, behandlinger }} />
-            </div>
           </SpaceChildren>
         ),
       })}
     </Container>
-    // <GridContainer>
-    //     (sakOgBehandlinger) => (
-    //       <>
-    //         <MainContent>
-    //           {isSuccess(hentFlyktningStatus) && hentFlyktningStatus.data?.erFlyktning && (
-    //             <>
-    //               <FlexRow>
-    //                 <Alert variant="info">
-    //                   Saken er markert med flyktning i Pesys og første virkningstidspunkt var{' '}
-    //                   {formaterStringDato(hentFlyktningStatus.data.virkningstidspunkt)}
-    //                 </Alert>
-    //               </FlexRow>
-    //               <hr />
-    //             </>
-    //           )}
-    //           {isFailureHandler({
-    //             apiResult: hentFlyktningStatus,
-    //             errorMessage: 'Klarte ikke hente informasjon om flyktningstatus',
-    //           })}
-    //           {mapResult(yrkesskadefordelStatus, {
-    //             success: (yrkesskadefordelSvar) =>
-    //               yrkesskadefordelSvar.migrertYrkesskadefordel && (
-    //                 <>
-    //                   <FlexRow>
-    //                     <Alert variant="info">
-    //                       Søker har yrkesskadefordel fra før 01.01.2024 og har rett til stønad til fylte 21 år.
-    //                     </Alert>
-    //                   </FlexRow>
-    //                   <hr />
-    //                 </>
-    //               ),
-    //             pending: <Spinner visible={true} label="Henter status yrkesskadefordel" />,
-    //             error: () => <ApiErrorAlert>Klarte ikke hente informasjon om yrkesskadefordel</ApiErrorAlert>,
-    //           })}
-    //           <SelectWrapper>
-    //             <BodyShort spacing>Denne saken tilhører enhet {sakOgBehandlinger.sak.enhet}.</BodyShort>
-    //             {enhetErSkrivbar(sakOgBehandlinger.sak.enhet, innloggetSaksbehandler.skriveEnheter) && (
-    //               <FlexRow>
-    //                 <EndreEnhet sakId={sakOgBehandlinger.sak.id} />
-    //                 <HelpText strategy="fixed">
-    //                   Om saken tilhører en annen enhet enn den du jobber i, overfører du saken til riktig enhet ved å
-    //                   klikke på denne knappen. Skriv først i kommentarfeltet i Sjekklisten inne i behandlingen hvilken
-    //                   enhet saken er overført til og hvorfor. Gå så til saksoversikten, og klikk på knappen &rsquo;Endre
-    //                   enhet&rsquo;, og overfør til riktig behandlende enhet.
-    //                 </HelpText>
-    //               </FlexRow>
-    //             )}
-    //           </SelectWrapper>
-    //
-    //           <KlageListe sakId={sakOgBehandlinger.sak.id} />
-    //           <TilbakekrevingListe sakId={sakOgBehandlinger.sak.id} />
-    //         </MainContent>
-    //         <HendelseSidebar>
-    //           <RelevanteHendelser sak={sakOgBehandlinger.sak} behandlingliste={sakOgBehandlinger.behandlinger} />
-    //         </HendelseSidebar>
-    //       </>
-    //     )
-    //   )}
-    // </GridContainer>
   )
 }
 
 const Skille = styled.hr`
   border-color: var(--a-surface-active);
   width: 100%;
-  align-self: center;
-`
-
-const MainContent = styled.div`
-  flex: 1 0 auto;
-  margin: 3em 1em;
-`
-
-const HendelseSidebar = styled.div`
-  min-width: 40rem;
-  border-left: 1px solid gray;
-  padding: 3em 2rem;
-  margin: 0 1em;
 `
 
 export const HeadingWrapper = styled.div`
--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversiktHeader.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversiktHeader.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { mapResult, Result } from '~shared/api/apiUtils'
-import { Navkontor } from '~shared/api/sak'
+import React, { useEffect } from 'react'
+import { mapResult } from '~shared/api/apiUtils'
+import { hentNavkontorForPerson } from '~shared/api/sak'
 import { BodyShort, Heading, ReadMore, Tag } from '@navikt/ds-react'
 import { FlexRow, SpaceChildren } from '~shared/styled'
 import { tagColors } from '~shared/Tags'
 import { formaterEnumTilLesbarString } from '~utils/formattering'
 import { Buildings3Icon, LocationPinIcon } from '@navikt/aksel-icons'
 import Spinner from '~shared/Spinner'
-import { EndreEnhet } from '~components/person/EndreEnhet'
+import { EndreEnhet } from '~components/person/sakOgBehandling/EndreEnhet'
 import { ISakMedUtlandstilknytning } from '~shared/types/sak'
 import styled from 'styled-components'
+import { useAppSelector } from '~store/Store'
+import { enhetErSkrivbar } from '~components/behandling/felles/utils'
+import { useApiCall } from '~shared/hooks/useApiCall'
 
-export const SakOversiktHeader = ({
-  sak,
-  navkontorResult,
-}: {
-  sak: ISakMedUtlandstilknytning
-  navkontorResult: Result<Navkontor>
-}) => {
+export const SakOversiktHeader = ({ sak, fnr }: { sak: ISakMedUtlandstilknytning; fnr: string }) => {
+  const innloggetSaksbehandler = useAppSelector((state) => state.saksbehandlerReducer.innloggetSaksbehandler)
+
+  const [navkontorResult, hentNavkontor] = useApiCall(hentNavkontorForPerson)
+
+  useEffect(() => {
+    hentNavkontor(fnr)
+  }, [])
+
   return (
     <SpaceChildren>
       <Heading size="large">Saksnummer {sak.id}</Heading>
@@ -46,18 +51,20 @@ export const SakOversiktHeader = ({
         <FlexRow align="center">
           <Buildings3Icon aria-hidden width="1.75rem" height="1.75rem" />
           <BodyShort>Denne saken tilhører enhet {sak.enhet}</BodyShort>
-          <EndreEnhet sakId={sak.id} />
+          {enhetErSkrivbar(sak.enhet, innloggetSaksbehandler.skriveEnheter) && <EndreEnhet sakId={sak.id} />}
         </FlexRow>
       </SpaceChildren>
-      <ByttEnhetReadMore header="Ønsker du å bytte enhet?">
-        <ol>
-          <li>
-            Skriv i kommentarfeltet i sjekklisten inne i behandlingen hvilken enhet saken overføres til og hvorfor.
-          </li>
-          <li>Deretter går du til saksoversikten og klikker på knappen ovenfor for å endre enhet.</li>
-          <li>Overfør til riktig behandlende enhet.</li>
-        </ol>
-      </ByttEnhetReadMore>
+      {enhetErSkrivbar(sak.enhet, innloggetSaksbehandler.skriveEnheter) && (
+        <ByttEnhetReadMore header="Ønsker du å bytte enhet?">
+          <ol>
+            <li>
+              Skriv i kommentarfeltet i sjekklisten inne i behandlingen hvilken enhet saken overføres til og hvorfor.
+            </li>
+            <li>Deretter går du til saksoversikten og klikker på knappen ovenfor for å endre enhet.</li>
+            <li>Overfør til riktig behandlende enhet.</li>
+          </ol>
+        </ByttEnhetReadMore>
+      )}
     </SpaceChildren>
   )
 }

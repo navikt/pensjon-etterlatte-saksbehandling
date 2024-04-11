bf3185694cf85971df6a464ea5a0f191d8f36eff
Emil Schrøder <emil.schroder@nav.no>
2024-04-11 07:48:28 +0200

    Fjern skrivetilgang ved get og fiks bedre logging

--- apps/etterlatte-beregning/src/main/kotlin/sanksjon/SanksjonRoutes.kt
+++ apps/etterlatte-beregning/src/main/kotlin/sanksjon/SanksjonRoutes.kt
@@ -2,11 +2,9 @@ package no.nav.etterlatte.sanksjon
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.application.log
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.application
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
@@ -15,16 +13,17 @@ import no.nav.etterlatte.klienter.BehandlingKlient
 import no.nav.etterlatte.libs.ktor.brukerTokenInfo
 import no.nav.etterlatte.libs.ktor.route.BEHANDLINGID_CALL_PARAMETER
 import no.nav.etterlatte.libs.ktor.route.withBehandlingId
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("sanksjonRoute")
 
 fun Route.sanksjon(
     sanksjonService: SanksjonService,
     behandlingKlient: BehandlingKlient,
 ) {
     route("/api/beregning/sanksjon/{$BEHANDLINGID_CALL_PARAMETER}") {
-        val logger = application.log
-
         get {
-            withBehandlingId(behandlingKlient, skrivetilgang = true) {
+            withBehandlingId(behandlingKlient, skrivetilgang = false) {
                 logger.info("Henter sanksjoner for behandlingId=$it")
                 val sanksjoner = sanksjonService.hentSanksjon(it)
                 when (sanksjoner) {
@@ -47,7 +46,7 @@ fun Route.sanksjon(
         delete {
             withBehandlingId(behandlingKlient, skrivetilgang = true) {
                 logger.info("Sletter sanksjon for behandlingId=$it")
-                call.respond(HttpStatusCode.OK)
+                call.respond(HttpStatusCode.NotImplemented)
             }
         }
     }

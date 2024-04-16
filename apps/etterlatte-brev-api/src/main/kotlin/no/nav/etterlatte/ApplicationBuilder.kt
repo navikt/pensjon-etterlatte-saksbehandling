1817e48968edd04d12c25eb8bb3efa5d4145de6e
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-04-16 15:07:44 +0200

    Send brev i fire iverksatte gjenoppretta saker (#4537)
    * Revert "Tar bort FiksEnkeltbrevRiver, per no har vi ingen enkeltbrev å fikse (#4189)"
    
    This reverts commit 08dcb8e3
    
    * Fire brev til fiks som er oppretta, men ikkje generert pdf av eller ferdigstilt

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/ApplicationBuilder.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/ApplicationBuilder.kt
@@ -77,13 +77,17 @@ import no.nav.etterlatte.rivers.OpprettJournalfoerOgDistribuerRiver
 import no.nav.etterlatte.rivers.StartBrevgenereringRepository
 import no.nav.etterlatte.rivers.StartInformasjonsbrevgenereringRiver
 import no.nav.etterlatte.rivers.VedtaksbrevUnderkjentRiver
+import no.nav.etterlatte.rivers.migrering.FiksEnkeltbrevRiver
 import no.nav.etterlatte.rivers.migrering.OpprettVarselbrevForGjenopprettaRiver
 import no.nav.etterlatte.rivers.migrering.OpprettVedtaksbrevForMigreringRiver
+import no.nav.etterlatte.rivers.migrering.behandlingerAaJournalfoereBrevFor
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.pensjon.brevbaker.api.model.RenderedJsonLetter
 import org.slf4j.Logger
+import java.util.UUID
+import kotlin.concurrent.thread
 
 val sikkerLogg: Logger = sikkerlogger()
 
@@ -272,6 +276,8 @@ class ApplicationBuilder {
                     behandlingKlient,
                     featureToggleService,
                 )
+                FiksEnkeltbrevRiver(this, vedtaksbrevService, ferdigstillJournalfoerOgDistribuerBrev)
+                    .also { fiksEnkeltbrev() }
                 OpprettJournalfoerOgDistribuerRiver(
                     this,
                     brevdataFacade,
@@ -283,6 +289,19 @@ class ApplicationBuilder {
                 VedtaksbrevUnderkjentRiver(this, vedtaksbrevService)
                 DistribuerBrevRiver(this, brevdistribuerer)
             }
+
+    private fun fiksEnkeltbrev() {
+        thread {
+            Thread.sleep(60_000)
+            behandlingerAaJournalfoereBrevFor.forEach {
+                rapidsConnection.publish(
+                    message = lagMelding(behandlingId = it),
+                    key = UUID.randomUUID().toString(),
+                )
+                Thread.sleep(3000)
+            }
+        }
+    }
 
     private fun lagMelding(behandlingId: String) =
         JsonMessage.newMessage(
---
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/migrering/FiksEnkeltbrevRiver.kt
@@ -0,0 +1,70 @@
+package no.nav.etterlatte.rivers.migrering
+
+import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.brev.Brevkoder
+import no.nav.etterlatte.brev.MigreringBrevRequest
+import no.nav.etterlatte.brev.VedtaksbrevService
+import no.nav.etterlatte.libs.ktor.token.Systembruker
+import no.nav.etterlatte.rapidsandrivers.BEHANDLING_ID_KEY
+import no.nav.etterlatte.rapidsandrivers.Kontekst
+import no.nav.etterlatte.rapidsandrivers.ListenerMedLoggingOgFeilhaandtering
+import no.nav.etterlatte.rapidsandrivers.behandlingId
+import no.nav.etterlatte.rapidsandrivers.migrering.Migreringshendelser
+import no.nav.etterlatte.rivers.FerdigstillJournalfoerOgDistribuerBrev
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+
+internal class FiksEnkeltbrevRiver(
+    rapidsConnection: RapidsConnection,
+    private val service: VedtaksbrevService,
+    private val ferdigstillJournalfoerOgDistribuerBrev: FerdigstillJournalfoerOgDistribuerBrev,
+) : ListenerMedLoggingOgFeilhaandtering() {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        initialiserRiver(rapidsConnection, Migreringshendelser.FIKS_ENKELTBREV) {
+            validate { it.requireKey(BEHANDLING_ID_KEY) }
+        }
+    }
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        val behandlingId = packet.behandlingId
+        logger.info("Fikser vedtaksbrev for behandling $behandlingId")
+
+        runBlocking {
+            val bruker = Systembruker.migrering
+            val vedtaksbrev = requireNotNull(service.hentVedtaksbrev(behandlingId))
+            logger.info("Henta brev ${vedtaksbrev.id} for ferdigstilling og utsending")
+            service.genererPdf(
+                id = vedtaksbrev.id,
+                bruker = bruker,
+                automatiskMigreringRequest = MigreringBrevRequest(0, true, null),
+            )
+            logger.info("Genererte pdf for $vedtaksbrev.id")
+            service.ferdigstillVedtaksbrev(vedtaksbrev.behandlingId!!, bruker, true)
+            logger.info("Ferdigstilte $vedtaksbrev.id, klar til journalføring")
+            ferdigstillJournalfoerOgDistribuerBrev.journalfoerOgDistribuer(
+                Brevkoder.BP_INNVILGELSE,
+                vedtaksbrev.sakId,
+                vedtaksbrev.id,
+                bruker,
+            )
+        }
+        context.publish(packet.toJson())
+    }
+
+    override fun kontekst() = Kontekst.MIGRERING
+}
+
+val behandlingerAaJournalfoereBrevFor =
+    listOf(
+        "36fb86d4-698e-43cb-ab5f-ee7a0febda2e",
+        "038dd6b8-c17b-482e-ae26-51ae591d5c42",
+        "278d1bdb-2f38-484a-8d98-b74a4220dcea",
+        "00051cfe-0405-4ebf-93f6-5775d23afcba",
+    )

dd14e58a5e7f9b7d3bb3db5782bf628ffad1f9b1
Mads Opheim <mads.opheim@nav.no>
2024-04-16 15:45:31 +0200

    Workaround for å få gjennom brev i gjenopprettingssaker

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/bp/BarnepensjonBeregning.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/bp/BarnepensjonBeregning.kt
@@ -11,6 +11,7 @@ import no.nav.etterlatte.brev.model.fromDto
 import no.nav.etterlatte.grunnbeloep.Grunnbeloep
 import no.nav.etterlatte.libs.common.trygdetid.TrygdetidDto
 import no.nav.pensjon.brevbaker.api.model.Kroner
+import java.util.UUID
 
 internal fun barnepensjonBeregningsperioder(utbetalingsinfo: Utbetalingsinfo) =
     utbetalingsinfo.beregningsperioder.map {
@@ -48,8 +49,17 @@ internal fun barnepensjonBeregning(
         trygdetid = trygdetid.map { it.fromDto(anvendtMetode, metodeFraGrunnlag, avdoede) },
         erForeldreloes = erForeldreloes,
         bruktTrygdetid =
-            trygdetid.find { it.ident == sisteBeregningsperiode.trygdetidForIdent }
-                ?.fromDto(anvendtMetode, metodeFraGrunnlag, avdoede)
+            trygdetid.find {
+                (it.ident == sisteBeregningsperiode.trygdetidForIdent) || (
+                    it.behandlingId in
+                        listOf(
+                            UUID.fromString("36fb86d4-698e-43cb-ab5f-ee7a0febda2e"),
+                            UUID.fromString("038dd6b8-c17b-482e-ae26-51ae591d5c42"),
+                            UUID.fromString("278d1bdb-2f38-484a-8d98-b74a4220dcea"),
+                            UUID.fromString("00051cfe-0405-4ebf-93f6-5775d23afcba"),
+                        )
+                )
+            }?.fromDto(anvendtMetode, metodeFraGrunnlag, avdoede)
                 ?: throw ManglerAvdoedBruktTilTrygdetid(),
     )
 }

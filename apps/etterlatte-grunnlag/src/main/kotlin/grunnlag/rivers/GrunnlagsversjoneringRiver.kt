0be69577b0fdeffc4f80474d946630afb2102cb7
Nicolas N <Watercolours@users.noreply.github.com>
2024-04-16 09:16:15 +0200

    EY-3816 Fjerne rester fra vi versjonerte grunnlag (#4513)
    * EY-3816 Fjerne rester fra vi versjonerte grunnlag
    
    * fjerne github workflow også

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/rivers/GrunnlagsversjoneringRiver.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/rivers/GrunnlagsversjoneringRiver.kt
@@ -1,11 +1,7 @@
 package no.nav.etterlatte.grunnlag.rivers
 
-import kotlinx.coroutines.runBlocking
 import no.nav.etterlatte.grunnlag.GrunnlagService
-import no.nav.etterlatte.grunnlag.klienter.BehandlingKlient
-import no.nav.etterlatte.libs.common.behandling.BehandlingStatus
 import no.nav.etterlatte.libs.common.vedtak.VedtakKafkaHendelseHendelseType
-import no.nav.etterlatte.rapidsandrivers.EventNames
 import no.nav.etterlatte.rapidsandrivers.ListenerMedLogging
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
@@ -13,45 +9,6 @@ import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.UUID
-
-// TODO: Fjerne når grunnlag er versjonert (EY-2567)
-class InitBehandlingVersjonRiver(
-    rapidsConnection: RapidsConnection,
-    private val behandlingKlient: BehandlingKlient,
-    private val grunnlagService: GrunnlagService,
-) : ListenerMedLogging() {
-    private val logger: Logger = LoggerFactory.getLogger(GrunnlagsversjoneringRiver::class.java)
-
-    init {
-        initialiserRiver(rapidsConnection, EventNames.GRUNNLAGSVERSJONERING_EVENT_NAME) {}
-    }
-
-    override fun haandterPakke(
-        packet: JsonMessage,
-        context: MessageContext,
-    ) {
-        try {
-            val alleSakIderFraGrunnlag = grunnlagService.hentAlleSakIder()
-
-            logger.info("Fant ${alleSakIderFraGrunnlag.size} unike sak ider i grunnlag")
-
-            alleSakIderFraGrunnlag
-                .flatMap { sakId ->
-                    runBlocking {
-                        behandlingKlient.hentBehandlinger(sakId).behandlinger
-                            .also { logger.info("Fant ${it.size} behandlinger for sak=$sakId") }
-                    }
-                }
-                .forEach {
-                    logger.info("Forsøker å koble behandling mot hendelsenummer")
-                    val skalLaaseVersjon = it.status !in BehandlingStatus.underBehandling()
-                    grunnlagService.oppdaterVersjonForBehandling(it.sakId, it.behandlingId, skalLaaseVersjon)
-                }
-        } catch (e: Exception) {
-            logger.error("Feil ved oppdatering av versjon for behandling: ", e)
-        }
-    }
-}
 
 class GrunnlagsversjoneringRiver(
     rapidsConnection: RapidsConnection,

23c9fa3df8a3b4d0e1aed3e25f4f7330e85a2986
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-04-18 15:14:11 +0200

    Vi må fikse der resultatet er satt feilaktig

--- apps/etterlatte-statistikk/src/main/resources/db/migration/V34__fix_avbrudd_klage_resultat.sql
+++ apps/etterlatte-statistikk/src/main/resources/db/migration/V34__fix_avbrudd_klage_resultat.sql
@@ -2,5 +2,4 @@ UPDATE sak
 set behandling_resultat = 'AVBRUTT',
     ferdigbehandlet_tid = registrert_tid
 WHERE behandling_type = 'KLAGE'
-  and behandling_resultat is null
   and behandling_status = 'AVBRUTT';

e4d9f8e1feef3b0c22a8bda4c7a9f0ae93293a50
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-04-18 10:46:06 +0200

    Sjekke virk mot iverksatt eller samordet

--- apps/etterlatte-behandling/src/main/kotlin/common/TidligsteIverksatteVirkningstidspunkt.kt
+++ apps/etterlatte-behandling/src/main/kotlin/common/TidligsteIverksatteVirkningstidspunkt.kt
@@ -5,6 +5,6 @@ import no.nav.etterlatte.libs.common.behandling.BehandlingStatus
 import no.nav.etterlatte.libs.common.behandling.Virkningstidspunkt
 
 fun List<Behandling>.tidligsteIverksatteVirkningstidspunkt(): Virkningstidspunkt? =
-    this.filter { it.status == BehandlingStatus.IVERKSATT }
+    this.filter { it.status in listOf(BehandlingStatus.IVERKSATT, BehandlingStatus.SAMORDNET) }
         .mapNotNull { it.virkningstidspunkt }
         .minByOrNull { it.dato }

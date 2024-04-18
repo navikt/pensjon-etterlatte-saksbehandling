619dcd4991f9537b81273fa822adb3e19c698458
Mads Opheim <mads.opheim@nav.no>
2024-04-18 17:48:06 +0200

    Sender med gruppeid som param, og justerer med det samme ned så vi eksperimenterer med færre familiar

--- apps/etterlatte-testdata/src/main/kotlin/no/nav/etterlatte/testdata/features/OpprettOgBehandle.kt
+++ apps/etterlatte-testdata/src/main/kotlin/no/nav/etterlatte/testdata/features/OpprettOgBehandle.kt
@@ -52,7 +52,7 @@ class OpprettOgBehandle(private val dollyService: DollyService) : TestDataFeatur
 
             post {
                 val familier = opprettFamilierIDolly(
-                    100,
+                    2,
                     call.receiveParameters()["gruppeId"]!!.toLong(),
                     Duration.ofSeconds(45)
                 )

dead19a88545baec788df7d9e06e94a2d602bab6
Mads Opheim <mads.opheim@nav.no>
2024-04-22 16:43:33 +0200

    Initialiserer opp klassane

--- apps/etterlatte-testdata-behandler/build.gradle.kts
+++ apps/etterlatte-testdata-behandler/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":libs:etterlatte-beregning-model"))
     implementation(project(":libs:etterlatte-brev-model"))
     implementation(project(":libs:etterlatte-migrering-model"))
+    implementation(project(":libs:etterlatte-ktor"))
     implementation(libs.ktor2.clientcore)
     implementation(libs.ktor2.clientauth)
     implementation(libs.ktor2.clientjackson)
---
+++ apps/etterlatte-testdata-behandler/src/main/kotlin/no/nav/etterlatte/testdata/AppBuilder.kt
@@ -0,0 +1,73 @@
+package no.nav.etterlatte.testdata
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import no.nav.etterlatte.libs.ktor.httpClientClientCredentials
+import no.nav.etterlatte.testdata.automatisk.AvkortingService
+import no.nav.etterlatte.testdata.automatisk.BeregningService
+import no.nav.etterlatte.testdata.automatisk.BrevService
+import no.nav.etterlatte.testdata.automatisk.SakService
+import no.nav.etterlatte.testdata.automatisk.TrygdetidService
+import no.nav.etterlatte.testdata.automatisk.VedtaksvurderingService
+import no.nav.etterlatte.testdata.automatisk.VilkaarsvurderingService
+
+class AppBuilder {
+    private val config: Config = ConfigFactory.load()
+
+    private fun settOppHttpClient(appnavn: String) =
+        httpClientClientCredentials(
+            azureAppClientId = config.getString("azure.app.client.id"),
+            azureAppJwk = config.getString("azure.app.jwk"),
+            azureAppWellKnownUrl = config.getString("azure.app.well.known.url"),
+            azureAppScope = config.getString("$appnavn.azure.scope"),
+        )
+
+    fun lagBehandler(): Behandler {
+        val vilkaarsvurderingService =
+            VilkaarsvurderingService(
+                settOppHttpClient("vilkaarsvurdering"),
+                "http://etterlatte-vilkaarsvurdering",
+            )
+        val httpClientBehandling = settOppHttpClient("behandling")
+        val sakService =
+            SakService(
+                httpClientBehandling,
+                "http://etterlatte-behandling",
+            )
+        val trygdetidService =
+            TrygdetidService(
+                settOppHttpClient("trygdetid"),
+                "http://etterlatte-trygdetid",
+            )
+        val avkortingService =
+            AvkortingService(
+                httpClientBehandling,
+                "http://etterlatte-behandling",
+            )
+        val beregningService =
+            BeregningService(
+                settOppHttpClient("beregning"),
+                "http://etterlatte-beregning",
+            )
+        val vedtaksvurderingService =
+            VedtaksvurderingService(
+                settOppHttpClient("vedtaksvurdering"),
+                "http://etterlatte-vilkaarsvurdering",
+            )
+        val brevService =
+            BrevService(
+                settOppHttpClient("brev-api"),
+                "http://etterlatte-brev-api",
+            )
+
+        return Behandler(
+            avkortingService = avkortingService,
+            beregningService = beregningService,
+            brevService = brevService,
+            sakService = sakService,
+            trygdetidService = trygdetidService,
+            vedtaksvurderingService = vedtaksvurderingService,
+            vilkaarsvurderingService = vilkaarsvurderingService,
+        )
+    }
+}

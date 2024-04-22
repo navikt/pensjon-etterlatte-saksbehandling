d9900211b6b0f9452e9176736eb1b7d386687f7e
Mads Opheim <mads.opheim@nav.no>
2024-04-22 12:58:27 +0200

    Tynt skall for ny app

---
+++ apps/etterlatte-testdata-behandler/src/main/kotlin/no/nav/etterlatte/Application.kt
@@ -0,0 +1,10 @@
+package no.nav.etterlatte
+
+import no.nav.etterlatte.rapidsandrivers.getRapidEnv
+import no.nav.helse.rapids_rivers.RapidApplication
+
+fun main() {
+    val rapidEnv = getRapidEnv()
+    RapidApplication.create(rapidEnv).also { rapidsConnection ->
+    }.start()
+}

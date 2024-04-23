524a1399a7776d5df01697ff5e39b5d7af76ebb1
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-04-23 19:54:11 +0200

    Litt tester

--- apps/etterlatte-behandling/src/main/resources/db/migration/V113__opprettet_tid_sak.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V113__opprettet_tid_sak.sql
@@ -1,4 +1,8 @@
 ALTER TABLE sak ADD COLUMN opprettet TIMESTAMP;
 
 UPDATE sak as s
-SET s.opprettet = (select behandling_opprettet from behandling as b where b.sak_id = s.id);
+SET opprettet = (select behandling_opprettet from behandling as b where b.sak_id = s.id);
+
+UPDATE sak
+SET opprettet = now()::timestamp
+WHERE sak.opprettet IS NULL;

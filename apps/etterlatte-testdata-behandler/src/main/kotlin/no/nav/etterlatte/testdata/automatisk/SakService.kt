1c85c157d1688cc93bf6abbd4569c000d5d6e1db
Mads Opheim <mads.opheim@nav.no>
2024-04-23 12:42:24 +0200

    Rettar sti til hentSak-endepunktet

--- apps/etterlatte-testdata-behandler/src/main/kotlin/no/nav/etterlatte/testdata/automatisk/SakService.kt
+++ apps/etterlatte-testdata-behandler/src/main/kotlin/no/nav/etterlatte/testdata/automatisk/SakService.kt
@@ -4,5 +4,5 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 
 class SakService(private val klient: HttpClient, private val url: String) {
-    suspend fun hentSak(sakId: Long) = klient.get("$url/$sakId")
+    suspend fun hentSak(sakId: Long) = klient.get("$url/saker/$sakId")
 }

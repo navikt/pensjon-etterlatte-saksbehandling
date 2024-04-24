b5ea4a593f7bd1a35637e915ee72f15f463fc5d5
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-04-24 12:25:21 +0200

    Ikke sette noe tid for de vi ikke vet

--- apps/etterlatte-behandling/src/main/resources/db/migration/V113__opprettet_tid_sak.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V113__opprettet_tid_sak.sql
@@ -2,7 +2,3 @@ ALTER TABLE sak ADD COLUMN opprettet TIMESTAMP;
 
 UPDATE sak as s
 SET opprettet = (select behandling_opprettet from behandling as b where b.sak_id = s.id);
-
-UPDATE sak
-SET opprettet = now()::timestamp
-WHERE sak.opprettet IS NULL;

39aacfbcbe76f9822bc9a72bf87d2986c1c1df9d
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-04-25 08:45:55 +0200

    Legg til wrapper for selftests i ui

--- apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
@@ -26,8 +26,16 @@ selftestRouter.get('/', express.json(), async (req, res) => {
         return pingresult
       })
     )
-    .then((all) => {
-      res.json(all)
+    .then((allchecks) => {
+      const aggregateResult = allchecks.filter((res) => res.result == ServiceStatus.DOWN)
+      const selfTestReport = {
+        application: 'etterlatte-saksbehandling',
+        timestamp: new Date().toISOString(),
+        aggregateResult: aggregateResult,
+        checks: allchecks,
+      }
+
+      res.json(selfTestReport)
     })
     .catch((err) => res.status(500).send(err))
     .finally(() => {

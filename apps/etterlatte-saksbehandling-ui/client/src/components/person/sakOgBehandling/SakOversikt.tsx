9d182264ff46292869b0c97bea0f9e061fb52c32
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-04-26 15:44:03 +0200

    Flytt ys og flyktning alerts

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversikt.tsx
@@ -1,16 +1,11 @@
 import styled from 'styled-components'
 import { Container, SpaceChildren } from '~shared/styled'
 import Spinner from '~shared/Spinner'
-import { Alert, Heading, ToggleGroup } from '@navikt/ds-react'
-import { formaterStringDato } from '~utils/formattering'
+import { Heading, ToggleGroup } from '@navikt/ds-react'
 import { SakMedBehandlinger } from '~components/person/typer'
 import { isSuccess, mapResult, Result } from '~shared/api/apiUtils'
 import React, { useEffect, useState } from 'react'
 import { useApiCall } from '~shared/hooks/useApiCall'
-import { hentFlyktningStatusForSak } from '~shared/api/sak'
-import { SakType } from '~shared/types/sak'
-import { hentMigrertYrkesskadeFordel } from '~shared/api/vilkaarsvurdering'
-import { Vedtaksloesning } from '~shared/types/IDetaljertBehandling'
 import { SakOversiktHeader } from '~components/person/sakOgBehandling/SakOversiktHeader'
 import { SakIkkeFunnet } from '~components/person/sakOgBehandling/SakIkkeFunnet'
 import { ForenkletOppgaverTable } from '~components/person/sakOgBehandling/ForenkletOppgaverTable'
@@ -19,8 +14,6 @@ import { ApiErrorAlert } from '~ErrorBoundary'
 import { Behandlingsliste } from '~components/person/sakOgBehandling/Behandlingsliste'
 import { KlageListe } from '~components/person/sakOgBehandling/KlageListe'
 import { TilbakekrevingListe } from '~components/person/sakOgBehandling/TilbakekrevingListe'
-
-const ETTERLATTEREFORM_DATO = '2024-01'
 
 export enum OppgaveValg {
   AKTIVE = 'AKTIVE',
@@ -32,23 +25,8 @@ export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehand
 
   const [oppgaverResult, oppgaverFetch] = useApiCall(hentOppgaverTilknyttetSak)
 
-  const [flyktningResult, hentFlyktning] = useApiCall(hentFlyktningStatusForSak)
-  const [yrkesskadefordelResult, hentYrkesskadefordel] = useApiCall(hentMigrertYrkesskadeFordel)
-
   useEffect(() => {
     if (isSuccess(sakResult)) {
-      hentFlyktning(sakResult.data.sak.id)
-
-      const migrertBehandling =
-        sakResult.data.sak.sakType === SakType.BARNEPENSJON &&
-        sakResult.data.behandlinger.find(
-          (behandling) =>
-            behandling.kilde === Vedtaksloesning.PESYS && behandling.virkningstidspunkt?.dato === ETTERLATTEREFORM_DATO
-        )
-      if (migrertBehandling) {
-        hentYrkesskadefordel(migrertBehandling.id)
-      }
-
       oppgaverFetch(sakResult.data.sak.id)
     }
   }, [fnr, sakResult])
@@ -61,41 +39,10 @@ export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehand
         success: ({ sak, behandlinger }) => (
           <SpaceChildren direction="row">
             <SakHeaderWrapper>
-              <SakOversiktHeader fnr={fnr} sak={sak} />
+              <SakOversiktHeader sak={sak} behandlinger={behandlinger} fnr={fnr} />
             </SakHeaderWrapper>
             <Container>
               <SpaceChildren gap="2rem">
-                {mapResult(flyktningResult, {
-                  success: (data) =>
-                    !!data?.erFlyktning && (
-                      <>
-                        <div>
-                          <Alert variant="info" size="small">
-                            Saken er markert med flyktning i Pesys og første virkningstidspunkt var{' '}
-                            {formaterStringDato(data.virkningstidspunkt)}
-                          </Alert>
-                        </div>
-
-                        <HorisontaltSkille />
-                      </>
-                    ),
-                })}
-
-                {mapResult(yrkesskadefordelResult, {
-                  success: (data) =>
-                    data.migrertYrkesskadefordel && (
-                      <>
-                        <div>
-                          <Alert variant="info" size="small">
-                            Søker har yrkesskadefordel fra før 01.01.2024 og har rett til stønad til fylte 21 år.
-                          </Alert>
-                        </div>
-
-                        <HorisontaltSkille />
-                      </>
-                    ),
-                })}
-
                 <SpaceChildren>
                   <Heading size="medium">Oppgaver</Heading>
                   <ToggleGroup
@@ -136,11 +83,6 @@ export const SakOversikt = ({ sakResult, fnr }: { sakResult: Result<SakMedBehand
   )
 }
 
-const HorisontaltSkille = styled.hr`
-  border-color: var(--a-surface-active);
-  width: 100%;
-`
-
 export const HeadingWrapper = styled.div`
   display: inline-flex;
   margin-top: 3em;
@@ -149,5 +91,6 @@ export const HeadingWrapper = styled.div`
 const SakHeaderWrapper = styled.div`
   padding: 2rem;
   border-right: 1px solid var(--a-surface-active);
-  width: fit-content;
+  height: 100vh;
+  width: 25rem;
 `
--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversiktHeader.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/SakOversiktHeader.tsx
@@ -1,25 +1,50 @@
 import React, { useEffect } from 'react'
 import { mapResult } from '~shared/api/apiUtils'
-import { hentNavkontorForPerson } from '~shared/api/sak'
-import { BodyShort, Heading, Loader, ReadMore, Tag } from '@navikt/ds-react'
+import { hentFlyktningStatusForSak, hentNavkontorForPerson } from '~shared/api/sak'
+import { Alert, BodyShort, Heading, Loader, ReadMore, Tag } from '@navikt/ds-react'
 import { FlexRow, SpaceChildren } from '~shared/styled'
 import { tagColors } from '~shared/Tags'
-import { formaterEnumTilLesbarString } from '~utils/formattering'
+import { formaterEnumTilLesbarString, formaterStringDato } from '~utils/formattering'
 import { Buildings3Icon, LocationPinIcon } from '@navikt/aksel-icons'
 import { EndreEnhet } from '~components/person/sakOgBehandling/EndreEnhet'
-import { ISakMedUtlandstilknytning } from '~shared/types/sak'
+import { ISakMedUtlandstilknytning, SakType } from '~shared/types/sak'
 import { enhetErSkrivbar } from '~components/behandling/felles/utils'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { SakStatus } from '~components/person/sakOgBehandling/SakStatus'
 import { useInnloggetSaksbehandler } from '~components/behandling/useInnloggetSaksbehandler'
+import { IBehandlingsammendrag } from '~components/person/typer'
+import { hentMigrertYrkesskadeFordel } from '~shared/api/vilkaarsvurdering'
+import { Vedtaksloesning } from '~shared/types/IDetaljertBehandling'
+import styled from 'styled-components'
 
-export const SakOversiktHeader = ({ sak, fnr }: { sak: ISakMedUtlandstilknytning; fnr: string }) => {
+const ETTERLATTEREFORM_DATO = '2024-01'
+
+interface Props {
+  sak: ISakMedUtlandstilknytning
+  behandlinger: IBehandlingsammendrag[]
+  fnr: string
+}
+
+export const SakOversiktHeader = ({ sak, behandlinger, fnr }: Props) => {
   const innloggetSaksbehandler = useInnloggetSaksbehandler()
 
   const [navkontorResult, hentNavkontor] = useApiCall(hentNavkontorForPerson)
+  const [yrkesskadefordelResult, hentYrkesskadefordel] = useApiCall(hentMigrertYrkesskadeFordel)
+  const [flyktningResult, hentFlyktning] = useApiCall(hentFlyktningStatusForSak)
 
   useEffect(() => {
     hentNavkontor(fnr)
+    hentFlyktning(sak.id)
+
+    const migrertBehandling =
+      sak.sakType === SakType.BARNEPENSJON &&
+      behandlinger.find(
+        (behandling) =>
+          behandling.kilde === Vedtaksloesning.PESYS && behandling.virkningstidspunkt?.dato === ETTERLATTEREFORM_DATO
+      )
+    if (migrertBehandling) {
+      hentYrkesskadefordel(migrertBehandling.id)
+    }
   }, [])
 
   return (
@@ -67,6 +92,40 @@ export const SakOversiktHeader = ({ sak, fnr }: { sak: ISakMedUtlandstilknytning
           </ol>
         </ReadMore>
       )}
+
+      {mapResult(flyktningResult, {
+        success: (data) =>
+          !!data?.erFlyktning && (
+            <>
+              <HorisontaltSkille />
+              <div>
+                <Alert variant="info" size="small" inline>
+                  Saken er markert med flyktning i Pesys og første virkningstidspunkt var{' '}
+                  {formaterStringDato(data.virkningstidspunkt)}
+                </Alert>
+              </div>
+            </>
+          ),
+      })}
+
+      {mapResult(yrkesskadefordelResult, {
+        success: (data) =>
+          data.migrertYrkesskadefordel && (
+            <>
+              <HorisontaltSkille />
+              <div>
+                <Alert variant="info" size="small" inline>
+                  Søker har yrkesskadefordel fra før 01.01.2024 og har rett til stønad til fylte 21 år.
+                </Alert>
+              </div>
+            </>
+          ),
+      })}
     </SpaceChildren>
   )
 }
+
+const HorisontaltSkille = styled.hr`
+  border-color: var(--a-surface-active);
+  width: 100%;
+`

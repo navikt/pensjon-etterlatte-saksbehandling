8dba032516bcf5ea05b52eacd03f9f267e6fcac6
Mads Opheim <mads.opheim@nav.no>
2024-05-06 13:33:51 +0200

    EY-3894: Flyttar kall som går mot trygdetids-appen frå beregning-kafka over til trygdetid-kafka, kor det høyrer meir naturleg heime

---
+++ apps/etterlatte-trygdetid-kafka/src/main/kotlin/no/nav/etterlatte/trygdetid/kafka/KopierTrygdetidRiver.kt
@@ -0,0 +1,43 @@
+package no.nav.etterlatte.trygdetid.kafka
+
+import no.nav.etterlatte.libs.common.rapidsandrivers.setEventNameForHendelseType
+import no.nav.etterlatte.rapidsandrivers.BEHANDLING_ID_KEY
+import no.nav.etterlatte.rapidsandrivers.BEHANDLING_VI_OMREGNER_FRA_KEY
+import no.nav.etterlatte.rapidsandrivers.Kontekst
+import no.nav.etterlatte.rapidsandrivers.ListenerMedLoggingOgFeilhaandtering
+import no.nav.etterlatte.rapidsandrivers.ReguleringHendelseType
+import no.nav.etterlatte.rapidsandrivers.behandlingId
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.toUUID
+import org.slf4j.LoggerFactory
+
+internal class KopierTrygdetidRiver(
+    rapidsConnection: RapidsConnection,
+    private val trygdetidService: TrygdetidService,
+) : ListenerMedLoggingOgFeilhaandtering() {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        initialiserRiver(rapidsConnection, ReguleringHendelseType.VILKAARSVURDERT) {
+            validate { it.requireKey(BEHANDLING_ID_KEY) }
+            validate { it.requireKey(BEHANDLING_VI_OMREGNER_FRA_KEY) }
+        }
+    }
+
+    override fun kontekst() = Kontekst.REGULERING
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        logger.info("Mottatt omregninghendelse, kopierer trygdetid")
+        val behandlingId = packet.behandlingId
+        val behandlingViOmregnerFra = packet[BEHANDLING_VI_OMREGNER_FRA_KEY].asText().toUUID()
+        trygdetidService.kopierTrygdetidFraForrigeBehandling(behandlingId, behandlingViOmregnerFra)
+        packet.setEventNameForHendelseType(ReguleringHendelseType.TRYGDETID_KOPIERT)
+        context.publish(packet.toJson())
+        logger.info("Trygdetid kopiert. Publiserte oppdatert omregningshendelse")
+    }
+}

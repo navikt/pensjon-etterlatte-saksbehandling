669f3b0f19371926fff90d53c55c497944466dcd
Mads Opheim <mads.opheim@nav.no>
2024-05-06 14:39:15 +0200

    EY-3879: MQ-biblioteket vi bruker trekkjer med seg bouncycastle-versjonar med kjente sårbarheiter, korav nokre er tagga med risiko høg. Det er ikkje så kult å ha i prod.
    Det er ikkje meir enn eit par veker sia forrige versjon av MQ-biblioteket kom heller, så det er ikkje grunn til å tru at det blir retta med det første. Innfører derfor samme workaround som vi per no bruker i etterlatte-proxy, altså at vi eksplisitt overstyrer versjoneringa for dette biblioteket.
    
    Har kikka på release notes for bouncycastle, og ved å gå frå 1.77 som vi bruker i dag til 1.78.1 ser det ut som vi får retta fleire CVE-ar, og at det utover det ser det ut som berre småting.

--- apps/etterlatte-tilbakekreving/build.gradle.kts
+++ apps/etterlatte-tilbakekreving/build.gradle.kts
@@ -30,7 +30,11 @@ dependencies {
     implementation(libs.navfelles.tokenclientcore)
     implementation(libs.navfelles.tokenvalidationktor2)
 
-    implementation(libs.mq.jakarta.client)
+    implementation(libs.mq.jakarta.client) {
+        exclude("org.bouncycastle:bcutil-jdk18on")
+        exclude("org.bouncycastle:bcpkix-jdk18on")
+        exclude("org.bouncycastle:bcprov-jdk18on")
+    }
     implementation(libs.messaginghub.pooled.jms)
     implementation(libs.navfelles.tjenestespesifikasjoner.tilbakekreving)
 
@@ -47,4 +51,10 @@ dependencies {
     testImplementation(testFixtures((project(":libs:etterlatte-database"))))
     testImplementation(testFixtures(project(":libs:etterlatte-mq")))
     testImplementation(testFixtures((project(":libs:etterlatte-ktor"))))
+
+    // Avhengigheter fra patching av sårbarheter i IBM MQ.
+    // Vi bør kunne ta bort alle disse og exclude-lista for neste IBM MQ-versjon
+    implementation(libs.bcpkix)
+    implementation(libs.bcprov)
+    implementation(libs.bcutil)
 }

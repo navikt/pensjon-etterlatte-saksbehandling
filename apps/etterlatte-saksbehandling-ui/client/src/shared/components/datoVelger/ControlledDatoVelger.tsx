9384132ef517db5e1f97648d6ed6578cf603f3a6
Trond Valen <trond.valen@nav.no>
2024-05-10 15:40:50 +0200

    Forslag til validering

--- apps/etterlatte-saksbehandling-ui/client/src/shared/components/datoVelger/ControlledDatoVelger.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/components/datoVelger/ControlledDatoVelger.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from 'react'
-import { DatePicker, useDatepicker } from '@navikt/ds-react'
+import React, { ReactNode, useState } from 'react'
+import { DatePicker, DateValidationT, useDatepicker } from '@navikt/ds-react'
 import { UseDatepickerOptions } from '@navikt/ds-react/esm/date/hooks/useDatepicker'
 import { Control, FieldValues, Path, useController } from 'react-hook-form'
 import { formatDateToLocaleDateOrEmptyString } from '~shared/components/datoVelger/datoVelgerUtils'
@@ -33,18 +33,27 @@ export const ControlledDatoVelger = <T extends FieldValues>({
         if (errorVedTomInput && !value) {
           return errorVedTomInput
         }
-        return undefined
+        if (validationError) {
+          return validationError
+        }
       },
     },
     shouldUnregister: shouldUnregister,
   })
 
+  const [validationError, setValidationError] = useState<string | null>(null)
+
+  const onValidate = (validation: DateValidationT) => {
+    setValidationError(validation.isInvalid ? 'Ugyldig dato' : null)
+  }
+
   const { datepickerProps, inputProps, setSelected, selectedDay } = useDatepicker({
     onDateChange: (date: Date) => {
-      date && field.onChange(formatDateToLocaleDateOrEmptyString(date))
+      field.onChange(date ? formatDateToLocaleDateOrEmptyString(date) : null)
     },
     locale: 'nb',
     inputFormat: 'dd.MM.yyyy',
+    onValidate: onValidate,
     defaultSelected: field.value ? new Date(field.value) : undefined,
   } as UseDatepickerOptions)
 

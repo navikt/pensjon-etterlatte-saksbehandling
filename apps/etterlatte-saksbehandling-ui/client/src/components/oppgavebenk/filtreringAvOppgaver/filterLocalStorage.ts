5b9a2f018ba712ab5c458c05aa4304dabcf32082
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-05-14 08:47:05 +0200

    Legg til lagring av min oppgaveliste filter i localstorage

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/filtreringAvOppgaver/filterLocalStorage.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/filtreringAvOppgaver/filterLocalStorage.ts
@@ -1,12 +1,21 @@
-import { initialFilter } from '~components/oppgavebenk/filtreringAvOppgaver/filtrerOppgaver'
+import {
+  initialFilter,
+  initialMinOppgavelisteFiltre,
+} from '~components/oppgavebenk/filtreringAvOppgaver/filtrerOppgaver'
 import { logger } from '~utils/logger'
 import { Filter, GosysFilter } from '~components/oppgavebenk/filtreringAvOppgaver/typer'
 
 const OPPGAVELISTEN_FILTER_KEY = 'oppgavelistenFilter'
+const MIN_OPPGAVELISTE_FILTER_KEY = 'minOppgavelisteFilter'
 const GOSYS_OPPGAVER_FILTER_KEY = 'gosysOppgaverFilter'
 
-export const leggOppgavelistenFilterILocalStorage = (filter: Filter) =>
+export const leggOppgavelistenFilterILocalStorage = (filter: Filter) => {
   localStorage.setItem(OPPGAVELISTEN_FILTER_KEY, JSON.stringify(filter))
+}
+
+export const leggMinOppgavelisteFilterILocalsotrage = (filter: Filter) => {
+  localStorage.setItem(MIN_OPPGAVELISTE_FILTER_KEY, JSON.stringify(filter))
+}
 
 export const leggGosysOppgaverFilterILocalStorage = (filter: GosysFilter) => {
   localStorage.setItem(GOSYS_OPPGAVER_FILTER_KEY, JSON.stringify(filter))
@@ -21,6 +30,18 @@ export const hentOppgavelistenFilterFraLocalStorage = (): Filter => {
     logger.generalError({ message: 'Feil i hentingen av filter for oppgavelisten fra localstorage' })
 
     return initialFilter()
+  }
+}
+
+export const hentMinOppgavelisteFilterFraLocalStorage = (): Filter => {
+  try {
+    const filter = localStorage[MIN_OPPGAVELISTE_FILTER_KEY]
+    if (!!filter) return JSON.parse(filter)
+    else return initialMinOppgavelisteFiltre()
+  } catch (error) {
+    logger.generalError({ message: 'Feil i hentingen av filter for min oppgaveliste fra localstorage' })
+
+    return initialMinOppgavelisteFiltre()
   }
 }
 

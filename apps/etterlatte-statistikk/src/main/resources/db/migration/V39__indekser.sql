1e9a26c19c4e1e4732e67295f622258caa14c666
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-05-15 12:46:02 +0200

    Lagt til indekser for behandlingsID i statistikk

---
+++ apps/etterlatte-statistikk/src/main/resources/db/migration/V39__indekser.sql
@@ -0,0 +1,7 @@
+CREATE INDEX on soeknad_statistikk (gyldig_for_behandling);
+
+CREATE INDEX on sak (behandling_id);
+
+CREATE INDEX on stoenad (behandlingid);
+
+CREATE INDEX on maaned_stoenad (behandlingid);

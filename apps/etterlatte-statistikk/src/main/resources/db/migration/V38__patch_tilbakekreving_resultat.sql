7738447fabdc2650abe181daf66cfb9529d68bd3
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-05-15 09:13:11 +0200

    bedre kommentar i sql-script

--- apps/etterlatte-statistikk/src/main/resources/db/migration/V38__patch_tilbakekreving_resultat.sql
+++ apps/etterlatte-statistikk/src/main/resources/db/migration/V38__patch_tilbakekreving_resultat.sql
@@ -1,3 +1,3 @@
--- pat
+-- patcher til strukturert resultat for to rader som har fått feil
 UPDATE sak set behandling_resultat = 'INGEN_TILBAKEKREV' where id = 68729;
 UPDATE sak set behandling_resultat = 'INGEN_TILBAKEKREV' where id = 68417;

5a5bd66505ade10aa837fa21ee89bcc3ffe6e548
Henrik Gundersen <henrik.gundersen@nav.no>
2024-05-27 12:41:37 +0200

    Reguler enkelte spesielle saker (#4874)

--- jobs/start-regulering/src/main/kotlin/Application.kt
+++ jobs/start-regulering/src/main/kotlin/Application.kt
@@ -42,7 +42,13 @@ fun createRecord(dato: LocalDate) =
             ReguleringHendelseType.REGULERING_STARTA.lagParMedEventNameKey(),
             ReguleringEvents.DATO to dato.toString(),
             ReguleringEvents.KJOERING to "Regulering-$year",
-            ReguleringEvents.ANTALL to 1,
-            ReguleringEvents.SPESIFIKKE_SAKER to listOf<Long>(),
+            ReguleringEvents.ANTALL to 4,
+            ReguleringEvents.SPESIFIKKE_SAKER to
+                listOf<Long>(
+                    17258, // opphør frem i tid
+                    14600, // manuelt overstyrt
+                    10341, // revurdering frem i tid
+                    12541, // inntektsendring frem i tid
+                ),
         ),
     ).toJson()

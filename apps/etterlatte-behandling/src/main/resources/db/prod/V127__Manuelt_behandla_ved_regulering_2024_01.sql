83785187d8fa3fd3abb7649ddfb92789f998054a
Trond Valen <trond.valen@nav.no>
2024-05-27 13:57:31 +0200

    De første sakene vi fikk gjennom :)

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V127__Manuelt_behandla_ved_regulering_2024_01.sql
@@ -0,0 +1,5 @@
+INSERT INTO omregningskjoering (kjoering, status, sak_id) VALUES ('Regulering-2024', 'FERDIGSTILT', 1);
+INSERT INTO omregningskjoering (kjoering, status, sak_id) VALUES ('Regulering-2024', 'FERDIGSTILT', 17258);
+INSERT INTO omregningskjoering (kjoering, status, sak_id) VALUES ('Regulering-2024', 'FERDIGSTILT', 14600);
+INSERT INTO omregningskjoering (kjoering, status, sak_id) VALUES ('Regulering-2024', 'FERDIGSTILT', 10341);
+INSERT INTO omregningskjoering (kjoering, status, sak_id) VALUES ('Regulering-2024', 'FERDIGSTILT', 12541);

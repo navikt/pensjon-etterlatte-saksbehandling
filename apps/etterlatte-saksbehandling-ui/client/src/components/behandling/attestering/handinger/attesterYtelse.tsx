e0200176088528ac509dcd3621b3bfc22b242766
Trond Valen <trond.valen@nav.no>
2024-05-27 13:08:16 +0200

    Slutter å lyve ved attestering om at det skal sendes brev.

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/attestering/handinger/attesterYtelse.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/attestering/handinger/attesterYtelse.tsx
@@ -65,7 +65,9 @@ export const AttesterYtelse = ({ behandling, kommentar }: { behandling: IDetalje
       </FlexRow>
       <GeneriskModal
         tittel="Er du sikker på at du vil iverksette vedtaket?"
-        beskrivelse="Vedtaksbrevet sendes ut automatisk ved iverksettelse"
+        beskrivelse={
+          skalSendeBrev ? 'Vedtaksbrevet sendes ut automatisk ved iverksettelse' : 'Det sendes ikke ut vedtaksbrev'
+        }
         tekstKnappJa="Ja, iverksett vedtak"
         tekstKnappNei="Nei, gå tilbake"
         onYesClick={attester}

86e481e84c7bc198274041b393cb9f99ecb19936
Mads Opheim <mads.opheim@nav.no>
2024-05-27 12:09:15 +0200

    Flyttar sjekken til meir eigna plass

--- apps/etterlatte-vedtaksvurdering/src/main/kotlin/no/nav/etterlatte/vedtaksvurdering/VedtakBehandlingService.kt
+++ apps/etterlatte-vedtaksvurdering/src/main/kotlin/no/nav/etterlatte/vedtaksvurdering/VedtakBehandlingService.kt
@@ -56,7 +56,7 @@ class VedtakBehandlingService(
         dato: LocalDate,
     ): LoependeYtelse {
         logger.info("Sjekker om det finnes løpende vedtak for sak $sakId på dato $dato")
-        val alleVedtakForSak = repository.hentVedtakForSak(sakId)
+        val alleVedtakForSak = repository.hentVedtakForSak(sakId).filter { it.vedtakFattet != null }
         return Vedtakstidslinje(alleVedtakForSak).harLoependeVedtakPaaEllerEtter(dato)
     }
 

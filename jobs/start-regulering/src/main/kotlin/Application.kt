2d9be58a66a670172214ad9f8b529e93917382e8
Mads Opheim <mads.opheim@nav.no>
2024-05-28 11:26:58 +0200

    Køyre regulering på nokre få saker

--- jobs/start-regulering/src/main/kotlin/Application.kt
+++ jobs/start-regulering/src/main/kotlin/Application.kt
@@ -44,7 +44,7 @@ fun createRecord(dato: LocalDate) =
             ReguleringHendelseType.REGULERING_STARTA.lagParMedEventNameKey(),
             ReguleringEvents.DATO to dato.toString(),
             ReguleringEvents.KJOERING to "Regulering-$year",
-            ReguleringEvents.ANTALL to 500,
+            ReguleringEvents.ANTALL to 5,
             ReguleringEvents.SPESIFIKKE_SAKER to "",
             SAK_TYPE to SakType.BARNEPENSJON.toString(),
         ),

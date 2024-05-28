c5255b5109e0f2a6435e1d57e2e81505cfe568d7
Mads Opheim <mads.opheim@nav.no>
2024-05-28 17:59:49 +0200

    Tar sak med feilande grunnlag seinare

--- apps/etterlatte-oppdater-behandling/src/main/kotlin/regulering/ReguleringsforespoerselRiver.kt
+++ apps/etterlatte-oppdater-behandling/src/main/kotlin/regulering/ReguleringsforespoerselRiver.kt
@@ -137,6 +137,8 @@ private val sakerViIkkeRegulererAutomatiskNaa =
         11606, // Revurdering med overstyrt beregning åpen behandling
         11848, // Revurdering med overstyrt beregning åpen behandling
         6402, // EKSPORT: Feilmelding: Virkningstidspunkt kan ikke være etter opphør
+        8883, // Feil i grunnlag.
+        // https://logs.adeo.no/app/discover#/doc/96e648c0-980a-11e9-830a-e17bbd64b4db/.ds-navlogs-2024.05.28-000011?id=aiDgv48BZiQzzTM0q6EU
         // Herifra og ut: overstyrte beregninger. Tas seinare i separat køyring.
         11510,
         11580,

af6ad0a35caabfed0959b335b7d65fc59d26aac1
Henrik Gundersen <henrik.gundersen@nav.no>
2024-05-29 09:52:00 +0200

    Ekskludert sak skal ikke reguleres

--- jobs/start-regulering/src/main/kotlin/Application.kt
+++ jobs/start-regulering/src/main/kotlin/Application.kt
@@ -44,8 +44,8 @@ fun createRecord(dato: LocalDate) =
             ReguleringHendelseType.REGULERING_STARTA.lagParMedEventNameKey(),
             ReguleringEvents.DATO to dato.toString(),
             ReguleringEvents.KJOERING to "Regulering-$year",
-            ReguleringEvents.ANTALL to 1000,
-            ReguleringEvents.SPESIFIKKE_SAKER to "",
+            ReguleringEvents.ANTALL to 1,
+            ReguleringEvents.SPESIFIKKE_SAKER to "6402",
             SAK_TYPE to SakType.BARNEPENSJON.toString(),
         ),
     ).toJson()

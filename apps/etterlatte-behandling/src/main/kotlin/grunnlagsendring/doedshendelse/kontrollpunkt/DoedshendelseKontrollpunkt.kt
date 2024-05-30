8cc4a9004782f83da521c393eaa9dca3ba54ee31
Espen Schei <espen.schei@nav.no>
2024-05-30 14:43:29 +0200

    Personer som har giftet seg på nytt har ikke rett på OMS.
    Ikke opprett oppgave for disse personene
    
    EY-3378

--- apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/kontrollpunkt/DoedshendelseKontrollpunkt.kt
+++ apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/kontrollpunkt/DoedshendelseKontrollpunkt.kt
@@ -186,6 +186,17 @@ sealed class DoedshendelseKontrollpunkt {
                     " Saksbehandler må vurdere lengden på ekteskapet og eventuelt sende informasjonsbrev til gjenlevende."
     }
 
+    @JsonTypeName("EPS_ER_GIFT_PAA_NYTT")
+    data class EpsErGiftPaaNytt(val doedsdato: LocalDate, val fnr: String, val nyEktefelleFnr: String) : DoedshendelseKontrollpunkt() {
+        override val kode = "EPS_GIFT_UKJENT_GIFTEMAAL_VARIGHET"
+        override val beskrivelse: String = "Tidligere ektefelle har giftet seg på nytt med en annen person"
+        override val sendBrev: Boolean = false
+        override val opprettOppgave: Boolean = false
+        override val avbryt: Boolean = true
+        override val oppgaveTekst: String
+            get() = "Tidligere ektefelle ($fnr) døde ($doedsdato), men brukeren er gift med en ny person ($nyEktefelleFnr)."
+    }
+
     @JsonTypeName("AVDOED_HAR_UTVANDRET")
     data object AvdoedHarUtvandret : DoedshendelseKontrollpunkt() {
         override val kode = "AVDOED_HAR_UTVANDRET"

a7e5ac500e2bf22fd11497677fa847308143213e
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-05-30 15:37:24 +0200

    Rydde opp i trygde avtale

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/styled.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/styled.tsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 import { Textarea } from '@navikt/ds-react'
 
-export const FormWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  margin-right: 1em;
-`
-
 export const FormKnapper = styled.div`
   margin-top: 1rem;
   margin-right: 1em;

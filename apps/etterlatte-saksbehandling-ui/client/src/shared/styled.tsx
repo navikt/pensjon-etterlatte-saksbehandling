b90ecad502567fc46312038e7ffb551965d07bf8
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-05-30 21:24:54 +0200

    Cleanup

--- apps/etterlatte-saksbehandling-ui/client/src/shared/styled.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/styled.tsx
@@ -44,7 +44,7 @@ export const SidebarContent = styled.div<{ $collapsed: boolean }>`
 export const Scroller = styled.div`
   overflow-y: scroll;
   height: calc(100vh - 182px); // 182 px er høyden av dekoratøren + pdlpersonbar og stegmenyen
-  padding-bottom: 4rem;
+  padding-bottom: 2rem;
 `
 
 export const SidebarTools = styled.div`

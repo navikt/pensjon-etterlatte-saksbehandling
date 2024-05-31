2a345c14e7f88283e571c7f652dbeab7e33c650e
Bjørn Golberg <bjorn.golberg@nav.no>
2024-05-31 15:34:51 +0200

    Setter tilbake status til oppgave knyttet til kravpakke behandling til attestering
    Blei feilaktig tilbakestilt under regulering

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V131__fikse_kravpakke_behandling_etter_regulering.sql
@@ -0,0 +1,5 @@
+update oppgave set status = 'ATTESTERING' where id = 'bd7e9f5f-941e-4e30-ae0b-4e595379c2f9';
+update oppgave set merknad = '' where id = 'bd7e9f5f-941e-4e30-ae0b-4e595379c2f9';
+
+update oppgave set status = 'ATTESTERING' where id = '69344736-8c7d-4de9-bf5e-4fdb8038dac1';
+update oppgave set merknad = '' where id = '69344736-8c7d-4de9-bf5e-4fdb8038dac1';

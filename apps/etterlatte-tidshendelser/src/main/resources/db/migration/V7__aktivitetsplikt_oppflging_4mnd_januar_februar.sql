bdc4f3782e82c5e048c8496835bc14846a5b8971
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-06-03 15:10:51 +0200

    Tidshendelsekjoering om vurdering av aktivitetsplikt 4mnd

---
+++ apps/etterlatte-tidshendelser/src/main/resources/db/migration/V7__aktivitetsplikt_oppflging_4mnd_januar_februar.sql
@@ -0,0 +1,7 @@
+-- Jobben oppretter oppgave for å vurdere aktivitetsplikt etter 4 måneder for sakene der dødsdato var i a) januar 2024
+-- og b) februar 2024.
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_4MND', '2024-06-03', '2024-05');
+
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_4MND', '2024-06-10', '2024-06');

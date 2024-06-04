39170a2e74b4c67feaffda19f22bdb71dd0f33f4
Trond Valen <trond.valen@nav.no>
2024-06-04 13:58:11 +0200

    Vise melding om at regulering ikke har tatt høyde for grunnlagsendringer (#5019)
    * Vise melding om at regulering ikke har tatt høyde for grunnlagsendringer.
    
    * Layout-fiks
    
    * Liten tekstendring

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/hendelser/Hendelser.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/hendelser/Hendelser.tsx
@@ -77,6 +77,11 @@ export const Hendelser = ({ sakResult, fnr }: { sakResult: Result<SakMedBehandli
               {hendelseValg === HendelseValg.NYE && (
                 <>
                   <RevurderingInfo>
+                    <Alert variant="warning">
+                      Obs! I G-reguleringen vises det nyeste grunnlaget i saken på reguleringstidspunktet, men selve
+                      beregningen har ikke tatt høyde for eventuelle endringer i grunnlaget. Se derfor på beregningen i
+                      den forrige behandlingen ved vurdering av endring i grunnlagsdata.
+                    </Alert>
                     Nye hendelser kan kreve revurdering. Vurder derfor om hendelsen har konsekvens for ytelsen. Hvis
                     ikke kan du arkivere hendelsen.
                   </RevurderingInfo>
@@ -144,7 +149,7 @@ export const Hendelser = ({ sakResult, fnr }: { sakResult: Result<SakMedBehandli
 }
 
 const RevurderingInfo = styled(BodyShort)`
-  max-width: 32rem;
+  max-width: 60rem;
   margin-top: 1rem;
 `
 

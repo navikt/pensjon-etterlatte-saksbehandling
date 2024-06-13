b58334522ad71bf11fbbc4aacd9ac5ef29a4c4b6
Nicolas N <nicolas.nordhagen@nav.no>
2024-06-13 13:03:40 +0200

    EY-3728 Småfikser på notat design

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/notat/NyttNotatModal.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/notat/NyttNotatModal.tsx
@@ -1,44 +1,34 @@
 import { DocPencilIcon } from '@navikt/aksel-icons'
-import { isPending } from '~shared/api/apiUtils'
-import { Alert, Box, Button, HStack, Modal } from '@navikt/ds-react'
-import React, { useState } from 'react'
+import { Box, Button } from '@navikt/ds-react'
+import React from 'react'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { Notat, NotatMal, opprettNotatForSak } from '~shared/api/notat'
+import { isPending } from '~shared/api/apiUtils'
 
+/**
+ * Modal blir lagt tilbake når andre maler er på plass.
+ * Enkel løsning med umiddelbar opprettelse midlertidig.
+ **/
 export const NyttNotatModal = ({ sakId, leggTilNotat }: { sakId: number; leggTilNotat: (notat: Notat) => void }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   const [opprettNotatStatus, opprettNotat] = useApiCall(opprettNotatForSak)
 
   const opprettNyttNotat = () => {
     opprettNotat({ sakId, mal: NotatMal.TOM_MAL }, (notat) => {
       leggTilNotat(notat)
-      setIsOpen(false)
     })
   }
 
   return (
     <Box>
-      <Button variant="primary" icon={<DocPencilIcon />} iconPosition="right" onClick={() => setIsOpen(true)}>
+      <Button
+        variant="primary"
+        icon={<DocPencilIcon />}
+        iconPosition="right"
+        onClick={opprettNyttNotat}
+        loading={isPending(opprettNotatStatus)}
+      >
         Nytt notat
       </Button>
-
-      <Modal open={isOpen} onClose={() => setIsOpen(false)} aria-labelledby="modal-heading" width="medium">
-        <Modal.Body>
-          <Alert variant="info">Du oppretter nå et tomt notat</Alert>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <HStack gap="4" justify="end">
-            <Button variant="tertiary" onClick={() => setIsOpen(false)}>
-              Avbryt
-            </Button>
-            <Button variant="primary" onClick={opprettNyttNotat} loading={isPending(opprettNotatStatus)}>
-              Opprett
-            </Button>
-          </HStack>
-        </Modal.Footer>
-      </Modal>
     </Box>
   )
 }

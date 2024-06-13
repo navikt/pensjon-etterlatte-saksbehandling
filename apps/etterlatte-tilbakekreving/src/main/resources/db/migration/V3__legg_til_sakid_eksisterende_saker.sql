587d8928955e60cee4a3739d8dc485e5a5ec0d4d
Henrik Gundersen <henrik.gundersen@nav.no>
2024-06-13 12:16:47 +0200

    EY-4047: Setter sakId på eksisterende tilbakekrevingshendelser (#5115)

---
+++ apps/etterlatte-tilbakekreving/src/main/resources/db/migration/V3__legg_til_sakid_eksisterende_saker.sql
@@ -0,0 +1,13 @@
+UPDATE tilbakekreving_hendelse SET sak_id = 13090 WHERE kravgrunnlag_id = '474975';
+UPDATE tilbakekreving_hendelse SET sak_id = 10154 WHERE kravgrunnlag_id = '465220';
+UPDATE tilbakekreving_hendelse SET sak_id = 11814 WHERE kravgrunnlag_id = '473638';
+UPDATE tilbakekreving_hendelse SET sak_id = 15127 WHERE kravgrunnlag_id = '468873';
+UPDATE tilbakekreving_hendelse SET sak_id = 6402 WHERE kravgrunnlag_id = '462257';
+UPDATE tilbakekreving_hendelse SET sak_id = 1590 WHERE kravgrunnlag_id = '472027';
+UPDATE tilbakekreving_hendelse SET sak_id = 6871 WHERE kravgrunnlag_id = '461175';
+UPDATE tilbakekreving_hendelse SET sak_id = 9785 WHERE kravgrunnlag_id = '473802';
+UPDATE tilbakekreving_hendelse SET sak_id = 3257 WHERE kravgrunnlag_id = '472026';
+UPDATE tilbakekreving_hendelse SET sak_id = 11739 WHERE kravgrunnlag_id = '463836';
+UPDATE tilbakekreving_hendelse SET sak_id = 12060 WHERE kravgrunnlag_id = '470809';
+UPDATE tilbakekreving_hendelse SET sak_id = 11958 WHERE kravgrunnlag_id = '457687';
+UPDATE tilbakekreving_hendelse SET sak_id = 11889 WHERE kravgrunnlag_id = '472419';

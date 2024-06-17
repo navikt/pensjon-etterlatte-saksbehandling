babb2514dfc11bb8d5beca39682487fe98e29d90
Henrik Gundersen <henrik.gundersen@nav.no>
2024-06-17 11:10:02 +0200

    EY-4067: Nytt endepunkt for å hente kravgrunnlag

--- apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/vedtak/TilbakekrevingRoutes.kt
+++ apps/etterlatte-tilbakekreving/src/main/kotlin/tilbakekreving/vedtak/TilbakekrevingRoutes.kt
@@ -5,22 +5,38 @@ import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.etterlatte.libs.common.tilbakekreving.TilbakekrevingVedtak
 import no.nav.etterlatte.libs.ktor.route.kunSystembruker
 import no.nav.etterlatte.libs.ktor.route.routeLogger
 
-fun Route.tilbakekrevingVedtakRoutes(tilbakekrevingVedtakService: TilbakekrevingVedtakService) {
+fun Route.tilbakekrevingRoutes(tilbakekrevingVedtakService: TilbakekrevingService) {
     val logger = routeLogger
 
-    route("/api/tilbakekreving/tilbakekrevingsvedtak") {
-        post {
+    route("/api/tilbakekreving/{sakId}") {
+        post("/vedtak") {
             kunSystembruker {
                 logger.info("Sender tilbakekrevingsvedtak")
                 val vedtak = call.receive<TilbakekrevingVedtak>()
                 tilbakekrevingVedtakService.sendTilbakekrevingsvedtak(vedtak)
                 call.respond(HttpStatusCode.Created)
+            }
+        }
+
+        get("/kravgrunnlag/{kravgrunnlagId}") {
+            kunSystembruker {
+                logger.info("Henter oppdatert kravgrunnlag")
+                val sakId = requireNotNull(call.parameters["sakId"]).toLong()
+                val kravgrunnlagId = requireNotNull(call.parameters["kravgrunnlagId"]).toLong()
+
+                val oppdatertKravgrunnlag =
+                    tilbakekrevingVedtakService.hentKravgrunnlag(
+                        kravgrunnlagId = kravgrunnlagId,
+                        sakId = sakId,
+                    )
+                call.respond(oppdatertKravgrunnlag)
             }
         }
     }

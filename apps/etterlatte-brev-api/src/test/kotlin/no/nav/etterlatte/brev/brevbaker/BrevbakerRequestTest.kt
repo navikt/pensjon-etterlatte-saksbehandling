c25fb1b002293c93a52b3c4069d4492111ef012a
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-06-17 17:43:29 +0200

    Flere testscenario vergenavn

--- apps/etterlatte-brev-api/src/test/kotlin/no/nav/etterlatte/brev/brevbaker/BrevbakerRequestTest.kt
+++ apps/etterlatte-brev-api/src/test/kotlin/no/nav/etterlatte/brev/brevbaker/BrevbakerRequestTest.kt
@@ -30,16 +30,31 @@ class BrevbakerRequestTest {
     }
 
     @Test
-    fun `skal sette verges navn til søkers navn ved verge hvis verge ikke finnes`() {
+    fun `skal ikke sette verges navn hvis verge ikke finner og det er barnepensjon over 18 år`() {
         val request =
             brevbakerRequest(
+                sakType = SakType.BARNEPENSJON,
                 soekerOgEventuellVerge =
                     SoekerOgEventuellVerge(
-                        Soeker("", "", "", Foedselsnummer("08498224343"), null, true, true),
+                        Soeker("Søker", null, "Søkersen", Foedselsnummer("08498224343"), false, false, false),
                         null,
                     ),
             )
-        request.felles.vergeNavn shouldBe "Palle Poulsen"
+        request.felles.vergeNavn shouldBe null
+    }
+
+    @Test
+    fun `skal sette verges navn til søkers navn ved verge hvis verge ikke finnes og det er barnepensjon der vi ikke er over 18`() {
+        val request =
+            brevbakerRequest(
+                sakType = SakType.BARNEPENSJON,
+                soekerOgEventuellVerge =
+                    SoekerOgEventuellVerge(
+                        Soeker("Søker", null, "Søkersen", Foedselsnummer("08498224343"), null, true, true),
+                        null,
+                    ),
+            )
+        request.felles.vergeNavn shouldBe "Søker Søkersen ved verge"
     }
 
     @Test

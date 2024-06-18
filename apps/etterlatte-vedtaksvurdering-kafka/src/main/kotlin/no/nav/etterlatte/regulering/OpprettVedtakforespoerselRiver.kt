81f5f11f6a1963f369cd015f13104079a3be5c9c
Mads Opheim <mads.opheim@nav.no>
2024-06-18 11:59:32 +0200

    Fyllar ut vedtak beløp-kode

--- apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/no/nav/etterlatte/regulering/OpprettVedtakforespoerselRiver.kt
+++ apps/etterlatte-vedtaksvurdering-kafka/src/main/kotlin/no/nav/etterlatte/regulering/OpprettVedtakforespoerselRiver.kt
@@ -2,20 +2,28 @@ package no.nav.etterlatte.regulering
 
 import no.nav.etterlatte.VedtakService
 import no.nav.etterlatte.funksjonsbrytere.FeatureToggleService
+import no.nav.etterlatte.libs.common.vedtak.VedtakInnholdDto
 import no.nav.etterlatte.no.nav.etterlatte.regulering.ReguleringFeatureToggle
 import no.nav.etterlatte.rapidsandrivers.BEHANDLING_ID_KEY
 import no.nav.etterlatte.rapidsandrivers.DATO_KEY
 import no.nav.etterlatte.rapidsandrivers.Kontekst
 import no.nav.etterlatte.rapidsandrivers.ListenerMedLoggingOgFeilhaandtering
+import no.nav.etterlatte.rapidsandrivers.ReguleringEvents
 import no.nav.etterlatte.rapidsandrivers.ReguleringHendelseType
 import no.nav.etterlatte.rapidsandrivers.SAK_ID_KEY
 import no.nav.etterlatte.rapidsandrivers.behandlingId
+import no.nav.etterlatte.rapidsandrivers.dato
 import no.nav.etterlatte.rapidsandrivers.sakId
 import no.nav.etterlatte.vedtaksvurdering.RapidUtsender
+import no.nav.etterlatte.vedtaksvurdering.VedtakOgRapid
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.LoggerFactory
+import tidspunkt.erEtter
+import tidspunkt.erFoerEllerPaa
+import java.math.BigDecimal
+import java.time.LocalDate
 
 internal class OpprettVedtakforespoerselRiver(
     rapidsConnection: RapidsConnection,
@@ -48,7 +56,22 @@ internal class OpprettVedtakforespoerselRiver(
             } else {
                 vedtak.opprettVedtakFattOgAttester(packet.sakId, behandlingId)
             }
+        hentBeloep(respons, packet.dato)?.let { packet[ReguleringEvents.VEDTAK_BELOEP] = it }
         logger.info("Opprettet vedtak ${respons.vedtak.id} for sak: $sakId og behandling: $behandlingId")
         RapidUtsender.sendUt(respons, packet, context)
     }
+
+    private fun hentBeloep(
+        respons: VedtakOgRapid,
+        dato: LocalDate,
+    ): BigDecimal? =
+        if (respons.vedtak.innhold is VedtakInnholdDto.VedtakBehandlingDto) {
+            (respons.vedtak.innhold as VedtakInnholdDto.VedtakBehandlingDto)
+                .utbetalingsperioder
+                .filter { it.periode.fom.erFoerEllerPaa(dato) }
+                .first { it.periode.tom.erEtter(dato) }
+                .beloep
+        } else {
+            null
+        }
 }

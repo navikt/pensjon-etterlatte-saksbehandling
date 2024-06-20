ea72dc47ef959f19fa02c3b8deb5e66626df1d6f
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-06-20 15:05:59 +0200

    Veldig enkelt eksempel

--- apps/etterlatte-design/src/main.tsx
+++ apps/etterlatte-design/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@navikt/ds-css";
+import { Box, Heading, HStack, Tag, VStack } from "@navikt/ds-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Box paddingBlock="8" paddingInline="8">
+      <VStack gap="4">
+        <Heading size="large">
+          Etterlatte sine visuelle bestemmelser for Gjenny!
+        </Heading>
+
+        <Heading size="medium">Tags</Heading>
+        <VStack gap="2">
+          <div>
+            <Tag variant="warning">Barnepejson</Tag>
+          </div>
+          <div>
+            <Tag variant="warning">Omstillingstønad</Tag>
+          </div>
+        </VStack>
+      </VStack>
+    </Box>
   </React.StrictMode>,
-)
+);

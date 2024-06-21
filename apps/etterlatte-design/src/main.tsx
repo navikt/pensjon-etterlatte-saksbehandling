440e2caf2bec4e25787a701af2e82fcf6859af31
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-06-21 08:48:14 +0200

    Test conditional workflow

--- apps/etterlatte-design/src/main.tsx
+++ apps/etterlatte-design/src/main.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@navikt/ds-css";
-import { Box, Heading, Tag, VStack } from "@navikt/ds-react";
+import { Box, Heading, HStack, Label, Tag, VStack } from "@navikt/ds-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Box paddingBlock="8" paddingInline="8">
       <VStack gap="4">
-        <Heading size="large">
+        <Heading size="xlarge">
           Etterlatte sine visuelle bestemmelser for Gjenny!
         </Heading>
 
-        <Heading size="medium">Tags</Heading>
-        <VStack gap="2">
-          <div>
-            <Tag variant="warning">Barnepejson</Tag>
-          </div>
-          <div>
-            <Tag variant="warning">Omstillingstønad</Tag>
-          </div>
-        </VStack>
+        <Heading size="large">Tags</Heading>
+
+        <HStack gap="4">
+          <VStack gap="2">
+            <Heading size="medium">Ytelse</Heading>
+
+            <Label>Lang</Label>
+            <div>
+              <Tag variant="warning">Barnepejson</Tag>
+            </div>
+            <div>
+              <Tag variant="warning">Omstillingstønad</Tag>
+            </div>
+
+            <Label>Kort</Label>
+          </VStack>
+
+          <VStack gap="2">
+            <Heading size="medium">Behandlingstype</Heading>
+          </VStack>
+        </HStack>
       </VStack>
     </Box>
   </React.StrictMode>,

a88c990f999defbae80a042c2da2aebb813e8536
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-06-21 15:40:25 +0200

    3rd time's the charm.. methinks

--- apps/etterlatte-hendelser-samordning/src/main/kotlin/no/nav/etterlatte/Application.kt
+++ apps/etterlatte-hendelser-samordning/src/main/kotlin/no/nav/etterlatte/Application.kt
@@ -4,15 +4,13 @@ import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.Application
 import no.nav.etterlatte.kafka.startLytting
 import no.nav.etterlatte.libs.ktor.initialisering.initEmbeddedServerUtenRest
-import no.nav.etterlatte.libs.ktor.route.logger
 import no.nav.etterlatte.libs.ktor.setReady
 import no.nav.etterlatte.samordning.ApplicationContext
 import no.nav.etterlatte.samordning.FAGOMRADE_OMS
 import no.nav.etterlatte.samordning.SAKSTYPE_OMS
 import no.nav.etterlatte.samordning.SamordningVedtakHendelse
 import org.slf4j.LoggerFactory
-import java.util.Timer
-import java.util.TimerTask
+import kotlin.concurrent.timer
 
 fun main() {
     Server(ApplicationContext()).run()
@@ -29,27 +27,31 @@ class Server(
 
     fun run() {
         startLytting(context.konsument, LoggerFactory.getLogger(Application::class.java))
-        setReady().also { engine.start(true) }
+        setReady()
+            .also { engine.start(true) }
+            .also {
+                if (context.isProd) {
+                    context.handler.logger.info("isProd=true, enabling FAGSYSTEM-335360")
 
-        if (context.isProd) {
-            context.handler.logger.info("isProd=true, enabling FAGSYSTEM-335360")
-            Timer("fake-tp-svar").schedule(
-                object : TimerTask() {
-                    override fun run() {
-                        context.handler.logger.info("Behandler spesialtilfelle FAGSYSTEM-335360")
-                        context.handler.handleSamordningHendelse(
-                            SamordningVedtakHendelse().apply {
-                                fagomrade = FAGOMRADE_OMS
-                                artTypeKode = SAKSTYPE_OMS
-                                vedtakId = 32136L
-                            },
-                        )
-                    }
-                },
-                60_000L,
-            )
-        } else {
-            context.handler.logger.info("isProd=false, skipping FAGSYSTEM-335360")
-        }
+                    timer(
+                        name = "fake-tp-svar",
+                        daemon = false,
+                        initialDelay = 60_000L,
+                        period = 0,
+                        action = {
+                            context.handler.logger.info("Behandler spesialtilfelle FAGSYSTEM-335360")
+                            context.handler.handleSamordningHendelse(
+                                SamordningVedtakHendelse().apply {
+                                    fagomrade = FAGOMRADE_OMS
+                                    artTypeKode = SAKSTYPE_OMS
+                                    vedtakId = 32136L
+                                },
+                            )
+                        },
+                    )
+                } else {
+                    context.handler.logger.info("isProd=false, skipping FAGSYSTEM-335360")
+                }
+            }
     }
 }

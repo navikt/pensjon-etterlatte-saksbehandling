d11b6d9183fcf4d887405a0c47937051cfcf420c
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-06-21 13:11:33 +0200

    Fiks opp i tags for tema

--- apps/etterlatte-saksbehandling-ui/client/src/shared/tags/GosysTemaTag.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/tags/GosysTemaTag.tsx
@@ -1,14 +1,15 @@
 import { GosysTema } from '~shared/types/Gosys'
 import { Tag } from '@navikt/ds-react'
-import { Variants } from '~shared/Tags'
 
 export const GosysTemaTag = ({ tema }: { tema: GosysTema }) => {
   switch (tema) {
     case 'EYO':
-      return <Tag variant={Variants.ALT2}>Omstillingsstønad</Tag>
+      return <Tag variant="alt2-moderate">Omstillingsstønad</Tag>
     case 'EYB':
-      return <Tag variant={Variants.INFO}>Barnepensjon</Tag>
+      return <Tag variant="alt1-moderate">Barnepensjon</Tag>
     case 'PEN':
-      return <Tag variant={Variants.ALT1}>Pensjon</Tag>
+      return <Tag variant="info-filled">Pensjon</Tag>
+    default:
+      return <Tag variant="error-filled">Ukjent tema</Tag>
   }
 }

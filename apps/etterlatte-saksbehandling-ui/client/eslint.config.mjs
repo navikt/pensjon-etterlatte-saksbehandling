4b1b425d515907b26779ee4aaf4439e4059a1d14
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-06-24 14:24:00 +0200

    Ny flat filer for eslint config

---
+++ apps/etterlatte-saksbehandling-ui/client/eslint.config.mjs
@@ -0,0 +1,75 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["node_modules/*", "nais/*", "build/*"],
+}, ...fixupConfigRules(
+    compat.extends("plugin:react/recommended", "plugin:@typescript-eslint/recommended"),
+), {
+    plugins: {
+        react: fixupPluginRules(react),
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        "jsx-a11y": jsxA11Y,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            process: true,
+            require: true,
+            module: true,
+            __dirname: true,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 12,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "17.0.2",
+        },
+    },
+
+    rules: {
+        "react/jsx-curly-brace-presence": "error",
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "react/display-name": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-extra-non-null-assertion": "off",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "@typescript-eslint/no-extra-semi": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error",
+    },
+}];

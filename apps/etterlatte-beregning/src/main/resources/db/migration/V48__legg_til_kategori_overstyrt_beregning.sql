2705504ad9439617379e52877479b4ce7441b0f6
Andreas Balevik <andreasbalevik@gmail.no>
2024-06-24 09:23:43 +0200

    EY-4083 rettet sql for setting av default kategori for eksisterende overstyringer

--- apps/etterlatte-beregning/src/main/resources/db/migration/V48__legg_til_kategori_overstyrt_beregning.sql
+++ apps/etterlatte-beregning/src/main/resources/db/migration/V48__legg_til_kategori_overstyrt_beregning.sql
@@ -1,3 +1,4 @@
-ALTER TABLE overstyr_beregning ADD COLUMN kategori TEXT
+ALTER TABLE overstyr_beregning ADD COLUMN kategori TEXT;
 
-UPDATE overstyrt_beregning SET kategori = "UKJENT_KATEGORI"
+UPDATE overstyr_beregning
+SET kategori = 'UKJENT_KATEGORI';

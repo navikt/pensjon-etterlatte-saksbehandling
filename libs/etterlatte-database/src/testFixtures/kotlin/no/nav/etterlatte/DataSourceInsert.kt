eaf4160376e3a2f94ff50f2f6b7a1c27c878c591
Mads Opheim <mads.opheim@nav.no>
2024-06-24 13:41:45 +0200

    Tilpassar test-hjelpemetode for å kunne lage databasetabellar

--- libs/etterlatte-database/src/testFixtures/kotlin/no/nav/etterlatte/DataSourceInsert.kt
+++ libs/etterlatte-database/src/testFixtures/kotlin/no/nav/etterlatte/DataSourceInsert.kt
@@ -1,20 +1,22 @@
 package no.nav.etterlatte
 
+import kotliquery.TransactionalSession
 import kotliquery.queryOf
 import no.nav.etterlatte.libs.database.transaction
 import javax.sql.DataSource
 
 fun DataSource.insert(
     tabellnavn: String,
-    params: Map<String, Any>,
+    params: (tx: TransactionalSession) -> Map<String, Any>,
 ) {
     this.transaction { tx ->
+        val paramMap = params(tx)
         queryOf(
             """INSERT INTO $tabellnavn 
-                    (${params.keys.joinToString(", ") { it }})
+                    (${paramMap.keys.joinToString(", ") { it }})
                      VALUES 
-                    (${params.keys.joinToString(", ") { ":$it" }})""",
-            paramMap = params,
+                    (${paramMap.keys.joinToString(", ") { ":$it" }})""",
+            paramMap = paramMap,
         ).let { query -> tx.run(query.asUpdate) }
     }
 }

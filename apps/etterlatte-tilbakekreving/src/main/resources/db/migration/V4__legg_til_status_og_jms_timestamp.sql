1d7c8fd43c06aaca350b5edc434bbde6d59ed766
Henrik Gundersen <henrik.gundersen@nav.no>
2024-06-25 11:47:53 +0200

    EY-4124: Oppdaterer db-script

--- apps/etterlatte-tilbakekreving/src/main/resources/db/migration/V4__legg_til_status_og_jms_timestamp.sql
+++ apps/etterlatte-tilbakekreving/src/main/resources/db/migration/V4__legg_til_status_og_jms_timestamp.sql
@@ -4,7 +4,7 @@ ALTER TABLE tilbakekreving_hendelse ADD COLUMN jms_timestamp TIMESTAMP WITH TIME
 
 -- Migrerer eksisterende rader med verdier for nye kolonner
 UPDATE tilbakekreving_hendelse SET status = 'FERDIGSTILT' WHERE status IS NULL;
-UPDATE tilbakekreving_hendelse t SET jms_timestamp = (SELECT opprettet FROM tilbakekreving_hendelse WHERE id = t.id) WHERE jms_timestamp IS NULL;
+UPDATE tilbakekreving_hendelse t SET jms_timestamp = (SELECT opprettet FROM tilbakekreving_hendelse WHERE id = t.id) WHERE jms_timestamp IS NULL AND (type = 'KRAVGRUNNLAG_MOTTATT' OR type = 'KRAV_VEDTAK_STATUS_MOTTATT');
 
 -- Setter påkrevet constraint for status
 ALTER TABLE tilbakekreving_hendelse ALTER COLUMN status SET NOT NULL;

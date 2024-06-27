94c0c2f7480442edba7724fc1ee59a0b9c501ecd
Nicolas N <nicolas.nordhagen@nav.no>
2024-06-27 15:03:12 +0200

    Opprydding i komponenter brukt til oversikt (innvilget, underkjent, m.m.)

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/attestering/attestering/godkjenn.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/attestering/attestering/godkjenn.tsx
@@ -1,29 +1,26 @@
-import { Textarea } from '@navikt/ds-react'
+import { Textarea, VStack } from '@navikt/ds-react'
 import { useState } from 'react'
-import { BeslutningWrapper, Text } from '../styled'
 import { AttesterVedtak } from '~components/behandling/attestering/handinger/attesterVedtak'
 
 export const Godkjenn = () => {
   const [tilbakemeldingFraAttestant, setTilbakemeldingFraAttestant] = useState('')
 
   return (
-    <BeslutningWrapper>
+    <VStack gap="4">
+      <Textarea
+        style={{ padding: '10px' }}
+        label="Tilbakemelding fra attestant"
+        placeholder="Beskriv etterarbeid som er gjort. f.eks. overført oppgave til NØP om kontonummer / skattetrekk."
+        value={tilbakemeldingFraAttestant}
+        onChange={(e) => setTilbakemeldingFraAttestant(e.target.value)}
+        minRows={3}
+        size="small"
+        autoComplete="off"
+      />
+
       <div>
-        <Text>Tilbakemelding fra attestant</Text>
-        <Textarea
-          style={{ padding: '10px' }}
-          label="Kommentar fra attestant"
-          hideLabel={true}
-          placeholder="Beskriv etterarbeid som er gjort. f.eks. overført oppgave til NØP om kontonummer / skattetrekk."
-          value={tilbakemeldingFraAttestant}
-          onChange={(e) => setTilbakemeldingFraAttestant(e.target.value)}
-          minRows={3}
-          size="small"
-          autoComplete="off"
-        />
+        <AttesterVedtak kommentar={tilbakemeldingFraAttestant} />
       </div>
-      <br />
-      <AttesterVedtak kommentar={tilbakemeldingFraAttestant} />
-    </BeslutningWrapper>
+    </VStack>
   )
 }

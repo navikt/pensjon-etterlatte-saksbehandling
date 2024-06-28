466d9a3b45b326c728b78530989baad0c7bf23fc
Espen Schei <espen.schei@nav.no>
2024-06-28 13:28:38 +0200

    Legg til kjøring for juni også. Skal kun være én sak.

--- apps/etterlatte-tidshendelser/src/main/resources/db/migration/V9__aktivitetsplikt_kjoeringer_juli_august.sql
+++ apps/etterlatte-tidshendelser/src/main/resources/db/migration/V9__aktivitetsplikt_kjoeringer_juli_august.sql
@@ -6,6 +6,8 @@ values ('OMS_DOED_4MND', '2024-08-01', '2024-08');
 
 -- Jobben for å vurdere aktivitetsplikt etter 6 mnd
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_6MND', '2024-07-01', '2024-06');
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
 values ('OMS_DOED_6MND', '2024-07-01', '2024-07');
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
 values ('OMS_DOED_6MND', '2024-08-01', '2024-08');

936bb03ae4e3428db4ac6b151996941e99310f4a
Nicolas N <Watercolours@users.noreply.github.com>
2024-06-28 15:00:19 +0200

    Hotfix: Flere tilfeller av dato som er NULL (#5280)

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/familieforhold/omstillingsstoenad/BarneListe.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/familieforhold/omstillingsstoenad/BarneListe.tsx
@@ -2,7 +2,7 @@ import { CopyButton, Heading, HStack, Link, Table, VStack } from '@navikt/ds-rea
 import { Familieforhold, IPdlPerson } from '~shared/types/Person'
 import styled from 'styled-components'
 import { IAdresse } from '~shared/types/IAdresse'
-import { formaterDato } from '~utils/formatering/dato'
+import { formaterDato, formaterKanskjeStringDato } from '~utils/formatering/dato'
 import { IconSize } from '~shared/types/Icon'
 import { ChildEyesIcon } from '@navikt/aksel-icons'
 import { hentAlderForDato } from '~components/behandling/felles/utils'
@@ -87,7 +87,7 @@ const BarnRow = ({ barn, familieforhold }: { barn: IPdlPerson; familieforhold: F
   const aktivAdresse: IAdresse | undefined = barn.bostedsadresse?.find((adresse: IAdresse) => adresse.aktiv)
   const adresse = `${aktivAdresse?.adresseLinje1}, ${aktivAdresse?.postnr ?? ''} ${aktivAdresse?.poststed ?? ''}`
   const periode = aktivAdresse
-    ? `${formaterDato(aktivAdresse!!.gyldigFraOgMed!!)} - ${
+    ? `${formaterKanskjeStringDato(aktivAdresse.gyldigFraOgMed)} - ${
         aktivAdresse?.gyldigTilOgMed ? formaterDato(aktivAdresse!!.gyldigTilOgMed!!) : 'nå'
       }`
     : 'Mangler adresse'

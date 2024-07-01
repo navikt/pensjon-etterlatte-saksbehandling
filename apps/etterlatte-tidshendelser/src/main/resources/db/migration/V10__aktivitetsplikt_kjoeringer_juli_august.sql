356d64fdb4ebd34b6e1e6557ad7182119323a829
Espen Schei <espen.schei@nav.no>
2024-07-01 13:35:51 +0200

    Aktivitetsplikt-jobber for juli og august (#5283)
    EY-4135

---
+++ apps/etterlatte-tidshendelser/src/main/resources/db/migration/V10__aktivitetsplikt_kjoeringer_juli_august.sql
@@ -0,0 +1,11 @@
+-- Jobben for å vurdere aktivitetsplikt etter 4 mnd
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_4MND', '2024-07-01', '2024-07');
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_4MND', '2024-08-01', '2024-08');
+
+-- Jobben for å vurdere aktivitetsplikt etter 6 mnd
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_6MND', '2024-07-01', '2024-07');
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('OMS_DOED_6MND', '2024-08-01', '2024-08');

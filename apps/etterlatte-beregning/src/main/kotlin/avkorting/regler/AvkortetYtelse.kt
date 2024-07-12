8de3ce8028a0f626040b9ccbd70f3f9101b6d088
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-07-12 15:13:15 +0200

    TODO

--- apps/etterlatte-beregning/src/main/kotlin/avkorting/regler/AvkortetYtelse.kt
+++ apps/etterlatte-beregning/src/main/kotlin/avkorting/regler/AvkortetYtelse.kt
@@ -96,6 +96,7 @@ val avkortetYtelseMedRestanse =
             .let { max(it, 0) }
     }
 
+// TODO: her må inst hensyntas 17-13 femte ledd
 val avkortetYtelseMedRestanseOgSanksjon =
     RegelMeta(
         gjelderFra = OMS_GYLDIG_FRA,

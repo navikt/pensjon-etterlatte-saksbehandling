2d704359afc7bc3aa42153831980ada35dd624e2
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-07-18 12:46:53 +0200

    Nytt skjema for overstyr beregning

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregningsgrunnlag/overstyrGrunnlagsBeregning/OverstyrGrunnlagsBeregning.tsx
@@ -0,0 +1,78 @@
+import React, { Dispatch, useState } from 'react'
+import { ArrowCirclepathIcon, ArrowsCirclepathIcon } from '@navikt/aksel-icons'
+import { Button, Heading, HStack, Select, Textarea, VStack } from '@navikt/ds-react'
+import { OverstyrtBeregningKategori } from '~shared/types/OverstyrtBeregning'
+import { OverstyrBeregning } from '~shared/types/Beregning'
+import { useApiCall } from '~shared/hooks/useApiCall'
+import { opprettOverstyrBeregning } from '~shared/api/beregning'
+import { isPending } from '~shared/api/apiUtils'
+import { isFailureHandler } from '~shared/api/IsFailureHandler'
+
+export const OverstyrGrunnlagsBeregning = ({
+  behandlingId,
+  setOverstyrt,
+}: {
+  behandlingId: string
+  setOverstyrt: Dispatch<OverstyrBeregning>
+}) => {
+  const [aarsak, setAarsak] = useState<OverstyrtBeregningKategori>()
+  const [begrunnelse, setBegrunnelse] = useState<string>('')
+
+  const [aarsakError, setAarsakError] = useState<string>('')
+
+  const [opprettOverstyrBeregningResult, opprettOverstyrBeregningRequest] = useApiCall(opprettOverstyrBeregning)
+
+  const overstyrBeregning = () => {
+    if (!!aarsak) {
+      setAarsakError('')
+      opprettOverstyrBeregningRequest(
+        {
+          behandlingId,
+          beskrivelse: begrunnelse,
+          kategori: aarsak,
+        },
+        (result) => {
+          if (result) setOverstyrt(result)
+        }
+      )
+    } else setAarsakError('Du må velge en årsak')
+  }
+
+  return (
+    <VStack gap="4" maxWidth="20rem">
+      <HStack gap="2" align="center">
+        <ArrowsCirclepathIcon aria-hidden fontSize="1.5rem" />
+        <Heading size="small">Skal beregning overstyres?</Heading>
+      </HStack>
+      <Select
+        label="Årsak for overstyring"
+        error={aarsakError}
+        onChange={(e) => setAarsak(e.target.value as OverstyrtBeregningKategori)}
+      >
+        <option value="">Velg årsak</option>
+        {Object.entries(OverstyrtBeregningKategori).map(([key, value]) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </Select>
+      <Textarea label="Begrunnelse (valgfri)" onChange={(e) => setBegrunnelse(e.target.value)} />
+
+      {isFailureHandler({
+        apiResult: opprettOverstyrBeregningResult,
+        errorMessage: 'Feil under overstyring av beregning',
+      })}
+
+      <div>
+        <Button
+          size="small"
+          icon={<ArrowCirclepathIcon aria-hidden />}
+          loading={isPending(opprettOverstyrBeregningResult)}
+          onClick={overstyrBeregning}
+        >
+          Overstyr
+        </Button>
+      </div>
+    </VStack>
+  )
+}

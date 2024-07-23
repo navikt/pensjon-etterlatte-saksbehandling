ed6d81909b783460dce6f8af51e966f69dc3c59b
Mads Opheim <mads.opheim@nav.no>
2024-07-23 15:25:31 +0200

    Viss vi får ei klasse vi ikkje har tillatt å deserialisere, loggar vi det som warning

--- libs/etterlatte-ktor/src/main/kotlin/Serialiseringsfilter.kt
+++ libs/etterlatte-ktor/src/main/kotlin/Serialiseringsfilter.kt
@@ -1,16 +1,22 @@
 package no.nav.etterlatte.libs.ktor
 
+import org.slf4j.LoggerFactory
 import java.io.ObjectInputFilter
 import java.io.ObjectInputFilter.FilterInfo
 import java.io.ObjectInputFilter.Status
 
 class Serialiseringsfilter : ObjectInputFilter {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override fun checkInput(info: FilterInfo): Status {
         val serialClass = info.serialClass()
         return when {
             serialClass == null -> Status.UNDECIDED
             tillatt(serialClass) -> Status.ALLOWED
-            else -> Status.REJECTED
+            else -> {
+                logger.warn("Klasse ${serialClass.name} er ikke tillatt for deserialisering")
+                Status.REJECTED
+            }
         }
     }
 

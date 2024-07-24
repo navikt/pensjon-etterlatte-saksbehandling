7070091faa741e73bb1c008688ef3bb841928693
Mads Opheim <mads.opheim@nav.no>
2024-07-24 10:39:50 +0200

    Forenklar opplysninger-fra-soeknad

--- apps/etterlatte-opplysninger-fra-soeknad/src/main/kotlin/Application.kt
+++ apps/etterlatte-opplysninger-fra-soeknad/src/main/kotlin/Application.kt
@@ -2,13 +2,9 @@ package no.nav.etterlatte
 
 import no.nav.etterlatte.opplysningerfrasoknad.StartUthentingFraSoeknadRiver
 import no.nav.etterlatte.opplysningerfrasoknad.opplysningsuthenter.Opplysningsuthenter
-import no.nav.etterlatte.rapidsandrivers.getRapidEnv
-import no.nav.helse.rapids_rivers.RapidApplication
+import rapidsandrivers.initRogR
 
-fun main() {
-    RapidApplication
-        .create(getRapidEnv())
-        .also { rapidsConnection ->
-            StartUthentingFraSoeknadRiver(rapidsConnection, Opplysningsuthenter())
-        }.start()
-}
+fun main() =
+    initRogR { rapidsConnection, _ ->
+        StartUthentingFraSoeknadRiver(rapidsConnection, Opplysningsuthenter())
+    }

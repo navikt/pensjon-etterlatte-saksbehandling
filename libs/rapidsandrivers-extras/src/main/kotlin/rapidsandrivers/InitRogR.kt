a0f227ecfe5cdfad7cbc05ca200253b2e460dd46
Mads Opheim <mads.opheim@nav.no>
2024-07-24 11:20:10 +0200

    Opnar for å overstyre konfig

--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/InitRogR.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/InitRogR.kt
@@ -2,16 +2,19 @@ package rapidsandrivers
 
 import io.ktor.server.application.Application
 import no.nav.etterlatte.rapidsandrivers.getRapidEnv
+import no.nav.helse.rapids_rivers.AivenConfig
+import no.nav.helse.rapids_rivers.Config
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 
 fun initRogR(
     restModule: (Application.() -> Unit)? = null,
+    configFromEnvironment: Config = AivenConfig.default,
     settOppRivers: (RapidsConnection, rapidEnv: Map<String, String>) -> Unit,
 ) {
     val rapidEnv = getRapidEnv()
 
-    var builder = RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(rapidEnv))
+    var builder = RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(rapidEnv, configFromEnvironment))
     if (restModule != null) {
         builder = builder.withKtorModule(restModule)
     }

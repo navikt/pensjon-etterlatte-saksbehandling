8cb1f04dfc4fecb1095733de5805a2814729d6ea
Mads Opheim <mads.opheim@nav.no>
2024-07-24 11:22:14 +0200

    Får da også standardisert tidshendelser-init

--- apps/etterlatte-tidshendelser/src/main/kotlin/no/nav/etterlatte/Application.kt
+++ apps/etterlatte-tidshendelser/src/main/kotlin/no/nav/etterlatte/Application.kt
@@ -3,42 +3,35 @@ package no.nav.etterlatte
 import no.nav.etterlatte.libs.common.Miljoevariabler
 import no.nav.etterlatte.libs.database.migrate
 import no.nav.etterlatte.rapidsandrivers.configFromEnvironment
-import no.nav.etterlatte.rapidsandrivers.getRapidEnv
 import no.nav.etterlatte.tidshendelser.AppContext
 import no.nav.etterlatte.tidshendelser.HendelseRiver
-import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
+import rapidsandrivers.initRogR
 import java.util.Timer
 
-fun main() {
-    val rapidEnv = getRapidEnv()
-    val miljoevariabler = Miljoevariabler(rapidEnv)
+fun main() =
+    initRogR(configFromEnvironment = { configFromEnvironment(it) }) { rapidsConnection, rapidEnv ->
+        val miljoevariabler = Miljoevariabler(rapidEnv)
+        val appContext =
+            AppContext(miljoevariabler) { key, message -> rapidsConnection.publish(key.toString(), message) }
 
-    RapidApplication
-        .Builder(
-            RapidApplication.RapidApplicationConfig.fromEnv(rapidEnv, configFromEnvironment(rapidEnv)),
-        ).build()
-        .also { rapidsConnection ->
-            val appContext = AppContext(miljoevariabler) { key, message -> rapidsConnection.publish(key.toString(), message) }
+        HendelseRiver(rapidsConnection, appContext.hendelseDao)
 
-            HendelseRiver(rapidsConnection, appContext.hendelseDao)
+        rapidsConnection.apply {
+            val timers = mutableListOf<Timer>()
 
-            rapidsConnection.apply {
-                val timers = mutableListOf<Timer>()
+            register(
+                object : RapidsConnection.StatusListener {
+                    override fun onStartup(rapidsConnection: RapidsConnection) {
+                        appContext.dataSource.migrate()
+                        timers.add(appContext.jobbPollerTask.schedule())
+                        timers.add(appContext.hendelsePollerTask.schedule())
+                    }
 
-                register(
-                    object : RapidsConnection.StatusListener {
-                        override fun onStartup(rapidsConnection: RapidsConnection) {
-                            appContext.dataSource.migrate()
-                            timers.add(appContext.jobbPollerTask.schedule())
-                            timers.add(appContext.hendelsePollerTask.schedule())
-                        }
-
-                        override fun onShutdownSignal(rapidsConnection: RapidsConnection) {
-                            timers.forEach { it.cancel() }
-                        }
-                    },
-                )
-            }
-        }.start()
-}
+                    override fun onShutdownSignal(rapidsConnection: RapidsConnection) {
+                        timers.forEach { it.cancel() }
+                    }
+                },
+            )
+        }
+    }

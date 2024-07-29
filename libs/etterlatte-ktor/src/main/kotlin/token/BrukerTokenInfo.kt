1a16b56eec852bb19e3450c3fe7b3462ac4d5c5f
Mads Opheim <mads.opheim@nav.no>
2024-07-29 10:56:53 +0200

    Kan no forenkle bort identForBrev

--- libs/etterlatte-ktor/src/main/kotlin/token/BrukerTokenInfo.kt
+++ libs/etterlatte-ktor/src/main/kotlin/token/BrukerTokenInfo.kt
@@ -41,15 +41,6 @@ sealed class Systembruker(
 ) : BrukerTokenInfo() {
     override fun ident() = ident
 
-    fun identForBrev(): String {
-        val systemBrukereInternt = Systembrukere.entries.map { it.appName }
-        if (ident in systemBrukereInternt) {
-            return Fagsaksystem.EY.navn
-        } else {
-            return ident
-        }
-    }
-
     override fun accessToken() = throw NotImplementedError("Kun relevant for saksbehandler")
 
     override fun getClaims() = jwtTokenClaims

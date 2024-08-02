885fa4fe3ea9f61e28bcd24bb60ec3a591e1d012
Andreas Balevik <andreasbalevik@gmail.no>
2024-08-02 11:59:17 +0200

    EY-4093 endre type for saksId argument SaksoversiktLenke

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/components/SaksoversiktLenke.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/components/SaksoversiktLenke.tsx
@@ -1,7 +1,7 @@
 import { Link } from '@navikt/ds-react'
 
-const SaksoversiktLenke = ({ sakId }: { sakId: string }) => {
-  return <Link href={`/sak/${sakId}`}>{sakId}</Link>
+const SaksoversiktLenke = ({ sakId }: { sakId: number }) => {
+  return <Link href={`/person/${sakId}`}>{sakId}</Link>
 }
 
 export default SaksoversiktLenke

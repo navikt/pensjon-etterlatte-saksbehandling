46c336a3f745050d7581fc966f684a57d8cb787b
Mads Opheim <mads.opheim@nav.no>
2024-08-12 12:45:02 +0200

    Readiness-proben var litt malplassert, så flyttar

--- libs/etterlatte-ktor/src/main/kotlin/initialisering/ReadinessProbe.kt
+++ libs/etterlatte-ktor/src/main/kotlin/initialisering/ReadinessProbe.kt
@@ -5,8 +5,6 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 private var ready = AtomicBoolean(false)
 
-fun setReady(value: Boolean = true) {
-    ready.set(value)
-}
+fun setReady(value: Boolean = true) = ready.set(value)
 
 fun isReady() = if (ready.get()) HttpStatusCode.OK else HttpStatusCode.ServiceUnavailable

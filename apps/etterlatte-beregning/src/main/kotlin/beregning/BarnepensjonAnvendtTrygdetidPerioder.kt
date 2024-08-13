2f5a659f44a70a15bb5a719175d2d3c43f617a73
Mads Opheim <mads.opheim@nav.no>
2024-08-13 14:03:15 +0200

    EY-4190: BeregnBarnepensjonService-fila er så omfattande at det kjens knotete å jobbe der, så trekkjer heile trygdetidperioder-objektet ut. Ingen endringar her, utover å endre namnet på klassa til å indikere BP

---
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/BarnepensjonAnvendtTrygdetidPerioder.kt
@@ -0,0 +1,104 @@
+package no.nav.etterlatte.beregning
+
+import no.nav.etterlatte.beregning.grunnlag.BeregningsGrunnlag
+import no.nav.etterlatte.beregning.grunnlag.GrunnlagMedPeriode
+import no.nav.etterlatte.beregning.grunnlag.kombinerOverlappendePerioder
+import no.nav.etterlatte.beregning.regler.AnvendtTrygdetid
+import no.nav.etterlatte.beregning.regler.barnepensjon.trygdetidsfaktor.TrygdetidGrunnlag
+import no.nav.etterlatte.beregning.regler.barnepensjon.trygdetidsfaktor.anvendtTrygdetidRegel
+import no.nav.etterlatte.beregning.regler.toSamlet
+import no.nav.etterlatte.libs.common.beregning.SamletTrygdetidMedBeregningsMetode
+import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
+import no.nav.etterlatte.libs.common.feilhaandtering.UgyldigForespoerselException
+import no.nav.etterlatte.libs.common.logging.sikkerlogger
+import no.nav.etterlatte.libs.common.trygdetid.TrygdetidDto
+import no.nav.etterlatte.libs.regler.FaktumNode
+import no.nav.etterlatte.libs.regler.KonstantGrunnlag
+import no.nav.etterlatte.libs.regler.RegelPeriode
+import no.nav.etterlatte.libs.regler.RegelkjoeringResultat
+import no.nav.etterlatte.libs.regler.eksekver
+import org.slf4j.LoggerFactory
+import java.time.LocalDate
+
+object BarnepensjonAnvendtTrygdetidPerioder {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun finnAnvendtTrygdetidPerioder(
+        trygdetider: List<TrygdetidDto>,
+        beregningsGrunnlag: BeregningsGrunnlag,
+    ) = anvendtPerioder(beregningsGrunnlag.finnMuligeTrygdetidPerioder(trygdetider))
+
+    fun finnKonstantTrygdetidPerioder(
+        trygdetider: List<TrygdetidDto>,
+        beregningsGrunnlag: BeregningsGrunnlag,
+        fom: LocalDate,
+    ): List<AnvendtTrygdetid> {
+        if (trygdetider.size != 1) {
+            throw UgyldigForespoerselException(
+                code = "FEIL_ANTALL_TRYGDETIDER",
+                detail = "Fant flere trygdetider - men fikk bare en beregningsgrunnlag",
+            )
+        }
+
+        return trygdetider.first().toSamlet(beregningsGrunnlag.beregningsMetode.beregningsMetode)?.let {
+            anvendtPerioder(
+                listOf(
+                    GrunnlagMedPeriode(it, fom, null),
+                ),
+            ).first().data
+        } ?: throw TrygdetidIkkeOpprettet()
+    }
+
+    private fun anvendtPerioder(muligePerioder: List<GrunnlagMedPeriode<SamletTrygdetidMedBeregningsMetode>>) =
+        muligePerioder
+            .map {
+                anvendtTrygdetidRegel.eksekver(
+                    KonstantGrunnlag(
+                        TrygdetidGrunnlag(
+                            FaktumNode(it.data, kilde = "Trygdetid", beskrivelse = "Beregnet trygdetid for avdød"),
+                        ),
+                    ),
+                    RegelPeriode(it.fom, it.tom),
+                )
+            }.map {
+                when (it) {
+                    is RegelkjoeringResultat.Suksess -> {
+                        val aktueltResultat = it.periodiserteResultater.single()
+                        GrunnlagMedPeriode(
+                            data = aktueltResultat.resultat.verdi,
+                            fom = aktueltResultat.periode.fraDato,
+                            tom = aktueltResultat.periode.tilDato,
+                        )
+                    }
+
+                    is RegelkjoeringResultat.UgyldigPeriode -> throw InternfeilException(
+                        "Ugyldig regler for periode: ${it.ugyldigeReglerForPeriode}",
+                    )
+                }
+            }.kombinerOverlappendePerioder()
+
+    private fun BeregningsGrunnlag.finnMuligeTrygdetidPerioder(trygdetider: List<TrygdetidDto>) =
+        begegningsmetodeFlereAvdoede.map { beregningsmetodeForAvdoedPeriode ->
+            GrunnlagMedPeriode(
+                data =
+                    trygdetider
+                        .finnForAvdoed(
+                            beregningsmetodeForAvdoedPeriode.data.avdoed,
+                        ).toSamlet(
+                            beregningsmetodeForAvdoedPeriode.data.beregningsMetode.beregningsMetode,
+                        ) ?: throw InternfeilException("Kunne ikke samle trygdetid for avdoed").also {
+                        logger.warn("Kunne ikke samle trygdetid for avdoed - se sikkerlogg")
+                        sikkerlogger().warn("Kunne ikke samle trygdetid for avdoed ${beregningsmetodeForAvdoedPeriode.data.avdoed}")
+                    },
+                fom = beregningsmetodeForAvdoedPeriode.fom,
+                tom = beregningsmetodeForAvdoedPeriode.tom,
+            )
+        }
+
+    fun List<TrygdetidDto>.finnForAvdoed(avdoed: String) =
+        this.find { it.ident == avdoed }
+            ?: throw InternfeilException("Manglende trygdetid for avdoed").also {
+                logger.warn("Fant ikke trygdetid for avdoed - se sikkerlogg")
+                sikkerlogger().warn("Fant ikke trygdetid for avdoed $avdoed i $this")
+            }
+}
--- apps/etterlatte-beregning/src/main/kotlin/beregning/BeregnBarnepensjonService.kt
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/BeregnBarnepensjonService.kt
@@ -1,24 +1,20 @@
 package no.nav.etterlatte.beregning
 
-import no.nav.etterlatte.beregning.AnvendtTrygdetidPerioder.finnForAvdoed
+import no.nav.etterlatte.beregning.BarnepensjonAnvendtTrygdetidPerioder.finnForAvdoed
 import no.nav.etterlatte.beregning.grunnlag.BeregningsGrunnlag
 import no.nav.etterlatte.beregning.grunnlag.BeregningsGrunnlagService
 import no.nav.etterlatte.beregning.grunnlag.GrunnlagMedPeriode
 import no.nav.etterlatte.beregning.grunnlag.PeriodisertBeregningGrunnlag
-import no.nav.etterlatte.beregning.grunnlag.kombinerOverlappendePerioder
 import no.nav.etterlatte.beregning.grunnlag.mapVerdier
 import no.nav.etterlatte.beregning.regler.AnvendtTrygdetid
 import no.nav.etterlatte.beregning.regler.barnepensjon.PeriodisertBarnepensjonGrunnlag
 import no.nav.etterlatte.beregning.regler.barnepensjon.kroneavrundetBarnepensjonRegelMedInstitusjon
 import no.nav.etterlatte.beregning.regler.barnepensjon.sats.avdodeForeldre2024
 import no.nav.etterlatte.beregning.regler.barnepensjon.sats.grunnbeloep
-import no.nav.etterlatte.beregning.regler.barnepensjon.trygdetidsfaktor.TrygdetidGrunnlag
-import no.nav.etterlatte.beregning.regler.barnepensjon.trygdetidsfaktor.anvendtTrygdetidRegel
 import no.nav.etterlatte.beregning.regler.barnepensjon.trygdetidsfaktor.trygdetidBruktRegel
 import no.nav.etterlatte.beregning.regler.finnAnvendtGrunnbeloep
 import no.nav.etterlatte.beregning.regler.finnAnvendtTrygdetid
 import no.nav.etterlatte.beregning.regler.finnAvdodeForeldre
-import no.nav.etterlatte.beregning.regler.toSamlet
 import no.nav.etterlatte.grunnbeloep.GrunnbeloepRepository
 import no.nav.etterlatte.klienter.GrunnlagKlient
 import no.nav.etterlatte.klienter.TrygdetidKlient
@@ -28,12 +24,8 @@ import no.nav.etterlatte.libs.common.behandling.DetaljertBehandling
 import no.nav.etterlatte.libs.common.behandling.virkningstidspunkt
 import no.nav.etterlatte.libs.common.beregning.Beregningsperiode
 import no.nav.etterlatte.libs.common.beregning.Beregningstype
-import no.nav.etterlatte.libs.common.beregning.SamletTrygdetidMedBeregningsMetode
-import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
-import no.nav.etterlatte.libs.common.feilhaandtering.UgyldigForespoerselException
 import no.nav.etterlatte.libs.common.grunnlag.Grunnlag
 import no.nav.etterlatte.libs.common.grunnlag.Metadata
-import no.nav.etterlatte.libs.common.logging.sikkerlogger
 import no.nav.etterlatte.libs.common.objectMapper
 import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.common.trygdetid.TrygdetidDto
@@ -89,7 +81,7 @@ class BeregnBarnepensjonService(
         }
 
         val anvendtTrygdetider =
-            AnvendtTrygdetidPerioder.finnAnvendtTrygdetidPerioder(trygdetidListe, beregningsGrunnlag)
+            BarnepensjonAnvendtTrygdetidPerioder.finnAnvendtTrygdetidPerioder(trygdetidListe, beregningsGrunnlag)
 
         val barnepensjonGrunnlag =
             opprettBeregningsgrunnlag(
@@ -309,7 +301,7 @@ class BeregnBarnepensjonService(
                 KonstantGrunnlag(
                     FaktumNode(
                         verdi =
-                            AnvendtTrygdetidPerioder.finnKonstantTrygdetidPerioder(
+                            BarnepensjonAnvendtTrygdetidPerioder.finnKonstantTrygdetidPerioder(
                                 trygdetider,
                                 beregningsGrunnlag,
                                 fom,
@@ -343,87 +335,4 @@ class BeregnBarnepensjonService(
                     },
             ) { _, _, _ -> FaktumNode(null, beregningsGrunnlag.kilde, "Institusjonsopphold") },
     )
-}
-
-object AnvendtTrygdetidPerioder {
-    private val logger = LoggerFactory.getLogger(AnvendtTrygdetidPerioder::class.java)
-
-    fun finnAnvendtTrygdetidPerioder(
-        trygdetider: List<TrygdetidDto>,
-        beregningsGrunnlag: BeregningsGrunnlag,
-    ) = anvendtPerioder(beregningsGrunnlag.finnMuligeTrygdetidPerioder(trygdetider))
-
-    fun finnKonstantTrygdetidPerioder(
-        trygdetider: List<TrygdetidDto>,
-        beregningsGrunnlag: BeregningsGrunnlag,
-        fom: LocalDate,
-    ): List<AnvendtTrygdetid> {
-        if (trygdetider.size != 1) {
-            throw UgyldigForespoerselException(
-                code = "FEIL_ANTALL_TRYGDETIDER",
-                detail = "Fant flere trygdetider - men fikk bare en beregningsgrunnlag",
-            )
-        }
-
-        return trygdetider.first().toSamlet(beregningsGrunnlag.beregningsMetode.beregningsMetode)?.let {
-            anvendtPerioder(
-                listOf(
-                    GrunnlagMedPeriode(it, fom, null),
-                ),
-            ).first().data
-        } ?: throw TrygdetidIkkeOpprettet()
-    }
-
-    private fun anvendtPerioder(muligePerioder: List<GrunnlagMedPeriode<SamletTrygdetidMedBeregningsMetode>>) =
-        muligePerioder
-            .map {
-                anvendtTrygdetidRegel.eksekver(
-                    KonstantGrunnlag(
-                        TrygdetidGrunnlag(
-                            FaktumNode(it.data, kilde = "Trygdetid", beskrivelse = "Beregnet trygdetid for avdød"),
-                        ),
-                    ),
-                    RegelPeriode(it.fom, it.tom),
-                )
-            }.map {
-                when (it) {
-                    is RegelkjoeringResultat.Suksess -> {
-                        val aktueltResultat = it.periodiserteResultater.single()
-                        GrunnlagMedPeriode(
-                            data = aktueltResultat.resultat.verdi,
-                            fom = aktueltResultat.periode.fraDato,
-                            tom = aktueltResultat.periode.tilDato,
-                        )
-                    }
-
-                    is RegelkjoeringResultat.UgyldigPeriode -> throw InternfeilException(
-                        "Ugyldig regler for periode: ${it.ugyldigeReglerForPeriode}",
-                    )
-                }
-            }.kombinerOverlappendePerioder()
-
-    private fun BeregningsGrunnlag.finnMuligeTrygdetidPerioder(trygdetider: List<TrygdetidDto>) =
-        begegningsmetodeFlereAvdoede.map { beregningsmetodeForAvdoedPeriode ->
-            GrunnlagMedPeriode(
-                data =
-                    trygdetider
-                        .finnForAvdoed(
-                            beregningsmetodeForAvdoedPeriode.data.avdoed,
-                        ).toSamlet(
-                            beregningsmetodeForAvdoedPeriode.data.beregningsMetode.beregningsMetode,
-                        ) ?: throw InternfeilException("Kunne ikke samle trygdetid for avdoed").also {
-                        logger.warn("Kunne ikke samle trygdetid for avdoed - se sikkerlogg")
-                        sikkerlogger().warn("Kunne ikke samle trygdetid for avdoed ${beregningsmetodeForAvdoedPeriode.data.avdoed}")
-                    },
-                fom = beregningsmetodeForAvdoedPeriode.fom,
-                tom = beregningsmetodeForAvdoedPeriode.tom,
-            )
-        }
-
-    fun List<TrygdetidDto>.finnForAvdoed(avdoed: String) =
-        this.find { it.ident == avdoed }
-            ?: throw InternfeilException("Manglende trygdetid for avdoed").also {
-                logger.warn("Fant ikke trygdetid for avdoed - se sikkerlogg")
-                sikkerlogger().warn("Fant ikke trygdetid for avdoed $avdoed i $this")
-            }
 }

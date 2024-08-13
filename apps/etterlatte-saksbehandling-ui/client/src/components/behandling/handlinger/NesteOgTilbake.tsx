8ab3c49754aa20a56a7c5326316ad2b943fd2a44
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-08-13 15:55:23 +0200

    EY-4243 Vise tilbakeknapp på førstesiden også

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/handlinger/NesteOgTilbake.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/handlinger/NesteOgTilbake.tsx
@@ -2,18 +2,18 @@ import { Button, HStack, VStack } from '@navikt/ds-react'
 import { useBehandlingRoutes } from '../BehandlingRoutes'
 import AvbrytBehandling from './AvbrytBehandling'
 import { handlinger } from './typer'
+import { useNavigate } from 'react-router-dom'
 
 export const NesteOgTilbake = () => {
   const { next, back, lastPage, firstPage } = useBehandlingRoutes()
+  const navigate = useNavigate()
 
   return (
     <VStack gap="4">
       <HStack gap="4" justify="center">
-        {!firstPage && (
-          <Button variant="secondary" onClick={back}>
-            {handlinger.TILBAKE.navn}
-          </Button>
-        )}
+        <Button variant="secondary" onClick={() => (firstPage ? navigate(-1) : back())}>
+          {handlinger.TILBAKE.navn}
+        </Button>
         {!lastPage && (
           <Button variant="primary" onClick={next}>
             {handlinger.NESTE.navn}
--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/handlinger/Tilbake.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/handlinger/Tilbake.tsx
@@ -1,17 +1,15 @@
 import { Button } from '@navikt/ds-react'
 import { useBehandlingRoutes } from '../BehandlingRoutes'
 import { handlinger } from './typer'
+import { useNavigate } from 'react-router-dom'
 
 export const Tilbake = () => {
   const { back, firstPage } = useBehandlingRoutes()
+  const navigate = useNavigate()
 
   return (
-    <>
-      {!firstPage && (
-        <Button variant="secondary" onClick={back}>
-          {handlinger.TILBAKE.navn}
-        </Button>
-      )}
-    </>
+    <Button variant="secondary" onClick={() => (firstPage ? navigate(-1) : back())}>
+      {handlinger.TILBAKE.navn}
+    </Button>
   )
 }

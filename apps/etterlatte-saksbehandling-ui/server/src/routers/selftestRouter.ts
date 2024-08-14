34ba94beef85f830fe38cc9bc0fae6e1a461ccd1
Mads Opheim <mads.opheim@nav.no>
2024-08-14 11:26:03 +0200

    Nytt syntaks-forsøk

--- apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
@@ -7,8 +7,9 @@ export const selftestRouter = express.Router()
 
 selftestRouter.get('/', express.json(), async (req, res) => {
   const results: Promise<IPingResult>[] = Object.entries(ApiConfig).map(async ([serviceName, urlscope]) => {
-    const sti =
-      serviceName in ['grunnlag', 'utbetaling', 'brev'] ? `${urlscope.url}/isready` : `${urlscope.url}/health/isready`
+    const sti = ['grunnlag', 'utbetaling', 'brev'].some((n) => serviceName.includes(n))
+      ? `${urlscope.url}/isready`
+      : `${urlscope.url}/health/isready`
 
     const statuscode = await fetch(sti)
       .then((res) => res.status)

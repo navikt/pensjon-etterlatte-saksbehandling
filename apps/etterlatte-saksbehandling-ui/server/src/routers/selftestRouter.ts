412a5f572708fdcd6938f4eb4a9936572384feaf
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-08-14 10:34:03 +0200

    Tilpassar sti for sjølvtest (#5527)

--- apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/routers/selftestRouter.ts
@@ -7,7 +7,10 @@ export const selftestRouter = express.Router()
 
 selftestRouter.get('/', express.json(), async (req, res) => {
   const results: Promise<IPingResult>[] = Object.entries(ApiConfig).map(async ([serviceName, urlscope]) => {
-    const statuscode = await fetch(`${urlscope.url}/health/isready`)
+    const sti =
+      serviceName in ['grunnlag', 'utbetaling', 'brev'] ? `${urlscope.url}/isready` : `${urlscope.url}/health/isready`
+
+    const statuscode = await fetch(sti)
       .then((res) => res.status)
       .catch((err) => {
         logger.warn(`${serviceName} is down.`, err)

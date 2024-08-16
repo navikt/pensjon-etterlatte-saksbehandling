10c22f6cc8bab5c95b8fc11f4bff15b4b68f0feb
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-08-16 13:59:45 +0200

    EY-4310 Token mot kodeverk-api (#5549)
    * EY-4310 Token mot kodeverk-api
    
    * Ikke sett scope i clientid env
    
    * Update yaml, fjern cluster da det er samme
    
    * Fjern kommentar

--- apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/KodeverkKlient.kt
+++ apps/etterlatte-trygdetid/src/main/kotlin/trygdetid/klienter/KodeverkKlient.kt
@@ -1,33 +1,48 @@
 package no.nav.etterlatte.trygdetid.klienter
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.michaelbull.result.mapBoth
 import com.typesafe.config.Config
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.network.sockets.SocketTimeoutException
-import io.ktor.client.request.accept
 import io.ktor.client.request.get
-import io.ktor.http.ContentType
 import no.nav.etterlatte.libs.common.feilhaandtering.TimeoutForespoerselException
-import no.nav.etterlatte.libs.ktor.navConsumerId
+import no.nav.etterlatte.libs.common.objectMapper
+import no.nav.etterlatte.libs.ktor.Headers.NAV_CONSUMER_ID
+import no.nav.etterlatte.libs.ktor.ktor.ktorobo.AzureAdClient
+import no.nav.etterlatte.libs.ktor.ktor.ktorobo.DownstreamResourceClient
+import no.nav.etterlatte.libs.ktor.ktor.ktorobo.Resource
+import no.nav.etterlatte.libs.ktor.token.BrukerTokenInfo
 import org.slf4j.LoggerFactory
 
 class KodeverkKlient(
     config: Config,
-    private val httpKlient: HttpClient,
+    httpKlient: HttpClient,
 ) {
     private val logger = LoggerFactory.getLogger(KodeverkKlient::class.java)
     private val url = config.getString("kodeverk.resource.url")
+    private val clientId = config.getString("kodeverk.client.id")
+    private val applicationName = config.getString("applicationName")
+    private val azureAdClient = AzureAdClient(config)
+    private val downstreamResourceClient = DownstreamResourceClient(azureAdClient, httpKlient)
 
-    suspend fun hentLandkoder(): KodeverkResponse =
+    suspend fun hentLandkoder(brukerTokenInfo: BrukerTokenInfo): KodeverkResponse =
         try {
             logger.info("Henter alle landkoder fra Kodeverk")
-
-            httpKlient
-                .get("$url/Landkoder/koder/betydninger?ekskluderUgyldige=false&spraak=nb") {
-                    accept(ContentType.Application.Json)
-                    navConsumerId("etterlatte-trygdetid")
-                }.body()
+            downstreamResourceClient
+                .get(
+                    resource =
+                        Resource(
+                            clientId = clientId,
+                            url = "$url/Landkoder/koder/betydninger?ekskluderUgyldige=false&spraak=nb",
+                            additionalHeaders = mapOf(NAV_CONSUMER_ID to applicationName),
+                        ),
+                    brukerTokenInfo = brukerTokenInfo,
+                ).mapBoth(
+                    success = { resource -> resource.response.let { objectMapper.readValue(it.toString()) } },
+                    failure = { throwableErrorMessage -> throw throwableErrorMessage },
+                )
         } catch (e: SocketTimeoutException) {
             logger.warn("Timeout mot kodeverk ved henting av landkoder")
 

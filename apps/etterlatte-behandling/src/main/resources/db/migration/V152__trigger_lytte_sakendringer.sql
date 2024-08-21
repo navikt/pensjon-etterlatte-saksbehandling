d13036e05509cb92e4da00b378054cff3682050a
Mads Opheim <mads.opheim@nav.no>
2024-08-21 12:24:41 +0200

    EY-4268: Trigger for å lytte på oppdateringar av sak-tabellen

---
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V152__trigger_lytte_sakendringer.sql
@@ -0,0 +1,43 @@
+-- Basert på https://www.cybertec-postgresql.com/en/tracking-changes-in-postgresql/
+CREATE SCHEMA logging;
+
+CREATE TABLE logging.t_history
+(
+    id         serial,
+    timestamp  timestamp DEFAULT now(),
+    schemaname text,
+    tabname    text,
+    operation  text,
+    who        text      DEFAULT current_user,
+    new_val    jsonb,
+    old_val    jsonb
+);
+
+CREATE FUNCTION logging.change_trigger() RETURNS trigger AS
+$$
+BEGIN
+    IF TG_OP = 'INSERT'
+    THEN
+        INSERT INTO logging.t_history (tabname, schemaname, operation, new_val)
+        VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(NEW));
+        RETURN NEW;
+    ELSIF TG_OP = 'UPDATE'
+    THEN
+        INSERT INTO logging.t_history (tabname, schemaname, operation, new_val, old_val)
+        VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP,
+                row_to_json(NEW), row_to_json(OLD));
+        RETURN NEW;
+    ELSIF TG_OP = 'DELETE'
+    THEN
+        INSERT INTO logging.t_history (tabname, schemaname, operation, old_val)
+        VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(OLD));
+        RETURN OLD;
+    END IF;
+END;
+$$ LANGUAGE 'plpgsql' SECURITY DEFINER;
+
+CREATE TRIGGER audit_sak
+    AFTER INSERT OR UPDATE OR DELETE
+    ON sak
+    FOR EACH ROW
+EXECUTE PROCEDURE logging.change_trigger();

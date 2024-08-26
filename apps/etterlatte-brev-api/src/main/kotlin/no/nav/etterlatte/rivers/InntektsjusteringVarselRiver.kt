0e52de6a1e784b7f6c1b204b09000feba075f7b6
Andreas Balevik <andreasbalevik@gmail.no>
2024-08-26 10:31:40 +0200

    EY-4056 wip varsel inntektsjustering

---
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/rivers/InntektsjusteringVarselRiver.kt
@@ -0,0 +1,34 @@
+package no.nav.etterlatte.rivers
+
+import no.nav.etterlatte.libs.common.event.InntektsjusteringInnsendt
+import no.nav.etterlatte.rapidsandrivers.ListenerMedLogging
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+import rapidsandrivers.InntektsjusteringHendelseEvents
+import rapidsandrivers.InntektsjusteringHendelseType
+
+internal class InntektsjusteringVarselRiver(
+    rapidsConnection: RapidsConnection,
+) : ListenerMedLogging() {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    init {
+        initialiserRiver(rapidsConnection, InntektsjusteringHendelseType.SEND_VARSEL) {
+            validate { it.requireKey(InntektsjusteringHendelseEvents.inntektsaar) }
+        }
+    }
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        val inntektsaar = packet[InntektsjusteringInnsendt.inntektsaar].textValue()
+        logger.info("Starter varsel om inntektsjustering for $inntektsaar")
+
+        // TODO: hente saker som skal varsles
+
+        // TODO: sende varsel om inntektsjustering
+    }
+}
--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/InntektsjusteringHendelse.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/InntektsjusteringHendelse.kt
@@ -2,6 +2,10 @@ package rapidsandrivers
 
 import no.nav.etterlatte.libs.common.event.EventnameHendelseType
 
+object InntektsjusteringHendelseEvents {
+    val inntektsaar = "@inntektsaar"
+}
+
 enum class InntektsjusteringHendelseType : EventnameHendelseType {
     SEND_VARSEL,
     ;

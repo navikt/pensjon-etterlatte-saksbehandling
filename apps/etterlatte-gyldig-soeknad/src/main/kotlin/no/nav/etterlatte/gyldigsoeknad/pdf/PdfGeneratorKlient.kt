400a0122d0c8954fa56e638367bff0f02325d880
Mads Opheim <mads.opheim@nav.no>
2024-08-28 09:28:56 +0200

    Startar å gjera PdfGeneratorKlient-implementasjonane like

--- apps/etterlatte-gyldig-soeknad/src/main/kotlin/no/nav/etterlatte/gyldigsoeknad/pdf/PdfGeneratorKlient.kt
+++ apps/etterlatte-gyldig-soeknad/src/main/kotlin/no/nav/etterlatte/gyldigsoeknad/pdf/PdfGeneratorKlient.kt
@@ -11,19 +11,25 @@ import io.ktor.http.content.TextContent
 import io.ktor.http.contentType
 import no.nav.etterlatte.libs.common.logging.CORRELATION_ID
 import no.nav.etterlatte.libs.common.logging.getCorrelationId
+import org.slf4j.LoggerFactory
 
 class PdfGeneratorKlient(
-    private val client: HttpClient,
+    private val klient: HttpClient,
     private val apiUrl: String,
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     suspend fun genererPdf(
         payload: JsonNode,
         mal: String,
-    ): ByteArray =
-        client
+    ): ByteArray {
+        logger.info("Genererer PDF med ey-pdfgen (mal=$mal)")
+
+        return klient
             .post("$apiUrl/$mal") {
                 header(CORRELATION_ID, getCorrelationId())
                 contentType(ContentType.Application.Json)
                 setBody(TextContent(payload.toPrettyString(), ContentType.Application.Json))
             }.body()
+    }
 }

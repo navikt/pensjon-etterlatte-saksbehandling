e0ce31d3914f35d47906731ab37e63a0196e6feb
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-08-29 10:10:47 +0200

    Håndtere shutdown illegalstateexception

--- libs/saksbehandling-common/src/main/kotlin/ShutdownHook.kt
+++ libs/saksbehandling-common/src/main/kotlin/ShutdownHook.kt
@@ -3,14 +3,24 @@ package no.nav.etterlatte
 import java.util.Timer
 import java.util.concurrent.atomic.AtomicBoolean
 
+class ShutdownInProgressException(
+    detail: String,
+    cause: Throwable,
+) : Exception(detail, cause)
+
 val shuttingDown: AtomicBoolean = AtomicBoolean(false)
 
 fun addShutdownHook(timers: Collection<Timer>) = addShutdownHook(*timers.toTypedArray())
 
-fun addShutdownHook(vararg timer: Timer): Unit =
-    Runtime.getRuntime().addShutdownHook(
-        Thread {
-            shuttingDown.set(true)
-            timer.forEach { it.cancel() }
-        },
-    )
+fun addShutdownHook(vararg timer: Timer) {
+    try {
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                shuttingDown.set(true)
+                timer.forEach { it.cancel() }
+            },
+        )
+    } catch (e: IllegalStateException) {
+        throw ShutdownInProgressException("App er på vei ned allerede, kan ikke legge til shutdownhooks", e)
+    }
+}

37d97e660db5b8311c911110fcccfaaa56ded957
Emil Schrøder <emil.schroder@nav.no>
2024-08-29 10:56:43 +0200

    Oppdater fom for eksisterende unntak

---
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V153__populer_fom_paa_eksisterende_unntak.sql
@@ -0,0 +1,9 @@
+UPDATE aktivitetsplikt_unntak
+SET fom = subquery.opprettet_date,
+    beskrivelse = beskrivelse || ' - FOM ble automatisk satt av Gjenny -'
+FROM (
+         SELECT CAST(opprettet::jsonb ->>'tidspunkt' AS DATE) AS opprettet_date, id
+         FROM aktivitetsplikt_unntak
+         WHERE fom IS NULL
+     ) AS subquery
+WHERE aktivitetsplikt_unntak.id = subquery.id;

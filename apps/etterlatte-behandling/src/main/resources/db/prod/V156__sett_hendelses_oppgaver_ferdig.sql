6e7a080f9cbe69ab38013c600d42be4fc330c6d8
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-08-30 15:19:57 +0200

    Sett status ferdig, fikk feil status pga manglende sjekk som er rettet opp i nå (#5663)

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V156__sett_hendelses_oppgaver_ferdig.sql
@@ -0,0 +1,2 @@
+UPDATE oppgave SET status = 'FERDIGSTILT' where id = '838b4520-1ee2-47b1-bc95-21fb3dd1fa4b';
+UPDATE oppgave SET status = 'FERDIGSTILT' where id = '5dbc7ff8-182e-4955-885c-a5b08907fe8c';

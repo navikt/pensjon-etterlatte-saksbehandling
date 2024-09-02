503416b8943ecf1fe79276cf08e3b9b489aed0f3
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-09-02 13:56:02 +0200

    Tok ikke med hele uuiden i kopieringen så flyway tryna på ::uuid konverteringen

--- apps/etterlatte-behandling/src/main/resources/db/prod/V157__oppgaver_skal_vaere_ferdig.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V157__oppgaver_skal_vaere_ferdig.sql
@@ -1,4 +1,4 @@
-UPDATE oppgave SET status = 'FERDIGSTILT', saksbehandler = 'EY' where id = 'e9208b47-7085-47e0-8b27-420966ede83';
+UPDATE oppgave SET status = 'FERDIGSTILT', saksbehandler = 'EY' where id = 'e9208b47-7085-47e0-8b27-420966ede838';
 UPDATE oppgave SET status = 'FERDIGSTILT' where id = 'd9ac567d-1737-4a49-bcad-77dd42b21716';
 UPDATE oppgave SET status = 'FERDIGSTILT' where id = 'ccb27f55-f390-4a10-8e24-d1b6158dc2a2';
 UPDATE oppgave SET status = 'FERDIGSTILT' where id = '78fba7bd-3c01-478f-b736-61a99e25e3e2';

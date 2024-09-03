fb5e1698d073de0973057f8b055b1b8fad91cfe7
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2024-09-03 08:48:23 +0200

    Mer forklarende feilmelding hvis vi ikke finner vedlegg (#5685)

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/InnholdMedVedlegg.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/model/InnholdMedVedlegg.kt
@@ -1,16 +1,30 @@
 package no.nav.etterlatte.brev.model
 
 import no.nav.etterlatte.libs.common.feilhaandtering.InternfeilException
+import org.slf4j.LoggerFactory
 
 class ManglerPayloadForVedleggInnholdBrev(
     override val detail: String,
 ) : InternfeilException(detail)
 
+private val logger = LoggerFactory.getLogger(InnholdMedVedlegg::class.java)
+
 data class InnholdMedVedlegg(
     val innhold: () -> List<Slate.Element>,
     val innholdVedlegg: () -> List<BrevInnholdVedlegg>,
 ) {
-    fun finnVedlegg(key: BrevVedleggKey): List<Slate.Element> =
-        innholdVedlegg().find { vedlegg -> vedlegg.key == key }?.payload?.elements
-            ?: throw ManglerPayloadForVedleggInnholdBrev("Fant ikke vedlegg for brev av typen $key")
+    fun finnVedlegg(key: BrevVedleggKey): List<Slate.Element> {
+        val vedlegg = innholdVedlegg().find { vedlegg -> vedlegg.key == key }?.payload?.elements
+        if (vedlegg == null) {
+            logger.warn(
+                "Fant ikke vedlegg for brev av type=$key, lette " +
+                    "etter ${innholdVedlegg().joinToString { it.key.name }}",
+            )
+            throw ManglerPayloadForVedleggInnholdBrev(
+                "Fant ikke vedlegg som hører til brevet. Brevet må tilbakestilles for å " +
+                    "få med seg riktig vedlegg og tekst. Hvis det ikke går, meld sak i porten.",
+            )
+        }
+        return vedlegg
+    }
 }

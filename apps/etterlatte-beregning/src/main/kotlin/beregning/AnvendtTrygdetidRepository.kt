6b73bb1ee279df11c44e3aa40eb0a07b00376e67
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-09-05 10:08:09 +0200

    EY-4190 persister anvendt trygdetid-resultat (#5553)
    * EY-4190: BeregnBarnepensjonService-fila er så omfattande at det kjens knotete å jobbe der, så trekkjer heile trygdetidperioder-objektet ut. Ingen endringar her, utover å endre namnet på klassa til å indikere BP
    
    * Forenklar litt ved å inline og mappe
    
    * Innfører eit objekt for å kunne bruke seinare til å returnere meir
    
    * Tar med heile utrekninga i return-objektet
    
    * Returnerer samme return-objekt også for konstant grunnlag
    
    * Lagrar anvendt trygdetid-utrekninga
    
    * EY-4190: Lagrar ned anvendt trygdetid i databasen
    
    * Riktig språkbruk
    
    * Post-merge-namnefiks

---
+++ apps/etterlatte-beregning/src/main/kotlin/beregning/AnvendtTrygdetidRepository.kt
@@ -0,0 +1,30 @@
+package no.nav.etterlatte.beregning
+
+import no.nav.etterlatte.libs.common.toJson
+import no.nav.etterlatte.libs.database.oppdater
+import no.nav.etterlatte.libs.database.transaction
+import java.util.UUID
+import javax.sql.DataSource
+
+class AnvendtTrygdetidRepository(
+    private val dataSource: DataSource,
+) {
+    fun lagreAnvendtTrygdetid(
+        behandlingId: UUID,
+        utrekning: AnvendtTrygdetidPeriodeUtrekning,
+    ) = dataSource.transaction { tx ->
+        tx.oppdater(
+            query =
+                """insert into anvendt_trygdetid (behandling_id, foer_kombinering, etter_kombinering) 
+                    values(:behandling_id, :foer_kombinering::jsonb, :etter_kombinering::jsonb)
+                """.trimMargin(),
+            params =
+                mapOf(
+                    "behandling_id" to behandlingId.toString(),
+                    "foer_kombinering" to utrekning.utrekning.toJson(),
+                    "etter_kombinering" to utrekning.anvendt.toJson(),
+                ),
+            loggtekst = "Lagrer anvendt trygdetid-utrekninga",
+        )
+    }
+}

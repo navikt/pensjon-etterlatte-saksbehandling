7b867f649ea2d50e129e23c2a2a3b9a97a37af10
Nicolas N <nicolas.nordhagen@nav.no>
2024-09-10 10:02:36 +0200

    EY-4277 Nytt endepunkt for at EESSI kan hente Gjenny-sak

--- apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/samordning/sak/BehandlingSakRoutes.kt
+++ apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/samordning/sak/BehandlingSakRoutes.kt
@@ -1,16 +1,20 @@
 package no.nav.etterlatte.samordning.sak
 
 import com.typesafe.config.Config
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import no.nav.etterlatte.AuthorizationPlugin
 import no.nav.etterlatte.libs.common.isProd
 import no.nav.etterlatte.libs.ktor.route.FoedselsnummerDTO
+import no.nav.etterlatte.libs.ktor.route.SAKID_CALL_PARAMETER
+import no.nav.etterlatte.libs.ktor.route.sakId
 import no.nav.etterlatte.libs.ktor.token.Issuer
 
 fun generateRoles(config: Config): Set<String> {
@@ -38,6 +42,17 @@ fun Route.behandlingSakRoutes(
         post("/person/sak") {
             val fnrOgSaktype = call.receive<FoedselsnummerDTO>()
             call.respond(behandlingService.hentSakforPerson(fnrOgSaktype))
+        }
+    }
+
+    route("api/sak") {
+        install(AuthorizationPlugin) {
+            accessPolicyRolesEllerAdGrupper = setOf("les-bp-sak", "les-oms-sak")
+        }
+
+        get("/{$SAKID_CALL_PARAMETER}") {
+            val sak = behandlingService.hentSak(sakId)
+            call.respond(sak ?: HttpStatusCode.NoContent)
         }
     }
 }

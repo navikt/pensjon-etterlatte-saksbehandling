7517461e965bd5459536dbf2fdf03f4ea3cdf234
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-09-13 09:04:46 +0200

    EY-4356: Strukturere pdfgen-kall (#5741)
    * Ny libs med nytt PDF-interface
    
    * Trur det er lurt med ein pdfgen-pakke
    
    * Slår saman desse to metodane som var så godt som like
    
    * Innfører mal-interfacet
    
    * Innfører ei slags typing av mal-payloaden. Ikkje heilt sofistikert, men det er ein start
    
    * Innkapsling
    
    * Litt generalisering
    
    * Startar å gjera PdfGeneratorKlient-implementasjonane like
    
    * Startar å gjera PdfGeneratorKlient-implementasjonane like
    
    * Startar å gjera PdfGeneratorKlient-implementasjonane like
    
    * Tilpassar til å bruke PDFMal-interfacet også i gyldig-soeknad
    
    * Startar å gjera PdfGeneratorKlient-implementasjonane like
    
    * Tar i bruk også sti-variabelen

--- apps/etterlatte-gyldig-soeknad/src/main/kotlin/no/nav/etterlatte/gyldigsoeknad/pdf/PdfGeneratorKlient.kt
+++ apps/etterlatte-gyldig-soeknad/src/main/kotlin/no/nav/etterlatte/gyldigsoeknad/pdf/PdfGeneratorKlient.kt
@@ -1,6 +1,5 @@
 package no.nav.etterlatte.gyldigsoeknad.pdf
 
-import com.fasterxml.jackson.databind.JsonNode
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.header
@@ -8,27 +7,36 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.content.TextContent
-import org.slf4j.MDC
-import java.util.UUID
-
-interface PdfGenerator {
-    suspend fun genererPdf(
-        input: JsonNode,
-        template: String,
-    ): ByteArray
-}
+import io.ktor.http.contentType
+import no.nav.etterlatte.libs.common.innsendtsoeknad.common.PDFMal
+import no.nav.etterlatte.libs.common.logging.CORRELATION_ID
+import no.nav.etterlatte.libs.common.logging.getCorrelationId
+import no.nav.etterlatte.libs.common.toJsonNode
+import org.slf4j.LoggerFactory
 
 class PdfGeneratorKlient(
-    private val client: HttpClient,
+    private val klient: HttpClient,
     private val apiUrl: String,
-) : PdfGenerator {
-    override suspend fun genererPdf(
-        input: JsonNode,
-        template: String,
-    ): ByteArray =
-        client
-            .post("$apiUrl/$template") {
-                header("X-Correlation-ID", MDC.get("X-Correlation-ID") ?: UUID.randomUUID().toString())
-                setBody(TextContent(input.toPrettyString(), ContentType.Application.Json))
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    suspend fun genererPdf(
+        payload: PDFMal,
+        mal: String,
+    ) = genererPdf(TextContent(payload.toJsonNode().toPrettyString(), ContentType.Application.Json), mal, mal)
+
+    private suspend fun genererPdf(
+        body: Any,
+        mal: String,
+        sti: String,
+    ): ByteArray {
+        logger.info("Genererer PDF med ey-pdfgen (mal=$mal)")
+
+        return klient
+            .post("$apiUrl/$sti") {
+                header(CORRELATION_ID, getCorrelationId())
+                contentType(ContentType.Application.Json)
+                setBody(body)
             }.body()
+    }
 }

1ee2131a77881106a9636840182f0dbcc6dba472
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2024-09-16 12:05:48 +0200

    Begge disse kjøringene ligger som "STARTET" i basen, uten noen oppgaver (#5758)
    Jobben med å finne ut av hvorfor tas senere.

---
+++ apps/etterlatte-tidshendelser/src/main/resources/db/migration/V14__kjoerer_AO_BP_20_paa_nytt.sql
@@ -0,0 +1,8 @@
+-- Jobben for å kjøre aldersovergang BP20 år (forrige kjøring stuck)
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('AO_BP20', '2024-09-16', '2024-08');
+
+
+-- Jobben for å kjøre aldersovergang BP20 år (forrige kjøring stuck)
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('AO_BP20', '2024-09-16', '2024-09');

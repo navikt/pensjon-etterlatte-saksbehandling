6d5c102f410d32ccbf25842918341b3eaa6e8798
Trond Valen <trond.valen@nav.no>
2024-09-17 14:39:48 +0200

    EY-4400: Justerer layout.

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregningsgrunnlag/beregningsMetode/BeregningsMetodeBrukt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregningsgrunnlag/beregningsMetode/BeregningsMetodeBrukt.tsx
@@ -88,134 +88,136 @@ export const BeregningsMetodeBrukt = ({
           Trygdetid i beregning
         </Heading>
       </HStack>
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell />
-            <Table.HeaderCell scope="col">Forelder</Table.HeaderCell>
-            <Table.HeaderCell scope="col">Trygdetid i beregningen</Table.HeaderCell>
-            <Table.HeaderCell />
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.ExpandableRow
-            open={true}
-            expansionDisabled={true}
-            key="1"
-            content={
-              redigerTrydgetidMetodeBrukt ? (
-                <>
-                  <form onSubmit={handleSubmit(lagreBeregningsMetode)}>
-                    <VStack gap="4">
-                      {kunEnJuridiskForelder && (
-                        <ControlledMaanedVelger
-                          name="datoTilKunEnJuridiskForelder"
-                          label="Til og med dato for kun én juridisk forelder(Valgfritt)"
-                          description="Siste måneden med kun én juridisk forelder"
-                          control={control}
-                        />
-                      )}
-
-                      <ControlledRadioGruppe
-                        name="beregningsMetode"
-                        control={control}
-                        legend="Trygdetid i beregning"
-                        errorVedTomInput="Du må velge en metode"
-                        radios={
-                          <>
-                            <Radio value={BeregningsMetode.NASJONAL}>Nasjonal beregning (folketrygdberegning)</Radio>
-                            <Radio value={BeregningsMetode.PRORATA}>
-                              Prorata (EØS/avtaleland, der rettighet er oppfylt ved sammenlegging)
-                            </Radio>
-                            <Radio value={BeregningsMetode.BEST}>
-                              Den som gir høyest verdi av nasjonal/prorata (EØS/avtale-land, der rettighet er oppfylt
-                              etter nasjonale regler)
-                            </Radio>
-                          </>
-                        }
-                      />
-                      <Box width="15rem">
-                        <Textarea {...register('begrunnelse')} label="Begrunnelse (valgfritt)" />
-                      </Box>
-                      <HStack gap="4">
-                        <Button
-                          size="small"
-                          icon={<FloppydiskIcon aria-hidden />}
-                          loading={isPending(lagreBeregningsGrunnlagResult)}
-                        >
-                          Lagre
-                        </Button>
-                        <Button
-                          type="button"
-                          variant="secondary"
-                          size="small"
-                          icon={<XMarkIcon aria-hidden />}
-                          onClick={() => {
-                            reset()
-                            setRedigerTrygdetidMetodeBrukt(false)
-                          }}
-                        >
-                          Avbryt
-                        </Button>
-                      </HStack>
-                    </VStack>
-                  </form>
-                </>
-              ) : (
-                <>
-                  <Heading size="small" level="4">
-                    Begrunnelse
-                  </Heading>
-                  <BodyShort>{beregningsMetode?.begrunnelse || '-'}</BodyShort>
-                </>
-              )
-            }
-          >
-            <Table.DataCell>
-              {navn}{' '}
-              {kunEnJuridiskForelder && (
-                <Tag variant="alt1" size="small">
-                  {tagTekstForKunEnJuridiskForelder(behandling)}
-                </Tag>
-              )}
-            </Table.DataCell>
-            <Table.DataCell>
-              {beregningsMetode?.beregningsMetode
-                ? formaterEnumTilLesbarString(beregningsMetode.beregningsMetode)
-                : 'Metode er ikke satt'}
-            </Table.DataCell>
-            <Table.DataCell>
-              <HStack gap="4">
-                {redigerbar && (
+      <Box maxWidth="fit-content">
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell scope="col">Forelder</Table.HeaderCell>
+              <Table.HeaderCell scope="col">Trygdetid i beregningen</Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.ExpandableRow
+              open={true}
+              expansionDisabled={true}
+              key="1"
+              content={
+                redigerTrydgetidMetodeBrukt ? (
                   <>
-                    <Button
-                      type="button"
-                      variant="secondary"
-                      size="small"
-                      icon={<PencilIcon aria-hidden />}
-                      disabled={redigerTrydgetidMetodeBrukt}
-                      onClick={() => setRedigerTrygdetidMetodeBrukt(true)}
-                    >
-                      {beregningsMetode ? 'Rediger' : 'Legg til'}
-                    </Button>
+                    <form onSubmit={handleSubmit(lagreBeregningsMetode)}>
+                      <VStack gap="4">
+                        {kunEnJuridiskForelder && (
+                          <ControlledMaanedVelger
+                            name="datoTilKunEnJuridiskForelder"
+                            label="Til og med dato for kun én juridisk forelder(Valgfritt)"
+                            description="Siste måneden med kun én juridisk forelder"
+                            control={control}
+                          />
+                        )}
+
+                        <ControlledRadioGruppe
+                          name="beregningsMetode"
+                          control={control}
+                          legend="Trygdetid i beregning"
+                          errorVedTomInput="Du må velge en metode"
+                          radios={
+                            <>
+                              <Radio value={BeregningsMetode.NASJONAL}>Nasjonal beregning (folketrygdberegning)</Radio>
+                              <Radio value={BeregningsMetode.PRORATA}>
+                                Prorata (EØS/avtaleland, der rettighet er oppfylt ved sammenlegging)
+                              </Radio>
+                              <Radio value={BeregningsMetode.BEST}>
+                                Den som gir høyest verdi av nasjonal/prorata (EØS/avtale-land, der rettighet er oppfylt
+                                etter nasjonale regler)
+                              </Radio>
+                            </>
+                          }
+                        />
+                        <Box width="15rem">
+                          <Textarea {...register('begrunnelse')} label="Begrunnelse (valgfritt)" />
+                        </Box>
+                        <HStack gap="4">
+                          <Button
+                            size="small"
+                            icon={<FloppydiskIcon aria-hidden />}
+                            loading={isPending(lagreBeregningsGrunnlagResult)}
+                          >
+                            Lagre
+                          </Button>
+                          <Button
+                            type="button"
+                            variant="secondary"
+                            size="small"
+                            icon={<XMarkIcon aria-hidden />}
+                            onClick={() => {
+                              reset()
+                              setRedigerTrygdetidMetodeBrukt(false)
+                            }}
+                          >
+                            Avbryt
+                          </Button>
+                        </HStack>
+                      </VStack>
+                    </form>
                   </>
+                ) : (
+                  <>
+                    <Heading size="small" level="4">
+                      Begrunnelse
+                    </Heading>
+                    <BodyShort>{beregningsMetode?.begrunnelse || '-'}</BodyShort>
+                  </>
+                )
+              }
+            >
+              <Table.DataCell>
+                {navn}{' '}
+                {kunEnJuridiskForelder && (
+                  <Tag variant="alt1" size="small">
+                    {tagTekstForKunEnJuridiskForelder(behandling)}
+                  </Tag>
                 )}
-                {redigerbar && beregningsMetode && (
-                  <Button
-                    size="small"
-                    variant="secondary"
-                    icon={<TrashIcon aria-hidden />}
-                    loading={isPending(lagreBeregningsGrunnlagResult)}
-                    onClick={slettBeregningsMetode}
-                  >
-                    Slett
-                  </Button>
-                )}
-              </HStack>
-            </Table.DataCell>
-          </Table.ExpandableRow>
-        </Table.Body>
-      </Table>
+              </Table.DataCell>
+              <Table.DataCell>
+                {beregningsMetode?.beregningsMetode
+                  ? formaterEnumTilLesbarString(beregningsMetode.beregningsMetode)
+                  : 'Metode er ikke satt'}
+              </Table.DataCell>
+              <Table.DataCell>
+                <HStack gap="4">
+                  {redigerbar && (
+                    <>
+                      <Button
+                        type="button"
+                        variant="secondary"
+                        size="small"
+                        icon={<PencilIcon aria-hidden />}
+                        disabled={redigerTrydgetidMetodeBrukt}
+                        onClick={() => setRedigerTrygdetidMetodeBrukt(true)}
+                      >
+                        {beregningsMetode ? 'Rediger' : 'Legg til'}
+                      </Button>
+                    </>
+                  )}
+                  {redigerbar && beregningsMetode && (
+                    <Button
+                      size="small"
+                      variant="secondary"
+                      icon={<TrashIcon aria-hidden />}
+                      loading={isPending(lagreBeregningsGrunnlagResult)}
+                      onClick={slettBeregningsMetode}
+                    >
+                      Slett
+                    </Button>
+                  )}
+                </HStack>
+              </Table.DataCell>
+            </Table.ExpandableRow>
+          </Table.Body>
+        </Table>
+      </Box>
     </VStack>
   )
 }

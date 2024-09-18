b3e47365a9b99c43f25ce6d64b46d1d50c582bc6
Trond Valen <trond.valen@nav.no>
2024-09-18 15:13:13 +0200

    EY-4430: Begrenser valgbare virkningstidspunkt til å være etter det første dødsfallet. Før ble det brukt et vilkårlig dødsfall.

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/virkningstidspunkt/Virkningstidspunkt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/virkningstidspunkt/Virkningstidspunkt.tsx
@@ -42,7 +42,7 @@ const Virkningstidspunkt = (props: {
   erBosattUtland: boolean
 }) => {
   const { behandling, erBosattUtland } = props
-  const avdoede = usePersonopplysninger()?.avdoede.find((po) => po)
+
   const dispatch = useAppDispatch()
   const [fastsettVirkStatus, fastsettVirkningstidspunktRequest, resetToInitial] = useApiCall(fastsettVirkningstidspunkt)
 
@@ -68,12 +68,22 @@ const Virkningstidspunkt = (props: {
     // Denne siste fallbacken er altså tenkt for disse sakene
   }
 
+  function foersteDoedsdato() {
+    const avdoede = usePersonopplysninger()?.avdoede
+    const foersteAvdoede =
+      avdoede &&
+      avdoede.reduce((prev, current) =>
+        current.opplysning.doedsdato && prev.opplysning.doedsdato
+          ? current.opplysning.doedsdato < prev.opplysning.doedsdato
+            ? current
+            : prev
+          : prev
+      )
+    return foersteAvdoede?.opplysning.doedsdato
+  }
+
   const { monthpickerProps, inputProps } = useMonthpicker({
-    fromDate: hentMinimumsVirkningstidspunkt(
-      avdoede?.opplysning?.doedsdato,
-      getSoeknadMottattDato(),
-      behandling.sakType
-    ),
+    fromDate: hentMinimumsVirkningstidspunkt(foersteDoedsdato(), getSoeknadMottattDato(), behandling.sakType),
     toDate: addMonths(new Date(), 4),
     onMonthChange: (date: Date) => setVirkningstidspunkt(date),
     inputFormat: 'dd.MM.yyyy',

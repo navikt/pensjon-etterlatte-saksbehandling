2c4d2bb03d354124e9e0b652161e01ebbcb63b7f
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-09-19 14:01:21 +0200

    Bruke public schema for vilkårsvurdering igjen

---
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V162__vilkaarsvurdering_public_schema.sql
@@ -0,0 +1,18 @@
+ALTER TABLE vilkaarsvurdering.vilkaarsvurdering
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.delvilkaar
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.migrert_yrkesskade
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.vilkaar
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.vilkaarsvurdering_kilde
+    SET SCHEMA public;
+
+DROP EXTENSION IF EXISTS "vilkaarsvurdering.uuid-ossp";
+
+DROP SCHEMA vilkaarsvurdering CASCADE;
---
+++ apps/etterlatte-vilkaarsvurdering/src/main/resources/db/migration/V30__bruke_public.sql
@@ -0,0 +1,14 @@
+ALTER TABLE vilkaarsvurdering.vilkaarsvurdering
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.delvilkaar
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.migrert_yrkesskade
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.vilkaar
+    SET SCHEMA public;
+
+ALTER TABLE vilkaarsvurdering.vilkaarsvurdering_kilde
+    SET SCHEMA public;

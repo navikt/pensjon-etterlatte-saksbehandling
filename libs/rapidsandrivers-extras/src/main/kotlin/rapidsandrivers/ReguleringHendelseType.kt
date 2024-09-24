1189d72f9118333b700dee97ac133593143a27b0
Bjørn Golberg <bjorn.golberg@nav.no>
2024-09-24 10:34:17 +0200

    Enum for MasseOmregning og doc

--- libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/ReguleringHendelseType.kt
+++ libs/rapidsandrivers-extras/src/main/kotlin/rapidsandrivers/ReguleringHendelseType.kt
@@ -2,6 +2,12 @@ package no.nav.etterlatte.rapidsandrivers
 
 import no.nav.etterlatte.libs.common.event.EventnameHendelseType
 
+/*
+* En hendelseflyt som automatisk revurderer en sak.
+* Flyt benyttes fra flere hold blant annet Regulering, Inntektsjsutering, etc.
+*
+* TODO omregning med vedtaksbrev støttes ikke enda
+*/
 enum class OmregningHendelseType : EventnameHendelseType {
     KLAR_FOR_OMREGNING,
     BEHANDLING_OPPRETTA,
@@ -22,4 +28,16 @@ enum class ReguleringHendelseType : EventnameHendelseType {
     ;
 
     override fun lagEventnameForType(): String = "REGULERING:${this.name}"
+}
+
+/*
+* TODO
+* Ubrukt enn så lenge men tanken er at dette vil bli starten på en flyt
+* til en jobb hvor flere saker gjennomfører omregning
+*/
+enum class MasseOmregningHendelseType : EventnameHendelseType {
+    START_MASSE_OMREGNING,
+    ;
+
+    override fun lagEventnameForType(): String = "MASSE_OMREGNING:${this.name}"
 }

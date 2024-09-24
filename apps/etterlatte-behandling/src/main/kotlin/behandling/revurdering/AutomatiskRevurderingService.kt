a4d9191407351bc53d34d671b48908afc8dfd159
Bjørn Golberg <bjorn.golberg@nav.no>
2024-09-24 09:56:18 +0200

    Flytter oppretting av revurdering vekk fra omregning (#5822)
    * Endepunkt og service for å opprette revurdering og oppfølging med retries
    
    * Rename og flytte klasser
    
    rename
    
    * Fjerner omregning endepunkt som oppretter revurdering
    
    * Fjerner ubrukte felter fra Omregninghendelse

--- apps/etterlatte-behandling/src/main/kotlin/behandling/revurdering/AutomatiskRevurderingService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/revurdering/AutomatiskRevurderingService.kt
@@ -1,19 +1,66 @@
 package no.nav.etterlatte.behandling.revurdering
 
+import no.nav.etterlatte.behandling.BehandlingService
+import no.nav.etterlatte.behandling.GrunnlagServiceImpl
 import no.nav.etterlatte.behandling.domain.Behandling
+import no.nav.etterlatte.inTransaction
 import no.nav.etterlatte.libs.common.Vedtaksloesning
 import no.nav.etterlatte.libs.common.behandling.Persongalleri
 import no.nav.etterlatte.libs.common.behandling.Prosesstype
 import no.nav.etterlatte.libs.common.behandling.Revurderingaarsak
 import no.nav.etterlatte.libs.common.behandling.tilVirkningstidspunkt
+import no.nav.etterlatte.libs.common.retryOgPakkUt
+import no.nav.etterlatte.libs.common.revurdering.AutomatiskRevurderingRequest
+import no.nav.etterlatte.libs.common.revurdering.AutomatiskRevurderingResponse
 import no.nav.etterlatte.libs.common.sak.SakId
 import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.ktor.token.Fagsaksystem
 import java.time.LocalDate
+import java.time.LocalTime
 
 class AutomatiskRevurderingService(
     private val revurderingService: RevurderingService,
+    private val behandlingService: BehandlingService,
+    private val grunnlagService: GrunnlagServiceImpl,
 ) {
+    suspend fun oppprettRevurderingOgOppfoelging(request: AutomatiskRevurderingRequest): AutomatiskRevurderingResponse {
+        validerSakensTilstand(request.sakId, request.revurderingAarsak)
+
+        val forrigeBehandling =
+            inTransaction {
+                behandlingService.hentSisteIverksatte(request.sakId)
+                    ?: throw IllegalArgumentException("Fant ikke forrige behandling i sak ${request.sakId}")
+            }
+        val persongalleri = grunnlagService.hentPersongalleri(forrigeBehandling.id)
+
+        val revurderingOgOppfoelging =
+            inTransaction {
+                opprettAutomatiskRevurdering(
+                    sakId = request.sakId,
+                    forrigeBehandling = forrigeBehandling,
+                    revurderingAarsak = request.revurderingAarsak,
+                    virkningstidspunkt = request.fraDato,
+                    kilde = Vedtaksloesning.GJENNY,
+                    persongalleri = persongalleri,
+                    frist = request.oppgavefrist?.let { Tidspunkt.ofNorskTidssone(it, LocalTime.NOON) },
+                )
+            }
+
+        retryOgPakkUt { revurderingOgOppfoelging.leggInnGrunnlag() }
+        retryOgPakkUt {
+            inTransaction {
+                revurderingOgOppfoelging.opprettOgTildelOppgave()
+            }
+        }
+        retryOgPakkUt { revurderingOgOppfoelging.sendMeldingForHendelse() }
+
+        return AutomatiskRevurderingResponse(
+            behandlingId = revurderingOgOppfoelging.behandlingId(),
+            forrigeBehandlingId = forrigeBehandling.id,
+            sakType = revurderingOgOppfoelging.sakType(),
+        )
+    }
+
     fun opprettAutomatiskRevurdering(
         sakId: SakId,
         forrigeBehandling: Behandling,
--- apps/etterlatte-behandling/src/test/kotlin/behandling/BehandlingFactoryTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/BehandlingFactoryTest.kt
@@ -112,6 +112,8 @@ class BehandlingFactoryTest {
                 aktivitetspliktDao,
                 aktivitetspliktKopierService,
             ),
+            mockk(),
+            mockk(),
         )
     private val behandlingFactory =
         BehandlingFactory(
@@ -504,9 +506,19 @@ class BehandlingFactoryTest {
             )
 
         every { sakServiceMock.finnSak(sak.id) } returns sak
-        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns listOf(avslaattFoerstegangsbehandling, revurdering)
+        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns
+            listOf(
+                avslaattFoerstegangsbehandling,
+                revurdering,
+            )
 
-        assertThrows<AvslagOmgjoering.HarAapenBehandling> { behandlingFactory.opprettOmgjoeringAvslag(sak.id, saksbehandler, false) }
+        assertThrows<AvslagOmgjoering.HarAapenBehandling> {
+            behandlingFactory.opprettOmgjoeringAvslag(
+                sak.id,
+                saksbehandler,
+                false,
+            )
+        }
 
         verify {
             sakServiceMock.finnSak(sak.id)
@@ -527,7 +539,11 @@ class BehandlingFactoryTest {
             )
 
         every { sakServiceMock.finnSak(sak.id) } returns sak
-        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns listOf(avslaattFoerstegangsbehandling, revurdering)
+        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns
+            listOf(
+                avslaattFoerstegangsbehandling,
+                revurdering,
+            )
         every { behandlingDaoMock.hentBehandling(avslaattFoerstegangsbehandling.id) } returns avslaattFoerstegangsbehandling
         every { behandlingDaoMock.hentBehandling(any()) } returns foerstegangsbehandling(sak = sak)
         every { behandlingDaoMock.lagreNyttVirkningstidspunkt(any(), any()) } returns 1
@@ -547,7 +563,14 @@ class BehandlingFactoryTest {
         every { behandlingDaoMock.opprettBehandling(capture(opprettBehandlingSlot)) } just runs
         coEvery { grunnlagService.hentPersongalleri(avslaattFoerstegangsbehandling.id) } returns Persongalleri(sak.ident)
         coEvery { grunnlagService.leggInnNyttGrunnlag(any(), any(), any()) } just runs
-        every { oppgaveService.opprettFoerstegangsbehandlingsOppgaveForInnsendtSoeknad(any(), any(), any(), any()) } returns
+        every {
+            oppgaveService.opprettFoerstegangsbehandlingsOppgaveForInnsendtSoeknad(
+                any(),
+                any(),
+                any(),
+                any(),
+            )
+        } returns
             OppgaveIntern(
                 id = UUID.randomUUID(),
                 status = Status.PAA_VENT,
@@ -605,7 +628,11 @@ class BehandlingFactoryTest {
             )
 
         every { sakServiceMock.finnSak(sak.id) } returns sak
-        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns listOf(avslaattFoerstegangsbehandling, revurdering)
+        every { behandlingDaoMock.hentBehandlingerForSak(sak.id) } returns
+            listOf(
+                avslaattFoerstegangsbehandling,
+                revurdering,
+            )
         every { behandlingDaoMock.hentBehandling(avslaattFoerstegangsbehandling.id) } returns avslaattFoerstegangsbehandling
         every { behandlingDaoMock.hentBehandling(any()) } returns foerstegangsbehandling(sak = sak)
 
@@ -613,7 +640,14 @@ class BehandlingFactoryTest {
         every { behandlingDaoMock.opprettBehandling(capture(opprettBehandlingSlot)) } just runs
         coEvery { grunnlagService.hentPersongalleri(avslaattFoerstegangsbehandling.id) } returns Persongalleri(sak.ident)
         coEvery { grunnlagService.leggInnNyttGrunnlag(any(), any(), any()) } just runs
-        every { oppgaveService.opprettFoerstegangsbehandlingsOppgaveForInnsendtSoeknad(any(), any(), any(), any()) } returns
+        every {
+            oppgaveService.opprettFoerstegangsbehandlingsOppgaveForInnsendtSoeknad(
+                any(),
+                any(),
+                any(),
+                any(),
+            )
+        } returns
             OppgaveIntern(
                 id = UUID.randomUUID(),
                 status = Status.PAA_VENT,

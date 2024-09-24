ffdb31867d0b05786c8d311f562a3ae2958237d4
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-09-24 14:34:17 +0200

    Forbedringer i flyway test (#5817)
    * Forbedringer i flyway test
    
    * Navneforbedringer
    
    * Splitt ut validering av flyway filer
    
    * Naming
    
    * Testblemme og testnavnendring

--- libs/etterlatte-database/src/test/kotlin/DataSourceValidatorTest.kt
+++ libs/etterlatte-database/src/test/kotlin/DataSourceValidatorTest.kt
@@ -1,9 +1,11 @@
 import no.nav.etterlatte.libs.database.InvalidMigrationScriptVersion
+import no.nav.etterlatte.libs.database.ManglerDobbelUnderscore
+import no.nav.etterlatte.libs.database.SqlMaaHaaStorforbokstav
 import no.nav.etterlatte.libs.database.validateMigrationScriptVersions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class DataSourceBuilderTest {
+class DataSourceValidatorTest {
     @Test
     fun `Skal migrere unike versjoner i migrering og prod`() {
         val migrationScripts = listOf("V1__numerouno.sql", "V3__numerotres.sql")
@@ -18,6 +20,24 @@ class DataSourceBuilderTest {
 
         assertThrows<InvalidMigrationScriptVersion> {
             validateMigrationScriptVersions(listOf(migrationScripts, migrationScriptsProd).flatten())
+        }
+    }
+
+    @Test
+    fun `Må ha dobbel underscore i migreringsfil`() {
+        val migrationScripts = listOf("V1_numerouno.sql", "V2__endreoppgavetabell.sql")
+
+        assertThrows<ManglerDobbelUnderscore> {
+            validateMigrationScriptVersions(listOf(migrationScripts).flatten())
+        }
+    }
+
+    @Test
+    fun `Må ha stor forbokstav i migreringsfil`() {
+        val migrationScripts = listOf("v1__numerouno.sql", "V2__endreoppgavetabell.sql")
+
+        assertThrows<SqlMaaHaaStorforbokstav> {
+            validateMigrationScriptVersions(listOf(migrationScripts).flatten())
         }
     }
 }

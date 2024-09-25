a0f8cedf8cf9f49bbfa3afc9e50f469313cf0ec2
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-09-25 15:49:08 +0200

    Flytte metode for å hente status i egen mappe

--- apps/etterlatte-saksbehandling-ui/client/src/shared/tags/AktivitetspliktStatusTag.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/tags/AktivitetspliktStatusTag.tsx
@@ -1,67 +1,14 @@
 import React, { ReactNode } from 'react'
-import {
-  AktivitetspliktUnntakType,
-  AktivitetspliktVurderingType,
-  IAktivitetspliktUnntak,
-  IAktivitetspliktVurderingNy,
-} from '~shared/types/Aktivitetsplikt'
+import { IAktivitetspliktVurderingNy } from '~shared/types/Aktivitetsplikt'
 import { Tag } from '@navikt/ds-react'
 import { formaterDatoMedFallback } from '~utils/formatering/dato'
-
-enum AktivitetspliktStatus {
-  OPPFYLT,
-  IKKE_OPPFYLT,
-  UNNTAK,
-  VARIG_UNNTAK,
-  IKKE_VURDERT,
-}
+import { AktivitetspliktStatus, finnStatusPaaAktivitetsplikt } from '~components/person/aktivitet/utils'
 
 export const AktivitetspliktStatusTag = ({
   aktivitetspliktVurdering,
 }: {
   aktivitetspliktVurdering: IAktivitetspliktVurderingNy
 }): ReactNode => {
-  const finnStatusPaaAktivitetsplikt = (
-    aktivitetspliktVurdering: IAktivitetspliktVurderingNy
-  ): { aktivitetspliktStatus: AktivitetspliktStatus; dato?: Date | string } => {
-    if (!!aktivitetspliktVurdering.unntak?.length) {
-      const gjeldendeUnntak: IAktivitetspliktUnntak = [...aktivitetspliktVurdering.unntak].reduce((a, b) => {
-        return new Date(a.fom) > new Date(b.fom) ? a : b
-      })
-
-      if (gjeldendeUnntak.unntak === AktivitetspliktUnntakType.FOEDT_1963_ELLER_TIDLIGERE_OG_LAV_INNTEKT) {
-        return {
-          aktivitetspliktStatus: AktivitetspliktStatus.VARIG_UNNTAK,
-        }
-      } else {
-        return {
-          aktivitetspliktStatus: AktivitetspliktStatus.UNNTAK,
-          dato: gjeldendeUnntak?.tom,
-        }
-      }
-    } else if (!!aktivitetspliktVurdering.aktivitet?.length) {
-      const gjeldendeAktivitet = [...aktivitetspliktVurdering.aktivitet].reduce((a, b) => {
-        return new Date(a.fom) > new Date(b.fom) ? a : b
-      })
-
-      if (gjeldendeAktivitet?.aktivitetsgrad === AktivitetspliktVurderingType.AKTIVITET_UNDER_50) {
-        return {
-          aktivitetspliktStatus: AktivitetspliktStatus.IKKE_OPPFYLT,
-          dato: gjeldendeAktivitet.fom,
-        }
-      } else {
-        return {
-          aktivitetspliktStatus: AktivitetspliktStatus.OPPFYLT,
-          dato: gjeldendeAktivitet?.fom,
-        }
-      }
-    }
-
-    return {
-      aktivitetspliktStatus: AktivitetspliktStatus.IKKE_VURDERT,
-    }
-  }
-
   const statusPaaAktivitetsplikt = finnStatusPaaAktivitetsplikt(aktivitetspliktVurdering)
 
   switch (statusPaaAktivitetsplikt.aktivitetspliktStatus) {

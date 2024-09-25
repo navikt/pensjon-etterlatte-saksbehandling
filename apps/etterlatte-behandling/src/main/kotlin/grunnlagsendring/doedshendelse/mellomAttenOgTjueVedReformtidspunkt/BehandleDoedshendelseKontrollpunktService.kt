5bce4e4f5f4d1e4203812ca242bb401b9b668b3f
Henrik Gundersen <henrik.gundersen@nav.no>
2024-09-25 15:42:49 +0200

    EY-4478: Fjerner sjekk av utvandring for brev mellom 18 og 20 år

--- apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/mellomAttenOgTjueVedReformtidspunkt/BehandleDoedshendelseKontrollpunktService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/grunnlagsendring/doedshendelse/mellomAttenOgTjueVedReformtidspunkt/BehandleDoedshendelseKontrollpunktService.kt
@@ -29,8 +29,13 @@ class BehandleDoedshendelseKontrollpunktService(
         return if (avdoed.doedsdato == null) {
             listOf(DoedshendelseKontrollpunkt.AvdoedLeverIPDL)
         } else {
-            val barnKontrollpunkter = kontrollpunktBarnService.identifiser(hendelse, avdoed, sak, barn, false)
-            val avdoedKontrollpunkter = kontrollpunktAvdoedService.identifiser(avdoed, false)
+            val barnKontrollpunkter = kontrollpunktBarnService.identifiser(hendelse, avdoed, sak, barn, kontrollerSamtidigDoedsfall = false)
+            val avdoedKontrollpunkter =
+                kontrollpunktAvdoedService.identifiser(
+                    avdoed,
+                    kontrollerDNummer = false,
+                    kontrollerUtvandring = false,
+                )
             val fellesKontrollpunkter = fellesKontrollpunkter(barn)
 
             barnKontrollpunkter + avdoedKontrollpunkter + fellesKontrollpunkter

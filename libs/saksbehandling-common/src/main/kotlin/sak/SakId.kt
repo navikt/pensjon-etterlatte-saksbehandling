70034ebc0058a0c54a06a6c632cd24e6e7100856
Mads Opheim <mads.opheim@nav.no>
2024-09-25 15:20:24 +0200

    Går over til inline value class, som er meir lettvekts og meir enn nok her

--- libs/saksbehandling-common/src/main/kotlin/sak/SakId.kt
+++ libs/saksbehandling-common/src/main/kotlin/sak/SakId.kt
@@ -3,7 +3,8 @@ package no.nav.etterlatte.libs.common.sak
 import com.fasterxml.jackson.annotation.JsonValue
 import java.io.Serializable
 
-data class SakId(
+@JvmInline
+value class SakId(
     @JsonValue val sakId: Long,
 ) : Serializable {
     override fun toString() = sakId.toString()

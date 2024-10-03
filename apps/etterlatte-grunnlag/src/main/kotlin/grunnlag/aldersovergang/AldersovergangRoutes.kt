6fda7715741691863d9eb95945bddedf2dbfd157
Bjørn Golberg <bjorn.golberg@nav.no>
2024-10-03 09:18:42 +0200

    Endepunkt i grunnlag for å hente måned for aldersoverang

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/aldersovergang/AldersovergangRoutes.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/aldersovergang/AldersovergangRoutes.kt
@@ -1,10 +1,13 @@
 package no.nav.etterlatte.grunnlag.aldersovergang
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.feilhaandtering.UgyldigForespoerselException
 import java.time.YearMonth
 
 fun Route.aldersovergangRoutes(aldersovergangService: AldersovergangService) {
@@ -12,6 +15,21 @@ fun Route.aldersovergangRoutes(aldersovergangService: AldersovergangService) {
         get("{yearMonth}") {
             val yearMonth = call.parameters["yearMonth"].toString().let { YearMonth.parse(it) }
             call.respond(aldersovergangService.hentSoekereFoedtIEnGittMaaned(yearMonth))
+        }
+        get("sak/{sakId}/{sakType}") {
+            val sakId =
+                call.parameters["sakId"]?.toLong() ?: throw UgyldigForespoerselException(
+                    "MANGLER_SAKID",
+                    "Mangler sakId",
+                )
+            val sakType =
+                call.parameters["sakType"]?.let { SakType.valueOf(it) }
+                    ?: throw UgyldigForespoerselException("MANGLER_SAKTYPE", "Mangler sakType")
+
+            when (val maaned = aldersovergangService.aldersovergangMaaned(sakId, sakType)) {
+                null -> call.respond(HttpStatusCode.NoContent)
+                else -> call.respond(maaned)
+            }
         }
     }
 
--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/aldersovergang/AldersovergangService.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/aldersovergang/AldersovergangService.kt
@@ -1,5 +1,6 @@
 package no.nav.etterlatte.grunnlag.aldersovergang
 
+import no.nav.etterlatte.libs.common.behandling.SakType
 import no.nav.etterlatte.libs.common.grunnlag.opplysningstyper.Opplysningstype
 import no.nav.etterlatte.libs.common.person.PersonRolle
 import no.nav.etterlatte.libs.common.sak.SakId
@@ -9,6 +10,17 @@ import java.time.YearMonth
 class AldersovergangService(
     private val dao: AldersovergangDao,
 ) {
+    fun aldersovergangMaaned(
+        sakId: SakId,
+        sakType: SakType,
+    ): YearMonth? {
+        val foedselsdato = dao.hentFoedselsdato(sakId, Opplysningstype.SOEKER_PDL_V1)
+        return when (sakType) {
+            SakType.BARNEPENSJON -> TODO()
+            SakType.OMSTILLINGSSTOENAD -> foedselsdato?.let { YearMonth.from(it.plusYears(67)) }
+        }
+    }
+
     fun hentSoekereFoedtIEnGittMaaned(maaned: YearMonth) = dao.hentSoekereFoedtIEnGittMaaned(maaned)
 
     fun hentSakerHvorDoedsfallForekomIGittMaaned(behandlingsmaaned: YearMonth) =
@@ -20,7 +32,7 @@ class AldersovergangService(
         paaDato: LocalDate,
     ): Alder? =
         when (rolle) {
-            PersonRolle.BARN -> dao.hentAlder(sakId, Opplysningstype.SOEKER_PDL_V1, paaDato)
+            PersonRolle.BARN -> dao.hentFoedselsdato(sakId, Opplysningstype.SOEKER_PDL_V1)?.until(paaDato)?.years
             else -> throw NotImplementedError("Ikke støtta å finne alder per nå for rolle $rolle")
         }
 }

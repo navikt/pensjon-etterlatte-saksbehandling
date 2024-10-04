bb16b9c4470f881fac3da973c8c6a94bdc7f97c9
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-10-04 11:48:46 +0200

    Fikser tvetydig migrering

--- apps/etterlatte-statistikk/src/main/resources/db/migration/V52__rydd_saksstatistikk.sql
+++ apps/etterlatte-statistikk/src/main/resources/db/migration/V52__rydd_saksstatistikk.sql
@@ -6,13 +6,13 @@ where behandling_status = 'AVBRUTT'
   and behandling_type = 'TILBAKEKREVING';
 
 -- Fiks registrert tidspunkt klage
-update sak
+update sak s
 set registrert_tid = foerste_paa_klage.rtid
 from (select min(registrert_tid) as rtid, behandling_id
       from sak
       where behandling_type = 'KLAGE'
       group by behandling_id) as foerste_paa_klage
-where behandling_id = foerste_paa_klage.behandling_id;
+where s.behandling_id = foerste_paa_klage.behandling_id;
 
 -- Fiks status klager vi har sendt til kabal
 -- Ønske om en annen mapping for de som vi sender til kabal
@@ -25,13 +25,13 @@ where behandling_type = 'KLAGE'
 -- fix forsvinnende utlandstilknytning revurdering
 -- manglende oppdatering i påfølgende meldinger historisk, feilen er fikset men denne patcher
 -- gamle saker som ligger i databasen
-update sak
+update sak s
 set sak_utland = fasit.sak_utland
 from (select distinct on (behandling_id) behandling_id, sak_utland from sak
     where sak_utland is not null order by behandling_id, teknisk_tid desc
 ) as fasit
-where behandling_id = fasit.behandling_id
-and sak_utland is null;
+where s.behandling_id = fasit.behandling_id
+and s.sak_utland is null;
 
 -- Fjerner en duplisert hendelse på grunn av en bug i ferdigstilling
 delete from sak where id = 144748;

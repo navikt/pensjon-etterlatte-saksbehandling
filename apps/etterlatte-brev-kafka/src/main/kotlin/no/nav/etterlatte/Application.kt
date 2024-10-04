5f2d7b253f7f687d69ab76f1eef73c7b83cb3de4
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-10-04 16:43:10 +0200

    Ikke aktiver rivers

--- apps/etterlatte-brev-kafka/src/main/kotlin/no/nav/etterlatte/Application.kt
+++ apps/etterlatte-brev-kafka/src/main/kotlin/no/nav/etterlatte/Application.kt
@@ -5,7 +5,6 @@ import com.typesafe.config.ConfigFactory
 import io.ktor.client.HttpClient
 import no.nav.etterlatte.klienter.BrevapiKlient
 import no.nav.etterlatte.klienter.GrunnlagKlient
-import no.nav.etterlatte.libs.common.isDev
 import no.nav.etterlatte.libs.ktor.httpClientClientCredentials
 import no.nav.etterlatte.rapidsandrivers.configFromEnvironment
 import no.nav.etterlatte.rivers.DistribuerBrevRiver
@@ -56,7 +55,7 @@ class ApplicationBuilder {
                 },
             )
             // TODO: start med å merge inn uten å aktivere rivers
-            if (isDev()) {
+            if (false) {
                 OpprettJournalfoerOgDistribuerRiver(
                     brevapiKlient,
                     grunnlagKlient,

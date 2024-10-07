10ed5876354880b86c880a5701c97191c7d67d97
Nicolas N. <Watercolours@users.noreply.github.com>
2024-10-07 14:01:06 +0200

    mer logging for feilsøking (#5939)

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/navansatt/NavansattKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/navansatt/NavansattKlient.kt
@@ -5,7 +5,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.get
-import no.nav.etterlatte.libs.common.retryOgPakkUt
+import no.nav.etterlatte.libs.common.RetryResult
+import no.nav.etterlatte.libs.common.retry
 import org.slf4j.LoggerFactory
 import java.time.Duration
 
@@ -21,7 +22,18 @@ class NavansattKlient(
             .expireAfterWrite(Duration.ofMinutes(15))
             .build<String, SaksbehandlerInfo>()
 
-    suspend fun hentSaksbehandlerInfo(ident: String): SaksbehandlerInfo = retryOgPakkUt { hentSaksbehandler(ident) }
+    suspend fun hentSaksbehandlerInfo(ident: String): SaksbehandlerInfo =
+        retry {
+            hentSaksbehandler(ident)
+        }.let {
+            when (it) {
+                is RetryResult.Success -> it.content
+                is RetryResult.Failure -> {
+                    logger.warn("Feil ved uthenting av saksbehandlerInfo", it.samlaExceptions())
+                    throw it.samlaExceptions()
+                }
+            }
+        }
 
     private suspend fun hentSaksbehandler(ident: String): SaksbehandlerInfo =
         try {
@@ -36,7 +48,10 @@ class NavansattKlient(
                 client
                     .get("$url/navansatt/$ident")
                     .body<SaksbehandlerInfo>()
-                    .also { cache.put(ident, it) }
+                    .also {
+                        cache.put(ident, it)
+                        logger.info("Info funnet og cachet for saksbehandler med ident $ident")
+                    }
             }
         } catch (re: ResponseException) {
             logger.warn("Fikk ResponseException ved kall mot navansattklient", re)

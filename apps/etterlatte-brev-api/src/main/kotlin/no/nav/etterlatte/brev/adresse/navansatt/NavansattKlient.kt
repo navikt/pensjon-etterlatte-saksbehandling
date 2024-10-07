83efd1373722bf666e80297ed4b59765c4dd5755
Nicolas N. <Watercolours@users.noreply.github.com>
2024-10-07 12:16:34 +0200

    midlertidig justering for å debugge navansattklient error (#5936)

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/navansatt/NavansattKlient.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/adresse/navansatt/NavansattKlient.kt
@@ -3,6 +3,7 @@ package no.nav.etterlatte.brev.adresse.navansatt
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ResponseException
 import io.ktor.client.request.get
 import no.nav.etterlatte.libs.common.retryOgPakkUt
 import org.slf4j.LoggerFactory
@@ -20,7 +21,9 @@ class NavansattKlient(
             .expireAfterWrite(Duration.ofMinutes(15))
             .build<String, SaksbehandlerInfo>()
 
-    suspend fun hentSaksbehandlerInfo(ident: String): SaksbehandlerInfo =
+    suspend fun hentSaksbehandlerInfo(ident: String): SaksbehandlerInfo = retryOgPakkUt { hentSaksbehandler(ident) }
+
+    private suspend fun hentSaksbehandler(ident: String): SaksbehandlerInfo =
         try {
             val saksbehandlerCache = cache.getIfPresent(ident)
 
@@ -30,10 +33,14 @@ class NavansattKlient(
             } else {
                 logger.info("Henter info om saksbehandler med ident $ident")
 
-                retryOgPakkUt<SaksbehandlerInfo> {
-                    client.get("$url/navansatt/$ident").body()
-                }.also { cache.put(ident, it) }
+                client
+                    .get("$url/navansatt/$ident")
+                    .body<SaksbehandlerInfo>()
+                    .also { cache.put(ident, it) }
             }
+        } catch (re: ResponseException) {
+            logger.warn("Fikk ResponseException ved kall mot navansattklient", re)
+            throw re
         } catch (exception: Exception) {
             throw RuntimeException("Feil i kall mot navansatt med ident: $ident", exception)
         }

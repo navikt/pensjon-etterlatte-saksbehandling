b416ff6dafe09a89eb82bc72bd69a4a180153eb7
Bjørn Golberg <bjorn.golberg@nav.no>
2024-10-08 12:12:27 +0200

    Kunne overstyrte innvilga måneder - utkast med feature toggle (#5941)
    * Kunne overstyrte innvilga måneder - utkast med feature toggle
    
    * Egen klasse/interface for overstyrt innvilga måneder
    
    * Select og enum for enum for årsak innvilga måneder
    
    * Legger til felter for overstyrt innvilga måneder backend - ikke database
    
    * Legger til felter overstyrt innvilga måneder i db
    
    * Tilbakemeldinger PR

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/OverstyrInnvilgaMaaneder.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { Box, Button, Heading, HStack, Select, TextField, VStack } from '@navikt/ds-react'
+import { useFormContext } from 'react-hook-form'
+import {
+  hentLesbarTekstForInnvilgaMaanederType,
+  IAvkortingGrunnlagLagre,
+  OverstyrtInnvilaMaanederAarsak,
+} from '~shared/types/IAvkorting'
+
+export default function OverstyrInnvilgaMaander() {
+  const [skalOverstyre, setSkalOverstyre] = useState(false)
+
+  const {
+    register,
+    reset,
+    formState: { errors },
+  } = useFormContext<IAvkortingGrunnlagLagre>()
+
+  const toggleOverstyrtInnvilgaMaaneder = () => {
+    if (skalOverstyre) {
+      reset({ overstyrtInnvilgaMaaneder: undefined })
+    }
+    setSkalOverstyre(!skalOverstyre)
+  }
+
+  return (
+    <HStack marginBlock="4" gap="1" align="start" wrap={false}>
+      {skalOverstyre ? (
+        <>
+          <VStack>
+            <Heading size="xsmall">Overstyrt innvilga måneder</Heading>
+            <HStack marginBlock="2" gap="2" align="start" wrap={false}>
+              <TextField
+                {...register('overstyrtInnvilgaMaaneder.antall', {
+                  pattern: { value: /^\d+$/, message: 'Kun tall' },
+                  required: { value: true, message: 'Må fylles ut' },
+                })}
+                label="Antall"
+                size="medium"
+                inputMode="numeric"
+                error={errors?.overstyrtInnvilgaMaaneder?.antall?.message}
+              />
+              <Select
+                label="Årsak"
+                {...register('overstyrtInnvilgaMaaneder.aarsak', {
+                  required: { value: true, message: 'Du må velge årsak' },
+                })}
+                error={errors?.overstyrtInnvilgaMaaneder?.aarsak?.message}
+              >
+                <option value="">Velg årsak</option>
+                {Object.values(OverstyrtInnvilaMaanederAarsak).map((type) => (
+                  <option key={type} value={type}>
+                    {hentLesbarTekstForInnvilgaMaanederType(type as OverstyrtInnvilaMaanederAarsak)}
+                  </option>
+                ))}
+              </Select>
+              <Box width="17.5rem">
+                <TextField
+                  {...register('overstyrtInnvilgaMaaneder.begrunnelse', {
+                    required: { value: true, message: 'Må fylles ut' },
+                  })}
+                  label="Begrunnelse"
+                  size="medium"
+                  inputMode="text"
+                  error={errors?.overstyrtInnvilgaMaaneder?.begrunnelse?.message}
+                />
+              </Box>
+            </HStack>
+            <HStack>
+              <Button size="small" variant="secondary" onClick={toggleOverstyrtInnvilgaMaaneder}>
+                Fjern overstyrt innvilga måneder
+              </Button>
+            </HStack>
+          </VStack>
+        </>
+      ) : (
+        <Button size="small" variant="danger" onClick={toggleOverstyrtInnvilgaMaaneder}>
+          Overstyr innvilga måneder
+        </Button>
+      )}
+    </HStack>
+  )
+}

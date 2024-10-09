67cce5843f890f831c1856c846ed8b92d2bd91ed
Mads Opheim <mads.opheim@nav.no>
2024-10-09 08:58:29 +0200

    Tar bort eiget skjema, etter erfaringar med at det ikkje er kjempegodt støtta i nais

--- apps/etterlatte-behandling/src/main/resources/db/migration/V169__trigger_lytte_endringer.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V169__trigger_lytte_endringer.sql
@@ -1,7 +1,5 @@
 -- Basert på https://www.cybertec-postgresql.com/en/tracking-changes-in-postgresql/
-CREATE SCHEMA logging;
-
-CREATE TABLE logging.t_history
+CREATE TABLE t_history
 (
     id          serial,
     timestamp   timestamp DEFAULT now(),
@@ -13,23 +11,23 @@ CREATE TABLE logging.t_history
     etter       jsonb
 );
 
-CREATE FUNCTION logging.change_trigger() RETURNS trigger AS
+CREATE FUNCTION change_trigger() RETURNS trigger AS
 $$
 BEGIN
     IF TG_OP = 'INSERT'
     THEN
-        INSERT INTO logging.t_history (tabell, skjema, operation, foer)
+        INSERT INTO t_history (tabell, skjema, operation, foer)
         VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(NEW));
         RETURN NEW;
     ELSIF TG_OP = 'UPDATE'
     THEN
-        INSERT INTO logging.t_history (tabell, skjema, operation, foer, etter)
+        INSERT INTO t_history (tabell, skjema, operation, foer, etter)
         VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP,
                 row_to_json(NEW), row_to_json(OLD));
         RETURN NEW;
     ELSIF TG_OP = 'DELETE'
     THEN
-        INSERT INTO logging.t_history (tabell, skjema, operation, etter)
+        INSERT INTO t_history (tabell, skjema, operation, etter)
         VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(OLD));
         RETURN OLD;
     END IF;
@@ -40,10 +38,10 @@ CREATE TRIGGER audit_sak
     AFTER INSERT OR UPDATE OR DELETE
     ON sak
     FOR EACH ROW
-EXECUTE PROCEDURE logging.change_trigger();
+EXECUTE PROCEDURE change_trigger();
 
 CREATE TRIGGER audit_behandling
     AFTER INSERT OR UPDATE OR DELETE
     ON behandling
     FOR EACH ROW
-EXECUTE PROCEDURE logging.change_trigger();
+EXECUTE PROCEDURE change_trigger();

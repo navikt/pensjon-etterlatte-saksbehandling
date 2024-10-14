e14755e860dd6ac10e4b35bd8974cbd99c6656ac
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-10-14 15:45:45 +0200

    Bytt til serviceaccount role generell

--- apps/etterlatte-behandling/src/main/resources/db/prod/V172__grante_servicebruker.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V172__grante_servicebruker.sql
@@ -1,2 +1,2 @@
-GRANT USAGE on SCHEMA public to "migrering-user@etterlatte-prod-207c.iam";
-GRANT INSERT ON ALL TABLES IN SCHEMA public TO "migrering-user@etterlatte-prod-207c.iam";
+GRANT USAGE on SCHEMA public to "cloudsqliamserviceaccount";
+GRANT INSERT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";

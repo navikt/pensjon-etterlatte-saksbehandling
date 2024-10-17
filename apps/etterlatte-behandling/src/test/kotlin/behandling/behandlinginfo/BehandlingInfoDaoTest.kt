fb55c03eccae2363b99eb8d684865a12d34d4ab7
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-10-17 08:48:18 +0200

    La singleornull fungere som tiltenkt i behandlinginfo (#6033)
    * La singleornull fungere som tiltenkt i behandlinginfo
    
    * Kommentar

--- apps/etterlatte-behandling/src/test/kotlin/behandling/behandlinginfo/BehandlingInfoDaoTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/behandling/behandlinginfo/BehandlingInfoDaoTest.kt
@@ -100,6 +100,10 @@ internal class BehandlingInfoDaoTest(
                 kilde = Grunnlagsopplysning.Saksbehandler("123456", Tidspunkt.now()),
             )
         dao.hentSluttbehandling(behandlingId) shouldBe null
+        // Kun for å teste at det fungerer med singleornull selvom det finnes noe på behandlingiden
+        val etterbetaling = etterbetaling(behandlingId)
+        dao.lagreEtterbetaling(etterbetaling)
+        dao.hentSluttbehandling(behandlingId) shouldBe null
         dao.lagreSluttbehandling(behandlingId, sluttbehandling)
         dao.hentSluttbehandling(behandlingId) shouldBe sluttbehandling
     }

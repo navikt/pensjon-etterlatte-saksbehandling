2a60e302bdf8c2565f47e9a3c24b4a8e79496112
Bjørn Golberg <bjorn.golberg@nav.no>
2024-10-22 11:42:06 +0200

    Script for å avbryte oppgaver som feilet under automatisk aldersovergang og legger til ny jobb (#6062)

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V174__avbryte_oppgaver_for_feilet_aldersovergang.sql
@@ -0,0 +1,12 @@
+-- Setter oppgaver til alle feilede jobber til avbrytt
+-- Se V16__kjoerer_AO_BP_20_paa_nytt_igjen.sql (tidshendelser)
+update oppgave
+set status  = 'AVBRUTT',
+    merknad = 'Avbrutt på grunn av bug under automatisk jobb'
+where sak_id in (select id, sak_id, status, merknad, opprettet, frist
+                 from oppgave
+                 where status = 'NY'
+                   and opprettet >= '2024-10-21'
+                   and opprettet < '2024-10-22'
+                   and merknad = 'Aldersovergang barnepensjon ved 20 år'
+                   and type = 'REVURDERING')

f1c4d2013942db31c5b425597d10a8a419c1abdf
Bjørn Golberg <bjorn.golberg@nav.no>
2024-10-22 12:44:42 +0200

    Omstrukturere inputfelter innvilga måneder

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/OverstyrInnvilgaMaaneder.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/OverstyrInnvilgaMaaneder.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, HStack, Select, TextField, VStack } from '@navikt/ds-react'
+import { Box, Heading, HStack, Select, Textarea, TextField, VStack } from '@navikt/ds-react'
 import { useFormContext } from 'react-hook-form'
 import {
   hentLesbarTekstForInnvilgaMaanederType,
@@ -16,44 +16,49 @@ export default function OverstyrInnvilgaMaander() {
   return (
     <HStack marginBlock="4" gap="1" align="start" wrap={false}>
       <VStack>
-        <Heading size="xsmall">Overstyrt innvilga måneder</Heading>
-        <HStack marginBlock="2" gap="2" align="start" wrap={false}>
-          <TextField
-            {...register('overstyrtInnvilgaMaaneder.antall', {
-              pattern: { value: /^\d+$/, message: 'Kun tall' },
-              required: { value: true, message: 'Må fylles ut' },
-            })}
-            label="Antall"
-            size="medium"
-            inputMode="numeric"
-            error={errors?.overstyrtInnvilgaMaaneder?.antall?.message}
-          />
-          <Select
-            label="Årsak"
-            {...register('overstyrtInnvilgaMaaneder.aarsak', {
-              required: { value: true, message: 'Du må velge årsak' },
-            })}
-            error={errors?.overstyrtInnvilgaMaaneder?.aarsak?.message}
-          >
-            <option value="">Velg årsak</option>
-            {Object.values(OverstyrtInnvilaMaanederAarsak).map((type) => (
-              <option key={type} value={type}>
-                {hentLesbarTekstForInnvilgaMaanederType(type as OverstyrtInnvilaMaanederAarsak)}
-              </option>
-            ))}
-          </Select>
-          <Box width="17.5rem">
+        <Box marginBlock="2">
+          <Heading size="small">Overstyrt innvilga måneder</Heading>
+        </Box>
+        <HStack marginBlock="2" gap="4" align="start" wrap={false}>
+          <Box width="13rem">
             <TextField
-              {...register('overstyrtInnvilgaMaaneder.begrunnelse', {
+              {...register('overstyrtInnvilgaMaaneder.antall', {
+                pattern: { value: /^\d+$/, message: 'Kun tall' },
                 required: { value: true, message: 'Må fylles ut' },
               })}
-              label="Begrunnelse"
+              label="Antall måneder"
               size="medium"
-              inputMode="text"
-              error={errors?.overstyrtInnvilgaMaaneder?.begrunnelse?.message}
+              inputMode="numeric"
+              error={errors?.overstyrtInnvilgaMaaneder?.antall?.message}
             />
           </Box>
+          <Box width="19rem">
+            <Select
+              label="Årsak"
+              {...register('overstyrtInnvilgaMaaneder.aarsak', {
+                required: { value: true, message: 'Du må velge årsak' },
+              })}
+              error={errors?.overstyrtInnvilgaMaaneder?.aarsak?.message}
+            >
+              <option value="">Velg årsak</option>
+              {Object.values(OverstyrtInnvilaMaanederAarsak).map((type) => (
+                <option key={type} value={type}>
+                  {hentLesbarTekstForInnvilgaMaanederType(type as OverstyrtInnvilaMaanederAarsak)}
+                </option>
+              ))}
+            </Select>
+          </Box>
         </HStack>
+        <Box width="33rem">
+          <Textarea
+            {...register('overstyrtInnvilgaMaaneder.begrunnelse', {
+              required: { value: true, message: 'Må fylles ut' },
+            })}
+            label="Begrunnelse"
+            inputMode="text"
+            error={errors?.overstyrtInnvilgaMaaneder?.begrunnelse?.message}
+          />
+        </Box>
       </VStack>
     </HStack>
   )

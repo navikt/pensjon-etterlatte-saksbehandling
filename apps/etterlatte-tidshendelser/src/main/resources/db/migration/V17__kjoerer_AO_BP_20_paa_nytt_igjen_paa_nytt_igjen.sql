64a9c8150dd21ceafdfb8a245f1ae3edb5f414b4
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-10-23 15:20:45 +0200

    Legger til ny kjøring for AO_BP20

---
+++ apps/etterlatte-tidshendelser/src/main/resources/db/migration/V17__kjoerer_AO_BP_20_paa_nytt_igjen_paa_nytt_igjen.sql
@@ -0,0 +1,3 @@
+-- Kjører aldersovergang BP20 på nytt igjen på nytt igjen i riktig behandlingsmåned
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
+values ('AO_BP20', '2024-10-24', '2024-11');

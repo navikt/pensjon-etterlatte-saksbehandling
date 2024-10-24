f797867ea0d49a3f8653c3af7334cc60f190c9a1
Andreas Balevik <andreasbalevik@gmail.com>
2024-10-24 10:50:19 +0200

    EY-4618 opprett oppgave om opphør ved tidlig alderspensjon  (#6065)
    * EY-4618 wip sjekk om tidlig alderspensjon river
    
    * EY-4618 wip
    
    * EY-4618 sjekke om avkorting innvilga måneder overstyres
    
    * EY-4618 opprette oppgave ved tidlig alderspensjon
    
    * EY-4618 fikset bygging
    
    * EY-4618 rettet tilbakemeldinger
    
    * EY-4618 rettet tilbakemeldinger
    
    * EY-4618 la til logg hvis overstyrtInntektsavkorting == null
    
    * EY-4618 la til frist
    
    * EY-4618 la til test
    
    * EY-4618 kaste exception hvis ingen avkorting
    
    * EY-4618 rettet tilbakemeldinger
    
    * EY-4618 rettet tilbakemeldinger
    
    ---------
    
    Co-authored-by: Andreas Balevik <andreasbalevik@gmail.no>

---
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/SjekkOmTidligAlderpensjonRiver.kt
@@ -0,0 +1,38 @@
+package no.nav.etterlatte.beregningkafka
+
+import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.vedtak.VedtakKafkaHendelseHendelseType
+import no.nav.etterlatte.rapidsandrivers.Kontekst
+import no.nav.etterlatte.rapidsandrivers.ListenerMedLoggingOgFeilhaandtering
+import no.nav.etterlatte.rapidsandrivers.asUUID
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+
+class SjekkOmTidligAlderpensjonRiver(
+    rapidsConnection: RapidsConnection,
+    private val beregningService: BeregningService,
+) : ListenerMedLoggingOgFeilhaandtering() {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun kontekst() = Kontekst.INNTEKTSJUSTERING
+
+    init {
+        initialiserRiver(rapidsConnection, VedtakKafkaHendelseHendelseType.ATTESTERT) {
+            validate { it.requireKey("vedtak.behandlingId") }
+            validate { it.requireKey("vedtak.sak") }
+            validate { it.requireKey("vedtak.sak.id") }
+            validate { it.requireKey("vedtak.sak.sakType", SakType.OMSTILLINGSSTOENAD.toString()) }
+        }
+    }
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        val behandlingId = packet["vedtak.behandlingId"].asUUID()
+        logger.info("Mottatt sjekk om tidlig alderpensjon for $behandlingId")
+        beregningService.haandterTidligAlderpensjon(behandlingId)
+    }
+}

60b24238123df16b0698382d0cb661652595f024
Andreas Balevik <andreasbalevik@gmail.no>
2024-10-28 16:08:16 +0100

    la til respons som manglet

--- apps/etterlatte-beregning/src/main/kotlin/avkorting/AvkortingService.kt
+++ apps/etterlatte-beregning/src/main/kotlin/avkorting/AvkortingService.kt
@@ -96,7 +96,7 @@ class AvkortingService(
         brukerTokenInfo: BrukerTokenInfo,
     ) {
         val behandling = behandlingKlient.hentBehandling(behandlingId, brukerTokenInfo)
-        logger.info("Sjekker om vi skal opprette oppgave om pååhør grunnen tidlig alderspensjon for sakId=${behandling.sak.sakId}")
+        logger.info("Sjekker om vi skal opprette oppgave om oppgør grunnen tidlig alderspensjon for sakId=${behandling.sak.sakId}")
         val avkorting = avkortingRepository.hentAvkorting(behandlingId) ?: throw AvkortingFinnesIkkeException(behandlingId)
 
         val aarsoppgjoer = avkorting.aarsoppgjoer.single { it.aar == behandling.virkningstidspunkt?.dato?.year }

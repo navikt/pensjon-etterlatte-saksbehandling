50f0f604432e2471b47eef51bf8c1a6573801353
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2024-10-30 14:40:29 +0100

    EY-4644 Lagre brevdata for aktivitetsplikt 10mnd (#6151)
    * EY-4644 Lagre brevdata for aktivitetsplikt 10mnd
    
    * Sleng med redigering og visning hvis ting allerede er lagret
    
    * pr feedback
    
    * Kun vise rediger om oppgave er redigerbar
    
    * Fiks visning med lagring etter restkall

--- apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/Vurderinger.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/Vurderinger.tsx
@@ -4,21 +4,21 @@ import { Aktivitetsgrad } from '~components/person/aktivitet/vurderingAvAktivite
 import { Unntak } from '~components/person/aktivitet/vurderingAvAktivitetsplikt/Unntak'
 import { erOppgaveRedigerbar } from '~shared/types/oppgave'
 import { Heading } from '@navikt/ds-react'
+import { BrevAktivitetsplikt } from '~components/person/aktivitet/vurderingAvAktivitetsplikt/BrevAktivitetsplikt'
 
 export function Vurderinger() {
   const { oppgave, vurdering } = useAktivitetspliktOppgaveVurdering()
 
-  return erOppgaveRedigerbar(oppgave.status) ? (
+  return (
     <>
-      <Heading size="small">Redigering av vurderinger i en oppgave er ikke støttet enda</Heading>
+      <Heading size="small">
+        {erOppgaveRedigerbar(oppgave.status)
+          ? 'Redigering av vurderinger i en oppgave er ikke støttet enda'
+          : 'Vurderinger i oppgave'}
+      </Heading>
       <Aktivitetsgrad aktiviteter={vurdering.aktivitet} />
       <Unntak unntaker={vurdering.unntak} />
-    </>
-  ) : (
-    <>
-      <Heading size="small">Vurderinger i oppgave</Heading>
-      <Aktivitetsgrad aktiviteter={vurdering.aktivitet} />
-      <Unntak unntaker={vurdering.unntak} />
+      <BrevAktivitetsplikt />
     </>
   )
 }

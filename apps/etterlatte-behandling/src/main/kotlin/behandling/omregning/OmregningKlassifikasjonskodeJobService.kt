f73be9deb9840e9fa28f83a898018ea5752367ac
Henrik Gundersen <henrik.gundersen@nav.no>
2024-10-31 15:09:42 +0100

    EY-4513: Feiler med flere saker på etterbetaling

--- apps/etterlatte-behandling/src/main/kotlin/behandling/omregning/OmregningKlassifikasjonskodeJobService.kt
+++ apps/etterlatte-behandling/src/main/kotlin/behandling/omregning/OmregningKlassifikasjonskodeJobService.kt
@@ -138,6 +138,12 @@ class OmregningKlassifikasjonskodeJobService(
     companion object {
         val kjoering = "ENDRE_KLASSIFIKASJONSKODE_FOR_PERIODER_2023"
         val antall = 20
-        val ekskluderteSaker = listOf(SakId(11958))
+        val ekskluderteSaker =
+            listOf(
+                SakId(11958), // Overstyrt
+                SakId(12703), // Feiler med etterbetaling
+                SakId(12718), // Feiler med etterbetaling
+                SakId(12655), // Feiler med klassifikasjonskode UTBETALING
+            )
     }
 }

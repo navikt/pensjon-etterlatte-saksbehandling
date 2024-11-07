3c803a6148a3fd74a01415419e425916a57c5793
Bjørn Golberg <bjorn.golberg@nav.no>
2024-11-07 18:26:44 +0100

    Videreføre inntekt inn i nytt år under årlig innntektsjustering (#6259)
    * Videreføre inntekt inn i nytt år under årlig innntektsjustering
    
    * Legger ved år for å kunne verifisere inntekt som videreføres

--- apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/BeregningService.kt
@@ -5,10 +5,14 @@ import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.put
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.http.isSuccess
 import kotlinx.coroutines.runBlocking
 import no.nav.etterlatte.grunnbeloep.Grunnbeloep
+import no.nav.etterlatte.libs.common.beregning.AarligInntektsjusteringAvkortingRequest
 import java.util.UUID
 
 class BeregningService(
@@ -59,6 +63,24 @@ class BeregningService(
     fun hentAvkorting(behandlingId: UUID) =
         runBlocking {
             beregningApp.get("$url/api/beregning/avkorting/$behandlingId/ferdig")
+        }
+
+    fun omregnAarligInntektsjustering(
+        aar: Int,
+        behandlingId: UUID,
+        forrigeBehandlingId: UUID,
+    ): HttpResponse =
+        runBlocking {
+            beregningApp.post("$url/api/beregning/avkorting/aarlig-inntektsjustering") {
+                contentType(ContentType.Application.Json)
+                setBody(
+                    AarligInntektsjusteringAvkortingRequest(
+                        aar = aar,
+                        nyBehandling = behandlingId,
+                        forrigeBehandling = forrigeBehandlingId,
+                    ),
+                )
+            }
         }
 
     suspend fun hentGrunnbeloep(): Grunnbeloep =

fa7a43bd8302ad889903564db46d7dc97799bb97
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-12 19:41:46 +0100

    ..ooog i server

---
+++ apps/etterlatte-saksbehandling-ui/server/eslint.config.mjs
@@ -0,0 +1,42 @@
+import jest from "eslint-plugin-jest";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+        jest,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...jest.environments.globals.globals,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "12",
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-console": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-explicit-any": "error",
+    },
+}];

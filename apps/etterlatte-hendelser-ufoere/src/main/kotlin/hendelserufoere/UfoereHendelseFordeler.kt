7542197387217a832473051f1f642449641a3668
Emil Schrøder <emil.schroder@nav.no>
2024-11-15 13:17:31 +0100

    EY-4703: Enkelt oppsett for app som lytter på uføre hendelser (#6255)
    * Enkelt oppsett for app som lytter på uføre hendelser
    
    * Fjern avro og legg inn topic og group-id
    
    * Legg til behandlingklient
    
    * Bedre sjekk for om bruker er mellom 18 og 21 år
    
    * Oppdater ufoerehendelse og oppdater sjekk for alder
    
    * Kast exception ved feil
    
    * Typing av ufoerehendelse
    
    * Fjern prod yaml

---
+++ apps/etterlatte-hendelser-ufoere/src/main/kotlin/hendelserufoere/UfoereHendelseFordeler.kt
@@ -0,0 +1,49 @@
+package no.nav.etterlatte.hendelserufoere
+
+import no.nav.etterlatte.BehandlingKlient
+import no.nav.etterlatte.libs.common.person.maskerFnr
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.time.temporal.ChronoUnit
+
+class UfoereHendelseFordeler(
+    private val behandlingKlient: BehandlingKlient,
+) {
+    private val logger: Logger = LoggerFactory.getLogger(UfoereHendelseFordeler::class.java)
+
+    suspend fun haandterHendelse(hendelse: UfoereHendelse) {
+        try {
+            val attenAarIMaaneder = 12 * 18
+            val tjueenAarIMaaneder = 12 * 21
+
+            val alderVedVirkningstidspunkt = ChronoUnit.MONTHS.between(hendelse.fodselsdato, hendelse.virkningsdato)
+
+            if (alderVedVirkningstidspunkt in attenAarIMaaneder..tjueenAarIMaaneder) {
+                logger.info("Bruker er mellom 18 og 21 på på virkningstidspunktet. Sender ufoerehendelse til behandling")
+                behandlingKlient.postTilBehandling(
+                    ufoereHendelse = hendelse,
+                )
+            } else {
+                logger.info(
+                    "Ufoerehendelse med personidentifikator=${hendelse.personIdent.maskerFnr()} " +
+                        "og alderVedVirkningstidspunkt=$alderVedVirkningstidspunkt " +
+                        "er ikke innenfor aldersgruppen 18-21. Hendelsen blir ikke sendt videre.",
+                )
+            }
+        } catch (e: Exception) {
+            loggFeilVedHaandteringAvHendelse(hendelse.vedtaksType.name, e)
+            throw e
+        }
+    }
+
+    private fun loggFeilVedHaandteringAvHendelse(
+        hendelsestype: String?,
+        e: Exception,
+    ) {
+        logger.error(
+            "Kunne ikke haandtere $hendelsestype. Dette skyldes sannsynligvis at " +
+                "ufoerehendelsen ser annerledes ut enn forventet.",
+            e,
+        )
+    }
+}

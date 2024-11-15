361d886f0157d73764d2a5a0f205c806e76a531c
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2024-11-15 12:45:15 +0100

    Valg av aktivitetsgrad skal være over datoer (#6344)

--- apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/aktivitetsgrad/VurderingAktivitetsgradForm.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/aktivitetsgrad/VurderingAktivitetsgradForm.tsx
@@ -107,22 +107,6 @@ export function VurderingAktivitetsgradForm(props: {
   return (
     <form onSubmit={handleSubmit(lagreOgOppdater)}>
       <VStack gap="6">
-        <HStack gap="6">
-          <ControlledDatoVelger
-            name="vurderingAvAktivitet.fom"
-            label="Fra og med"
-            control={control}
-            description="Fra dato oppgitt"
-          />
-          <ControlledDatoVelger
-            name="vurderingAvAktivitet.tom"
-            label="Til og med"
-            control={control}
-            required={false}
-            description="Hvis det er oppgitt sluttdato"
-          />
-        </HStack>
-
         <ControlledRadioGruppe
           control={control}
           name="vurderingAvAktivitet.aktivitetsgrad"
@@ -144,7 +128,21 @@ export function VurderingAktivitetsgradForm(props: {
             </>
           }
         />
-
+        <HStack gap="6">
+          <ControlledDatoVelger
+            name="vurderingAvAktivitet.fom"
+            label="Fra og med"
+            control={control}
+            description="Fra dato oppgitt"
+          />
+          <ControlledDatoVelger
+            name="vurderingAvAktivitet.tom"
+            label="Til og med"
+            control={control}
+            required={false}
+            description="Hvis det er oppgitt sluttdato"
+          />
+        </HStack>
         {harKanskjeUnntak && (
           <ControlledRadioGruppe
             name="harUnntak"

dfebde2730cbf5c24ccdfa3801a2b1a6a03c328e
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2024-11-20 11:56:45 +0100

    EY-4657 endring i innvilga måneder må indikeres (#6398)
    * Legg inn tooltip hvis invilga måneder er overstyrt
    
    * Legg inn ikon for hvis person er 67
    
    * Annet ikon for gammel

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/AvkortingInntekt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/AvkortingInntekt.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Heading, HelpText, HStack, Table, VStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Button, Heading, HelpText, HStack, Table, Tooltip, VStack } from '@navikt/ds-react'
 import React, { useState } from 'react'
 import { NOK } from '~utils/formatering/formatering'
 import { formaterDato } from '~utils/formatering/dato'
@@ -11,7 +11,7 @@ import { useInnloggetSaksbehandler } from '../useInnloggetSaksbehandler'
 import { lastDayOfMonth } from 'date-fns'
 import { AvkortingInntektForm } from '~components/behandling/avkorting/AvkortingInntektForm'
 import { IAvkortingGrunnlagFrontend } from '~shared/types/IAvkorting'
-import { PencilIcon } from '@navikt/aksel-icons'
+import { ArrowCirclepathIcon, HeadCloudIcon, PencilIcon } from '@navikt/aksel-icons'
 import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
 
 export const AvkortingInntekt = ({
@@ -134,7 +134,21 @@ export const AvkortingInntekt = ({
                       <Table.DataCell key="InntektTotalt">
                         {NOK(forventetInntekt + forventetInntektUtland)}
                       </Table.DataCell>
-                      <Table.DataCell>{avkortingGrunnlag.innvilgaMaaneder}</Table.DataCell>
+                      <Table.DataCell>
+                        <HStack gap="4" align="center">
+                          <BodyShort>{avkortingGrunnlag.innvilgaMaaneder}</BodyShort>
+                          {fyller67 && !avkortingGrunnlag.overstyrtInnvilgaMaaneder && (
+                            <Tooltip content="Fyller 67 år">
+                              <HeadCloudIcon aria-hidden fontSize="1.5rem" />
+                            </Tooltip>
+                          )}
+                          {!!avkortingGrunnlag.overstyrtInnvilgaMaaneder && (
+                            <Tooltip content="Antall innvilga måneder er overstyrt">
+                              <ArrowCirclepathIcon aria-hidden fontSize="1.5rem" />
+                            </Tooltip>
+                          )}
+                        </HStack>
+                      </Table.DataCell>
                       <Table.DataCell key="Periode">
                         {avkortingGrunnlag.fom && formaterDato(avkortingGrunnlag.fom)} -{' '}
                         {avkortingGrunnlag.tom && formaterDato(lastDayOfMonth(new Date(avkortingGrunnlag.tom)))}

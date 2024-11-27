bed33a4a4f1c50af60ebe1d589384e987fbb881e
Henrik Gundersen <henrik.gundersen@nav.no>
2024-11-27 15:23:22 +0100

    FAGSYSTEM-358535: Fjerner referanse til feil behandling i aldersovergang-oppgave

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V192__oppdater_oppgave_feil_referanse_aldersovergang.sql
@@ -0,0 +1,10 @@
+/* Denne oppgavens referanse peker på siste behandling (når oppgaven ble opprettet). Dette gir ingen mening da det var
+   en regulering og denne oppgaven opprettes fordi det feilet å lage en opphørsbehandling.
+   Saken er nå løst i en anne behandling så oppgaven skal derfor ferdigstilles.
+
+   Referanse når disse oppgavene opprettes skal bare være tom. Dette er løst her:
+   https://github.com/navikt/pensjon-etterlatte-saksbehandling/pull/6417
+
+   Se også porten-sak: https://jira.adeo.no/browse/FAGSYSTEM-358535
+ */
+UPDATE oppgave SET referanse = '' WHERE id = 'edf8f646-fe34-454b-acba-cbc95c9df8f5';

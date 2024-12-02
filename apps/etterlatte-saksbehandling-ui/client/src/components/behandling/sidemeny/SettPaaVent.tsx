5419cd09f3c73d45426e418f9b5f51f600b9bdcb
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-02 09:26:10 +0100

    Gjøre ferdig ny modal for å sette oppgave på vent

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVent.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVent.tsx
@@ -1,143 +1,37 @@
-import { Alert, Button, Heading, HStack, Label, Select, Textarea, VStack } from '@navikt/ds-react'
-import { settOppgavePaaVentApi } from '~shared/api/oppgaver'
-import { ClockDashedIcon, ClockIcon } from '@navikt/aksel-icons'
+import { Alert, Heading, HStack } from '@navikt/ds-react'
 import { formaterDato } from '~utils/formatering/dato'
-import React, { useState } from 'react'
-import { useApiCall } from '~shared/hooks/useApiCall'
+import React from 'react'
 import { Info } from '~components/behandling/soeknadsoversikt/Info'
-import { isPending } from '~shared/api/apiUtils'
 import { erOppgaveRedigerbar, OppgaveDTO } from '~shared/types/oppgave'
-import { useAppDispatch } from '~store/Store'
-import { settOppgave } from '~store/reducers/OppgaveReducer'
-import { OppgaveFrist } from '~components/oppgavebenk/frist/OppgaveFrist'
+import { SettPaaVentModal } from '~components/behandling/sidemeny/SettPaaVentModal'
 
 interface Props {
   oppgave: OppgaveDTO | null
 }
 
-enum PaaventAarsak {
-  OPPLYSNING_FRA_BRUKER = 'Opplysning fra bruker',
-  OPPLYSNING_FRA_ANDRE = 'Opplysning fra andre',
-  KRAVGRUNNLAG_SPERRET = 'Kravgrunnlag sperret',
-  ANNET = 'Annet',
-}
-
 export const SettPaaVent = ({ oppgave }: Props) => {
   if (!oppgave || !erOppgaveRedigerbar(oppgave?.status)) return null
 
-  const dispatch = useAppDispatch()
-  type settPaaVentTyper = keyof typeof PaaventAarsak
-
-  const [, setFrist] = useState<string>(oppgave.frist)
-  const [merknad, setMerknad] = useState<string>(oppgave.merknad || '')
-  const [settPaaVent, setVisPaaVent] = useState(false)
-  const [aarsak, setAarsak] = useState<settPaaVentTyper[number]>()
-  const [aarsakError, setAarsakError] = useState<boolean>(false)
-
-  const [settPaaVentStatus, settOppgavePaaVent] = useApiCall(settOppgavePaaVentApi)
-
-  const oppdater = () => {
-    const paaVent = oppgave.status !== 'PAA_VENT'
-    if (paaVent && !aarsak) {
-      setAarsakError(true)
-      return
-    }
-
-    settOppgavePaaVent(
-      {
-        oppgaveId: oppgave.id,
-        settPaaVentRequest: {
-          aarsak: aarsak,
-          merknad,
-          paaVent: paaVent,
-        },
-      },
-      (oppgave) => {
-        dispatch(settOppgave(oppgave))
-
-        setVisPaaVent(false)
-      }
-    )
-  }
-
   return (
     <div>
-      {settPaaVent && (
-        <>
-          <hr />
-          <Textarea label="Merknad" value={merknad} onChange={(e) => setMerknad(e.target.value)} />
+      <>
+        {oppgave?.status === 'PAA_VENT' && (
+          <>
+            <Alert variant="warning" size="small">
+              <Heading size="xsmall" spacing>
+                Oppgaven står på vent!
+              </Heading>
+              <Info label="Merknad" tekst={oppgave.merknad || 'Ingen'} />
+              <Info label="Ny frist" tekst={formaterDato(oppgave.frist)} />
+            </Alert>
+          </>
+        )}
 
-          <br />
-          {oppgave.status !== 'PAA_VENT' && (
-            <VStack gap="4">
-              <Select
-                label="Årsak for å sette på vent"
-                value={aarsak || ''}
-                onChange={(e) => {
-                  setAarsakError(false)
-                  setAarsak(e.target.value as settPaaVentTyper)
-                }}
-                error={aarsakError && 'Du må velge en årsak'}
-              >
-                <option value="" disabled={true}>
-                  Velg
-                </option>
-                {Object.entries(PaaventAarsak).map(([aarsak, aarsakBeskrivelse]) => (
-                  <option key={aarsak} value={aarsak}>
-                    {aarsakBeskrivelse}
-                  </option>
-                ))}
-              </Select>
-
-              <VStack gap="2" justify="start">
-                <Label>Frist</Label>
-                {/*TODO: etter at frist har blitt endret, må man hente frist/oppgave på nytt*/}
-                <div>
-                  <OppgaveFrist oppgave={oppgave} oppdaterFrist={(_, frist: string) => setFrist(frist)} />
-                </div>
-              </VStack>
-            </VStack>
-          )}
-
-          <HStack gap="4" justify="end">
-            <Button size="small" variant="secondary" onClick={() => setVisPaaVent(false)}>
-              Avbryt
-            </Button>
-            <Button size="small" variant="primary" onClick={oppdater} loading={isPending(settPaaVentStatus)}>
-              Lagre
-            </Button>
-          </HStack>
-        </>
-      )}
-
-      {!settPaaVent && (
-        <>
-          {oppgave?.status === 'PAA_VENT' && (
-            <>
-              <Alert variant="warning" size="small">
-                <Heading size="xsmall" spacing>
-                  Oppgaven står på vent!
-                </Heading>
-                <Info label="Merknad" tekst={oppgave.merknad || 'Ingen'} />
-                <Info label="Ny frist" tekst={formaterDato(oppgave.frist)} />
-              </Alert>
-            </>
-          )}
-
-          <br />
-          <HStack justify="end">
-            <Button
-              size="small"
-              variant="secondary"
-              onClick={() => setVisPaaVent(true)}
-              icon={oppgave.status === 'PAA_VENT' ? <ClockDashedIcon /> : <ClockIcon />}
-              iconPosition="right"
-            >
-              {oppgave.status === 'PAA_VENT' ? 'Ta av vent' : 'Sett på vent'}
-            </Button>
-          </HStack>
-        </>
-      )}
+        <br />
+        <HStack justify="end">
+          <SettPaaVentModal oppgave={oppgave} />
+        </HStack>
+      </>
     </div>
   )
 }
--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVentModal.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/sidemeny/SettPaaVentModal.tsx
@@ -1,11 +1,15 @@
-import { Button, Heading, Modal, Select, TextField, VStack } from '@navikt/ds-react'
+import { Box, Button, Heading, HStack, Modal, Select, Textarea, VStack } from '@navikt/ds-react'
 import { erOppgaveRedigerbar, OppgaveDTO, Oppgavestatus } from '~shared/types/oppgave'
 import { useState } from 'react'
 import { ClockDashedIcon, ClockIcon } from '@navikt/aksel-icons'
 import { useForm } from 'react-hook-form'
-import { EndrePaaVentRequest } from '~shared/api/oppgaver'
+import { EndrePaaVentRequest, redigerFristApi, settOppgavePaaVentApi } from '~shared/api/oppgaver'
 import { ControlledDatoVelger } from '~shared/components/datoVelger/ControlledDatoVelger'
 import { datoIMorgen, datoToAarFramITid } from '~components/oppgavebenk/frist/utils'
+import { useApiCall } from '~shared/hooks/useApiCall'
+import { useAppDispatch } from '~store/Store'
+import { settOppgave } from '~store/reducers/OppgaveReducer'
+import { isPending } from '~shared/api/apiUtils'
 
 enum AarsakForAaSettePaaVent {
   OPPLYSNING_FRA_BRUKER = 'Opplysning fra bruker',
@@ -15,15 +19,46 @@ enum AarsakForAaSettePaaVent {
 }
 
 interface SettPaaVentSkjema extends EndrePaaVentRequest {
-  nyFrist: string
+  nyFrist: Date
 }
 
 export const SettPaaVentModal = ({ oppgave }: { oppgave: OppgaveDTO }) => {
   const [aapen, setAapen] = useState<boolean>(false)
 
-  const { register, control } = useForm<SettPaaVentSkjema>({ defaultValues: { merknad: oppgave.merknad } })
+  const dispatch = useAppDispatch()
+
+  const [settOppgavePaaVentResult, settOppgavePaaVentFunc] = useApiCall(settOppgavePaaVentApi)
+  const [redigerFristResult, redigerFristFunc] = useApiCall(redigerFristApi)
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SettPaaVentSkjema>({
+    defaultValues: { merknad: oppgave.merknad },
+  })
 
   if (!erOppgaveRedigerbar(oppgave.status)) return null
+
+  const settPaaEllerAvVent = (data: SettPaaVentSkjema) => {
+    redigerFristFunc({ oppgaveId: oppgave.id, redigerFristRequest: { frist: data.nyFrist, versjon: null } }, () => {
+      settOppgavePaaVentFunc(
+        {
+          oppgaveId: oppgave.id,
+          settPaaVentRequest: {
+            aarsak: data.aarsak,
+            merknad: data.merknad,
+            paaVent: oppgave.status !== Oppgavestatus.PAA_VENT,
+          },
+        },
+        (oppgave) => {
+          dispatch(settOppgave(oppgave))
+          setAapen(false)
+        }
+      )
+    })
+  }
 
   return (
     <>
@@ -38,46 +73,63 @@ export const SettPaaVentModal = ({ oppgave }: { oppgave: OppgaveDTO }) => {
       </Button>
 
       <Modal open={aapen} onClose={() => setAapen(false)} aria-labelledby="modal for å sette oppgave på vent">
+        <Modal.Header>
+          <Heading size="medium" spacing>
+            {oppgave.status === Oppgavestatus.PAA_VENT ? 'Ta av vent' : 'Sett på vent'}
+          </Heading>
+        </Modal.Header>
         <Modal.Body>
-          <Modal.Header>
-            <Heading size="medium" spacing>
-              {oppgave.status === Oppgavestatus.PAA_VENT ? 'Ta av vent' : 'Sett på vent'}
-            </Heading>
-          </Modal.Header>
-          <form>
-            <VStack gap="2">
-              {oppgave.status !== Oppgavestatus.PAA_VENT && (
-                <Select
-                  {...register('aarsak', {
-                    required: {
-                      value: true,
-                      message: 'Du må velge en årsak',
-                    },
-                  })}
-                  label="Årsak"
-                >
-                  <option value="">Velg årsak</option>
-                  {Object.entries(AarsakForAaSettePaaVent).map(([aarsak, beskrivelse]) => (
-                    <option key={aarsak} value={aarsak}>
-                      {beskrivelse}
-                    </option>
-                  ))}
-                </Select>
-              )}
-              <TextField {...register('merknad')} label="Merknad" />
+          <Box width="25rem">
+            <form onSubmit={handleSubmit(settPaaEllerAvVent)}>
+              <VStack gap="4">
+                {oppgave.status !== Oppgavestatus.PAA_VENT && (
+                  <Select
+                    {...register('aarsak', {
+                      required: {
+                        value: true,
+                        message: 'Du må velge en årsak',
+                      },
+                    })}
+                    label="Årsak"
+                    error={errors.aarsak?.message}
+                  >
+                    <option value="">Velg årsak</option>
+                    {Object.entries(AarsakForAaSettePaaVent).map(([aarsak, beskrivelse]) => (
+                      <option key={aarsak} value={aarsak}>
+                        {beskrivelse}
+                      </option>
+                    ))}
+                  </Select>
+                )}
+                <Textarea {...register('merknad')} label="Merknad" />
 
-              {oppgave.status !== Oppgavestatus.PAA_VENT && (
-                <ControlledDatoVelger
-                  name="nyFrist"
-                  label="Ny frist"
-                  control={control}
-                  errorVedTomInput="Du må velge en ny frist"
-                  fromDate={datoIMorgen()}
-                  toDate={datoToAarFramITid()}
-                />
-              )}
-            </VStack>
-          </form>
+                {oppgave.status !== Oppgavestatus.PAA_VENT && (
+                  <ControlledDatoVelger
+                    name="nyFrist"
+                    label="Ny frist"
+                    control={control}
+                    errorVedTomInput="Du må velge en ny frist"
+                    fromDate={datoIMorgen()}
+                    toDate={datoToAarFramITid()}
+                  />
+                )}
+
+                <HStack gap="2" justify="end">
+                  <Button
+                    type="button"
+                    variant="secondary"
+                    disabled={isPending(settOppgavePaaVentResult) || isPending(redigerFristResult)}
+                    onClick={() => setAapen(false)}
+                  >
+                    Avbryt
+                  </Button>
+                  <Button type="submit" loading={isPending(settOppgavePaaVentResult) || isPending(redigerFristResult)}>
+                    {oppgave.status === Oppgavestatus.PAA_VENT ? 'Ta av vent' : 'Sett på vent'}
+                  </Button>
+                </HStack>
+              </VStack>
+            </form>
+          </Box>
         </Modal.Body>
       </Modal>
     </>

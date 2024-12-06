01e09316c641638366f1cfc08ea312b14d0fdc52
Henrik Gundersen <henrik.gundersen@nav.no>
2024-12-06 11:15:45 +0100

    EY-4832: Støtte flere tilbakekrevingsbeløper i frontend

--- apps/etterlatte-saksbehandling-ui/client/src/components/tilbakekreving/utbetalinger/TilbakekrevingVurderingPerioderVisning.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/tilbakekreving/utbetalinger/TilbakekrevingVurderingPerioderVisning.tsx
@@ -8,6 +8,7 @@ import React, { useState } from 'react'
 import { Box, Button, HStack, Table } from '@navikt/ds-react'
 import { useNavigate } from 'react-router'
 import { formaterMaanedDato } from '~utils/formatering/dato'
+import { klasseTypeYtelse, leggPaaOrginalIndex } from '~components/tilbakekreving/tilbakekreving'
 
 export function TilbakekrevingVurderingPerioderVisning({ behandling }: { behandling: TilbakekrevingBehandling }) {
   const navigate = useNavigate()
@@ -33,24 +34,28 @@ export function TilbakekrevingVurderingPerioderVisning({ behandling }: { behandl
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {perioder.map((periode, index) => {
-            const beloeper = periode.ytelse
-            return (
-              <Table.Row key={'beloeperRad' + index}>
-                <Table.DataCell key="maaned">{formaterMaanedDato(periode.maaned)}</Table.DataCell>
-                <Table.DataCell key="bruttoUtbetaling">{beloeper.bruttoUtbetaling} kr</Table.DataCell>
-                <Table.DataCell key="nyBruttoUtbetaling">{beloeper.nyBruttoUtbetaling} kr</Table.DataCell>
-                <Table.DataCell key="beregnetFeilutbetaling">{beloeper.beregnetFeilutbetaling} kr</Table.DataCell>
-                <Table.DataCell key="skatteprosent">{beloeper.skatteprosent} %</Table.DataCell>
-                <Table.DataCell key="bruttoTilbakekreving">{beloeper.bruttoTilbakekreving} kr</Table.DataCell>
-                <Table.DataCell key="nettoTilbakekreving">{beloeper.nettoTilbakekreving} kr</Table.DataCell>
-                <Table.DataCell key="skatt">{beloeper.skatt} kr</Table.DataCell>
-                <Table.DataCell key="skyld">{teksterTilbakekrevingSkyld[beloeper.skyld!!]}</Table.DataCell>
-                <Table.DataCell key="resultat">{teksterTilbakekrevingResultat[beloeper.resultat!!]}</Table.DataCell>
-                <Table.DataCell key="tilbakekrevingsprosent">{beloeper.tilbakekrevingsprosent} %</Table.DataCell>
-                <Table.DataCell key="rentetillegg">{beloeper.rentetillegg} kr</Table.DataCell>
-              </Table.Row>
-            )
+          {perioder.map((periode, indexPeriode) => {
+            return periode.tilbakekrevingsbeloep
+              .map(leggPaaOrginalIndex)
+              .filter(klasseTypeYtelse)
+              .map((beloeper) => {
+                return (
+                  <Table.Row key={`beloeperRad-${indexPeriode}-${beloeper.originalIndex}`}>
+                    <Table.DataCell key="maaned">{formaterMaanedDato(periode.maaned)}</Table.DataCell>
+                    <Table.DataCell key="bruttoUtbetaling">{beloeper.bruttoUtbetaling} kr</Table.DataCell>
+                    <Table.DataCell key="nyBruttoUtbetaling">{beloeper.nyBruttoUtbetaling} kr</Table.DataCell>
+                    <Table.DataCell key="beregnetFeilutbetaling">{beloeper.beregnetFeilutbetaling} kr</Table.DataCell>
+                    <Table.DataCell key="skatteprosent">{beloeper.skatteprosent} %</Table.DataCell>
+                    <Table.DataCell key="bruttoTilbakekreving">{beloeper.bruttoTilbakekreving} kr</Table.DataCell>
+                    <Table.DataCell key="nettoTilbakekreving">{beloeper.nettoTilbakekreving} kr</Table.DataCell>
+                    <Table.DataCell key="skatt">{beloeper.skatt} kr</Table.DataCell>
+                    <Table.DataCell key="skyld">{teksterTilbakekrevingSkyld[beloeper.skyld!!]}</Table.DataCell>
+                    <Table.DataCell key="resultat">{teksterTilbakekrevingResultat[beloeper.resultat!!]}</Table.DataCell>
+                    <Table.DataCell key="tilbakekrevingsprosent">{beloeper.tilbakekrevingsprosent} %</Table.DataCell>
+                    <Table.DataCell key="rentetillegg">{beloeper.rentetillegg} kr</Table.DataCell>
+                  </Table.Row>
+                )
+              })
           })}
         </Table.Body>
       </Table>

080675e00a803da4522d95e199e1e92a6cc5ff10
Bjørn Golberg <bjorn.golberg@nav.no>
2024-12-19 13:37:34 +0100

    Ny regel for avkorting - REGEL-AVKORTET-YTELSE-MAKS-MIN

--- apps/etterlatte-beregning/src/test/kotlin/avkorting/regler/AvkortetYtelseTest.kt
+++ apps/etterlatte-beregning/src/test/kotlin/avkorting/regler/AvkortetYtelseTest.kt
@@ -2,6 +2,7 @@ package no.nav.etterlatte.beregning.regler.avkorting.regler
 
 import io.kotest.matchers.shouldBe
 import no.nav.etterlatte.avkorting.regler.avkortetYtelseMedRestanse
+import no.nav.etterlatte.avkorting.regler.avkortetYtelseMedRestanseMinMaks
 import no.nav.etterlatte.beregning.regler.avkortetYtelseGrunnlag
 import no.nav.etterlatte.libs.regler.RegelPeriode
 import org.junit.jupiter.api.Test
@@ -19,12 +20,22 @@ class AvkortetYtelseTest {
     }
 
     @Test
-    fun `avkorteYtelse skal bli 0 dersom avkortingsbeloep er stoerre enn beregnet ytelse`() {
+    fun `avkorteYtelse skal bli 0 dersom avkortingsbeloep eller restanse er stoerre enn beregnet ytelse`() {
         val avkortetYtelse =
-            avkortetYtelseMedRestanse.anvend(
+            avkortetYtelseMedRestanseMinMaks.anvend(
                 avkortetYtelseGrunnlag(beregning = 50000, avkorting = 100000),
                 RegelPeriode(LocalDate.of(2024, 1, 1)),
             )
         avkortetYtelse.verdi shouldBe 0
+    }
+
+    @Test
+    fun `avkorteYtelse skal bli maks beregningsbeloep dersom ytelse etter restanse er stoerre enn beregnet ytelse`() {
+        val avkortetYtelse =
+            avkortetYtelseMedRestanseMinMaks.anvend(
+                avkortetYtelseGrunnlag(beregning = 50000, avkorting = 25000, fordeltRestanse = -25001),
+                RegelPeriode(LocalDate.of(2024, 1, 1)),
+            )
+        avkortetYtelse.verdi shouldBe 50000
     }
 }

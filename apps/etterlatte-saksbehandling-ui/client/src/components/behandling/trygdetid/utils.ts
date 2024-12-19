1216531e0f4e76d3d61ece666b0ebd75a581b7e1
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-12-19 17:06:41 +0100

    Disse snek seg tilbake igjen :O

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/utils.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/utils.ts
@@ -2,18 +2,6 @@ import { IBehandlingsType, IDetaljertBehandling } from '~shared/types/IDetaljert
 import { Revurderingaarsak } from '~shared/types/Revurderingaarsak'
 
 export const skalViseTrygdeavtale = (behandling: IDetaljertBehandling): boolean => {
-  console.log(
-    'behandling.boddEllerArbeidetUtlandet?.vurdereAvoededsTrygdeavtale: ',
-    behandling.boddEllerArbeidetUtlandet?.vurdereAvoededsTrygdeavtale
-  )
-  console.log(
-    'behandling.behandlingType === IBehandlingsType.REVURDERING: ',
-    behandling.behandlingType === IBehandlingsType.REVURDERING
-  )
-  console.log(
-    'behandling.revurderingsaarsak === Revurderingaarsak.SLUTTBEHANDLING_UTLAND: ',
-    behandling.revurderingsaarsak === Revurderingaarsak.SLUTTBEHANDLING_UTLAND
-  )
   return (
     behandling.boddEllerArbeidetUtlandet?.vurdereAvoededsTrygdeavtale ||
     (behandling.behandlingType === IBehandlingsType.REVURDERING &&

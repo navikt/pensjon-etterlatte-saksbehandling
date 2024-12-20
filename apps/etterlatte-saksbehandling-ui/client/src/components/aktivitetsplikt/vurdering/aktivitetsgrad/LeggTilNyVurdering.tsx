7e961e6e32d1d9e33f1561244be6fac07df348e5
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-12-20 13:10:01 +0100

    Fikse vurdering opprettelsetekst

--- apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/aktivitetsgrad/LeggTilNyVurdering.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/vurdering/aktivitetsgrad/LeggTilNyVurdering.tsx
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import { Box, Button, Heading, VStack } from '@navikt/ds-react'
 import { PlusIcon } from '@navikt/aksel-icons'
 import { useDispatch } from 'react-redux'
-import { IAktivitetspliktVurderingNyDto } from '~shared/types/Aktivitetsplikt'
+import { AktivitetspliktOppgaveType, IAktivitetspliktVurderingNyDto } from '~shared/types/Aktivitetsplikt'
 import { setAktivitetspliktVurdering } from '~store/reducers/Aktivitetsplikt12mnd'
 import { VurderingAktivitetsgradOgUnntak } from '~components/aktivitetsplikt/vurdering/VurderingAktivitetsgradOgUnntak'
+import { useAktivitetspliktOppgaveVurdering } from '~components/aktivitetsplikt/AktivitetspliktOppgaveVurderingRoutes'
 
 export function LeggTilNyVurdering(props: { doedsdato?: Date }) {
   const dispatch = useDispatch()
   const [leggerTilVurdering, setLeggerTilVurdering] = useState(false)
+  const { vurderingType } = useAktivitetspliktOppgaveVurdering()
 
   function oppdaterStateVedLagring(data: IAktivitetspliktVurderingNyDto) {
     dispatch(setAktivitetspliktVurdering(data))
@@ -28,7 +30,11 @@ export function LeggTilNyVurdering(props: { doedsdato?: Date }) {
   return (
     <Box paddingBlock="4 0" borderWidth="1 0 0 0" borderColor="border-subtle">
       <VStack gap="6">
-        <Heading size="medium">Vurdering av brukers aktivitet ved 12 måneder</Heading>
+        <Heading size="medium">
+          {vurderingType === AktivitetspliktOppgaveType.TOLV_MAANEDER
+            ? 'Vurdering av brukers aktivitet ved 12 måneder'
+            : 'Vurdering av brukers aktivitet ved 6 måneder'}
+        </Heading>
         <VurderingAktivitetsgradOgUnntak
           doedsdato={props.doedsdato}
           onSuccess={oppdaterStateVedLagring}

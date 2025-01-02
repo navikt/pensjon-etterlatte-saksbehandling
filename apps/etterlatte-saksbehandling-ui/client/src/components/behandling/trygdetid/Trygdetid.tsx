9fc3960e91b7927e5ecee5666ad048e4e598df69
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-01-02 13:39:53 +0100

    Ikke vis loader default for henting av trygdetid fra pesys (#6701)
    * Ikke vis loader default for henting av trygdetid fra pesys
    
    * Yolo

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/Trygdetid.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/Trygdetid.tsx
@@ -13,7 +13,7 @@ import { TrygdeAvtale } from './avtaler/TrygdeAvtale'
 import { IBehandlingStatus, IBehandlingsType } from '~shared/types/IDetaljertBehandling'
 import { IBehandlingReducer, oppdaterBehandlingsstatus } from '~store/reducers/BehandlingReducer'
 import { useAppDispatch } from '~store/Store'
-import { isFailure, isPending, mapApiResult } from '~shared/api/apiUtils'
+import { isFailure, isPending, mapResult } from '~shared/api/apiUtils'
 import { isFailureHandler } from '~shared/api/IsFailureHandler'
 import { behandlingErIverksatt } from '~components/behandling/felles/utils'
 import { VedtakResultat } from '~components/behandling/useVedtaksResultat'
@@ -162,13 +162,11 @@ export const Trygdetid = ({ redigerbar, behandling, vedtaksresultat, virkningsti
         {skalViseTrygdeavtale(behandling) && <TrygdeAvtale redigerbar={redigerbar} />}
         {kanHenteTrygdetidFraPesys && (
           <>
-            {mapApiResult(
-              sjekkOmAvodedHarTTIPesysStatus,
-              <Spinner label="Sjekker om avdøed har trygdetidsgrunnlag i Pesys" />,
-              () => (
-                <Alert variant="warning">Kunne ikke sjekke trygdetidsgrunnag i Pesys</Alert>
-              ),
-              (harTrygdetidsgrunnlagIPesys) => {
+            {mapResult(sjekkOmAvodedHarTTIPesysStatus, {
+              initial: null,
+              pending: <Spinner label="Sjekker om avdøed har trygdetidsgrunnlag i Pesys" />,
+              error: () => <Alert variant="warning">Kunne ikke sjekke trygdetidsgrunnag i Pesys</Alert>,
+              success: (harTrygdetidsgrunnlagIPesys) => {
                 return (
                   <>
                     {harTrygdetidsgrunnlagIPesys && (
@@ -189,8 +187,8 @@ export const Trygdetid = ({ redigerbar, behandling, vedtaksresultat, virkningsti
                     )}
                   </>
                 )
-              }
-            )}
+              },
+            })}
           </>
         )}
         {landListe && (

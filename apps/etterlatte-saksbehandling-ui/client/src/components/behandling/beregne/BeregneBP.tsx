d076ab4af9486369cef0eda53a59c48292903dbc
Bjørn Golberg <bjorn.golberg@nav.no>
2025-01-09 10:38:29 +0100

    Beregning overskrift dobbelt opp for BP

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/BeregneBP.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/BeregneBP.tsx
@@ -1,6 +1,4 @@
 import { behandlingErRedigerbar } from '../felles/utils'
-import { formaterDato } from '~utils/formatering/dato'
-import { useVedtaksResultat } from '../useVedtaksResultat'
 import { useAppDispatch, useAppSelector } from '~store/Store'
 import { BehandlingRouteContext } from '../BehandlingRoutes'
 import React, { useContext, useEffect, useState } from 'react'
@@ -8,7 +6,7 @@ import { hentBeregning } from '~shared/api/beregning'
 import { IBehandlingReducer, oppdaterBehandlingsstatus, oppdaterBeregning } from '~store/reducers/BehandlingReducer'
 import Spinner from '~shared/Spinner'
 import { BehandlingHandlingKnapper } from '~components/behandling/handlinger/BehandlingHandlingKnapper'
-import { Alert, Box, Button, Heading, HStack } from '@navikt/ds-react'
+import { Alert, Box, Button, HStack } from '@navikt/ds-react'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { IBehandlingStatus, Vedtaksloesning } from '~shared/types/IDetaljertBehandling'
 import { NesteOgTilbake } from '../handlinger/NesteOgTilbake'
@@ -17,7 +15,6 @@ import { BarnepensjonSammendrag } from '~components/behandling/beregne/Barnepens
 import { fattVedtak, upsertVedtak } from '~shared/api/vedtaksvurdering'
 import { ApiErrorAlert } from '~ErrorBoundary'
 import { handlinger } from '~components/behandling/handlinger/typer'
-import { Vedtaksresultat } from '~components/behandling/felles/Vedtaksresultat'
 
 import { isPending, mapApiResult } from '~shared/api/apiUtils'
 import { isFailureHandler } from '~shared/api/IsFailureHandler'
@@ -33,12 +30,7 @@ export const BeregneBP = (props: { behandling: IBehandlingReducer }) => {
   const [beregning, hentBeregningRequest] = useApiCall(hentBeregning)
   const [vedtakStatus, oppdaterVedtakRequest] = useApiCall(upsertVedtak)
   const [visAttesteringsmodal, setVisAttesteringsmodal] = useState(false)
-  const virkningstidspunkt = behandling.virkningstidspunkt?.dato
-    ? formaterDato(behandling.virkningstidspunkt.dato)
-    : undefined
   const innloggetSaksbehandler = useInnloggetSaksbehandler()
-
-  const vedtaksresultat = useVedtaksResultat()
 
   const redigerbar = behandlingErRedigerbar(
     behandling.status,
@@ -88,12 +80,6 @@ export const BeregneBP = (props: { behandling: IBehandlingReducer }) => {
 
   return (
     <>
-      <Box paddingInline="16" paddingBlock="16 4">
-        <Heading spacing size="large" level="1">
-          Beregning og vedtak
-        </Heading>
-        <Vedtaksresultat vedtaksresultat={vedtaksresultat} virkningstidspunktFormatert={virkningstidspunkt} />
-      </Box>
       {erOpphoer ? (
         <Box paddingInline="18" paddingBlock="4">
           <Brevutfall behandling={behandling} resetBrevutfallvalidering={() => setManglerbrevutfall(false)} />

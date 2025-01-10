f2fcfbf2b4712f5426dfb654451c664b10b1c1c4
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-01-10 18:40:17 +0100

    Backend validering

--- apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/BrevService.kt
+++ apps/etterlatte-brev-api/src/main/kotlin/no/nav/etterlatte/brev/BrevService.kt
@@ -36,6 +36,7 @@ import no.nav.etterlatte.libs.ktor.token.BrukerTokenInfo
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.util.UUID
+import kotlin.collections.contains
 
 class BrevService(
     private val db: BrevRepository,
@@ -372,11 +373,22 @@ class BrevService(
             brevDataMapping = { ManueltBrevMedTittelData(it.innholdMedVedlegg.innhold(), it.tittel) },
         )
 
+    /*NB må kun brukes av route endepunktet for ferdigstilling med mindre sjekken mot brevkoder under flyttes et annet sted som ikke påvirker den flyten.
+    der brukes i dag routen ferdigstill-journalfoer-og-distribuer som kaller rett på lagrePdfOgFerdigstillBrev
+     */
     suspend fun ferdigstill(
         id: BrevID,
         bruker: BrukerTokenInfo,
     ) {
         val brev = sjekkOmBrevKanEndres(id)
+        if (listOf(
+                Brevkoder.OMSTILLINGSSTOENAD_AKTIVITETSPLIKT_INFORMASJON_10MND_INNHOLD,
+                Brevkoder.OMSTILLINGSSTOENAD_AKTIVITETSPLIKT_INFORMASJON_6MND_INNHOLD,
+            ).contains(brev.brevkoder)
+        ) {
+            throw BrevKanIkkeEndres(brev)
+            // se https://jira.adeo.no/browse/FAGSYSTEM-363686?atlLinkOrigin=c2xhY2staW50ZWdyYXRpb258aXNzdWU%3D
+        }
 
         if (brev.mottakere.size !in 1..2) {
             logger.error("Brev ${brev.id} har ${brev.mottakere.size} mottakere. Dette skal ikke være mulig...")

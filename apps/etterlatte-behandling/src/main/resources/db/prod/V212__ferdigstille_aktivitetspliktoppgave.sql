ed37d44d5bf0cf95d14a5d45191cd4a8c5e0bd54
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-01-10 18:25:17 +0100

    Sb distribuerte i eget brev vindu fra sak ikke via aktivitetspliktflyt (#6757)
    * Sb distribuerte i eget brev vindu fra sak ikke via aktivitetspliktflyt
    
    * Rename V212_ferdigstille_aktivitetspliktoppgave.sql to V212__ferdigstille_aktivitetspliktoppgave.sql

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V212__ferdigstille_aktivitetspliktoppgave.sql
@@ -0,0 +1,4 @@
+update oppgave set status = 'FERDIGSTILT' where id = '3d87fe2a-0ac3-44b6-99e6-accddc5fa622';
+/*
+se https://jira.adeo.no/browse/FAGSYSTEM-363686?atlLinkOrigin=c2xhY2staW50ZWdyYXRpb258aXNzdWU%3D
+ */

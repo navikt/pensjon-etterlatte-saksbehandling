dd2c3208de0c143ee5a06c0c7696d3f46cbd0a69
Henrik Gundersen <henrik.gundersen@nav.no>
2025-01-15 15:39:33 +0100

    Forbedrer logging i etterlatte-hendelser-ufoere og fikser integrasjonstest

--- apps/etterlatte-hendelser-ufoere/src/test/kotlin/hendelserufoere/UfoereHendelseFordelerTest.kt
+++ apps/etterlatte-hendelser-ufoere/src/test/kotlin/hendelserufoere/UfoereHendelseFordelerTest.kt
@@ -5,6 +5,7 @@ import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import no.nav.etterlatte.BehandlingKlient
+import no.nav.etterlatte.libs.testdata.grunnlag.SOEKER_FOEDSELSNUMMER
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -24,7 +25,7 @@ internal class UfoereHendelseFordelerTest {
 
         val ufoereHendelse =
             UfoereHendelse(
-                personIdent = "12312312312",
+                personIdent = SOEKER_FOEDSELSNUMMER.value,
                 virkningsdato = LocalDate.parse("2018-01-01"),
                 fodselsdato = LocalDate.parse("2000-01-01"),
                 vedtaksType = VedtaksType.INNV,
@@ -38,12 +39,12 @@ internal class UfoereHendelseFordelerTest {
     }
 
     @Test
-    fun `skal håndtere ufoerehendelse der bruker fyller 21 på virkningstidspunkt`() {
+    fun `skal håndtere ufoerehendelse der bruker fyller 21 i måneden for virkningstidspunkt`() {
         coEvery { behandlingKlient.postTilBehandling(any()) } returns Unit
 
         val ufoereHendelse =
             UfoereHendelse(
-                personIdent = "12312312312",
+                personIdent = SOEKER_FOEDSELSNUMMER.value,
                 virkningsdato = LocalDate.parse("2021-01-01"),
                 fodselsdato = LocalDate.parse("2000-01-01"),
                 vedtaksType = VedtaksType.INNV,
@@ -57,17 +58,36 @@ internal class UfoereHendelseFordelerTest {
     }
 
     @Test
-    fun `skal ignorere ufoerehendelse der bruker ikke er mellom og 18 og 21 på virkningstidspunkt`() {
+    fun `skal ignorere ufoerehendelse der bruker er 21 år og 1 mnd på virkningstidspunkt`() {
         val ufoereHendelse =
             UfoereHendelse(
-                personIdent = "12312312312",
-                virkningsdato = LocalDate.parse("2021-01-01"),
-                fodselsdato = LocalDate.parse("1980-01-01"),
+                personIdent = SOEKER_FOEDSELSNUMMER.value,
+                virkningsdato = LocalDate.parse("2021-02-01"),
+                fodselsdato = LocalDate.parse("2000-01-01"),
                 vedtaksType = VedtaksType.INNV,
             )
 
         runBlocking {
             ufoereHendelseFordeler.haandterHendelse(ufoereHendelse)
         }
+
+        coVerify(exactly = 0) { behandlingKlient.postTilBehandling(any()) }
+    }
+
+    @Test
+    fun `skal ignorere ufoerehendelse der bruker er 17 år og 11 mnd på virkningstidspunkt`() {
+        val ufoereHendelse =
+            UfoereHendelse(
+                personIdent = SOEKER_FOEDSELSNUMMER.value,
+                virkningsdato = LocalDate.parse("2017-12-01"),
+                fodselsdato = LocalDate.parse("2000-01-01"),
+                vedtaksType = VedtaksType.INNV,
+            )
+
+        runBlocking {
+            ufoereHendelseFordeler.haandterHendelse(ufoereHendelse)
+        }
+
+        coVerify(exactly = 0) { behandlingKlient.postTilBehandling(any()) }
     }
 }

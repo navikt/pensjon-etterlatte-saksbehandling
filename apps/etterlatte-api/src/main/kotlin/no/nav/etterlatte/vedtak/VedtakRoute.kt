d5d46d3a3abc535ad59a0e6febafdd260c34e136
Bjørn Golberg <bjorn.golberg@nav.no>
2025-01-16 10:26:35 +0100

    Autorisering

--- apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/vedtak/VedtakRoute.kt
+++ apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/vedtak/VedtakRoute.kt
@@ -3,13 +3,16 @@ package no.nav.etterlatte.vedtak
 import com.typesafe.config.Config
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondNullable
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import no.nav.etterlatte.AuthorizationPlugin
 import no.nav.etterlatte.libs.common.vedtak.VedtakForPersonRequest
+import no.nav.etterlatte.libs.ktor.token.Issuer
 
 fun Route.vedtakRoute(
     vedtaksvurderingKlient: VedtaksvurderingKlient,
@@ -17,8 +20,12 @@ fun Route.vedtakRoute(
 ) {
     // Tiltenkt for eksternt for etterlatte men internt i Nav. Initelt gjelder dette EESSI.
     route("api/vedtak/personident") {
+        install(AuthorizationPlugin) {
+            accessPolicyRolesEllerAdGrupper = setOf("vedtak-for-eksterne-i-nav")
+            issuers = setOf(Issuer.AZURE.issuerName)
+        }
+
         post {
-            // TODO mer autentisering?
             try {
                 val request = call.receive<VedtakForPersonRequest>()
                 val vedtak = vedtaksvurderingKlient.hentVedtak(request)

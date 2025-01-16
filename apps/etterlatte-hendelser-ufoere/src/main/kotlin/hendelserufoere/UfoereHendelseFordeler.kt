7c554ff7f3069307d4f2a82eb64a6bbde8da0275
Henrik Gundersen <henrik.gundersen@nav.no>
2025-01-16 10:36:41 +0100

    Forbedrer logging i etterlatte-hendelser-ufoere og fikser integrasjonstest (#6777)

--- apps/etterlatte-hendelser-ufoere/src/main/kotlin/hendelserufoere/UfoereHendelseFordeler.kt
+++ apps/etterlatte-hendelser-ufoere/src/main/kotlin/hendelserufoere/UfoereHendelseFordeler.kt
@@ -1,7 +1,9 @@
 package no.nav.etterlatte.hendelserufoere
 
 import no.nav.etterlatte.BehandlingKlient
+import no.nav.etterlatte.libs.common.logging.sikkerlogger
 import no.nav.etterlatte.libs.common.person.maskerFnr
+import no.nav.etterlatte.libs.common.toJson
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.temporal.ChronoUnit
@@ -13,37 +15,52 @@ class UfoereHendelseFordeler(
 
     suspend fun haandterHendelse(hendelse: UfoereHendelse) {
         try {
-            val attenAarIMaaneder = 12 * 18
-            val tjueenAarIMaaneder = 12 * 21
+            logger.info("Mottok hendelse for vedtak av type ${hendelse.vedtaksType} for bruker ${hendelse.personIdent.maskerFnr()}")
+            sikkerlogger().info("Mottok hendelse for vedtak av type ${hendelse.vedtaksType} for bruker ${hendelse.personIdent}")
 
-            val alderVedVirkningstidspunkt = ChronoUnit.MONTHS.between(hendelse.fodselsdato, hendelse.virkningsdato)
+            val alderIMaanederVedVirkningstidspunkt = ChronoUnit.MONTHS.between(hendelse.fodselsdato, hendelse.virkningsdato)
+            val alderIAarVedVirkningstidspunkt = ChronoUnit.YEARS.between(hendelse.fodselsdato, hendelse.virkningsdato)
 
-            if (alderVedVirkningstidspunkt in attenAarIMaaneder..tjueenAarIMaaneder) {
-                logger.info("Bruker er mellom 18 og 21 på på virkningstidspunktet. Sender ufoerehendelse til behandling")
-                behandlingKlient.postTilBehandling(
-                    ufoereHendelse = hendelse,
+            // Skal inkludere personer fra og med måneden de fyller 18 år og til og med måneden de fyller 21 år
+            if (alderIMaanederVedVirkningstidspunkt in ATTEN_AAR_I_MAANEDER..TJUEEN_AAR_I_MAANEDER) {
+                logger.info(
+                    "Bruker er mellom 18 og 21 år på virkningstidspunktet (alderIAarVedVirkningstidspunkt = " +
+                        "$alderIAarVedVirkningstidspunkt, alderIMaanederVedVirkningstidspunkt = " +
+                        "$alderIMaanederVedVirkningstidspunkt). Hendelse sendes videre til behandling for sjekk mot " +
+                        "løpende barnepensjonssaker.",
                 )
+                behandlingKlient.postTilBehandling(hendelse)
             } else {
                 logger.info(
-                    "Ufoerehendelse med personidentifikator=${hendelse.personIdent.maskerFnr()} " +
-                        "og alderVedVirkningstidspunkt=$alderVedVirkningstidspunkt " +
-                        "er ikke innenfor aldersgruppen 18-21. Hendelsen blir ikke sendt videre.",
+                    "Hendelse ignoreres da bruker ikke er innenfor intervallet som kvalifiserer for barnepensjon " +
+                        "(alderIAarVedVirkningstidspunkt = $alderIAarVedVirkningstidspunkt, " +
+                        "alderIMaanederVedVirkningstidspunkt = $alderIMaanederVedVirkningstidspunkt).",
                 )
             }
         } catch (e: Exception) {
-            loggFeilVedHaandteringAvHendelse(hendelse.vedtaksType.name, e)
+            loggFeilVedHaandteringAvHendelse(hendelse, e)
             throw e
         }
     }
 
     private fun loggFeilVedHaandteringAvHendelse(
-        hendelsestype: String?,
+        hendelse: UfoereHendelse,
         e: Exception,
     ) {
         logger.error(
-            "Kunne ikke haandtere $hendelsestype. Dette skyldes sannsynligvis at " +
-                "ufoerehendelsen ser annerledes ut enn forventet.",
+            "Kunne ikke håndtere hendelse for ${hendelse.personIdent.maskerFnr()}. " +
+                "Dette skyldes sannsynligvis at hendelsen ser annerledes ut enn forventet. Se sikkerlogg for detaljer.",
             e,
         )
+        sikkerlogger().error(
+            "Kunne ikke håndtere hendelse for ${hendelse.personIdent}. Dette skyldes sannsynligvis at " +
+                "hendelsen ser annerledes ut enn forventet: ${hendelse.toJson()}",
+            e,
+        )
+    }
+
+    private companion object {
+        private const val ATTEN_AAR_I_MAANEDER = 12 * 18
+        private const val TJUEEN_AAR_I_MAANEDER = 12 * 21
     }
 }

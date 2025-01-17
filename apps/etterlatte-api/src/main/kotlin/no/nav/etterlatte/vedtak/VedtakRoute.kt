469d51f91f7944e799b4669720190d1f05293258
Bjørn Golberg <bjorn.golberg@nav.no>
2025-01-17 09:10:55 +0100

    Endre url og rolle for generelt vedtak api

--- apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/vedtak/VedtakRoute.kt
+++ apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/vedtak/VedtakRoute.kt
@@ -1,6 +1,5 @@
 package no.nav.etterlatte.vedtak
 
-import com.typesafe.config.Config
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.application.install
@@ -14,14 +13,11 @@ import no.nav.etterlatte.AuthorizationPlugin
 import no.nav.etterlatte.libs.common.vedtak.VedtakForPersonRequest
 import no.nav.etterlatte.libs.ktor.token.Issuer
 
-fun Route.vedtakRoute(
-    vedtaksvurderingKlient: VedtaksvurderingKlient,
-    config: Config,
-) {
+fun Route.vedtakRoute(vedtaksvurderingKlient: VedtaksvurderingKlient) {
     // Tiltenkt for eksternt for etterlatte men internt i Nav. Initelt gjelder dette EESSI.
-    route("api/vedtak/personident") {
+    route("api/v1/vedtak") {
         install(AuthorizationPlugin) {
-            accessPolicyRolesEllerAdGrupper = setOf("vedtak-for-eksterne-i-nav")
+            accessPolicyRolesEllerAdGrupper = setOf("les-bp-vedtak", "les-oms-vedtak")
             issuers = setOf(Issuer.AZURE.issuerName)
         }
 

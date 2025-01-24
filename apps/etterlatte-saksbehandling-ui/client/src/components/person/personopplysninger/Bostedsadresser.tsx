f2c288e4b5ff25c0287dae5a4fe46475d2894b1c
Emil Schrøder <emil.schroder@nav.no>
2025-01-24 16:03:58 +0100

    Sorter bostedsadresse slik at nyeste kommer på toppen

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/personopplysninger/Bostedsadresser.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/personopplysninger/Bostedsadresser.tsx
@@ -5,8 +5,16 @@ import { Heading, Table } from '@navikt/ds-react'
 import { formaterDato } from '~utils/formatering/dato'
 import { BostedsadresseDataCell } from '~components/person/personopplysninger/components/BostedsadresseDataCell'
 import { Bostedsadresse } from '~shared/types/familieOpplysninger'
+import { compareDesc } from 'date-fns'
 
 export const Bostedsadresser = ({ bostedsadresse }: { bostedsadresse?: Bostedsadresse[] }): ReactNode => {
+  const sortedBostedsadresse = bostedsadresse?.sort((a: Bostedsadresse, b: Bostedsadresse) => {
+    if (a.gyldigFraOgMed && b.gyldigFraOgMed) {
+      return compareDesc(new Date(a.gyldigFraOgMed), new Date(b.gyldigFraOgMed))
+    }
+    return 0
+  })
+
   return (
     <Personopplysning heading="Bostedsadresser" icon={<HouseIcon aria-hidden />}>
       <Table>
@@ -18,9 +26,9 @@ export const Bostedsadresser = ({ bostedsadresse }: { bostedsadresse?: Bostedsad
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {!!bostedsadresse?.length ? (
+          {!!sortedBostedsadresse?.length ? (
             <>
-              {bostedsadresse.map((adresse: Bostedsadresse, index: number) => (
+              {sortedBostedsadresse.map((adresse: Bostedsadresse, index: number) => (
                 <Table.Row key={index}>
                   <BostedsadresseDataCell bostedsadresse={bostedsadresse} index={index} visAktiv />
                   <Table.DataCell>

068d7c0acc0f35a984c89b7fb754cfc572dd1e96
Bjørn Golberg <bjorn.golberg@nav.no>
2025-01-27 10:18:57 +0100

    Rydde i test

--- apps/etterlatte-vedtaksvurdering/src/test/kotlin/no/nav/etterlatte/vedtaksvurdering/VedtaksvurderingRepositoryTest.kt
+++ apps/etterlatte-vedtaksvurdering/src/test/kotlin/no/nav/etterlatte/vedtaksvurdering/VedtaksvurderingRepositoryTest.kt
@@ -470,15 +470,15 @@ internal class VedtaksvurderingRepositoryTest(
     fun `skal hente vedtak for fnr selv om ikke saktype er spesifisert`() {
         val soeker1 = Folkeregisteridentifikator.of(FNR_1)
 
-        listOf(
+        val vedtak =
             opprettVedtak(
                 sakId = SakId(10),
                 soeker = soeker1,
                 virkningstidspunkt = YearMonth.of(2024, Month.JANUARY),
                 status = VedtakStatus.IVERKSATT,
-            ),
-        ).map { repository.opprettVedtak(it) }
-            .forEach { repository.iverksattVedtak(it.behandlingId) }
+            )
+        repository.opprettVedtak(vedtak)
+        repository.iverksattVedtak(vedtak.behandlingId)
 
         val results = repository.hentFerdigstilteVedtak(soeker1)
 

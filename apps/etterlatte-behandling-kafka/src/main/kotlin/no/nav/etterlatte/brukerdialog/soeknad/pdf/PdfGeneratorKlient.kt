62011a00ee80bf7f35f2b55af63bc02f77a2f9fc
Nicolas N. <Watercolours@users.noreply.github.com>
2025-01-28 09:29:20 +0100

    EY-4966 Flytte gyldig-soeknad til behandling-kafka (#6867)
    * EY-4966 Flytte gyldig-soeknad til behandling-kafka
    
    * oppdatere readme

---
+++ apps/etterlatte-behandling-kafka/src/main/kotlin/no/nav/etterlatte/brukerdialog/soeknad/pdf/PdfGeneratorKlient.kt
@@ -0,0 +1,42 @@
+package no.nav.etterlatte.brukerdialog.soeknad.pdf
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.content.TextContent
+import io.ktor.http.contentType
+import no.nav.etterlatte.libs.common.innsendtsoeknad.common.PDFMal
+import no.nav.etterlatte.libs.common.logging.CORRELATION_ID
+import no.nav.etterlatte.libs.common.logging.getCorrelationId
+import no.nav.etterlatte.libs.common.toJsonNode
+import org.slf4j.LoggerFactory
+
+class PdfGeneratorKlient(
+    private val klient: HttpClient,
+    private val apiUrl: String,
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    suspend fun genererPdf(
+        payload: PDFMal,
+        mal: String,
+    ) = genererPdf(TextContent(payload.toJsonNode().toPrettyString(), ContentType.Application.Json), mal, mal)
+
+    private suspend fun genererPdf(
+        body: Any,
+        mal: String,
+        sti: String,
+    ): ByteArray {
+        logger.info("Genererer PDF med ey-pdfgen (mal=$mal)")
+
+        return klient
+            .post("$apiUrl/$sti") {
+                header(CORRELATION_ID, getCorrelationId())
+                contentType(ContentType.Application.Json)
+                setBody(body)
+            }.body()
+    }
+}
---
+++ apps/etterlatte-behandling-kafka/src/test/kotlin/no/nav/etterlatte/brukerdialog/soeknad/client/BehandlingClientTest.kt
@@ -0,0 +1,72 @@
+package no.nav.etterlatte.brukerdialog.soeknad.client
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.engine.mock.toByteArray
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestData
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.headersOf
+import io.ktor.serialization.jackson.jackson
+import io.ktor.utils.io.ByteReadChannel
+import kotlinx.coroutines.runBlocking
+import no.nav.etterlatte.behandling.sakId1
+import no.nav.etterlatte.brukerdialog.soeknad.PersongalleriMapper
+import no.nav.etterlatte.libs.common.behandling.BehandlingsBehov
+import no.nav.etterlatte.libs.common.event.FordelerFordelt
+import no.nav.etterlatte.libs.common.innsendtsoeknad.barnepensjon.Barnepensjon
+import no.nav.etterlatte.libs.common.objectMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+internal class BehandlingClientTest {
+    @Test
+    fun testInitierBehandling() {
+        val behandlingId = UUID.randomUUID()
+        val requestList = mutableListOf<HttpRequestData>()
+
+        val behandlingClient = createBehandlingClient(requestList, behandlingId)
+        val hendelseJson = objectMapper.readTree(javaClass.getResource("/behandlingsbehov/barnepensjon.json")!!.readText())
+        val soeknad = objectMapper.treeToValue<Barnepensjon>(hendelseJson[FordelerFordelt.skjemaInfoKey])
+        val hentetSaksid =
+            behandlingClient.opprettBehandling(
+                sakId1,
+                soeknad.mottattDato,
+                PersongalleriMapper.hentPersongalleriFraSoeknad(soeknad),
+            )
+
+        assertEquals(behandlingId, hentetSaksid)
+        assertEquals(
+            sakId1,
+            objectMapper
+                .readValue<BehandlingsBehov>(
+                    (runBlocking { String(requestList[0].body.toByteArray()) }),
+                ).sakId,
+        )
+    }
+
+    private fun createBehandlingClient(
+        requestList: MutableList<HttpRequestData>,
+        randomUUID: UUID,
+    ): BehandlingClient {
+        val httpClient =
+            HttpClient(
+                MockEngine { request ->
+                    requestList.add(request)
+                    respond(
+                        content = ByteReadChannel(randomUUID.toString()),
+                        status = HttpStatusCode.OK,
+                        headers = headersOf(HttpHeaders.ContentType, "text/plain"),
+                    )
+                },
+            ) {
+                install(ContentNegotiation) { jackson {} }
+            }
+        return BehandlingClient(httpClient, "")
+    }
+}

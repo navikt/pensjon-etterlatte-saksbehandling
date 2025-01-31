5cbc5dacc2d4c4abf05d211f596aafe2d14f8713
Nicolas N. <Watercolours@users.noreply.github.com>
2025-01-31 10:01:53 +0100

    EY-4834 Fikse mottakerType på barn (#6907)
    * EY-4834 Fikse mottakerType på barn
    
    * korrigere logg og respons ved feil
    
    * korrigere docker-compose

--- apps/etterlatte-pdltjenester/src/main/kotlin/person/PersonRoute.kt
+++ apps/etterlatte-pdltjenester/src/main/kotlin/person/PersonRoute.kt
@@ -6,13 +6,16 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import no.nav.etterlatte.libs.common.feilhaandtering.IkkeFunnetException
 import no.nav.etterlatte.libs.common.person.HentAdressebeskyttelseRequest
 import no.nav.etterlatte.libs.common.person.HentGeografiskTilknytningRequest
 import no.nav.etterlatte.libs.common.person.HentPdlIdentRequest
 import no.nav.etterlatte.libs.common.person.HentPersonHistorikkForeldreAnsvarRequest
 import no.nav.etterlatte.libs.common.person.HentPersonRequest
 import no.nav.etterlatte.libs.common.person.HentPersongalleriRequest
+import no.nav.etterlatte.libs.ktor.route.kunSaksbehandler
 import no.nav.etterlatte.libs.ktor.route.medBody
+import no.nav.etterlatte.sikkerLogg
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("PersonRoute")
@@ -72,6 +75,20 @@ fun Route.personRoute(service: PersonService) {
         val identer = service.hentPdlFolkeregisterIdenter(request)
 
         call.respond(identer)
+    }
+
+    post("foedselsdato") {
+        kunSaksbehandler {
+            val request = call.receive<HentPdlIdentRequest>()
+
+            val foedselsdato =
+                service.hentFoedselsdato(request.ident.value).foedselsdato
+                    ?: throw IkkeFunnetException("IKKE_FUNNET", "Fant ingen fødselsdato for bruker (se sikkerlogg)")
+
+            sikkerLogg.error("Fant ingen fødselsdato i PDL for ident=${request.ident.value}")
+
+            call.respond(foedselsdato)
+        }
     }
 
     route("aktoerid") {

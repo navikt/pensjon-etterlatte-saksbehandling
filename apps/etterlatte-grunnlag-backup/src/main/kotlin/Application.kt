afdf71cb8bff5d87b3154ed3868b6768b5c893f7
Nicolas N <nicolas.nordhagen@nav.no>
2025-02-03 08:43:06 +0100

    nytt forsøk uten custom restModule

--- apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
+++ apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
@@ -1,12 +1,29 @@
 package no.nav.etterlatte
 
 import com.typesafe.config.ConfigFactory
-import no.nav.etterlatte.libs.ktor.initialisering.initEmbeddedServerUtenRest
+import io.ktor.server.cio.CIO
+import io.ktor.server.config.HoconApplicationConfig
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.routing.routing
+import no.nav.etterlatte.libs.ktor.healthApi
+import no.nav.etterlatte.libs.ktor.ktor.shutdownPolicyEmbeddedServer
 
 fun main() {
     // sikre is_healthy / is_alive
-    initEmbeddedServerUtenRest(
-        httpPort = 8080,
-        applicationConfig = ConfigFactory.load(),
+    embeddedServer(
+        configure = shutdownPolicyEmbeddedServer(),
+        factory = CIO,
+        environment =
+            applicationEngineEnvironment {
+                config = HoconApplicationConfig(ConfigFactory.load())
+                module {
+                    routing {
+                        healthApi()
+                    }
+                }
+                connector { port = 8080 }
+            },
     )
 }

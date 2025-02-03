891581dc81f5c3109e26e2ebd5156f9fe5e721e7
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-03 18:54:11 +0100

    Bedre feilhåndtering

--- apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
@@ -1,11 +1,16 @@
 do $body$
 DECLARE
-antallSluttbehandlingerEndret integer = 0;
+antallfeil integer = 0;
+antallSluttbehandling integer = 0;
+antallSluttbehandlingerRiktig integer = 0;
 BEGIN
+    antallfeil := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING_UTLAND');
+    antallSluttbehandling := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
+
     update revurdering_info set info = jsonb_set(info, '{type}', '"SLUTTBEHANDLING"'::JSONB) where info->>'type' = 'SLUTTBEHANDLING_UTLAND';
 
-    antallSluttbehandlingerEndret := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
-    if antallSluttbehandlingerEndret = 11 then
+    antallSluttbehandlingerRiktig := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
+    if (antallfeil + antallSluttbehandling) = antallSluttbehandlingerRiktig then
         commit;
     else
         RAISE EXCEPTION 'Feil antall oppdatert, committer ikke migrering';

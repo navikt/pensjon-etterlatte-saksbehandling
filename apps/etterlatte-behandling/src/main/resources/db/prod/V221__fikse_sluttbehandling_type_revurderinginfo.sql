efd809f68085b9c3b1883e74d5325186d3fefc5e
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-03 18:36:30 +0100

    Bruke strukturert oppdatering

--- apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
@@ -1,3 +1,15 @@
-update revurdering_info
-set info = jsonb_set(info, '{type}', '"SLUTTBEHANDLING"'::JSONB)
-where info->>'type' = 'SLUTTBEHANDLING_UTLAND';
+do $body$
+DECLARE
+antallSluttbehandlingerEndret integer;
+BEGIN
+    update revurdering_info set info = jsonb_set(info, '{type}', '"SLUTTBEHANDLING"'::JSONB) where info->>'type' = 'SLUTTBEHANDLING_UTLAND';
+
+    antallSluttbehandlingerEndret := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
+    if antallSluttbehandlingerEndret == 8 then
+        commit;
+    else
+        RAISE EXCEPTION 'Feil antall oppdatert, committer ikke migrering';
+    end if;
+END;
+    $body$
+LANGUAGE 'plpgsql';

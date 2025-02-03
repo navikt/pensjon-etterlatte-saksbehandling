b15c7da8b9921598239c031665833685d875e65b
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-03 18:37:06 +0100

    Riktig sammenligning av int

--- apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
@@ -5,7 +5,7 @@ BEGIN
     update revurdering_info set info = jsonb_set(info, '{type}', '"SLUTTBEHANDLING"'::JSONB) where info->>'type' = 'SLUTTBEHANDLING_UTLAND';
 
     antallSluttbehandlingerEndret := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
-    if antallSluttbehandlingerEndret == 8 then
+    if antallSluttbehandlingerEndret = 8 then
         commit;
     else
         RAISE EXCEPTION 'Feil antall oppdatert, committer ikke migrering';

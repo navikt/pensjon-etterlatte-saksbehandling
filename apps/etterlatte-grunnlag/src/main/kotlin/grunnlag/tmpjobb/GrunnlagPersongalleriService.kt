36f29d31441f64858973a96b8d47d102e0f02fd1
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-02-03 13:28:53 +0100

    Testkjoring med 100 for å sjekke performance (#6948)
    * Kjoer alle i prod
    
    * Kjoer 100
    
    * Flytt sjekken øverst

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/tmpjobb/GrunnlagPersongalleriService.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/tmpjobb/GrunnlagPersongalleriService.kt
@@ -13,6 +13,10 @@ class GrunnlagPersongalleriService(
         var count = 0
         logger.info("Kjører jobb for å hente tomme persongallerier")
         while (true) {
+            if (count == 100) {
+                logger.info("avsluttet etter 100 for å sjekke performance")
+                break
+            }
             val opplysningsid = grunnlagJobbDao.hentTommePersongalleriV1()
             if (opplysningsid == null) {
                 logger.info("Fant ingen flere persongallerier")

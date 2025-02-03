77bc854cd861ade0e55b19a4119aeb26dcc48dda
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-03 12:17:01 +0100

    Logg antall kjøringer

--- apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/tmpjobb/GrunnlagPersongalleriService.kt
+++ apps/etterlatte-grunnlag/src/main/kotlin/grunnlag/tmpjobb/GrunnlagPersongalleriService.kt
@@ -10,18 +10,20 @@ class GrunnlagPersongalleriService(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun kjoer() {
+        var count = 0
         logger.info("Kjører jobb for å hente tomme persongallerier")
         while (true) {
             val opplysningsid = grunnlagJobbDao.hentTommePersongalleriV1()
             if (opplysningsid == null) {
-                logger.info("Fant ingen flere PERSONGALLERIER")
+                logger.info("Fant ingen flere persongallerier")
                 break
             }
             logger.info("Henter data for opplysningsid $opplysningsid")
             val hentetDataForOpplysningId = grunnlagBackupKlient.hentPersonGallerierFraBackup(opplysningsid)
             grunnlagJobbDao.oppdaterTomtPersongalleri(hentetDataForOpplysningId)
+            count += 1
         }
 
-        logger.info("Avsluttet jobb, fant ingen flere tomme persongallerier")
+        logger.info("Avsluttet jobb, fant ingen flere tomme persongallerier, kjørte $count antall ganger")
     }
 }

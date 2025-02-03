57f5cbb07fda63c1369e48d9e12be0dacbb9ca79
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-03 18:51:21 +0100

    Skal bli 11

--- apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V221__fikse_sluttbehandling_type_revurderinginfo.sql
@@ -1,11 +1,11 @@
 do $body$
 DECLARE
-antallSluttbehandlingerEndret integer;
+antallSluttbehandlingerEndret integer = 0;
 BEGIN
     update revurdering_info set info = jsonb_set(info, '{type}', '"SLUTTBEHANDLING"'::JSONB) where info->>'type' = 'SLUTTBEHANDLING_UTLAND';
 
     antallSluttbehandlingerEndret := (select count(*) from revurdering_info where info->>'type' = 'SLUTTBEHANDLING');
-    if antallSluttbehandlingerEndret = 8 then
+    if antallSluttbehandlingerEndret = 11 then
         commit;
     else
         RAISE EXCEPTION 'Feil antall oppdatert, committer ikke migrering';

3eac9eac4c60591f05c8a4e96ed04ac243a34f27
Nicolas N <nicolas.nordhagen@nav.no>
2025-02-03 08:54:53 +0100

    fikse server

--- apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
+++ apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
@@ -1,23 +1,18 @@
 package no.nav.etterlatte
 
-import com.typesafe.config.ConfigFactory
 import io.ktor.server.cio.CIO
-import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.routing.routing
 import no.nav.etterlatte.libs.ktor.healthApi
-import no.nav.etterlatte.libs.ktor.ktor.shutdownPolicyEmbeddedServer
 
 fun main() {
     // sikre is_healthy / is_alive
     embeddedServer(
-        configure = shutdownPolicyEmbeddedServer(),
         factory = CIO,
         environment =
             applicationEngineEnvironment {
-                config = HoconApplicationConfig(ConfigFactory.load())
                 module {
                     routing {
                         healthApi()
@@ -25,5 +20,5 @@ fun main() {
                 }
                 connector { port = 8080 }
             },
-    )
+    ).start(wait = true)
 }

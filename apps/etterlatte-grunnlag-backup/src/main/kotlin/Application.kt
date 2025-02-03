40629c5eb8ac62672b72a132e853c3b9cd188fd1
Nicolas N. <Watercolours@users.noreply.github.com>
2025-02-03 09:49:19 +0100

    Endepunkt for å hente hendelse fra backup (#6942)

--- apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
+++ apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
@@ -1,15 +1,28 @@
 package no.nav.etterlatte
 
 import com.typesafe.config.ConfigFactory
+import no.nav.etterlatte.libs.common.Miljoevariabler
+import no.nav.etterlatte.libs.database.ApplicationProperties
+import no.nav.etterlatte.libs.database.DataSourceBuilder
 import no.nav.etterlatte.libs.ktor.initialisering.initEmbeddedServer
 import no.nav.etterlatte.libs.ktor.initialisering.run
 
 fun main() {
-    // sikre is_healthy / is_alive
+    val properties: ApplicationProperties = ApplicationProperties.fromEnv(Miljoevariabler.systemEnv())
+
+    val ds =
+        DataSourceBuilder.createDataSource(
+            jdbcUrl = properties.jdbcUrl,
+            username = properties.dbUsername,
+            password = properties.dbPassword,
+        )
+
     initEmbeddedServer(
         httpPort = 8080,
         applicationConfig = ConfigFactory.load(),
     ) {
-        // run
+        grunnlagRoute(
+            dao = OpplysningDao(ds),
+        )
     }.run()
 }

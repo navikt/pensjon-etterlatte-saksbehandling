043bbfd8ff1fbeb1ffbfba4035d509244f230bb5
Nicolas N <nicolas.nordhagen@nav.no>
2025-02-03 09:13:59 +0100

    nytt forsøk med azure

--- apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
+++ apps/etterlatte-grunnlag-backup/src/main/kotlin/Application.kt
@@ -1,24 +1,15 @@
 package no.nav.etterlatte
 
-import io.ktor.server.cio.CIO
-import io.ktor.server.engine.applicationEngineEnvironment
-import io.ktor.server.engine.connector
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.routing.routing
-import no.nav.etterlatte.libs.ktor.healthApi
+import com.typesafe.config.ConfigFactory
+import no.nav.etterlatte.libs.ktor.initialisering.initEmbeddedServer
+import no.nav.etterlatte.libs.ktor.initialisering.run
 
 fun main() {
     // sikre is_healthy / is_alive
-    embeddedServer(
-        factory = CIO,
-        environment =
-            applicationEngineEnvironment {
-                module {
-                    routing {
-                        healthApi()
-                    }
-                }
-                connector { port = 8080 }
-            },
-    ).start(wait = true)
+    initEmbeddedServer(
+        httpPort = 8080,
+        applicationConfig = ConfigFactory.load(),
+    ) {
+        // run
+    }.run()
 }

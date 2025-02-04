5e29704a7f3ad0d6f5ab906dba56e985c8b89770
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-02-04 11:29:11 +0100

    Legg inn bekreftelse på at meld inn endring er mottat i kafka

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveModal/MeldtInnEndringOppgaveModal.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveModal/MeldtInnEndringOppgaveModal.tsx
@@ -3,10 +3,17 @@ import { useInnloggetSaksbehandler } from '~components/behandling/useInnloggetSa
 import { useEffect, useState } from 'react'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { hentJournalpost } from '~shared/api/dokument'
-import { Alert, BodyShort, Button, HStack, Link, Modal, VStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Button, HStack, Modal, VStack } from '@navikt/ds-react'
 import { ExternalLinkIcon, EyeIcon } from '@navikt/aksel-icons'
 import { mapResult } from '~shared/api/apiUtils'
 import Spinner from '~shared/Spinner'
+import { Link } from 'react-router-dom'
+
+enum HandlingValgt {
+  AVSLUTT_OPPGAVE,
+  OPPRETT_REVURDERING,
+  INGEN,
+}
 
 interface Props {
   oppgave: OppgaveDTO
@@ -17,6 +24,7 @@ export const MeldtInnEndringOppgaveModal = ({ oppgave, oppdaterStatus }: Props) 
   const innloggetSaksbehandler = useInnloggetSaksbehandler()
 
   const [open, setOpen] = useState<boolean>(false)
+  const [handlingValgt, setHandlingValgt] = useState<HandlingValgt>(HandlingValgt.INGEN)
 
   const [hentJournalpostResult, hentJournalpostFetch] = useApiCall(hentJournalpost)
 
@@ -29,7 +37,7 @@ export const MeldtInnEndringOppgaveModal = ({ oppgave, oppdaterStatus }: Props) 
 
   return (
     <>
-      <Button variant="primary" icon={<EyeIcon aria-hidden />} onClick={() => setOpen(true)}>
+      <Button variant="primary" size="small" icon={<EyeIcon aria-hidden />} onClick={() => setOpen(true)}>
         Se oppgave
       </Button>
 
@@ -49,20 +57,20 @@ export const MeldtInnEndringOppgaveModal = ({ oppgave, oppdaterStatus }: Props) 
                     pending: <Spinner label="Henter journalpost..." />,
                     success: (journalpost) => (
                       <Alert variant="info">
-                        <HStack justify="space-between">
+                        <VStack gap="2">
                           <BodyShort>
                             Bruker har meldt inn en endring. Velg mellom å opprette en revurdering eller å avslutte
                             oppgaven dersom den ikke skal behandles
                           </BodyShort>
                           <Link
-                            href={`/api/dokumenter/${journalpost.journalpostId}/${journalpost.dokumenter[0].dokumentInfoId}`}
+                            to={`/api/dokumenter/${journalpost.journalpostId}/${journalpost.dokumenter[0].dokumentInfoId}`}
                             target="_blank"
                           >
-                            <Button as="a" icon={<ExternalLinkIcon aria-hidden />}>
+                            <Button as="a" icon={<ExternalLinkIcon aria-hidden />} size="small">
                               Vis (åpnes i ny fane)
                             </Button>
                           </Link>
-                        </HStack>
+                        </VStack>
                       </Alert>
                     ),
                   })}
@@ -76,11 +84,20 @@ export const MeldtInnEndringOppgaveModal = ({ oppgave, oppdaterStatus }: Props) 
                 Avbryt
               </Button>
               {kanRedigeres && erTildeltSaksbehandler && (
-                <HStack gap="4">
-                  {/* TODO: switche på skjema for avslutt oppgave og revurdering, ved revurdering må vi ha select og beskrivelse */}
-                  <Button variant="secondary">Avslutt oppgave</Button>
-                  <Button>Opprett revurdering</Button>
-                </HStack>
+                <>
+                  {handlingValgt === HandlingValgt.INGEN && (
+                    <HStack gap="4">
+                      <Button variant="secondary">Avslutt oppgave</Button>
+                      <Button>Opprett revurdering</Button>
+                    </HStack>
+                  )}
+                  {handlingValgt === HandlingValgt.AVSLUTT_OPPGAVE && (
+                    <HStack gap="4">
+                      <Button variant="secondary">Tilbake</Button>
+                      <Button>Avslutt</Button>
+                    </HStack>
+                  )}
+                </>
               )}
             </HStack>
           </VStack>

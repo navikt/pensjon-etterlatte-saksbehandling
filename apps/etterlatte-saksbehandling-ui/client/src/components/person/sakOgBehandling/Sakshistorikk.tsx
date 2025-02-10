935762391b433c79cf619e5bae577efff8033d30
Henrik Gundersen <henrik.gundersen@nav.no>
2025-02-10 14:55:04 +0100

    Return

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/Sakshistorikk.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/Sakshistorikk.tsx
@@ -47,33 +47,31 @@ export const Sakshistorikk = ({ sakId }: { sakId: number }) => {
 
   return mapResult(hentSaksendringerStatus, {
     pending: <Spinner label="Henter historikk..." />,
-    success: (sakshistorikk) => {
-      return (
-        <List as="ul" size="small">
-          {sakshistorikk.sort(sorterNyligsteFoerstOgBakover).map((endring) => (
-            <List.Item
-              title={tekstEndringstype[endring.endringstype]}
-              icon={<Ikon endringstype={endring.endringstype} />}
-              key={endring.id}
-            >
-              <Box maxWidth="18rem" borderWidth="0 0 1 0" paddingBlock="0 2" borderColor="border-subtle">
-                <BodyShort size="small">
-                  <Endringstekst endring={endring} />
-                </BodyShort>
-                {endring.kommentar && (
-                  <Box marginBlock="3 3">
-                    <BodyShort size="small">{endring.kommentar}</BodyShort>
-                  </Box>
-                )}
-                <Detail textColor="subtle">{formaterDatoMedKlokkeslett(endring.tidspunkt)}</Detail>
-                <Detail textColor="subtle">
-                  {endring.identtype == Identtype.SAKSBEHANDLER ? endring.ident : 'Gjenny (automatisk)'}
-                </Detail>
-              </Box>
-            </List.Item>
-          ))}
-        </List>
-      )
-    },
+    success: (sakshistorikk) => (
+      <List as="ul" size="small">
+        {sakshistorikk.sort(sorterNyligsteFoerstOgBakover).map((endring) => (
+          <List.Item
+            title={tekstEndringstype[endring.endringstype]}
+            icon={<Ikon endringstype={endring.endringstype} />}
+            key={endring.id}
+          >
+            <Box maxWidth="18rem" borderWidth="0 0 1 0" paddingBlock="0 2" borderColor="border-subtle">
+              <BodyShort size="small">
+                <Endringstekst endring={endring} />
+              </BodyShort>
+              {endring.kommentar && (
+                <Box marginBlock="3 3">
+                  <BodyShort size="small">{endring.kommentar}</BodyShort>
+                </Box>
+              )}
+              <Detail textColor="subtle">{formaterDatoMedKlokkeslett(endring.tidspunkt)}</Detail>
+              <Detail textColor="subtle">
+                {endring.identtype == Identtype.SAKSBEHANDLER ? endring.ident : 'Gjenny (automatisk)'}
+              </Detail>
+            </Box>
+          </List.Item>
+        ))}
+      </List>
+    ),
   })
 }

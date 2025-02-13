2a15eb1d1974e61177fb86676b52f9372aaa448d
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-02-13 09:19:42 +0100

    Opprydning i virkningstidspunkt

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/Soeknadoversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/soeknadsoversikt/Soeknadoversikt.tsx
@@ -15,15 +15,8 @@ import { BoddEllerArbeidetUtlandet } from '~components/behandling/soeknadsoversi
 import OppdaterGrunnlagModal from '~components/behandling/handlinger/OppdaterGrunnlagModal'
 import { SkalViseBosattUtland } from '~components/behandling/soeknadsoversikt/bosattUtland/SkalViseBosattUtland'
 import {
-  BP_FOERSTEGANGSBEHANDLING_BESKRIVELSE,
-  BP_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_BESKRIVELSE,
-  BP_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_HJEMLER,
-  BP_FOERSTEGANGSBEHANDLING_HJEMLER,
-  BP_FORELDRELOES_PAA_GAMMELT_REGELVERK_BEHANDLES_I_PESYS_BESKRIVELSE,
-  OMS_FOERSTEGANGSBEHANDLING_BESKRIVELSE,
-  OMS_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_BESKRIVELSE,
-  OMS_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_HJEMLER,
-  OMS_FOERSTEGANGSBEHANDLING_HJEMLER,
+  beskrivelseVirkningstidspunkt,
+  hjemlerVirkningstidspunkt,
 } from '~components/behandling/virkningstidspunkt/utils'
 import Virkningstidspunkt from '~components/behandling/virkningstidspunkt/Virkningstidspunkt'
 import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
@@ -45,37 +38,6 @@ export const Soeknadsoversikt = (props: { behandling: IDetaljertBehandling }) =>
   const personopplysninger = usePersonopplysninger()
   const erBosattUtland = behandling.utlandstilknytning?.type === UtlandstilknytningType.BOSATT_UTLAND
   const erForeldreloes = (personopplysninger?.avdoede || []).length >= 2
-
-  const hjemlerVirkningstidspunkt = (sakType: SakType, erBosattUtland: boolean) => {
-    switch (sakType) {
-      case SakType.BARNEPENSJON:
-        return erBosattUtland ? BP_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_HJEMLER : BP_FOERSTEGANGSBEHANDLING_HJEMLER
-      case SakType.OMSTILLINGSSTOENAD:
-        return erBosattUtland ? OMS_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_HJEMLER : OMS_FOERSTEGANGSBEHANDLING_HJEMLER
-    }
-  }
-
-  const beskrivelseVirkningstidspunkt = (sakType: SakType, erBosattUtland: boolean, erForeldreloes: boolean) => {
-    switch (sakType) {
-      case SakType.BARNEPENSJON:
-        return bpBeskrivelseVirkningstidspunkt(erBosattUtland, erForeldreloes)
-      case SakType.OMSTILLINGSSTOENAD:
-        return omsBeskrivelseVirkningstidspunkt(erBosattUtland)
-    }
-  }
-
-  function bpBeskrivelseVirkningstidspunkt(erBosattUtland: boolean, erForeldreloes: boolean) {
-    const standard = erBosattUtland
-      ? BP_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_BESKRIVELSE
-      : BP_FOERSTEGANGSBEHANDLING_BESKRIVELSE
-    return erForeldreloes ? standard + BP_FORELDRELOES_PAA_GAMMELT_REGELVERK_BEHANDLES_I_PESYS_BESKRIVELSE : standard
-  }
-
-  function omsBeskrivelseVirkningstidspunkt(erBosattUtland: boolean) {
-    return erBosattUtland
-      ? OMS_FOERSTEGANGSBEHANDLING_BOSATT_UTLAND_BESKRIVELSE
-      : OMS_FOERSTEGANGSBEHANDLING_BESKRIVELSE
-  }
 
   return (
     <>
@@ -131,7 +93,7 @@ export const Soeknadsoversikt = (props: { behandling: IDetaljertBehandling }) =>
               hjemler={hjemlerVirkningstidspunkt(behandling.sakType, erBosattUtland)}
               beskrivelse={beskrivelseVirkningstidspunkt(behandling.sakType, erBosattUtland, erForeldreloes)}
             >
-              {{ info: <GrunnlagForVirkningstidspunkt /> }}
+              <GrunnlagForVirkningstidspunkt />
             </Virkningstidspunkt>
             <ViderefoereOpphoer behandling={behandling} redigerbar={redigerbar} />
             <BoddEllerArbeidetUtlandet behandling={behandling} redigerbar={redigerbar} />

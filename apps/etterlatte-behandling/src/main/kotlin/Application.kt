b853d338881d6408e7de92912139560bc1d41b31
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2025-02-14 13:14:27 +0100

    Fjerner ryddejobb for feilopprettede revurderinger (#7027)

--- apps/etterlatte-behandling/src/main/kotlin/Application.kt
+++ apps/etterlatte-behandling/src/main/kotlin/Application.kt
@@ -97,7 +97,6 @@ private fun timerJobs(context: ApplicationContext): List<TimerJob> =
         context.doedsmeldingerReminderJob,
         context.saksbehandlerJob,
         context.aktivitetspliktOppgaveUnntakUtloeperJob,
-        context.avbryterFeilopprettedeRevurderingJob,
     )
 
 @Deprecated("Denne blir brukt i veldig mange testar. Bør rydde opp, men tar det etter denne endringa er inne")

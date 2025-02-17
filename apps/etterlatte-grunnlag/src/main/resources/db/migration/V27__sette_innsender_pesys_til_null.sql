171ef5568b808e65546d728b8007f9e276dc0958
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-17 13:16:29 +0100

    Kopiere tabell

--- apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__sette_innsender_pesys_til_null.sql
+++ apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__sette_innsender_pesys_til_null.sql
@@ -1,5 +1,5 @@
+CREATE TABLE grunnlagshendelse_backup AS TABLE grunnlagshendelse;
+
 update grunnlagshendelse
 set opplysning = jsonb_set(opplysning::jsonb, '{innsender}', 'null')
 where LENGTH(opplysning::JSON ->> 'innsender') < 11 AND opplysning_type = 'PERSONGALLERI_V1';
-
--- todo hvordan gjøre denne med rollback?

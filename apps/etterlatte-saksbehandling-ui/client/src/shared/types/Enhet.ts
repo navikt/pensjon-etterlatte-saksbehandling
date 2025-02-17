efbc1e0b8b4a237d83a2d1c85292f6510c631d32
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2025-02-17 15:05:10 +0100

    Oppdatere navnet på enhet 4862 (#7063)

--- apps/etterlatte-saksbehandling-ui/client/src/shared/types/Enhet.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/types/Enhet.ts
@@ -2,7 +2,7 @@ export const ENHETER: Record<string, string> = {
   '4815': 'Ålesund - 4815',
   '4808': 'Porsgrunn - 4808',
   '4817': 'Steinkjer - 4817',
-  '4862': 'Ålesund utland - 4862',
+  '4862': 'Ålesund utland (bosatt Norge) - 4862',
   '0001': 'Utland - 0001',
   '4883': 'Egne ansatte - 4883',
   '2103': 'Vikafossen - 2103',

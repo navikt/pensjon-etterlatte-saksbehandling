590c36af1bf205e149f3590b294813ba5d35922e
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-18 11:56:22 +0100

    Select ikke insert

--- apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__grante_servicebruker.sql
+++ apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__grante_servicebruker.sql
@@ -1,2 +1,2 @@
 GRANT USAGE on SCHEMA public to "cloudsqliamserviceaccount";
-GRANT INSERT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";

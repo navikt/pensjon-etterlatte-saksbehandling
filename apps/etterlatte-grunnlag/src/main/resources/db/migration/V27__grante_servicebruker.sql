bfe581587d9bce6459cbb5d6c32935db46dfc367
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-02-18 11:54:53 +0100

    copy error

--- apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__grante_servicebruker.sql
+++ apps/etterlatte-grunnlag/src/main/resources/db/migration/V27__grante_servicebruker.sql
@@ -1,2 +1,2 @@
-USAGE on SCHEMA public to "cloudsqliamserviceaccount";
+GRANT USAGE on SCHEMA public to "cloudsqliamserviceaccount";
 GRANT INSERT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";

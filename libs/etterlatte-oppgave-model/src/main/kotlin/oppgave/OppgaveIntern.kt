63268b3d945b530b410f54ab52c4afbea042fad8
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-02-20 10:40:04 +0100

    Oppretter journalføringsopppgave med standardfrist 1 dag

--- libs/etterlatte-oppgave-model/src/main/kotlin/oppgave/OppgaveIntern.kt
+++ libs/etterlatte-oppgave-model/src/main/kotlin/oppgave/OppgaveIntern.kt
@@ -202,11 +202,7 @@ fun opprettNyOppgaveMedReferanseOgSak(
 ): OppgaveIntern {
     val opprettet = Tidspunkt.now()
 
-    val oppgaveFrist =
-        frist ?: opprettet
-            .toLocalDatetimeUTC()
-            .plusMonths(1L)
-            .toTidspunkt()
+    val oppgaveFrist = frist ?: standardfristForOppgave(opprettet, type)
 
     return OppgaveIntern(
         id = UUID.randomUUID(),
@@ -225,3 +221,12 @@ fun opprettNyOppgaveMedReferanseOgSak(
         type = type,
     )
 }
+
+fun standardfristForOppgave(
+    opprettet: Tidspunkt,
+    oppgaveType: OppgaveType,
+): Tidspunkt =
+    when (oppgaveType) {
+        OppgaveType.JOURNALFOERING -> opprettet.toLocalDatetimeUTC().plusDays(1L).toTidspunkt()
+        else -> opprettet.toLocalDatetimeUTC().plusMonths(1L).toTidspunkt()
+    }

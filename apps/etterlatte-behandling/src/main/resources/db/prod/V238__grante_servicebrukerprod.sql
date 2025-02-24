af1b2b9bd43187cfe93b6cd36d2d68ca5f3eb0b2
Sebastian Oliver Søberg <82504565+sebassonav@users.noreply.github.com>
2025-02-24 20:21:16 +0100

    grante servicebruker prod pga nye tabeller må det gjøres på nytt (#7115)

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V238__grante_servicebrukerprod.sql
@@ -0,0 +1,3 @@
+GRANT USAGE on SCHEMA public to "cloudsqliamserviceaccount";
+GRANT INSERT ON TABLE public.behandling_versjon TO "cloudsqliamserviceaccount";
+GRANT INSERT ON TABLE public.grunnlagshendelse TO "cloudsqliamserviceaccount";

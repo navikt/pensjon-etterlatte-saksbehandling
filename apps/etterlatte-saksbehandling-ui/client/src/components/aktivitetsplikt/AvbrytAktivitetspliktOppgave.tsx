ed8b3317ae21c0e61df5fa225d37b229859cdeec
Andreas Balevik <andreas.balevik@nav.no>
2025-03-04 13:22:01 +0100

    rette kode etter tilbakemeldinger

--- apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/AvbrytAktivitetspliktOppgave.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/aktivitetsplikt/AvbrytAktivitetspliktOppgave.tsx
@@ -1,12 +1,11 @@
 import { useAktivitetspliktOppgaveVurdering } from '~components/aktivitetsplikt/AktivitetspliktOppgaveVurderingRoutes'
 import { useApiCall } from '~shared/hooks/useApiCall'
 import { avbrytOppgaveMedMerknad } from '~shared/api/oppgaver'
-import { Button } from '@navikt/ds-react'
+import { Alert, Button } from '@navikt/ds-react'
 import { isPending } from '~shared/api/apiUtils'
 import React from 'react'
 import { erOppgaveRedigerbar } from '~shared/types/oppgave'
 import { useInnloggetSaksbehandler } from '~components/behandling/useInnloggetSaksbehandler'
-import { useForm } from 'react-hook-form'
 
 export function AvbrytAktivitetspliktOppgave() {
   const innloggetSaksbehandler = useInnloggetSaksbehandler()
@@ -17,20 +16,18 @@ export function AvbrytAktivitetspliktOppgave() {
 
   const [avbrytOppgaveMedMerknadResult, avbrytOppgaveMedMerknadRequest] = useApiCall(avbrytOppgaveMedMerknad)
 
-  const { handleSubmit } = useForm<{ kommentar: string }>({ defaultValues: { kommentar: '' } })
-
   const avbryt = () => {
-    avbrytOppgave({ id: oppgave.id, merknad: 'Oppgaven er avbrutt av saksbehandler' })
+    avbrytOppgaveMedMerknadRequest({ id: oppgave.id, merknad: 'Oppgaven er avbrutt av saksbehandler' })
   }
 
   return (
     <>
       {kanRedigeres && erTildeltSaksbehandler ? (
-        <Button variant="danger" onClick={handleSubmit(avbryt)} loading={isPending(avbrytOppgaveStatus)}>
+        <Button variant="danger" onClick={avbryt} loading={isPending(avbrytOppgaveMedMerknadResult)}>
           Avbryt oppgave
         </Button>
       ) : (
-        <Alert variant="warning">Du må tildele deg oppgaven</Alert>
+        <Alert variant="warning">Du må tildele deg oppgaven for å utføre handlinger</Alert>
       )}
     </>
   )

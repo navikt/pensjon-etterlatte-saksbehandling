89f3110ace2f40c31458c7d7170415c0a7466f41
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-03-05 14:51:10 +0100

    Fikser oppreting oppgaver aktivitetsplikt

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/aktivitet/AktivitetspliktSakoversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/aktivitet/AktivitetspliktSakoversikt.tsx
@@ -1,13 +1,9 @@
 import { isFailure, isPending, isSuccess, mapResult, mapSuccess, Result } from '~shared/api/apiUtils'
 import { SakMedBehandlinger } from '~components/person/typer'
 import React, { ReactNode, useEffect } from 'react'
-import { Alert, BodyShort, Box, Button, Heading, Label, VStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Box, Button, Heading, HStack, Label, VStack } from '@navikt/ds-react'
 import { useApiCall } from '~shared/hooks/useApiCall'
-import {
-  hentAktivitspliktVurderingForSak,
-  hentOppfoelgingsoppgaver,
-  opprettOppfoelgingsoppgave,
-} from '~shared/api/aktivitetsplikt'
+import { hentAktivitspliktVurderingForSak, opprettOppfoelgingsoppgave } from '~shared/api/aktivitetsplikt'
 import Spinner from '~shared/Spinner'
 import { ApiErrorAlert, ApiWarningAlert } from '~ErrorBoundary'
 import { AktivitetspliktTidslinje } from '~components/behandling/aktivitetsplikt/AktivitetspliktTidslinje'
@@ -15,13 +11,7 @@ import { hentFamilieOpplysninger } from '~shared/api/pdltjenester'
 import { Familiemedlem } from '~shared/types/familieOpplysninger'
 import { VurderingAvAktivitetspliktSak } from '~components/person/aktivitet/vurderingAvAktivitetsplikt/VurderingAvAktivitetspliktSak'
 import { AktivitetspliktStatusTagOgGyldig } from '~shared/tags/AktivitetspliktStatusOgGyldig'
-import {
-  AktivitetspliktOppfoelgingsOppgave,
-  AktivitetspliktOppfoelgingsOppgaver,
-  AktivitetspliktOppgaveVurderingType,
-  harVurdering,
-} from '~shared/types/Aktivitetsplikt'
-import { Oppgavetype } from '~shared/types/oppgave'
+import { AktivitetspliktOppgaveVurderingType, harVurdering } from '~shared/types/Aktivitetsplikt'
 
 export const velgDoedsdato = (avdoede: Familiemedlem[] | []): Date => {
   if (avdoede.length === 0) return new Date()
@@ -44,15 +34,12 @@ export const AktivitetspliktSakoversikt = ({
   )
   const [familieOpplysningerResult, familieOpplysningerFetch] = useApiCall(hentFamilieOpplysninger)
 
-  const [oppfoelgingsOppgaver, hentOppfoelgingsoppgaverForSak] = useApiCall(hentOppfoelgingsoppgaver)
-
   const [svaropprettOppgave, opprettOppfoelgingsoppgaveReq] = useApiCall(opprettOppfoelgingsoppgave)
 
   useEffect(() => {
     if (isSuccess(sakResult)) {
       hentAktivitetspliktVurderingForSakRequest({ sakId: sakResult.data.sak.id })
       familieOpplysningerFetch({ ident: fnr, sakType: sakResult.data.sak.sakType })
-      hentOppfoelgingsoppgaverForSak({ sakId: sakResult.data.sak.id })
     }
   }, [sakResult])
 
@@ -106,75 +93,59 @@ export const AktivitetspliktSakoversikt = ({
           ),
         })}
       </Box>
-      <Box padding="8" maxWidth="70rem">
-        {mapSuccess(sakResult, (data) =>
-          mapResult(oppfoelgingsOppgaver, {
-            pending: <Spinner label="Henter oppfølgingsoppgave status for sak" />,
-            error: (error) => (
-              <ApiErrorAlert>{error.detail || 'Kunne ikke hente oppfølgingsoppgave status for sak'}</ApiErrorAlert>
-            ),
-            success: (oppgaver) => (
-              <VStack gap="4">
-                <Box maxWidth="30rem">
-                  {!oppfoelging12mndErFerdigstilt(oppgaver) &&
-                    kanOppretteOppgaveAvType(oppgaver, Oppgavetype.AKTIVITETSPLIKT) && (
-                      <Button
-                        loading={isPending(svaropprettOppgave)}
-                        onClick={() =>
-                          opprettOppfoelgingsoppgaveReq({
-                            sakId: data.sak.id,
-                            vurderingType: AktivitetspliktOppgaveVurderingType.SEKS_MAANEDER,
-                          })
-                        }
-                      >
-                        Opprett 6 månderers oppfølgingsoppgave
-                      </Button>
-                    )}
-                  {kanOppretteOppgaveAvType(oppgaver, Oppgavetype.AKTIVITETSPLIKT_12MND) &&
-                    har6MndVurdering(oppgaver, Oppgavetype.AKTIVITETSPLIKT) && (
-                      <Button
-                        loading={isPending(svaropprettOppgave)}
-                        onClick={() =>
-                          opprettOppfoelgingsoppgaveReq({
-                            sakId: data.sak.id,
-                            vurderingType: AktivitetspliktOppgaveVurderingType.TOLV_MAANEDER,
-                          })
-                        }
-                      >
-                        Opprett 12 månderers oppfølgingsoppgave
-                      </Button>
-                    )}
-                </Box>
+      <VStack paddingInline="8 16" paddingBlock="0 16" maxWidth="70rem" gap="4">
+        <Heading level="3" size="small">
+          Informasjonsbrev og vurdering manuelt
+        </Heading>
+        <Box maxWidth="42.5rem">
+          <BodyShort>
+            Hvis det er behov for å sende ut informasjon om aktivitetsplikt manuelt kan en egen oppgave for infomasjon
+            om aktivitetsplikt opprettes.
+          </BodyShort>
+        </Box>
 
-                {isSuccess(svaropprettOppgave) && <Alert variant="success">Oppgaven ble opprettet.</Alert>}
-                {isFailure(svaropprettOppgave) && (
-                  <Alert variant="error">
-                    Kunne ikke opprett oppfølgingsoppgave: {svaropprettOppgave.error.detail}
-                  </Alert>
-                )}
-              </VStack>
-            ),
-          })
+        {isFailure(svaropprettOppgave) && (
+          <Alert variant="error">Kunne ikke opprett oppfølgingsoppgave: {svaropprettOppgave.error.detail}</Alert>
         )}
-      </Box>
+
+        {isSuccess(svaropprettOppgave) && <Alert variant="success">Oppgaven ble opprettet.</Alert>}
+
+        {mapSuccess(sakResult, (sakOgBehandlinger) => (
+          <>
+            <Heading size="xsmall">Opprett oppgave</Heading>
+            <HStack gap="4">
+              <div>
+                <Button
+                  variant="secondary"
+                  loading={isPending(svaropprettOppgave)}
+                  onClick={() =>
+                    opprettOppfoelgingsoppgaveReq({
+                      sakId: sakOgBehandlinger.sak.id,
+                      vurderingType: AktivitetspliktOppgaveVurderingType.SEKS_MAANEDER,
+                    })
+                  }
+                >
+                  Aktivitetsplikt fra 6 måneder
+                </Button>
+              </div>
+              <div>
+                <Button
+                  variant="secondary"
+                  loading={isPending(svaropprettOppgave)}
+                  onClick={() =>
+                    opprettOppfoelgingsoppgaveReq({
+                      sakId: sakOgBehandlinger.sak.id,
+                      vurderingType: AktivitetspliktOppgaveVurderingType.TOLV_MAANEDER,
+                    })
+                  }
+                >
+                  Akvitivetsplikt fra 12 måneder
+                </Button>
+              </div>
+            </HStack>
+          </>
+        ))}
+      </VStack>
     </VStack>
   )
-}
-
-function oppfoelging12mndErFerdigstilt(oppgaver: AktivitetspliktOppfoelgingsOppgave[]): boolean {
-  return oppgaver.filter((o) => o.oppgaveType == Oppgavetype.AKTIVITETSPLIKT_12MND).some((o) => o.erFerdigstilt)
-}
-
-function har6MndVurdering(
-  oppgaver: AktivitetspliktOppfoelgingsOppgave[],
-  aksOppgavetype: AktivitetspliktOppfoelgingsOppgaver
-): boolean {
-  return oppgaver.filter((o) => o.oppgaveType == aksOppgavetype).some((o) => o.erFerdigstilt)
-}
-
-function kanOppretteOppgaveAvType(
-  oppgaver: AktivitetspliktOppfoelgingsOppgave[],
-  aksOppgavetype: AktivitetspliktOppfoelgingsOppgaver
-): boolean {
-  return oppgaver.filter((o) => o.oppgaveType == aksOppgavetype).every((o) => o.kanOpprette)
 }

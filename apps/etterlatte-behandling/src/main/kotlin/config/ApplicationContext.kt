a8ef844a25da8c71a947f0567054953d12ece215
Henrik Gundersen <henrik.gundersen@nav.no>
2025-03-07 09:15:06 +0100

    FAGSYSTEM-372475: Oppdaterer statistikk for tilbakekreving som feilet

--- apps/etterlatte-behandling/src/main/kotlin/config/ApplicationContext.kt
+++ apps/etterlatte-behandling/src/main/kotlin/config/ApplicationContext.kt
@@ -48,6 +48,7 @@ import no.nav.etterlatte.behandling.jobs.AktivitetspliktOppgaveUnntakUtloeperJob
 import no.nav.etterlatte.behandling.jobs.DoedsmeldingJob
 import no.nav.etterlatte.behandling.jobs.DoedsmeldingReminderJob
 import no.nav.etterlatte.behandling.jobs.SaksbehandlerJob
+import no.nav.etterlatte.behandling.jobs.sjekkloependeover20.FiksTilbakekrevingStatistikkJob
 import no.nav.etterlatte.behandling.jobs.sjekkloependeover20.UttrekkLoependeYtelseEtter20Job
 import no.nav.etterlatte.behandling.jobs.sjekkloependeover20.UttrekkLoependeYtelseEtter20JobService
 import no.nav.etterlatte.behandling.klage.KlageBrevService
@@ -796,6 +797,20 @@ internal class ApplicationContext(
             erLeader = { leaderElectionKlient.isLeader() },
             initialDelay = Duration.of(8, ChronoUnit.MINUTES).toMillis(),
             interval = Duration.of(1, ChronoUnit.HOURS),
+        )
+    }
+
+    val fiksStatistikk: FiksTilbakekrevingStatistikkJob by lazy {
+        FiksTilbakekrevingStatistikkJob(
+            tilbakekrevinghendelser = tilbakekrevingHendelserService,
+            behandlingService = behandlingService,
+            tilbakekrevingService = tilbakekrevingService,
+            featureToggleService = featureToggleService,
+            dataSource = dataSource,
+            sakTilgangDao = sakTilgangDao,
+            erLeader = { leaderElectionKlient.isLeader() },
+            initialDelay = Duration.of(3, ChronoUnit.MINUTES).toMillis(),
+            interval = Duration.of(20, ChronoUnit.HOURS),
         )
     }
 

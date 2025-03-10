81a3a9531e34fa26063d7b769618918caf497566
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-03-10 15:02:45 +0100

    kladding tabeller og etc

--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/oversikt/EtteroppgjoerOversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/oversikt/EtteroppgjoerOversikt.tsx
@@ -1,13 +1,26 @@
-import { BodyShort, Button, Heading, VStack } from '@navikt/ds-react'
+import { BodyShort, Button, Heading, TextField, VStack } from '@navikt/ds-react'
 import { formaterDato } from '~utils/formatering/dato'
 import { OpplysningerForEtteroppgjoer } from '~components/etteroppgjoer/oversikt/OpplysningerForEtteroppgjoer'
 import { Link } from 'react-router-dom'
 import { useEtteroppgjoer } from '~store/reducers/EtteroppgjoerReducer'
 import { EtteroppgjoerFramTilbakeKnapperad } from '~components/etteroppgjoer/stegmeny/EtteroppgjoerFramTilbakeKnapperad'
 import { EtteroppjoerSteg } from '~components/etteroppgjoer/stegmeny/EtteroppjoerForbehandlingSteg'
+import { YtelseEtterAvkorting } from '~components/behandling/avkorting/YtelseEtterAvkorting'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { NOK } from '~utils/formatering/formatering'
+
+interface Inntekt {
+  loenn: number
+  afp: number
+  naeringsinntekt: number
+  utlandsinntekt: number
+}
 
 export function EtteroppgjoerOversikt() {
   const etteroppgjoer = useEtteroppgjoer()
+
+  const { watch, register } = useForm<Inntekt>()
 
   return (
     <VStack gap="8" paddingInline="16" paddingBlock="16 4">
@@ -20,6 +33,32 @@ export function EtteroppgjoerOversikt() {
       <Heading size="medium" level="2">
         Fastsette inntekt
       </Heading>
+      <BodyShort>Her skal du fastsette faktisk inntekt for bruker i innvilget periode (april - desember)</BodyShort>
+      <VStack width="fit-content" gap="4">
+        <TextField {...register('loenn')} label="Lønnsinntekt (eksl. OMS)"></TextField>
+        <TextField {...register('afp')} label="AFP"></TextField>
+        <TextField {...register('naeringsinntekt')} label="Næringsinntekt"></TextField>
+        <TextField {...register('utlandsinntekt')} label="Utlandsinntekt"></TextField>
+      </VStack>
+
+      <Heading size="small" level="3">
+        Sum:
+      </Heading>
+      <BodyShort>
+        {NOK(+watch('loenn') + +watch('afp') + +watch('naeringsinntekt') + +watch('utlandsinntekt'))}
+      </BodyShort>
+
+      <VStack>
+        <YtelseEtterAvkorting
+          avkortetYtelse={etteroppgjoer.opplysninger.tidligereAvkorting.avkortetYtelse}
+          tidligereAvkortetYtelse={[]}
+        />
+      </VStack>
+
+      <Heading size="small" level="3">
+        Resultat
+      </Heading>
+      <BodyShort>Resultatet av etteroppgjøret er tilbakekreving av {NOK(2000)}</BodyShort>
 
       <EtteroppgjoerFramTilbakeKnapperad>
         <div>
--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/oversikt/OpplysningerForEtteroppgjoer.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/oversikt/OpplysningerForEtteroppgjoer.tsx
@@ -1,8 +1,7 @@
-import { Heading, Table, VStack } from '@navikt/ds-react'
+import { Heading, ReadMore, Table, VStack } from '@navikt/ds-react'
 import React from 'react'
 import { AInntekt, EtteroppgjoerOpplysninger, PensjonsgivendeInntektFraSkatt } from '~shared/types/Etteroppgjoer'
 import { NOK } from '~utils/formatering/formatering'
-import { YtelseEtterAvkorting } from '~components/behandling/avkorting/YtelseEtterAvkorting'
 import { AvkortingInntektTabell } from '~components/behandling/avkorting/AvkortingInntektTabell'
 
 export const OpplysningerForEtteroppgjoer = ({ opplysninger }: { opplysninger: EtteroppgjoerOpplysninger }) => {
@@ -12,7 +11,7 @@ export const OpplysningerForEtteroppgjoer = ({ opplysninger }: { opplysninger: E
         <Heading size="small">Opplysninger skatt</Heading>
         <SkattTabell skatt={opplysninger.skatt} />
       </VStack>
-      <VStack maxWidth="50rem">
+      <VStack maxWidth="80rem">
         <Heading size="small">Opplysninger A-Inntekt</Heading>
         <AInntektTabell ainntekt={opplysninger.ainntekt} />
       </VStack>
@@ -20,12 +19,6 @@ export const OpplysningerForEtteroppgjoer = ({ opplysninger }: { opplysninger: E
         <AvkortingInntektTabell
           avkortingGrunnlagListe={opplysninger.tidligereAvkorting.avkortingGrunnlag}
           fyller67={false}
-        />
-      </VStack>
-      <VStack>
-        <YtelseEtterAvkorting
-          avkortetYtelse={opplysninger.tidligereAvkorting.avkortetYtelse}
-          tidligereAvkortetYtelse={[]}
         />
       </VStack>
     </>
@@ -61,23 +54,53 @@ const SkattTabell = ({ skatt }: { skatt: PensjonsgivendeInntektFraSkatt }) => {
 
 const AInntektTabell = ({ ainntekt }: { ainntekt: AInntekt }) => {
   return (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Måned</Table.HeaderCell>
-          <Table.HeaderCell>Summert inntekt</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {ainntekt.inntektsmaaneder.map((maaned, i) => {
-          return (
-            <Table.Row key={i}>
-              <Table.DataCell>{maaned.maaned}</Table.DataCell>
-              <Table.DataCell>{NOK(maaned.summertBeloep)}</Table.DataCell>
-            </Table.Row>
-          )
-        })}
-      </Table.Body>
-    </Table>
+    <>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Type</Table.HeaderCell>
+            {ainntekt.inntektsmaaneder.map((maaned) => (
+              <Table.HeaderCell key={maaned.maaned}>{maaned.maaned}</Table.HeaderCell>
+            ))}
+            <Table.HeaderCell>Sum</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.DataCell>Lønnsinntekst</Table.DataCell>
+            {ainntekt.inntektsmaaneder.map((maaned) => {
+              return <Table.DataCell key={maaned.maaned}>{NOK(maaned.summertBeloep)}</Table.DataCell>
+            })}
+            <Table.DataCell>{NOK(ainntekt.inntektsmaaneder.reduce((a, b) => a + b.summertBeloep, 0))}</Table.DataCell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.DataCell>Omstillingsstønad</Table.DataCell>
+            {ainntekt.inntektsmaaneder.map((maaned) => {
+              return <Table.DataCell key={maaned.maaned}>{NOK(maaned.summertBeloep)}</Table.DataCell>
+            })}
+            <Table.DataCell>{NOK(ainntekt.inntektsmaaneder.reduce((a, b) => a + b.summertBeloep, 0))}</Table.DataCell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+      <ReadMore header="se detaljer">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Måned</Table.HeaderCell>
+            <Table.HeaderCell>Summert inntekt</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {ainntekt.inntektsmaaneder.map((maaned, i) => {
+            return (
+              <Table.Row key={i}>
+                <Table.DataCell>{maaned.maaned}</Table.DataCell>
+                <Table.DataCell>{NOK(maaned.summertBeloep)}</Table.DataCell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </ReadMore>
+    </>
   )
 }

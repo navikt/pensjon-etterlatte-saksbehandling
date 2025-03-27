3214df87d25f0490d14c7d4a596e8791e231c15e
Bjørn Golberg <bjorn.golberg@nav.no>
2025-03-27 12:24:20 +0100

    Fikse migreringscript

--- apps/etterlatte-behandling/src/main/resources/db/migration/V254__legge_til_aar_og_periode_etteroppgjoer_behandling.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V254__legge_til_aar_og_periode_etteroppgjoer_behandling.sql
@@ -9,13 +9,15 @@ ALTER TABLE etteroppgjoer_behandling
     ADD COLUMN fom DATE;
 -- kun test behandlinger som finnes så kan sette en dato som ikke stemmer
 update etteroppgjoer_behandling
-set fom = '2024-01-01';
+set fom = '2024-01-01'
+where fom is null;
 ALTER TABLE etteroppgjoer_behandling
     ALTER COLUMN fom SET NOT NULL;
 
 ALTER TABLE etteroppgjoer_behandling
     ADD COLUMN tom DATE;
 update etteroppgjoer_behandling
-set tom = '2024-12-31';
+set tom = '2024-12-31'
+where tom is null;
 ALTER TABLE etteroppgjoer_behandling
     ALTER COLUMN tom SET NOT NULL;

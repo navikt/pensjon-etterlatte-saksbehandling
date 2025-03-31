9115fcdbe569e3c44641e73fb6f581d8a32f1ef0
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2025-03-31 12:21:16 +0200

    EY-5128 Håndtere fnr skjuling hvis enhet er 2103 men ikke markert riktig

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaverTable/OppgaverTableRow.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaverTable/OppgaverTableRow.tsx
@@ -11,6 +11,7 @@ import { OppgaveDTO, OppgaveSaksbehandler, Oppgavestatus } from '~shared/types/o
 import styled from 'styled-components'
 import { PersonLink } from '~components/person/lenker/PersonLink'
 import { OppgaveFrist } from '~components/oppgavebenk/frist/OppgaveFrist'
+import { useInnloggetSaksbehandler } from '~components/behandling/useInnloggetSaksbehandler'
 
 interface Props {
   oppgave: OppgaveDTO
@@ -26,43 +27,56 @@ export const OppgaverTableRow = ({
   oppdaterTildeling,
   oppdaterFrist,
   oppdaterStatus,
-}: Props): ReactNode => (
-  <Table.Row>
-    <Table.DataCell>{oppgave.sakId}</Table.DataCell>
-    <Table.DataCell>{formaterDato(oppgave.opprettet)}</Table.DataCell>
-    <Table.DataCell>
-      <OppgaveFrist oppgave={oppgave} oppdaterFrist={oppdaterFrist} />
-    </Table.DataCell>
-    <Table.DataCell>
-      {oppgave.fnr ? <PersonLink fnr={oppgave.fnr}>{oppgave.fnr}</PersonLink> : 'Mangler'}
-    </Table.DataCell>
-    <Table.DataCell>
-      <HStack align="center">
-        <SakTypeTag sakType={oppgave.sakType} kort />
-      </HStack>
-    </Table.DataCell>
-    <Table.DataCell>
-      {oppgave.type ? <OppgavetypeTag oppgavetype={oppgave.type} /> : <div>oppgaveid {oppgave.id}</div>}
-    </Table.DataCell>
-    <Table.DataCell>{oppgave.merknad}</Table.DataCell>
-    <Table.DataCell>
-      <OppgavestatusTag oppgavestatus={oppgave.status} />
-    </Table.DataCell>
-    <Table.DataCell>{oppgave.enhet}</Table.DataCell>
-    <Table.DataCell>
-      <VelgSaksbehandler
-        saksbehandlereIEnhet={saksbehandlereIEnhet}
-        oppdaterTildeling={oppdaterTildeling}
-        oppgave={oppgave}
-        key={`${oppgave.id}${oppgave.saksbehandler?.ident}`}
-        // For å trigge unmount og resetting av state når man bytte liste. Se https://react.dev/learn/preserving-and-resetting-state#same-component-at-the-same-position-preserves-state
-      />
-    </Table.DataCell>
-    <HandlingerDataCell>
-      <HandlingerForOppgave oppgave={oppgave} oppdaterStatus={oppdaterStatus} />
-    </HandlingerDataCell>
-  </Table.Row>
-)
+}: Props): ReactNode => {
+  const innloggetSaksbehandler = useInnloggetSaksbehandler()
+  let kanSeDetaljertInfo = true
+  const spesialEnhet = oppgave.enhet === '2103' || oppgave.enhet === '4883'
+  if (spesialEnhet) {
+    kanSeDetaljertInfo = innloggetSaksbehandler.skriveEnheter.some((e) => e === oppgave.enhet)
+  }
+
+  return (
+    <Table.Row>
+      <Table.DataCell>{oppgave.sakId}</Table.DataCell>
+      <Table.DataCell>{formaterDato(oppgave.opprettet)}</Table.DataCell>
+      <Table.DataCell>
+        <OppgaveFrist oppgave={oppgave} oppdaterFrist={oppdaterFrist} />
+      </Table.DataCell>
+      <Table.DataCell>
+        {kanSeDetaljertInfo ? (
+          ''
+        ) : (
+          <>{oppgave.fnr ? <PersonLink fnr={oppgave.fnr}>{oppgave.fnr}</PersonLink> : 'Mangler'}</>
+        )}
+      </Table.DataCell>
+      <Table.DataCell>
+        <HStack align="center">
+          <SakTypeTag sakType={oppgave.sakType} kort />
+        </HStack>
+      </Table.DataCell>
+      <Table.DataCell>
+        {oppgave.type ? <OppgavetypeTag oppgavetype={oppgave.type} /> : <div>oppgaveid {oppgave.id}</div>}
+      </Table.DataCell>
+      <Table.DataCell>{kanSeDetaljertInfo && oppgave.merknad}</Table.DataCell>
+      <Table.DataCell>
+        <OppgavestatusTag oppgavestatus={oppgave.status} />
+      </Table.DataCell>
+      <Table.DataCell>{oppgave.enhet}</Table.DataCell>
+      <Table.DataCell>
+        <VelgSaksbehandler
+          saksbehandlereIEnhet={saksbehandlereIEnhet}
+          oppdaterTildeling={oppdaterTildeling}
+          oppgave={oppgave}
+          key={`${oppgave.id}${oppgave.saksbehandler?.ident}`}
+          // For å trigge unmount og resetting av state når man bytte liste. Se https://react.dev/learn/preserving-and-resetting-state#same-component-at-the-same-position-preserves-state
+        />
+      </Table.DataCell>
+      <HandlingerDataCell>
+        <HandlingerForOppgave oppgave={oppgave} oppdaterStatus={oppdaterStatus} />
+      </HandlingerDataCell>
+    </Table.Row>
+  )
+}
 
 const HandlingerDataCell = styled(Table.DataCell)`
   min-width: 13rem;

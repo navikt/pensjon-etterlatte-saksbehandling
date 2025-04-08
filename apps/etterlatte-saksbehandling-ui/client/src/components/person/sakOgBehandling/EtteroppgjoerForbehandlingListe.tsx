377cbdbb12ba9edebd9c725db1a0549eeee5b827
Henrik Gundersen <henrik.gundersen@nav.no>
2025-04-08 19:19:55 +0200

    EY-5127: Endrer revurdering-lenke til å bli knapp

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/EtteroppgjoerForbehandlingListe.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/EtteroppgjoerForbehandlingListe.tsx
@@ -2,11 +2,11 @@ import { useApiCall } from '~shared/hooks/useApiCall'
 import React, { useEffect } from 'react'
 import Spinner from '~shared/Spinner'
 import { ApiErrorAlert } from '~ErrorBoundary'
-import { Alert, Link, Table } from '@navikt/ds-react'
+import { Alert, Button, Link, Table } from '@navikt/ds-react'
 import { formaterDato } from '~utils/formatering/dato'
 import { hentEtteroppgjoerForbehandlinger } from '~shared/api/etteroppgjoer'
 import { EtteroppgjoerBehandling } from '~shared/types/Etteroppgjoer'
-import { mapResult } from '~shared/api/apiUtils'
+import { isPending, mapResult } from '~shared/api/apiUtils'
 import { opprettRevurderingEtteroppgjoer as opprettRevurderingApi } from '~shared/api/revurdering'
 
 function lenkeTilForbehandlingMedId(id: string): string {
@@ -20,7 +20,7 @@ function EtteroppgjoerForbehandlingTabell({
   sakId: number
   forbehandlinger: Array<EtteroppgjoerBehandling>
 }) {
-  const [, opprettRevurderingRequest] = useApiCall(opprettRevurderingApi)
+  const [opprettRevurderingResult, opprettRevurderingRequest] = useApiCall(opprettRevurderingApi)
 
   if (!forbehandlinger?.length) {
     return (
@@ -30,8 +30,11 @@ function EtteroppgjoerForbehandlingTabell({
     )
   }
 
+  // TODO disse revurderingene skal antageligvis ikke opprettes på denne måten, men vi trenger en måte å komme fra forbehandling
   const opprettRevurderingEtteroppgjoer = (forbehandlingId: string) => {
-    opprettRevurderingRequest({ sakId: sakId, forbehandlingId: forbehandlingId }, () => {})
+    opprettRevurderingRequest({ sakId: sakId, forbehandlingId: forbehandlingId }, () => {
+      window.location.reload()
+    })
   }
 
   return (
@@ -60,7 +63,13 @@ function EtteroppgjoerForbehandlingTabell({
               <Link href={lenkeTilForbehandlingMedId(forbehandling.id)}>Gå til behandling</Link>
             </Table.DataCell>
             <Table.DataCell>
-              <Link onClick={() => opprettRevurderingEtteroppgjoer(forbehandling.id)}>Opprett revurdering</Link>
+              <Button
+                loading={isPending(opprettRevurderingResult)}
+                size="small"
+                onClick={() => opprettRevurderingEtteroppgjoer(forbehandling.id)}
+              >
+                Opprett revurdering
+              </Button>
             </Table.DataCell>
           </Table.Row>
         ))}

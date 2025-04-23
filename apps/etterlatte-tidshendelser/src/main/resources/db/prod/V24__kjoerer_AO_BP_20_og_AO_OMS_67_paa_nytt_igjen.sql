33b69ddcb6a204b6219fda17523ce24ba9cb4dff
Henrik Gundersen <henrik.gundersen@nav.no>
2025-04-23 22:26:05 +0200

    Endrer dato

--- apps/etterlatte-tidshendelser/src/main/resources/db/prod/V24__kjoerer_AO_BP_20_og_AO_OMS_67_paa_nytt_igjen.sql
+++ apps/etterlatte-tidshendelser/src/main/resources/db/prod/V24__kjoerer_AO_BP_20_og_AO_OMS_67_paa_nytt_igjen.sql
@@ -1,9 +1,9 @@
 -- Kjører aldersovergang BP20, BP21 og OMS67 på nytt etter at de feilet
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
-values ('AO_BP20', '2025-04-23', '2025-05');
+values ('AO_BP20', '2025-04-24', '2025-05');
 
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
-values ('AO_BP21', '2025-04-23', '2025-05');
+values ('AO_BP21', '2025-04-24', '2025-05');
 
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned)
-values ('AO_OMS67', '2025-04-23', '2025-05');
+values ('AO_OMS67', '2025-04-24', '2025-05');

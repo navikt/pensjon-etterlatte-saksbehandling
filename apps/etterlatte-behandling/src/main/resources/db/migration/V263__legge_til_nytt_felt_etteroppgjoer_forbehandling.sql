73c105b7d66c56e165757eaacde1c796a3db68a6
Andreas Balevik <andreas.balevik@nav.no>
2025-04-24 13:13:36 +0200

    legge til felt hvis det ikke allerede eksisterer

--- apps/etterlatte-behandling/src/main/resources/db/migration/V263__legge_til_nytt_felt_etteroppgjoer_forbehandling.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V263__legge_til_nytt_felt_etteroppgjoer_forbehandling.sql
@@ -1,16 +1,9 @@
-DO $$
-    BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.columns
-            WHERE table_name = 'etteroppgjoer_behandling'
-              AND column_name = 'relatert_forbehandling_id'
-        ) THEN
-            ALTER TABLE etteroppgjoer_behandling
-                ADD COLUMN relatert_forbehandling_id UUID;
-        END IF;
-    END
-$$;
+/* den ble lagt til i dev ved et uhell og bryter flyway */
+ALTER TABLE etteroppgjoer_behandling
+    DROP COLUMN IF EXISTS relatert_forbehandling_id;
+
+ALTER TABLE etteroppgjoer_behandling
+    ADD COLUMN relatert_forbehandling_id UUID;
 
 UPDATE etteroppgjoer_behandling
 SET status = 'FERDIGSTILT'

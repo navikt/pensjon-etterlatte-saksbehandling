854826e21c9f9168f1a0e46d5d0203933fa27445
Andreas Balevik <andreas.balevik@nav.no>
2025-04-24 13:08:48 +0200

    flyway fix

--- apps/etterlatte-behandling/src/main/resources/db/migration/V263__legge_til_nytt_felt_etteroppgjoer_forbehandling.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V263__legge_til_nytt_felt_etteroppgjoer_forbehandling.sql
@@ -1,5 +1,7 @@
+/* denne ble lagt til når behandling ble lånt i dev ved et uhell...
 ALTER TABLE etteroppgjoer_behandling
     ADD COLUMN relatert_forbehandling_id UUID;
+*/
 
 UPDATE etteroppgjoer_behandling
 SET status = 'FERDIGSTILT'

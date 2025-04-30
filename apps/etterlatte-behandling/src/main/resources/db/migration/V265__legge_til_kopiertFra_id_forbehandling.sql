5d4c85e20bc805d9797f1b5c351068617e59ce6a
Andreas Balevik <andreas.balevik@nav.no>
2025-04-30 10:48:58 +0200

    rete flyway

--- apps/etterlatte-behandling/src/main/resources/db/migration/V265__legge_til_kopiertFra_id_forbehandling.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V265__legge_til_kopiertFra_id_forbehandling.sql
@@ -2,4 +2,4 @@ ALTER TABLE etteroppgjoer_behandling
     DROP COLUMN IF EXISTS kopiert_fra;
 
 ALTER TABLE etteroppgjoer_behandling
-    ADD COLUMN IF NOT EXISTS kopiert_fra UUID;
+    ADD COLUMN kopiert_fra UUID;

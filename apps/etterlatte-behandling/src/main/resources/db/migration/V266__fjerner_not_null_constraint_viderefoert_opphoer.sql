f0e2d31b4e3f835a0bb1accf616abb742018e066
Henrik Gundersen <henrik.gundersen@nav.no>
2025-05-02 15:55:08 +0200

    Oppdaterer migreringsscript

--- apps/etterlatte-behandling/src/main/resources/db/migration/V266__fjerner_not_null_constraint_viderefoert_opphoer.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V266__fjerner_not_null_constraint_viderefoert_opphoer.sql
@@ -1,3 +1,6 @@
 -- Dersom videreført opphør settes til Nei, skal dato være null
 alter table viderefoert_opphoer
     alter column dato drop not null;
+
+-- Endrer fra string-verdien "null" til faktisk null
+update viderefoert_opphoer set dato = null where dato = 'null';

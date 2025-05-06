618acb880323719c053f7a346ac8b4abb26ea75d
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2025-05-06 11:39:46 +0200

    Setter opp jobben for sjekking igjen for å kjøre igjennom en sak (#7451)

--- apps/etterlatte-behandling/src/main/kotlin/Application.kt
+++ apps/etterlatte-behandling/src/main/kotlin/Application.kt
@@ -103,6 +103,7 @@ private fun timerJobs(context: ApplicationContext): List<TimerJob> =
         context.saksbehandlerJob,
         context.etteroppgjoerJob,
         context.aktivitetspliktOppgaveUnntakUtloeperJob,
+        context.sjekkAdressebeskyttelseJob,
     )
 
 @Deprecated("Denne blir brukt i veldig mange testar. Bør rydde opp, men tar det etter denne endringa er inne")

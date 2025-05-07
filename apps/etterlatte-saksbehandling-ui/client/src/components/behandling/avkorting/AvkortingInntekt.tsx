d5c97f9925f7b96f91b23af39c26169eec561493
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-05-07 09:58:19 +0200

    Alltid vise hele historikken av inntektsavkorting

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/AvkortingInntekt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/avkorting/AvkortingInntekt.tsx
@@ -1,12 +1,10 @@
-import { Alert, Button, VStack } from '@navikt/ds-react'
-import React, { useState } from 'react'
+import { Alert, VStack } from '@navikt/ds-react'
+import React from 'react'
 import { IBehandlingReducer } from '~store/reducers/BehandlingReducer'
 import { IAvkorting } from '~shared/types/IAvkorting'
-import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons'
 import { usePersonopplysninger } from '~components/person/usePersonopplysninger'
 import { AvkortingInntektTabell } from '~components/behandling/avkorting/AvkortingInntektTabell'
 import { AvkortingInntektForm } from '~components/behandling/avkorting/AvkortingInntektForm'
-import { Revurderingaarsak } from '~shared/types/Revurderingaarsak'
 
 export const AvkortingInntekt = ({
   behandling,
@@ -21,9 +19,6 @@ export const AvkortingInntekt = ({
   redigerbar: boolean
   resetInntektsavkortingValidering: () => void
 }) => {
-  const [visHistorikk, setVisHistorikk] = useState(behandling.revurderingsaarsak === Revurderingaarsak.ETTEROPPGJOER)
-  const erFoerstegangsbehandling = behandling.revurderingsaarsak == null
-
   // TODO sjekke begge/alle?
   const personopplysning = usePersonopplysninger()
   const fyller67 =
@@ -38,19 +33,6 @@ export const AvkortingInntekt = ({
           personopplysning.soeker.opplysning.foedselsaar ===
           67))
 
-  const listeVisningAvkortingGrunnlag = () => {
-    if (visHistorikk) {
-      return avkorting.avkortingGrunnlag
-    }
-    if (erFoerstegangsbehandling) {
-      return [avkorting.redigerbarForventetInntekt ?? [], avkorting.redigerbarForventetInntektNesteAar ?? []].flat()
-    } else {
-      const siste =
-        avkorting.redigerbarForventetInntekt ?? avkorting.avkortingGrunnlag[avkorting.avkortingGrunnlag.length - 1]
-      return [siste]
-    }
-  }
-
   return (
     <VStack maxWidth="70rem">
       {avkorting && avkorting.avkortingGrunnlag.length > 0 && (
@@ -60,19 +42,8 @@ export const AvkortingInntekt = ({
               Bruker fyller 67 år i inntektsåret og antall innvilga måneder vil bli tilpasset deretter.
             </Alert>
           )}
-          <AvkortingInntektTabell avkortingGrunnlagListe={listeVisningAvkortingGrunnlag()} fyller67={fyller67} />
+          <AvkortingInntektTabell avkortingGrunnlagListe={avkorting.avkortingGrunnlag} fyller67={fyller67} />
         </VStack>
-      )}
-      {avkorting && !erFoerstegangsbehandling && avkorting.avkortingGrunnlag.length > 1 && (
-        <Button
-          variant="tertiary"
-          onClick={() => setVisHistorikk(!visHistorikk)}
-          icon={
-            visHistorikk ? <ChevronUpIcon className="dropdownIcon" /> : <ChevronDownIcon className="dropdownIcon" />
-          }
-        >
-          Vis alle
-        </Button>
       )}
       <AvkortingInntektForm
         behandling={behandling}

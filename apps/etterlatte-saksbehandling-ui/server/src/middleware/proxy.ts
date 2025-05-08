49e5145f58d4df1f06050e24d665e6eec56743d8
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-05-08 09:24:51 +0200

    Ikke oppgrader http proxy middleware

--- apps/etterlatte-saksbehandling-ui/server/src/middleware/proxy.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/middleware/proxy.ts
@@ -1,18 +1,16 @@
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import { legacyCreateProxyMiddleware } from 'http-proxy-middleware'
 import { ClientRequest } from 'http'
 import { Request, Response } from 'express'
 import { logger } from '../monitoring/logger'
 import { randomUUID } from 'crypto'
 
 export const proxy = (host: string) =>
-  createProxyMiddleware({
+  legacyCreateProxyMiddleware({
     target: host,
     changeOrigin: true,
-    on: {
-      proxyReq: (proxyReq: ClientRequest, req: Request, res: Response) => {
-        logger.info(`proxying ${host}`)
-        proxyReq.setHeader('Authorization', `Bearer ${res.locals.token}`)
-        proxyReq.setHeader('X-Correlation-ID', randomUUID())
-      },
+    onProxyReq: (proxyReq: ClientRequest, req: Request, res: Response) => {
+      logger.info(`proxying ${host}`)
+      proxyReq.setHeader('Authorization', `Bearer ${res.locals.token}`)
+      proxyReq.setHeader('X-Correlation-ID', randomUUID())
     },
   })

4b765552ab69ecd0a6733ec4131a37500945f25f
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-05-08 08:33:13 +0200

    Bytt ut legacy proxy middleware med nytt

--- apps/etterlatte-saksbehandling-ui/server/src/middleware/proxy.ts
+++ apps/etterlatte-saksbehandling-ui/server/src/middleware/proxy.ts
@@ -1,16 +1,18 @@
-import { legacyCreateProxyMiddleware } from 'http-proxy-middleware'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import { ClientRequest } from 'http'
 import { Request, Response } from 'express'
 import { logger } from '../monitoring/logger'
 import { randomUUID } from 'crypto'
 
 export const proxy = (host: string) =>
-  legacyCreateProxyMiddleware({
+  createProxyMiddleware({
     target: host,
     changeOrigin: true,
-    onProxyReq: (proxyReq: ClientRequest, req: Request, res: Response) => {
-      logger.info(`proxying ${host}`)
-      proxyReq.setHeader('Authorization', `Bearer ${res.locals.token}`)
-      proxyReq.setHeader('X-Correlation-ID', randomUUID())
+    on: {
+      proxyReq: (proxyReq: ClientRequest, _: Request, res: Response) => {
+        logger.info(`proxying ${host}`)
+        proxyReq.setHeader('Authorization', `Bearer ${res.locals.token}`)
+        proxyReq.setHeader('X-Correlation-ID', randomUUID())
+      },
     },
   })

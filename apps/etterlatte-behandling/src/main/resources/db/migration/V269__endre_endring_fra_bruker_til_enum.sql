40d859d63b25f1417c5692391cce27c5664e05c5
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-05-28 08:46:35 +0200

    Endre data

--- apps/etterlatte-behandling/src/main/resources/db/migration/V269__endre_endring_fra_bruker_til_enum.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V269__endre_endring_fra_bruker_til_enum.sql
@@ -1,2 +1,8 @@
 ALTER TABLE etteroppgjoer_behandling ALTER COLUMN har_mottatt_ny_informasjon TYPE TEXT;
 ALTER TABLE etteroppgjoer_behandling ALTER COLUMN endring_er_til_ugunst_for_bruker TYPE TEXT;
+
+UPDATE etteroppgjoer_behandling SET har_mottatt_ny_informasjon = 'JA' WHERE har_mottatt_ny_informasjon = 'true';
+UPDATE etteroppgjoer_behandling SET har_mottatt_ny_informasjon = 'NEI' WHERE har_mottatt_ny_informasjon = 'false';
+
+UPDATE etteroppgjoer_behandling SET endring_er_til_ugunst_for_bruker = 'JA' WHERE endring_er_til_ugunst_for_bruker = 'true';
+UPDATE etteroppgjoer_behandling SET endring_er_til_ugunst_for_bruker = 'NEI' WHERE endring_er_til_ugunst_for_bruker = 'false';

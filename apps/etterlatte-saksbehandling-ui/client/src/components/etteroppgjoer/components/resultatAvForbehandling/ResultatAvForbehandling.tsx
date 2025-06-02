ea18f21b5e09c8c33903697aac59ec8a7c2a9f52
Andreas Balevik <andreas.balevik@nav.no>
2025-06-02 15:43:12 +0200

    EY-5256 forenkle resultat av forbehandling

--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/resultatAvForbehandling/ResultatAvForbehandling.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/resultatAvForbehandling/ResultatAvForbehandling.tsx
@@ -5,14 +5,38 @@ import { EtteroppgjoerResultatType } from '~shared/types/EtteroppgjoerForbehandl
 
 export const ResultatAvForbehandling = () => {
   const { beregnetEtteroppgjoerResultat, behandling } = useEtteroppgjoer()
-
   if (!beregnetEtteroppgjoerResultat) return null
+
+  const { resultatType, differanse } = beregnetEtteroppgjoerResultat
+  const absoluttBeloep = NOK(Math.abs(differanse))
 
   const resultatTekst: Record<EtteroppgjoerResultatType, string> = {
     TILBAKEKREVING: 'Tilbakekreving',
     ETTERBETALING: 'Etterbetaling',
     IKKE_ETTEROPPGJOER: 'Ikke etteroppgjør',
   }
+
+  const beskrivelse = (() => {
+    if (resultatType === EtteroppgjoerResultatType.TILBAKEKREVING) {
+      return `Resultatet viser at det er utbetalt ${absoluttBeloep} for mye stønad i ${behandling.aar}. Beløpet blir derfor krevd tilbake.`
+    }
+
+    if (resultatType === EtteroppgjoerResultatType.ETTERBETALING) {
+      return `Resultatet viser at det er utbetalt ${absoluttBeloep} for lite stønad i ${behandling.aar}. Beløpet blir derfor etterbetalt.`
+    }
+
+    if (resultatType === EtteroppgjoerResultatType.IKKE_ETTEROPPGJOER) {
+      if (differanse > 0) {
+        return `Resultatet viser at det er utbetalt ${absoluttBeloep} for mye stønad i ${behandling.aar}, men beløpet er innenfor toleransegrensen for tilbakekreving, og det kreves derfor ikke tilbake.`
+      }
+
+      if (differanse < 0) {
+        return `Resultatet viser at det er utbetalt ${absoluttBeloep} for lite stønad i ${behandling.aar}, men beløpet er innenfor toleransegrensen for etterbetaling, og det blir derfor ikke utbetalt.`
+      }
+
+      return `Resultatet viser ingen endring, bruker fikk utbetalt rett stønad i ${behandling.aar}.`
+    }
+  })()
 
   return (
     <Box
@@ -26,28 +50,8 @@ export const ResultatAvForbehandling = () => {
     >
       <HStack gap="2" maxWidth="fit-content">
         <VStack gap="2" maxWidth="42.5rem" marginBlock="05 0">
-          <Label>{resultatTekst[beregnetEtteroppgjoerResultat.resultatType]}</Label>
-          <BodyShort>
-            {(() => {
-              const diff = beregnetEtteroppgjoerResultat.differanse
-              const diffBeloep = NOK(Math.abs(diff))
-              if (diff > 0) {
-                if (diff <= beregnetEtteroppgjoerResultat.grense.tilbakekreving) {
-                  return `Resultatet viser at det er utbetalt ${diffBeloep} for mye stønad i ${behandling.aar}, men beløpet er innenfor toleransegrense for tilbakekreving, og det kreves derfor ikke tilbake.`
-                }
-                return `Resultatet viser at det er utbetalt ${diffBeloep} for mye stønad i ${behandling.aar}. Beløpet blir derfor krevd tilbake.`
-              }
-
-              if (diff < 0) {
-                if (diff >= -beregnetEtteroppgjoerResultat.grense.etterbetaling) {
-                  return `Resultatet viser at det er utbetalt ${diffBeloep} for lite stønad i${behandling.aar}, men beløpet er innenfor toleransegrense for etterbetaling, og det blir derfor ikke utbetalt.`
-                }
-                return `Resultatet viser at det er utbetalt ${diffBeloep} for lite stønad i ${behandling.aar}. Beløpet blir derfor etterbetalt.`
-              }
-
-              return `Resultatet viser ingen endring, bruker fikk utbetalt rett stønad i ${behandling.aar}.`
-            })()}
-          </BodyShort>
+          <Label>{resultatTekst[resultatType]}</Label>
+          <BodyShort>{beskrivelse}</BodyShort>
         </VStack>
       </HStack>
     </Box>

f57fd873a0d2c80eb5139f963db44b1a9d8102d2
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-06-06 14:27:10 +0200

    Fikser migrering

--- apps/etterlatte-behandling/src/main/resources/db/prod/V271__setter_fortrolig_utland_eessi_sak.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V271__setter_fortrolig_utland_eessi_sak.sql
@@ -1,2 +1,2 @@
 -- fikser konsistens med eessi-oppgave
-update sak set enhet = '2103', adressebeskyttelse = 'STRENGT_FORTROLIG_UTLAND' where sak_id = 22597;
+update sak set enhet = '2103', adressebeskyttelse = 'STRENGT_FORTROLIG_UTLAND' where id = 22597;

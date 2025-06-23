8671559cf7fe3b3b68f15c6c88a85757391dc546
Andreas Balevik <andreas.balevik@nav.no>
2025-06-23 14:09:42 +0200

    legge til kjøring for juli og august

--- apps/etterlatte-tidshendelser/src/main/resources/db/prod/V27__OP_BP_FYLT_18_06_2025.sql
+++ apps/etterlatte-tidshendelser/src/main/resources/db/prod/V27__OP_BP_FYLT_18_06_2025.sql
@@ -1,2 +1,8 @@
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned, dryrun)
 values ('OP_BP_FYLT_18', '2025-06-23', '2025-06', false);
+
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned, dryrun)
+values ('OP_BP_FYLT_18', '2025-07-07', '2025-07', false);
+
+INSERT INTO jobb (type, kjoeredato, behandlingsmaaned, dryrun)
+values ('OP_BP_FYLT_18', '2025-07-14', '2025-08', false);

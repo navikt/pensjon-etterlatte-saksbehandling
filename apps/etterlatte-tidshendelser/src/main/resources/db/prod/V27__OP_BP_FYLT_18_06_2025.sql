4433b4f58affae31b9a4f26856c30a8ea0ebe931
Andreas Balevik <andreasbalevik@gmail.com>
2025-06-24 09:42:17 +0200

    Update V27__OP_BP_FYLT_18_06_2025.sql

--- apps/etterlatte-tidshendelser/src/main/resources/db/prod/V27__OP_BP_FYLT_18_06_2025.sql
+++ apps/etterlatte-tidshendelser/src/main/resources/db/prod/V27__OP_BP_FYLT_18_06_2025.sql
@@ -1,5 +1,5 @@
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned, dryrun)
-values ('OP_BP_FYLT_18', '2025-06-23', '2025-06', false);
+values ('OP_BP_FYLT_18', '2025-06-24', '2025-06', false);
 
 INSERT INTO jobb (type, kjoeredato, behandlingsmaaned, dryrun)
 values ('OP_BP_FYLT_18', '2025-07-07', '2025-07', false);

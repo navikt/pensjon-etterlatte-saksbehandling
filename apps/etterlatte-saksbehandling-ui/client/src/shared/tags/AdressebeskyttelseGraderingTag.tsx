779e595813550ca8ea592aca36dcadc19f0ea561
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-06-25 13:51:17 +0200

    Hus ikon

--- apps/etterlatte-saksbehandling-ui/client/src/shared/tags/AdressebeskyttelseGraderingTag.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/tags/AdressebeskyttelseGraderingTag.tsx
@@ -1,5 +1,6 @@
 import { AdressebeskyttelseGradering } from '~shared/types/sak'
 import { Tag } from '@navikt/ds-react'
+import { HouseIcon } from '@navikt/aksel-icons'
 
 export const AdressebeskyttelseGraderingTag = ({
   adressebeskyttelse,
@@ -8,29 +9,24 @@ export const AdressebeskyttelseGraderingTag = ({
   adressebeskyttelse: AdressebeskyttelseGradering
   size?: 'medium' | 'small' | 'xsmall'
 }) => {
+  // Viser ingenting hvis adressebeskytelse er satt til ugradert
   switch (adressebeskyttelse) {
-    case AdressebeskyttelseGradering.UGRADERT:
-      return (
-        <Tag variant="neutral" size={size}>
-          Ugradert
-        </Tag>
-      )
     case AdressebeskyttelseGradering.FORTROLIG:
       return (
-        <Tag variant="warning" size={size}>
-          Fortrolig
+        <Tag variant="warning" size={size} icon={<HouseIcon aria-hidden />}>
+          Fortrolig adresse
         </Tag>
       )
     case AdressebeskyttelseGradering.STRENGT_FORTROLIG:
       return (
-        <Tag variant="error" size={size}>
-          Strengt fortrolig
+        <Tag variant="error" size={size} icon={<HouseIcon aria-hidden />}>
+          Strengt fortrolig adresse
         </Tag>
       )
     case AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND:
       return (
-        <Tag variant="error" size={size}>
-          Strengt fortrolig utland
+        <Tag variant="error" size={size} icon={<HouseIcon aria-hidden />}>
+          Strengt fortrolig utland adresse
         </Tag>
       )
   }

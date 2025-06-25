9f23fb572e6a357532ce1fedfab70009bf5903cb
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-06-25 10:45:08 +0200

    Endre datastruktur i gjenny for sak

--- apps/etterlatte-saksbehandling-ui/client/src/shared/types/sak.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/types/sak.ts
@@ -5,6 +5,8 @@ export interface ISak {
   ident: string
   sakType: SakType
   enhet: string
+  adressebeskyttelse?: AdressebeskyttelseGradering
+  erSkjermet?: boolean
 }
 
 export interface ISakMedUtlandstilknytning {
@@ -13,11 +15,6 @@ export interface ISakMedUtlandstilknytning {
   sakType: SakType
   enhet: string
   utlandstilknytning?: IUtlandstilknytning
-}
-
-export interface IKomplettSak extends ISak {
-  adressebeskyttelse?: AdressebeskyttelseGradering
-  erSkjermet?: boolean
 }
 
 export enum SakType {
@@ -33,8 +30,8 @@ export interface FoersteVirk {
 export interface ISaksendring {
   id: string
   endringstype: Endringstype
-  foer: IKomplettSak
-  etter: IKomplettSak
+  foer: ISak
+  etter: ISak
   ident: string
   identtype: Identtype
   kommentar: string

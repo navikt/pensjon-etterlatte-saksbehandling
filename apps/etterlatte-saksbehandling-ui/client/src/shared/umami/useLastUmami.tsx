e1d338d1a3feb77d04bcb78fbf81583bfd3f3844
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-07-14 08:57:01 +0200

    Fikse miljø sjekk

--- apps/etterlatte-saksbehandling-ui/client/src/shared/umami/useLastUmami.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/umami/useLastUmami.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react'
-import { miljoeErProd } from '~utils/miljoe'
+import { miljoeErDev } from '~utils/miljoe'
 
 export const useLastUmami = () => {
   useEffect(() => {
-    const websiteId = miljoeErProd ? '98e556e1-0606-4f3d-a7bd-0c72e8476bb3' : 'ffc09260-0a09-48a2-a225-0ad9ae1e1e0a'
+    const websiteId = miljoeErDev ? 'ffc09260-0a09-48a2-a225-0ad9ae1e1e0a' : '98e556e1-0606-4f3d-a7bd-0c72e8476bb3'
 
     const script = document.createElement('script')
     script.src = 'https://cdn.nav.no/team-researchops/sporing/sporing.js'
@@ -12,6 +12,8 @@ export const useLastUmami = () => {
     script.setAttribute('data-website-id', websiteId)
 
     document.body.appendChild(script)
+
+    console.log(window.location.host)
 
     return () => {
       try {
--- apps/etterlatte-saksbehandling-ui/client/src/utils/miljoe.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/utils/miljoe.ts
@@ -1,1 +1,1 @@
-export const miljoeErProd = !window.location.host.includes('dev') || !window.location.host.includes('localhost')
+export const miljoeErDev = window.location.host.includes('dev') || window.location.host.includes('localhost')

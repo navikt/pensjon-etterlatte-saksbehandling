508020ea98b6d94f328f7022bc468d853ca1efb5
Andreas Balevik <andreas.balevik@nav.no>
2025-07-14 13:42:05 +0200

    rette tilbakemeldinger

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brevutfall/hjelpeTekster/EtterbetalingHjelpeTekst.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/brevutfall/hjelpeTekster/EtterbetalingHjelpeTekst.tsx
@@ -1,18 +1,23 @@
 import React, { ReactNode } from 'react'
-import { HelpText, HStack } from '@navikt/ds-react'
+import { BodyLong, HelpText, HStack, VStack } from '@navikt/ds-react'
 
 export const EtterbetalingHjelpeTekst = (): ReactNode => {
   return (
     <HStack gap="2">
       Skal det etterbetales?
       <HelpText placement="top">
-        Velg &#34;ja&#34; hvis ytelsen er innvilget tilbake i tid, og normal utbetalingsdato for gjeldende periode er
-        forbi. Dette gjelder dersom det vises perioder med &#34;Etterbetaling&#34; under &#34;Simulere utbetaling&#34;.
-        <br />
-        <br />
-        &#34;Fra og med&#34;-dato settes lik virkningstidspunktet i kravet, og &#34;Til og med&#34;-dato settes lik
-        siste passerte normale utbetalingsdato. Det vil si at inneværende måned skal være med som etterbetaling.
-        Simuleringen ovenfor viser inneværende måned som etterbetaling dersom normal utbetalingsdato er passert.
+        <VStack gap="4">
+          <BodyLong>
+            Velg &quot;ja&quot; hvis ytelsen er innvilget tilbake i tid, og normal utbetalingsdato for gjeldende periode
+            er forbi. Dette gjelder dersom det vises perioder med &quot;Etterbetaling&quot; under &quot;Simulere
+            utbetaling&quot;.
+          </BodyLong>
+          <BodyLong>
+            &quot;Fra og med&quot;-dato settes lik virkningstidspunktet i kravet, og &quot;Til og med&quot;-dato settes
+            lik siste passerte normale utbetalingsdato. Det vil si at inneværende måned skal være med som etterbetaling.
+            Simuleringen ovenfor viser inneværende måned som etterbetaling dersom normal utbetalingsdato er passert.
+          </BodyLong>
+        </VStack>
       </HelpText>
     </HStack>
   )

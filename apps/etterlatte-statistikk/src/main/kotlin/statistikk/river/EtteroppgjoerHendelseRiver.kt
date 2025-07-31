f46dacb1b5e59405ba01e5f807b34d7b80da2ef5
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-07-31 11:54:11 +0200

    Lagrer og sender hendelser for etteroppgjør forbehandling
    Løsningen følger et lignende mønster som for klage / tilbakekreving, i at vi
    både lagrer ned hendelser i behandlingshendelse-databasen, og også sender
    hendelser til statistikkappen som registrer saksbehandlingsstatistikk på det.
    
    Oppretter i tillegg en dedikert tabell for etteroppgjør-statistikk, som kan
    holde på informasjon litt mer uavhengig av hva vi skal levere til statistikk-
    teamene.

---
+++ apps/etterlatte-statistikk/src/main/kotlin/statistikk/river/EtteroppgjoerHendelseRiver.kt
@@ -0,0 +1,70 @@
+package no.nav.etterlatte.statistikk.river
+
+import com.fasterxml.jackson.databind.node.NullNode
+import com.fasterxml.jackson.module.kotlin.treeToValue
+import no.nav.etterlatte.libs.common.behandling.ETTEROPPGJOER_RESULTAT_RIVER_KEY
+import no.nav.etterlatte.libs.common.behandling.ETTEROPPGJOER_STATISTIKK_RIVER_KEY
+import no.nav.etterlatte.libs.common.behandling.EtteroppgjoerForbehandlingStatistikkDto
+import no.nav.etterlatte.libs.common.behandling.EtteroppgjoerHendelseType
+import no.nav.etterlatte.libs.common.beregning.BeregnetEtteroppgjoerResultatDto
+import no.nav.etterlatte.libs.common.objectMapper
+import no.nav.etterlatte.libs.common.rapidsandrivers.EVENT_NAME_KEY
+import no.nav.etterlatte.libs.common.rapidsandrivers.TEKNISK_TID_KEY
+import no.nav.etterlatte.libs.common.rapidsandrivers.correlationId
+import no.nav.etterlatte.libs.common.tidspunkt.toTidspunkt
+import no.nav.etterlatte.rapidsandrivers.ListenerMedLogging
+import no.nav.etterlatte.statistikk.service.StatistikkService
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
+
+class EtteroppgjoerHendelseRiver(
+    rapidsConnection: RapidsConnection,
+    private val statistikkService: StatistikkService,
+) : ListenerMedLogging() {
+    private val logger = LoggerFactory.getLogger(EtteroppgjoerHendelseRiver::class.java)
+
+    private val etteroppgjoerHendelser = EtteroppgjoerHendelseType.entries.map { it.lagEventnameForType() }
+
+    init {
+        initialiserRiverUtenEventName(rapidsConnection) {
+            validate { it.demandAny(EVENT_NAME_KEY, etteroppgjoerHendelser) }
+            validate { it.requireKey(ETTEROPPGJOER_STATISTIKK_RIVER_KEY) }
+            validate { it.interestedIn(TEKNISK_TID_KEY) }
+            validate { it.interestedIn(ETTEROPPGJOER_RESULTAT_RIVER_KEY) }
+        }
+    }
+
+    override fun haandterPakke(
+        packet: JsonMessage,
+        context: MessageContext,
+    ) {
+        try {
+            val hendelse: EtteroppgjoerHendelseType = enumValueOf(packet[EVENT_NAME_KEY].textValue().split(":")[1])
+            val tekniskTid = parseTekniskTid(packet, logger)
+            val statistikkDto: EtteroppgjoerForbehandlingStatistikkDto =
+                objectMapper.treeToValue(packet[ETTEROPPGJOER_STATISTIKK_RIVER_KEY])
+            val resultat: BeregnetEtteroppgjoerResultatDto? =
+                when (val resultatNode = packet[ETTEROPPGJOER_RESULTAT_RIVER_KEY]) {
+                    is NullNode -> null
+                    else -> objectMapper.treeToValue(resultatNode)
+                }
+
+            statistikkService.registrerEtteroppgjoerHendelse(
+                hendelse = hendelse,
+                statistikkDto = statistikkDto,
+                tekniskTid = tekniskTid.toTidspunkt(),
+                resultat = resultat,
+            )
+            logger.info("Registrerte statistikk på etteroppgjør for pakke med korrelasjonsid ${packet.correlationId}")
+        } catch (e: Exception) {
+            logger.error(
+                "Kunne ikke registrere statistikk for etteroppgjør i pakken med korrelasjonsid" +
+                    "=${packet.correlationId}. Dette blokkerer lesing av statistikk og må sees på snarest!",
+                e,
+            )
+            throw e
+        }
+    }
+}
---
+++ libs/etterlatte-behandling-model/src/main/kotlin/no/nav/etterlatte/libs/common/behandling/Etteroppgjoer.kt
@@ -0,0 +1,49 @@
+package no.nav.etterlatte.libs.common.behandling
+
+import no.nav.etterlatte.libs.common.event.EventnameHendelseType
+import no.nav.etterlatte.libs.common.periode.Periode
+import no.nav.etterlatte.libs.common.sak.Sak
+import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
+import java.util.UUID
+
+enum class EtteroppgjoerForbehandlingStatus {
+    OPPRETTET,
+    BEREGNET,
+    FERDIGSTILT,
+}
+
+data class EtteroppgjoerForbehandlingDto(
+    val id: UUID,
+    val hendelseId: UUID,
+    val opprettet: Tidspunkt,
+    val status: EtteroppgjoerForbehandlingStatus,
+    val sak: Sak,
+    val aar: Int,
+    val innvilgetPeriode: Periode,
+    val brevId: Long?,
+    val kopiertFra: UUID? = null, // hvis vi oppretter en kopi av forbehandling for å bruke i en revurdering
+    val sisteIverksatteBehandlingId: UUID, // siste iverksatte behandling når forbehandling ble opprettet
+    val harMottattNyInformasjon: JaNei?,
+    val endringErTilUgunstForBruker: JaNei?,
+    val beskrivelseAvUgunst: String?,
+)
+
+enum class EtteroppgjoerHendelseType(
+    val skalSendeStatistikk: Boolean,
+) : EventnameHendelseType {
+    OPPRETTET(true),
+    BEREGNET(false),
+    FERDIGSTILT(true),
+    ;
+
+    override fun lagEventnameForType(): String = "ETTEROPPGJOER_FORBEHANDLING:${this.name}"
+}
+
+const val ETTEROPPGJOER_STATISTIKK_RIVER_KEY = "etteroppgjoer_statistikk"
+const val ETTEROPPGJOER_RESULTAT_RIVER_KEY = "etteroppgave_resultat"
+
+data class EtteroppgjoerForbehandlingStatistikkDto(
+    val forbehandling: EtteroppgjoerForbehandlingDto,
+    val utlandstilknytningType: UtlandstilknytningType?,
+    val saksbehandler: String?,
+)

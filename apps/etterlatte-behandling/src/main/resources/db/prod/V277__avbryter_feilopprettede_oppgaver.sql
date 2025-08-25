5d1532b57c5c4d9121d052ededcadf8e51e30ca5
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-08-25 13:46:56 +0200

    Sjekk også at oppgavene ikke allerede er avbrutt

--- apps/etterlatte-behandling/src/main/resources/db/prod/V277__avbryter_feilopprettede_oppgaver.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V277__avbryter_feilopprettede_oppgaver.sql
@@ -2,4 +2,4 @@ update oppgave
 set status  = 'AVBRUTT',
     merknad = ('Oppgaven er avbrutt på grunn av feil opprettelse. ' || merknad)
 where id in ('1a82a86c-b0c4-4df6-9c98-15b2e0589c21',
-    '22f6e234-1781-4455-b832-a04e2204c58e');
+    '22f6e234-1781-4455-b832-a04e2204c58e') and status != 'AVBRUTT';

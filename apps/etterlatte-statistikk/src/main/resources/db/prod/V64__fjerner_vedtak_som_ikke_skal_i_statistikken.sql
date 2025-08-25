7322fa88a2bccb811d526ba19c706f8c8bb851bf
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-08-25 09:59:55 +0200

    Fjerner vedtak som ikke skal med i stønadsstatistikken

---
+++ apps/etterlatte-statistikk/src/main/resources/db/prod/V64__fjerner_vedtak_som_ikke_skal_i_statistikken.sql
@@ -0,0 +1,12 @@
+delete from maaned_stoenad where behandlingid in ('7baf642e-99ef-4e35-9ce7-16534cdc6085',
+                                                  '1f865001-30f8-4db2-affa-e88967a7fb9d',
+                                                  '887d73fd-7867-4e11-bf38-520505c7af07',
+                                                  'c5a40359-0334-49ef-8470-2f416656e3fa',
+                                                  'a9a06f76-ff08-4d61-afc2-93493cc231e4',
+                                                  '30c51b4d-d1f5-4df6-849e-d517a1a0c487');
+delete from stoenad where behandlingid in ('7baf642e-99ef-4e35-9ce7-16534cdc6085',
+                                           '1f865001-30f8-4db2-affa-e88967a7fb9d',
+                                           '887d73fd-7867-4e11-bf38-520505c7af07',
+                                           'c5a40359-0334-49ef-8470-2f416656e3fa',
+                                           'a9a06f76-ff08-4d61-afc2-93493cc231e4',
+                                           '30c51b4d-d1f5-4df6-849e-d517a1a0c487');

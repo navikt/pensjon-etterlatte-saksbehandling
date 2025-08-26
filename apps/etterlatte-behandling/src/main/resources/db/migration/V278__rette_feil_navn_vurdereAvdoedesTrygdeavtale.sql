116151b115e2db55e8bd52baca17fca522ad5bf9
Andreas Balevik <andreas.balevik@nav.no>
2025-08-26 10:39:14 +0200

    rette feil ved bygging

--- apps/etterlatte-behandling/src/main/resources/db/migration/V278__rette_feil_navn_vurdereAvdoedesTrygdeavtale.sql
+++ apps/etterlatte-behandling/src/main/resources/db/migration/V278__rette_feil_navn_vurdereAvdoedesTrygdeavtale.sql
@@ -1,6 +1,10 @@
 UPDATE behandling
-SET bodd_eller_arbeidet_utlandet = jsonb_set(
-        bodd_eller_arbeidet_utlandet - 'vurdereAvoededsTrygdeavtale',
-        '{vurdereAvdoedesTrygdeavtale}',
-        bodd_eller_arbeidet_utlandet->'vurdereAvoededsTrygdeavtale')
-WHERE bodd_eller_arbeidet_utlandet ? 'vurdereAvoededsTrygdeavtale';
+SET bodd_eller_arbeidet_utlandet = (
+    jsonb_set(
+            (bodd_eller_arbeidet_utlandet::jsonb - 'vurdereAvoededsTrygdeavtale'),
+            '{vurdereAvdoedesTrygdeavtale}',
+            (bodd_eller_arbeidet_utlandet::jsonb -> 'vurdereAvoededsTrygdeavtale')
+    )::text
+)
+WHERE bodd_eller_arbeidet_utlandet IS NOT NULL
+  AND (bodd_eller_arbeidet_utlandet::jsonb ? 'vurdereAvoededsTrygdeavtale');

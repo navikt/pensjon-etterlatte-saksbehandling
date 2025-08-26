bba1c65f26297b3f1121ec112f9a2282a75c7da7
Andreas Balevik <andreas.balevik@nav.no>
2025-08-26 13:39:44 +0200

    rett visning toggle vurdereAvdoedesTrygdeavtale

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/utils.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/trygdetid/utils.ts
@@ -1,10 +1,5 @@
-import { IBehandlingsType, IDetaljertBehandling } from '~shared/types/IDetaljertBehandling'
-import { Revurderingaarsak } from '~shared/types/Revurderingaarsak'
+import { IDetaljertBehandling } from '~shared/types/IDetaljertBehandling'
 
 export const skalViseTrygdeavtale = (behandling: IDetaljertBehandling): boolean => {
-  return (
-    behandling.boddEllerArbeidetUtlandet?.vurdereAvdoedesTrygdeavtale ||
-    (behandling.behandlingType === IBehandlingsType.REVURDERING &&
-      behandling.revurderingsaarsak === Revurderingaarsak.SLUTTBEHANDLING)
-  )
+  return behandling.boddEllerArbeidetUtlandet?.vurdereAvdoedesTrygdeavtale === true
 }

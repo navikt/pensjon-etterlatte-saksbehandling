d7ff1227347dd95f4bdb1ff4cb0fc81c3c167d9c
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-09-02 10:46:55 +0200

    Korrigerer migrering for vedtak

--- apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V46__oppdater_status_vedtak_stuck_samordnet.sql
+++ apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V46__oppdater_status_vedtak_stuck_samordnet.sql
@@ -1,6 +1,6 @@
 -- bare manuelt setter iverksatt til å ha skjedd kort etter attestering
 update vedtak
-set datoiverksatt = datoattestert + interval '1 minute',
+set datoiverksatt = (datoattestert::timestamp + interval '1 minute')::timestamp,
     vedtakstatus  = 'IVERKSATT'
 where sakid = 20602
   and id = 60094

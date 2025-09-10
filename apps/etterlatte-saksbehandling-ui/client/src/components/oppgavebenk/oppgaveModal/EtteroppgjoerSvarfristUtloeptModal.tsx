6911a7c027e6cc36739b8eaef059de3e442afed4
Andreas Balevik <andreas.balevik@nav.no>
2025-09-10 11:53:00 +0200

    kunne avslutte oppgave om tidsfrist utloept

--- apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveModal/EtteroppgjoerSvarfristUtloeptModal.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/oppgavebenk/oppgaveModal/EtteroppgjoerSvarfristUtloeptModal.tsx
@@ -1,4 +1,4 @@
-import { Alert, BodyShort, Button, HStack, Modal, VStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Button, HStack, Modal, Textarea, VStack } from '@navikt/ds-react'
 import { EyeIcon } from '@navikt/aksel-icons'
 import React, { useState } from 'react'
 
@@ -11,6 +11,7 @@ import { Info } from '~components/behandling/soeknadsoversikt/Info'
 import { formaterDato } from '~utils/formatering/dato'
 import { opprettRevurderingEtteroppgjoer as opprettRevurderingApi } from '~shared/api/revurdering'
 import { ApiErrorAlert } from '~ErrorBoundary'
+import { useForm } from 'react-hook-form'
 
 type Props = {
   oppgave: OppgaveDTO
@@ -21,19 +22,34 @@ export const EtteroppgjoerSvarfristUtloeptModal = ({ oppgave, oppdaterStatus }: 
   const [open, setOpen] = useState(false)
   const innloggetSaksbehandler = useInnloggetSaksbehandler()
   const [ferdigstillStatus, ferdigstill] = useApiCall(ferdigstillOppgaveMedMerknad)
+  const [ferdigstillOppgaveStatus, avsluttOppgave] = useApiCall(ferdigstillOppgaveMedMerknad)
 
   const erTildeltSaksbehandler = innloggetSaksbehandler.ident === oppgave.saksbehandler?.ident
   const kanRedigeres = erOppgaveRedigerbar(oppgave.status)
 
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<{ kommentar: string }>({ defaultValues: { kommentar: '' } })
+
   const [opprettRevurderingResult, opprettRevurderingRequest] = useApiCall(opprettRevurderingApi)
 
   const opprettRevurderingEtteroppgjoer = () => {
-    // TODO: burde vi heller opprette fra oppgave.referanse
     opprettRevurderingRequest({ sakId: oppgave.sakId }, () => {
       ferdigstill({ id: oppgave.id, merknad: oppgave.merknad }, () => {
         oppdaterStatus(oppgave.id, Oppgavestatus.FERDIGSTILT)
         setOpen(false)
       })
+    })
+  }
+
+  const avslutt = ({ kommentar }: { kommentar: string }) => {
+    const nyMerknad = `${oppgave.merknad} – \nKommentar: ${kommentar}`
+
+    avsluttOppgave({ id: oppgave.id, merknad: nyMerknad }, (oppgave) => {
+      oppdaterStatus(oppgave.id, oppgave.status)
+      setOpen(false)
     })
   }
 
@@ -59,11 +75,27 @@ export const EtteroppgjoerSvarfristUtloeptModal = ({ oppgave, oppdaterStatus }: 
 
             {oppgave.merknad && <Alert variant="info">{oppgave.merknad}</Alert>}
 
-            {kanRedigeres && !erTildeltSaksbehandler && (
-              <BodyShort>Du må tildele deg oppgaven for å endre den.</BodyShort>
-            )}
-
             <BodyShort>Etteroppgjøret kan ferdigstilles</BodyShort>
+
+            {kanRedigeres &&
+              (erTildeltSaksbehandler ? (
+                <Textarea
+                  {...register('kommentar', {
+                    required: {
+                      value: true,
+                      message: 'Du må legge til en kommentar',
+                    },
+                    minLength: {
+                      value: 10,
+                      message: 'Kommentaren må bestå av minst 10 tegn',
+                    },
+                  })}
+                  label="Kommentar"
+                  error={errors.kommentar?.message}
+                />
+              ) : (
+                <BodyShort>Du må tildele deg oppgaven for å endre den.</BodyShort>
+              ))}
 
             {mapResult(opprettRevurderingResult, {
               error: (error) => (
@@ -75,6 +107,12 @@ export const EtteroppgjoerSvarfristUtloeptModal = ({ oppgave, oppdaterStatus }: 
               <Button variant="secondary" onClick={() => setOpen(false)} disabled={isPending(ferdigstillStatus)}>
                 Lukk
               </Button>
+
+              {kanRedigeres && erTildeltSaksbehandler && (
+                <Button variant="danger" onClick={handleSubmit(avslutt)} loading={isPending(ferdigstillOppgaveStatus)}>
+                  Avslutt oppgave
+                </Button>
+              )}
 
               {kanRedigeres && erTildeltSaksbehandler && (
                 <Button

bb8e546a981b538dcd0949baebba0a7cb4bfe28f
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2025-09-11 13:07:07 +0200

    Update apps/etterlatte-saksbehandling-ui/client/src/shared/types/IDetaljertBehandling.ts
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>

--- apps/etterlatte-saksbehandling-ui/client/src/shared/types/IDetaljertBehandling.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/types/IDetaljertBehandling.ts
@@ -31,7 +31,7 @@ export interface IDetaljertBehandling {
   viderefoertOpphoer: ViderefoertOpphoer | null
   tidligereFamiliepleier: ITidligereFamiliepleier | null
   erSluttbehandling: boolean
-  opprinnelse?: Opprinnelse
+  opprinnelse: Opprinnelse
 }
 
 export const virkningstidspunkt = (behandling: IDetaljertBehandling) => {

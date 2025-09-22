de9535a9fee3dfe84a7109aff2e95ed4c71ddee6
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-09-22 10:29:52 +0200

    Lage eget komponent for lenke til a-inntekt

---
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/inntektsopplysninger/LenkeTilInntektOversikt.tsx
@@ -0,0 +1,26 @@
+import { Box, HStack, Link, Tag } from '@navikt/ds-react'
+import { ExternalLinkIcon } from '@navikt/aksel-icons'
+import React from 'react'
+
+export function LenkeTilInntektOversikt() {
+  return (
+    <Box
+      borderWidth="2"
+      paddingInline="4"
+      paddingBlock="3"
+      maxWidth="fit-content"
+      background="bg-default"
+      borderRadius="4"
+      borderColor="border-on-inverted"
+    >
+      <HStack gap="2">
+        <Tag variant="alt3" size="small">
+          Ai
+        </Tag>
+        <Link href="asdad" target="_blank">
+          Oversikt i Arbeid og Inntekt <ExternalLinkIcon aria-hidden />
+        </Link>
+      </HStack>
+    </Box>
+  )
+}

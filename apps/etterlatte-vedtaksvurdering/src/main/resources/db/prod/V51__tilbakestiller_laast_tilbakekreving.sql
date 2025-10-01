81bd685e947d5d82c0a4a86b9bc9d38576627290
Andreas Balevik <andreasbalevik@gmail.com>
2025-10-01 08:40:26 +0200

    fjerne flyway som tilbakestiller tilbakekreving (#7868)

--- apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V51__tilbakestiller_laast_tilbakekreving.sql
+++
@@ -1,8 +0,0 @@
--- Setter riktig status i vedtak om tilbakekreving, som gikk i vranglås igjen
-UPDATE vedtak
-SET vedtakstatus         = 'FATTET_VEDTAK',
-    attestertvedtakenhet = null,
-    attestant            = null,
-    datoattestert        = null
-WHERE id = 59970
-  and sakid = 21803;

021d88e90e03dedcfcf098ae01658b6cfbb0a74a
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-10-03 10:13:38 +0200

    Formater dato

--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/inntektsopplysninger/OpplysningerFraAInntektSummert.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/inntektsopplysninger/OpplysningerFraAInntektSummert.tsx
@@ -2,7 +2,7 @@ import { SummerteInntekterAOrdningen } from '~shared/types/EtteroppgjoerForbehan
 import { BodyShort, Heading, Label, VStack, Table } from '@navikt/ds-react'
 import { NOK } from '~utils/formatering/formatering'
 import React from 'react'
-import { formaterDatoMedKlokkeslett } from '~utils/formatering/dato'
+import { formaterDato, formaterDatoMedKlokkeslett } from '~utils/formatering/dato'
 import { LenkeTilInntektOversikt } from '~components/etteroppgjoer/components/inntektsopplysninger/LenkeTilInntektOversikt'
 
 export function OpplysningerFraAInntektSummert({ inntekter }: { inntekter: SummerteInntekterAOrdningen }) {
@@ -21,7 +21,7 @@ export function OpplysningerFraAInntektSummert({ inntekter }: { inntekter: Summe
             <Table.HeaderCell>Type inntekt</Table.HeaderCell>
             {inntekter.afp.inntekter.map((maaned) => (
               <Table.HeaderCell key={maaned.maaned} scope="col" align="right">
-                {maaned.maaned}
+                {formaterDato(maaned.maaned)}
               </Table.HeaderCell>
             ))}
           </Table.Row>

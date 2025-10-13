a1cd073dd208ccf6cd2e54976d567377420a2c71
Hanne Olsen <hanne.olsen@nav.no>
2025-10-13 14:01:11 +0200

    Viewet kan ikke hete det samme som tabellen

--- apps/etterlatte-statistikk/src/main/resources/db/migration/V66__view_for_etteroppgjoer_statistikk.sql
+++ apps/etterlatte-statistikk/src/main/resources/db/migration/V66__view_for_etteroppgjoer_statistikk.sql
@@ -1,4 +1,4 @@
 -- Statistikk for etteroppgjør, i første omgang hvilke resultater som kom fra varsel og vedtak
-CREATE OR REPLACE VIEW etteroppgjoer_statistikk AS
+CREATE VIEW etteroppgjoer_statistikk_view AS
 SELECT eo.sak_id, eo.resultat_type, eo.tilknyttet_revurdering, eo.aar
 FROM etteroppgjoer_statistikk eo

907c458141118eb2213edab658a8f7e30fba0e98
Hanne Olsen <hanne.olsen@nav.no>
2025-10-14 10:26:33 +0200

    Kombinerer flyway-script siden de begge går på samme oppgave

--- apps/etterlatte-statistikk/src/main/resources/db/migration/V66__view_for_etteroppgjoer_statistikk.sql
+++
@@ -1,4 +0,0 @@
--- Statistikk for etteroppgjør, i første omgang hvilke resultater som kom fra varsel og vedtak
-CREATE VIEW etteroppgjoer_statistikk_view AS
-SELECT eo.sak_id, eo.resultat_type, eo.tilknyttet_revurdering, eo.aar
-FROM etteroppgjoer_statistikk eo

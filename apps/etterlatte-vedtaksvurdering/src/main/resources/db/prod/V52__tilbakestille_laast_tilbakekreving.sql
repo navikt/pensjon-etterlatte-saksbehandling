5041c181fad0e6f247edb4f173fa370238b2646d
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-10-27 15:22:52 +0100

    Tilbakestiller låst vedtak
    FAGSYSTEM-396290

---
+++ apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V52__tilbakestille_laast_tilbakekreving.sql
@@ -0,0 +1,8 @@
+-- Setter riktig status i vedtak om tilbakekreving
+UPDATE vedtak
+SET vedtakstatus         = 'FATTET_VEDTAK',
+    attestertvedtakenhet = null,
+    attestant            = null,
+    datoattestert        = null
+WHERE id = 59970
+  and sakid = 21803;

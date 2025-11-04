766dd5d3e4019eb2bac4e7a741dc6dddfa326bfa
Hanne Olsen <hanne.olsen@nav.no>
2025-11-04 09:58:45 +0100

    Fjerner styled component, erstatter det med aksel sine primitives

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/UtbetalingTable.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/beregne/UtbetalingTable.tsx
@@ -1,6 +1,5 @@
 import { Box, Heading, Label, Table } from '@navikt/ds-react'
 import { compareDesc } from 'date-fns'
-import { styled } from 'styled-components'
 import { SimulertBeregningsperiode } from '~shared/types/Utbetaling'
 import { formaterDato, formaterKanskjeStringDato } from '~utils/formatering/dato'
 import { NOK } from '~utils/formatering/formatering'
@@ -9,20 +8,12 @@ export function summerPerioder(perioder: SimulertBeregningsperiode[]) {
   return perioder.map((row) => row.beloep).reduce((sum, current) => sum + current, 0)
 }
 
-const TableWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  max-width: 70rem;
-  margin-top: 1em;
-  margin-bottom: 1em;
-`
-
 export const UtbetalingTable = ({ tittel, perioder }: { tittel: string; perioder: SimulertBeregningsperiode[] }) => {
   const sortertePerioder = [...perioder].sort((a, b) => compareDesc(new Date(a.fom), new Date(b.fom)))
 
   if (sortertePerioder.length === 0) {
     return (
-      <Box>
+      <Box maxWidth="70rem">
         <Heading level="3" size="xsmall">
           {tittel}
         </Heading>
@@ -32,7 +23,7 @@ export const UtbetalingTable = ({ tittel, perioder }: { tittel: string; perioder
   }
 
   return (
-    <TableWrapper>
+    <Box maxWidth="70rem" marginBlock="space-16">
       <Heading level="3" size="xsmall">
         {tittel}
       </Heading>
@@ -68,6 +59,6 @@ export const UtbetalingTable = ({ tittel, perioder }: { tittel: string; perioder
           </Table.Row>
         </Table.Body>
       </Table>
-    </TableWrapper>
+    </Box>
   )
 }

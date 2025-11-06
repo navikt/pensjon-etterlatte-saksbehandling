dca7e3fc6ccb2dab6555c592e68b93758b4d2627
Andreas Balevik <andreasbalevik@gmail.com>
2025-11-06 13:39:28 +0100

    sette brevid for forbehandlinger som mangler (#8023)

---
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V299__sette_brevid_for_forbehandling.sql
@@ -0,0 +1,3 @@
+UPDATE etteroppgjoer_behandling SET brev_id = 43389 WHERE sak_id = 18500 AND brev_id IS NULL AND status = 'BEREGNET';
+UPDATE etteroppgjoer_behandling SET brev_id = 43375 WHERE sak_id = 21032 AND brev_id IS NULL AND status = 'BEREGNET';
+

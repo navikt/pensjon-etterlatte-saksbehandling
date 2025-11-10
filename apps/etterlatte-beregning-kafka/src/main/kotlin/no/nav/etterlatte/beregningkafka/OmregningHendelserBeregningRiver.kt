9f457e81f414520f42a9939a8f64c6a60cc65c78
Trond Valen <trond.valen@nav.no>
2025-11-10 12:44:35 +0100

    Utelukker g-justering bare for årlig inntektsjustering.

--- apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/OmregningHendelserBeregningRiver.kt
+++ apps/etterlatte-beregning-kafka/src/main/kotlin/no/nav/etterlatte/beregningkafka/OmregningHendelserBeregningRiver.kt
@@ -89,7 +89,7 @@ internal class OmregningHendelserBeregningRiver(
         val revurderingaarsak = omregningData.revurderingaarsak
         beregningService.opprettBeregningsgrunnlagFraForrigeBehandling(behandlingId, behandlingViOmregnerFra)
 
-        if (revurderingaarsak == Revurderingaarsak.REGULERING) {
+        if (revurderingaarsak != Revurderingaarsak.AARLIG_INNTEKTSJUSTERING) {
             beregningService.tilpassOverstyrtBeregningsgrunnlagForRegulering(behandlingId)
         }
         val beregning = beregningService.beregn(behandlingId).body<BeregningDTO>()

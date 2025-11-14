84dd11792e498013f29588b25d18967a1a7f9e55
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-11-14 07:38:47 +0100

    Forenklinger

--- apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/HarSvartIModiaModal.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/person/sakOgBehandling/HarSvartIModiaModal.tsx
@@ -1,40 +1,32 @@
-import { BodyShort, Button, Heading, HStack, Modal, Textarea, VStack } from '@navikt/ds-react'
+import { BodyShort, Button, Heading, HStack, Modal, VStack } from '@navikt/ds-react'
 import { PersonChatIcon } from '@navikt/aksel-icons'
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
 import { useApiCall } from '~shared/hooks/useApiCall'
-import { opprettRevurdering as opprettRevurderingApi } from '~shared/api/revurdering'
-import { Revurderingaarsak } from '~shared/types/Revurderingaarsak'
+import { opprettRevurderingEtteroppgjoer } from '~shared/api/revurdering'
 import { useNavigate } from 'react-router-dom'
 import { isPending } from '~shared/api/apiUtils'
 import { isFailureHandler } from '~shared/api/IsFailureHandler'
+import { Opprinnelse } from '~shared/types/IDetaljertBehandling'
 
 export const HarSvartIModiaModal = ({ sakId }: { sakId: number }) => {
   const [aapen, setAapen] = useState<boolean>(false)
 
-  const [opprettRevurderingResult, opprettRevurderingRequest, resetApiCall] = useApiCall(opprettRevurderingApi)
+  const [opprettRevurderingResult, opprettRevurderingRequest, resetApiCall] = useApiCall(
+    opprettRevurderingEtteroppgjoer
+  )
 
   const navigate = useNavigate()
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<{ begrunnelse: string }>({ defaultValues: { begrunnelse: '' } })
-
   const lukkModal = () => {
     resetApiCall()
-    reset()
     setAapen(false)
   }
 
-  const opprettRevurdering = (begrunnelse: string) => {
+  const opprettRevurdering = () => {
     opprettRevurderingRequest(
       {
         sakId,
-        aarsak: Revurderingaarsak.ETTEROPPGJOER,
-        begrunnelse,
+        opprinnelse: Opprinnelse.SVAR_I_MODIA,
       },
       (revurderingId) => navigate(`/behandling/${revurderingId}/`)
     )
@@ -58,22 +50,13 @@ export const HarSvartIModiaModal = ({ sakId }: { sakId: number }) => {
               Hvis bruker har svart på etteroppgjøret i Modia kan du opprette revurdering for etteroppgjøret her.
             </BodyShort>
 
-            <Textarea
-              {...register('begrunnelse', { required: { value: true, message: 'Du må gi en begrunnelse' } })}
-              label="Begrunnelse"
-              error={errors.begrunnelse?.message}
-            />
-
             {isFailureHandler({
               apiResult: opprettRevurderingResult,
               errorMessage: 'Feil under opprettelse av revurdering',
             })}
 
             <HStack gap="4" justify="end">
-              <Button
-                loading={isPending(opprettRevurderingResult)}
-                onClick={handleSubmit((data) => opprettRevurdering(data.begrunnelse))}
-              >
+              <Button loading={isPending(opprettRevurderingResult)} onClick={opprettRevurdering}>
                 Opprett revurdering
               </Button>
               <Button variant="secondary" onClick={lukkModal} disabled={isPending(opprettRevurderingResult)}>

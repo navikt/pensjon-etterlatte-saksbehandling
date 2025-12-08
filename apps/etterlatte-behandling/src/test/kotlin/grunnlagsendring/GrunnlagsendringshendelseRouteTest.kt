f2e9faa470bc9a98162f0720811faded8cb405d0
Trond Valen <trond.valen@nav.no>
2025-12-08 12:10:37 +0100

    EY-5497: Reaktiverer behandling av hendelser om institusjonsopphold.

--- apps/etterlatte-behandling/src/test/kotlin/grunnlagsendring/GrunnlagsendringshendelseRouteTest.kt
+++ apps/etterlatte-behandling/src/test/kotlin/grunnlagsendring/GrunnlagsendringshendelseRouteTest.kt
@@ -1,20 +1,38 @@
 package no.nav.etterlatte.grunnlagsendring
 
+import io.kotest.matchers.shouldBe
+import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.request.url
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.server.testing.testApplication
+import kotlinx.coroutines.asContextElement
 import no.nav.etterlatte.BehandlingIntegrationTest
+import no.nav.etterlatte.Context
+import no.nav.etterlatte.Kontekst
+import no.nav.etterlatte.behandling.domain.GrunnlagsendringStatus
+import no.nav.etterlatte.behandling.domain.GrunnlagsendringsType
+import no.nav.etterlatte.behandling.domain.Grunnlagsendringshendelse
+import no.nav.etterlatte.institusjonsopphold.InstitusjonsoppholdHendelseBeriket
+import no.nav.etterlatte.institusjonsopphold.InstitusjonsoppholdKilde
+import no.nav.etterlatte.institusjonsopphold.InstitusjonsoppholdsType
 import no.nav.etterlatte.ktor.runServerWithModule
 import no.nav.etterlatte.libs.common.behandling.BehandlingsBehov
 import no.nav.etterlatte.libs.common.behandling.Persongalleri
 import no.nav.etterlatte.libs.common.behandling.SakType
+import no.nav.etterlatte.libs.common.oppgave.OppgaveIntern
+import no.nav.etterlatte.libs.common.oppgave.OppgaveType
+import no.nav.etterlatte.libs.common.oppgave.Status
 import no.nav.etterlatte.libs.common.pdlhendelse.Adressebeskyttelse
 import no.nav.etterlatte.libs.common.pdlhendelse.Endringstype
 import no.nav.etterlatte.libs.common.sak.Sak
@@ -22,18 +40,30 @@ import no.nav.etterlatte.libs.common.tidspunkt.Tidspunkt
 import no.nav.etterlatte.libs.common.tidspunkt.toLocalDatetimeUTC
 import no.nav.etterlatte.libs.ktor.route.FoedselsnummerDTO
 import no.nav.etterlatte.libs.testdata.grunnlag.AVDOED_FOEDSELSNUMMER
+import no.nav.etterlatte.libs.testdata.grunnlag.INNSENDER_FOEDSELSNUMMER
+import no.nav.etterlatte.mockSaksbehandler
 import no.nav.etterlatte.module
+import no.nav.etterlatte.nyKontekstMedBrukerOgDatabase
+import no.nav.etterlatte.soeker
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import java.time.LocalDate
 import java.util.UUID
+import kotlin.random.Random
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
+    private lateinit var context: Context
+
     @BeforeEach
-    fun start() = startServer()
+    fun start() {
+        startServer()
+        context = nyKontekstMedBrukerOgDatabase(mockSaksbehandler("Z123456"), applicationContext.dataSource)
+    }
 
     @AfterEach
     fun afterEach() {
@@ -58,7 +88,7 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                         setBody(FoedselsnummerDTO(fnr))
                         header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
                     }.let {
-                        Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                        assertEquals(HttpStatusCode.OK, it.status)
                         it.body()
                     }
             Assertions.assertNotNull(sak.id)
@@ -68,15 +98,9 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                     .post("/behandlinger/opprettbehandling") {
                         addAuthToken(tokenSaksbehandler)
                         header(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                        setBody(
-                            BehandlingsBehov(
-                                sak.id,
-                                Persongalleri(fnr, "innsender", emptyList(), emptyList(), emptyList()),
-                                Tidspunkt.now().toLocalDatetimeUTC().toString(),
-                            ),
-                        )
+                        setBody(behandlingsBehov(sak))
                     }.let {
-                        Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                        assertEquals(HttpStatusCode.OK, it.status)
                         UUID.fromString(it.body())
                     }
 
@@ -84,7 +108,7 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                 .get("/behandlinger/$behandlingId") {
                     addAuthToken(this@GrunnlagsendringshendelseRouteTest.systemBruker)
                 }.let {
-                    Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                    assertEquals(HttpStatusCode.OK, it.status)
                 }
 
             client
@@ -99,7 +123,7 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                         ),
                     )
                 }.also {
-                    Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                    assertEquals(HttpStatusCode.OK, it.status)
                 }
 
             client
@@ -114,7 +138,7 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                         ),
                     )
                 }.also {
-                    Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                    assertEquals(HttpStatusCode.OK, it.status)
                 }
 
             client
@@ -129,8 +153,125 @@ class GrunnlagsendringshendelseRouteTest : BehandlingIntegrationTest() {
                         ),
                     )
                 }.also {
-                    Assertions.assertEquals(HttpStatusCode.OK, it.status)
+                    assertEquals(HttpStatusCode.OK, it.status)
                 }
         }
     }
+
+    @Test
+    fun `skal opprette hendelse om institusjonsopphold`() {
+        withTestApplication(context) { client ->
+            val sak: Sak =
+                client
+                    .postJson("/personer/saker/${SakType.OMSTILLINGSSTOENAD}") {
+                        setBody(FoedselsnummerDTO(soeker))
+                    }.apply {
+                        assertEquals(HttpStatusCode.OK, status)
+                    }.body()
+
+            client
+                .postJson("/behandlinger/opprettbehandling") {
+                    setBody(behandlingsBehov(sak))
+                }.also { assertEquals(HttpStatusCode.OK, it.status) }
+
+            client
+                .postJson("/grunnlagsendringshendelse/institusjonsopphold") {
+                    setBody(institusjonsopphold())
+                }.also { assertEquals(HttpStatusCode.OK, it.status) }
+
+            val hendelserResponse =
+                client
+                    .get("/api/grunnlagsendringshendelse/${sak.id}/institusjon") {
+                    }.also { assertEquals(HttpStatusCode.OK, it.status) }
+
+            val hendelser: List<Grunnlagsendringshendelse> = hendelserResponse.body()
+            val grunnlagsendringshendelse =
+                hendelser
+                    .single()
+                    .also {
+                        it.status shouldBe GrunnlagsendringStatus.SJEKKET_AV_JOBB
+                        it.sakId shouldBe sak.id
+                        it.type shouldBe GrunnlagsendringsType.INSTITUSJONSOPPHOLD
+                        it.behandlingId shouldBe null
+                    }
+
+            println(
+                client
+                    .get("/sak/${sak.id}/oppgaver")
+                    .bodyAsText(),
+            )
+
+            val oppgave: OppgaveIntern =
+                client
+                    .get("/api/oppgaver//sak/${sak.id}/oppgaver")
+                    .body<List<OppgaveIntern>>()
+                    .single { it.type == OppgaveType.VURDER_KONSEKVENS }
+
+            oppgave.referanse shouldBe grunnlagsendringshendelse.id
+            oppgave.status shouldBe Status.NY
+            oppgave.saksbehandler shouldBe null
+        }
+    }
+
+    private fun behandlingsBehov(sak: Sak): BehandlingsBehov =
+        BehandlingsBehov(
+            sak.id,
+            Persongalleri(
+                soeker,
+                INNSENDER_FOEDSELSNUMMER.value,
+                emptyList(),
+                emptyList(),
+                emptyList(),
+            ),
+            Tidspunkt.now().toLocalDatetimeUTC().toString(),
+        )
+
+    private fun withTestApplication(
+        context: Context,
+        block: suspend (client: HttpClient) -> Unit,
+    ) {
+        testApplication(Kontekst.asContextElement(context)) {
+            val client =
+                runServerWithModule(mockOAuth2Server) {
+                    module(applicationContext)
+                }
+            block(client)
+        }
+    }
+
+    private fun institusjonsopphold(): InstitusjonsoppholdHendelseBeriket =
+        InstitusjonsoppholdHendelseBeriket(
+            hendelseId = Random.nextLong(),
+            oppholdId = Random.nextLong(),
+            norskident = soeker,
+            institusjonsoppholdsType = InstitusjonsoppholdsType.INNMELDING,
+            institusjonsoppholdKilde = InstitusjonsoppholdKilde.INST,
+            institusjonsType = "fengsel",
+            startdato = LocalDate.now().minusDays(6),
+            faktiskSluttdato = null,
+            forventetSluttdato = LocalDate.now().plusMonths(18),
+            institusjonsnavn = "Feng Shel fengsel",
+            organisasjonsnummer = "957838938",
+        )
+
+    private suspend fun HttpClient.postJson(
+        urlString: String,
+        block: HttpRequestBuilder.() -> Unit = {},
+    ): HttpResponse =
+        post {
+            addAuthToken(this@GrunnlagsendringshendelseRouteTest.systemBruker)
+            contentType(ContentType.Application.Json)
+            url(urlString)
+            block()
+        }
+
+    private suspend fun HttpClient.get(
+        urlString: String,
+        block: HttpRequestBuilder.() -> Unit = {},
+    ): HttpResponse =
+        get {
+            addAuthToken(this@GrunnlagsendringshendelseRouteTest.systemBruker)
+            url(urlString)
+            block()
+        }
 }

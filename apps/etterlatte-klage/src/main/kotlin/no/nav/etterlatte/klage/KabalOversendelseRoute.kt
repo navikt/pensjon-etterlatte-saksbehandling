03c1c1b7407e3199fc48cd1caa4a9ad232b72d5c
Trond Valen <trond.valen@nav.no>
2026-01-05 12:43:23 +0100

    fjerner marker for team logs

--- apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseRoute.kt
+++ apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseRoute.kt
@@ -1,7 +1,6 @@
 package no.nav.etterlatte.klage
 
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
@@ -11,11 +10,9 @@ import no.nav.etterlatte.libs.ktor.route.kunSystembruker
 import no.nav.etterlatte.libs.ktor.route.medBody
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.MarkerFactory
 
 private val sikkerlogger: Logger = LoggerFactory.getLogger("team-logs-logger")
 private val logger: Logger = LoggerFactory.getLogger(ApplicationContext::class.java)
-private val teamLogsMarker = MarkerFactory.getMarker("TEAM_LOGS")
 
 fun Route.kabalOvesendelseRoute(kabalOversendelseService: KabalOversendelseService) {
     route("api") {
@@ -35,7 +32,6 @@ fun Route.kabalOvesendelseRoute(kabalOversendelseService: KabalOversendelseServi
                 sikkerlogger.info("temp-test ${Parent(listOf("Ole","Dole","Doffen"))}")
                 sikkerLogg.info("Gammel logger")
                 logger.info("Normal")
-                logger.info(teamLogsMarker, "logging med team logs marker")
                 call.respond(HttpStatusCode.OK)
             }
         }

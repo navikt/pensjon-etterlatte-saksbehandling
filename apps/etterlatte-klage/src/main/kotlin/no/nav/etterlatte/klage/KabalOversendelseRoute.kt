fe66e6d4cab7d0beafc9b800efe1325a0de6e6bf
Trond Valen <trond.valen@nav.no>
2026-01-05 15:22:26 +0100

    Fjerner testkode

--- apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseRoute.kt
+++ apps/etterlatte-klage/src/main/kotlin/no/nav/etterlatte/klage/KabalOversendelseRoute.kt
@@ -1,6 +1,7 @@
 package no.nav.etterlatte.klage
 
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
@@ -8,10 +9,6 @@ import io.ktor.server.routing.route
 import no.nav.etterlatte.libs.common.behandling.KlageOversendelseDto
 import no.nav.etterlatte.libs.ktor.route.kunSystembruker
 import no.nav.etterlatte.libs.ktor.route.medBody
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-private val logger: Logger = LoggerFactory.getLogger(ApplicationContext::class.java)
 
 fun Route.kabalOvesendelseRoute(kabalOversendelseService: KabalOversendelseService) {
     route("api") {
@@ -25,22 +22,5 @@ fun Route.kabalOvesendelseRoute(kabalOversendelseService: KabalOversendelseServi
                 }
             }
         }
-
-        route("temp-test") {
-            post {
-                try {
-                    doit()
-                } catch (err: Throwable) {
-                    sikkerLogg.error("Feilet", err)
-                    logger.error("Feilet, vanlig logger", err)
-                }
-                logger.info("Normal")
-                call.respond(HttpStatusCode.OK)
-            }
-        }
     }
-}
-
-private fun doit() {
-    if (1 == 1) throw RuntimeException("fds")
 }

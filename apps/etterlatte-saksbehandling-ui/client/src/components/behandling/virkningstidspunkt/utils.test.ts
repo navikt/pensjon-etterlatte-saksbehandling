a9af4fc29684a8b685e2718e4da10c8bc535a568
Hanne Olsen <hanne.olsen@nav.no>
2026-01-21 12:07:39 +0100

    Fikser testene

--- apps/etterlatte-saksbehandling-ui/client/src/components/behandling/virkningstidspunkt/utils.test.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/components/behandling/virkningstidspunkt/utils.test.ts
@@ -3,31 +3,12 @@ import { hentMinimumsVirkningstidspunkt } from '~components/behandling/virknings
 import { SakType } from '~shared/types/sak'
 
 describe('minimumstidspunkt for virkningstidspunkt er', () => {
-  it('maaned etter doedsdato hvis soknad er mottatt innenfor tre aar', () => {
+  it('maaned etter doedsdato', () => {
     const avdoedDoedsdato = new Date('2020-01-01')
-    const soeknadMottattDato = '2020-03-01'
 
-    const minimumsVirkningstidspunkt = hentMinimumsVirkningstidspunkt(
-      avdoedDoedsdato,
-      new Date(soeknadMottattDato),
-      SakType.BARNEPENSJON
-    )
+    const minimumsVirkningstidspunkt = hentMinimumsVirkningstidspunkt(avdoedDoedsdato, SakType.BARNEPENSJON)
 
     const manedEtterDoedsDato = new Date('2020-02-01')
     expect(minimumsVirkningstidspunkt).toStrictEqual(manedEtterDoedsDato)
-  })
-
-  it('tre aar fra mottatt soknad hvis soknad er mottatt tre aar etter doedsdato', () => {
-    const avdoedDoedsdato = new Date('2016-12-31')
-    const soeknadMottattDato = '2020-01-01'
-
-    const minimumsVirkningstidspunkt = hentMinimumsVirkningstidspunkt(
-      avdoedDoedsdato,
-      new Date(soeknadMottattDato),
-      SakType.BARNEPENSJON
-    )
-
-    const treAarFoerSoknad = new Date('2017-01-01')
-    expect(minimumsVirkningstidspunkt).toStrictEqual(treAarFoerSoknad)
   })
 })

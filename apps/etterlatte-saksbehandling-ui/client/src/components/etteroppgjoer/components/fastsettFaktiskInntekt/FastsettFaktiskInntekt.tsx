c5836e8e3cfa60fa09bc3ce9c48448ee46567593
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2026-03-03 14:01:33 +0100

    Viser om det er innvilget ytelse > 0 for måneder i etteroppgjøret (#8359)
    * Viser om det er innvilget ytelse > 0 for måneder i etteroppgjøret
    
    * Legger på advarsel om måneder uten ytelse før avkorting
    
    * Oppdaterer helptext

--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/fastsettFaktiskInntekt/FastsettFaktiskInntekt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/fastsettFaktiskInntekt/FastsettFaktiskInntekt.tsx
@@ -1,6 +1,6 @@
-import { BodyShort, Button, Heading, Tag, VStack } from '@navikt/ds-react'
+import { Alert, BodyShort, Button, Heading, Tag, VStack } from '@navikt/ds-react'
 import { useEtteroppgjoerForbehandling } from '~store/reducers/EtteroppgjoerReducer'
-import { maanedNavn } from '~utils/formatering/dato'
+import { formaterMaanednavnAar, maanedNavn } from '~utils/formatering/dato'
 import { FaktiskInntektSkjema } from '~components/etteroppgjoer/components/fastsettFaktiskInntekt/FaktiskInntektSkjema'
 import { FaktiskInntektVisning } from '~components/etteroppgjoer/components/fastsettFaktiskInntekt/FaktiskInntektVisning'
 import { PencilIcon } from '@navikt/aksel-icons'
@@ -14,21 +14,49 @@ interface Props {
 }
 
 export const FastsettFaktiskInntekt = ({ erRedigerbar, setFastsettFaktiskInntektSkjemaErrors }: Props) => {
-  const { forbehandling, faktiskInntekt } = useEtteroppgjoerForbehandling()
+  const { forbehandling, faktiskInntekt, opplysninger } = useEtteroppgjoerForbehandling()
 
   const [faktiskInntektSkjemaErAapen, setFaktiskInntektSkjemaErAapen] = useState<boolean>(
     erRedigerbar && !faktiskInntekt
   )
 
+  const maanederMedIngenBeregnetYtelseEtteroppgjoeret =
+    opplysninger.tidligereAvkorting?.avkortetYtelse
+      ?.filter(
+        (ytelse) => ytelse.fom >= forbehandling.innvilgetPeriode.fom && ytelse.tom <= forbehandling.innvilgetPeriode.tom
+      )
+      ?.filter((ytelse) => ytelse.ytelseFoerAvkorting === 0) ?? []
+
   return (
     <VStack gap="4">
       <Heading size="large">Fastsett faktisk inntekt</Heading>
       <BodyShort>Fastsett den faktiske inntekten for bruker i den innvilgede perioden.</BodyShort>
-      <div>
-        <Tag variant="neutral">
-          {maanedNavn(forbehandling.innvilgetPeriode.fom)} - {maanedNavn(forbehandling.innvilgetPeriode.tom)}
-        </Tag>
-      </div>
+      <VStack gap="4" maxWidth="42.5rem">
+        <div>
+          <Tag variant="neutral">
+            {maanedNavn(forbehandling.innvilgetPeriode.fom)} - {maanedNavn(forbehandling.innvilgetPeriode.tom)}
+          </Tag>
+        </div>
+        {maanederMedIngenBeregnetYtelseEtteroppgjoeret.length > 0 && (
+          <Alert variant="warning">
+            <VStack gap="2">
+              <BodyShort>
+                Bruker har ikke hatt innvilget omstillingsstønad hele året, på grunn av sanksjon, stans eller fengsel.
+                Dette gjelder følgende perioder:
+              </BodyShort>
+              <div>
+                <ul>
+                  {maanederMedIngenBeregnetYtelseEtteroppgjoeret.map((maaned) => (
+                    <li key={maaned.fom}>
+                      {formaterMaanednavnAar(maaned.fom)} - {formaterMaanednavnAar(maaned.tom)}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </VStack>
+          </Alert>
+        )}
+      </VStack>
 
       {faktiskInntektSkjemaErAapen && erRedigerbar ? (
         <FaktiskInntektSkjema

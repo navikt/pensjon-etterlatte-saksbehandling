edaec2fa08e0bcaa25db916cac4a3bcd4cf85661
Andreas Balevik <andreas.balevik@nav.no>
2026-03-05 12:02:11 +0100

    renaming

--- apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/EtteroppgjoerForbehandlingOversikt.tsx
+++ apps/etterlatte-saksbehandling-ui/client/src/components/etteroppgjoer/components/EtteroppgjoerForbehandlingOversikt.tsx
@@ -94,7 +94,7 @@ export function EtteroppgjoerOversikt({ kontekst }: Props) {
   const erRevurdering = kontekst.type === EtteroppgjoerKontekstType.REVURDERING
   const erRedigerbar = erRedigerbart(kontekst, forbehandling, innloggetSaksbehandler.skriveEnheter)
   const erRedigerbartFaktiskInntekt = kanRedigereFaktiskInntekt(erRedigerbar, kontekst, forbehandling)
-  const revurderingSkalAvsluttes = erRevurdering && forbehandling.endringErTilUgunstForBruker === JaNei.JA
+  const forbehandlingSkalAvsluttes = erRevurdering && forbehandling.endringErTilUgunstForBruker === JaNei.JA
 
   const visBeregnetResultat = erRevurdering
     ? forbehandling.endringErTilUgunstForBruker !== JaNei.JA
@@ -138,14 +138,14 @@ export function EtteroppgjoerOversikt({ kontekst }: Props) {
         />
       )}
 
-      {forbehandling.harVedtakAvTypeOpphoer && !revurderingSkalAvsluttes && (
+      {forbehandling.harVedtakAvTypeOpphoer && (
         <OpphoerSkyldesDoedsfall
           erRedigerbar={erRedigerbar}
           setOpphoerSkyldesDoedsfallSkjemaErrors={setOpphoerDoedsfallErrors}
         />
       )}
 
-      {!doedsfallIEtteroppgjoersaaret && !revurderingSkalAvsluttes && (
+      {!doedsfallIEtteroppgjoersaaret && !forbehandlingSkalAvsluttes && (
         <FastsettFaktiskInntekt
           erRedigerbar={erRedigerbartFaktiskInntekt}
           setFastsettFaktiskInntektSkjemaErrors={setFaktiskInntektErrors}

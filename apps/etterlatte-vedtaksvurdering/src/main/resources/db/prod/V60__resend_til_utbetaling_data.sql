f08248f9a191762c2540812a611bf0998accfae1
Hanne Olsen <hanne.olsen@nav.no>
2026-04-15 14:29:01 +0200

    Sletter utbetalingshendelse i samme slengen

--- apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V60__resend_til_utbetaling_data.sql
+++ apps/etterlatte-vedtaksvurdering/src/main/resources/db/prod/V60__resend_til_utbetaling_data.sql
@@ -3,3 +3,4 @@
 -- Slett utbetalingen som har feilet, slik at den kan opprettes på nytt.
 INSERT INTO resend_til_utbetaling (behandling_id) VALUES ('75d3a456-5728-445c-a836-e2c7be37010d');
 DELETE FROM utbetaling WHERE id = 'ff5cb526-cbb8-43b2-abbf-9c4538fb7307' AND vedtak_id = '68905';
+DELETE FROM utbetalingshendelse WHERE id = '653463da-cd89-4433-a2fd-cc5295323c65' AND status = 'MOTTATT';

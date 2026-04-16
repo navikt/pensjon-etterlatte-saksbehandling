fe8cf00eae728edc16a473ce2af508190d57029f
Hanne Olsen <hanne.olsen@nav.no>
2026-04-16 12:33:56 +0200

    Revert "Legg til resend av vedtak til utbetaling via Flyway-drevet kø (#8510)"
    This reverts commit 2f0af0025d350386afffa26b9064bda15605d4b9.

--- apps/etterlatte-vedtaksvurdering/src/main/resources/db/migration/V59__resend_til_utbetaling.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE resend_til_utbetaling
-(
-    behandling_id UUID                      NOT NULL PRIMARY KEY,
-    opprettet     TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT NOW(),
-    prosessert    BOOLEAN                   NOT NULL DEFAULT FALSE
-);

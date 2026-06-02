8201bf6ef259c21628f422ed4a1dbfecfe043e9c
Trond Valen <trond.valen@nav.no>
2026-06-02 13:56:02 +0200

    Baserer utvalg på behandling-id.

--- apps/etterlatte-behandling/src/main/resources/db/prod/V341__tilbakestill_eo_behandling_for_ny_attestering.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V341__tilbakestill_eo_behandling_for_ny_attestering.sql
@@ -7,6 +7,6 @@ update behandling set status = 'RETURNERT' where sak_id = 12912 and id = 'd95be9
 update behandling set status = 'RETURNERT' where sak_id = 19447 and id = '9987dcf3-61ea-4db8-87a2-7727f06aa75a';
 update behandling set status = 'RETURNERT' where sak_id = 19425 and id = '145cb975-c6b9-4138-9fd9-b59a359a6065';
 
-update vedtak set vedtakstatus = 'RETURNERT' where vedtak.sakid = 12912 and id = 66537;
-update vedtak set vedtakstatus = 'RETURNERT' where vedtak.sakid = 19425 and id = 67311;
-update vedtak set vedtakstatus = 'RETURNERT' where vedtak.sakid = 19447 and id = 66628;
+update vedtak set vedtakstatus = 'RETURNERT' where sakid = 12912 and behandlingid = 'd95be9fc-3acb-462b-96a6-91bac22d2fea';
+update vedtak set vedtakstatus = 'RETURNERT' where sakid = 19447 and behandlingid = '9987dcf3-61ea-4db8-87a2-7727f06aa75a';
+update vedtak set vedtakstatus = 'RETURNERT' where sakid = 19425 and behandlingid = '145cb975-c6b9-4138-9fd9-b59a359a6065';

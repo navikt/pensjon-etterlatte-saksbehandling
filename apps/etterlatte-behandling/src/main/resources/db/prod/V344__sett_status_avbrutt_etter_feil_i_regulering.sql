31ffe49d4a1636e7d15fb1b290fc27ccc3cab3bb
Hanne Olsen <hanne.olsen@nav.no>
2026-06-11 10:54:09 +0200

    Fjerner komma

--- apps/etterlatte-behandling/src/main/resources/db/prod/V344__sett_status_avbrutt_etter_feil_i_regulering.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V344__sett_status_avbrutt_etter_feil_i_regulering.sql
@@ -5,7 +5,7 @@ update behandling set status = 'AVBRUTT'
     and id ='395c4864-4b04-44c1-9daa-83fe2b8b6c38';
 
 update vedtak
-set vedtakstatus = 'RETURNERT',
+set vedtakstatus = 'RETURNERT'
 where sakid = 12995
 and behandlingid = '395c4864-4b04-44c1-9daa-83fe2b8b6c38';
 

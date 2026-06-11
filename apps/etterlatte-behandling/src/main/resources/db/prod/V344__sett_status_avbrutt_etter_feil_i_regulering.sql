c265461954834b324a77d62dd8503a59b570017e
Hanne Olsen <hanne.olsen@nav.no>
2026-06-11 12:47:48 +0200

    Legger på kommentar og årsak på hvorfor behandlingen avbrytes

--- apps/etterlatte-behandling/src/main/resources/db/prod/V344__sett_status_avbrutt_etter_feil_i_regulering.sql
+++ apps/etterlatte-behandling/src/main/resources/db/prod/V344__sett_status_avbrutt_etter_feil_i_regulering.sql
@@ -1,5 +1,8 @@
 
-update behandling set status = 'AVBRUTT'
+update behandling
+    set status = 'AVBRUTT',
+        aarsak_til_avbrytelse = 'AVBRUTT_PAA_GRUNN_AV_FEIL',
+        kommentar_til_avbrytelse = 'Avbrutt på grunn av feil i regulering'
     where status = 'ATTESTERT'
     and sak_id = 12995
     and id = '395c4864-4b04-44c1-9daa-83fe2b8b6c38';

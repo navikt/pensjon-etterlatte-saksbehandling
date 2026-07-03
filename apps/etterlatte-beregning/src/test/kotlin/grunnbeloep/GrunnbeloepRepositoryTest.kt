25cfd0e76232100ee272dcc8aadb30dd73e8b8c8
Trond Valen <trond.valen@nav.no>
2026-07-03 11:04:08 +0200

    Fjerner grunnbeløp per måned (#8626)
    * Fjerner grunnbeløp per måned, men beholder historikken i grunnbelop.json

--- apps/etterlatte-beregning/src/test/kotlin/grunnbeloep/GrunnbeloepRepositoryTest.kt
+++ apps/etterlatte-beregning/src/test/kotlin/grunnbeloep/GrunnbeloepRepositoryTest.kt
@@ -15,20 +15,17 @@ internal class GrunnbeloepRepositoryTest {
     fun `skal sjekke at verdier er riktig for et gitt grunnbeloep`() {
         val grunnbeloep = GrunnbeloepRepository.historiskeGrunnbeloep.first { it.dato == YearMonth.of(2022, 5) }
         grunnbeloep.grunnbeloep shouldBe 111477
-        grunnbeloep.grunnbeloepPerMaaned shouldBe 9290
     }
 
     @Test
     fun `skal hente grunnbeloep for en gitt periode - start maaned`() {
         val grunnbeloep = GrunnbeloepRepository.hentGjeldendeGrunnbeloep(YearMonth.of(2021, 5))
         grunnbeloep.grunnbeloep shouldBe 106399
-        grunnbeloep.grunnbeloepPerMaaned shouldBe 8867
     }
 
     @Test
     fun `skal hente grunnbeloep for en gitt periode - siste maaned`() {
         val grunnbeloep = GrunnbeloepRepository.hentGjeldendeGrunnbeloep(YearMonth.of(2022, 4))
         grunnbeloep.grunnbeloep shouldBe 106399
-        grunnbeloep.grunnbeloepPerMaaned shouldBe 8867
     }
 }

56f1451ee55936e268a0ea8d044a4dafb989d957
krikof <kristian.kofoed@nav.no>
2026-07-07 13:59:28 +0200

    Legger til grants for db migrering. Gjør det mulig for service bruker å select, update og insert før migrering. (#8772)

---
+++ apps/etterlatte-trygdetid/src/main/resources/db/gcp/V27__grant_select_trygdetid.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA public TO "cloudsqliamserviceaccount";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "cloudsqliamserviceaccount";
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO "cloudsqliamserviceaccount";

171d4618810766c57458b21190c914d3f0ba395b
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-07-10 12:36:42 +0200

    Legger tilbake etteroppgjør endring til ugunst

--- apps/etterlatte-saksbehandling-ui/client/src/shared/types/AnnullerBehandling.ts
+++ apps/etterlatte-saksbehandling-ui/client/src/shared/types/AnnullerBehandling.ts
@@ -7,6 +7,7 @@ export enum AarsakTilAvslutting {
   SOEKNAD_TRUKKET = 'SOEKNAD_TRUKKET',
   SAKEN_HENLEGGES_ELLER_SOEKNADEN_ER_IKKE_AKTUELL = 'SAKEN_HENLEGGES_ELLER_SOEKNADEN_ER_IKKE_AKTUELL',
   AVSLUTTE_SAK_VENTER_SVAR_FRA_UTLAND = 'AVSLUTTE_SAK_VENTER_SVAR_FRA_UTLAND',
+  ETTEROPPGJOER_ENDRING_ER_TIL_UGUNST = 'ETTEROPPGJOER_ENDRING_ER_TIL_UGUNST',
   ANNET = 'ANNET',
 }
 
@@ -14,5 +15,6 @@ export const teksterAArsakTilAvslutting: Record<AarsakTilAvslutting, string> = {
   SOEKNAD_TRUKKET: 'Søknad trukket',
   SAKEN_HENLEGGES_ELLER_SOEKNADEN_ER_IKKE_AKTUELL: 'Saken henlegges / søknaden er ikke aktuell',
   AVSLUTTE_SAK_VENTER_SVAR_FRA_UTLAND: 'Avslutt sak i påvente av opplysninger fra utlandet',
+  ETTEROPPGJOER_ENDRING_ER_TIL_UGUNST: 'Endring i etteroppgjør er til ugunst',
   ANNET: 'Annet',
 }

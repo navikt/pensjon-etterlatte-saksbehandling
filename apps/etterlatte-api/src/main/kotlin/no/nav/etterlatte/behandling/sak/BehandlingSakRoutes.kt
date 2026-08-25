0db5c80d5ff3e2158cf48d17d19e4efdd46ec55e
krikof <kristian.kofoed@nav.no>
2026-08-25 13:50:10 +0200

    Revert "I forrige endring med ad grupper ble det unlatt å faktisk fjerne roll…" (#8869)
    This reverts commit 3df693ee124c0537042593e42850e2e756c4d5c6.

--- apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/behandling/sak/BehandlingSakRoutes.kt
+++ apps/etterlatte-api/src/main/kotlin/no/nav/etterlatte/behandling/sak/BehandlingSakRoutes.kt
@@ -19,6 +19,7 @@ import no.nav.etterlatte.libs.ktor.token.Issuer
 
 private fun generateGjennyRoller(config: Config): Set<String> =
     setOf(
+        config.getString("roller.pensjon-saksbehandler"),
         config.getString("roller.gjenny-saksbehandler"),
     )
 

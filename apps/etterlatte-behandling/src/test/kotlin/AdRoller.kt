2148561629e5797b7270b610d84a7388fd9ab550
krikof <kristian.kofoed@nav.no>
2026-08-25 13:50:19 +0200

    Revert "Fjerner pensjon relaterte ad grupper fra app-porteføljen (#8859)"
    This reverts commit 467e2d4397d23be982598fa9bfecb6faaf010700.

--- apps/etterlatte-behandling/src/test/kotlin/AdRoller.kt
+++ apps/etterlatte-behandling/src/test/kotlin/AdRoller.kt
@@ -8,10 +8,26 @@ val azureAdFortroligClaim: String by lazy {
     "2e1dc582-f762-4510-a660-88bf68fb7128"
 }
 
+val azureAdSaksbehandlerClaim: String by lazy {
+    "8bb9b8d1-f46a-4ade-8ee8-5895eccdf8cf"
+}
+
+val azureAdAttestantClaim: String by lazy {
+    "63f46f74-84a8-4d1c-87a8-78532ab3ae60"
+}
+
 val azureAdAttestantGjennyClaim: String by lazy {
     "70cfce24-7865-4676-9fdc-b676e90bfc92"
 }
 
 val azureAdEgenAnsattClaim: String by lazy {
     "dbe4ad45-320b-4e9a-aaa1-73cca4ee124d"
+}
+
+val azureAdNasjonUtenLoggClaim: String by lazy {
+    "ea7411eb-8b48-41a0-bc56-7b521fbf0c25"
+}
+
+val azureAdNasjonMedLoggClaim: String by lazy {
+    "753805ea-65a7-4855-bdc3-e6130348df9f"
 }
